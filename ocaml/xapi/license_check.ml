70004bcce4136f923b2728e2a8c8c31baf12934b
David Scott <david.scott@xensource.com>
2009-08-18 18:18:47 +0100

    CA-31546: fix deadlock in reboot/shutdown handling code
    The code has now been untangled and is structured as follows:
    
    -----------------------------------------------------------+
    Reboot                      | Shutdown                     |
    in-guest       | in-dom0    | in-guest      | in-dom0      |
    -----------------------------------------------------------+
         actions_after_{reboot,shutdown} for Egenera           |
    -----------------------------------------------------------+
      separate queueing for API vs internally-generated        |
    -----------------------------------------------------------+
                          locking                              |
    -----------------------------------------------------------+
    
    The in-guest bit is treated separately from the in-dom0 bit because the former blocks for ages and is very parallel unlike the latter. Queueing and locking is added as close to the entrypoints as possible -- rather than in the guts of the thing -- to make it more obviously fine and not prone to deadlocks.

--- ocaml/xapi/license_check.ml
+++ ocaml/xapi/license_check.ml
@@ -2,9 +2,11 @@ module L = Debug.Debugger(struct let name="license" end)
 open Vmopshelpers
 open Stringext
 
-
-let with_vm_license_check ~__context vm f =
+let vm ~__context vm =
   (* Here we check that the license is still valid - this should be the only place where this happens *)
-  if not (License.license_valid ()) then raise (Api_errors.Server_error (Api_errors.license_expired, []));
+  if not (License.license_valid ()) then raise (Api_errors.Server_error (Api_errors.license_expired, []))
 
+(* XXX: why use a "with_" style function here? *)
+let with_vm_license_check ~__context v f =
+  vm ~__context v;
   f()
