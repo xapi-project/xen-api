00b9be6a32c90c9029299f6fcd29a1d20030101d
Jonathan Knowles <jonathan.knowles@eu.citrix.com>
2009-09-01 14:01:20 +0100

    [DMC] Allowing set-memory-dynamic-range for domain zero.

--- ocaml/xapi/xapi_vm_lifecycle.ml
+++ ocaml/xapi/xapi_vm_lifecycle.ml
@@ -211,6 +211,7 @@ let check_operation_error ~vmr ~vmgmr ~ref ~clone_suspended_vm_enabled ~op =
 		&& op <> `changing_memory_live
 		&& op <> `awaiting_memory_live
 		&& op <> `metadata_export
+		&& op <> `changing_dynamic_range
 	then Some (Api_errors.operation_not_allowed, ["Operations on domain 0 are not allowed"])
 
 	(* check PV drivers constraints if needed *)
