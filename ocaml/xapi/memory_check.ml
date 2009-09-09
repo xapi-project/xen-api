bc5942520449ffee2067a31b2c3b833fb791c026
Jonathan Knowles <jonathan.knowles@eu.citrix.com>
2009-09-09 18:06:03 +0100

    Reformatting functions in line with OCaml best practices guide.

--- ocaml/xapi/memory_check.ml
+++ ocaml/xapi/memory_check.ml
@@ -39,38 +39,40 @@ let vm_compute_required_memory vm_record guest_memory_kib =
 		then vm_compute_required_memory_for_hvm vm_record guest_memory_kib
 		else vm_compute_required_memory_for_pv  vm_record guest_memory_kib
 
-(** Different users will wish to use a different VM accounting policy, depending on how conservative they are *)
+(** Different users will wish to use a different VM accounting policy, depending
+on how conservative or liberal they are. *)
 type accounting_policy = 
 	| Static_max
 		(** use static_max: super conservative, useful for HA *)
 	| Dynamic_target
 		(** upper-bound on the VMs dynamic memory usage from the master's PoV *)
 
-(** Calculates the amount of system memory required in both 'normal' and 'shadow'  *)
-(** memory, to start a VM. If the given VM is a PV guest and if memory ballooning  *)
-(** is enabled, this function returns values derived from the VM's dynamic memory  *)
-(** target (since PV guests are able to start in a pre-ballooned state). If memory *)
-(** ballooning is not enabled or if the VM is an HVM guest, this function returns  *)
-(** values derived from the VM's static memory maximum (since currently HVM guests *)
-(** are not able to start in a pre-ballooned state).                               *)
+(** Calculates the amount of memory required in both 'normal' and 'shadow'
+memory, to start a VM. If the given VM is a PV guest and if memory ballooning
+is enabled, this function returns values derived from the VM's dynamic memory
+target (since PV guests are able to start in a pre-ballooned state). If memory
+ballooning is not enabled or if the VM is an HVM guest, this function returns
+values derived from the VM's static memory maximum (since currently HVM guests
+are not able to start in a pre-ballooned state). *)
 let vm_compute_start_memory ~__context ?(policy=Dynamic_target) vm_record =
 	if Xapi_fist.disable_memory_checks () then (0L, 0L) else
 	let memory_static_max = vm_record.API.vM_memory_static_max in
-	let ballooning_enabled = Helpers.ballooning_enabled_for_vm ~__context vm_record in
+	let ballooning_enabled =
+		Helpers.ballooning_enabled_for_vm ~__context vm_record in
 	let memory_dynamic_min = vm_record.API.vM_memory_dynamic_min in
-	let memory_required = if ballooning_enabled && policy = Dynamic_target then memory_dynamic_min else memory_static_max in
-	vm_compute_required_memory vm_record (Memory.kib_of_bytes_used memory_required)
+	let memory_required =
+		if ballooning_enabled && policy = Dynamic_target
+		then memory_dynamic_min
+		else memory_static_max in
+	vm_compute_required_memory vm_record
+		(Memory.kib_of_bytes_used memory_required)
 
-(** Calculates the amount of system memory required in both 'normal' and *)
-(** 'shadow' memory, for a running VM. If the VM is currently subject to *)
-(** a memory balloon operation, this function returns the maximum amount *)
-(** of memory that the VM will need between now, and the point in future *)
-(** time when the operation completes.                                   *)
+(** Calculates the amount of memory required in both 'normal' and 'shadow'
+memory, for a running VM. If the VM is currently subject to a memory balloon
+operation, this function returns the maximum amount of memory that the VM will
+need between now, and the point in future time when the operation completes. *)
 let vm_compute_used_memory ~__context policy vm_ref =
 	if Xapi_fist.disable_memory_checks () then (0L, 0L) else
-	(* Fetch records relating to the current memory state of the given VM:                   *)
-	(*   + the VM main record holds current values of memory_{dynamic_{min,max},target};     *)
-	(*   + the VM boot record holds current values of memory_static_{min,max};               *)
 	let vm_main_record = Db.VM.get_record ~__context ~self:vm_ref in
 	let vm_boot_record = Helpers.get_boot_record ~__context ~self:vm_ref in
 	(* Extract the current memory state from the fetched records: *)
@@ -78,20 +80,24 @@ let vm_compute_used_memory ~__context policy vm_ref =
 	let memory_dynamic_min = vm_main_record.API.vM_memory_dynamic_min in
 	(* When phase proportional ballooning is enabled, we trust that *)
 	(* we will be able to balloon guests down to their dynamic_min. *)
-	let ballooning_enabled = Helpers.ballooning_enabled_for_vm ~__context vm_boot_record in
+	let ballooning_enabled =
+		Helpers.ballooning_enabled_for_vm ~__context vm_boot_record in
 	let memory_required =
 		if ballooning_enabled && policy = Dynamic_target 
 		then memory_dynamic_min
 		else memory_static_max in
-	vm_compute_required_memory vm_boot_record (Memory.kib_of_bytes_used memory_required)
+	vm_compute_required_memory vm_boot_record
+		(Memory.kib_of_bytes_used memory_required)
 
 let vm_compute_resume_memory ~__context vm_ref =
 	if Xapi_fist.disable_memory_checks () then 0L else
 	let vm_boot_record = Helpers.get_boot_record ~__context ~self:vm_ref in
 	let (_, shadow_bytes) = vm_compute_required_memory
 		vm_boot_record vm_boot_record.API.vM_memory_static_max in
-	(* CA-31759: use the live target field for this rather than the LBR to make upgrade easy *)
-	let suspended_memory_usage_bytes = Db.VM.get_memory_target ~__context ~self:vm_ref in
+	(* CA-31759: use the live target field for this *)
+	(* rather than the LBR to make upgrade easy.    *)
+	let suspended_memory_usage_bytes =
+		Db.VM.get_memory_target ~__context ~self:vm_ref in
 	Int64.add suspended_memory_usage_bytes shadow_bytes
 
 let vm_compute_migrate_memory ~__context vm_ref =
@@ -99,7 +105,7 @@ let vm_compute_migrate_memory ~__context vm_ref =
 	let vm_record = Db.VM.get_record ~__context ~self:vm_ref in
 	let (_, shadow_bytes) = vm_compute_required_memory
 		vm_record vm_record.API.vM_memory_static_max in
-	(* Only used when in rolling upgrade mode (from a pre-ballooning product): *)
+	(* Only used when in rolling upgrade mode (from a pre-ballooning product) *)
 	let current_memory_usage_bytes = vm_record.API.vM_memory_static_max in
 	Int64.add current_memory_usage_bytes shadow_bytes
 
