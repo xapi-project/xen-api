09fbfa345da37e5ea1189c8a0477c89160d992fe
Magnus Therning <magnus.therning@eu.citrix.com>
2009-09-15 12:31:41 +0100

    imported patch modify_wlb_to_use_secrets2

--- ocaml/xapi/dbsync_master.ml
+++ ocaml/xapi/dbsync_master.ml
@@ -15,7 +15,7 @@ let create_pool_record ~__context =
 			~default_SR:Ref.null ~suspend_image_SR:Ref.null ~crash_dump_SR:Ref.null ~other_config:[]
 			~ha_enabled:false ~ha_configuration:[] ~ha_statefiles:[]
 			~ha_host_failures_to_tolerate:0L ~ha_plan_exists_for:0L ~ha_allow_overcommit:false ~ha_overcommitted:false ~blobs:[] ~tags:[] ~gui_config:[] 
-			~wlb_url:"" ~wlb_username:"" ~wlb_password:"" ~wlb_enabled:false ~wlb_verify_cert:false
+			~wlb_url:"" ~wlb_username:"" ~wlb_password:Ref.null ~wlb_enabled:false ~wlb_verify_cert:false
 			~redo_log_enabled:false ~redo_log_vdi:Ref.null
 
 let set_master_ip ~__context =
