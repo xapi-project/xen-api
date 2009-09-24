2363d17940e6977bb3734917311035167a5015f5
David Scott <david.scott@xensource.com>
2009-09-24 22:53:28 +0100

    [CAPRI]: add (C)opyright notice and LGPL with static linking exception to .ml and .mli files
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/wlb_reports.ml
+++ ocaml/xapi/wlb_reports.ml
@@ -1,4 +1,17 @@
 (*
+ * Copyright (C) 2006-2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+(*
   This module serves the /wlb_report and /wlb_diagnostics HTTP requests.
   In the former case, we receive some basic parameters (report name, report
   params) and pass those to the WLB server as a SOAP request.  The latter
--- ocaml/xenops/balloon.mli
+++ ocaml/xenops/balloon.mli
@@ -1,3 +1,16 @@
+(*
+ * Copyright (C) 2006-2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
 val parse_proc_xen_balloon : unit -> (string * int64 option) list
 val set_memory_target : xs:Xs.xsh -> Xs.domid -> int64 -> unit
 val _current_allocation : string
