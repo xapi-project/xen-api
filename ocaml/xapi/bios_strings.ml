bde5c66f5765cd1386c71d458a560e89f316fb85
Rob Hoes <rob.hoes@eu.citrix.com>
2009-10-07 18:20:35 +0100

    CP-1380: Bios strings - module to capture and store BIOS strings at a host
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>
    Acked-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/bios_strings.ml
+++ ocaml/xapi/bios_strings.ml
@@ -1,16 +1,3 @@
-(*
- * Copyright (C) 2006-2009 Citrix Systems Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; version 2.1 only. with the special
- * exception on linking described in file LICENSE.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *)
 module D = Debug.Debugger(struct let name="bios_strings" end)
 open D
 open Stringext
@@ -47,26 +34,20 @@ let get_bios_string name =
 		else str
 	with _ -> ""
 
-(* Obtain the Type 11 OEM strings from dmidecode, and prepend with the standard ones. *)
+(* obtain the Type 11 OEM strings from dmidecode *)
 let get_oem_strings () =
-	let standard = Xapi_globs.standard_type11_strings in
 	try
 		let result, _ = Forkhelpers.execute_command_get_output dmidecode_prog [dmidecode_prog; "-t11"; "-q"] in
-		let n = List.length standard in
+		let result = trim (remove_invisible result) in
 		let rec loop index a =
 			try
 				let b = String.index_from result a ':' in
 				let c = String.index_from result b '\n' in
-				let str = "oem-" ^ (string_of_int index) in
-				let value = trim (remove_invisible (String.sub result (b+2) (c-b-2))) in
-				if value <> "" then
-					(str, value) :: loop (index+1) c
-				else
-					loop index c
+				(index, String.sub result (b+2) (c-b-2)) :: loop (index+1) c
 			with _ -> []
 		in
-		standard @ (loop (n+1) 0)
-	with _ -> standard
+		loop 1 0
+	with _ -> []
 
 (* Get the HP-specific ROMBIOS OEM string:
  * 6 bytes from the memory starting at 0xfffea *)
@@ -80,19 +61,31 @@ let get_hp_rombios () =
 		(fun () -> Unix.close mem)
 	with _ -> ()
 	end;
-	if trim (remove_invisible hp_rombios) = "COMPAQ" then "COMPAQ" else ""
+	trim (remove_invisible hp_rombios)
 
 let set_host_bios_strings ~__context ~host =
 	info "Setting host BIOS strings.";
+	(* first clear the current strings *)
+	Db.Host.set_bios_strings ~__context ~self:host ~value:[];
+	
 	(* named BIOS strings *)
+	let fetch_and_add_string str =
+		let value = get_bios_string str in
+		Db.Host.add_to_bios_strings ~__context ~self:host ~key:str ~value:value
+	in
 	let dmidecode_strings = ["bios-vendor"; "bios-version"; "system-manufacturer";
 		"system-product-name"; "system-version"; "system-serial-number"] in
-	let named_strings = List.map (fun str -> str, (get_bios_string str)) dmidecode_strings in
+	List.iter fetch_and_add_string dmidecode_strings;
+	
 	(* type 11 OEM strings *)
 	let oem_strings = get_oem_strings () in
+	let add_oem_string (index, value) =
+		let str = "oem-" ^ (string_of_int index) in
+		Db.Host.add_to_bios_strings ~__context ~self:host ~key:str ~value:value
+	in
+	List.iter add_oem_string oem_strings;
+
 	(* HP-specific ROMBIOS OEM string *)
-	let hp_rombios = ["hp-rombios", get_hp_rombios ()] in
-	(* combine *)
-	let bios_strings = named_strings @ oem_strings @ hp_rombios in
-	Db.Host.set_bios_strings ~__context ~self:host ~value:bios_strings
+	let hp_rombios = get_hp_rombios () in
+	Db.Host.add_to_bios_strings ~__context ~self:host ~key:"hp-rombios" ~value:hp_rombios
 	
