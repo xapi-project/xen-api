b497b11bbcb3968b36af23157493e8c1cfc2a26a
Rob Hoes <rob.hoes@eu.citrix.com>
2009-10-07 18:20:35 +0100

    CP-1381: Bios strings - on VM start, fix the BIOS strings and put them into XenStore
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>
    Acked-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/cli_operations.ml
+++ ocaml/xapi/cli_operations.ml
@@ -3082,7 +3082,7 @@ let vm_is_bios_customized printer rpc session_id params =
   	let bios_strings = Client.VM.get_bios_strings rpc session_id (vm.getref ()) in
   	if List.length bios_strings = 0 then
 	  printer (Cli_printer.PMsg "The BIOS strings of this VM have not yet been set.")
-  	else if List.mem_assoc "bios_vendor" bios_strings && List.assoc "bios_vendor" bios_strings = "Xen" then
+  	else if bios_strings = Bios_strings.generic_bios_strings then
 	  printer (Cli_printer.PMsg "This VM is BIOS-generic.")
 	else
 	  printer (Cli_printer.PMsg "This VM is BIOS-customized.")
