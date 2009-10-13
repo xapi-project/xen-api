39d5fc1bc432c93c5db8ca44226d894925ec19fc
David Scott <dave.scott@eu.citrix.com>
2009-10-13 11:54:16 +0100

    [XENOPS]: pass the number of vcpus to the domain builder via xenstore, like we do with acpi_s3, acpi_s4, pae, acpi, apic etc.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xenguest/xenguest_main.ml
+++ ocaml/xenguest/xenguest_main.ml
@@ -236,11 +236,11 @@ let linux_build_real domid mem_max_mib mem_start_mib image ramdisk cmdline featu
 		                    Nativeint.to_string console_mfn; proto ]
 	)
 
-let hvm_build_real domid mem_max_mib mem_start_mib image vcpus store_port =
+let hvm_build_real domid mem_max_mib mem_start_mib image store_port =
 	with_xenguest (fun xc ->
 		let store_mfn =
 			Xenguest.hvm_build xc domid mem_max_mib mem_start_mib image
-			                   vcpus store_port in
+			                   store_port in
 		Nativeint.to_string store_mfn
 	)
 
@@ -261,14 +261,14 @@ let domain_restore_real fd domid store_port console_port hvm =
 
 (** fake operations *)
 let linux_build_fake domid mem_max_mib mem_start_mib image ramdisk cmdline features flags store_port console_port = "10 10 x86-32"
-let hvm_build_fake domid mem_max_mib mem_start_mib image vcpus store_port = "2901"
+let hvm_build_fake domid mem_max_mib mem_start_mib image store_port = "2901"
 let domain_save_fake fd domid x y flags hvm = Unix.sleep 1; ignore (suspend_callback domid); ""
 let domain_restore_fake fd domid store_port console_port hvm = "10 10"
 
 (** operation vector *)
 type ops = {
 	linux_build: int -> int -> int -> string -> string option -> string -> string -> int -> int -> int -> string;
-	hvm_build: int -> int -> int -> string -> int -> int -> string;
+	hvm_build: int -> int -> int -> string -> int -> string;
 	domain_save: Unix.file_descr -> int -> int -> int -> Xenguest.suspend_flags list -> bool -> string;
 	domain_restore: Unix.file_descr -> int -> int -> int -> bool -> string;
 }
@@ -289,7 +289,6 @@ let _ =
 	add_param "flags" "";
 	add_param "mem_max_mib" "maximum memory allocation / MiB";
 	add_param "mem_start_mib" "initial memory allocation / MiB";
-	add_param "vcpus" "number of HVM VCPUs";
 	add_param "fork" "true to fork a background thread to capture stdout and stderr";
 
 	let fake = ref false in
@@ -401,16 +400,15 @@ let _ =
 		                          cmdline features flags store_port console_port)
 	      | Some "hvm_build" ->
 		  debug "hvm_build mode selected";
-		  require [ "domid"; "mem_max_mib"; "mem_start_mib"; "image"; "vcpus"; "store_port" ];
+		  require [ "domid"; "mem_max_mib"; "mem_start_mib"; "image"; "store_port" ];
 		  let domid = int_of_string (get_param "domid")
 		  and mem_max_mib = int_of_string (get_param "mem_max_mib")
 		  and mem_start_mib = int_of_string (get_param "mem_start_mib")
 		  and image = get_param "image"
-		  and vcpus = int_of_string (get_param "vcpus")
 		  and store_port = int_of_string (get_param "store_port") in
 
 		  with_logging (fun () -> ops.hvm_build domid mem_max_mib mem_start_mib image
-		                          vcpus store_port)
+		                          store_port)
 	      | Some "test" ->
 		  debug "test mode selected";
 		  with_logging (fun () -> ignore(Unix.system "/tmp/test"); "result")
