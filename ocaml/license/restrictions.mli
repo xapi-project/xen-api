11655d86a71dd25f351339d448395f95151fbec4
Rob Hoes <rob.hoes@citrix.com>
2009-10-26 16:47:35 +0000

    [licensing] Move all license-related code to ocaml/v6
    Moved:
    - ocaml/xapi/restrictions.*
    - ocaml/v6/*
    
    NOTE: After pushing this, changes in the scripts in v6.hg, and in the patch
          v6-hook-in-other-repo (lines 35-36) need to be made to reflect the change
          from ocaml/v6 to ocaml/license!
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/license/restrictions.mli
+++ ocaml/license/restrictions.mli
@@ -11,91 +11,61 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-(** Module that controls license entitlements.
- * @group Licensing
- *)
 
-(** Licensing mode. *)
-type sku =
-| Express		(** Express (free) license *)
-| Enterprise	(** Enterprise (paid-for) license *)
+(** Module that controls license entitlements *)
 
-(* the following three functions are used by the CLI *)
+(** Licensing mode *)
+type sku = Express | Enterprise
 
-(** Convert a string to a {!sku}. *)
+(* used by CLI: *)
 val sku_of_string : string -> sku
-
-(** Whether whether the given {!sku} corresponds to the free edition. *)
 val is_floodgate_free : sku -> bool
-
-(** Convert a {!sku} to a cryptic abbreviation. *)
 val obfuscated_string_of_sku : sku -> string
 
-(** Holding the flags that control which features are enabled or not. *)
+(** Holding the flags that control which features are enabled or not *)
 type restrictions = {
-	enable_vlans          : bool; (** not used anymore *)
-	enable_qos            : bool; (** not used anymore *)
-	enable_shared_storage : bool; (** not used anymore; perhaps XenCenter does? *)
-	enable_netapp         : bool; (** used by XenCenter? *)
-	enable_equalogic      : bool; (** used by XenCenter? *)
-	enable_pooling        : bool; (** not used anymore *)
-	enable_xha            : bool; (** enable High Availability (HA) *)
-	enable_mtc_pci        : bool; (** not used anymore *)
-	enable_email          : bool; (** enable email alerting *)
-	enable_performance    : bool; (** used by XenCenter? *)
-	enable_wlb            : bool; (** enable Workload Balancing (WLB) *)
-	enable_rbac           : bool; (** enable Role-Based Access Control (RBAC) *)
-	enable_dmc            : bool; (** enable Dynamic Memory Control (DMC) *)
-	enable_checkpoint     : bool; (** enable Checkpoint *)
-	enable_vswitch_controller : bool; (** enable use of a Distributed VSwitch (DVS) Controller *)
-	enable_cpu_masking    : bool; (** enable masking of CPU features *)
-	restrict_connection   : bool; (** not used anymore; perhaps XenCenter does? *)
-	platform_filter       : bool; (** filter platform data on domain create? *)
-	regular_nag_dialog    : bool; (** used by XenCenter *)
+  enable_vlans : bool;			(** not used anymore *)
+  enable_qos : bool;			(** not used anymore *)
+  enable_shared_storage : bool;	(** not used anymore; perhaps XenCenter does? *)
+  enable_netapp : bool;			(** used by XenCenter? *)
+  enable_equalogic : bool;		(** used by XenCenter? *)
+  enable_pooling : bool;		(** not used anymore *)
+  enable_xha : bool;			(** enable High Availability (HA) *)
+  enable_mtc_pci : bool;		(** not used anymore *)
+  enable_email : bool;			(** enable email alerting *)
+  enable_performance : bool;	(** used by XenCenter? *)
+  enable_wlb : bool;			(** enable Workload Balancing (WLB) *)
+  enable_rbac : bool;			(** enable Role-Based Access Control (RBAC) *)
+  restrict_connection : bool;	(** not used anymore; perhaps XenCenter does? *)
+  platform_filter : bool;		(** filter platform data on domain create? *)
+  regular_nag_dialog : bool;	(** used by XenCenter *)
 }
 
-(** Returns a compact list of the current restrictions. *)
+(** Returns a compact list of the current restrictions *)
 val to_compact_string : restrictions -> string
 
 (** Return the 'pool_restrictions' being the greatest set of permissions allowed by all licenses. *)
 val pool_restrictions_of_list : restrictions list -> restrictions
 
-(** Convert a {!restrictions} value into an association list. *)
 val to_assoc_list : restrictions -> (string * string) list
-
-(** Convert and association list of restictions into a {!restrictions} value. *)
 val of_assoc_list : (string * string) list -> restrictions
 
-(** Get the current restrictions. *)
+(** Get the current restrictions *)
 val get : unit -> restrictions
 
 (** Return cache of pool restrictions, always updated at least once when the master reads its license.
  *  Called on the master to gate some operations. *)
-val get_pool : __context:Context.t -> restrictions
+val get_pool : unit -> restrictions
 
 (* called by xapi_host *)
-(** Called whenever a slave resets its Host.license_params after reading in a license. *)
-val update_pool_restrictions : __context:Context.t -> unit
+(** Called whenever a slave resets its Host.license_params after reading in a license *)
+val update_pool_restrictions : __context:'a -> unit
 
-(** Object the {!restrictions} for a given {!sku}. *)
 val restrictions_of_sku : sku -> restrictions
 
-(** Checks whether we are entitled to enable Workload Balancing (WLB) in the pool. *)
-val license_ok_for_wlb : __context:Context.t -> bool
+(** Checks whether we are entitled to enable Workload Balancing (WLB) in the pool *)
+val license_ok_for_wlb : __context:'a -> bool
 
 (** Checks whether we are entitled to enable Role-Based Access Control (RBAC) in the pool *)
-val license_ok_for_rbac : __context:Context.t -> bool
-
-(** Checks whether we are entitled to enable Dynamic Memory Control (DMC)
-  * in the pool. *)
-val context_ok_for_dmc : __context:Context.t -> bool
-
-(** Checks whether we are entitled to enable checkpoint *)
-val ok_for_checkpoint : __context:Context.t -> bool
-
-(** Checks whether we are entitled to use a VSwitch Controller *)
-val ok_for_vswitch_controller : __context:Context.t -> bool
-
-(** Checks whether we are entitled to mask CPU features *)
-val ok_for_cpu_masking : __context:Context.t -> bool
+val license_ok_for_rbac : __context:'a -> bool
 
--- ocaml/license/v6daemon.ml
+++ ocaml/license/v6daemon.ml
@@ -14,12 +14,9 @@
 
 (* v6 licensing daemon *)
 open Stringext
-open Printf
 
 module D=Debug.Debugger(struct let name="v6daemon" end)
 open D
-
-module W=Debug.Debugger(struct let name="watchdog" end)
 
 let xmlrpc_handler process req bio =
 	let path = match String.split '/' req.Http.uri with
@@ -35,131 +32,6 @@ let xmlrpc_handler process req bio =
 	let str = Xml.to_string result in
 	debug "Response: %s" str;
 	Http_svr.response_str req s str
-	
-
-let daemon_init post_daemonize_hook process =
-	post_daemonize_hook ();
-	
-	(* unix socket *)
-	let unix_socket_path = "/var/xapi/v6" in
-	Unixext.mkdir_safe (Filename.dirname unix_socket_path) 0o700;
-	Unixext.unlink_safe unix_socket_path;
-	let domain_sock = Http_svr.bind (Unix.ADDR_UNIX(unix_socket_path)) in
-	ignore(Http_svr.start (domain_sock, "unix-RPC"));
-	Http_svr.add_handler Http.Post "/" (Http_svr.BufIO (xmlrpc_handler process));
-
-	(* TCP socket: only use for testing! *)
-(*	let localhost = Unix.inet_addr_of_string "127.0.0.1" in
-	let localhost_sock = Http_svr.bind (Unix.ADDR_INET(localhost, 4094)) in
-	Unix.setsockopt localhost_sock Unix.SO_REUSEADDR true;
-	ignore(Http_svr.start (localhost_sock, "inet-RPC"));*)
-
-	(* keep daemon alive *)
-	Threadext.keep_alive ()
-
-
-let watchdog f =
-	(* parent process blocks sigint and forward sigterm to child. *)
-	ignore(Unix.sigprocmask Unix.SIG_BLOCK [Sys.sigint]);
-	Sys.catch_break false;
-	Logs.append "watchdog" Log.Info "syslog:v6d_watchdog";
-
-	(* watchdog logic *)
-	let loginfo fmt = W.info fmt in
-
-	let restart = ref true
-	and error_msg = ref "" and exit_code = ref 0
-	and last_badsig = ref (0.) and pid = ref 0
-	and last_badexit = ref (0.) and no_retry_interval = 10. in
-
-	while !restart
-	do
-		begin
-			loginfo "(Re)starting v6d...";
-			if !pid = 0 then
-				begin
-					let newpid = Unix.fork () in
-					if newpid = 0 then
-						begin
-							try
-								ignore(Unix.sigprocmask Unix.SIG_UNBLOCK [Sys.sigint]);
-								f ();
-								exit 127
-							with e ->
-								error "Caught exception at toplevel: '%s'" (Printexc.to_string e);
-								log_backtrace ();
-								raise e (* will exit the process with rc=2 *)
-						end;
-					(* parent just reset the sighandler *)
-					Sys.set_signal Sys.sigterm (Sys.Signal_handle (fun i -> restart := false; Unix.kill newpid Sys.sigterm));
-					pid := newpid
-				end;
-			try
-				(* remove the pid in all case, except stop *)
-				match snd (Unix.waitpid [] !pid) with
-				| Unix.WEXITED 0 ->
-					loginfo "Received exit code 0. Not restarting.";
-					pid := 0;
-					restart := false;
-					error_msg := "";
-				| Unix.WEXITED i ->
-					loginfo "Received exit code %d" i;
-					exit_code := i;
-					pid := 0;
-					let ctime = Unix.time () in
-					if ctime < (!last_badexit +. no_retry_interval) then
-						begin
-							restart := false;
-							loginfo "Received 2 bad exits within no-retry-interval. Giving up.";
-						end
-					else
-						begin
-							(* restart := true; -- don't need to do this - it's true already *)
-							loginfo "Received bad exit, retrying";
-							last_badexit := ctime
-						end
-				| Unix.WSIGNALED i ->
-					loginfo "Received signal %d" i;
-					pid := 0;
-					(* arbitrary choice of signals, probably need more though, for real use *)
-					if i = Sys.sigsegv || i = Sys.sigpipe then
-						begin
-							let ctime = Unix.time () in
-							if ctime < (!last_badsig +. no_retry_interval) then
-								begin
-									restart := false;
-									error_msg := sprintf "v6d died with signal %d: not restarting (2 bad signals within no_retry_interval)" i;
-									exit_code := 13
-								end
-							else
-								begin
-									loginfo "v6d died with signal %d: restarting" i;
-									last_badsig := ctime
-								end
-						end
-					else
-						begin
-							restart := false;
-							error_msg := sprintf "v6d died with signal %d: not restarting (watchdog never restarts on this signal)" i;
-							exit_code := 12
-						end
-				| Unix.WSTOPPED i ->
-					loginfo "Receive stop code %i" i;
-					Unix.sleep 1;
-					(* well, just resume the stop process. the watchdog cannot do anything if the process is stopped *)
-					Unix.kill !pid Sys.sigcont;
-			with
-			| Unix.Unix_error(Unix.EINTR,_,_) -> ()
-			| e -> loginfo "Watchdog received unexpected exception: %s" (Printexc.to_string e)
-		end;
-	done;
-	if !error_msg <> "" then
-		begin
-			loginfo "v6d watchdog exiting.";
-			loginfo "Fatal: %s" !error_msg;
-			eprintf "%s\n" !error_msg;
-		end;
-	exit !exit_code		
 
 
 let daemon = ref false
@@ -179,5 +51,22 @@ let startup post_daemonize_hook process =
 	if !pidfile <> "" then
 		Unixext.pidfile_write !pidfile;
 
-	watchdog (fun () -> daemon_init post_daemonize_hook process)
+	post_daemonize_hook ();
+	
+	(* unix socket *)
+	let unix_socket_path = "/var/xapi/v6" in
+	Unixext.mkdir_safe (Filename.dirname unix_socket_path) 0o700;
+	Unixext.unlink_safe unix_socket_path;
+	let domain_sock = Http_svr.bind (Unix.ADDR_UNIX(unix_socket_path)) in
+	ignore(Http_svr.start (domain_sock, "unix-RPC"));
+	Http_svr.add_handler Http.Post "/" (Http_svr.BufIO (xmlrpc_handler process));
+
+	(* TCP socket: only use for testing! *)
+(*	let localhost = Unix.inet_addr_of_string "127.0.0.1" in
+	let localhost_sock = Http_svr.bind (Unix.ADDR_INET(localhost, 4094)) in
+	Unix.setsockopt localhost_sock Unix.SO_REUSEADDR true;
+	ignore(Http_svr.start (localhost_sock, "inet-RPC"));*)
+
+	(* keep daemon alive *)
+	Threadext.keep_alive ()
 	
