95ca0f680143d177d4aa0c0e15c738bc37c9b5fc
Rob Hoes <rob.hoes@citrix.com>
2009-10-26 16:47:34 +0000

    [ocamldoc] Added license headers
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/doc/ocamldoc.js
+++ ocaml/doc/ocamldoc.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2006-2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
 // function from http://www.bloggingdeveloper.com/post/JavaScript-QueryString-ParseGet-QueryString-with-Client-Side-JavaScript.aspx
 function getQuerystring(key, default_)
 {
