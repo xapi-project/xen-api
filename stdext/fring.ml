faa0a01ad351ceb3f5728d4265ca8ae3d19e0d6e
Rob Hoes <rob.hoes@citrix.com>
2009-10-28 17:06:00 +0000

    [ocamldoc] Build-in Ocamldoc support
    Typing 'make doc' will generate documentation for the libraries in xen-api-libs. The documentation is integrated in the xen-api documentation (the custom ocamldoc generator in xen-api.hg is used). This only works when xen-api.hg is present in myrepos and 'make doc' has been executed in it.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- stdext/fring.ml
+++ stdext/fring.ml
@@ -14,7 +14,6 @@
 
 type t = { size: int; mutable current: int; data: (float,Bigarray.float32_elt, Bigarray.c_layout) Bigarray.Array1.t ; }
 
-(** create a ring structure with @size record. records inited to @initval *)
 let make size init =
 	let ring = 
 		{ size = size; current = size - 1; data = Bigarray.Array1.create Bigarray.float32 Bigarray.c_layout size; }
@@ -24,17 +23,14 @@ let make size init =
 	done;
 	ring
 
-(** length of the ring *)
 let length ring = ring.size
 
-(** push into the ring one element *)
 let push ring e =
 	ring.current <- ring.current + 1;
 	if ring.current = ring.size then
 		ring.current <- 0;
 	Bigarray.Array1.set ring.data ring.current e
 
-(** get the @ith old element from the ring *)
 let peek ring i =
 	if i >= ring.size then
 		raise (Invalid_argument "peek: index");
@@ -43,10 +39,8 @@ let peek ring i =
 		if offset >= 0 then offset else ring.size + offset in
 	Bigarray.Array1.get ring.data index
 
-(** get the top element of the ring *)
 let top ring = Bigarray.Array1.get ring.data ring.current
 
-(** iterate over nb element of the ring, starting from the top *)
 let iter_nb ring f nb =
 	if nb > ring.size then
 		raise (Invalid_argument "iter_nb: nb");
@@ -56,7 +50,7 @@ let iter_nb ring f nb =
 		f (peek ring i)
 	done
 
-(** iter directly on all element without using the index *)
+(* iter directly on all element without using the index *)
 let iter f a = 
 	for i=0 to Bigarray.Array1.dim a - 1 do
 		f (Bigarray.Array1.get a i)
@@ -65,10 +59,8 @@ let iter f a =
 let raw_iter ring f =
 	iter f ring.data
 
-(** iterate over all element of the ring, starting from the top *)
 let iter ring f = iter_nb ring f (ring.size)
 
-(** get array of latest #nb value *)
 let get_nb ring nb =
 	if nb > ring.size then
 		raise (Invalid_argument "get_nb: nb");
@@ -81,3 +73,4 @@ let get_nb ring nb =
 	a
 
 let get ring = get_nb ring (ring.size)
+
