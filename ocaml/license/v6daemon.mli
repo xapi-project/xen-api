8de1f6d8d99a5453c8d6aa9b8d8e96d894af946a
Rob Hoes <rob.hoes@citrix.com>
2009-11-03 12:56:58 +0000

    [ocamldoc] Additional documentation for licensing modules
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/license/v6daemon.mli
+++ ocaml/license/v6daemon.mli
@@ -12,13 +12,7 @@
  * GNU Lesser General Public License for more details.
  *)
 
-(** An example implementation of a licensing service which always returns "real" 
-    licenses that never expire. *)
+(** Licensing daemon creation module *)
 
-val initialise : string -> int32 -> string -> string * int32
-(** Obtain a license *)
-val shutdown : unit -> bool
-(** Release the license *)
-val reopen_logs : unit -> bool
-(** Close and re-open the log file *)
-
+(** Create and start up the licensing daemon *)
+val startup : (unit -> 'a) -> (Xml.xml -> Xml.xml) -> unit
