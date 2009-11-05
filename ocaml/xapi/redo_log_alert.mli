e2bf1fa6f372919f105eff2c81e812a4c3b64169
David Scott <dave.scott@eu.citrix.com>
2009-11-05 10:06:28 +0000

    Add a trivial interface hiding the implementation of the redo_log_alert module.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

---
+++ ocaml/xapi/redo_log_alert.mli
@@ -0,0 +1,17 @@
+(*
+ * Copyright (C) 2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+(** Runs forever waiting for the redo log's status to change i.e. for
+	it to fail or to recover, generating alerts on transitions.*)
+val loop: unit -> unit
