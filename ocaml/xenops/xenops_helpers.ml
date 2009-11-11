41ea9843f6ae09e7576c31216685f7acc0051fdf
Jonathan Knowles <jonathan.knowles@xxxxxxxxxxxxx>
2009-11-11 11:02:01 +0000

    Moves low-level functions with_{xal,xc,xs,xc_and_xs,xs_and_xs_final} from the Xapi layer into the Xenops layer, allowing greater reuse. Signed-off-by: Jonathan Knowles <jonathan.knowles@xxxxxxxxxxxxx>

--- ocaml/xenops/xenops_helpers.ml
+++ ocaml/xenops/xenops_helpers.ml
@@ -13,27 +13,20 @@
  *)
 open Pervasiveext
 
-module D = Debug.Debugger(struct let name="xapi" end)
-open D
-
 (** {2 XC, XS and XAL interface helpers.} *)
 
-open Xenops_helpers
+let with_xc f = Xc.with_intf f
 
-let with_xc              = with_xc
-let with_xs              = with_xs
-let with_xal             = with_xal
-let with_xc_and_xs       = with_xc_and_xs
-let with_xc_and_xs_final = with_xc_and_xs_final
+let with_xs f =
+	let xs = Xs.daemon_open () in
+	finally (fun () -> f xs) (fun () -> Xs.close xs)
 
-exception Vm_corresponding_to_domid_not_in_db of int
-let uuid_of_domid domid =
-  Uuid.to_string (with_xc (fun xc -> Domain.get_uuid xc domid))
+let with_xal f =
+	let xal = Xal.init () in
+	finally (fun () -> f xal) (fun () -> Xal.close xal)
 
-let vm_of_domid ~__context domid =
-	try
-		let uuid = Uuid.to_string (with_xc (fun xc -> Domain.get_uuid xc domid)) in
-		Db.VM.get_by_uuid ~__context ~uuid
-	with Xc.Error _
-		-> raise (Vm_corresponding_to_domid_not_in_db domid)
+let with_xc_and_xs f =
+	Xc.with_intf (fun xc -> with_xs (fun xs -> f xc xs))
 
+let with_xc_and_xs_final f cf =
+	with_xc_and_xs (fun xc xs -> finally (fun () -> f xc xs) cf)
