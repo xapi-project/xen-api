1705406b1ff915d1b3e0b9acd5eccbd271d0b54a
Rob Hoes <rob.hoes@citrix.com>
2009-12-11 11:54:19 +0000

    CA-35467: Periodically probe license server after getting grace license
    After obtaining a grace license, call Host.apply_edition every hour until a real license comes out.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/license/grace_retry.ml
+++ ocaml/license/grace_retry.ml
@@ -13,11 +13,6 @@
  *)
 (** Helper to keep trying to get a "real" license after a "grace" license was checked out.
  *  @group Licensing *)
- 
-open Client
-
-let event_name = "retry after obtaining grace license"
-let schedule = Xapi_periodic_scheduler.OneShot
 
 (** Schedule a timer to call [Host.apply_edition] again after an hour. Call this
  *  after getting a "grace" license in order to check whether the license server
@@ -25,31 +20,13 @@ let schedule = Xapi_periodic_scheduler.OneShot
  *  Note: the LPE already does a similar thing, but does not notify the product (xapi)
  *  if it succeeds to check out a "real" license! *)
 let retry_periodically host edition =
-	let period =
-		if Xapi_fist.reduce_grace_retry_period () then
-			300.	(* 5min *)
-		else
-			3600.	(* 1h *)
+	let period = 3600. in
+	let schedule = Xapi_periodic_scheduler.OneShot in
+	let retry_fn () = Server_helpers.exec_with_new_task "grace_retry"
+		(fun __context ->
+			Helpers.call_api_functions ~__context
+			(fun rpc session_id -> Client.Client.Host.apply_edition rpc session_id host edition)
+		)
 	in
-	let retry_fn () = 
-		let now = (Unix.gettimeofday ()) in
-		Server_helpers.exec_with_new_task "grace_retry"
-			(fun __context ->
-				Helpers.call_api_functions ~__context (fun rpc session_id ->
-					(* Retry checkout *)
-					Client.Host.apply_edition rpc session_id host edition;
-					(* Remove any newly generated grace alerts *)
-					let alerts = Client.Message.get_since rpc session_id (Date.of_float now) in
-					let check_and_maybe_remove (ref, msg) =
-						if msg.API.message_name = "GRACE_LICENSE" then
-							Helpers.call_api_functions ~__context
-								(fun rpc session_id -> Client.Message.destroy rpc session_id ref)
-					in
-					List.iter check_and_maybe_remove alerts
-				)
-			)
-	in
-	Xapi_periodic_scheduler.add_to_queue event_name schedule period retry_fn
+	Xapi_periodic_scheduler.add_to_queue "retry after obtaining grace license" schedule period retry_fn
 	
-let cancel () =
-	Xapi_periodic_scheduler.remove_from_queue event_name
