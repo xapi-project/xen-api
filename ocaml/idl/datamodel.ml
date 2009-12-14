4585849b58e7bafbf8d0e57a58654a950455dbe0
Thomas Gazagnaire <thomas.gazagnaire@citrix.com>
2009-12-14 17:39:22 +0000

    CA-34933: link sessions and tasks in the datamodel in order to know whether a session is used.
    Signed-off-by: Thomas Gazagnaire <thomas.gazagnaire@citrix.com>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -2794,6 +2794,7 @@ let session =
 		  field ~in_product_since:rel_george ~qualifier:DynamicRO ~default_value:(Some(VDateTime(Date.of_float 0.))) ~ty:DateTime "validation_time" "time when session was last validated";
 		  field ~in_product_since:rel_george ~qualifier:DynamicRO ~default_value:(Some(VString(""))) ~ty:String "auth_user_sid" "the subject identifier of the user that was externally authenticated. If a session instance has is_local_superuser set, then the value of this field is undefined.";
 		  field ~in_product_since:rel_midnight_ride ~qualifier:StaticRO ~default_value:(Some(VSet [])) ~ty:(Set(String)) "rbac_permissions" "list with all RBAC permissions for this session";
+          field ~in_product_since:rel_midnight_ride ~qualifier:DynamicRO ~ty:(Set(Ref _task)) "tasks" "list of tasks created using the current session";
 		]
 	()
 
@@ -5853,6 +5854,7 @@ let all_relations =
 
     (* subtasks hierarchy *)
     (_task, "subtask_of"), (_task, "subtasks");
+    (_task, "session"), (_session, "tasks");
     
     (_pif, "bond_slave_of"), (_bond, "slaves");
     (_bond, "master"), (_pif, "bond_master_of");
