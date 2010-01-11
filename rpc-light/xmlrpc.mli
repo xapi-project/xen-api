14d0a409773465b1a48fbba1666a6941fed4a355
Thomas Gazagnaire <thomas.gazagnaire@eu.citrix.com>
2010-01-11 17:44:38 +0000

    [rpc-light] Add a function to marshal and unmarshal XMLRPC to a bigbuffer
    Signed-off-by: Thomas Gazagnaire <thomas.gazagnaire@citrix.com>

--- rpc-light/xmlrpc.mli
+++ rpc-light/xmlrpc.mli
@@ -15,10 +15,14 @@
 val to_string : Rpc.t -> string
 val of_string : ?callback:Rpc.callback -> string -> Rpc.t
 
+val to_a : empty:(unit -> 'a) -> append:('a -> string -> unit) -> Rpc.t -> 'a
+val of_a : ?callback:Rpc.callback -> next_char:('a  -> char) -> 'a -> Rpc.t
+
 val string_of_call: Rpc.call -> string
 val call_of_string: ?callback:Rpc.callback -> string -> Rpc.call
 
 val string_of_response: Rpc.response -> string
+val a_of_response : empty:(unit -> 'a) -> append:('a -> string -> unit) -> Rpc.response -> 'a
 
 val response_of_string: ?callback:Rpc.callback -> string -> Rpc.response
 val response_of_in_channel: ?callback:Rpc.callback -> in_channel -> Rpc.response
