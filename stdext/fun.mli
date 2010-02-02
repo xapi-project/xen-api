e2bf7a0900be70883e7a0f501ce2d6ae8a09a53f
Matthias Goergens <matthias.goergens@citrix.com>
2010-02-02 15:42:13 +0000

    Module Fun gets operators for function composition and application.
    Exposes function composition (++) and function composition with two
    arguments (+++) to the Fun module of our extended standard library.
    Adds and exposes function application ($).
    
    Signed-off-by: Matthias Goergens <matthias.goergens@citrix.com>

--- stdext/fun.mli
+++ stdext/fun.mli
@@ -4,3 +4,6 @@ val id : 'a -> 'a
 val flip : ('a -> 'b -> 'c) -> ('b -> 'a -> 'c)
 val on : ('b -> 'b -> 'c) -> ('a -> 'b) -> 'a -> 'a -> 'c
 val comp : ('b -> 'c) -> ('a -> 'b) -> ('a -> 'c)
+val (+++) : ('c -> 'd) -> ('a -> 'b -> 'c) -> 'a -> 'b -> 'd
+val (++) : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c
+val ($) : ('a -> 'b) -> 'a -> 'b
