6c112b34b76d5e602b673d95163d901a0986acc3
Rob Hoes <rob.hoes@citrix.com>
2010-03-08 16:30:46 +0000

    CP-1591: Make Network.MTU writable on the CLI
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/client_records/records.ml
+++ ocaml/client_records/records.ml
@@ -344,7 +344,7 @@ let net_record rpc session_id net =
 	  ~get_set:(fun () -> (List.map (fun vif -> get_uuid_from_ref vif) (x ()).API.network_VIFs)) ();
 	make_field ~name:"PIF-uuids" ~get:(fun () -> String.concat "; " (List.map (fun pif -> get_uuid_from_ref pif) (x ()).API.network_PIFs)) 
 	  ~get_set:(fun () -> (List.map (fun pif -> get_uuid_from_ref pif) (x ()).API.network_PIFs)) ();
-	make_field ~name:"MTU" ~get:(fun () -> (Int64.to_string (x ()).API.network_MTU)) ();
+	make_field ~name:"MTU" ~get:(fun () -> (Int64.to_string (x ()).API.network_MTU)) ~set:(fun x -> Client.Network.set_MTU rpc session_id net (Int64.of_string x)) ();
 	make_field ~name:"bridge" ~get:(fun () -> (x ()).API.network_bridge) ();
 	make_field ~name:"other-config"         ~get:(fun () -> Record_util.s2sm_to_string "; " (x ()).API.network_other_config)
 	  ~add_to_map:(fun k v -> Client.Network.add_to_other_config rpc session_id net k v)
