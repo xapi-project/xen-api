21ca32524dbff3b9389e2eb712d481a7da8705fd
Matthias Goergens <Matthias.Goergens@citrix.com>
2010-06-17 22:16:18 +0100

    stdext/Either: A module for type ('a, 'b) t = Left of 'a | Right of 'b and helper functions.
    Signed-off-by: Matthias Goergens <matthias.goergens@citrix.com>

---
+++ stdext/either.ml
@@ -0,0 +1,24 @@
+open Pervasiveext
+
+type ('a,'b) t = Left of 'a | Right of 'b
+
+let left x = Left x
+let right x = Right x
+let is_left = function
+	| Left _ -> true
+	| Right _ -> false
+let is_right x = not ++ is_left $ x
+let to_option = function
+	| Right x -> Some x
+	| Left _ -> None
+
+let cat_right l = Opt.cat_some ++ List.map to_option $ l
+
+let join = function
+	| Right (Right x) -> Right x
+	| Left x -> Left (Left x)
+	| Right (Left x) -> Left (Right x)
+
+let swap = function
+	| Right x -> Left x
+	| Left x -> Right x
