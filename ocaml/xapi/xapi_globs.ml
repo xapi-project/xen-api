f71f250ee9828f399f8bc0910a3259722a4d0ef6
Rob Hoes <rob.hoes@citrix.com>
2010-10-12 10:40:21 +0100

    Increase the API version number to 1.8
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -32,7 +32,7 @@ let xapi_user_agent = "xapi/"^(string_of_int version_major)^"."^(string_of_int v
 
 (* api version *)
 let api_version_major = 1L
-let api_version_minor = 7L
+let api_version_minor = 8L
 let api_version_string =
   Printf.sprintf "%Ld.%Ld" api_version_major api_version_minor
 let api_version_vendor = "XenSource"
@@ -42,8 +42,8 @@ let api_version_vendor_implementation = []
 let tools_version = ref (-1, -1, -1, -1)
 
 (* client min/max version range *)
-let xencenter_min_verstring = "1.7"
-let xencenter_max_verstring = "1.7"
+let xencenter_min_verstring = "1.8"
+let xencenter_max_verstring = "1.8"
 
 (** Date-Based Version: burn-in date of current XenServer release (RTM date) *)
 let dbv = "2009.0201"
