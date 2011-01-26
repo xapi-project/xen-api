fb0cda34e495eeeb1f95691263b7b53bc5f2dca7
David Scott <dave.scott@eu.citrix.com>
2011-01-26 17:39:05 +0000

    Remove the control_domain_uuid from the database manifest (since it duplicated information already in the database)
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/database/db_cache_types.mli
+++ ocaml/database/db_cache_types.mli
@@ -23,7 +23,6 @@ type where_record = {
 }
 type structured_op_t = AddSet | RemoveSet | AddMap | RemoveMap
 type db_dump_manifest = {
-  control_domain_uuid : string;
   pool_conf : string;
   pool_token : string;
   schema_major_vsn : int;
