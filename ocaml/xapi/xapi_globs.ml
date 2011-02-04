0db50b5e7a76d8336567470872cc9d622a8954d6
Rob Hoes <rob.hoes@citrix.com>
2011-02-04 16:48:57 +0000

    CP-2058: On host start, check whether all bonds follow the rules, and fix them if not.
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -351,6 +351,7 @@ let sync_bios_strings = "sync_bios_strings"
 let sync_chipset_info = "sync_chipset_info"
 let sync_pci_devices = "sync_pci_devices"
 let sync_gpus = "sync_gpus"
+let sync_fix_bonds = "sync_fix_bonds"
 
 (* create_storage *)
 let sync_create_pbds = "sync_create_pbds"
