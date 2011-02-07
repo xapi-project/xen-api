19a417d187e2eb941dd4863e04f417b72f7e9f7f
Mike McClurg <mike.mcclurg@citrix.com>
2011-02-07 17:52:26 +0000

    CR-1011 Streamline rolling-pool upgrade
    Add a xapi plugin that tells the host installer where to look for an
    installation repository. Works with HTTP, FTP and NFS repos.
    
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

---
+++ scripts/plugins/prepare_host_upgrade.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+# Copyright (c) 2011 Citrix Systems, Inc. All use and distribution of this
+# copyrighted material is governed by and subject to terms and conditions
+# as licensed by Citrix Systems, Inc. All other rights reserved.
+# Xen, XenSource and XenEnterprise are either registered trademarks or
+# trademarks of Citrix Systems, Inc. in the United States and/or other 
+# countries.
+
+import os
+import shutil
+import sys
+import tempfile
+import StringIO
+
+import accessor
+import bootloader
+import cpiofile
+import repository
+import xelogging
+import XenAPI
+import XenAPIPlugin
+
+boot_files = [ 'install.img', 'boot/vmlinuz', 'boot/xen.gz']
+
+def get_boot_files(accessor, dest_dir):
+    done = True
+    accessor.start()
+    for f in boot_files:
+        try:
+            xelogging.log("Fetching "+f)
+            inf = accessor.openAddress(f)
+            if dest_dir:
+                outf = open(os.path.join(dest_dir, os.path.basename(f)), 'w')
+                outf.writelines(inf)
+                outf.close()
+            inf.close()
+        except:
+            done = False
+            break
+    accessor.finish()
+    return done
+
+def gen_answerfile(installer_dir, url):
+    root_device = None
+
+    try:
+        # determine root disk
+        f = open('/etc/xensource-inventory')
+        for l in f:
+            line = l.strip()
+            if line.startswith('PRIMARY_DISK='):
+                root_device = line[13:].strip("'")
+                break
+        f.close()
+    except:
+        return False
+    if not root_device:
+        return False
+
+    if not os.path.exists(root_device):
+        return False
+
+    xelogging.log("Root device: "+root_device)
+    
+    in_arc = cpiofile.CpioFile.open(installer_dir+'/install.img', 'r|*')
+    out_arc = cpiofile.CpioFile.open(installer_dir+'/upgrade.img', 'w|gz')
+    out_arc.hardlinks = False
+
+    # copy initrd
+    xelogging.log("Copying initrd...")
+    for f in in_arc:
+        data = None
+        if f.size > 0:
+            data = in_arc.extractfile(f)
+        out_arc.addfile(f, data)
+    in_arc.close()
+
+    # create answerfile
+    xelogging.log("Creating answerfile")
+    text = '<?xml version="1.0"?>\n'
+    text += ' <installation mode="upgrade">\n'
+    text += '  <existing-installation>%s</existing-installation>\n' % root_device
+    text += '  <source type="url">%s</source>\n' % url
+    text += ' </installation>\n'
+    
+    contents = StringIO.StringIO(text)
+
+    f = cpiofile.CpioInfo('answerfile')
+    f.size = len(contents.getvalue())
+    out_arc.addfile(f, contents)
+
+    out_arc.close()
+
+    return True
+
+def get_mgmt_config():
+    ret = None
+
+    session = XenAPI.xapi_local()
+    session.xenapi.login_with_password('', '')
+    this_host = session.xenapi.session.get_this_host(session._session)
+    host_record = session.xenapi.host.get_record(this_host)
+
+    for pif in host_record['PIFs']:
+        pif_record = session.xenapi.PIF.get_record(pif)
+        if pif_record['management']:
+            ret = pif_record
+            break
+        
+    session.xenapi.session.logout()
+    return ret
+
+def set_boot_config(installer_dir):
+    try:
+        config = bootloader.Bootloader.loadExisting()
+
+        default = config.menu[config.default]
+        if 'upgrade' in config.menu_order:
+            config.remove('upgrade')
+
+        pif = get_mgmt_config()
+
+        xen_args = ['dom0_mem=752M']
+        xen_args.extend(filter(lambda x: x.startswith('com') or x.startswith('console='), default.hypervisor_args.split()))
+        kernel_args = filter(lambda x: x.startswith('console=') or x.startswith('xencons='), default.kernel_args.split())
+        kernel_args.extend(['install', 'answerfile=file:///answerfile'])
+
+        if pif['ip_configuration_mode'] == 'Static':
+            config_str = "static;ip=%s;netmask=%s" % (pif['IP'], pif['netmask'])
+            if 'gateway' in pif:
+                config_str += ";gateway=" + pif['gateway']
+            if 'DNS' in pif:
+                config_str += ";dns=" + ','.join(pif['DNS'])
+            kernel_args.extend(['network_device='+pif['MAC'],
+                                'network_config=static;'+config_str])
+        else:
+            kernel_args.append('network_device=' + pif['MAC'])
+
+        e = bootloader.MenuEntry(installer_dir+'/xen.gz', ' '.join(xen_args),
+                                 installer_dir+'/vmlinuz', ' '.join(kernel_args),
+                                 installer_dir+'/upgrade.img', 'Rolling pool upgrade')
+        config.append('upgrade', e)
+        config.default = 'upgrade'
+        xelogging.log("Writing updated bootloader config")
+        config.commit()
+    except:
+        return False
+
+    return True
+
+def test_repo(url):
+    try:
+        a = accessor.createAccessor(url)
+    except AssertionError:
+        return False
+    if not get_boot_files(a, None):
+        return False
+    r = repository.Repository.findRepositories(a)
+    return len(r) > 0
+
+def prepare_host_upgrade(url):
+    installer_dir = '/boot/installer'
+    done = True
+
+    # download the installer files
+    try:
+        shutil.rmtree(installer_dir)
+    except:
+        pass
+    os.mkdir(installer_dir, 0700)
+
+    a = accessor.createAccessor(url)
+    done = get_boot_files(a, installer_dir)
+
+    if done:
+        done = gen_answerfile(installer_dir, url)
+        
+    if done:
+        # create bootloader entry
+        set_boot_config(installer_dir)
+        
+    if not done:
+        try:
+            shutil.rmtree(installer_dir)
+        except:
+            pass
+    return done
+
+# plugin url test
+def testUrl(session, args):
+    try:
+        url = args['url']
+    except KeyError:
+        return "Missing argument 'url'"
+
+    if test_repo(url):
+        return "true"
+    else:
+        return "false"
+    
+# plugin entry point
+def main(session, args):
+    xelogging.logToStderr()
+
+    try:
+        url = args['url']
+    except KeyError:
+        xelogging.log("Missing argument 'url'")
+        return "Missing argument 'url'"
+
+    xelogging.log("Verifying repo...")
+    succeeded = False
+    if not test_repo(url):
+        xelogging.log("%s is not a valid repo" % url)
+        return "%s is not a valid repo " % url
+    else:
+        xelogging.log("Repo ok, preparing for upgrade.")
+        succeeded = prepare_host_upgrade(url)
+
+    if succeeded:
+        return "Host ready for upgrade."
+    else:
+        return "Preparation failed."
+
+if __name__ == '__main__':
+    XenAPIPlugin.dispatch({"main": main,
+                           "testUrl": testUrl})
