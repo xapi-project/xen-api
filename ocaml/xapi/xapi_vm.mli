f3a8e7686cc51ca2fb0fe3fc6fa2158f4268da20
John Else <john.else@citrix.com>
2011-03-07 17:07:26 +0000

    Implement VM.assert_can_be_recovered.
    Add the VM method assert_can_be_recovered. This takes two arguments
    - a VM and a recovery session. It returns unit if the VM is not part of
    an appliance and all required SRs are visible to the recovery session,
    otherwise it throws an exception.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_vm.mli
+++ ocaml/xapi/xapi_vm.mli
@@ -271,3 +271,5 @@ val set_protection_policy : __context:Context.t -> self:API.ref_VM -> value:API.
 val set_start_delay : __context:Context.t -> self:API.ref_VM -> value:int64 -> unit
 val set_shutdown_delay : __context:Context.t -> self:API.ref_VM -> value:int64 -> unit
 val set_order : __context:Context.t -> self:API.ref_VM -> value:int64 -> unit
+
+val assert_can_be_recovered : __context:Context.t -> self:API.ref_VM -> session_to:API.ref_session -> unit
