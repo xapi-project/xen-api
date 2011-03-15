f17a44393e2a29da55a0fdc3f468fcc904894fa7
Rob Hoes <rob.hoes@citrix.com>
2011-03-15 14:44:26 +0000

    CP-2321: VGPU functions
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_vgpu.ml
+++ ocaml/xapi/xapi_vgpu.ml
@@ -14,10 +14,28 @@
 module D=Debug.Debugger(struct let name="xapi" end)
 open D
 
+(* Mutex to prevent duplicate VGPUs being created by accident *)
+let m = Mutex.create ()
+
+(* Only allow device = "0" for now, as we support just a single vGPU per VM *)
+let valid_device device =
+	device = "0"
+
 let create ~__context  ~vM ~gPU_group ~device ~other_config =
 	let vgpu = Ref.make () in
 	let uuid = Uuid.to_string (Uuid.make_uuid ()) in
-	Db.VGPU.create ~__context ~ref:vgpu ~uuid ~vM ~gPU_group ~device ~currently_attached:false ~other_config;
+	if not(valid_device device)
+		then raise (Api_errors.Server_error (Api_errors.invalid_device,[device]));
+	Threadext.Mutex.execute m (fun () ->
+		(* Check to make sure the device is unique *)
+		let all = Db.VM.get_VGPUs ~__context ~self:vM in
+		let all_devices = List.map (fun self -> Db.VGPU.get_device ~__context ~self) all in
+		if List.mem device all_devices then
+			raise (Api_errors.Server_error (Api_errors.device_already_exists, [device]));
+
+		Db.VGPU.create ~__context ~ref:vgpu ~uuid ~vM ~gPU_group ~device ~currently_attached:false ~other_config;
+	);
+	debug "VGPU ref='%s' created (VM = '%s')" (Ref.string_of vgpu) (Ref.string_of vM);
 	vgpu
 
 let destroy ~__context ~self =
