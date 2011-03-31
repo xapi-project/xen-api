d62aa29a494379143c8e89f6c6e5b6558a42bb0a
Rob Hoes <rob.hoes@citrix.com>
2011-03-31 17:27:35 +0100

    Expose primary-slave in bond params in CLI
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/client_records/records.ml
+++ ocaml/client_records/records.ml
@@ -143,6 +143,7 @@ let bond_record rpc session_id bond =
         make_field ~name:"master"       ~get:(fun () -> get_uuid_from_ref (x ()).API.bond_master) ();
         make_field ~name:"slaves"       ~get:(fun () -> String.concat "; " (List.map get_uuid_from_ref (x ()).API.bond_slaves)) ();
         make_field ~name:"mode" ~get:(fun () -> Record_util.bond_mode_to_string (x ()).API.bond_mode) ();
+        make_field ~name:"primary-slave" ~get:(fun () -> get_uuid_from_ref (x ()).API.bond_primary_slave) ();
       ]
   }
 
