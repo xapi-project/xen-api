962ef68e8552fcb70998c66636c3e702c23f7312
David Scott <dave.scott@eu.citrix.com>
2011-05-11 11:53:23 +0100

    CA-46505: add new xapi storage_access module, to use the new (simpler) API.
    Note the following series of commits will plumb this through the rest of the code.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/storage_access.mli
+++ ocaml/xapi/storage_access.mli
@@ -15,27 +15,47 @@
  * @group Storage
  *)
  
-module SR :
-sig
-  val attach : __context:Context.t -> self:API.ref_SR -> unit
-  val detach : __context:Context.t -> self:API.ref_SR -> unit
-end
+(** [rpc_of_sr __context sr] returns an Rpc.call -> Rpc.response function
+    for talking to the implementation of [sr], which could be in xapi, in domain 0
+    or in a driver domain. *)
+val rpc_of_sr: __context:Context.t -> sr:API.ref_SR -> Rpc.call -> Rpc.response
 
-module VDI :
-sig
-  val initialise_refcounts_from_db : unit -> unit
+(** [rpc_of_vbd __context vbd] returns an Rpc.call -> Rpc.response function
+    for talking to the SR underlying the VDI corresponding to [vbd]. See rpc_of_sr *)
+val rpc_of_vbd: __context:Context.t -> vbd:API.ref_VBD -> Rpc.call -> Rpc.response
 
-  val attach     : __context:Context.t -> self:API.ref_VDI -> mode:[`RO|`RW] -> unit
-  val detach     : __context:Context.t -> self:API.ref_VDI -> unit
-  val activate   : __context:Context.t -> self:API.ref_VDI -> mode:[`RO|`RW] -> unit
-  val deactivate : __context:Context.t -> self:API.ref_VDI -> unit
+(** RPC function for calling the main storage multiplexor *)
+val rpc: Rpc.call -> Rpc.response
 
-  val check_enclosing_sr_for_capability : Context.t -> Smint.capability -> [ `VDI ] Ref.t -> bool
-  val get_physical_path:[`VDI] Uuid.t -> string
-end
+(** [datapath_of_vbd domid devid] returns the name of the datapath which corresponds
+    to device [devid] on domain [domid] *)
+val datapath_of_vbd: domid:int -> devid:int -> Storage_interface.dp
 
-val use_vdi                          : __context:Context.t -> vdi:API.ref_VDI -> mode:[`RO|`RW] -> unit
-val deactivate_and_detach            : __context:Context.t -> vdi:API.ref_VDI -> unit
-val with_careful_attach_and_activate : __context:Context.t -> vdis:(API.ref_VDI * [`RO|`RW]) list -> leave_activated:bool -> (unit -> 'a ) -> 'a
-val use_vdi_from_vbd                 : __context:Context.t -> [`VBD] Ref.t -> unit
+val expect_vdi: (Storage_interface.physical_device -> 'a) -> Storage_interface.result -> 'a
 
+val expect_unit: (unit -> 'a) -> Storage_interface.result -> 'a
+
+(** [attach_and_activate __context vbd domid f] calls [f physical_device] where
+    [physical_device] is the result of attaching a VDI which is also activated.
+    This should be used everywhere except the migrate code, where we want fine-grained
+    control of the ordering of attach/activate/deactivate/detach *)
+val attach_and_activate: __context:Context.t -> vbd:API.ref_VBD -> domid:int -> (Storage_interface.physical_device -> 'a) -> 'a
+
+(** [deactivate_and_detach __context vbd domid] idempotent function which ensures
+    that any attached or activated VDI gets properly deactivated and detached. *)
+val deactivate_and_detach: __context:Context.t -> vbd:API.ref_VBD -> domid:int -> unit
+
+(** [on_vdi __context vbd domid f] calls [f rpc dp sr vdi] which is
+    useful for executing Storage_interface.Client.VDI functions, applying the
+    standard convention mapping VBDs onto DPs *)
+val on_vdi: __context:Context.t -> vbd:API.ref_VBD -> domid:int -> ((Rpc.call -> Rpc.response) -> Storage_interface.task -> Storage_interface.dp -> Storage_interface.sr -> Storage_interface.vdi -> 'a) -> 'a
+
+(** [resynchronise_pbds __context pbds] sets the currently_attached state of
+    each of [pbd] to match the state of the storage layer. *)
+val resynchronise_pbds: __context:Context.t -> pbds:API.ref_PBD list -> unit
+
+(** [diagnostics __context] returns a printable snapshot of SM system state *)
+val diagnostics: __context:Context.t -> string
+
+(** [dp_destroy __context dp allow_leak] attempts to cleanup and detach a given DP *)
+val dp_destroy: __context:Context.t -> string -> bool -> unit
