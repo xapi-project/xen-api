bc0a7c6e87666be521b79f534cffda036a6a5cb6
Zheng Li <zheng.li@eu.citrix.com>
2011-06-15 15:58:38 +0000

    Put remote copy code to the remot copy branch
    ... instead of common code path.
    
    Signed-off-by: Zheng Li <zheng.li@eu.citrix.com>

--- ocaml/xapi/sm_fs_ops.ml
+++ ocaml/xapi/sm_fs_ops.ml
@@ -206,8 +206,6 @@ let copy_vdi ~__context vdi_src vdi_dst =
 		debug "Sm_fs_ops.copy_vdi: %s-copying %Ld%s preserving sparseness" (if local_copy then "locally" else "remotely") size (if sparse then "" else " NOT");
 
 		try
-			let remote_uri = import_vdi_url ~__context rpc session_id vdi_dst in
-			debug "remote_uri = %s" remote_uri;
 			with_block_attached_device __context rpc session_id vdi_src `RO
 				(fun device_src ->
 					if local_copy
@@ -215,7 +213,10 @@ let copy_vdi ~__context vdi_src vdi_dst =
 						(fun device_dst ->
 							Sparse_dd_wrapper.dd ~__context sparse device_src device_dst size
 						)
-					else Sparse_dd_wrapper.dd ~__context sparse device_src remote_uri size
+					else
+						let remote_uri = import_vdi_url ~__context rpc session_id vdi_dst in
+						debug "remote_uri = %s" remote_uri;
+						Sparse_dd_wrapper.dd ~__context sparse device_src remote_uri size
 				)
 		with
 		| Unix.Unix_error(Unix.EIO, _, _) ->
