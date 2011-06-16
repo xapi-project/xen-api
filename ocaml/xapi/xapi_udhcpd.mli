78af236c963a7d2d9d8b957df71fa02b6af89cb5
David Scott <dave.scott@eu.citrix.com>
2011-06-16 21:52:07 +0100

    Host internal management network: add "get_ip" function to discover the IP assigned to a specific VIF
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/xapi_udhcpd.mli
+++ ocaml/xapi/xapi_udhcpd.mli
@@ -6,3 +6,7 @@ val maybe_add_lease: __context:Context.t -> API.ref_VIF -> unit
 (* [maybe_remove_lease __context vif]: if [vif] is on the Host internal
    management network then remove the assigned DHCP lease *)
 val maybe_remove_lease: __context:Context.t -> API.ref_VIF -> unit
+
+(* [get_ip __context vif]: if [vif] is on the Host internal management
+   network then return the assigned IP *)
+val get_ip: __context:Context.t -> API.ref_VIF -> (int * int * int * int) option
