d8a64d61f8fcb1a00debf37346635dc91eaf1de8
Zheng Li <zheng.li@eu.citrix.com>
2011-06-18 21:10:00 +0000

    [INDENTATION] ocaml/xapi/xapi_host_crashdump.ml
    Auto generated/verified with the following script:
    
    ----
    #!/bin/sh
    
    set -e -v
    
    emacs -l ~/.emacs -batch $@ --eval '(progn (indent-region (point-min) (point-max) nil) (delete-trailing-whitespace) (save-buffer))'
    
    git diff --check
    
    DIFFSET=$(git diff -w $@)
    
    if [ -n "$DIFFSET" ]
    then
    	echo "$DIFFSET"
    	echo
    	echo "Some differences besides whitespaces:"
    	echo "$DIFFSET"
    	echo
    	exit 1
    else
    	echo
    	echo "No differences except whitespaces"
    	echo
    	git add $@
    	(echo -e "[INDENTATION] $@\n\nAuto generated/verified with the following script:\n\n----" ; cat $(realpath $(which $0)) ; echo -e "----") | git commit -s -F -
    	exit 0
    fi
    ----
    
    Signed-off-by: Zheng Li <zheng.li@eu.citrix.com>

--- ocaml/xapi/xapi_host_crashdump.ml
+++ ocaml/xapi/xapi_host_crashdump.ml
@@ -12,9 +12,9 @@
  * GNU Lesser General Public License for more details.
  *)
 (**
- * @group Host Management
- *)
- 
+   * @group Host Management
+*)
+
 open Pervasiveext
 open Stringext
 open Xapi_support
@@ -29,72 +29,72 @@ let du = "/usr/bin/du"
 (* Host crashes are directories placed into /var/crash by the crash kernel *)
 let crash_dir = "/var/crash"
 
-let delete_crashdump_dir filename = 
-  let path = Filename.concat crash_dir filename in
-  try
-    let stat = Unix.stat path in 
-    match stat.Unix.st_kind with
-    | Unix.S_DIR ->
-	(* crash dumps are directories *)
-	let cmd = Printf.sprintf "%s -rf %s" rm path in
-	let output = Helpers.get_process_output cmd in
-	if output <> "" then warn "Output from %s: %s" cmd output
-    | _ ->
-	error "Crashdump path %s refers to something other than a directory!" path;
-  with e ->
-    error "Caught exception while deleting crashdump at path %s (%s)" filename (ExnHelper.string_of_exn e);
-    raise e
+let delete_crashdump_dir filename =
+	let path = Filename.concat crash_dir filename in
+	try
+		let stat = Unix.stat path in
+		match stat.Unix.st_kind with
+		| Unix.S_DIR ->
+			(* crash dumps are directories *)
+			let cmd = Printf.sprintf "%s -rf %s" rm path in
+			let output = Helpers.get_process_output cmd in
+			if output <> "" then warn "Output from %s: %s" cmd output
+		| _ ->
+			error "Crashdump path %s refers to something other than a directory!" path;
+	with e ->
+		error "Caught exception while deleting crashdump at path %s (%s)" filename (ExnHelper.string_of_exn e);
+		raise e
 
 let set_difference a b = List.filter (fun x -> not(List.mem x b)) a
 
 (* Called once on host boot to resync the crash directory with the database *)
-let resynchronise ~__context ~host = 
-  debug "Xapi_host_crashdump.resynchronise";
-  let all_refs = Db.Host.get_crashdumps ~__context ~self:host in
-  let db_filenames = List.map 
-    (fun self -> 
-       Db.Host_crashdump.get_filename ~__context ~self) all_refs in
+let resynchronise ~__context ~host =
+	debug "Xapi_host_crashdump.resynchronise";
+	let all_refs = Db.Host.get_crashdumps ~__context ~self:host in
+	let db_filenames = List.map
+		(fun self ->
+			Db.Host_crashdump.get_filename ~__context ~self) all_refs in
 
-  let real_filenames = try Array.to_list (Sys.readdir crash_dir) with _ -> [] in
-  let gone_away = set_difference db_filenames real_filenames
-  and arrived = set_difference real_filenames db_filenames in
+	let real_filenames = try Array.to_list (Sys.readdir crash_dir) with _ -> [] in
+	let gone_away = set_difference db_filenames real_filenames
+	and arrived = set_difference real_filenames db_filenames in
 
-  let was_shutdown_cleanly = try bool_of_string (Localdb.get Constants.host_restarted_cleanly) with _ -> false in
-  Localdb.put Constants.host_restarted_cleanly "false";
-  (* If HA is enabled AND no crashdump appeared AND we weren't shutdown cleanly then assume it was a fence. *)
-  let ha_is_enabled =
-    try Db.Pool.get_ha_enabled ~__context ~self:(Helpers.get_pool ~__context)
-    with _ -> false (* on first boot no-pool=>exn, but on first boot HA is never enabled *) in
-  begin
-    if ha_is_enabled && (arrived = []) && not was_shutdown_cleanly && !Xapi_globs.on_system_boot
-    then Xapi_alert.add ~name:Api_messages.ha_host_was_fenced ~priority:1L ~cls:`Host ~obj_uuid:(Db.Host.get_uuid ~__context ~self:host) ~body:""
-  end;
+	let was_shutdown_cleanly = try bool_of_string (Localdb.get Constants.host_restarted_cleanly) with _ -> false in
+	Localdb.put Constants.host_restarted_cleanly "false";
+	(* If HA is enabled AND no crashdump appeared AND we weren't shutdown cleanly then assume it was a fence. *)
+	let ha_is_enabled =
+		try Db.Pool.get_ha_enabled ~__context ~self:(Helpers.get_pool ~__context)
+		with _ -> false (* on first boot no-pool=>exn, but on first boot HA is never enabled *) in
+	begin
+		if ha_is_enabled && (arrived = []) && not was_shutdown_cleanly && !Xapi_globs.on_system_boot
+		then Xapi_alert.add ~name:Api_messages.ha_host_was_fenced ~priority:1L ~cls:`Host ~obj_uuid:(Db.Host.get_uuid ~__context ~self:host) ~body:""
+	end;
 
-  let table = List.combine db_filenames all_refs in
-  List.iter (fun filename ->
-	       debug "Deleting record corresponding to old crashdump %s" filename;
-	       let r = List.assoc filename table in
-	       Db.Host_crashdump.destroy ~__context ~self:r) gone_away;
-  List.iter (fun filename ->
-	       debug "Adding record corresponding to new crashdump %s" filename;
-	       let cmd = Printf.sprintf "%s --bytes -s %s/%s" du crash_dir filename in
-	       let size = match String.split_f String.isspace (Helpers.get_process_output cmd) with
-		 | size :: _ -> Int64.of_string size
-		 | _ -> (-1L) in
-	       let r = Ref.make () and uuid = Uuid.to_string (Uuid.make_uuid ()) in
-	       Db.Host_crashdump.create ~__context ~ref:r ~uuid ~other_config:[]
-		  ~host ~timestamp:(Date.of_float (Unix.gettimeofday ())) ~size ~filename) arrived
-		   
-
-let destroy ~__context ~self = 
-  let filename = Db.Host_crashdump.get_filename ~__context ~self in
-  finally 
-    (fun () -> delete_crashdump_dir filename)
-    (fun () -> Db.Host_crashdump.destroy ~__context ~self)
+	let table = List.combine db_filenames all_refs in
+	List.iter (fun filename ->
+		debug "Deleting record corresponding to old crashdump %s" filename;
+		let r = List.assoc filename table in
+		Db.Host_crashdump.destroy ~__context ~self:r) gone_away;
+	List.iter (fun filename ->
+		debug "Adding record corresponding to new crashdump %s" filename;
+		let cmd = Printf.sprintf "%s --bytes -s %s/%s" du crash_dir filename in
+		let size = match String.split_f String.isspace (Helpers.get_process_output cmd) with
+			| size :: _ -> Int64.of_string size
+			| _ -> (-1L) in
+		let r = Ref.make () and uuid = Uuid.to_string (Uuid.make_uuid ()) in
+		Db.Host_crashdump.create ~__context ~ref:r ~uuid ~other_config:[]
+			~host ~timestamp:(Date.of_float (Unix.gettimeofday ())) ~size ~filename) arrived
 
 
-let upload ~__context ~self ~url ~options = 
-  let filename = Db.Host_crashdump.get_filename ~__context ~self in
-  let url = if url = "" then (upload_url filename) else url in
-  do_upload "host-crash-upload" (crash_dir ^ "/" ^ filename) url options
-  
+let destroy ~__context ~self =
+	let filename = Db.Host_crashdump.get_filename ~__context ~self in
+	finally
+		(fun () -> delete_crashdump_dir filename)
+		(fun () -> Db.Host_crashdump.destroy ~__context ~self)
+
+
+let upload ~__context ~self ~url ~options =
+	let filename = Db.Host_crashdump.get_filename ~__context ~self in
+	let url = if url = "" then (upload_url filename) else url in
+	do_upload "host-crash-upload" (crash_dir ^ "/" ^ filename) url options
+
