e056cee5d31a90da87ebf59e49e139ec737fe36e
Rok Strniša <rok.strnisa@citrix.com>
2011-06-29 16:10:09 +0100

    [INDENTATION]
    Signed-off-by: Rok Strnisa <rok.strnisa@citrix.com>

--- ocaml/xapi/xapi_config.ml
+++ ocaml/xapi/xapi_config.ml
@@ -22,100 +22,100 @@ module D=Debug.Debugger(struct let name="xapi" end)
 open D
 
 let clear_log level key =
-    let clear_f =
-        if key = "" then
-            Logs.clear_default
-        else
-            Logs.clear key in
-    if level = "" then (
-        List.iter (fun level -> clear_f level)
-              [ Log.Error; Log.Warn; Log.Info; Log.Debug ]
-    ) else (
-        let loglevel = match level with
-        | "debug" -> Log.Debug
-        | "info"  -> Log.Info
-        | "warn"  -> Log.Warn
-        | "error" -> Log.Error
-        | s       -> failwith (sprintf "Unknown log level: %s" s) in
-        clear_f loglevel
-    )
+	let clear_f =
+		if key = "" then
+			Logs.clear_default
+		else
+			Logs.clear key in
+	if level = "" then (
+		List.iter (fun level -> clear_f level)
+			[ Log.Error; Log.Warn; Log.Info; Log.Debug ]
+	) else (
+		let loglevel = match level with
+			| "debug" -> Log.Debug
+			| "info"  -> Log.Info
+			| "warn"  -> Log.Warn
+			| "error" -> Log.Error
+			| s       -> failwith (sprintf "Unknown log level: %s" s) in
+		clear_f loglevel
+	)
 
 let append_log level key logger =
-    (* if key is empty, append to the default logger *)
-    let append =
-        if key = "" then
-            Logs.append_default
-        else
-            Logs.append key in
-    (* if level is empty, append to all level *)
-    if level = "" then (
-        List.iter (fun level -> append level logger)
-              [ Log.Error; Log.Warn; Log.Info; Log.Debug ]
-    ) else (
-        let loglevel = match level with
-        | "debug" -> Log.Debug
-        | "info"  -> Log.Info
-        | "warn"  -> Log.Warn
-        | "error" -> Log.Error
-        | s       -> failwith (sprintf "Unknown log level: %s" s) in
-        append loglevel logger
-    )
+	(* if key is empty, append to the default logger *)
+	let append =
+		if key = "" then
+			Logs.append_default
+		else
+			Logs.append key in
+	(* if level is empty, append to all level *)
+	if level = "" then (
+		List.iter (fun level -> append level logger)
+			[ Log.Error; Log.Warn; Log.Info; Log.Debug ]
+	) else (
+		let loglevel = match level with
+			| "debug" -> Log.Debug
+			| "info"  -> Log.Info
+			| "warn"  -> Log.Warn
+			| "error" -> Log.Error
+			| s       -> failwith (sprintf "Unknown log level: %s" s) in
+		append loglevel logger
+	)
 
 let read_log_config filename =
-    let trim_end lc s =
-        let i = ref (String.length s - 1) in
-        while !i > 0 && (List.mem s.[!i] lc)
-        do
-            decr i
-        done;
-        if !i >= 0 then String.sub s 0 (!i + 1) else ""
-        in
-    Unixext.readfile_line (fun line ->
-        let line = trim_end [ ' '; '\t' ] line in
-        if String.startswith "#" line then
-            ()
-        else
-            let ls = String.split ~limit:3 ';' line in
-            match ls with
-            | [ "reset" ] ->
-                Logs.reset_all []
-            | [ level; key; "clear" ] ->
-                clear_log level key
-            | [ level; key; logger ] ->
-                append_log level key logger
-            | _ ->
-                ()
-    ) filename
-    
+	let trim_end lc s =
+		let i = ref (String.length s - 1) in
+		while !i > 0 && (List.mem s.[!i] lc)
+		do
+			decr i
+		done;
+		if !i >= 0 then String.sub s 0 (!i + 1) else ""
+	in
+	Unixext.readfile_line
+		(fun line ->
+			 let line = trim_end [ ' '; '\t' ] line in
+			 if String.startswith "#" line then
+				 ()
+			 else
+				 let ls = String.split ~limit:3 ';' line in
+				 match ls with
+					 | [ "reset" ] ->
+						 Logs.reset_all []
+					 | [ level; key; "clear" ] ->
+						 clear_log level key
+					 | [ level; key; logger ] ->
+						 append_log level key logger
+					 | _ ->
+						 ()
+		) filename
+
 let read_config filename =
-    let set_log s =
-        let ls = String.split ~limit:3 ';' s in
-        match ls with
-        | [ level; key; logger ] ->
-            append_log level key logger
-        | _ ->
-            warn "format mismatch: expecting 3 arguments"
-        in
+	let set_log s =
+		let ls = String.split ~limit:3 ';' s in
+		match ls with
+			| [ level; key; logger ] ->
+				append_log level key logger
+			| _ ->
+				warn "format mismatch: expecting 3 arguments"
+	in
 
-    let configargs = [
-(*      "license_filename", Config.Set_string License_file.filename; *)
-        "stunnelng", Config.Set_bool Stunnel.use_new_stunnel;
-        "log", Config.String set_log;
-	"gc-debug", Config.Set_bool Xapi_globs.xapi_gc_debug;
-    ] in
-    try
-        Config.read filename configargs (fun _ _ -> ())
-    with Config.Error ls ->
-        List.iter (fun (p,s) ->
-            eprintf "config file error: %s: %s\n" p s) ls;
-        exit 2
+	let configargs = [
+		(* "license_filename", Config.Set_string License_file.filename; *)
+		"stunnelng", Config.Set_bool Stunnel.use_new_stunnel;
+		"log", Config.String set_log;
+		"gc-debug", Config.Set_bool Xapi_globs.xapi_gc_debug;
+	] in
+	try
+		Config.read filename configargs (fun _ _ -> ())
+	with Config.Error ls ->
+		List.iter (fun (p,s) ->
+								 eprintf "config file error: %s: %s\n" p s) ls;
+		exit 2
 
-let dump_config () = 
-    debug "Server configuration:";
-    debug "product_version: %s" Version.product_version;
-    debug "product_brand: %s" Version.product_brand;
-    debug "build_number: %s" Version.build_number;
-    debug "hg changeset: %s" Version.hg_id;
-    debug "version: %d.%d" version_major version_minor;
-(*  debug "License filename: %s" !License_file.filename *)
-    
+let dump_config () =
+	debug "Server configuration:";
+	debug "product_version: %s" Version.product_version;
+	debug "product_brand: %s" Version.product_brand;
+	debug "build_number: %s" Version.build_number;
+	debug "hg changeset: %s" Version.hg_id;
+	debug "version: %d.%d" version_major version_minor;
+	(* debug "License filename: %s" !License_file.filename *)
--- ocaml/xapi/xapi_mgmt_iface.ml
+++ ocaml/xapi/xapi_mgmt_iface.ml
@@ -20,7 +20,7 @@ open D
 (** Keep track of the management interface server thread *)
 
 (* Stores a key into the table in Http_srv which identifies the server thread bound
-   to the management IP. *)
+	 to the management IP. *)
 let management_interface_server = ref None
 let management_m = Mutex.create ()
 
@@ -28,97 +28,98 @@ let rewrite_management_interface_script = "/opt/xensource/libexec/rewrite-manage
 
 let script = "/opt/xensource/libexec/rewrite-management-interface"
 
-let rewrite_management_interface interface = 
-  (* XXX: probably should decompose this into Xapi_inventory.update <k> <v> and then a special hook script *)
-  let (_: string*string) = Forkhelpers.execute_command_get_output rewrite_management_interface_script [ interface ] in
-  Xapi_inventory.reread_inventory ()
+let rewrite_management_interface interface =
+	(* XXX: probably should decompose this into Xapi_inventory.update <k> <v> and
+	   then a special hook script *)
+	let (_: string*string) = Forkhelpers.execute_command_get_output rewrite_management_interface_script [ interface ] in
+	Xapi_inventory.reread_inventory ()
 
-let restart_stunnel () = 
-  ignore(Forkhelpers.execute_command_get_output "/sbin/service" [ "xapissl"; "restart" ])
+let restart_stunnel () =
+	ignore(Forkhelpers.execute_command_get_output "/sbin/service" [ "xapissl"; "restart" ])
 
-let stop () = 
-  debug "Shutting down the old management interface (if any)";
-  maybe Http_svr.stop !management_interface_server;
-  management_interface_server := None
+let stop () =
+	debug "Shutting down the old management interface (if any)";
+	maybe Http_svr.stop !management_interface_server;
+	management_interface_server := None
 
-let change (interface, ip) = 
-  stop ();
-  debug "Starting new server on IP: %s" ip;
-  let socket = Xapi_http.bind (Unix.ADDR_INET(Unix.inet_addr_of_string ip, Xapi_globs.http_port)) in
-  let server = Http_svr.start (socket, "inet_rpc") in
-  management_interface_server := Some server;
-  
-  rewrite_management_interface interface;
-  debug "Restarting stunnel";
-  restart_stunnel ();
+let change (interface, ip) =
+	stop ();
+	debug "Starting new server on IP: %s" ip;
+	let socket = Xapi_http.bind (Unix.ADDR_INET(Unix.inet_addr_of_string ip, Xapi_globs.http_port)) in
+	let server = Http_svr.start (socket, "inet_rpc") in
+	management_interface_server := Some server;
 
-  if Pool_role.is_master () then begin
-    (* NB if we synchronously bring up the management interface on a master with a blank 
-       database this can fail... this is ok because the database will be synchronised later *)
-    Server_helpers.exec_with_new_task "refreshing consoles" (fun __context ->
-    Dbsync_master.set_master_ip ~__context;
-    Dbsync_master.refresh_console_urls ~__context)
-  end
+	rewrite_management_interface interface;
+	debug "Restarting stunnel";
+	restart_stunnel ();
+
+	if Pool_role.is_master () then begin
+		(* NB if we synchronously bring up the management interface on a master with a blank
+		   database this can fail... this is ok because the database will be synchronised later *)
+		Server_helpers.exec_with_new_task "refreshing consoles"
+			(fun __context ->
+				Dbsync_master.set_master_ip ~__context;
+				Dbsync_master.refresh_console_urls ~__context)
+	end
 
 let management_ip_mutex = Mutex.create ()
 let management_ip_cond = Condition.create ()
 
-let wait_for_management_ip () = 
-    let ip = ref (match Helpers.get_management_ip_addr () with Some x -> x | None -> "") in
-    Mutex.execute management_ip_mutex (fun () -> begin
-      while !ip = "" do
-	Condition.wait management_ip_cond management_ip_mutex;
-	ip := (match Helpers.get_management_ip_addr () with Some x -> x | None -> "")
-      done;
-    end);
-    !ip
+let wait_for_management_ip () =
+	let ip = ref (match Helpers.get_management_ip_addr () with Some x -> x | None -> "") in
+	Mutex.execute management_ip_mutex
+		(fun () -> begin while !ip = "" do
+			Condition.wait management_ip_cond management_ip_mutex;
+			ip := (match Helpers.get_management_ip_addr () with Some x -> x | None -> "")
+		done; end);
+	!ip
 
 let on_dom0_networking_change ~__context =
-  debug "Checking to see if hostname or management IP has changed";
-  (* Need to update:
-     1 Host.hostname
-     2 Host.address
-     3. Console URIs *)
-  let new_hostname = Helpers.reget_hostname () in
-  let localhost = Helpers.get_localhost ~__context in
-  if Db.Host.get_hostname ~__context ~self:localhost <> new_hostname then begin
-    debug "Changing Host.hostname in database to: %s" new_hostname;
-    Db.Host.set_hostname ~__context ~self:localhost ~value:new_hostname
-  end;
-  begin match Helpers.get_management_ip_addr () with
-  | Some ip ->
-      if Db.Host.get_address ~__context ~self:localhost <> ip then begin
-	debug "Changing Host.address in database to: %s" ip;
-	Db.Host.set_address ~__context ~self:localhost ~value:ip;
-	debug "Refreshing console URIs";
-	Dbsync_master.refresh_console_urls ~__context
-      end
-  | None ->
-      if Db.Host.get_address ~__context ~self:localhost <> "" then begin
-	debug "Changing Host.address in database to: '' (host has no management IP address)";
-	Db.Host.set_address ~__context ~self:localhost ~value:""
-      end
-  end;
-  debug "Signalling anyone waiting for the management IP address to change";
-  Mutex.execute management_ip_mutex
-    (fun () -> Condition.broadcast management_ip_cond)
+	debug "Checking to see if hostname or management IP has changed";
+	(* Need to update:
+	   1 Host.hostname
+	   2 Host.address
+	   3. Console URIs *)
+	let new_hostname = Helpers.reget_hostname () in
+	let localhost = Helpers.get_localhost ~__context in
+	if Db.Host.get_hostname ~__context ~self:localhost <> new_hostname then begin
+		debug "Changing Host.hostname in database to: %s" new_hostname;
+		Db.Host.set_hostname ~__context ~self:localhost ~value:new_hostname
+	end;
+	begin match Helpers.get_management_ip_addr () with
+		| Some ip ->
+			if Db.Host.get_address ~__context ~self:localhost <> ip then begin
+				debug "Changing Host.address in database to: %s" ip;
+				Db.Host.set_address ~__context ~self:localhost ~value:ip;
+				debug "Refreshing console URIs";
+				Dbsync_master.refresh_console_urls ~__context
+			end
+		| None ->
+			if Db.Host.get_address ~__context ~self:localhost <> "" then begin
+				debug "Changing Host.address in database to: '' (host has no management IP address)";
+				Db.Host.set_address ~__context ~self:localhost ~value:""
+			end
+	end;
+	debug "Signalling anyone waiting for the management IP address to change";
+	Mutex.execute management_ip_mutex
+		(fun () -> Condition.broadcast management_ip_cond)
 
 
 let change_ip interface ip = Mutex.execute management_m (fun () -> change (interface, ip))
 
-let rebind () = Mutex.execute management_m 
-  (fun () -> 
-     (* We must check to see if the IP address has changed *)
-     let interface = Xapi_inventory.lookup Xapi_inventory._management_interface in
-     match Helpers.get_management_ip_addr () with
-     | Some ip -> change (interface, ip)
-     | None -> failwith "Management interface has no IP address"
-  )
-  
-let stop () = Mutex.execute management_m 
-  (fun () -> 
-     stop (); 
-     rewrite_management_interface "";
-     debug "Restarting stunnel";
-     restart_stunnel ();
-  )
+let rebind () = Mutex.execute management_m
+	(fun () ->
+		(* We must check to see if the IP address has changed *)
+		let interface = Xapi_inventory.lookup Xapi_inventory._management_interface in
+		match Helpers.get_management_ip_addr () with
+			| Some ip -> change (interface, ip)
+			| None -> failwith "Management interface has no IP address"
+	)
+
+let stop () = Mutex.execute management_m
+	(fun () ->
+		stop ();
+		rewrite_management_interface "";
+		debug "Restarting stunnel";
+		restart_stunnel ();
+	)
