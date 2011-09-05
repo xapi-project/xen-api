ef329e3328796c8fa018724f623072cfb07d0f3e
John Else <john.else@citrix.com>
2011-09-05 18:12:54 +0100

    Turn on build of local authentication test.
    This also removes auth.ml as it's a duplicate of pam.ml
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/auth/testauth.ml
+++ ocaml/auth/testauth.ml
@@ -18,13 +18,13 @@ let usage() =
   Printf.printf "%s chpasswd <username> <new password>\n" Sys.argv.(0);
   exit 1
 
-let _ = 
+let _ =
   if Array.length Sys.argv <> 4 then usage ();
   let username = Sys.argv.(2)
   and password = Sys.argv.(3) in
   match Sys.argv.(1) with
   | "auth" ->
-      Auth.authenticate username password
+      Pam.authenticate username password
   | "chpasswd" ->
-      Auth.change_password username password      
+      Pam.change_password username password
   | _ -> usage()
