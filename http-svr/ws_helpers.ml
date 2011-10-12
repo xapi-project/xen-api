e221cc2aecf621ae13ecdd7c37d39445f7bf5cf0
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2011-10-12 17:33:26 +0100

    Add copyright annotation and license for the sha1 function
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- http-svr/ws_helpers.ml
+++ http-svr/ws_helpers.ml
@@ -12,6 +12,13 @@
  * GNU Lesser General Public License for more details.
  *)
 
+(* Portions of this code are distributed under the following copyright notice *) 
+(*----------------------------------------------------------------------------
+   Copyright (c) 2009-11, Daniel C. Bünzli. All rights reserved.
+   Distributed under a BSD license, see license at the end of the file.
+   Uuidm version 0.9.3
+  ----------------------------------------------------------------------------*)
+
 (* Websockets helper functions *)
 
 (* A couple of short helper functions for upgrading an HTTP 
@@ -219,3 +226,38 @@ let hixie_v76_upgrade req s =
    current version. hixie76 has security problems anyway *)
 let upgrade req s = 
   v10_upgrade req s
+
+(* The following copyright notice is relevant to the function marked above *)
+
+(*----------------------------------------------------------------------------
+  Copyright (c) 2009-11, Daniel C. Bünzli
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+        
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the
+     distribution.
+
+  3. Neither the name of the Daniel C. Bünzli nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  ----------------------------------------------------------------------------*)
