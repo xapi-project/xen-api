a165c4e7b96cf4cf411285c59d175f9d9d4abc3c
David Scott <dave.scott@eu.citrix.com>
2011-10-25 21:25:42 +0100

    [templates] update copyright message (no need to include year, apparently)
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/create_templates.ml
+++ ocaml/xapi/create_templates.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (C) 2006-2010 Citrix Systems Inc.
+ * Copyright (C) Citrix Systems Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
