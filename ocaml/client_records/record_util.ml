7befcf03b338a3b571b7b3ada7ae65ee28f3739c
John Else <john.else@citrix.com>
2011-10-31 15:26:12 +0000

    CP-2764: CLI changes for new VIF/Network fields.
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/client_records/record_util.ml
+++ ocaml/client_records/record_util.ml
@@ -142,9 +142,21 @@ let vif_locking_mode_to_string = function
 	| `unlocked -> "unlocked"
 	| `disabled -> "disabled"
 
+let string_to_vif_locking_mode = function
+	| "default" -> `default
+	| "locked" -> `locked
+	| "unlocked" -> `unlocked
+	| "disabled" -> `disabled
+	| s -> raise (Record_failure ("Expected 'default', 'locked', 'unlocked', 'disabled', got "^s))
+
 let network_default_locking_mode_to_string = function
 	| `unlocked -> "unlocked"
 	| `disabled -> "disabled"
+
+let string_to_network_default_locking_mode = function
+	| "unlocked" -> `unlocked
+	| "disabled" -> `disabled
+	| s -> raise (Record_failure ("Expected 'unlocked' or 'disabled', got "^s))
 
 let vm_appliance_operation_to_string = function
   | `start -> "start"
