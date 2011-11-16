adbbfe5c219121a946e8cf1caa97e43bf8d4183d
Mike McClurg <mike.mcclurg@citrix.com>
2011-11-16 14:53:19 +0000

    Comment out debian tagging line; let gbp-dch do that
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- git-tag-version.sh
+++ git-tag-version.sh
@@ -7,4 +7,4 @@ VERSION=$(cat VERSION | tr -d '\n')
 
 # Retag master and debian
 git tag -f -a master/${VERSION} master -m "Tagged master branch with version $version"
-git tag -f -a debian/${VERSION} debian -m "Tagged debian branch with version $version"
+#git tag -f -a debian/${VERSION} debian -m "Tagged debian branch with version $version"
