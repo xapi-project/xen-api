178010dec8af5a0fce3f195bbb12eb68540e6da8
John Else <john.else@citrix.com>
2011-12-02 15:08:44 +0000

    CP-2767: Fix whitespace in vm_config.ml.
    $ camlp4o -printer o -no_comments ocaml/xapi/vm_config.ml | md5sum
    00fbb308630655754842b2354bf365c1
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/vm_config.ml
+++ ocaml/xapi/vm_config.ml
@@ -22,83 +22,83 @@ module D = Debug.Debugger(struct let name="xapi" end)
 open D
 
 type vif = {
-  domid: int; (** current domain id of the VM *)
-  vif_ref: API.ref_VIF;
-  network_ref: API.ref_network;
-  devid: int;
-  protocol: Device_common.protocol; 
-  mac: string;
-  mtu: int;
-  bridge: string; (** name of the bridge in domain 0 *)
-  rate: (int64 * int64) option;
-  other_config: (string * string) list;
+	domid: int; (** current domain id of the VM *)
+	vif_ref: API.ref_VIF;
+	network_ref: API.ref_network;
+	devid: int;
+	protocol: Device_common.protocol; 
+	mac: string;
+	mtu: int;
+	bridge: string; (** name of the bridge in domain 0 *)
+	rate: (int64 * int64) option;
+	other_config: (string * string) list;
 }
 
 (* Note: we always pass in the VM's domid rather than use the one from the VM record because
    during migration the VM actually has two domids. *)
 
-let vif_of_vif ~__context ~vm vm_r domid protocol vif = 
-  let vif_r = Db.VIF.get_record ~__context ~self:vif in
-  let log_qos_failure reason = 
-    warn "vif QoS failed: %s (vm=%s,vif=%s)" reason vm_r.API.vM_uuid vif_r.API.vIF_uuid in
-  
-  let mac = vif_r.API.vIF_MAC in
-  let vif_mtu = if (List.mem_assoc "mtu" vif_r.API.vIF_other_config) then
-    Some (int_of_string (List.assoc "mtu" vif_r.API.vIF_other_config)) else None in
+let vif_of_vif ~__context ~vm vm_r domid protocol vif =
+	let vif_r = Db.VIF.get_record ~__context ~self:vif in
+	let log_qos_failure reason =
+		warn "vif QoS failed: %s (vm=%s,vif=%s)" reason vm_r.API.vM_uuid vif_r.API.vIF_uuid in
 
-  let qos_type = vif_r.API.vIF_qos_algorithm_type in
-  let qos_params = vif_r.API.vIF_qos_algorithm_params in
-  let rate = match qos_type with
-    | "ratelimit" -> 
-	let timeslice =
-	  try Int64.of_string (List.assoc "timeslice_us" qos_params)
-	  with _ -> 0L in
-	begin
-	  try
-	    let rate = Int64.of_string (List.assoc "kbps" qos_params) in
-	    Some (rate, timeslice)
-	  with
-	  | Failure "int_of_string" ->
-	      log_qos_failure "parameter \"kbps\" not an integer"; None
-	  | Not_found               ->
-	      log_qos_failure "necessary parameter \"kbps\" not found"; None
-	  | e                       ->
-	      log_qos_failure (Printf.sprintf "unexpected error: %s" (Printexc.to_string e)); None
-	end
-    | ""          -> None
-    | _           -> log_qos_failure (Printf.sprintf "unknown type: %s" qos_type); None in
-    
-  (* If we fail to find the network or bridge then assume this VIF is a dangling reference *)
-  try
-    let network_ref = vif_r.API.vIF_network in
-    let bridge = Db.Network.get_bridge ~__context ~self:network_ref in
-    let mtu = match vif_mtu with
-      | Some mtu -> mtu
-      | None -> Int64.to_int (Db.Network.get_MTU ~__context ~self:network_ref) in
-    Some {
-      domid = domid;
-      vif_ref = vif;
-      network_ref = network_ref;
-      devid = int_of_string vif_r.API.vIF_device;
-      protocol = protocol;
-      mac = mac;
-      mtu = mtu;
-      bridge = bridge;
-      rate = rate;
-      other_config = vif_r.API.vIF_other_config
-    }
-  with _ -> None 
-    
+	let mac = vif_r.API.vIF_MAC in
+	let vif_mtu = if (List.mem_assoc "mtu" vif_r.API.vIF_other_config) then
+		Some (int_of_string (List.assoc "mtu" vif_r.API.vIF_other_config)) else None in
+
+	let qos_type = vif_r.API.vIF_qos_algorithm_type in
+	let qos_params = vif_r.API.vIF_qos_algorithm_params in
+	let rate = match qos_type with
+	| "ratelimit" ->
+		let timeslice =
+		try Int64.of_string (List.assoc "timeslice_us" qos_params)
+		with _ -> 0L in
+		begin
+			try
+				let rate = Int64.of_string (List.assoc "kbps" qos_params) in
+				Some (rate, timeslice)
+			with
+			| Failure "int_of_string" ->
+				log_qos_failure "parameter \"kbps\" not an integer"; None
+			| Not_found               ->
+				log_qos_failure "necessary parameter \"kbps\" not found"; None
+			| e                       ->
+				log_qos_failure (Printf.sprintf "unexpected error: %s" (Printexc.to_string e)); None
+		end
+	| "" -> None
+	| _  -> log_qos_failure (Printf.sprintf "unknown type: %s" qos_type); None in
+
+	(* If we fail to find the network or bridge then assume this VIF is a dangling reference *)
+	try
+		let network_ref = vif_r.API.vIF_network in
+		let bridge = Db.Network.get_bridge ~__context ~self:network_ref in
+		let mtu = match vif_mtu with
+		| Some mtu -> mtu
+		| None -> Int64.to_int (Db.Network.get_MTU ~__context ~self:network_ref) in
+		Some {
+			domid = domid;
+			vif_ref = vif;
+			network_ref = network_ref;
+			devid = int_of_string vif_r.API.vIF_device;
+			protocol = protocol;
+			mac = mac;
+			mtu = mtu;
+			bridge = bridge;
+			rate = rate;
+			other_config = vif_r.API.vIF_other_config
+		}
+	with _ -> None 
+
 (** Return a list of vif records corresponding to all the non-dangling VIF references *)
 let vifs_of_vm ~__context ~vm domid = 
-  let vm_r = Db.VM.get_record ~__context ~self:vm in
-  let protocol = Helpers.device_protocol_of_string vm_r.API.vM_domarch in
-  
-  List.concat (List.map Opt.to_list (List.map (vif_of_vif ~__context ~vm vm_r domid protocol) vm_r.API.vM_VIFs))
+	let vm_r = Db.VM.get_record ~__context ~self:vm in
+	let protocol = Helpers.device_protocol_of_string vm_r.API.vM_domarch in
+
+	List.concat (List.map Opt.to_list (List.map (vif_of_vif ~__context ~vm vm_r domid protocol) vm_r.API.vM_VIFs))
 
 let device_of_vif vif = 
-  let backend = { Device_common.domid = 0; 
-		  kind = Device_common.Vif;
-		  devid = vif.devid } in
-  Device_common.device_of_backend backend vif.domid
+	let backend = { Device_common.domid = 0;
+		kind = Device_common.Vif;
+		devid = vif.devid } in
+	Device_common.device_of_backend backend vif.domid
 
