badf0e4a875d5459465c325d2d4b2282e24a3f06
David Scott <dave.scott@eu.citrix.com>
2011-12-12 15:03:33 +0000

    [SMAPI] Clean up all the bits and pieces when a failure occurs while starting a storage mirror.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/storage_migrate.ml
+++ ocaml/xapi/storage_migrate.ml
@@ -66,6 +66,12 @@ let with_activated_disk ~task ~sr ~vdi f =
 					Local.VDI.detach ~task ~dp:"migrate" ~sr ~vdi |> success |> unit)
 				vdi)
 
+let perform_cleanup_actions =
+	List.iter
+		(fun f ->
+			try f () with e -> error "Caught %s while performing cleanup actions" (Printexc.to_string e)
+		)
+
 let export' ~task ~sr ~vdi ~url ~dest =
 	let remote_url = Http.Url.of_string url in
 	let module Remote = Client(struct let rpc = rpc remote_url end) in
@@ -100,31 +106,39 @@ let export' ~task ~sr ~vdi ~url ~dest =
 			| None ->
 				debug "Creating a blank remote VDI";
 				Remote.VDI.create ~task ~sr:dest ~vdi_info:local_vdi ~params:[] |> success |> _vdi in
-	let dest_vdi_url = Printf.sprintf "%s/data/%s/%s" url dest dest_vdi.vdi in
-	debug "Will copy into new remote VDI: %s (%s)" dest_vdi.vdi dest_vdi_url;
+	let on_fail : (unit -> unit) list ref = ref [] in
+	try
+		on_fail := (fun () -> Remote.VDI.destroy ~task ~sr:dest ~vdi:dest_vdi.vdi |> success |> unit) :: !on_fail;
 
-	let base_vdi = Opt.map (fun x -> (fst x).vdi) nearest in
-	debug "Will base our copy from: %s" (Opt.default "None" base_vdi);
-	with_activated_disk ~task ~sr ~vdi:base_vdi
-		(fun base_path ->
-			with_activated_disk ~task ~sr ~vdi:(Some vdi)
-				(fun src ->
-					let args = [
-						"-src"; Opt.unbox src;
-						"-dest"; dest_vdi_url;
-						"-size"; Int64.to_string dest_vdi.virtual_size;
-						"-prezeroed"
-					] @ (Opt.default [] (Opt.map (fun x -> [ "-base"; x ]) base_path)) in
+		let dest_vdi_url = Printf.sprintf "%s/data/%s/%s" url dest dest_vdi.vdi in
+		debug "Will copy into new remote VDI: %s (%s)" dest_vdi.vdi dest_vdi_url;
 
-					let out, err = Forkhelpers.execute_command_get_output "/opt/xensource/libexec/sparse_dd" args in
-					debug "%s:%s" out err
-				)
-		);
-	debug "Updating remote content_id";
-	Remote.VDI.set_content_id ~task ~sr:dest ~vdi:dest_vdi.vdi ~content_id:local_vdi.content_id |> success |> unit;
-	(* XXX: this is useful because we don't have content_ids by default *)
-	Local.VDI.set_content_id ~task ~sr ~vdi:local_vdi.vdi ~content_id:local_vdi.content_id |> success |> unit;
-	dest_vdi
+		let base_vdi = Opt.map (fun x -> (fst x).vdi) nearest in
+		debug "Will base our copy from: %s" (Opt.default "None" base_vdi);
+		with_activated_disk ~task ~sr ~vdi:base_vdi
+			(fun base_path ->
+				with_activated_disk ~task ~sr ~vdi:(Some vdi)
+					(fun src ->
+						let args = [
+							"-src"; Opt.unbox src;
+							"-dest"; dest_vdi_url;
+							"-size"; Int64.to_string dest_vdi.virtual_size;
+							"-prezeroed"
+						] @ (Opt.default [] (Opt.map (fun x -> [ "-base"; x ]) base_path)) in
+
+						let out, err = Forkhelpers.execute_command_get_output "/opt/xensource/libexec/sparse_dd" args in
+						debug "%s:%s" out err
+					)
+			);
+		debug "Updating remote content_id";
+		Remote.VDI.set_content_id ~task ~sr:dest ~vdi:dest_vdi.vdi ~content_id:local_vdi.content_id |> success |> unit;
+		(* XXX: this is useful because we don't have content_ids by default *)
+		Local.VDI.set_content_id ~task ~sr ~vdi:local_vdi.vdi ~content_id:local_vdi.content_id |> success |> unit;
+		dest_vdi
+	with e ->
+		error "Caught %s: performing cleanup actions" (Printexc.to_string e);
+		perform_cleanup_actions !on_fail;
+		raise e
 
 let export ~task ~sr ~vdi ~url ~dest = Success (Vdi (export' ~task ~sr ~vdi ~url ~dest))
 
@@ -138,23 +152,33 @@ let start ~task ~sr ~vdi ~url ~dest =
 		try List.find (fun x -> x.vdi = vdi) vdis
 		with Not_found -> failwith (Printf.sprintf "Local VDI %s not found" vdi) in
 
-	(* XXX: this is a vhd-ism: We need to write into a .vhd leaf *)
-	let dummy = Remote.VDI.create ~task ~sr:dest ~vdi_info:local_vdi ~params:[] |> success |> _vdi in
-	let leaf = Remote.VDI.clone ~task ~sr:dest ~vdi:dummy.vdi ~vdi_info:local_vdi ~params:[] |> success |> _vdi in
-	debug "Created leaf on remote: %s" leaf.vdi;
-	(* XXX: this URI construction is fragile *)
-	let import_url =
-		let new_uri = Printf.sprintf "%s?vdi=%s" Constants.import_raw_vdi_uri leaf.vdi in
-		match remote_url with
-			| Http.Url.Http(a, b) -> Http.Url.Http(a, new_uri)
-			| Http.Url.File(a, b) -> Http.Url.File(a, new_uri) in
+	(* A list of cleanup actions to perform if the operation should fail. *)
+	let on_fail : (unit -> unit) list ref = ref [] in
+	try
+		(* XXX: this is a vhd-ism: We need to write into a .vhd leaf *)
+		let dummy = Remote.VDI.create ~task ~sr:dest ~vdi_info:local_vdi ~params:[] |> success |> _vdi in
+		on_fail := (fun () -> Remote.VDI.destroy ~task ~sr:dest ~vdi:dummy.vdi |> success |> unit) :: !on_fail;
+		let leaf = Remote.VDI.clone ~task ~sr:dest ~vdi:dummy.vdi ~vdi_info:local_vdi ~params:[] |> success |> _vdi in
+		on_fail := (fun () -> Remote.VDI.destroy ~task ~sr:dest ~vdi:leaf.vdi |> success |> unit) :: !on_fail;
+		debug "Created leaf on remote: %s" leaf.vdi;
+		(* XXX: this URI construction is fragile *)
+		let import_url =
+			let new_uri = Printf.sprintf "%s?vdi=%s" Constants.import_raw_vdi_uri leaf.vdi in
+			match remote_url with
+				| Http.Url.Http(a, b) -> Http.Url.Http(a, new_uri)
+				| Http.Url.File(a, b) -> Http.Url.File(a, new_uri) in
 
-	(* Enable mirroring on the local machine *)
-	let snapshot = Local.VDI.snapshot ~task ~sr ~vdi:local_vdi.vdi ~vdi_info:local_vdi ~params:["mirror", Http.Url.to_string import_url] |> success |> _vdi in
-	(* Copy the snapshot to the remote *)
-	let new_parent = export' ~task ~sr ~vdi:snapshot.vdi ~url ~dest in
-	Remote.VDI.compose ~task ~sr:dest ~vdi1:leaf.vdi ~vdi2:new_parent.vdi |> success |> unit;
-	Success (Vdi leaf)
+		(* Enable mirroring on the local machine *)
+		let snapshot = Local.VDI.snapshot ~task ~sr ~vdi:local_vdi.vdi ~vdi_info:local_vdi ~params:["mirror", Http.Url.to_string import_url] |> success |> _vdi in
+		on_fail := (fun () -> Local.VDI.destroy ~task ~sr ~vdi:snapshot.vdi |> success |> unit) :: !on_fail;
+		(* Copy the snapshot to the remote *)
+		let new_parent = export' ~task ~sr ~vdi:snapshot.vdi ~url ~dest in
+		Remote.VDI.compose ~task ~sr:dest ~vdi1:leaf.vdi ~vdi2:new_parent.vdi |> success |> unit;
+		Success (Vdi leaf)
+	with e ->
+		error "Caught %s: performing cleanup actions" (Printexc.to_string e);
+		perform_cleanup_actions !on_fail;
+		Failure (Internal_error (Printexc.to_string e))
 
 let stop ~task ~sr ~vdi =
 	(* Find the local VDI *)
