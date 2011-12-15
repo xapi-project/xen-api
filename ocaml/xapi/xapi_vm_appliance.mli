774cccdce1d59e43378020b52663fa5305a267a3
John Else <john.else@citrix.com>
2011-12-15 14:46:27 +0000

    Add missing copyright notice.
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_vm_appliance.mli
+++ ocaml/xapi/xapi_vm_appliance.mli
@@ -1,3 +1,17 @@
+(*
+ * Copyright (C) 2006-2011 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
 val create :
 	__context:Context.t -> name_label:string -> name_description:string -> [ `VM_appliance ] Ref.t
 val destroy :
