7cbc6b2fc7b468c2a1d673581006e7d035e5be1c
David Scott <dave.scott@eu.citrix.com>
2012-01-01 20:33:00 +0000

    [xenopsd]: switch over to using a new DB.read_exn function
    Most of the DB.read callers went on to call unbox, expecting a
    Does_not_exist exception if the object doesn't exist. Therefore
    we add a DB.read_exn which does this.

--- ocaml/xenops/xenops_server_simulator.ml
+++ ocaml/xenops/xenops_server_simulator.ml
@@ -39,6 +39,8 @@ end
 module DB = TypedTable(struct
 	include Domain
 	let namespace = "domain"
+	type key = string
+	let key x = [ x ]
 end)
 
 let updates = Updates.empty ()
@@ -48,7 +50,7 @@ let next_domid =
 	let get_next =
 		fun () -> match !next with
 			| None ->
-				let domains = List.filter_map (fun x -> DB.read [x]) (DB.list []) in
+				let domains = List.filter_map DB.read (DB.list []) in
 				let domids = List.map (fun x -> x.Domain.domid) domains in
 				let highest = List.fold_left max 0 domids in
 				next := Some (highest + 1);
@@ -61,21 +63,11 @@ let next_domid =
 		debug "using domid %d" result;
 		result
 
-let key_of vm = [ vm.Vm.id ]
-
 let m = Mutex.create ()
-
-let read x = match DB.read x with
-	| None ->
-		let k = String.concat "/" x in
-		debug "Failed to find key: %s" k;
-		raise (Exception(Does_not_exist("DB", k)))
-	| Some y -> y
 
 let create_nolock vm () =
 	debug "Domain.create vm=%s" vm.Vm.id;
-	let k = key_of vm in
-	if DB.exists k then begin
+	if DB.exists vm.Vm.id then begin
 		debug "VM.create_nolock %s: Already_exists" vm.Vm.id;
 		raise (Exception Already_exists)
 	end else begin
@@ -94,13 +86,12 @@ let create_nolock vm () =
 			pcis = [];
 			last_create_time = Unix.gettimeofday ();
 		} in
-		DB.write k domain
+		DB.write vm.Vm.id domain
 	end
 
 let get_state_nolock vm () =
-	let k = key_of vm in
-	if DB.exists k then begin
-		let d = read k in
+	if DB.exists vm.Vm.id then begin
+		let d = DB.read_exn vm.Vm.id in
 		{ halted_vm with
 			Vm.power_state = Running;
 			domids = [ d.Domain.domid ];
@@ -109,37 +100,31 @@ let get_state_nolock vm () =
 	end else halted_vm
 
 let get_domain_action_request_nolock vm () =
-	let k = key_of vm in
-	if DB.exists k then begin
-		let d = read k in
+	if DB.exists vm.Vm.id then begin
+		let d = DB.read_exn vm.Vm.id in
 		d.Domain.domain_action_request
 	end else Some Needs_poweroff
 
 let destroy_nolock vm () =
 	debug "Domain.destroy vm=%s" vm.Vm.id;
-	let k = key_of vm in
 	(* Idempotent *)
-	if DB.exists k then DB.delete k
+	if DB.exists vm.Vm.id then DB.delete vm.Vm.id
 
 let build_nolock vm vbds vifs () =
 	debug "Domain.build vm=%s" vm.Vm.id;
-	let k = key_of vm in
 	debug "setting built <- true";
-	DB.write k { read k with Domain.built = true }
+	DB.write vm.Vm.id { DB.read_exn vm.Vm.id with Domain.built = true }
 
 let create_device_model_nolock vm () =
 	debug "Domain.create_device_model vm=%s" vm.Vm.id;
-	let k = key_of vm in
-	DB.write k { read k with Domain.qemu_created = true }
+	DB.write vm.Vm.id { DB.read_exn vm.Vm.id with Domain.qemu_created = true }
 
 let destroy_device_model_nolock vm () =
 	debug "Domain.destroy_device_model vm=%s" vm.Vm.id;
-	let k = key_of vm in
-	DB.write k { read k with Domain.qemu_created = false }
+	DB.write vm.Vm.id { DB.read_exn vm.Vm.id with Domain.qemu_created = false }
 
 let request_shutdown_nolock vm reason () =
-	let k = key_of vm in
-	DB.write k { read k with Domain.domain_action_request =
+	DB.write vm.Vm.id { DB.read_exn vm.Vm.id with Domain.domain_action_request =
 			Some (match reason with
 				| Halt | PowerOff -> Needs_poweroff
 				| Reboot -> Needs_reboot
@@ -149,33 +134,28 @@ let request_shutdown_nolock vm reason () =
 	true
 
 let save_nolock vm _ data () =
-	let k = key_of vm in
-	DB.write k { read k with Domain.suspended = true }
+	DB.write vm.Vm.id { DB.read_exn vm.Vm.id with Domain.suspended = true }
 
 let restore_nolock vm data () =
-	let k = key_of vm in
-	DB.write k { read k with Domain.built = true }
+	DB.write vm.Vm.id { DB.read_exn vm.Vm.id with Domain.built = true }
 
 let do_pause_unpause_nolock vm paused () =
-	let k = key_of vm in
-	let d = read k in
+	let d = DB.read_exn vm.Vm.id in
 	if not d.Domain.built || (d.Domain.hvm && not(d.Domain.qemu_created))
 	then raise (Exception Domain_not_built)
-	else DB.write k { d with Domain.paused = paused }
+	else DB.write vm.Vm.id { d with Domain.paused = paused }
 
 let add_vif vm vif () =
-	let k = [ vm ] in
-	let d = read k in
+	let d = DB.read_exn vm in
 	let existing_positions = List.map (fun vif -> vif.Vif.position) d.Domain.vifs in
 	if List.mem vif.Vif.position existing_positions then begin
 		debug "VIF.plug %s.%s: Already exists" (fst vif.Vif.id) (snd vif.Vif.id);
 		raise (Exception Already_exists)
-	end else DB.write k { d with Domain.vifs = vif :: d.Domain.vifs }
+	end else DB.write vm { d with Domain.vifs = vif :: d.Domain.vifs }
 
 let add_vbd (vm: Vm.id) (vbd: Vbd.t) () =
 	debug "add_vbd";
-	let k = [ vm ] in
-	let d = read k in
+	let d = DB.read_exn vm in
 	(* there shouldn't be any None values in here anyway *)
 	let ps = List.map (fun vbd -> vbd.Vbd.position) d.Domain.vbds in
 	assert (not (List.mem None ps));
@@ -187,24 +167,22 @@ let add_vbd (vm: Vm.id) (vbd: Vbd.t) () =
 	if List.mem this_dn dns then begin
 		debug "VBD.plug %s.%s: Already exists" (fst vbd.Vbd.id) (snd vbd.Vbd.id);
 		raise (Exception Already_exists)
-	end else DB.write k { d with Domain.vbds = { vbd with Vbd.position = Some this_dn } :: d.Domain.vbds }
+	end else DB.write vm { d with Domain.vbds = { vbd with Vbd.position = Some this_dn } :: d.Domain.vbds }
 
 let add_pci (vm: Vm.id) (pci: Pci.t) () =
 	debug "add_pci";
-	let k = [ vm ] in
-	let d = read k in
+	let d = DB.read_exn vm in
 	let existing_positions = List.map (fun pci -> pci.Pci.position) d.Domain.pcis in
 	if List.mem pci.Pci.position existing_positions then begin
 		debug "PCI.plug %s.%s: Already exists" (fst pci.Pci.id) (snd pci.Pci.id);
 		raise (Exception Already_exists)
-	end else DB.write k { d with Domain.pcis = pci :: d.Domain.pcis }
+	end else DB.write vm { d with Domain.pcis = pci :: d.Domain.pcis }
 
 let pci_state vm pci () =
-	let k = [ vm ] in
-	if not (DB.exists k)
+	if not (DB.exists vm)
 	then unplugged_pci
 	else
-		let d = read k in
+		let d = DB.read_exn vm in
 		let this_one x = x.Pci.id = pci.Pci.id in
 		match List.filter this_one d.Domain.pcis with
 			| [ pci ] ->
@@ -215,11 +193,10 @@ let pci_state vm pci () =
 			| _ -> assert false (* at most one *)
 
 let vbd_state vm vbd () =
-	let k = [ vm ] in
-	if not (DB.exists k)
+	if not (DB.exists vm)
 	then unplugged_vbd
 	else
-		let d = read k in
+		let d = DB.read_exn vm in
 		let this_one x = x.Vbd.id = vbd.Vbd.id in
 		match List.filter this_one d.Domain.vbds with
 			| [ vbd ] ->
@@ -233,11 +210,10 @@ let vbd_state vm vbd () =
 				
 
 let vif_state vm vif () =
-	let k = [ vm ] in
-	if not (DB.exists k)
+	if not (DB.exists vm)
 	then unplugged_vif
 	else
-		let d = read k in
+		let d = DB.read_exn vm in
 		let this_one x = x.Vif.id = vif.Vif.id in
 		match List.filter this_one d.Domain.vifs with
 			| [ domain ] ->
@@ -249,28 +225,25 @@ let vif_state vm vif () =
 			| _ -> assert false (* at most one *)
 
 let remove_vif vm vif () =
-	let k = [ vm ] in
-	let d = read k in
+	let d = DB.read_exn vm in
 	let this_one x = x.Vif.id = vif.Vif.id in
 	if List.filter this_one d.Domain.vifs = []
 	then raise (Exception(Does_not_exist("VIF", Printf.sprintf "%s.%s" (fst vif.Vif.id) (snd vif.Vif.id))))
-	else DB.write k { d with Domain.vifs = List.filter (fun x -> not (this_one x)) d.Domain.vifs }
+	else DB.write vm { d with Domain.vifs = List.filter (fun x -> not (this_one x)) d.Domain.vifs }
 
 let remove_pci vm pci () =
-	let k = [ vm ] in
-	let d = read k in
+	let d = DB.read_exn vm in
 	let this_one x = x.Pci.id = pci.Pci.id in
 	if List.filter this_one d.Domain.pcis = []
 	then raise (Exception(Does_not_exist("PCI", Printf.sprintf "%s.%s" (fst pci.Pci.id) (snd pci.Pci.id))))
-	else DB.write k { d with Domain.pcis = List.filter (fun x -> not (this_one x)) d.Domain.pcis }
+	else DB.write vm { d with Domain.pcis = List.filter (fun x -> not (this_one x)) d.Domain.pcis }
 
 let remove_vbd vm vbd () =
-	let k = [ vm ] in
-	let d = read k in
+	let d = DB.read_exn vm in
 	let this_one x = x.Vbd.id = vbd.Vbd.id in
 	if List.filter this_one d.Domain.vbds = []
 	then raise (Exception(Does_not_exist("VBD", Printf.sprintf "%s.%s" (fst vbd.Vbd.id) (snd vbd.Vbd.id))))
-	else DB.write k { d with Domain.vbds = List.filter (fun x -> not (this_one x)) d.Domain.vbds }
+	else DB.write vm { d with Domain.vbds = List.filter (fun x -> not (this_one x)) d.Domain.vbds }
 	
 module VM = struct
 	let create _ vm = Mutex.execute m (create_nolock vm)
@@ -290,10 +263,9 @@ module VM = struct
 	let get_domain_action_request vm = Mutex.execute m (get_domain_action_request_nolock vm)
 
 	let get_internal_state vm =
-		vm |> key_of |> DB.read |> Opt.unbox |> Domain.rpc_of_t |> Jsonrpc.to_string
+		vm.Vm.id |> DB.read |> Opt.unbox |> Domain.rpc_of_t |> Jsonrpc.to_string
 	let set_internal_state vm s =
-		let k = key_of vm in
-		DB.write k (s |> Jsonrpc.of_string |> Domain.t_of_rpc)
+		DB.write vm.Vm.id (s |> Jsonrpc.of_string |> Domain.t_of_rpc)
 end
 
 module PCI = struct
@@ -330,14 +302,14 @@ end
 
 module DEBUG = struct
 	let trigger cmd args = match cmd, args with
-		| "reboot", k ->
-			let d = read k in
+		| "reboot", [ k ] ->
+			let d = DB.read_exn k in
 			DB.write k { d with Domain.domain_action_request = Some Needs_reboot };
-			Updates.add (Dynamic.Vm (List.hd k)) updates
-		| "halt", k ->
-			let d = read k in
+			Updates.add (Dynamic.Vm k) updates
+		| "halt", [ k ] ->
+			let d = DB.read_exn k in
 			DB.write k { d with Domain.domain_action_request = Some Needs_poweroff };
-			Updates.add (Dynamic.Vm (List.hd k)) updates
+			Updates.add (Dynamic.Vm k) updates
 		| _ ->
 			debug "DEBUG.trigger cmd=%s Not_supported" cmd;
 			raise (Exception Not_supported)
