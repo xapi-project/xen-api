f57491c148b7dde0ef13528241c1e5b7952c2373
David Scott <dave.scott@eu.citrix.com>
2012-01-03 17:14:54 +0000

    [storage-motion]: export metadata changes
    "xe vm-export --metadata include-snapshots=false"
      will create a metadata export without snapshots, important if we want
      to motion a single VM without its entire snapshot history coming along too

--- ocaml/xapi/cli_frontend.ml
+++ ocaml/xapi/cli_frontend.ml
@@ -1276,7 +1276,7 @@ there are two or more empty CD devices, please use the command 'vbd-insert' and 
    "vm-export",
     {
       reqd=["filename"];
-      optn=["preserve-power-state"; "compress"];
+      optn=["preserve-power-state"; "compress"; "include-snapshots"];
       help="Export a VM to <filename>.";
       implementation= With_fd Cli_operations.vm_export;
       flags=[Standard; Vm_selectors];
