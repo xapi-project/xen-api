f2a662b4f21df20cbd1908bf835c5e21f989da56
Rob Hoes <rob.hoes@citrix.com>
2012-02-22 11:23:42 +0000

    Stop using Netdev module in Create_misc
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_host.ml
+++ ocaml/xapi/xapi_host.ml
@@ -1266,7 +1266,7 @@ let license_apply ~__context ~host ~contents =
 
 let refresh_pack_info ~__context ~host =
 	debug "Refreshing software_version";
-	let software_version = Create_misc.make_software_version () in
+	let software_version = Create_misc.make_software_version ~__context in
 	Db.Host.set_software_version ~__context ~self:host ~value:software_version
 
 (* Network reset *)
