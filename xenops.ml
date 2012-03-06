cc7266a25ffa70ce696a60bef9420c70db3e3597
David Scott <dave.scott@eu.citrix.com>
2012-03-06 11:02:39 +0000

    Add xenops Query.info type

--- xenops.ml
+++ xenops.ml
@@ -5,5 +5,24 @@ let api =
     Interfaces.name = "xenops";
     description = "The Xen domain management API";
     type_decls = [];
-    interfaces = []
-  }
+    interfaces =
+      [
+        {
+          Interface.name = "Query";
+          description = "Query the properties of this service";
+	  type_decls = [ { TyDecl.name = "info";
+			   description = "Properties of this service";
+			   ty = Type.(Struct(
+			     ( "name", Basic String, "Human-readable name of this service" ),
+			     [ "vendor", Basic String, "Vendor of this service";
+			       "version", Basic String, "Version number of this service";
+			       "features", Array (Basic String), "A set of feature flags";
+			       "instance_id", Basic String, "A unique id for this instance, regenerated over service restart"
+			     ]
+			   ))
+			 }];
+          methods = [
+	  ]
+	}
+      ]
+    }
