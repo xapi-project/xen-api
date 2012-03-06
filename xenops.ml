a77b0b2e48c7d1c7326205805fb4c18ef09584e4
David Scott <dave.scott@eu.citrix.com>
2012-03-06 11:58:55 +0000

    xenops: Add VM.hvm_info

--- xenops.ml
+++ xenops.ml
@@ -57,16 +57,36 @@ let api =
 	}; {
           Interface.name = "Vm";
           description = "Types used to represent a VM configuration";
-	  type_decls = [ { TyDecl.name = "video_card";
-			   description = "Type of video hardware";
-			   ty = Type.(Variant(
-			     ("Cirrus", Unit, "Cirrus Logic"),
-			     [ "Standard_VGA", Unit, "Standard VGA" ]
-			   ));
-			 }
-
-		       ];
-	  methods = []
+	  methods = [];
+	  type_decls = [ {
+	    TyDecl.name = "video_card";
+	    description = "Type of video hardware";
+	    ty = Type.(Variant(
+	      ("Cirrus", Unit, "Cirrus Logic"),
+	      [ "Standard_VGA", Unit, "Standard VGA" ]
+	    ));
+	  }; {
+	    TyDecl.name = "hvm_info";
+	    description = "Hardware options specific to HVM VMs";
+	    ty = Type.(Struct(
+	      ("hap", Basic Boolean, "If true then Hardware Assisted Paging (HAP) will be enabled"),
+	      [ "shadow_multiplier", Basic Double, "Allow the amount of shadow memory to be increased beyond the default amount (1.0 means default)";
+		"timeoffset", Basic String, "Offset between the host clock (in UTC) and guest time stored in the virtual BIOS";
+		"video_mib", Basic Int64, "Amount of video memory (in MiB)";
+		"video", Type.Name "video_card", "Type of video hardware";
+		"acpi", Basic Boolean, "If true then ACPI will be enabled";
+		"serial", Option (Basic String), "";
+		"keymap", Option (Basic String), "";
+		"vnc_ip", Option (Basic String), "Specify the IP on which the VNC framebuffer will listen (default 127.0.0.1)";
+		"pci_emulations", Array (Basic String), "";
+		"pci_passthrough", Basic Boolean, "If true then PCI passthrough is enabled";
+		"boot_order", Basic String, "BIOS boot order ('cdn' = disk; cdrom; network)";
+		"qemu_disk_cmdline", Basic Boolean, "If true then the qemu commandline will be used to configure disks, otherwise xenstore will be used";
+		"qemu_stubdom", Basic Boolean, "If true then qemu will be run inside a stub domain";
+	      ]
+	       
+	    ));
+	  } ]
 	}
       ]
-    }
+  }
