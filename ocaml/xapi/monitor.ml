feea26b94cbad2d77c1d4ec9029480cc609012b6
David Scott <dave.scott@eu.citrix.com>
2012-03-18 16:38:01 +0000

    xapi: use the new xenopsd VIF.set_carrier
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/monitor.ml
+++ ocaml/xapi/monitor.ml
@@ -191,6 +191,7 @@ let update_loadavg () =
 
 type vif_device = {
 	pv: bool;
+	vif: Xenops_interface.Vif.id;
 	domid: int;
 	devid: int;
 }
@@ -199,9 +200,12 @@ let vif_device_of_string x =
 	try
 		let ty = String.sub x 0 3 and params = String.sub_to_end x 3 in
 		let domid, devid = Scanf.sscanf params "%d.%d" (fun x y -> x,y) in
+		let di = Xenctrl.with_intf (fun xc -> Xenctrl.domain_getinfo xc domid) in
+		let uuid = Uuid.uuid_of_int_array di.Xenctrl.handle |> Uuid.to_string in
+		let vif = (uuid, string_of_int devid) in
 		match ty with
-		| "vif" -> Some { pv = true; domid = domid; devid = devid }
-		| "tap" -> Some { pv = false; domid = domid; devid = devid }
+		| "vif" -> Some { pv = true; vif = vif; domid = domid; devid = devid }
+		| "tap" -> Some { pv = false; vif = vif; domid = domid; devid = devid }
 		| _ -> failwith "bad device"
 	with _ -> None
 
