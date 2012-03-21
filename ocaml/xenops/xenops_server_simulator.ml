d81ce89bbcbcd0a24cca96d2facd61d0e708e73b
David Scott <dave.scott@eu.citrix.com>
2012-03-21 22:46:17 +0000

    xapi: remap all the xenops errors to something sensible by default
    Specific functions (e.g. VBD.plug) get first refusal on the mapping, this
    new function is attempting to be a sensible default.

--- ocaml/xenops/xenops_server_simulator.ml
+++ ocaml/xenops/xenops_server_simulator.ml
@@ -391,7 +391,7 @@ module DEBUG = struct
 			end
 		| _ ->
 			debug "DEBUG.trigger cmd=%s Not_supported" cmd;
-			raise (Exception Not_supported)
+			raise (Exception(Unimplemented(cmd)))
 end
 
 let init () = ()
