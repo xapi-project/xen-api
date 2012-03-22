a391f4c274ed4fdaaa3edc1f4297812e0938ff3c
David Scott <dave.scott@eu.citrix.com>
2012-03-22 12:03:05 +0000

    xenopsd: add task.list, remove obvious source of task leaks
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xenops/xenops_interface.ml
+++ ocaml/xenops/xenops_interface.ml
@@ -306,6 +306,7 @@ module Task = struct
 
 	type t = {
 		id: id;
+		debug_info: string;
 		result: result;
 		subtasks: (string * result) list;
 	}
@@ -331,6 +332,7 @@ module TASK = struct
 	external stat: debug_info -> Task.id -> (Task.t option) * (error option) = ""
 	external cancel: debug_info -> Task.id -> (unit option) * (error option) = ""
 	external destroy: debug_info -> Task.id -> (unit option) * (error option) = ""
+	external list: debug_info -> (Task.t list option) * (error option) = ""
 end
 
 module HOST = struct
