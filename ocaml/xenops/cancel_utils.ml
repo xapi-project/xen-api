32b2c0b487653595f6e321c0a6a77d45e11eea1c
David Scott <dave.scott@eu.citrix.com>
2012-03-26 10:17:52 +0100

    xenopsd: when killing a subprocess to cancel it, catch the WSIGNALED exception and re-raise Cancelled

--- ocaml/xenops/cancel_utils.ml
+++ ocaml/xenops/cancel_utils.ml
@@ -19,6 +19,10 @@ open Xenstore
 open Xenops_helpers
 
 open Device_common
+
+module D = Debug.Debugger(struct let name = "xenops" end)
+open D
+
 let cancel_path_of_device ~xs device = backend_path_of_device ~xs device ^ "/tools/xenops/cancel"
 
 let cancel_path_of_domain ~xs domid = Printf.sprintf "%s/tools/xenops/cancel" (xs.Xs.getdomainpath domid)
@@ -48,6 +52,8 @@ let cancellable_subprocess (task: Xenops_task.t) ?env ?stdin ?(syslog_stdout=NoS
 	let stdinandpipes = Opt.map (fun str -> 
 		let (x,y) = Unix.pipe () in
 		(str,x,y)) stdin in
+	(* Used so that cancel -> kills subprocess -> Unix.WSIGNALED -> raise cancelled *)
+	let cancelled = ref false in
 	Pervasiveext.finally (fun () -> 
 		match with_logfile_fd "execute_command_get_out" (fun out_fd ->
 			with_logfile_fd "execute_command_get_err" (fun err_fd ->
@@ -57,7 +63,10 @@ let cancellable_subprocess (task: Xenops_task.t) ?env ?stdin ?(syslog_stdout=NoS
 					(fun () ->
 						let pid' = Forkhelpers.getpid t in
 						Xenops_task.with_cancel task
-							(fun () -> try Unix.kill pid' Sys.sigkill with _ -> ())
+							(fun () ->
+								cancelled := true;
+								try Unix.kill pid' Sys.sigkill with _ -> ()
+							)
 							(fun () ->
 								Opt.iter (fun (str,_,wr) -> Unixext.really_write_string wr str) stdinandpipes;
 								done_waitpid := true;
@@ -71,7 +80,11 @@ let cancellable_subprocess (task: Xenops_task.t) ?env ?stdin ?(syslog_stdout=NoS
 						| Unix.WEXITED 0 -> (out,err)
 						| Unix.WEXITED n -> raise (Spawn_internal_error(err,out,Unix.WEXITED n))
 						| Unix.WSTOPPED n -> raise (Spawn_internal_error(err,out,Unix.WSTOPPED n))
-						| Unix.WSIGNALED n -> raise (Spawn_internal_error(err,out,Unix.WSIGNALED n))
+						| Unix.WSIGNALED n ->
+							debug "Subprocess exitted with signal %d and cancel requested; raising Cancelled" n;
+							if !cancelled
+							then Xenops_task.raise_cancelled task
+							else raise (Spawn_internal_error(err,out,Unix.WSIGNALED n))
 				end
 			| Success(_,Failure(_,exn))
 			| Failure(_, exn) ->
