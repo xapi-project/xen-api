624ccd5c65d167ef02df6df063b39ba7d401dc95
Jerome Maloberti <jerome.maloberti@citrix.com>
2012-04-03 10:32:39 +0100

    Implement the alarm for status change of a bond.

--- ocaml/idl/api_messages.ml
+++ ocaml/idl/api_messages.ml
@@ -100,3 +100,15 @@ let vmpp_backup_missed_event = addMessage "VMPP_SNAPSHOT_MISSED_EVENT" (*'A sche
 let vmpp_archive_missed_event = addMessage "VMPP_ARCHIVE_MISSED_EVENT" (*'A scheduled archive event was missed due to another on-going scheduled archive run. This is unexpected behaviour, please re-configure your archive sub-policy'*)
 let vmpp_backup_failed = addMessage "VMPP_SNAPSHOT_FAILED" (*'The snapshot phase of the protection policy failed.'*)
 let vmpp_snapshot_archive_already_exists = addMessage "VMPP_SNAPSHOT_ARCHIVE_ALREADY_EXISTS" (*'Failed to archive the snapshot, it has already been archived on the specified target'*)
+
+let bond_status_changed = addMessage "BOND_STATUS_CHANGED" (* A link in a bond went down or came back up *)
+
+
+
+
+
+
+
+
+
+
