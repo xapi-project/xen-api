58a503ac1835f152c5f5e399e01865b5958fdf15
Jerome Maloberti <jerome.maloberti@citrix.com>
2012-04-12 15:00:24 +0100

    Pass the VIF map to xenops.
    Signed-off-by: Jerome Maloberti <jerome.maloberti@citrix.com>

--- ocaml/xenops-cli/xn.ml
+++ ocaml/xenops-cli/xn.ml
@@ -527,7 +527,7 @@ let resume x disk =
 let migrate x url =
 	let open Vm in
 	let vm, _ = find_by_name x in
-	Client.VM.migrate dbg vm.id [] url |> wait_for_task dbg
+	Client.VM.migrate dbg vm.id [] [] url |> wait_for_task dbg
 
 let trim limit str =
 	let l = String.length str in
