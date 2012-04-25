7c0d8fb10b5ff07b494834341669764e731b98fa
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2012-04-25 02:23:58 +0100

    Split the NBD library into Lwt and non-Lwt versions.
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- nbd/nbd.mli
+++ nbd/nbd.mli
@@ -1,30 +1,44 @@
+val nbd_cmd_read : int32
+val nbd_cmd_write : int32
+val nbd_cmd_disc : int32
+val nbd_cmd_flush : int32
+val nbd_cmd_trim : int32
+val nbd_request_magic : int32
+val nbd_reply_magic : int32
+val nbd_flag_has_flags : int
+val nbd_flag_read_only : int
+val nbd_flag_send_flush : int
+val nbd_flag_send_fua : int
+val nbd_flag_rotational : int
+val nbd_flag_send_trim : int
+val init_passwd : string
+val opts_magic : int64
+val cliserv_magic : int64
 type flag =
     NBD_read_only
   | NBD_send_flush
   | NBD_send_fua
   | NBD_rotational
   | NBD_send_trim
-
+type cmd =
+    NBD_cmd_read
+  | NBD_cmd_write
+  | NBD_cmd_disc
+  | NBD_cmd_flush
+  | NBD_cmd_trim
+val flags_of_flags : int -> flag list
+module Request :
+  sig type t = { ty : cmd; handle : int64; from : int64; len : int32; } end
+module Reply : sig type t = { error : int32; handle : int64; } end
+val ty_of_int32 : int32 -> cmd
+val int32_of_ty : cmd -> int32
+val parse_request : string * int * int -> Request.t
+val parse_reply : string * int * int -> Reply.t
+val construct_request : Request.t -> string
+val construct_reply : Reply.t -> string
+val get_int64 : string * int * int -> int64
+val get_int32 : string * int * int -> int32
 val negotiate : Unix.file_descr -> int64 * flag list
-
 val read : Unix.file_descr -> int64 -> int32 -> string option
-
 val write : Unix.file_descr -> string -> int64 -> int32 option
-
 val connect : string -> int -> Unix.file_descr * int64 * flag list
-
-
-module Lwt :
-  sig
-	type t
-
-    val negotiate : Lwt_unix.file_descr -> (t * int64 * flag list) Lwt.t
-
-    val connect :
-      string -> int -> (t * int64 * flag list) Lwt.t
-
-    val write : t -> string -> int64 -> unit Lwt.t
-
-    val read : t -> int64 -> int32 -> string Lwt.t
-  end
-
--- nbd/nbd_test.ml
+++ nbd/nbd_test.ml
@@ -1,7 +1,7 @@
 
 
 let test host port =
-	try_lwt
+	try
         let test_str1 = String.make 512 'a' in
         let test_str2 = String.make 512 'b' in
         let test_str3 = String.make 512 'c' in
@@ -11,26 +11,25 @@ let test host port =
         let test_str7 = String.make 512 'g' in
         let test_str8 = String.make 512 'h' in
 		Printf.printf "Connecting...\n";
-		lwt (sock,sz,flags) = Nbd.Lwt.connect host port in
+		let (sock,sz,flags) = Nbd.connect host port in
         Printf.printf "Connected: size=%Ld\n" sz;
-        let t1 = Nbd.Lwt.write sock test_str1 0L in
-        let t2 = Nbd.Lwt.write sock test_str2 512L in
-        let t3 = Nbd.Lwt.write sock test_str3 1024L in
-        let t4 = Nbd.Lwt.write sock test_str4 1536L in
-        let t5 = Nbd.Lwt.write sock test_str5 2048L in
-        let t6 = Nbd.Lwt.write sock test_str6 2560L in
-        let t7 = Nbd.Lwt.write sock test_str7 3072L in
-        let t8 = Nbd.Lwt.write sock test_str8 3584L in
+        let _ = Nbd.write sock test_str1 0L in
+        let _ = Nbd.write sock test_str2 512L in
+        let _ = Nbd.write sock test_str3 1024L in
+        let _ = Nbd.write sock test_str4 1536L in
+        let _ = Nbd.write sock test_str5 2048L in
+        let _ = Nbd.write sock test_str6 2560L in
+        let _ = Nbd.write sock test_str7 3072L in
+        let _ = Nbd.write sock test_str8 3584L in
 
-		lwt () = Lwt.join [t1; t2; t3; t4; t5; t6; t7; t8] in
         Printf.printf "Written\n";
-        lwt str2 = Nbd.Lwt.read sock 0L 4096l in
+        let Some str2 = Nbd.read sock 0L 4096l in
         Printf.printf "%s\n" str2;
-        Lwt.return ()
+		()
     with e -> 
 		Printf.printf "Caught exception: %s" (Printexc.to_string e);
-		Lwt.return ()
+		()
 
 let _ =
-	Lwt_main.run (test Sys.argv.(1) (int_of_string Sys.argv.(2)))
+	test Sys.argv.(1) (int_of_string Sys.argv.(2))
 
