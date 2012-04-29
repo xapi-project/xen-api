8264b748b7b879851f1f89d01c2ef4eb595c5465
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2012-04-29 15:29:01 +0100

    Permit shutdown,reboot,start while migrate_send in current_operations
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- ocaml/xapi/xapi_vm_lifecycle.ml
+++ ocaml/xapi/xapi_vm_lifecycle.ml
@@ -99,7 +99,13 @@ let is_allowed_concurrently ~(op:API.vm_operations) ~current_ops =
 	and allowed_operations = (* a list of valid state -> operation *)
 		[ [`snapshot_with_quiesce], `snapshot;
 		  [`reverting],             `hard_shutdown;
-		  [`migrate_send],               `metadata_export ] in
+		  [`migrate_send],               `metadata_export;
+		  [`migrate_send],          `hard_shutdown;
+		  [`migrate_send],          `clean_shutdown;
+          [`migrate_send],          `hard_reboot;
+		  [`migrate_send],          `clean_reboot;
+          [`migrate_send],          `start;
+          [`migrate_send],          `start_on;] in
 	let state_machine () = 
 		let current_state = List.map snd current_ops in
 		List.exists (fun (state, transition) -> state = current_state && transition = op) allowed_operations
