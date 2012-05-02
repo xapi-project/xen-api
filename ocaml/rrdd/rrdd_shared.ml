990898484673828238fd44fd617300f6564e27da
Rok Strniša <rok.strnisa@citrix.com>
2012-05-02 16:23:36 +0100

    Moved code for one HTTP handler, party into rrdd_proxy, partly into rrdd_http_handler.
    Signed-off-by: Rok Strnisa <rok.strnisa@citrix.com>

---
+++ ocaml/rrdd/rrdd_shared.ml
@@ -0,0 +1,28 @@
+let mutex = Mutex.create ()
+
+type rrd_info = {
+	rrd: Rrd.rrd;
+	mutable dss: Ds.ds list;
+	mutable domid: int;
+}
+
+(* RRDs *)
+let vm_rrds : (string, rrd_info) Hashtbl.t = Hashtbl.create 32
+let host_rrd : rrd_info option ref = ref None
+
+let rrd_of_fd fd =
+	let ic = Unix.in_channel_of_descr fd in
+	let input = Xmlm.make_input (`Channel ic) in
+	Rrd.from_xml input
+
+(** Helper function - path is the path to the file _without the extension .gz_ *)
+let rrd_of_gzip path =
+	let gz_path = path ^ ".gz" in
+	let gz_exists = try let (_: Unix.stats) = Unix.stat gz_path in true with _ -> false in
+	if gz_exists then begin
+		Unixext.with_file gz_path [ Unix.O_RDONLY ] 0o0
+			(fun fd -> Gzip.decompress_passive fd rrd_of_fd)
+	end else begin
+		(* If this fails, let the exception propagate *)
+		Unixext.with_file path [ Unix.O_RDONLY ] 0 rrd_of_fd
+	end
