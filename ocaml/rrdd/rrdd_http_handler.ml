099ef426b447d34223010b811763ec298f1e7dd8
Rok Strniša <rok.strnisa@citrix.com>
2012-05-03 10:17:00 +0100

    Re-coded the get_host_rrd HTTP handler for rrdd.
    Signed-off-by: Rok Strnisa <rok.strnisa@citrix.com>

--- ocaml/rrdd/rrdd_http_handler.ml
+++ ocaml/rrdd/rrdd_http_handler.ml
@@ -6,9 +6,10 @@ open Rrdd_shared
 
 (* A handler for unarchiving RRDs. Only called on pool master. *)
 let unarchive_rrd_handler (req: Http.Request.t) (s : Unix.file_descr) _ =
+	debug "unarchive_rrd_handler: start";
 	let query = req.Http.Request.query in
-	let vm_uuid = List.assoc "uuid" query in
-	let path = Xapi_globs.xapi_rrd_location ^ "/" ^ vm_uuid in
+	let uuid = List.assoc "uuid" query in
+	let path = Xapi_globs.xapi_rrd_location ^ "/" ^ uuid in
 	let rrd = rrd_of_gzip path in
 	let header_content =
 		Http.http_200_ok ~version:"1.0" ~keep_alive:false ()
@@ -22,10 +23,24 @@ let unarchive_rrd_handler (req: Http.Request.t) (s : Unix.file_descr) _ =
  * in xapi.
  *)
 let get_vm_rrd_handler (req: Http.Request.t) (s : Unix.file_descr) _ =
-	debug "put_rrd_handler: start";
+	debug "get_vm_rrd_handler: start";
 	let query = req.Http.Request.query in
 	let vm_uuid = List.assoc "uuid" query in
 	let rrd = Mutex.execute mutex
 		(fun () -> Rrd.copy_rrd (Hashtbl.find vm_rrds vm_uuid).rrd) in
 	Http_svr.headers s (Http.http_200_ok ~version:"1.0" ~keep_alive:false ());
 	Rrd.to_fd rrd s
+
+(* TODO: document! *)
+let get_host_rrd_handler (req: Http.Request.t) (s : Unix.file_descr) _ =
+	debug "get_host_rrd_handler: start";
+	let query = req.Http.Request.query in
+	let rrd = Mutex.execute mutex (fun () ->
+		debug "Received request for Host RRD";
+		Rrd.copy_rrd (match !host_rrd with
+			Some rrdi -> rrdi.rrd | None -> failwith "No host RRD available")
+	) in
+	Http_svr.headers s
+		(Http.http_200_ok ~version:"1.0" ~keep_alive:false () @
+			["Access-Control-Allow-Origin: *"]);
+	Rrd.to_fd ~json:(List.mem_assoc "json" query) rrd s
