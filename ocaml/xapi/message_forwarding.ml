2d6b82b8f219d5b35828f102b7957ec64b0e9b89
Jerome Maloberti <jerome.maloberti@citrix.com>
2012-05-04 16:24:41 +0100

    Implement VM.import_convert and add an SR parameter.
    Signed-off-by: Jerome Maloberti <jerome.maloberti@citrix.com>

--- ocaml/xapi/message_forwarding.ml
+++ ocaml/xapi/message_forwarding.ml
@@ -1908,9 +1908,10 @@ module Forward = functor(Local: Custom_actions.CUSTOM_ACTIONS) -> struct
 			info "VM.set_appliance: self = '%s'; value = '%s';" (vm_uuid ~__context self) (vm_appliance_uuid ~__context value);
 			Local.VM.set_appliance ~__context ~self ~value
 
-		let import_convert ~__context ~_type ~username ~password ~remote_config =
+		let import_convert ~__context ~_type ~username ~password ~sr ~remote_config =
 			info "VM.import_convert: type = '%s'; remote_config = '%s;'"
-				_type (String.concat "," (List.map (fun (k,v) -> k ^ "=" ^ v) remote_config))
+				_type (String.concat "," (List.map (fun (k,v) -> k ^ "=" ^ v) remote_config));
+			Local.VM.import_convert ~__context ~_type ~username ~password ~sr ~remote_config
 	end
 
 	module VM_metrics = struct
