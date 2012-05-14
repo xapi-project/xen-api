be24e7a23b54a0dd37ec78e48a3a947421cba2bc
David Scott <dave.scott@eu.citrix.com>
2012-05-14 18:18:51 +0100

    [squeezed] wire up the server-side using rpc-light
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xenops/memory_interface.ml
+++ ocaml/xenops/memory_interface.ml
@@ -21,21 +21,25 @@ let json_path = "/var/xapi/memory.json"
 type reservation_id = string
 
 exception Cannot_free_this_much_memory of (int64 * int64)
-exception Domains_refused_to_cooperate of int list
-exception Unknown_reservation of reservation_id
-exception Invalid_memory_value of int64
+exception Domains_refused_to_cooperate of (int list)
+exception Unknown_reservation of (reservation_id)
+exception Invalid_memory_value of (int64)
 
 type debug_info = string
 
+type session_id = string
+
 external get_diagnostics: debug_info -> string = ""
 
-external login: debug_info -> string -> string = ""
+external login: debug_info -> string -> session_id = ""
 
-external reserve_memory_range: debug_info -> int64 -> int64 -> reservation_id * int64 = ""
+external reserve_memory: debug_info -> session_id -> int64 -> reservation_id = ""
 
-external delete_reservation: debug_info -> reservation_id -> unit = ""
+external reserve_memory_range: debug_info -> session_id -> int64 -> int64 -> reservation_id * int64 = ""
 
-external transfer_reservation_to_domain: debug_info -> reservation_id -> int -> unit = ""
+external delete_reservation: debug_info -> session_id -> reservation_id -> unit = ""
+
+external transfer_reservation_to_domain: debug_info -> session_id -> reservation_id -> int -> unit = ""
 
 external balance_memory: debug_info -> unit = ""
 
