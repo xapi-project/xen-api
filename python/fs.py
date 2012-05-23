7b2591dbd786f2f902cb0bb015fcfe521caacff5
David Scott <dave.scott@eu.citrix.com>
2012-05-23 16:15:25 +0100

    fs: fix up interface following "debug_info" change; SR.scan and VDI.create now work

--- python/fs.py
+++ python/fs.py
@@ -23,21 +23,21 @@ import os, sys, commands, xmlrpclib
 import xcp
 from storage import *
 
-# [run task cmd] executes [cmd], throwing a BackendError if exits with
+# [run dbg cmd] executes [cmd], throwing a BackendError if exits with
 # a non-zero exit code.
-def run(task, cmd):
+def run(dbg, cmd):
     code, output = commands.getstatusoutput(cmd)
     if code <> 0:
-        log("%s: %s exitted with code %d: %s" % (task, cmd, code, output))
+        log("%s: %s exitted with code %d: %s" % (dbg, cmd, code, output))
         raise (BackendError("command failed", [ str(code), output ]))
-    log("%s: %s" % (task, cmd))
+    log("%s: %s" % (dbg, cmd))
     return output
 
 # Use Linux "losetup" to create block devices from files
 class Loop:
-    # [_find task path] returns the loop device associated with [path]
-    def _find(self, root, task, path):
-        for line in run(task, "losetup -a").split("\n"):
+    # [_find dbg path] returns the loop device associated with [path]
+    def _find(self, root, dbg, path):
+        for line in run(dbg, "losetup -a").split("\n"):
             line = line.strip()
             if line <> "":
                 bits = line.split()
@@ -46,19 +46,19 @@ class Loop:
                 if this_path == path:
                     return loop
         return None
-    # [add task path] creates a new loop device for [path] and returns it
-    def add(self, root, task, path):
-        run(task, "losetup -f %s" % path)
-        return self._find(root, task, path)
-    # [remove task path] removes the loop device associated with [path]
-    def remove(self, root, task, path):
-        loop = self._find(root, task, path)
-        run(task, "losetup -d %s" % loop)
+    # [add dbg path] creates a new loop device for [path] and returns it
+    def add(self, root, dbg, path):
+        run(dbg, "losetup -f %s" % path)
+        return self._find(root, dbg, path)
+    # [remove dbg path] removes the loop device associated with [path]
+    def remove(self, root, dbg, path):
+        loop = self._find(root, dbg, path)
+        run(dbg, "losetup -d %s" % loop)
 
 # [path_of_vdi vdi] returns the path in the local filesystem corresponding
 # to vdi location [vdi]
 def path_of_vdi(root, vdi):
-    return root + vdi
+    return root + "/" + vdi
 
 disk_suffix = ".raw"
 metadata_suffix = ".xml"
@@ -80,30 +80,31 @@ class SR(SR_skeleton):
     def __init__(self):
         SR_skeleton.__init__(self)
 
-    def list(self):
+    def list(self, dbg):
         return paths.keys()
-    def create(self, sr, device_config, physical_size):
+    def create(self, dbg, sr, device_config, physical_size):
         path = device_config["path"]
         if path in paths:
             raise (Sr_attached(sr))
         if not(os.path.exists(path)):
             raise Backend_error("SR directory doesn't exist", [ path ])
-    def attach(self, sr, device_config):
+    def attach(self, dbg, sr, device_config):
         path = device_config["path"]
         if not(os.path.exists(path)):
             raise Backend_error("SR directory doesn't exist", [ path ])
         paths[sr] = path
-    def detach(self, sr):
+    def detach(self, dbg, sr):
         if not sr in paths:
             raise Sr_not_attached(sr)
         del paths[sr]
-    def destroy(self, sr):
+    def destroy(self, dbg, sr):
         pass
-    def reset(self, sr):
+    def reset(self, dbg, sr):
         """Operations which act on Storage Repositories"""
         raise Unimplemented("SR.reset")
-    def scan(self, sr):
-        global root
+    def scan(self, dbg, sr):
+        if not sr in paths:
+            raise Sr_not_attached(sr)
         log("scanning")
         results = []
         root = paths[sr]
@@ -112,7 +113,8 @@ class SR(SR_skeleton):
                 path = root + "/" + name
                 f = open(path, "r")
                 try:
-                    vdi_info = xmlrpblic.loads(f.read())
+                    vdi_info = xmlrpclib.loads(f.read())[0][0]
+                    log("vdi_info = %s" % repr(vdi_info))
                     results.append(vdi_info)
                 finally:
                     f.close()
@@ -123,42 +125,47 @@ class VDI(VDI_skeleton):
         VDI_skeleton.__init__(self)
         self.device = Loop()
 
-    def create(self, sr, vdi_info, params):
-        filename = run(task, "uuidgen")
+    def create(self, dbg, sr, vdi_info, params):
+        if not sr in paths:
+            raise Sr_not_attached(sr)
+        filename = run(dbg, "uuidgen")
         root = paths[sr]
-        run(task, "dd if=/dev/zero of=%s%s bs=1 count=0 seek=%s" % (path_of_vdi(root, filename), disk_suffix, vdi_info["virtual_size"]))
+        run(dbg, "dd if=/dev/zero of=%s%s bs=1 count=0 seek=%s" % (path_of_vdi(root, filename), disk_suffix, vdi_info["virtual_size"]))
         vdi_info["vdi"] = filename
         f = open(path_of_vdi(root, filename) + metadata_suffix, "w")
         try:
-            f.write(xmlrpclib.dumps(vdi_info))
+            try:
+                f.write(xmlrpclib.dumps((vdi_info,), allow_none=True))
+            except Exception, e:
+                log("Exception writing metadata: %s" % (str(e)))
         finally:
             f.close()
             return vdi_info
-    def snapshot(self, sr, vdi, vdi_info, params):
+    def snapshot(self, dbg, sr, vdi, vdi_info, params):
         """Operations which operate on Virtual Disk Images"""
         raise Unimplemented("VDI.snapshot")
-    def clone(self, sr, vdi, vdi_info, params):
+    def clone(self, dbg, sr, vdi, vdi_info, params):
         """Operations which operate on Virtual Disk Images"""
         raise Unimplemented("VDI.clone")
-    def destroy(self, sr, vdi):
+    def destroy(self, dbg, sr, vdi):
         root = paths[sr]
         if not (os.path.exists(path_of_vdi(root, vdi) + disk_suffix)):
             raise Vdi_does_not_exist(vdi)
-        run(task, "rm -f %s%s" % (path_of_vdi(root, vdi), disk_suffix))
-        run(task, "rm -f %s%s" % (path_of_vdi(root, vdi), metadata_suffix))
-    def attach(self, dp, sr, vdi, read_write):
+        run(dbg, "rm -f %s%s" % (path_of_vdi(root, vdi), disk_suffix))
+        run(dbg, "rm -f %s%s" % (path_of_vdi(root, vdi), metadata_suffix))
+    def attach(self, dbg, dp, sr, vdi, read_write):
         path = path_of_vdi(root, vdi) + disk_suffix
-        loop = self.device.add(root, task, path)
+        loop = self.device.add(root, dbg, path)
         log("loop = %s" % repr(loop))
         return { "params": loop,
                  "xenstore_data": [] }
-    def activate(self, dp, sr, vdi):
+    def activate(self, dbg, dp, sr, vdi):
         pass
-    def deactivate(self, dp, sr, vdi):
+    def deactivate(self, dbg, dp, sr, vdi):
         pass
-    def detach(self, dp, sr, vdi):
+    def detach(self, dbg, dp, sr, vdi):
         path = path_of_vdi(root, vdi) + disk_suffix
-        self.device.remove(task, path)
+        self.device.remove(dbg, path)
         
 if __name__ == "__main__":
     from optparse import OptionParser
