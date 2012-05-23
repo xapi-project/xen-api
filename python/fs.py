d297378c509066a48a043b38a44a244bd5a9ef38
David Scott <dave.scott@eu.citrix.com>
2012-05-23 11:52:50 +0100

    fs: can now listen on a Unix domain socket and respond to Query messages.

--- python/fs.py
+++ python/fs.py
@@ -18,11 +18,10 @@
 
 # WARNING: this API is considered to be unstable and may be changed at-will
 
-import os, sys, commands, json
+import os, sys, commands, xmlrpclib
 
+import xcp
 from storage import *
-
-root = "/sr/"
 
 # [run task cmd] executes [cmd], throwing a BackendError if exits with
 # a non-zero exit code.
@@ -37,8 +36,7 @@ def run(task, cmd):
 # Use Linux "losetup" to create block devices from files
 class Loop:
     # [_find task path] returns the loop device associated with [path]
-    def _find(self, task, path):
-        global root
+    def _find(self, root, task, path):
         for line in run(task, "losetup -a").split("\n"):
             line = line.strip()
             if line <> "":
@@ -49,55 +47,34 @@ class Loop:
                     return loop
         return None
     # [add task path] creates a new loop device for [path] and returns it
-    def add(self, task, path):
+    def add(self, root, task, path):
         run(task, "losetup -f %s" % path)
-        return self._find(task, path)
+        return self._find(root, task, path)
     # [remove task path] removes the loop device associated with [path]
-    def remove(self, task, path):
-        loop = self._find(task, path)
+    def remove(self, root, task, path):
+        loop = self._find(root, task, path)
         run(task, "losetup -d %s" % loop)
-
-# Use FreeBSD "mdconfig" to create block devices from files
-class Mdconfig:
-    # [_find task path] returns the unit (mdX) associated with [path]
-    def _find(self, task, path):
-        # md0	vnode	 1024M	/root/big.img
-        for line in run(task, "mdconfig -l -v").split("\n"):
-            if line == "":
-                continue
-            bits = line.split()
-            this_path = bits[3]
-            if this_path == path:
-                return bits[0] # md0
-        return None
-    # [add task path] returns a block device associated with [path]
-    def add(self, task, path):
-        return "/dev/" + run(task, "mdconfig -a -t vnode -f %s" % path)
-    # [remove task path] removes the block device associated with [path]
-    def remove(self, task, path):
-        md = self._find(task, path)
-        if md:
-            run(task, "mdconfig -d -u %s" % md) 
 
 # [path_of_vdi vdi] returns the path in the local filesystem corresponding
 # to vdi location [vdi]
-def path_of_vdi(vdi):
-    global root
+def path_of_vdi(root, vdi):
     return root + vdi
 
 disk_suffix = ".raw"
-metadata_suffix = ".json"
+metadata_suffix = ".xml"
 
 class Query(Query_skeleton):
     def __init__(self):
         Query_skeleton.__init__(self)
     def query(self):
-        result = {}
-        result["query_result"] = { "name": "RawFiles",
-                                   "vendor": "XCP",
-                                   "version": "0.1",
-                                   "features": []
-                                   }
+        return { "name": "RawFiles",
+                 "vendor": "XCP",
+                 "version": "0.1",
+                 "features": []
+                 }
+
+# Store a mapping from attached SR -> path
+paths = {}
 
 class SR(SR_skeleton):
     def __init__(self):
@@ -107,10 +84,12 @@ class SR(SR_skeleton):
         """Operations which act on Storage Repositories"""
         raise UnimplementedException("SR", "create")
     def attach(self, sr, device_config):
-        if not(os.path.exists(root)):
-            raise BackendError("SR directory doesn't exist", [ root ])
+        path = device_config["path"]
+        if not(os.path.exists(path)):
+            raise BackendError("SR directory doesn't exist", [ path ])
+        paths[sr] = path
     def detach(self, sr):
-        pass
+        del paths[sr]
     def destroy(self, sr):
         pass
     def reset(self, sr):
@@ -120,13 +99,14 @@ class SR(SR_skeleton):
         global root
         log("scanning")
         results = []
+        root = paths[sr]
         for name in os.listdir(root):
             if name.endswith(metadata_suffix):
                 path = root + "/" + name
                 f = open(path, "r")
                 try:
-                    vdi_info = json.loads(f.read())
-                    results.append(smapiv2.make_vdi_info(vdi_info))
+                    vdi_info = xmlrpblic.loads(f.read())
+                    results.append(vdi_info)
                 finally:
                     f.close()
         return results
@@ -134,14 +114,16 @@ class SR(SR_skeleton):
 class VDI(VDI_skeleton):
     def __init__(self):
         VDI_skeleton.__init__(self)
+        self.device = Loop()
 
     def create(self, sr, vdi_info, params):
         filename = run(task, "uuidgen")
-        run(task, "dd if=/dev/zero of=%s%s bs=1 count=0 seek=%s" % (path_of_vdi(filename), disk_suffix, vdi_info["virtual_size"]))
+        root = paths[sr]
+        run(task, "dd if=/dev/zero of=%s%s bs=1 count=0 seek=%s" % (path_of_vdi(root, filename), disk_suffix, vdi_info["virtual_size"]))
         vdi_info["vdi"] = filename
-        f = open(path_of_vdi(filename) + metadata_suffix, "w")
+        f = open(path_of_vdi(root, filename) + metadata_suffix, "w")
         try:
-            f.write(json.dumps(vdi_info))
+            f.write(xmlrpclib.dumps(vdi_info))
         finally:
             f.close()
             return vdi_info
@@ -152,21 +134,23 @@ class VDI(VDI_skeleton):
         """Operations which operate on Virtual Disk Images"""
         raise UnimplementedException("VDI", "clone")
     def destroy(self, sr, vdi):
-        if not (os.path.exists(path_of_vdi(vdi) + disk_suffix)):
+        root = paths[sr]
+        if not (os.path.exists(path_of_vdi(root, vdi) + disk_suffix)):
             raise Vdi_does_not_exist(vdi)
-        run(task, "rm -f %s%s" % (path_of_vdi(vdi), disk_suffix))
-        run(task, "rm -f %s%s" % (path_of_vdi(vdi), metadata_suffix))
+        run(task, "rm -f %s%s" % (path_of_vdi(root, vdi), disk_suffix))
+        run(task, "rm -f %s%s" % (path_of_vdi(root, vdi), metadata_suffix))
     def attach(self, dp, sr, vdi, read_write):
-        path = path_of_vdi(vdi) + disk_suffix
-        loop = self.device.add(task, path)
+        path = path_of_vdi(root, vdi) + disk_suffix
+        loop = self.device.add(root, task, path)
         log("loop = %s" % repr(loop))
-        return loop
+        return { "params": loop,
+                 "xenstore_data": [] }
     def activate(self, dp, sr, vdi):
         pass
     def deactivate(self, dp, sr, vdi):
         pass
     def detach(self, dp, sr, vdi):
-        path = path_of_vdi(vdi) + disk_suffix
+        path = path_of_vdi(root, vdi) + disk_suffix
         self.device.remove(task, path)
         
 if __name__ == "__main__":
@@ -176,24 +160,31 @@ if __name__ == "__main__":
     parser.add_option("-l", "--log", dest="logfile", help="log to LOG", metavar="LOG")
     parser.add_option("-p", "--port", dest="port", help="listen on PORT", metavar="PORT")
     parser.add_option("-i", "--ip-addr", dest="ip", help="listen on IP", metavar="IP")
+    parser.add_option("-s", "--socket", dest="socket", help="listen on Unix domain socket", metavar="SOCK")
     parser.add_option("-d", "--daemon", action="store_true", dest="daemon", help="run as a background daemon", metavar="DAEMON")
     (options, args) = parser.parse_args()
     if options.logfile:
-        from smapiv2 import reopenlog
+        from xcp import reopenlog
         reopenlog(options.logfile)
-    if not options.ip and not options.ip:
-        print >>sys.stderr, "Need an --ip-addr and --port. Use -h for help"
+    tcp = options.ip and options.port
+    unix = options.socket
+    if not tcp and not unix:
+        print >>sys.stderr, "Need an --ip-addr and --port or a --socket. Use -h for help"
         sys.exit(1)
 
-    ip = options.ip
-    port = int(options.port)
+    if options.daemon:
+        log("daemonising")
+        xcp.daemonize()
 
-    arch = run("startup", "uname")
-    if arch == "Linux":
-        log("startup: Using loop devices")
-        start(RawFiles(Loop()), ip, port, options.daemon)
-    elif arch == "FreeBSD":
-        log("startup: Using mdconfig devices")
-        start(RawFiles(Mdconfig()), ip, port, options.daemon)
+    server = None
+    if tcp:
+        log("will listen on %s:%d" % (ip, port))
+        server = xcp.TCPServer(ip, port)
     else:
-        log("startup: Unknown architecture: %s" % arch)
+        log("will listen on %s" % options.socket)
+        server = xcp.UnixServer(options.socket)
+
+    server.register_introspection_functions() # for debugging
+    server.register_instance(storage_server_dispatcher(Query = Query_server_dispatcher(Query()), VDI = VDI_server_dispatcher(VDI()), SR = SR_server_dispatcher(SR())))
+    log("serving requests forever")
+    server.serve_forever()
--- python/xcp.py
+++ python/xcp.py
@@ -61,3 +61,63 @@ def is_long(x):
         return True
     except:
         return False
+
+# Helper function to daemonise ##############################################
+def daemonize():
+    def fork():
+        try:
+            if os.fork() > 0:
+                # parent
+                sys.exit(0)
+        except Exception, e:
+            print >>sys.stderr, "fork() failed: %s" % e
+            traceback.print_exc()
+            raise
+    fork()
+    os.umask(0)
+    os.chdir("/")
+    os.setsid()
+    fork()
+    devnull = open("/dev/null", "r")
+    os.dup2(devnull.fileno(), sys.stdin.fileno())
+    devnull = open("/dev/null", "aw")
+    os.dup2(devnull.fileno(), sys.stdout.fileno())
+    os.dup2(devnull.fileno(), sys.stderr.fileno())
+
+from SocketServer import UnixStreamServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler, SimpleXMLRPCDispatcher
+from xmlrpclib import ServerProxy, Fault, Transport
+from socket import socket, SOL_SOCKET, SO_REUSEADDR, AF_UNIX, SOCK_STREAM
+
+# Server XMLRPC from any HTTP POST path #####################################
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = []
+
+class UnixServer(UnixStreamServer, SimpleXMLRPCDispatcher):
+    def __init__(self, addr, requestHandler=RequestHandler):
+        self.logRequests = 0
+        os.unlink(addr)
+        SimpleXMLRPCDispatcher.__init__(self)
+        UnixStreamServer.__init__(self, addr, requestHandler)
+
+class TCPServer(SimpleXMLRPCServer):
+    def __init__(self, ip, port):
+        SimpleXMLRPCServer.__init__(self, (ip, port), requestHandler=RequestHandler)
+    def server_bind(self):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        SimpleXMLRPCServer.server_bind(self)
+
+# This is a hack to patch slow socket.getfqdn calls that
+# BaseHTTPServer (and its subclasses) make.
+# See: http://bugs.python.org/issue6085
+# See: http://www.answermysearches.com/xmlrpc-server-slow-in-python-how-to-fix/2140/
+import BaseHTTPServer
+
+def _bare_address_string(self):
+    host, port = self.client_address[:2]
+    return '%s' % host
+
+BaseHTTPServer.BaseHTTPRequestHandler.address_string = \
+        _bare_address_string
+
