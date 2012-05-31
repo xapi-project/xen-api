107bde8ef5dace0183fbf1b0c6e90eaee3e8745b
John Else <john.else@citrix.com>
2012-05-31 21:20:10 +0100

    CA-81599: Add tsc_mode to the list of allowed platform flags.
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -34,7 +34,8 @@ let xenapi_of_xenops_power_state = function
 
 (* This is only used to block the 'present multiple physical cores as one big hyperthreaded core' feature *)
 let filtered_platform_flags = ["acpi"; "apic"; "nx"; "pae"; "viridian";
-                               "acpi_s3";"acpi_s4"; "mmio_size_mib"; "revision"; "device_id"]
+                               "acpi_s3";"acpi_s4"; "mmio_size_mib"; "revision"; "device_id";
+                               "tsc_mode"]
 
 let xenops_vdi_locator_of_strings sr_uuid vdi_location =
 	Printf.sprintf "%s/%s" sr_uuid vdi_location
