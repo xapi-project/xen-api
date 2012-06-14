d00b98e6a6ef2a15c6597b3c96d4af126c734b85
David Scott <dave.scott@eu.citrix.com>
2012-06-14 23:17:21 +0100

    [NBD] add Nbd.write_{async,wait} to allow clients to pipeline requests
    Internally Nbd.write simply calls write_async and then write_wait.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- nbd/nbd.ml
+++ nbd/nbd.ml
@@ -150,22 +150,28 @@ let read sock from len =
 	else
 		None
 
-let write sock str from =
+let write_async sock dest_ofs str ofs len rid =
 	let request = {
 		Request.ty = NBD_cmd_write;
-		handle=1L;
-		from=from;
-		len=Int32.of_int (String.length str);
+		handle=rid;
+		from=dest_ofs;
+		len=Int32.of_int len;
 	} in
 	let msg = construct_request request in
 	Unixext.really_write_string sock msg;
-	Unixext.really_write_string sock str;
+	Unixext.really_write sock str ofs len
+
+let write_wait sock =
 	let reply = Bitstring.bitstring_of_file_descr_max sock 16 in
 	let parsed = parse_reply reply in
 	if parsed.Reply.error=0l then 
-		None
+		parsed.Reply.handle, None
 	else
-		Some parsed.Reply.error
+		parsed.Reply.handle, Some parsed.Reply.error
+
+let write sock dest_ofs str ofs len =
+	write_async sock dest_ofs str ofs len 0L;
+	snd (write_wait sock)
 
 let connect hostname port =
 	let socket = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
