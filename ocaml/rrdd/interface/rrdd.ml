4ba72c978d033d3ad35f3c6d427aa0e6df125852
Rok Strniša <rok.strnisa@citrix.com>
2012-06-19 16:26:43 +0100

    CP-3601: Implemented retrying for rrdd RPC.
    Signed-off-by: Rok Strniša <rok.strnisa@citrix.com>

--- ocaml/rrdd/interface/rrdd.ml
+++ ocaml/rrdd/interface/rrdd.ml
@@ -12,13 +12,38 @@
  * GNU Lesser General Public License for more details.
  *)
 
+module D = Debug.Debugger(struct let name = "rrdd" end)
+open D
+
 (* Establish connection to the server via a file descriptor. *)
 let make_rpc path  =
 	let open Xmlrpc_client in
 	let module Rpc = struct
-		let transport = ref (Unix path)
+		let transport = Unix path
+
 		let rpc call =
-			XMLRPC_protocol.rpc ~transport:!transport ~http:(xmlrpc ~version:"1.0" "/") call
+			let rec aux ~retrying =
+				let response' =
+					try
+						let response =
+							XMLRPC_protocol.rpc ~srcstr:"xapi" ~dststr:Rrdd_interface.name ~transport
+								~http:(xmlrpc ~version:"1.0" "/") call in
+						if retrying then
+							debug "Successfully communicated with service at %s after retrying!" path;
+						Some response
+					with Unix.Unix_error (code, _, _) as e ->
+						if code = Unix.ECONNREFUSED || code = Unix.ENOENT then begin
+							if not retrying then
+								error "Could not reach the service at %s. Retrying every second..." path;
+							Thread.delay 1.;
+							None
+						end else
+							raise e
+				in
+				match response' with
+				| Some response -> response
+				| None -> aux ~retrying:true
+			in aux ~retrying:false
 	end in
 	(module Rpc : Rrdd_interface.RPC)
 
