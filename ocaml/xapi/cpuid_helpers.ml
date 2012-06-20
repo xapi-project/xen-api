6e5cfa1fbd75b2f3b7f8516eee1b22f200a57918
John Else <john.else@citrix.com>
2012-06-20 15:01:56 +0100

    CA-81702: Take the pool feature mask into account when asserting a VM's compatibility with a host.
    Signed-off-by: John Else <john.else@citrix.com>
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/cpuid_helpers.ml
+++ ocaml/xapi/cpuid_helpers.ml
@@ -12,42 +12,68 @@
  * GNU Lesser General Public License for more details.
  *)
 
+open Fun
 open Listext
+
 module D=Debug.Debugger(struct let name="xapi" end)
 open D
 
+let vendor_key = "vendor"
+let features_key = "features"
+
+let get_pool_feature_mask ~__context =
+	let pool = Helpers.get_pool ~__context in
+	try
+		let other_config = Db.Pool.get_other_config ~__context ~self:pool in
+		let mask_string = List.assoc Xapi_globs.cpuid_feature_mask_key other_config in
+		debug "Found pool feature mask: %s" mask_string;
+		Some (Cpuid.string_to_features mask_string)
+	with _ ->
+		None
+
+let maybe_apply_mask mask features =
+	match mask with
+	| Some mask' -> Cpuid.mask_features features mask'
+	| None -> features
+
+let get_host_compatibility_info ~__context ~host =
+	let cpu_info = Db.Host.get_cpu_info ~__context ~self:host in
+	let vendor = List.assoc vendor_key cpu_info in
+	let features = List.assoc features_key cpu_info in
+	(vendor, features)
+
 (* Populate last_boot_CPU_flags with the vendor and feature set of the given host's CPU. *)
 let populate_cpu_flags ~__context ~vm ~host =
-	let add_or_replace (key, value) values =
-		if List.mem_assoc key values then
-			List.replace_assoc key value values
-		else
-			(key, value) :: values
+	let host_cpu_vendor, host_cpu_features = get_host_compatibility_info ~__context ~host in
+	let vm_cpu_flags = [
+		(vendor_key, host_cpu_vendor);
+		(features_key, host_cpu_features);]
 	in
-	let cpu_info = Db.Host.get_cpu_info ~__context ~self:host in
-	let flags = ref (Db.VM.get_last_boot_CPU_flags ~__context ~self:vm) in
-	if List.mem_assoc "vendor" cpu_info then
-		flags := add_or_replace ("vendor", List.assoc "vendor" cpu_info) !flags;
-	if List.mem_assoc "features" cpu_info then
-		flags := add_or_replace ("features", List.assoc "features" cpu_info) !flags;
-	Db.VM.set_last_boot_CPU_flags ~__context ~self:vm ~value:!flags
+	Db.VM.set_last_boot_CPU_flags ~__context ~self:vm ~value:vm_cpu_flags
 
+(* Compare the CPU on which the given VM was last booted to the CPU of the given host. *)
 let assert_vm_is_compatible ~__context ~vm ~host =
-	let host_cpu_info = Db.Host.get_cpu_info ~__context ~self:host in
+	let fail msg =
+		raise (Api_errors.Server_error(Api_errors.vm_incompatible_with_this_host,
+			[Ref.string_of vm; Ref.string_of host; msg]))
+	in
+	let host_cpu_vendor, host_cpu_features = get_host_compatibility_info ~__context ~host in
 	let vm_cpu_info = Db.VM.get_last_boot_CPU_flags ~__context ~self:vm in
-	if List.mem_assoc "vendor" vm_cpu_info then begin
+	if List.mem_assoc vendor_key vm_cpu_info then begin
 		(* Check the VM was last booted on a CPU with the same vendor as this host's CPU. *)
-		debug "VM has vendor %s; host has vendor %s" (List.assoc "vendor" vm_cpu_info) (List.assoc "vendor" host_cpu_info);
-		if (List.assoc "vendor" vm_cpu_info) <> (List.assoc "vendor" host_cpu_info) then
-			raise (Api_errors.Server_error(Api_errors.vm_incompatible_with_this_host,
-				[Ref.string_of vm; Ref.string_of host; "VM was last booted on a host which had a CPU from a different vendor."]))
+		let vm_cpu_vendor = List.assoc vendor_key vm_cpu_info in
+		debug "VM last booted on CPU of vendor %s; host CPUs are of vendor %s" vm_cpu_vendor host_cpu_vendor;
+		if vm_cpu_vendor <> host_cpu_vendor then
+			fail "VM last booted on a host which had a CPU from a different vendor."
 	end;
-	if List.mem_assoc "features" vm_cpu_info then begin
+	if List.mem_assoc features_key vm_cpu_info then begin
 		(* Check the VM was last booted on a CPU whose features are a subset of the features of this host's CPU. *)
-		let host_cpu_features = Cpuid.string_to_features (List.assoc "features" host_cpu_info) in
-		let vm_cpu_features = Cpuid.string_to_features (List.assoc "features" vm_cpu_info) in
-		debug "VM has features %s; host has features %s" (List.assoc "features" vm_cpu_info) (List.assoc "features" host_cpu_info);
-		if not((Cpuid.mask_features host_cpu_features vm_cpu_features) = vm_cpu_features) then
-			raise (Api_errors.Server_error(Api_errors.vm_incompatible_with_this_host,
-				[Ref.string_of vm; Ref.string_of host; "VM was last booted on a CPU with features this host's CPU does not have."]))
+		let vm_cpu_features = List.assoc features_key vm_cpu_info in
+		debug "VM last booted on CPU with features %s; host CPUs have features %s" vm_cpu_features host_cpu_features;
+		let pool_mask = get_pool_feature_mask ~__context in
+		let vm_cpu_features' = vm_cpu_features |> Cpuid.string_to_features |> maybe_apply_mask pool_mask in
+		let host_cpu_features' = host_cpu_features |> Cpuid.string_to_features |> maybe_apply_mask pool_mask in
+		if not((Cpuid.mask_features vm_cpu_features' host_cpu_features') = vm_cpu_features') then
+			fail "VM last booted on a CPU with features this host's CPU does not have."
 	end
+
