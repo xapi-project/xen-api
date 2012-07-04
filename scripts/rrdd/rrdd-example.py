c4af27e1640a3621d7e37170f280a8e3b0e71635
Rok Strniša <rok.strnisa@citrix.com>
2012-07-04 12:15:47 +0100

    rrdd plugins: improved data freshness and documentation.
    Signed-off-by: Rok Strniša <rok.strnisa@citrix.com>

--- scripts/rrdd/rrdd-example.py
+++ scripts/rrdd/rrdd-example.py
@@ -3,13 +3,16 @@
 import rrdd, os
 
 if __name__ == "__main__":
-  api = rrdd.API("host_mem")
+  # Create a proxy for communicating with xcp-rrdd.
+  api = rrdd.API(plugin_id = "host_mem")
   while True:
+    # Wait until 0.5 seconds before xcp-rrdd is going to read the output file.
+    api.wait_until_next_reading(neg_shift = .5)
+    # Collect measurements.
     cmd = "free -k | grep Mem | awk '{print $2, $3, $4}'"
-    values = os.popen(cmd).read().strip().split()
-    total = float(values[0])
-    used = float(values[1])
-    free = float(values[2])
-    ds_used = rrdd.DataSource("used_mem", used, min = 0, max = total)
-    ds_free = rrdd.DataSource("free_mem", free, min = 0, max = total)
-    api.update_and_sleep([ds_used, ds_free])
+    vs = os.popen(cmd).read().strip().split()
+    # Tell the proxy which datasources should be exposed in this iteration.
+    api.set_datasource("used_mem", vs[1], min_val = 0, max_val = vs[0])
+    api.set_datasource("free_mem", vs[2], min_val = 0, max_val = vs[0])
+    # Write all required information into a file about to be read by xcp-rrdd.
+    api.update()
