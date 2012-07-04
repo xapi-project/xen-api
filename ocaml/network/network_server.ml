511d692ea95ecefb13fb8e520c490817b82341f9
Rob Hoes <rob.hoes@citrix.com>
2012-07-04 13:47:10 +0100

    networkd: split off config file functions into separate module
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/network/network_server.ml
+++ ocaml/network/network_server.ml
@@ -22,56 +22,9 @@ open Listext
 module D = Debug.Debugger(struct let name = "network_server" end)
 open D
 
-let config_file_path = Filename.concat Fhs.vardir "networkd.db"
-
 type context = unit
 
-let empty_config = {interface_config = []; bridge_config = []; gateway_interface = None; dns_interface = None}
 let config : config_t ref = ref empty_config
-
-let read_management_conf () =
-	let management_conf = Unixext.string_of_file (Xapi_globs.first_boot_dir ^ "data/management.conf") in
-	let args = String.split '\n' (String.rtrim management_conf) in
-	let args = List.map (fun s -> match (String.split '=' s) with k :: [v] -> k, String.strip ((=) '\'') v | _ -> "", "") args in
-	debug "Firstboot file management.conf has: %s" (String.concat "; " (List.map (fun (k, v) -> k ^ "=" ^ v) args));
-	let device = List.assoc "LABEL" args in
-	Util_inventory.reread_inventory ();
-	let bridge_name = Util_inventory.lookup Util_inventory._management_interface in
-	debug "Management bridge in inventory file: %s" bridge_name;
-	let ipv4_conf, ipv4_gateway, dns =
-		match List.assoc "MODE" args with
-		| "static" ->
-			let ip = List.assoc "IP" args |> Unix.inet_addr_of_string in
-			let prefixlen = List.assoc "NETMASK" args |> netmask_to_prefixlen in
-			let gateway =
-				if List.mem_assoc "GATEWAY" args then
-					Some (List.assoc "GATEWAY" args |> Unix.inet_addr_of_string)
-				else None
-			in
-			let nameservers =
-				if List.mem_assoc "DNS" args && List.assoc "DNS" args <> "" then
-					List.map Unix.inet_addr_of_string (String.split ',' (List.assoc "DNS" args))
-				else []
-			in
-			let domains =
-				if List.mem_assoc "DOMAIN" args && List.assoc "DOMAIN" args <> "" then
-					String.split ' ' (List.assoc "DOMAIN" args)
-				else []
-			in
-			let dns = nameservers, domains in
-			Static4 [ip, prefixlen], gateway, dns
-		| "dhcp" | _ ->
-			DHCP4, None, ([], [])
-	in
-	let phy_interface = {default_interface with persistent_i = true} in
-	let bridge_interface = {default_interface with ipv4_conf; ipv4_gateway; persistent_i = true} in
-	let bridge = {default_bridge with
-		ports = [device, {default_port with interfaces = [device]}];
-		persistent_b = true
-	} in
-	{interface_config = [device, phy_interface; bridge_name, bridge_interface];
-		bridge_config = [bridge_name, bridge];
-		gateway_interface = Some bridge_name; dns_interface = Some bridge_name}
 
 let legacy_management_interface_start () =
 	try
@@ -82,13 +35,11 @@ let legacy_management_interface_start () =
 			(Printexc.to_string e) (Printexc.get_backtrace ())
 
 let write_config () =
-	let config_json = !config |> rpc_of_config_t |> Jsonrpc.to_string in
-	Unixext.write_string_to_file config_file_path config_json
+	Network_config.write_config !config
 
 let read_config () =
 	try
-		let config_json = Unixext.string_of_file config_file_path in
-		config := config_json |> Jsonrpc.of_string |> config_t_of_rpc;
+		config := Network_config.read_config ();
 		debug "Read configuration from networkd.db file."
 	with _ ->
 		(* No configuration file found. *)
@@ -99,7 +50,7 @@ let read_config () =
 		else
 			(* Try to get the initial network setup from the first-boot data written by the host installer. *)
 			try
-				config := read_management_conf ();
+				config := Network_config.read_management_conf ();
 				debug "Read configuration from management.conf file."
 			with _ ->
 				debug "Could not interpret the configuration in management.conf"
@@ -120,7 +71,7 @@ let clear_state _ () =
 	config := empty_config
 
 let reset_state _ () =
-	config := read_management_conf ()
+	config := Network_config.read_management_conf ()
 
 let set_gateway_interface _ dbg ~name =
 	debug "Setting gateway interface to %s" name;
