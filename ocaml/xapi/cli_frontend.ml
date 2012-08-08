c2da480b46ef6d0ada3c9a04e0a611e938da7a75
Mike McClurg <mike.mcclurg@citrix.com>
2012-08-08 15:57:00 +0100

    Remove encrypt option from vm-migrate CLI
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- ocaml/xapi/cli_frontend.ml
+++ ocaml/xapi/cli_frontend.ml
@@ -1156,8 +1156,8 @@ let rec cmdtable_data : (string*cmd_spec) list =
    "vm-migrate",
     {
       reqd=[];
-			optn=["live"; "host"; "host-uuid"; "remote-master"; "remote-username"; "remote-password"; "remote-network"; "destination-sr-uuid"; "encrypt"; "force"; "vif"; "vdi"];
-      help="Migrate the selected VM(s). The parameter '--live' will migrate the VM without shutting it down. The 'host' parameter matches can be either the name or the uuid of the host. The parameter '--encrypt' will encrypt the memory image transfer.";
+      optn=["live"; "host"; "host-uuid"; "remote-master"; "remote-username"; "remote-password"; "remote-network"; "destination-sr-uuid"; "force"; "vif"; "vdi"];
+      help="Migrate the selected VM(s). The parameter '--live' will migrate the VM without shutting it down. The 'host' parameter matches can be either the name or the uuid of the host.";
       implementation=No_fd Cli_operations.vm_migrate;
       flags=[Standard; Vm_selectors];
     };
