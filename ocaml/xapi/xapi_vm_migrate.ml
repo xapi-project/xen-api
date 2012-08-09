bea30b9e1678a38ad8b5cbd94401291e69e40a7d
John Else <john.else@citrix.com>
2012-08-09 08:57:13 +0100

    Remove unnecessary checks before removing VDI mapping keys.
    It's safe to call remove_from_other_config even if the key isn't
    present.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_vm_migrate.ml
+++ ocaml/xapi/xapi_vm_migrate.ml
@@ -347,9 +347,7 @@ let migrate_send'  ~__context ~vm ~dest ~live ~vdi_map ~vif_map ~options =
 		let xenops_vdi_map = List.map (fun (_, mirror_record) -> (mirror_record.mr_local_xenops_locator, mirror_record.mr_remote_xenops_locator)) (snapshots_map @ vdi_map) in
 		
 		List.iter (fun (vdi,mirror_record) ->
-			let other_config = Db.VDI.get_other_config ~__context ~self:vdi in
-			if List.mem_assoc Constants.storage_migrate_vdi_map_key other_config then
-				Db.VDI.remove_from_other_config ~__context ~self:vdi ~key:Constants.storage_migrate_vdi_map_key;
+			Db.VDI.remove_from_other_config ~__context ~self:vdi ~key:Constants.storage_migrate_vdi_map_key;
 			Db.VDI.add_to_other_config ~__context ~self:vdi ~key:Constants.storage_migrate_vdi_map_key ~value:(Ref.string_of mirror_record.mr_remote_vdi_reference)) (snapshots_map @ vdi_map);
 
 		List.iter (fun (vif,network) ->
