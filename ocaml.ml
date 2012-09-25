35986e8eae5f3cfc46ac245a9ecbd5502f57281d
David Scott <dave.scott@eu.citrix.com>
2012-09-25 12:23:48 +0100

    Generate marshalling code

--- ocaml.ml
+++ ocaml.ml
@@ -87,6 +87,39 @@ let exn_decl env e =
 		Line (sprintf "(** %s *)" e.TyDecl.description);
 	]
 
+let rpc_of_interface env i =
+	let type_of_arg a = Line (sprintf "type %s = %s with rpc" a.Arg.name (typeof env a.Arg.ty)) in
+	let field_of_arg a = Line (sprintf "%s: %s;" a.Arg.name a.Arg.name) in
+	let of_inputs_outputs which args =
+		[
+			Line (sprintf "module %s = struct" which);
+			Block ([
+			] @ (List.map type_of_arg args
+			) @ [
+				Line "type t = {";
+				Block (List.map field_of_arg args);
+				Line "}";
+				Line (sprintf "let of_rpc %s = {" (String.concat " " (List.map (fun a -> a.Arg.name) args)));
+				Block (List.map (function { Arg.name = a } -> Line (sprintf "%s = %s_of_rpc %s;" a a a)) args);
+				Line "}";
+				Line (sprintf "let to_rpc { %s } = [" (String.concat "; " (List.map (fun a -> a.Arg.name) args)));
+				Block (List.map (function { Arg.name = a } -> Line (sprintf "rpc_of_%s %s" a a)) args);
+				Line "]";
+			]);
+			Line "end";
+		] in
+	let of_method m =
+		[
+			Line (sprintf "module %s = struct" (String.capitalize m.Method.name));
+			Block (of_inputs_outputs "Inputs" m.Method.inputs @ (of_inputs_outputs "Outputs" m.Method.outputs));
+			Line "end";
+		] in
+	[
+		Line (sprintf "module %s = struct" i.Interface.name);
+		Block (List.concat (List.map of_method i.Interface.methods));
+		Line "end"
+	]
+
 let skeleton_method unimplemented env i m =
 	[
 		Line (sprintf "let %s x = %s" m.Method.name
@@ -124,15 +157,27 @@ let skeleton_of_interface = skeleton_of_interface true "skeleton"
 
 let server_of_interface env i =
 	let dispatch_method m =
-		Line (sprintf "| \"%s.%s\" -> Impl.%s call (* XXX *)" i.Interface.name m.Method.name m.Method.name) in
-
+		[
+			Line (sprintf "| \"%s.%s\", [%s] ->"
+				i.Interface.name m.Method.name
+				(String.concat "; " (List.map (fun a -> a.Arg.name) m.Method.inputs))
+			);
+			Block [
+				Line (sprintf "let request = %s.%s.Inputs.of_rpc %s in" i.Interface.name (String.capitalize m.Method.name) (String.concat " " (List.map (fun a -> a.Arg.name) m.Method.inputs)));
+				Line (sprintf "let response = Impl.%s request" (String.capitalize m.Method.name));
+				Line (sprintf "%s.%s.Outputs.to_rpc response" i.Interface.name (String.capitalize m.Method.name));
+			];
+			Line (sprintf "| \"%s.%s\", args -> failwith \"wrong number of arguments\""
+				i.Interface.name m.Method.name
+			);
+		] in
 	[
 		Line (sprintf "module %s_server_dispatcher = functor(Impl: %s) -> struct" i.Interface.name i.Interface.name);
 		Block [
 			Line "let dispatch (call: Rpc.call) : Rpc.response (* M.t *) =";
 			Block [
-				Line "match call.Rpc.name with";
-				Block (List.map dispatch_method i.Interface.methods);
+				Line "match call.Rpc.name, call.Rpc.params with";
+				Block (List.concat (List.map dispatch_method i.Interface.methods));
 			]
 		];
 		Line "end"
@@ -160,7 +205,11 @@ let of_interfaces env i =
 		List.concat (List.map (type_decl env) i.Interfaces.type_decls)
 	) @ (
 		List.concat (List.map (exn_decl env) i.Interfaces.exn_decls)
-	) @ (
+	) @ [
+		Line "modules Types = struct";
+		Block (List.concat (List.map (rpc_of_interface env) i.Interfaces.interfaces));
+		Line "end";
+	] @ (
 		List.fold_left (fun acc i -> acc @
 			(server_of_interface env i) @ (skeleton_of_interface env i) @ (test_impl_of_interface env i)
 		) [] i.Interfaces.interfaces
