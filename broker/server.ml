c9b4ebe029350b5cc6421c71a9a659d055dfa84c
David Scott <dave.scott@eu.citrix.com>
2012-10-18 21:29:53 +0100

    Move the server logic into a server module

--- broker/server.ml
+++ broker/server.ml
@@ -5,40 +5,51 @@ open Protocol
 let port = ref 8080
 let name = ref "server"
 
-let main () =
-	let token = Printf.sprintf "%d" (Unix.getpid ()) in
-	lwt c = Connection.make !port token in
+module Server = struct
 
-	lwt (_: string) = Connection.rpc c (In.Create (Some !name)) in
-	lwt (_: string) = Connection.rpc c (In.Subscribe (!name)) in
-	Printf.fprintf stdout "Serving requests forever\n%!";
-	let rec loop from =
-		let timeout = 5. in
-		let frame = In.Transfer(from, timeout) in
-		lwt raw = Connection.rpc c frame in
-		let transfer = Out.transfer_of_rpc (Jsonrpc.of_string raw) in
-		match transfer.Out.messages with
-		| [] -> loop from
-		| m :: ms ->
-			lwt () = Lwt_list.iter_s
-				(fun (i, m) ->
-					lwt () =
-						match m.Message.reply_to with
-						| None ->
-							Printf.fprintf stderr "No reply_to\n%!";
-							return ()
-						| Some reply_to ->
-							Printf.fprintf stderr "Sending reply to %s\n%!" reply_to;
-							let request = In.Send(reply_to, { m with Message.reply_to = None }) in
-							lwt (_: string) = Connection.rpc c request in
-							return () in
-					let request = In.Ack i in
-					lwt (_: string) = Connection.rpc c request in
-					return ()
-				) transfer.Out.messages in
-			let from = List.fold_left max (fst m) (List.map fst ms) in
-			loop from in
-	loop (-1L)
+	let listen process port name =
+		let token = Printf.sprintf "%d" (Unix.getpid ()) in
+		lwt c = Connection.make port token in
+
+		lwt (_: string) = Connection.rpc c (In.Create (Some name)) in
+		lwt (_: string) = Connection.rpc c (In.Subscribe name) in
+		Printf.fprintf stdout "Serving requests forever\n%!";
+
+		let rec loop from =
+			let timeout = 5. in
+			let frame = In.Transfer(from, timeout) in
+			lwt raw = Connection.rpc c frame in
+			let transfer = Out.transfer_of_rpc (Jsonrpc.of_string raw) in
+			match transfer.Out.messages with
+			| [] -> loop from
+			| m :: ms ->
+				lwt () = Lwt_list.iter_s
+					(fun (i, m) ->
+						lwt response = process m.Message.payload in
+						lwt () =
+							match m.Message.reply_to with
+							| None ->
+								Printf.fprintf stderr "No reply_to\n%!";
+								return ()
+							| Some reply_to ->
+								Printf.fprintf stderr "Sending reply to %s\n%!" reply_to;
+								let request = In.Send(reply_to, { m with Message.reply_to = None; payload = response }) in
+								lwt (_: string) = Connection.rpc c request in
+								return () in
+						let request = In.Ack i in
+						lwt (_: string) = Connection.rpc c request in
+						return ()
+					) transfer.Out.messages in
+				let from = List.fold_left max (fst m) (List.map fst ms) in
+				loop from in
+		loop (-1L)
+
+end
+
+let process x = return x
+
+let main () =
+	Server.listen process !port !name
 
 let _ =
 	Arg.parse [
