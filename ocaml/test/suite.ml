d214bd16d6e344b402b2f6739dff2b3073897e91
John Else <john.else@citrix.com>
2012-10-22 16:25:16 +0100

    Create base unit test suite.
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/test/suite.ml
+++ ocaml/test/suite.ml
@@ -14,9 +14,9 @@
 
 open OUnit
 
-let test_suite =
+let test_basic =
 	let open Test_basic in
-	"test_suite" >:::
+	"test_basic" >:::
 		[
 			"test_always_pass" >:: test_always_pass ;
 			"test_always_fail" >:: test_always_fail ;
@@ -24,4 +24,10 @@ let test_suite =
 			"test_assert_licensed_storage_motion" >:: test_assert_licensed_storage_motion ;
 		]
 
-let _ = run_test_tt_main test_suite
+let base_suite =
+	"base_suite" >:::
+		[
+			test_basic
+		]
+
+let _ = run_test_tt_main base_suite
