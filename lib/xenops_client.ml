25d5b4ec8e53bd7a350300e743dae11a065212e0
David Scott <dave.scott@eu.citrix.com>
2012-10-26 22:42:33 +0100

    Fix bug in client: need to allocate a buffer for the response
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/xenops_client.ml
+++ lib/xenops_client.ml
@@ -71,6 +71,7 @@ let binary_rpc string_of_call response_of_string ?(srcstr="unset") ?(dststr="uns
 			let len_buf = String.make 16 '\000' in
 			really_input ic len_buf 0 16;
 			let len = int_of_string len_buf in
+			let msg_buf = String.make len '\000' in
 			really_input ic msg_buf 0 len;
 			let (response: Rpc.response) = response_of_string msg_buf in
 			response
