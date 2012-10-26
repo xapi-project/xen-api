7cba04413e351a4252ac384016b8a0d392e7a9f0
David Scott <dave.scott@eu.citrix.com>
2012-10-26 12:07:39 +0100

    Immediately flush stderr debug output
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/xenops_utils.ml
+++ lib/xenops_utils.ml
@@ -35,7 +35,8 @@ module Debug = struct
 		| Warn -> "warn"
 		| Info -> "info"
 		| Error -> "error") x);
-		output_string stderr "\n"
+		output_string stderr "\n";
+		flush stderr
 
 	let syslog ?(facility=`LOG_LOCAL5) name () =
 		let t = Syslog.openlog ~facility name in
