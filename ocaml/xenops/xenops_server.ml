9c3613a5f8edf6839fb3fcc58a7e7e81821bf4b8
John Else <john.else@citrix.com>
2012-11-15 13:29:07 +0000

    Dump xenopsd updates every time we push an update.
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xenops/xenops_server.ml
+++ ocaml/xenops/xenops_server.ml
@@ -1802,6 +1802,7 @@ module UPDATES = struct
 			(fun () ->
 				debug "UPDATES.inject_barrier %d" id;
 				Updates.add (Dynamic.Barrier id) updates;
+				debug "Updates.Dump: %s" (updates |> Updates.Dump.make |> Updates.Dump.rpc_of_t |> Jsonrpc.to_string);
 				()
 			) ()
 
