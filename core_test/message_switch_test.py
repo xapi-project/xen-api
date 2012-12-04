18d0c1aaed4284348cfca0e254fe3e3328240e4e
David Scott <dave.scott@eu.citrix.com>
2012-12-04 14:08:07 +0000

    Complete renaming of python 'protocol' to 'message_switch'

--- core_test/message_switch_test.py
+++ core_test/message_switch_test.py
@@ -14,7 +14,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from protocol import *
+from message_switch import *
 
 basedir = "/tmp/link_test"
 
