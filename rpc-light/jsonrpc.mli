ede8588df16d5424396e1e44ba8d202934f8553f
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2012-12-20 13:15:50 +0000

    Fix Xmlrpc.a_of_response such that it doesn't call string_of_*
    Also expose json_of_response (Rpc.response -> Rpc.t) which returns
    the Rpc.t encoded Jsonrpc response suitable for calling to_string on.
    
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- rpc-light/jsonrpc.mli
+++ rpc-light/jsonrpc.mli
@@ -15,11 +15,16 @@
 val to_string : Rpc.t -> string
 val of_string : string -> Rpc.t
 
+val to_a : empty:(unit -> 'a) -> append:('a -> string -> unit) -> Rpc.t -> 'a
+val of_a : next_char:('a  -> char) -> 'a -> Rpc.t
+
 val string_of_call: Rpc.call -> string
 val call_of_string: string -> Rpc.call
 
 val string_of_response: Rpc.response -> string
-val response_of_string: string -> Rpc.response
+val a_of_response : empty:(unit -> 'a) -> append:('a -> string -> unit) -> Rpc.response -> 'a
 
+val response_of_string: string -> Rpc.response
+val response_of_in_channel: in_channel -> Rpc.response
 
 
