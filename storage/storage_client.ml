fbda87da4567ec9c34d601b60edcb11a5da5797c
David Scott <dave.scott@eu.citrix.com>
2012-12-28 13:41:21 +0000

    Add the ECONNREFUSED retry logic to the XCP storage interface directly, rather than only in the xenops client.
    This probably should be pushed into the generic XCP function, since
    the same behaviour will probably be expected for other interfaces.

--- storage/storage_client.ml
+++ storage/storage_client.ml
@@ -12,9 +12,25 @@
  * GNU Lesser General Public License for more details.
  *)
 
+let rec retry_econnrefused f =
+  try
+    f ()
+  with
+  | Unix.Unix_error(Unix.ECONNREFUSED, "connect", _) ->
+      (* debug "Caught ECONNREFUSED; retrying in 5s"; *)
+      Thread.delay 5.;
+      retry_econnrefused f
+  | e ->
+      (* error "Caught %s: does the storage service need restarting?" (Printexc.to_string e); *)
+      raise e
+
 module Client = Storage_interface.Client(struct
-  let rpc = Xcp_client.xml_http_rpc
-    ~srcstr:(Xcp_client.get_user_agent ())
-    ~dststr:"storage"
-    Storage_interface.uri
+  let rpc =
+    retry_econnrefused
+      (fun () ->
+        Xcp_client.xml_http_rpc
+          ~srcstr:(Xcp_client.get_user_agent ())
+          ~dststr:"storage"
+        Storage_interface.uri
+      )
 end)
