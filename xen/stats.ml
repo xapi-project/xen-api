608f13d9e504b51d12b671bbb3a641d7c14120b3
David Scott <dave.scott@eu.citrix.com>
2012-12-29 10:04:48 +0000

    Remove duplicate with_lock (look in lib/xenops_utils)

--- xen/stats.ml
+++ xen/stats.ml
@@ -55,16 +55,6 @@ open D
 let timings : (string, Normal_population.t) Hashtbl.t = Hashtbl.create 10
 let timings_m = Mutex.create ()
 
-let with_lock m f =
-  Mutex.lock m;
-  try
-    let result = f () in
-    Mutex.unlock m;
-    result
-  with e ->
-    Mutex.unlock m;
-    raise e
-
 let mean (p: Normal_population.t) = 
   let sigma = Normal_population.sd p in
   let mu = Normal_population.mean p in
@@ -82,7 +72,7 @@ let string_of (p: Normal_population.t) =
 let sample (name: string) (x: float) : unit = 
   (* Use the lognormal distribution: *)
   let x' = log x in
-  with_lock timings_m
+  Mutex.execute timings_m
     (fun () ->
        let p = 
 	 if Hashtbl.mem timings name 
@@ -111,7 +101,7 @@ let time_this (name: string) f =
     )
        
 let summarise () = 
-  with_lock timings_m
+  Mutex.execute timings_m
     (fun () ->
        Hashtbl.fold (fun k v acc -> (k, string_of v) :: acc) timings []
     )
@@ -134,7 +124,7 @@ let log_stats = ref false
 
 let log_db_call task_opt dbcall ty = 
   if not !log_stats then () else
-    with_lock dbstats_m (fun () ->
+    Mutex.execute dbstats_m (fun () ->
       let hashtbl = match ty with 
 	| Read -> dbstats_read_dbcalls 
 	| Write -> dbstats_write_dbcalls
@@ -161,7 +151,7 @@ let summarise_db_calls () =
     (Printf.sprintf "Total: %d" total) :: (List.map (fun (count,str) -> Printf.sprintf "%s: %d" str count) sorted)
   in
 
-  with_lock dbstats_m (fun () ->
+  Mutex.execute dbstats_m (fun () ->
     (summarise_table dbstats_write_dbcalls,
     summarise_table dbstats_read_dbcalls,
     summarise_table dbstats_create_dbcalls,
