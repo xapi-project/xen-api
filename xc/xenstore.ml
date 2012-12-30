d132fbdf07c5a415a44c9be7b63bb50c4c3c8463
David Scott <dave.scott@eu.citrix.com>
2012-12-30 15:06:39 +0000

    Attempt to diagnose some trivial errors, e.g. lack of xenstore

--- xc/xenstore.ml
+++ xc/xenstore.ml
@@ -17,7 +17,27 @@ module D = Debug.Make(struct let name = "xenstore" end)
 open D
 
 module Client = Xs_client_unix.Client(Xs_transport_unix_client)
-let client = Client.make ()
+let client =
+	try
+		Client.make ()
+	with e ->
+		error "Failed to connect to xenstore. The raw error was: %s" (Printexc.to_string e);
+		begin match e with
+		| Unix.Unix_error(Unix.EACCES, _, _) ->
+			error "Access to xenstore was denied.";
+			let euid = Unix.geteuid () in
+			if euid <> 0 then begin
+				error "My effective uid is %d." euid;
+				error "Typically xenstore can only be accessed by root (uid 0).";
+				error "Please switch to root (uid 0) and retry."
+			end
+		| Unix.Unix_error(Unix.ECONNREFUSED, _, _) ->
+			error "Access to xenstore was refused.";
+			error "This normally indicates that the service is not running.";
+			error "Please start the xenstore service and retry."
+		| _ -> ()
+		end;
+		raise e
 
 type domid = int
 
