d03521bd63c6bcaefc3e57bd2336650b50575eca
Jerome Maloberti <jerome.maloberti@citrix.com>
2013-01-08 14:59:06 +0000

    XOP-261: Reinstate the server on 127.0.0.1 when no management interface is defined
    Signed-off-by: Jerome Maloberti <jerome.maloberti@citrix.com>

--- ocaml/xapi/xapi.ml
+++ ocaml/xapi/xapi.ml
@@ -323,6 +323,7 @@ let bring_up_management_if ~__context () =
 			(Xapi_inventory.lookup Xapi_inventory._management_address_type) in
 		if management_if = "" then begin
 			debug "No management interface defined (management is disabled)";
+			Xapi_mgmt_iface.start_localhost_interface ~__context ()
 		end else begin
 			Xapi_mgmt_iface.run ~__context management_if management_address_type;
 			match Helpers.get_management_ip_addr ~__context with
