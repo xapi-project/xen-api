bc25a137b2f4b26b2eb2c297e32b4291d52a7300
David Scott <dave.scott@eu.citrix.com>
2013-01-25 16:18:15 +0000

    Add xcp.memory interface from xenopsd

---
+++ memory/memory_client.ml
@@ -0,0 +1,27 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open Memory_interface
+open Xenops_utils
+
+module D = Debug.Make(struct let name = "memory_client" end)
+open D
+module E = Debug.Make(struct let name = "mscgen" end)
+
+let json_url () = "file:" ^ json_path
+
+let json_binary_rpc = Xenops_client.binary_rpc Jsonrpc.string_of_call Jsonrpc.response_of_string
+
+module Client = Memory_interface.Client(struct let rpc = json_binary_rpc ~srcstr:"xenops" ~dststr:"squeezed" json_url end)
+
