e549c5638f5f31a582a06532f871012185f0bf4f
David Scott <dave.scott@eu.citrix.com>
2013-02-05 21:01:43 +0000

    Shift the SMAPI utilities into the shared library, so they can be used for all backends.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

---
+++ lib/storage.ml
@@ -0,0 +1,78 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open Xenops_interface
+open Xenops_task
+open Xenops_utils
+
+module D = Debug.Make(struct let name = service_name end)
+open D
+
+open Storage_interface
+module Client = Storage_client.Client
+
+let transform_exception f x =
+	try f x
+	with
+		| Backend_error(code, params) -> raise (Storage_backend_error(code, params))
+		| e -> raise e
+
+(* Used to identify this VBD to the storage layer *)
+let id_of frontend_domid vbd = Printf.sprintf "vbd/%d/%s" frontend_domid (snd vbd)
+
+let epoch_begin task sr vdi =
+	transform_exception
+		(fun () ->
+			Client.VDI.epoch_begin task.Xenops_task.dbg sr vdi
+		) ()
+
+let epoch_end task sr vdi =
+	transform_exception
+		(fun () ->
+			Client.VDI.epoch_end task.Xenops_task.dbg sr vdi
+		) ()
+
+let attach_and_activate ~xc ~xs task vm dp sr vdi read_write =
+	let result =
+		Xenops_task.with_subtask task (Printf.sprintf "VDI.attach %s" dp)
+			(transform_exception (fun () -> Client.VDI.attach "attach_and_activate" dp sr vdi read_write)) in
+
+	Xenops_task.with_subtask task (Printf.sprintf "VDI.activate %s" dp)
+		(transform_exception (fun () -> Client.VDI.activate "attach_and_activate" dp sr vdi));
+	result
+
+let deactivate task dp sr vdi =
+	debug "Deactivating disk %s %s" sr vdi;
+	Xenops_task.with_subtask task (Printf.sprintf "VDI.deactivate %s" dp)
+		(transform_exception (fun () -> Client.VDI.deactivate "deactivate" dp sr vdi))
+
+let dp_destroy task dp =
+	Xenops_task.with_subtask task (Printf.sprintf "DP.destroy %s" dp)
+		(transform_exception (fun () ->
+			try 
+				Client.DP.destroy "dp_destroy" dp false
+			with e ->
+				(* Backends aren't supposed to return exceptions on deactivate/detach, but they
+					   frequently do. Log and ignore *)
+					warn "DP destroy returned unexpected exception: %s" (Printexc.to_string e)
+			        ))
+
+let get_disk_by_name task path =
+	debug "Storage.get_disk_by_name %s" path;
+	match Re_str.bounded_split (Re_str.regexp "[/]") path 2 with
+		| [ sr; vdi ] -> sr, vdi
+		| _ ->
+			error "Failed to parse VDI name %s (expected SR/VDI)" path;
+			raise (Storage_interface.Vdi_does_not_exist path)
+
--- xc/xenops_server_xen.ml
+++ xc/xenops_server_xen.ml
@@ -216,37 +216,17 @@ let destroy_vbd_frontend ~xc ~xs task disk =
 		
 
 module Storage = struct
+	open Storage
 	open Storage_interface
 	module Client = Storage_client.Client
 
-	let transform_exception f x =
-		try f x
-		with
-			| Backend_error(code, params) -> raise (Storage_backend_error(code, params))
-			| e -> raise e
+	let id_of = id_of
+	let epoch_begin = epoch_begin
+	let epoch_end = epoch_end
 
-	(* Used to identify this VBD to the storage layer *)
-	let id_of frontend_domid vbd = Printf.sprintf "vbd/%d/%s" frontend_domid (snd vbd)
-
-	let epoch_begin task sr vdi =
-		transform_exception
-			(fun () ->
-				Client.VDI.epoch_begin task.Xenops_task.dbg sr vdi
-			) ()
-
-	let epoch_end task sr vdi =
-		transform_exception
-			(fun () ->
-				Client.VDI.epoch_end task.Xenops_task.dbg sr vdi
-			) ()
-
+	(* We need to deal with driver domains here: *)
 	let attach_and_activate ~xc ~xs task vm dp sr vdi read_write =
-		let result =
-			Xenops_task.with_subtask task (Printf.sprintf "VDI.attach %s" dp)
-				(transform_exception (fun () -> Client.VDI.attach "attach_and_activate" dp sr vdi read_write)) in
-
-		Xenops_task.with_subtask task (Printf.sprintf "VDI.activate %s" dp)
-			(transform_exception (fun () -> Client.VDI.activate "attach_and_activate" dp sr vdi));
+		let result = attach_and_activate ~xc ~xs task vm dp sr vdi read_write in
 		let backend = Xenops_task.with_subtask task (Printf.sprintf "Policy.get_backend_vm %s %s %s" vm sr vdi)
 			(transform_exception (fun () -> Client.Policy.get_backend_vm "attach_and_activate" vm sr vdi)) in
 		match domid_of_uuid ~xc ~xs Newest (uuid_of_string backend) with
@@ -255,29 +235,9 @@ module Storage = struct
 			| Some domid ->
 				{ domid = domid; attach_info = result }
 
-	let deactivate task dp sr vdi =
-		debug "Deactivating disk %s %s" sr vdi;
-		Xenops_task.with_subtask task (Printf.sprintf "VDI.deactivate %s" dp)
-			(transform_exception (fun () -> Client.VDI.deactivate "deactivate" dp sr vdi))
-
-	let dp_destroy task dp =
-		Xenops_task.with_subtask task (Printf.sprintf "DP.destroy %s" dp)
-			(transform_exception (fun () ->
-				try 
-					Client.DP.destroy "dp_destroy" dp false
-			    with e ->
-					(* Backends aren't supposed to return exceptions on deactivate/detach, but they
-					   frequently do. Log and ignore *)
-					warn "DP destroy returned unexpected exception: %s" (Printexc.to_string e)
-			        ))
-
-	let get_disk_by_name task path =
-		debug "Storage.get_disk_by_name %s" path;
-		match Re_str.bounded_split (Re_str.regexp "[/]") path 2 with
-			| [ sr; vdi ] -> sr, vdi
-			| _ ->
-				error "Failed to parse VDI name %s (expected SR/VDI)" path;
-				raise (Storage_interface.Vdi_does_not_exist path)
+	let deactivate = deactivate
+	let dp_destroy = dp_destroy
+	let get_disk_by_name = get_disk_by_name
 end
 
 let with_disk ~xc ~xs task disk write f = match disk with
