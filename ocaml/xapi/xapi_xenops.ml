bf13b84b07570d39f6743578b19f770f7913b5e6
John Else <john.else@citrix.com>
2013-02-25 15:07:00 +0000

    CP-4147: Change the USB emulation platform flag semantics
    Instead of having a single flag, "nousb", which turns off all USB
    emulation, we now have two flags:
    
      - "usb", which controls USB emulation
      - "usb_tablet", which controls USB emulation, as long as USB emulation
        is turned on
    
    xapi defaults to both keys being true, whereas xenopsd defaults to both
    keys being false.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -51,7 +51,8 @@ module Platform = struct
 	let pv_ramdisk_max_size = "pv-ramdisk-max-size"
 	let pv_postinstall_kernel_max_size = "pv-postinstall-kernel-max-size"
 	let pv_postinstall_ramdisk_max_size = "pv-postinstall-ramdisk-max-size"
-	let nousb = "nousb"
+	let usb = "usb"
+	let usb_tablet = "usb_tablet"
 	let parallel = "parallel"
 
 	(* This is only used to block the 'present multiple physical cores as one big hyperthreaded core' feature *)
@@ -73,13 +74,24 @@ module Platform = struct
 		pv_ramdisk_max_size;
 		pv_postinstall_kernel_max_size;
 		pv_postinstall_ramdisk_max_size;
-		nousb;
+		usb;
+		usb_tablet;
 		parallel;
 	]
 
 	(* Other keys we might want to write to the platform map. *)
 	let timeoffset = "timeoffset"
 	let generation_id = "generation-id"
+
+	(* Helper functions. *)
+	let is_true ~key ~platformdata ~default =
+		try
+			match List.assoc key platformdata with
+			| "true" | "1" -> true
+			| "false" | "0" -> false
+			| _ -> default
+		with Not_found ->
+			default
 end
 
 let xenops_vdi_locator_of_strings sr_uuid vdi_location =
@@ -450,6 +462,22 @@ module MD = struct
 				| "0:0" -> Xapi_vm_helpers.vm_fresh_genid ~__context ~self:vmref
 				| _ -> vm.API.vM_generation_id in
 			(Platform.generation_id, genid) :: platformdata
+		in
+		(* Add usb emulation flags.
+		   Make sure we don't send usb=false and usb_tablet=true,
+		   as that wouldn't make sense. *)
+		let usb_enabled =
+			Platform.is_true ~key:Platform.usb ~platformdata ~default:true in
+		let usb_tablet_enabled =
+			if usb_enabled
+			then Platform.is_true ~key:Platform.usb_tablet ~platformdata ~default:true
+			else false
+		in
+		let platformdata =
+			List.update_assoc
+				[(Platform.usb, string_of_bool usb_enabled);
+					(Platform.usb_tablet, string_of_bool usb_tablet_enabled)]
+				platformdata
 		in
 
 		let pci_msitranslate = true in (* default setting *)
