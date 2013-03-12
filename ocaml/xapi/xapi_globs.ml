024a07c2d1013623d17f265d41ce3d123ec6fbe1
Zheng Li <zheng.li@eu.citrix.com>
2013-03-12 11:32:22 +0000

    CI-44: Add max_sessions_per_originator config
    Signed-off-by: Zheng Li <zheng.li@eu.citrix.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -234,6 +234,9 @@ let max_tasks = 200
 (* We must allow for more sessions than running tasks *)
 let max_sessions = max_tasks * 2
 
+(* For sessions with specified originator, their session limits are counted independently. *)
+let max_sessions_per_originator = 50
+
 (* The Unix.time that represents the maximum time in the future that a 32 bit time can cope with *)
 let the_future = 2147483647.0
 
