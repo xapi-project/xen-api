aa6ab2145d3d8835a7ea19282145f4b2bdb8b3ab
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-03-14 16:39:19 +0000

    Add the network interface
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

---
+++ network/network_client.ml
@@ -0,0 +1,36 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+let rec retry_econnrefused f =
+  try
+    f ()
+  with
+  | Unix.Unix_error(Unix.ECONNREFUSED, "connect", _) ->
+      (* debug "Caught ECONNREFUSED; retrying in 5s"; *)
+      Thread.delay 5.;
+      retry_econnrefused f
+  | e ->
+      (* error "Caught %s: does the network service need restarting?" (Printexc.to_string e); *)
+      raise e
+
+module Client = Network_interface.Client(struct
+  let rpc =
+    retry_econnrefused
+      (fun () ->
+        Xcp_client.xml_http_rpc
+          ~srcstr:(Xcp_client.get_user_agent ())
+          ~dststr:"network"
+          (fun () -> Network_interface.uri)
+      )
+end)
