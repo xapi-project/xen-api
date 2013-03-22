d0a8dc61d7e3a75b6831dcaa04fd1d31d0bc4323
John Else <john.else@citrix.com>
2013-03-22 11:43:44 -0400

    Copyright notices
    Signed-off-by: John Else <john.else@citrix.com>

--- test/network_test.ml
+++ test/network_test.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (C) 2006-2012 Citrix Systems Inc.
+ * Copyright (C) 2006-2013 Citrix Systems Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
--- test/network_test_lacp_properties.ml
+++ test/network_test_lacp_properties.ml
@@ -1,4 +1,16 @@
-(* Test cases for PR-1510 *)
+(*
+ * Copyright (C) 2006-2013 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
 
 open Fun
 open OUnit
