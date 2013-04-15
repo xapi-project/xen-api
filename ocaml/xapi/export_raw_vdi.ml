5ec544f0db2d8d2e583cc3a66db2ef0332cf0a94
Si Beaumont <simon.beaumont@citrix.com>
2013-04-15 11:58:49 +0100

    CP-4500: Implemented export_raw_vdi.
    This can be called via HTTP at <host>/export_raw_vdi?vdi=<vdi-uuid> using, for
    example, curl and is the counterpart to import_raw_vdi.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/export_raw_vdi.ml
+++ ocaml/xapi/export_raw_vdi.ml
@@ -18,9 +18,53 @@
 module D = Debug.Debugger(struct let name="export_raw_vdi" end)
 open D
 
-open Http
+let localhost_handler rpc session_id vdi (req: Http.Request.t) (s: Unix.file_descr) =
+	req.Http.Request.close <- true;
+	Xapi_http.with_context "Exporting raw VDI" req s
+		(fun __context ->
+			let task_id = Context.get_task_id __context in
+			debug "export_raw_vdi task_id = %s vdi = %s;" (Ref.string_of task_id) (Ref.string_of vdi);
+			try
+				Sm_fs_ops.with_open_block_attached_device __context rpc session_id vdi `RO
+					(fun fd ->
+						let headers = Http.http_200_ok ~keep_alive:false () @
+							[ Http.Hdr.task_id ^ ":" ^ (Ref.string_of task_id); Http.Hdr.content_type ] in
+						Http_svr.headers s headers;
+						try
+							debug "Copying VDI contents...";
+							ignore(Unixext.copy_file fd s);
+							debug "Copying VDI complete.";
+							Unixext.fsync fd;
+						with Unix.Unix_error(Unix.EIO, _, _) ->
+							raise (Api_errors.Server_error (Api_errors.vdi_io_error, ["Device I/O errors"]))
+					)
+			with e ->
+				log_backtrace ();
+				TaskHelper.failed ~__context (Api_errors.internal_error, ["Caught exception: " ^ (ExnHelper.string_of_exn e)]);
+				raise e)
 
-let handler (req: Request.t) (s: Unix.file_descr) _  =
+let export_raw vdi (req: Http.Request.t) (s: Unix.file_descr) _ =
+	(* Check the SR is reachable (in a fresh task context) *)
+	Server_helpers.exec_with_new_task "VDI.export_raw_vdi"
+		(fun __context ->
+			Helpers.call_api_functions ~__context
+			(fun rpc session_id ->
+				let sr = Db.VDI.get_SR ~__context ~self:vdi in
+				debug "Checking whether localhost can see SR: %s" (Ref.string_of sr);
+				if (Importexport.check_sr_availability ~__context sr)
+				then localhost_handler rpc session_id vdi req s
+				else
+					let host = Importexport.find_host_for_sr ~__context sr in
+					let address = Db.Host.get_address ~__context ~self:host in
+					Importexport.return_302_redirect req s address
+			)
+		)
+
+let handler (req: Http.Request.t) (s: Unix.file_descr) _  =
 	debug "export_raw_vdi handler";
-	req.Request.close <- true;
-	Xapi_http.assert_credentials_ok "VDI.eport_raw" ~http_action:"get_export_raw_vdi" req
+	Xapi_http.assert_credentials_ok "VDI.eport_raw" ~http_action:"get_export_raw_vdi" req;
+
+	Server_helpers.exec_with_new_task "VDI.export_raw_vdi"
+		(fun __context ->
+			export_raw (Importexport.vdi_of_req ~__context req) req s ()
+		)
