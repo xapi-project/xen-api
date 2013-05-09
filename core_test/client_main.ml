eb2935ce25ed4c8779ca1803095690092664c5af
David Scott <dave.scott@eu.citrix.com>
2013-05-09 16:50:14 +0100

    The client test application now has a speed test option

--- core_test/client_main.ml
+++ core_test/client_main.ml
@@ -6,12 +6,24 @@ open Protocol_lwt
 let port = ref 8080
 let name = ref "server"
 let payload = ref "hello"
+let timeout = ref None
 
 let main () =
 	lwt c = Client.connect !port !name in
-
-	lwt x = Client.rpc c !payload in
-	Printf.fprintf stderr "%s\n%!" x;
+	let counter = ref 0 in
+	let one () =
+		incr counter;
+		lwt x = Client.rpc c !payload in
+		return () in
+	let start = Unix.gettimeofday () in
+	lwt () = match !timeout with
+	| None -> one ()
+	| Some t ->
+		while_lwt (Unix.gettimeofday () -. start < t) do
+			one ()
+		done in
+	let t = Unix.gettimeofday () -. start in
+	Printf.printf "Finished %d RPCs in %.02f\n" !counter t;
 	return ()
 
 let _ =
@@ -19,7 +31,7 @@ let _ =
 		"-port", Arg.Set_int port, (Printf.sprintf "port broker listens on (default %d)" !port);
 		"-name", Arg.Set_string name, (Printf.sprintf "name to send message to (default %s)" !name);
 		"-payload", Arg.Set_string payload, (Printf.sprintf "payload of message to send (default %s)" !payload);
-
+		"-secs", Arg.String (fun x -> timeout := Some (float_of_string x)), (Printf.sprintf "number of seconds to repeat the same message for (default %s)" (match !timeout with None -> "None" | Some x -> string_of_float x));
 	] (fun x -> Printf.fprintf stderr "Ignoring unexpected argument: %s" x)
 		"Send a message to a name, optionally waiting for a response";
 
