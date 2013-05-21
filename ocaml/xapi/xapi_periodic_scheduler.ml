f022bce9706e4d757469e4900fa3e8caa76bfef5
Rob Hoes <rob.hoes@citrix.com>
2013-05-21 23:27:57 +0100

    [WHITESPACE] Periodic scheduler loop
    $ git diff -w 3ddb861e63d4ffaa9ac0818f11fb027e3204208c^..3ddb861e63d4ffaa9ac0818f11fb027e3204208c

--- ocaml/xapi/xapi_periodic_scheduler.ml
+++ ocaml/xapi/xapi_periodic_scheduler.ml
@@ -40,31 +40,31 @@ let remove_from_queue name =
 	if index > -1 then begin
 		Ipq.remove queue index
 	end
-  
+
 let loop () =
-    debug "Periodic scheduler started";
-    while true do
-      try
-	let empty = Mutex.execute lock (fun () -> Ipq.is_empty queue) in
-	if empty 
-	then 
-	  (Thread.delay 10.0) (* Doesn't happen often - the queue isn't usually empty *)
-	else
-	  begin
-	    let next = Mutex.execute lock (fun () -> Ipq.maximum queue) in
-	    let now = Unix.gettimeofday () in
-	    if next.Ipq.time < now then begin
-	      let todo = (Mutex.execute lock (fun () -> Ipq.pop_maximum queue)).Ipq.ev in
-	      (try todo.func () with _ -> ());
-	      match todo.ty with 
-		| OneShot -> ()
-		| Periodic timer -> add_to_queue ~signal:false todo.name todo.ty timer todo.func
-	    end else begin
-	      (* Sleep until next event. *)
-	      ignore(Delay.wait delay (next.Ipq.time -. now +. 0.001))
-	    end
-	  end
-      with _ -> ()
-    done
+	debug "Periodic scheduler started";
+	while true do
+		try
+			let empty = Mutex.execute lock (fun () -> Ipq.is_empty queue) in
+			if empty
+			then
+				(Thread.delay 10.0) (* Doesn't happen often - the queue isn't usually empty *)
+			else
+			begin
+				let next = Mutex.execute lock (fun () -> Ipq.maximum queue) in
+				let now = Unix.gettimeofday () in
+				if next.Ipq.time < now then begin
+					let todo = (Mutex.execute lock (fun () -> Ipq.pop_maximum queue)).Ipq.ev in
+					(try todo.func () with _ -> ());
+					match todo.ty with
+					| OneShot -> ()
+					| Periodic timer -> add_to_queue ~signal:false todo.name todo.ty timer todo.func
+				end else begin
+					(* Sleep until next event. *)
+					ignore(Delay.wait delay (next.Ipq.time -. now +. 0.001))
+				end
+			end
+		with _ -> ()
+	done
 
 
