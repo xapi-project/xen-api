4e55bffbac55d3e76ab1bb1787c7d215459cc260
John Else <john.else@citrix.com>
2013-05-30 12:55:07 -0400

    Update OASIS
    Signed-off-by: John Else <john.else@citrix.com>

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 7e8eefc906f94d68ecb856796778fb40) *)
+(* DO NOT EDIT (digest: 8930af9bbb103ceb72185052fc6d2e86) *)
 module OASISGettext = struct
-(* # 21 "/local/scratch/djs/.opam/4.00.1/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
+(* # 21 "/usr/local2/.opam/4.00.1+annot/build/oasis.0.3.0/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-(* # 21 "/local/scratch/djs/.opam/4.00.1/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
+(* # 21 "/usr/local2/.opam/4.00.1+annot/build/oasis.0.3.0/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-(* # 21 "/local/scratch/djs/.opam/4.00.1/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
+(* # 21 "/usr/local2/.opam/4.00.1+annot/build/oasis.0.3.0/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-(* # 21 "/local/scratch/djs/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
+(* # 21 "/usr/local2/.opam/4.00.1+annot/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -323,7 +323,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-(* # 21 "/local/scratch/djs/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 21 "/usr/local2/.opam/4.00.1+annot/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -339,7 +339,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-(* # 56 "/local/scratch/djs/.opam/4.00.1/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 56 "/usr/local2/.opam/4.00.1+annot/build/oasis.0.3.0/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
@@ -482,6 +482,7 @@ let package_default =
           ("xcp", ["lib"]);
           ("xcp_storage", ["storage"]);
           ("xcp_network", ["network"]);
+          ("xcp_rrd", ["rrd"]);
           ("xcp_xen", ["xen"]);
           ("xcp_memory", ["memory"])
        ];
@@ -491,6 +492,7 @@ let package_default =
        [
           ("xen", ["lib"]);
           ("storage", ["lib"]);
+          ("rrd", ["lib"]);
           ("network", ["lib"]);
           ("memory", ["lib"])
        ];
@@ -499,6 +501,6 @@ let package_default =
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 503 "myocamlbuild.ml"
+# 505 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
