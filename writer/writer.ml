e88100dcd535cf6aee27b27f3e58ee666359f235
John Else <john.else@citrix.com>
2013-06-18 11:23:54 -0400

    Remove some boilerplate
    Signed-off-by: John Else <john.else@citrix.com>

--- writer/writer.ml
+++ writer/writer.ml
@@ -60,35 +60,8 @@ module PageWriter = MakeWriter(struct
 		failwith "not implemented"
 end)
 
-let with_gntshr f =
-	let handle = Gntshr.interface_open () in
-	let result = try
-		f handle
-	with e ->
-		Gntshr.interface_close handle;
-		raise e
-	in
-	Gntshr.interface_close handle;
-	result
-
-let share = ref None
-
-let cleanup _ =
-	match !share with
-	| None -> ()
-	| Some s ->
-		with_gntshr (fun handle -> Gntshr.munmap_exn handle s)
-
-let setup_signals () =
-	Sys.set_signal Sys.sigint (Sys.Signal_handle cleanup);
-	Sys.set_signal Sys.sigkill (Sys.Signal_handle cleanup)
-
 let () =
-	setup_signals ();
 	let target_domid = int_of_string Sys.argv.(1) in
-	share :=
-		Some (with_gntshr
-			(fun handle -> Gntshr.share_pages_exn handle target_domid 1 false));
 	while true do
 		Thread.delay 5.0
 	done
