27b7be366ad74b347b0ce10372cc9219536e040f
John Else <john.else@citrix.com>
2013-06-18 12:42:21 -0400

    Add cmdliner scaffolding
    Signed-off-by: John Else <john.else@citrix.com>

--- writer/writer.ml
+++ writer/writer.ml
@@ -1,5 +1,39 @@
+open Cmdliner
+
+let help_secs = [
+	`S "MORE HELP";
+	`P "Use `$(mname) $(i,command) --help' for help on a single command.";
+	`Noblank;
+]
+
+let default_cmd =
+	let doc = "RRD protocol writer" in
+	let man = help_secs in
+	Term.(ret (pure (fun _ -> `Help (`Pager, None)) $ pure ())),
+	Term.info "writer" ~version:"0.1" ~doc ~man
+
+let write_file_cmd =
+	let path =
+		let doc = "The path of the file to write" in
+		Arg.(required & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
+	in
+	let protocol =
+		let doc = "The protocol to use to write the rrd data" in
+		Arg.(required & pos 0 (some string) None & info [] ~docv:"PROTOCOL" ~doc)
+	in
+	let doc = "write to a file" in
+	let man = [
+		`S "DESCRIPTION";
+		`P "Write rrd data to a file, using the specified protocol"
+	] @ help_secs in
+	Term.(pure Writer_commands.write_file $ path $ protocol),
+	Term.info "file" ~doc ~man
+
+let cmds = [
+	write_file_cmd
+]
+
 let () =
-	let target_domid = int_of_string Sys.argv.(1) in
-	while true do
-		Thread.delay 5.0
-	done
+	match Term.eval_choice default_cmd cmds with
+	| `Error _ -> exit 1
+	| _ -> exit 0
---
+++ writer/writer_commands.ml
@@ -0,0 +1,1 @@
+let write_file path protocol = ()
