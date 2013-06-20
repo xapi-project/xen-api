294e02c649cfdd7a3dcfdfbe92a81fc72a2303f2
John Else <john.else@citrix.com>
2013-06-20 10:12:24 +0100

    Implement writing of a basic RRD to a file
    Signed-off-by: John Else <john.else@citrix.com>

--- writer/writer_commands.ml
+++ writer/writer_commands.ml
@@ -1,1 +1,18 @@
-let write_file path protocol = ()
+module Rrdp = Rrdp_common.Common(struct let name = "test_rrd_writer" end)
+
+let hdr = "DATASOURCES\n"
+
+let generate_data_sources () =
+	let current_time = Rrdp.now () in
+	[Ds.ds_make ~name:"current_time"
+		~description:"The current time"
+		~value:(Rrd.VT_Int64 current_time) ~ty:(Rrd.Gauge)
+		~default:true ~units:"seconds" (), Rrd.Host]
+
+let generate_data_source_string () =
+	let dss = generate_data_sources () in
+	Rrdp.json_of_dss ~hdr (Rrdp.now ()) dss
+
+let write_file path protocol =
+	let module W = Writer_types.FileWriter in
+	W.start 5.0 path generate_data_source_string
