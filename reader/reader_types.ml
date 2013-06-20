1e423f1bcc1f814723d1787ed3a09fb09a24d5fa
John Else <john.else@citrix.com>
2013-06-20 14:23:40 +0100

    Pass in the data interpreter as a callback to the start function
    Signed-off-by: John Else <john.else@citrix.com>

--- reader/reader_types.ml
+++ reader/reader_types.ml
@@ -7,7 +7,6 @@ module type Reader = sig
 	val cleanup: state_t -> unit
 
 	val read_data: state_t -> t
-	val string_of_t: t -> string
 end
 
 module MakeReader = functor (R: Reader) -> struct
@@ -22,14 +21,14 @@ module MakeReader = functor (R: Reader) -> struct
 		Sys.set_signal Sys.sigint
 			(Sys.Signal_handle (fun _ -> cleanup (); exit 0))
 
-	let start interval id =
+	let start interval id interpret_data =
 		setup_signals ();
 		let state = R.init id in
 		cached_state := Some state;
 		try
 			while true do
 				let data = R.read_data state in
-				Printf.printf "Got data:\n%s%!" (R.string_of_t data);
+				Printf.printf "Got data:\n%s%!" (interpret_data data);
 				Thread.delay interval
 			done
 		with _ ->
@@ -59,6 +58,4 @@ module FileReader = MakeReader(struct
 				Buffer.contents buf
 		in
 		read buf
-
-	let string_of_t s = s
 end)
