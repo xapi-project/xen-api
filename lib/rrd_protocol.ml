72752e42fe196fe7bf53e7fccf11dec97851db14
John Else <john.else@citrix.com>
2013-06-21 01:11:20 +0100

    Split the shared data into its component parts
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/rrd_protocol.ml
+++ lib/rrd_protocol.ml
@@ -1,8 +1,21 @@
 module Json = struct
 	module Rrdp = Rrdp_common.Common(struct let name = "test_rrd_writer" end)
 
-	let hdr = "DATASOURCES\n"
+	let header = "DATASOURCES\n"
+	let header_bytes = String.length header
+	let length_start = header_bytes
+	let length_bytes = 8 (* hex length of payload *)
+	let checksum_start = header_bytes + length_bytes + 1 (* newline *)
+	let checksum_bytes = 32 (* hex length of checksum *)
+	let payload_start = header_bytes + length_bytes + checksum_bytes + 2 (* 2 newlines *)
 
 	let of_dss dss =
-		Rrdp.json_of_dss ~hdr (Rrdp.now()) dss
+		Rrdp.json_of_dss ~hdr:header (Rrdp.now()) dss
+
+	let to_dss text =
+		let length_str = "0x" ^ (String.sub text length_start length_bytes) in
+		let length = int_of_string length_str in
+		let checksum = String.sub text checksum_start checksum_bytes in
+		let payload = String.sub text payload_start length in
+		length, checksum, payload
 end
--- reader/reader_commands.ml
+++ reader/reader_commands.ml
@@ -1,3 +1,11 @@
+open Rrd_protocol
+
+let interpret_data text =
+	let length, checksum, payload = Json.to_dss text in
+	Printf.printf "length = %d\n" length;
+	Printf.printf "checksum = %s\n" checksum;
+	Printf.printf "payload = %s\n%!" payload
+
 let read_file path protocol =
 	let module R = Reader_types.FileReader in
-	R.start 5.0 path (fun s -> Printf.sprintf "Got data:\n%s%!" s)
+	R.start 5.0 path interpret_data
