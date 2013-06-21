ffb5120c4d36b708a66b46c65c0aeca395b54754
John Else <john.else@citrix.com>
2013-06-21 16:07:53 +0100

    Only export the second data source for every other group of 8 intervals
    Signed-off-by: John Else <john.else@citrix.com>

--- writer/writer_commands.ml
+++ writer/writer_commands.ml
@@ -1,16 +1,35 @@
 open Rrd_protocol
+module Rrdp = Rrdp_common.Common(struct let name = "test_rrd_writer" end)
 
-let generate_data_sources () =
-	let module Rrdp = Rrdp_common.Common(struct let name = "test_rrd_writer" end) in
+let get_second_data_source_flag =
+	let counter = ref 0 in
+	(fun () ->
+		let result = match !counter / 8 with
+		| 0 -> false
+		| _ -> true
+		in
+		if !counter >= 15 then counter := 0 else incr counter;
+		result)
+
+let generate_time_data_source () =
 	let current_time = Rrdp.now () in
-	[Ds.ds_make ~name:"current_time"
+	Ds.ds_make ~name:"current_time"
 		~description:"The current time"
 		~value:(Rrd.VT_Int64 current_time) ~ty:(Rrd.Gauge)
-		~default:true ~units:"seconds" (), Rrd.Host;
+		~default:true ~units:"seconds" (), Rrd.Host
+
+let generate_random_data_source () =
 	Ds.ds_make ~name:"random_number"
 		~description:"A random number"
 		~value:(Rrd.VT_Int64 (Random.int64 256L)) ~ty:(Rrd.Absolute)
-		~default:true ~units:"things" (), Rrd.Host;]
+		~default:true ~units:"things" (), Rrd.Host
+
+let generate_data_sources () =
+	let include_second_data_source = get_second_data_source_flag () in
+	[generate_time_data_source ()] @
+	if include_second_data_source
+	then [generate_random_data_source ()]
+	else []
 
 let write_file path protocol =
 	Random.self_init ();
