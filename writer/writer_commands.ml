b8e7f60239db1c04b642471abed1633bed91b9d4
John Else <john.else@citrix.com>
2013-06-21 13:28:46 +0100

    Add a second data source
    Signed-off-by: John Else <john.else@citrix.com>

--- writer/writer_commands.ml
+++ writer/writer_commands.ml
@@ -6,9 +6,14 @@ let generate_data_sources () =
 	[Ds.ds_make ~name:"current_time"
 		~description:"The current time"
 		~value:(Rrd.VT_Int64 current_time) ~ty:(Rrd.Gauge)
-		~default:true ~units:"seconds" (), Rrd.Host]
+		~default:true ~units:"seconds" (), Rrd.Host;
+	Ds.ds_make ~name:"random_number"
+		~description:"A random number"
+		~value:(Rrd.VT_Int64 (Random.int64 256L)) ~ty:(Rrd.Absolute)
+		~default:true ~units:"things" (), Rrd.Host;]
 
 let write_file path protocol =
+	Random.self_init ();
 	let module W = Writer_types.FileWriter in
 	W.start 5.0 path
 		(fun () -> V1.of_dss (generate_data_sources ()))
