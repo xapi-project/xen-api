3085ddab3ee14ceec653b7446eb84e3712d5997d
David Scott <dave.scott@eu.citrix.com>
2013-06-21 14:06:50 +0000

    Hard-wire xenlight xenopsd to on.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -771,7 +771,6 @@ let options_of_xapi_globs_spec =
 
 let xapissl_path = ref (Filename.concat Fhs.libexecdir "xapissl")
 
-let use_xenlight = ref false
 let use_libvirt = ref false
 
 let other_options = [
@@ -789,9 +788,6 @@ let other_options = [
 
   "onsystemboot", Arg.Set on_system_boot, 
     (fun () -> string_of_bool !on_system_boot), "indicates that this server start is the first since the host rebooted";
-
-  "xenlight", Arg.Set use_xenlight,
-    (fun () -> string_of_bool !use_xenlight), "true if we expect a libxl xenopsd";
 
   "libvirt", Arg.Set use_libvirt,
     (fun () -> string_of_bool !use_libvirt), "true if we expect a libvirt xenopsd";
