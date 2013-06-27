7b4f974176967f0f9347897be65d24c146c64b83
David Scott <dave.scott@eu.citrix.com>
2013-06-27 20:31:52 +0000

    Add storage test boilerplate.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- storage/storage_test.ml
+++ storage/storage_test.ml
@@ -21,22 +21,64 @@ open Storage_client
    3. we always clean up (as best we can) after every test.
 *)
 
+open OUnit
+
+(* Names which are likely to cause problems *)
+let names = [
+  "";
+  ".";
+  "..";
+  "/";
+  "!";
+  String.make 128 '0';
+]
+
 (* For each VDI we check that:
    1. it shows up in a SR.scan
    2. attach RO, activate, deactivate, detach works
    3. attach RW, activate, deactivate, detach works
 *)
+let test_name n () = ()
 
-(* Create a VDI with name "" *)
+let vdi_name_suite = "vdi_name_suite" >::: (List.map (fun n -> n >:: test_name n) names)
 
-(* Create a VDI with name ".." *)
+open Cmdliner
 
-(* Create a VDI with name "/" *)
+let start verbose queue sr = match queue, sr with
+  | Some queue, Some sr ->
+    let suite = "storage" >:::
+      [
+        vdi_name_suite;
+      ] in
+    let (_: test_result list) = run_test_tt ~verbose suite in
+    ()
+  | _, _ ->
+    Printf.fprintf stderr "Please supply both a queue name and an SR\n%!";
+    ()
 
-(* Create a VDI with a very long name *)
+let cmd =
+  let doc = "Storage component test" in
+  let man = [
+    `S "DESCRIPTION";
+    `P "Test a storage implementation via the SMAPI.";
+    `S "USAGE";
+    `P "$(tname) <queue name> <sr name>";
+    `P "-- test the service listening on <queue name> using the existing attached SR <sr name>.";
+  ] in
+  let verbose =
+    let doc = "Print verbose output" in
+    Arg.(value & flag & info ["verbose"; "v"] ~doc) in
+  let queue =
+    let doc = "The queue name where the storage implementation is listening." in
+    Arg.(value & pos 0 (some string) None & info [] ~doc) in
+  let sr =
+    let doc = "The attached SR." in
+    Arg.(value & pos 1 (some string) None & info [] ~doc) in
 
-(* Create two VDIs with the same name *)
+  Term.(pure start $ verbose $ queue $ sr),
+  Term.info "test" ~doc ~man
 
+let () = match Term.eval cmd with
+ | `Error _ -> exit 1
+ | _ -> exit 0
 
-
-let _ = ()
