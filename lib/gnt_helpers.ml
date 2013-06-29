3c013ad4a7255160101eba4d524ddbc56f08b6ba
John Else <john.else@citrix.com>
2013-06-29 11:15:29 +0100

    Implement Gnt_helpers.with_gnttab
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/gnt_helpers.ml
+++ lib/gnt_helpers.ml
@@ -1,5 +1,16 @@
 open Gnt
 
+let with_gnttab f =
+	let handle = Gnttab.interface_open () in
+	let result = try
+		f handle
+	with e ->
+		Gnttab.interface_close handle;
+		raise e
+	in
+	Gnttab.interface_close handle;
+	result
+
 let with_gntshr f =
 	let handle = Gntshr.interface_open () in
 	let result = try
