fbf7d580dab19154ee0009c491aa60113ee3884e
Akshay Ramani <akshay.ramani@citrix.com>
2013-07-04 10:03:08 +0100

    Correct the spelling of vdi_on_boot_mode_incompatable_with_operation

--- ocaml/idl/api_errors.ml
+++ ocaml/idl/api_errors.ml
@@ -204,7 +204,7 @@ let vdi_missing = "VDI_MISSING"
 let vdi_incompatible_type = "VDI_INCOMPATIBLE_TYPE"
 let vdi_not_managed = "VDI_NOT_MANAGED"
 let vdi_io_error = "VDI_IO_ERROR"
-let vdi_on_boot_mode_incompatable_with_operation = "VDI_ON_BOOT_MODE_INCOMPATABLE_WITH_OPERATION"
+let vdi_on_boot_mode_incompatible_with_operation = "VDI_ON_BOOT_MODE_INCOMPATABLE_WITH_OPERATION"
 let vdi_not_in_map = "VDI_NOT_IN_MAP"
 let cannot_create_state_file = "CANNOT_CREATE_STATE_FILE"
 
