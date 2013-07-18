d27571da810497f08172e352ee6d6928144bc581
Si Beaumont <simon.beaumont@citrix.com>
2013-07-18 15:27:15 +0100

    Xapi_pci_helpers factored parse function into PCI_DB module
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/xapi_pci_helpers.ml
+++ ocaml/xapi/xapi_pci_helpers.ml
@@ -106,69 +106,66 @@ module PCI_DB = struct
 
 	let print t =
 		List.iter (fun s -> print_string s) (strings_of_t t)
+
+	let of_file path =
+		let lines = Unixext.read_lines ~path in
+		let lines =
+			List.filter (fun s -> (not (String.startswith "#" s || s = ""))) lines in
+		let strip = String.strip String.isspace in
+		let cur_class, cur_vendor, cur_device = ref None, ref None, ref None in
+		let parse_one_line line pci_db =
+			match String.explode (String.sub line 0 2) with
+			| 'C' :: _ -> (* this is a class definition *)
+				cur_vendor := None; cur_device := None;
+				let line = String.sub_to_end line 2 in
+				let [id; name] = String.split ' ' line ~limit:2 in
+				let pci_class =
+					{c_name = strip name; subclass_names = Hashtbl.create 8} in
+				add_class pci_db id pci_class;
+				cur_class := Some id
+			| ['\t'; '\t'] ->
+				let line = String.sub_to_end line 2 in
+				begin match !cur_device with
+				| Some d_id -> (* this is a subdevice definition *)
+					let [sv_id; sd_id; name] = String.split ' ' line ~limit:3 in
+					add_subdevice pci_db (Opt.unbox !cur_vendor) d_id sv_id sd_id (strip name)
+				| None -> ()
+				end
+			| '\t' :: _ ->
+				let line = String.sub_to_end line 1 in
+				begin match !cur_vendor with
+				| Some v -> (* this is a device definition *)
+					let [d_id; name] = String.split ' ' line ~limit:2 in
+					let device =
+						{d_name = strip name; subdevice_names = Hashtbl.create 0} in
+					add_device pci_db (Opt.unbox !cur_vendor) d_id device;
+					cur_device := Some d_id
+				| None -> (* this is a subclass definition *)
+					let [sc_id; name] = String.split ' ' line ~limit:2 in
+					add_subclass pci_db (Opt.unbox !cur_class) sc_id (strip name)
+				end
+			| _ -> (* this is a vendor definition *)
+				cur_class := None; cur_device := None;
+				let [v_id; name] = String.split ' ' line ~limit:2 in
+				let vendor =
+					{v_name = strip name; devices = Hashtbl.create 8} in
+				add_vendor pci_db v_id vendor;
+				cur_vendor := Some v_id
+		in
+		let rec parse_lines lines pci_db =
+			match lines with
+			| [] -> pci_db
+			| line :: remaining ->
+				parse_one_line line pci_db;
+				parse_lines remaining pci_db
+		in
+		let pci_db = make 24 2048 in
+		parse_lines lines pci_db
 end
-
-open PCI_DB
-
-let parse_file path =
-	let lines = Unixext.read_lines ~path in
-	let lines =
-		List.filter (fun s -> (not (String.startswith "#" s || s = ""))) lines in
-	let strip = String.strip String.isspace in
-	let cur_class, cur_vendor, cur_device = ref None, ref None, ref None in
-	let parse_one_line line pci_db =
-		match String.explode (String.sub line 0 2) with
-		| 'C' :: _ -> (* this is a class definition *)
-			cur_vendor := None; cur_device := None;
-			let line = String.sub_to_end line 2 in
-			let [id; name] = String.split ' ' line ~limit:2 in
-			let pci_class =
-				{c_name = strip name; subclass_names = Hashtbl.create 8} in
-			add_class pci_db id pci_class;
-			cur_class := Some id
-		| ['\t'; '\t'] ->
-			let line = String.sub_to_end line 2 in
-			begin match !cur_device with
-			| Some d_id -> (* this is a subdevice definition *)
-				let [sv_id; sd_id; name] = String.split ' ' line ~limit:3 in
-				add_subdevice pci_db (Opt.unbox !cur_vendor) d_id sv_id sd_id (strip name)
-			| None -> ()
-			end
-		| '\t' :: _ ->
-			let line = String.sub_to_end line 1 in
-			begin match !cur_vendor with
-			| Some v -> (* this is a device definition *)
-				let [d_id; name] = String.split ' ' line ~limit:2 in
-				let device =
-					{d_name = strip name; subdevice_names = Hashtbl.create 0} in
-				add_device pci_db (Opt.unbox !cur_vendor) d_id device;
-				cur_device := Some d_id
-			| None -> (* this is a subclass definition *)
-				let [sc_id; name] = String.split ' ' line ~limit:2 in
-				add_subclass pci_db (Opt.unbox !cur_class) sc_id (strip name)
-			end
-		| _ -> (* this is a vendor definition *)
-			cur_class := None; cur_device := None;
-			let [v_id; name] = String.split ' ' line ~limit:2 in
-			let vendor =
-				{v_name = strip name; devices = Hashtbl.create 8} in
-			add_vendor pci_db v_id vendor;
-			cur_vendor := Some v_id
-	in
-	let rec parse_lines lines pci_db =
-		match lines with
-		| [] -> pci_db
-		| line :: remaining ->
-			parse_one_line line pci_db;
-			parse_lines remaining pci_db
-	in
-	
-	let pci_db = PCI_DB.make 24 2048 in
-	parse_lines lines pci_db
 
 let () =
 	try
-		let db = parse_file "/usr/share/hwdata/pci.ids" in
-		print db
+		let db = PCI_DB.of_file "/usr/share/hwdata/pci.ids" in
+		PCI_DB.print db
 	with e ->
 		failwith "Failed to parse pci database"
