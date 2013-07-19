37cab28408aee17af8e036593b6c27f2196871d4
Si Beaumont <simon.beaumont@citrix.com>
2013-07-19 17:33:14 +0100

    Factor out Pci_db into separate module for use by Xapi_pci_helpers
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

---
+++ ocaml/xapi/pci_db.ml
@@ -0,0 +1,195 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open Stringext
+
+type class_id = string
+type subclass_id = string
+type vendor_id = string
+type subvendor_id = string
+type device_id = string
+type subdevice_id = string
+
+type pci_class = {
+	c_name : string;
+	subclass_names : (subclass_id, string) Hashtbl.t
+}
+
+type device = {
+	d_name : string;
+	subdevice_names : ((subvendor_id * subdevice_id), string) Hashtbl.t
+}
+
+type vendor = {
+	v_name : string;
+	devices : (device_id, device) Hashtbl.t
+}
+
+type t = {
+	classes : (class_id, pci_class) Hashtbl.t;
+	vendors : (vendor_id, vendor) Hashtbl.t
+}
+
+let make c_size v_size =
+	{classes = Hashtbl.create c_size; vendors = Hashtbl.create v_size}
+
+let add_class t c_id c = Hashtbl.add t.classes c_id c
+let get_class t c_id = Hashtbl.find t.classes c_id
+
+let add_subclass t c_id sc_id name =
+	let c = get_class t c_id in
+	Hashtbl.add c.subclass_names sc_id name
+let get_subclass t c_id sc_id =
+	let c = get_class t c_id in
+	Hashtbl.find c.subclass_names sc_id
+
+let add_vendor t v_id v = Hashtbl.add t.vendors v_id v
+let get_vendor t v_id = Hashtbl.find t.vendors v_id
+
+let add_device t v_id d_id d =
+	let v = get_vendor t v_id in
+	Hashtbl.add v.devices d_id d
+let get_device t v_id d_id =
+	let v = get_vendor t v_id in
+	Hashtbl.find v.devices d_id
+
+let add_subdevice t v_id d_id sv_id sd_id name =
+	let d = get_device t v_id d_id in
+	Hashtbl.add d.subdevice_names (sv_id, sd_id) name
+let get_subdevice t v_id d_id sv_id sd_id =
+	let d = get_device t v_id d_id in
+	Hashtbl.find d.subdevice_names (sv_id, sd_id)
+
+let strings_of_subclasses pci_class =
+	Hashtbl.fold
+		(fun sc_id sc_name lines ->
+			let line = Printf.sprintf "\t%s %s\n" sc_id sc_name in
+			line :: lines
+		) pci_class.subclass_names []
+
+let strings_of_classes t =
+	Hashtbl.fold
+		(fun c_id pci_class class_lines ->
+			let subclass_lines = strings_of_subclasses pci_class in
+			let class_line = Printf.sprintf "C %s %s\n" c_id pci_class.c_name in
+			class_line :: subclass_lines @ class_lines
+		) t.classes []
+
+let strings_of_subdevices device =
+	Hashtbl.fold
+		(fun (sv_id, sd_id) sd_name lines ->
+			let line = Printf.sprintf "\t\t%s %s  %s\n" sv_id sd_id sd_name in
+			line :: lines
+		) device.subdevice_names []
+
+let strings_of_devices vendor =
+	Hashtbl.fold
+		(fun d_id device device_lines ->
+			let subdevice_lines = strings_of_subdevices device in
+			let device_line = Printf.sprintf "\t%s  %s\n" d_id device.d_name in
+			device_line :: subdevice_lines @ device_lines
+		) vendor.devices []
+
+let strings_of_vendors t =
+	Hashtbl.fold
+		(fun v_id vendor vendor_lines ->
+			let device_lines = strings_of_devices vendor in
+			let vendor_line = Printf.sprintf "%s  %s\n" v_id vendor.v_name in
+			vendor_line :: device_lines @ vendor_lines
+		) t.vendors []
+
+let strings_of_t t =
+	strings_of_vendors t @ strings_of_classes t
+
+let print t =
+	List.iter (fun s -> print_string s) (strings_of_t t)
+
+let of_file path =
+	let lines = Unixext.read_lines ~path in
+	let lines =
+		List.filter (fun s -> (not (String.startswith "#" s || s = ""))) lines in
+	let strip = String.strip String.isspace in
+	let cur_class, cur_vendor, cur_device = ref None, ref None, ref None in
+	let parse_one_line line pci_db =
+		match String.explode (String.sub line 0 2) with
+		| 'C' :: _ -> (* this is a class definition *)
+			cur_vendor := None; cur_device := None;
+			let line = String.sub_to_end line 2 in
+			begin match String.split ' ' line ~limit:2 with
+			| [id; name] ->
+				let pci_class =
+					{c_name = strip name; subclass_names = Hashtbl.create 8} in
+				add_class pci_db id pci_class;
+				cur_class := Some id
+			| _ -> failwith "Malformed class definition"
+			end
+		| ['\t'; '\t'] ->
+			let line = String.sub_to_end line 2 in
+			begin match !cur_device with
+			| Some d_id -> (* this is a subdevice definition *)
+				begin match String.split ' ' line ~limit:3 with
+				| [sv_id; sd_id; name] ->
+					add_subdevice pci_db (Opt.unbox !cur_vendor) d_id sv_id sd_id (strip name)
+				| _ -> failwith "Malformed subdevice definition"
+				end
+			| None -> ()
+			end
+		| '\t' :: _ ->
+			let line = String.sub_to_end line 1 in
+			begin match !cur_vendor with
+			| Some v -> (* this is a device definition *)
+				begin match String.split ' ' line ~limit:2 with
+				| [d_id; name] ->
+					let device =
+						{d_name = strip name; subdevice_names = Hashtbl.create 0} in
+					add_device pci_db (Opt.unbox !cur_vendor) d_id device;
+					cur_device := Some d_id
+				| _ -> failwith "Malformed device definition"
+				end
+			| None -> (* this is a subclass definition *)
+				begin match String.split ' ' line ~limit:2 with
+				| [sc_id; name] ->
+					add_subclass pci_db (Opt.unbox !cur_class) sc_id (strip name)
+				| _ -> failwith "Malformed subclass definition"
+				end
+			end
+		| _ -> (* this is a vendor definition *)
+			cur_class := None; cur_device := None;
+			begin match String.split ' ' line ~limit:2 with
+			| [v_id; name] ->
+				let vendor =
+					{v_name = strip name; devices = Hashtbl.create 8} in
+				add_vendor pci_db v_id vendor;
+				cur_vendor := Some v_id
+			| _ -> failwith "Malformed vendor definition"
+			end
+	in
+	let rec parse_lines lines pci_db =
+		match lines with
+		| [] -> pci_db
+		| line :: remaining ->
+			parse_one_line line pci_db;
+			parse_lines remaining pci_db
+	in
+	let pci_db = make 24 2048 in
+	parse_lines lines pci_db
+
+let () =
+	(* For testing: makes full pci.ids db and prints it *)
+	try
+		let db = of_file "/usr/share/hwdata/pci.ids" in
+		print db
+	with e ->
+		print_string (Printf.sprintf "%s\n" (Printexc.to_string e));
+		failwith "Failed to parse pci database"
