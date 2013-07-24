a940917246fdc5eb646151a15df3fdf835fadd67
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-07-24 17:18:57 +0100

    Remove networkd from xen-api in favour of xcp-networkd
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

--- ocaml/test/suite.ml
+++ ocaml/test/suite.ml
@@ -22,7 +22,6 @@ let base_suite =
 			Test_xapi_db_upgrade.test;
 			Test_ca91480.test;
 			Test_vdi_allowed_operations.test;
-			Test_pr1510.suite;
 			Test_pool_apply_edition.test;
 			Test_pool_license.test;
 			Test_platformdata.test;
