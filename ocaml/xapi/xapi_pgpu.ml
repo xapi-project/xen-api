005d4461160ccea05bf073db5706388e30347e85
Si Beaumont <simon.beaumont@citrix.com>
2013-08-07 17:19:52 +0100

    CP-5881: Disable any newly-unsupported vGPU types
    If vGPU types become unsupported (as a result of uninstalling the necessary
    rpms from the vGPU vendor) then we should remove those types from the enabled
    types of the PGPU.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/xapi_pgpu.ml
+++ ocaml/xapi/xapi_pgpu.ml
@@ -56,6 +56,10 @@ let update_gpus ~__context ~host =
 						let (rf, rc) = List.find (fun (_, rc) -> rc.API.pGPU_PCI = pci) existing_pgpus in
 						(* Pick up any new supported vGPU configs on the host *)
 						Db.PGPU.set_supported_VGPU_types ~__context ~self:rf ~value:supported_VGPU_types;
+						let pruned_enabled_types = List.filter
+							(fun t -> List.mem t supported_VGPU_types)
+							(Db.PGPU.get_enabled_VGPU_types ~__context ~self:rf) in
+						Db.PGPU.set_enabled_VGPU_types ~__context ~self:rf ~value:pruned_enabled_types;
 						(rf, rc)
 					with Not_found ->
 						let self = create ~__context ~pCI:pci
