d2cf06fba0c048d0ab6f1640117162d04c566772
Si Beaumont <simon.beaumont@citrix.com>
2013-08-07 16:56:30 +0100

    CP-5881: Don't enable new vGPU types on startup
    If new vGPU types become supported then don't enable them by default (doing so
    might mess up a users reservation plans).
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/xapi_pgpu.ml
+++ ocaml/xapi/xapi_pgpu.ml
@@ -54,9 +54,8 @@ let update_gpus ~__context ~host =
 				let pgpu =
 					try
 						let (rf, rc) = List.find (fun (_, rc) -> rc.API.pGPU_PCI = pci) existing_pgpus in
-						(* Pick up any new vGPU configs on the host *)
+						(* Pick up any new supported vGPU configs on the host *)
 						Db.PGPU.set_supported_VGPU_types ~__context ~self:rf ~value:supported_VGPU_types;
-						Db.PGPU.set_enabled_VGPU_types ~__context ~self:rf ~value:supported_VGPU_types;
 						(rf, rc)
 					with Not_found ->
 						let self = create ~__context ~pCI:pci
