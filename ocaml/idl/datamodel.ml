1909cbb95335fe83d027df745d65892ae6affde8
Si Beaumont <simon.beaumont@citrix.com>
2013-08-09 17:16:11 +0100

    CP-5811: Add VGPU_type.{max_heads, pGPU_footprint}
    Added the above fields to the datamodel. The max_heads is the number of
    displays the VGPU type can support and is taken directly from the config file.
    
    The pGPU_footprint is an abstract utilisation tracking metric and is internal
    only. It is calculated using the max_instance field from the VGPU config.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -7955,6 +7955,8 @@ let vgpu_type =
 			field ~qualifier:StaticRO ~ty:String ~lifecycle:[Published, rel_vgpu_tech_preview, ""] ~default_value:(Some (VString "")) "vendor_name" "Name of VGPU vendor";
 			field ~qualifier:StaticRO ~ty:String ~lifecycle:[Published, rel_vgpu_tech_preview, ""] ~default_value:(Some (VString "")) "model_name" "Model name associated with the VGPU type";
 			field ~qualifier:StaticRO ~ty:Int ~lifecycle:[Published, rel_vgpu_tech_preview, ""] ~default_value:(Some (VInt 0L)) "framebuffer_size" "Framebuffer size of the VGPU type, in bytes";
+			field ~qualifier:StaticRO ~ty:Int ~lifecycle:[Published, rel_vgpu_tech_preview, ""] ~default_value:(Some (VInt 0L)) "max_heads" "Maximum number of displays supported by the VGPU type";
+			field ~qualifier:StaticRO ~ty:Int ~lifecycle:[Published, rel_vgpu_tech_preview, ""] ~internal_only:true ~default_value:(Some (VInt 0L)) "pGPU_footprint" "Abstract size for tracking PGPU utilisation";
 			field ~qualifier:DynamicRO ~ty:(Set (Ref _pgpu)) ~lifecycle:[Published, rel_vgpu_tech_preview, ""] "supported_on_PGPUs" "List of PGPUs that support this VGPU type";
 			field ~qualifier:DynamicRO ~ty:(Set (Ref _pgpu)) ~lifecycle:[Published, rel_vgpu_tech_preview, ""] "enabled_on_PGPUs" "List of PGPUs that have this VGPU type enabled";
 			field ~qualifier:DynamicRO ~ty:(Set (Ref _vgpu)) ~lifecycle:[Published, rel_vgpu_tech_preview, ""] "VGPUs" "List of VGPUs of this type";
