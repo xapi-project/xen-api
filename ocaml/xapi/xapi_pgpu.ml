49778068df6315900bd10b17b09440fb97d71dd9
John Else <john.else@citrix.com>
2013-08-12 11:08:20 +0100

    CP-5910: Implement Xapi_pgpu.assert_can_run_VGPU and supporting functions
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_pgpu.ml
+++ ocaml/xapi/xapi_pgpu.ml
@@ -80,6 +80,15 @@ let update_gpus ~__context ~host =
 	let obsolete_pgpus = List.set_difference existing_pgpus current_pgpus in
 	List.iter (fun (self, _) -> Db.PGPU.destroy ~__context ~self) obsolete_pgpus
 
+let assert_VGPU_type_enabled ~__context ~self ~value =
+	let enabled_VGPU_types =
+		Db.PGPU.get_enabled_VGPU_types ~__context ~self
+	in
+	if not (List.mem value enabled_VGPU_types)
+	then raise (Api_errors.Server_error
+		(Api_errors.vgpu_type_not_enabled,
+			List.map Ref.string_of (value :: enabled_VGPU_types)))
+
 let assert_VGPU_type_supported ~__context ~self ~value =
 	let supported_VGPU_types =
 		Db.PGPU.get_supported_VGPU_types ~__context ~self
@@ -160,3 +169,68 @@ let set_GPU_group ~__context ~self ~value =
 				(Api_errors.pgpu_not_compatible_with_gpu_group,
 				[gpu_type; "[" ^ String.concat ", " group_types ^ "]"]))
 	)
+
+(* Any VGPUs already running on this PGPU must have the same type as the new
+ * For now, we only allow one VGPU_type to run on a PGPU at any one time. *)
+let assert_VGPU_type_allowed ~__context ~self ~value =
+	let resident_VGPUs = Db.PGPU.get_resident_VGPUs ~__context ~self in
+	(match resident_VGPUs with
+	| [] -> ()
+	| resident_VGPU :: _ ->
+		let running_type =
+			Db.VGPU.get_type ~__context ~self:resident_VGPU
+		in
+		if running_type <> value
+		then raise (Api_errors.Server_error (
+			Api_errors.vgpu_type_not_compatible_with_running_type,
+			[
+				Ref.string_of self;
+				Ref.string_of value;
+				Ref.string_of running_type;
+			])))
+
+let assert_can_run_VGPU ~__context ~self ~vgpu =
+	let new_type = Db.VGPU.get_type ~__context ~self:vgpu in
+	(* Check user has enabled this type - this implies it's supported. *)
+	assert_VGPU_type_enabled ~__context ~self ~value:new_type;
+	(* Check our internal restrictions allow this VGPU to run on the PGPU. *)
+	assert_VGPU_type_allowed ~__context ~self ~value:new_type;
+	let pci = Db.PGPU.get_PCI ~__context ~self in
+	if Xapi_vgpu_type.requires_passthrough ~__context ~self:new_type
+	then begin
+		(* For passthrough VGPUs, we just check that there are functions
+		 * available. *)
+		if Pciops.get_free_functions ~__context pci = 0
+		then
+			let attached_VMs = Db.PCI.get_attached_VMs ~__context ~self:pci in
+			raise (Api_errors.Server_error (Api_errors.pgpu_in_use_by_vm,
+				List.map Ref.string_of attached_VMs))
+	end else begin
+		(* For virtual VGPUs, we check that:
+		 * 1. The underlying PCI device is not passed through to a VM. *)
+		(match Db.PCI.get_attached_VMs ~__context ~self:pci with
+		| [] -> ()
+		| attached_VMs ->
+			raise (Api_errors.Server_error (Api_errors.pgpu_in_use_by_vm,
+				List.map Ref.string_of attached_VMs)));
+		(* 2. There is remaining capacity to run this VGPU on the PGPU. *)
+		let capacity = Db.PGPU.get_capacity ~__context ~self in
+		let resident_VGPUs = Db.PGPU.get_resident_VGPUs ~__context ~self in
+		let utilisation =
+			List.fold_left
+				(fun acc vgpu ->
+					let _type = Db.VGPU.get_type ~__context ~self:vgpu in
+					let footprint =
+						Db.VGPU_type.get_pGPU_footprint ~__context ~self:_type
+					in
+					Int64.add acc footprint)
+				0L resident_VGPUs
+		in
+		let new_footprint =
+			Db.VGPU_type.get_pGPU_footprint ~__context ~self:new_type
+		in
+		if (Int64.add utilisation new_footprint) > capacity
+		then raise (Api_errors.Server_error (
+			Api_errors.pgpu_insufficient_capacity_for_vgpu,
+			[Ref.string_of self; Ref.string_of vgpu]))
+	end
