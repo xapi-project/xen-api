b75a5b389043a33c741088131b7c34c69353d82e
John Else <john.else@citrix.com>
2013-08-13 15:57:44 +0100

    CA-110805: Add a function to find the host's VGA device
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_pci.mli
+++ ocaml/xapi/xapi_pci.mli
@@ -28,3 +28,6 @@ val string_of_pci : __context:Context.t -> self:API.ref_PCI -> string
 
 (** Synchronise the PCI objects in the database with the actual devices in the host. *)
 val update_pcis : __context:Context.t -> host:API.ref_host -> unit
+
+(** Get the PCI id of the host's display device. *)
+val get_system_display_device : unit -> string option
