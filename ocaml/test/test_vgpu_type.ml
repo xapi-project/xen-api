209e13a254b5fab9ab1c64e014a7870d1334f8ad
John Else <john.else@citrix.com>
2013-08-30 01:24:34 +0100

    CA-114551: Add unit test
    find_or_create-ing a VGPU type, then find_or_create-ing the same type
    but with modified framebuffer_size and/or max_heads and/or size, should
    result in the original VGPU type being updated with the new values.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/test/test_vgpu_type.ml
+++ ocaml/test/test_vgpu_type.ml
@@ -13,6 +13,8 @@
  *)
 
 open OUnit
+open Test_common
+open Test_vgpu_common
 open Xapi_vgpu_type
 
 (* This test generates a lot of print --- set skip to false to enable *)
@@ -44,7 +46,52 @@ let print_nv_types () =
 		print_string (Printf.sprintf "%s\n" (Printexc.to_string e));
 		assert false (* fail *)
 
+let test_find_or_create () =
+	let __context = make_test_database () in
+	let k100_ref = find_or_create ~__context k100 in
+	(* Check the VGPU type created in the DB has the expected fields. *)
+	assert_equal
+		~msg:"k100 framebuffer_size is incorrect"
+		k100.framebuffer_size
+		(Db.VGPU_type.get_framebuffer_size ~__context ~self:k100_ref);
+	assert_equal
+		~msg:"k100 max_heads is incorrect"
+		k100.max_heads
+		(Db.VGPU_type.get_max_heads ~__context ~self:k100_ref);
+	assert_equal
+		~msg:"k100 size is incorrect"
+		k100.size
+		(Db.VGPU_type.get_size ~__context ~self:k100_ref);
+	(* Simulate an update of framebuffer_size, max_heads and size as if the
+	 * config file had been updated. *)
+	let new_k100 = {
+		k100 with
+		framebuffer_size = (Int64.mul k100.framebuffer_size 2L);
+		max_heads = (Int64.mul k100.max_heads 2L);
+		size = (Int64.mul k100.size 2L);
+	} in
+	(* We can ignore the result as it should be the same as the VGPU_type ref
+	 * obtained earlier. *)
+	let (_: API.ref_VGPU_type) = find_or_create ~__context new_k100 in
+	(* Make sure the existing VGPU type object in the database
+	 * has been updated. *)
+	assert_equal
+		~msg:"k100 framebuffer_size was not updated"
+		new_k100.framebuffer_size
+		(Db.VGPU_type.get_framebuffer_size ~__context ~self:k100_ref);
+	assert_equal
+		~msg:"k100 max_heads was not updated"
+		new_k100.max_heads
+		(Db.VGPU_type.get_max_heads ~__context ~self:k100_ref);
+	assert_equal
+		~msg:"k100 size was not updated"
+		new_k100.size
+		(Db.VGPU_type.get_size ~__context ~self:k100_ref)
+
 let test =
 	"test_vgpu_type" >:::
-		["test_of_conf_file" >:: test_of_conf_file k100_path;
-		 "print_nv_types" >:: print_nv_types;]
+		[
+			"test_of_conf_file" >:: test_of_conf_file k100_path;
+			"print_nv_types" >:: print_nv_types;
+			"test_find_or_create" >:: test_find_or_create;
+		]
