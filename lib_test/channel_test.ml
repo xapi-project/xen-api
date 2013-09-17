6ac09124bf9be29ce09268be8d1750f4b9fd716b
David Scott <dave.scott@eu.citrix.com>
2013-09-17 10:39:54 +0000

    test: add some unit tests for channel passing
    One of the tests currently fails.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib_test/channel_test.ml
+++ lib_test/channel_test.ml
@@ -13,24 +13,48 @@
  *)
 open OUnit
 
-let dup x =
+let dup_automatic x =
   let x = Xcp_channel.t_of_file_descr x in
   let y = Xcp_channel.rpc_of_t x in
   let z = Xcp_channel.t_of_rpc y in
   Xcp_channel.file_descr_of_t z
 
+let dup_sendmsg x =
+  let protos = Posix_channel.send x in
+  let proto = List.find (function
+    | Xcp_channel_protocol.Unix_sendmsg(_, _, _) -> true
+    | _ -> false
+  ) protos in
+  Posix_channel.receive [ proto ]
+
 let count_fds () = Array.length (Sys.readdir "/proc/self/fd")
 
 (* dup stdout, check /proc/pid/fd *)
-let check_for_leak () =
+let check_for_leak dup_function () =
   let before = count_fds () in
-  let stdout2 = dup Unix.stdout in
+  let stdout2 = dup_function Unix.stdout in
   let after = count_fds () in
   assert_equal ~printer:string_of_int (before + 1) after;
   Unix.close stdout2;
   let after' = count_fds () in
   assert_equal ~printer:string_of_int before after'
 
+let dup_proxy x =
+  let protos = Posix_channel.send x in
+  let proto = List.find (function
+    | Xcp_channel_protocol.TCP_proxy(ip, port) -> true
+    | _ -> false
+  ) protos in
+  Posix_channel.receive [ proto ]
+
+let check_for_leak_proxy () =
+  let before = count_fds () in
+  let stdout2 = dup_proxy Unix.stdout in
+  let after = count_fds () in
+  assert_equal ~printer:string_of_int (before + 2) after;
+  Unix.close stdout2;
+  let after' = count_fds () in
+  assert_equal ~printer:string_of_int before after'
 
 let _ =
   let verbose = ref false in
@@ -41,7 +65,9 @@ let _ =
 
   let suite = "channel" >:::
     [
-      "check_for_leak" >:: check_for_leak;
+      "check_for_leak with automatic selection" >:: (check_for_leak dup_automatic);
+      "check_for_leak with sendmsg" >:: (check_for_leak dup_sendmsg);
+      "check_for_leak_proxy" >:: check_for_leak_proxy;
     ] in
 
   run_test_tt ~verbose:!verbose suite
