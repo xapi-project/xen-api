c92196b28d4c7ac054bd3cfe383458c1f50e4f63
John Else <john.else@citrix.com>
2013-10-11 15:00:53 +0100

    Add some functions for converting datasource metadata to JSON
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/rrdp_common.ml
+++ lib/rrdp_common.ml
@@ -62,6 +62,53 @@ let json_of_dss ?hdr timestamp (dss : (Ds.ds * Rrd.ds_owner) list) =
 	in
 	Printf.sprintf "%s%s\n" header payload
 
+(** Write the JSON metadata of datasource [ds] (i.e. not the value itself) to
+ * buffer [buf]. [owner] is Host unless specified. *)
+let json_metadata_of_ds ?(owner=Rrd.Host) ds buf =
+	let open Ds in
+	let add_string str = Buffer.add_string buf str in
+	let json_line_string ?(last=false) n v = add_string (Printf.sprintf "\"%s\":\"%s\"%s" n v (if last then "" else ","))
+	and json_line_float  ?(last=false) n v = add_string (Printf.sprintf "\"%s\":\"%.2f\"%s" n v (if last then "" else ",")) in
+	begin
+		add_string (Printf.sprintf "\"%s\":{" ds.ds_name);
+		if ds.ds_description != "" then (json_line_string "description" ds.ds_description);
+		json_line_string "owner" (match owner with
+			| Rrd.Host -> "host"
+			| Rrd.VM vm -> "vm " ^ vm
+			| Rrd.SR sr -> "sr " ^ sr);
+		json_line_string "value_type" (match ds.ds_value with
+			| Rrd.VT_Int64 _ -> "int64"
+			| Rrd.VT_Float _ -> "float"
+			| Rrd.VT_Unknown -> failwith "to_json: Impossible to represent VT_Unknown type");
+		json_line_string "type" (match ds.ds_type with
+			| Rrd.Gauge -> "absolute"
+			| Rrd.Absolute -> "rate"
+			| Rrd.Derive -> "absolute_to_rate");
+		json_line_string "units" ds.ds_units;
+		json_line_float "min" ds.ds_min;
+		json_line_float ~last:true "max" ds.ds_max;
+		add_string "}"
+	end
+
+(** Return a string containing the JSON metadata of a list of (ds * ds_owner)
+  * tuples [dss]. The string will not contain the values of the datasources. *)
+let json_metadata_of_dss (dss : (Ds.ds * Rrd.ds_owner) list) =
+	if List.length dss = 0 then
+		info "No data sources exported";
+	let buf = Buffer.create 100 in
+	Buffer.add_string buf "{\"datasources\":{";
+	let rec add_dss = function
+		| [] -> ()
+		| (ds, owner) :: [] -> json_metadata_of_ds ~owner ds buf
+		| (ds, owner) :: rest ->
+			json_metadata_of_ds ~owner ds buf;
+			Buffer.add_char buf ',';
+			add_dss rest
+	in
+	add_dss dss;
+	Buffer.add_string buf "}}";
+	Buffer.contents buf
+
 let wait_until_next_reading ?(neg_shift=0.5) () =
 	let next_reading = RRDD.Plugin.register N.name Rrd.Five_Seconds in
 	let wait_time = next_reading -. neg_shift in
