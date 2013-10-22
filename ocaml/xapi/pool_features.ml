fb971c38a75e19fb9443f269b59ec951b418992b
John Else <john.else@citrix.com>
2013-10-22 15:14:53 +0100

    SCTX-1585: Handle hosts having different lists of features
    During an upgrade, the older hosts will not indicate whether or not they
    support newer features. If a host doesn't support a feature, always
    treat as restricted on that host for the purposes of working out the
    pool restrictions.
    
    Signed-off-by: John Else <john.else@citrix.com>
    (cherry picked from commit fb82876687e7e6d30f7707308914866a61fe9482)

--- ocaml/xapi/pool_features.ml
+++ ocaml/xapi/pool_features.ml
@@ -46,7 +46,14 @@ let update_pool_features ~__context =
 	let rec find_additional = function
 		| [] -> []
 		| flag :: rest ->
-			let switches = List.map (function params -> bool_of_string (List.assoc flag params)) hosts in
+			let switches =
+				List.map
+					(function params ->
+						if List.mem_assoc flag params
+						then bool_of_string (List.assoc flag params)
+						else true)
+					hosts
+			in
 			(flag, string_of_bool (List.fold_left (||) false switches)) :: find_additional rest
 	in
 	let additional_restrictions = find_additional additional_flags in
