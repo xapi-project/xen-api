8c4d3d93f8c0e069b8b0de9c2e2df02a391aa2ef
Mike McClurg <mike.mcclurg@citrix.com>
2013-10-30 18:03:23 +0000

    Support legacy clients for config_file_sync
    During a rolling pool upgrade, if a slave xapi restarts then it will attempt to
    sync config files, and will expect the old-style passwd file response. The
    config_file_sync handler now supports versioned URIs, so that clients accessing
    the original URI will receive the legacy config files, while clients accessing
    the URI with a version number appended will receive the new config files.
    
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- ocaml/xapi/config_file_sync.ml
+++ ocaml/xapi/config_file_sync.ml
@@ -21,6 +21,12 @@ let superuser = "root"
 
 type config = { password : string } with rpc
 
+(* Increment this if config type changes *)
+let config_sync_version = 2
+
+let config_sync_uri =
+	Filename.concat Constants.config_sync_uri (string_of_int config_sync_version)
+
 let read_config_file () = { password = get_password superuser }
 
 let parse_config_string config = Jsonrpc.of_string config |> config_of_rpc
@@ -29,42 +35,53 @@ let write_config config = put_password superuser config.password
 
 let rewrite_config_files config = parse_config_string config |> write_config
 
-let transmit_config_files s =
-	let msg = read_config_file () |> rpc_of_config |> Jsonrpc.to_string in
-	Unix.write s msg 0 (String.length msg) |> ignore
+let write_to_fd s msg = Unix.write s msg 0 (String.length msg) |> ignore
 
-(** URL used by slaves to fetch dom0 config files (currently /etc/passwd) *)
+let transmit_config_files s =
+	read_config_file () |> rpc_of_config |> Jsonrpc.to_string |> write_to_fd s
+
+(* We still need to respect older XenServer hosts which are expecting
+   the entire /etc/password file. We need to make sure we send the
+   "un-shadowed" passwd file, so that slaves don't overwrite root's
+   password with an 'x'. *)
+let legacy_transmit_passwd s = unshadow () |> write_to_fd s
+
+(** URL used by slaves to fetch dom0 config files (currently just root's password) *)
 let config_file_sync_handler (req: Http.Request.t) s _ =
+  let current version =
+    let version = try int_of_string version with _ -> -1 in
+    version >= config_sync_version in
   debug "received request to write out dom0 config files";
   Xapi_http.with_context "Syncing dom0 config files over HTTP" req s
     (fun __context ->
+      let uri = Re_str.(split (regexp "/") req.Http.Request.uri) in
       req.Http.Request.close <- true;
       debug "sending headers";
       Http_svr.headers s (Http.http_200_ok ~keep_alive:false ());
-      debug "writing dom0 config files";
-      transmit_config_files s;
-      debug "finished writing dom0 config files"
-    )
+      match uri with
+      | [path; version] when current version ->
+         debug "writing dom0 config files";
+         transmit_config_files s;
+         debug "finished writing dom0 config files"
+      | _ ->
+         debug "writing legacy dom0 config files";
+         legacy_transmit_passwd s;
+         debug "finished writing legacy dom0 config files")
 
 let fetch_config_files_internal ~master_address ~pool_secret =
-
   Server_helpers.exec_with_new_task "fetch_config_files"
     (fun __context ->
       Helpers.call_api_functions ~__context
 				(fun rpc session_id ->
-
-					let request = Xapi_http.http_request ~cookie:[ "session_id", Ref.string_of session_id ]
-						Http.Get Constants.config_sync_uri in
+					let request = Xapi_http.http_request
+						~cookie:[ "session_id", Ref.string_of session_id ]
+						Http.Get config_sync_uri in
 					let open Xmlrpc_client in
 					let transport = SSL (SSL.make (), master_address, !Xapi_globs.https_port) in
 					with_transport transport
 						(with_http request
 							 (fun (response, fd) ->
-								 Unixext.string_of_fd fd
-							 )
-						)
-				)
-    )
+								 Unixext.string_of_fd fd))))
 
 (* Invoked on slave as a notification that config files may have changed. Slaves can use
    this to decide whether to sync the new config files if the hash is different from the
