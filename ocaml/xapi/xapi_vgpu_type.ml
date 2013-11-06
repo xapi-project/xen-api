7b6d7859b07cd17d96053822609dd6e4c8b1dde5
Si Beaumont <simon.beaumont@citrix.com>
2013-11-06 15:55:06 +0000

    CP-6751: Add max_resolution_{x,y} to VGPU_type
    These are parsed from the NVIDIA config files in the same way as the other
    parameters. The config file specifies a single string for max_resolution which
    we split into its dimensions.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/xapi_vgpu_type.ml
+++ ocaml/xapi/xapi_vgpu_type.ml
@@ -29,6 +29,8 @@ type vgpu_conf = {
 	framebufferlength : int64;
 	num_heads : int64;
 	max_instance : int64;
+	max_x : int64;
+	max_y : int64;
 	file_path : string;
 }
 
@@ -37,6 +39,8 @@ type vgpu_type = {
 	model_name : string;
 	framebuffer_size : int64;
 	max_heads : int64;
+	max_resolution_x : int64;
+	max_resolution_y : int64;
 	size : int64;
 	internal_config : (string * string) list;
 }
@@ -46,15 +50,19 @@ let entire_gpu = {
 	model_name = "passthrough";
 	framebuffer_size = 0L;
 	max_heads = 0L;
+	max_resolution_x = 0L;
+	max_resolution_y = 0L;
 	size = 0L;
 	internal_config = [];
 }
 
-let create ~__context ~vendor_name ~model_name ~framebuffer_size ~max_heads ~size ~internal_config =
+let create ~__context ~vendor_name ~model_name ~framebuffer_size ~max_heads
+		~max_resolution_x ~max_resolution_y ~size ~internal_config =
 	let ref = Ref.make () in
 	let uuid = Uuid.to_string (Uuid.make_uuid ()) in
 	Db.VGPU_type.create ~__context ~ref ~uuid ~vendor_name ~model_name
-		~framebuffer_size ~max_heads ~size ~internal_config;
+		~framebuffer_size ~max_heads ~max_resolution_x ~max_resolution_y
+		~size ~internal_config;
 	debug "VGPU_type ref='%s' created (vendor_name = '%s'; model_name = '%s')"
 		(Ref.string_of ref) vendor_name model_name;
 	ref
@@ -79,6 +87,14 @@ let find_or_create ~__context vgpu_type =
 			Db.VGPU_type.set_max_heads ~__context
 				~self:vgpu_type_ref
 				~value:vgpu_type.max_heads;
+		if vgpu_type.max_resolution_x <> rc.Db_actions.vGPU_type_max_resolution_x then
+			Db.VGPU_type.set_max_resolution_x ~__context
+				~self:vgpu_type_ref
+				~value:vgpu_type.max_resolution_x;
+		if vgpu_type.max_resolution_y <> rc.Db_actions.vGPU_type_max_resolution_x then
+			Db.VGPU_type.set_max_resolution_y ~__context
+				~self:vgpu_type_ref
+				~value:vgpu_type.max_resolution_y;
 		if vgpu_type.size <> rc.Db_actions.vGPU_type_size then
 			Db.VGPU_type.set_size ~__context
 				~self:vgpu_type_ref
@@ -93,6 +109,8 @@ let find_or_create ~__context vgpu_type =
 			~model_name:vgpu_type.model_name
 			~framebuffer_size:vgpu_type.framebuffer_size
 			~max_heads:vgpu_type.max_heads
+			~max_resolution_x:vgpu_type.max_resolution_x
+			~max_resolution_y:vgpu_type.max_resolution_y
 			~size:vgpu_type.size
 			~internal_config:vgpu_type.internal_config
 	| _ ->
@@ -113,16 +131,19 @@ let of_conf_file file_path =
 			) args in
 		Scanf.sscanf (List.assoc "plugin0.pdev_id" args) "\"0x%Lx\"" (fun pdev_id ->
 			(* NVIDIA key is "device_id:subdevice_id", N.B. not subvendor id *)
-			Scanf.sscanf (List.assoc "plugin0.vdev_id" args) "\"0x%Lx:0x%Lx\""
-				(fun vdev_id vsubdev_id ->
+			Scanf.sscanf (List.assoc "plugin0.vdev_id" args) "\"0x%Lx:0x%Lx\"" (fun vdev_id vsubdev_id ->
+				Scanf.sscanf (List.assoc "plugin0.max_resolution" args) "%Ldx%Ld" (fun max_x max_y ->
 					let framebufferlength = Int64.of_string
 						(List.assoc "plugin0.framebufferlength" args) in
 					let num_heads = Int64.of_string
 						(List.assoc "plugin0.num_heads" args) in
 					let max_instance = Int64.of_string
 						(List.assoc "plugin0.max_instance" args) in
-					{pdev_id; vdev_id; vsubdev_id; framebufferlength;
-					 num_heads; max_instance; file_path}))
+							{pdev_id; vdev_id; vsubdev_id; framebufferlength;
+							 num_heads; max_instance; max_x; max_y; file_path}
+				)
+			)
+		)
 	with e ->
 		raise (Parse_error e)
 
@@ -165,11 +186,13 @@ let relevant_vgpu_types pci_db pci_dev_ids =
 						conf.vdev_id conf.vsubdev_id)
 				and framebuffer_size = conf.framebufferlength
 				and max_heads = conf.num_heads
+				and max_resolution_x = conf.max_x
+				and max_resolution_y = conf.max_y
 				and size = Int64.div Constants.pgpu_default_size conf.max_instance
 				and internal_config = [Xapi_globs.vgpu_config_key, conf.file_path] in
 				let vgpu_type = {
 					vendor_name; model_name; framebuffer_size; max_heads;
-					size; internal_config}
+					max_resolution_x; max_resolution_y; size; internal_config}
 				in
 				build_vgpu_types pci_db (vgpu_type :: ac) tl
 			with Not_found | Failure "hd" ->
