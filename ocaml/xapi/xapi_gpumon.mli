70d9d8061fb5c3bbc94006b2e0361fb639b49f1f
John Else <john.else@citrix.com>
2013-11-21 15:34:36 +0000

    CP-6968: Add code to allow restarting gpumon from xapi
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ ocaml/xapi/xapi_gpumon.mli
@@ -0,0 +1,17 @@
+(*
+ * Copyright (C) 2006-2013 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+(** Stop gpumon if it's running, perform f, then start gpumon if
+  * no other threads which require gpumon to be stopped are running. *)
+val with_gpumon_stopped : f:(unit -> 'a) -> 'a
