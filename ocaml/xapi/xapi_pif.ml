71cfa54e266df575b0c7486a232ed1185ff84db1
Rob Hoes <rob.hoes@citrix.com>
2013-11-25 14:30:37 +0000

    Only attempt to bring up managed PIFs at start of day

--- ocaml/xapi/xapi_pif.ml
+++ ocaml/xapi/xapi_pif.ml
@@ -693,14 +693,17 @@ let calculate_pifs_required_at_start_of_day ~__context =
 	Db.PIF.get_records_where ~__context
 		~expr:(
 			And (
+				Eq (Field "managed", Literal "true"),
 				And (
-					Eq (Field "host", Literal (Ref.string_of localhost)),
-					Eq (Field "bond_slave_of", Literal (Ref.string_of Ref.null))
-				),
-				Or (Or (
-					Not (Eq (Field "bond_master_of", Literal "()")),
-					Eq (Field "physical", Literal "true")),
-					Not (Eq (Field "ip_configuration_mode", Literal "None"))
+					And (
+						Eq (Field "host", Literal (Ref.string_of localhost)),
+						Eq (Field "bond_slave_of", Literal (Ref.string_of Ref.null))
+					),
+					Or (Or (
+						Not (Eq (Field "bond_master_of", Literal "()")),
+						Eq (Field "physical", Literal "true")),
+						Not (Eq (Field "ip_configuration_mode", Literal "None"))
+					)
 				)
 			)
 		)
@@ -708,7 +711,7 @@ let calculate_pifs_required_at_start_of_day ~__context =
 let start_of_day_best_effort_bring_up () =
 	begin
 		Server_helpers.exec_with_new_task
-			"Bringing up physical PIFs"
+			"Bringing up managed physical PIFs"
 			(fun __context ->
 				let dbg = Context.string_of_task __context in
 				debug
