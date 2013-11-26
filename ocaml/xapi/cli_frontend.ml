dfa9f26d215b7f0cd6a7954d8f8297bb30f35060
David Scott <dave.scott@eu.citrix.com>
2013-11-26 12:09:59 +0000

    VDI.export: add a 'base' argument to export only VDI differences
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>
    (cherry picked from commit 62590b892b74ae0facb67123fc9e7cb8f3ca78d2)

--- ocaml/xapi/cli_frontend.ml
+++ ocaml/xapi/cli_frontend.ml
@@ -1749,7 +1749,7 @@ there are two or more empty CD devices, please use the command 'vbd-insert' and 
    "vdi-export",
    {
      reqd = [ "uuid"; "filename" ];
-     optn = [ "format"; "progress" ];
+     optn = [ "format"; "base"; "progress" ];
      help = "Export a VDI.";
      implementation=With_fd Cli_operations.vdi_export;
      flags = [];
