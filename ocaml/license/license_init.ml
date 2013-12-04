8d8d3432dc35adba438e448ab2599d89e3b6b90e
Mike McClurg <mike.mcclurg@citrix.com>
2013-12-04 16:00:35 +0000

    Refactor License_init.initialise
    Clean up initialise, and move xcp-mode error handler into initialise, since we
    now call V6client functions from initialise.
    
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- ocaml/license/license_init.ml
+++ ocaml/license/license_init.ml
@@ -15,47 +15,54 @@
 module D = Debug.Make(struct let name="license" end)
 open D
 
-let v6_initialise ~__context ~edition ~params =
-	try
-		V6client.apply_edition ~__context edition params
-	with Api_errors.Server_error (code, []) when code = Api_errors.v6d_failure ->
-		(* Couldn't communicate with v6d, so fall back to running in free/libre
-		 * "xcp" mode, with all standard features enabled and no additional
-		 * features advertised. This is the same as the "free" edition from v6d
-		 * for most purposes but not for pool-join: see assert_restrictions_match
-		 * in pre_join_checks in ocaml/xapi/xapi_pool.ml *)
-		"free/libre", Features.all_features, []
+let fst4 (e,_,_,_) = e
+and lst4 (_,_,_,i) = i
+
+let find_min_edition allowed_editions =
+	List.fold_left
+		(fun a b ->
+		 if (lst4 a) < (lst4 b)
+		 then a else b)
+		("","","",max_int)
+		allowed_editions
+	|> fst4
 
 (* xapi calls this function upon startup *)
 let initialise ~__context ~host =
+	let set_licensing edition features additional =
+		Db.Host.set_edition ~__context ~self:host ~value:edition;
+		(* Copy resulting license to the database *)
+		Xapi_host.copy_license_to_db ~__context ~host ~features ~additional in
+
 	try
 		let edition = Db.Host.get_edition ~__context ~self:host in
-		let fst4 (e,_,_,_) = e in
 		let allowed_editions = V6client.get_editions "License_init" in
-		(* We changed the editions in a later version of XenServer. If an
-		   old xapi restarts during an upgrade, we may have an invalid
-		   edition in the database. *)
+
+		(* If we change the editions names in a later version of
+		   XenServer, and this xapi restarts during an upgrade, we may
+		   have an invalid edition in the database. *)
 		let edition =
 			if List.mem edition (List.map fst4 allowed_editions)
 			then edition
 			else
 				begin
-					let default_edition =
-						List.fold_left
-							(fun a b ->
-							 let lst4 (_,_,_,i) = i in
-							 if (lst4 a) < (lst4 b) then a else b)
-							("","","",max_int)
-							allowed_editions
-					|> fst4 in
+					let default_edition = find_min_edition allowed_editions in
 					warn "Edition %s not available on this host, defaulting \
 					      to %s edition instead" edition default_edition;
 					default_edition
 				end in
-		let edition', features, additional =
-			v6_initialise ~__context ~edition ~params:["force", "true"] in
-		Db.Host.set_edition ~__context ~self:host ~value:edition';
-		(* Copy resulting license to the database *)
-		Xapi_host.copy_license_to_db ~__context ~host ~features ~additional
-	with _ -> ()
 
+		let edition', features, additional =
+			V6client.apply_edition ~__context edition ["force", "true"] in
+		set_licensing edition' features additional
+
+	with
+	| Api_errors.Server_error (code, []) when code = Api_errors.v6d_failure ->
+		(* Couldn't communicate with v6d, so fall back to running in free/libre
+		 * "xcp" mode, with all standard features enabled and no additional
+		 * features advertised. This is the same as the "free" edition from v6d
+		 * for most purposes but not for pool-join: see assert_restrictions_match
+		 * in pre_join_checks in ocaml/xapi/xapi_pool.ml *)
+		set_licensing "free/libre" Features.all_features []
+
+	| _ -> ()
