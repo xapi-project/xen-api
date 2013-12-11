95d9a8d775e9741175b16ac9b68397ca96688d39
John Else <john.else@citrix.com>
2013-12-11 02:28:37 +0000

    Switch to using rrd-transport
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/rrdp_common.ml
+++ lib/rrdp_common.ml
@@ -12,103 +12,6 @@ module Common = functor (N : (sig val name : string end)) -> struct
 module D = Debug.Make(struct let name=N.name end)
 open D
 
-let json_of_ds ?(owner=Rrd.Host) ?(rshift=4) ds buf =
-	let open Ds in
-	let add_string str = 
-		for i=0 to rshift-1 do Buffer.add_char buf ' ' done; 
-		Buffer.add_string buf str in
-	let json_line_string ?(last=false) n v = add_string (Printf.sprintf "  \"%s\": \"%s\"%s\n" n v (if last then "" else ","))
-	and json_line_int64  ?(last=false) n v = add_string (Printf.sprintf "  \"%s\": \"%Ld\"%s\n" n v (if last then "" else ","))
-	and json_line_float ?(last=false) n v  = add_string (Printf.sprintf "  \"%s\": \"%.2f\"%s\n" n v (if last then "" else ",")) in
-	begin
-		add_string (Printf.sprintf "\"%s\": {\n" ds.ds_name);
-		if ds.ds_description != "" then (json_line_string "description" ds.ds_description);
-		json_line_string "owner" (match owner with | Rrd.Host -> "host" | Rrd.VM vm -> "vm " ^ vm | Rrd.SR sr -> "sr " ^ sr);
-		(match ds.ds_value with 
-			| Rrd.VT_Int64 i -> json_line_int64 "value" i; json_line_string "value_type" "int64"
-			| Rrd.VT_Float f -> json_line_float "value" f; json_line_string "value_type" "float"
-			| Rrd.VT_Unknown  -> failwith "to_json: Impossible to represent VT_Unknown type");
-		json_line_string "type" (match ds.ds_type with
-			| Rrd.Gauge -> "absolute"
-			| Rrd.Absolute -> "rate"
-			| Rrd.Derive -> "absolute_to_rate");
-		json_line_string "units" ds.ds_units;
-		json_line_float "min" ds.ds_min;
-		json_line_float ~last:true "max" ds.ds_max;
-		
-		add_string "},\n"; 
-		(* begin *)
-		(* 	Printf.printf "====== json_of_ds ======\n%!"; *)
-		(* 	Printf.printf "%s%!" (Buffer.contents buf); *)
-		(* 	Printf.printf "========================\n%!"; *)
-		(* end; *)
-	end
-
-let json_of_dss ?hdr timestamp (dss : (Ds.ds * Rrd.ds_owner) list) =
-	if List.length dss = 0 then
-		info "No data sources exported";
-	let buf = Buffer.create 100 in
-	List.iter (fun (ds, owner) -> json_of_ds ~owner ds buf) dss;
-	let dss = Buffer.contents buf in
-	let payload = 
-		Printf.sprintf "{\n  \"timestamp\": %Ld,\n  \"datasources\": {\n%s\n  }\n}" timestamp
-			(if String.length dss > 0 then (String.sub dss 0 (String.length dss - 2)) else "")
-	in 
-	let header = match hdr with
-		| Some hdrstr ->
-			(Printf.sprintf "%s%08x\n%s\n" hdrstr (String.length payload) 
-				(Digest.to_hex (Digest.string payload)))
-		| None -> ""
-	in
-	Printf.sprintf "%s%s\n" header payload
-
-(** Write the JSON metadata of datasource [ds] (i.e. not the value itself) to
- * buffer [buf]. [owner] is Host unless specified. *)
-let json_metadata_of_ds ?(owner=Rrd.Host) ds buf =
-	let open Ds in
-	let add_string str = Buffer.add_string buf str in
-	let json_line_string ?(last=false) n v = add_string (Printf.sprintf "\"%s\":\"%s\"%s" n v (if last then "" else ","))
-	and json_line_float  ?(last=false) n v = add_string (Printf.sprintf "\"%s\":\"%.2f\"%s" n v (if last then "" else ",")) in
-	begin
-		add_string (Printf.sprintf "\"%s\":{" ds.ds_name);
-		if ds.ds_description != "" then (json_line_string "description" ds.ds_description);
-		json_line_string "owner" (match owner with
-			| Rrd.Host -> "host"
-			| Rrd.VM vm -> "vm " ^ vm
-			| Rrd.SR sr -> "sr " ^ sr);
-		json_line_string "value_type" (match ds.ds_value with
-			| Rrd.VT_Int64 _ -> "int64"
-			| Rrd.VT_Float _ -> "float"
-			| Rrd.VT_Unknown -> failwith "to_json: Impossible to represent VT_Unknown type");
-		json_line_string "type" (match ds.ds_type with
-			| Rrd.Gauge -> "absolute"
-			| Rrd.Absolute -> "rate"
-			| Rrd.Derive -> "absolute_to_rate");
-		json_line_string "units" ds.ds_units;
-		json_line_float "min" ds.ds_min;
-		json_line_float ~last:true "max" ds.ds_max;
-		add_string "}"
-	end
-
-(** Return a string containing the JSON metadata of a list of (ds * ds_owner)
-  * tuples [dss]. The string will not contain the values of the datasources. *)
-let json_metadata_of_dss (dss : (Ds.ds * Rrd.ds_owner) list) =
-	if List.length dss = 0 then
-		info "No data sources exported";
-	let buf = Buffer.create 100 in
-	Buffer.add_string buf "{\"datasources\":{";
-	let rec add_dss = function
-		| [] -> ()
-		| (ds, owner) :: [] -> json_metadata_of_ds ~owner ds buf
-		| (ds, owner) :: rest ->
-			json_metadata_of_ds ~owner ds buf;
-			Buffer.add_char buf ',';
-			add_dss rest
-	in
-	add_dss dss;
-	Buffer.add_string buf "}}";
-	Buffer.contents buf
-
 let wait_until_next_reading ?(neg_shift=0.5) () =
 	let next_reading = RRDD.Plugin.register N.name Rrd.Five_Seconds in
 	let wait_time = next_reading -. neg_shift in
@@ -218,20 +121,24 @@ let initialise () =
 let main_loop ~neg_shift ~dss_f =
 	let rec main () =
 		try
-			let hdrstring = RRDD.Plugin.get_header ()
-			and path      = RRDD.Plugin.get_path ~uid:N.name in
+			let path = RRDD.Plugin.get_path ~uid:N.name in
 			let _ = mkdir_safe (Filename.dirname path) 0o644 in
-			let oc = open_out path in
+			let _, writer =
+				Rrd_writer.FileWriter.create path Rrd_protocol_v1.protocol
+			in
 			finally (fun () ->
-				info "Obtained hdr=%s, path=%s\n" hdrstring path;
+				info "Obtained path=%s\n" path;
 				while true do
 					wait_until_next_reading ~neg_shift ();
-					output_string oc (json_of_dss ~hdr:hdrstring (now ()) (dss_f ()));
-					flush oc; seek_out oc 0;
+					let payload = Rrd_protocol.({
+						timestamp = now ();
+						datasources = dss_f ();
+					}) in
+					writer.Rrd_writer.write_payload payload;
 					debug "Done outputting to %s" path;
 					Thread.delay 0.003
 				done)
-				(fun () -> close_out oc)
+				(fun () -> writer.Rrd_writer.cleanup ())
 		with 
 			| Unix.Unix_error (Unix.ENOENT, _, _) ->
 				warn "The %s seems not installed. You probably need to upgrade your version of XenServer.\n" 
