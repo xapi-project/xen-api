7f3e365c6f1558784a585148978a6d7dcf0d3393
Rob Hoes <rob.hoes@citrix.com>
2013-12-11 12:10:03 +0000

    Revert "xenlight: replace Xenctrl.domain_setmaxmem with xenlight equivalent"
    This reverts commit 4558b23c91a55ffa4d2325ffb8d51709b8fe5a63.
    
    We need to first upstream this libxl binding.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- xl/xenops_server_xenlight.ml
+++ xl/xenops_server_xenlight.ml
@@ -481,11 +481,12 @@ module Mem = struct
 						(* This happens when someone manually runs 'service squeezed stop' *)
 						Mutex.execute cached_session_id_m (fun () -> cached_session_id := None);
 						error "Ballooning daemon has disappeared. Manually setting domain maxmem for domid = %d to %Ld KiB" domid amount;
-						with_ctx (fun ctx -> Xenlight.Domain.setmaxmem ctx domid (amount |> Int64.to_int |> Int32.of_int))
+						(* TODO: replace domain_setmaxmem with libxl_domain_setmaxmem (not yet written!) *)
+						Xenctrl.with_intf (fun xc -> Xenctrl.domain_setmaxmem xc domid amount);
 				end
  			| None ->
 				info "No ballooning daemon. Manually setting domain maxmem for domid = %d to %Ld KiB" domid amount;
-				with_ctx (fun ctx -> Xenlight.Domain.setmaxmem ctx domid (amount |> Int64.to_int |> Int32.of_int))
+				Xenctrl.with_intf (fun xc -> Xenctrl.domain_setmaxmem xc domid amount)
 
 	let transfer_reservation_to_domain dbg domid r =
 		let (_: unit option) = wrap (fun () -> transfer_reservation_to_domain_exn dbg domid r) in
