b1209bb3446a21a38eae0760ccca2b20f652ca9b
John Else <john.else@citrix.com>
2013-12-15 08:49:13 +0000

    Add copywrite notices
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/rrd_rpc.ml
+++ lib/rrd_rpc.ml
@@ -1,3 +1,17 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
 let rec split ?limit:(limit=(-1)) c s =
 	let i = try String.index s c with Not_found -> -1 in
 	let nlimit = if limit = -1 || limit = 0 then limit else limit - 1 in
