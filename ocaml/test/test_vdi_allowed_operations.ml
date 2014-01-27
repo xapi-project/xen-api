096d12b09fe9fd646392ef5f05ace1a6da914d60
John Else <john.else@citrix.com>
2014-01-27 17:18:02 +0000

    Add tests for clone/snapshot of VDIs during copying
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/test/test_vdi_allowed_operations.ml
+++ ocaml/test/test_vdi_allowed_operations.ml
@@ -118,10 +118,32 @@ let test_ca125187 () =
 				~value:["mytask", `copy])
 		`copy None
 
+let test_ca126097 () =
+	let __context = Mock.make_context_with_new_db "Mock context" in
+
+	(* Attempting to clone a VDI being copied should fail with VDI_IN_USE. *)
+	run_assert_equal_with_vdi ~__context
+		(fun vdi_ref ->
+			let (_: API.ref_VBD) = make_vbd ~__context ~vDI:vdi_ref ~currently_attached:true ~mode:`RO () in
+			Db.VDI.set_current_operations ~__context
+				~self:vdi_ref
+				~value:["mytask", `copy])
+		`clone (Some (Api_errors.vdi_in_use, []));
+
+	(* Attempting to snapshot a VDI being copied should be allowed. *)
+	run_assert_equal_with_vdi ~__context
+		(fun vdi_ref ->
+			let (_: API.ref_VBD) = make_vbd ~__context ~vDI:vdi_ref ~currently_attached:true ~mode:`RO () in
+			Db.VDI.set_current_operations ~__context
+				~self:vdi_ref
+				~value:["mytask", `copy])
+		`snapshot None
+
 let test =
 	"test_vdi_allowed_operations" >:::
 		[
 			"test_ca98944" >:: test_ca98944;
 			"test_ca101669" >:: test_ca101669;
 			"test_ca125187" >:: test_ca125187;
+			"test_ca126097" >:: test_ca126097;
 		]
