84264fa1c9c2fba50c2a38341ed4005f800e821e
David Scott <dave.scott@eu.citrix.com>
2014-02-03 14:22:57 +0000

    Starting a VM should involve attaching storage
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- dbus/vm/ocaml/build.sh
+++ dbus/vm/ocaml/build.sh
@@ -5,14 +5,17 @@ set -x
 #obus-gen-server ../vm.xml
 
 obus-gen-interface -o vm ../vm.xml 
-
+obus-gen-interface -o resource ../resource.xml
 
 PACKS=obus,lwt.syntax
 
 ocamlfind ocamlc -syntax camlp4o -package $PACKS -c vm.mli
 ocamlfind ocamlc -syntax camlp4o -package $PACKS -c vm.ml
 
+ocamlfind ocamlc -syntax camlp4o -package $PACKS -c resource.mli
+ocamlfind ocamlc -syntax camlp4o -package $PACKS -c resource.ml
+
 ocamlfind ocamlc -syntax camlp4o -package $PACKS -c simulator.ml
 
-ocamlfind ocamlc -syntax camlp4o -package $PACKS -o simulator -linkpkg vm.cmo simulator.cmo
+ocamlfind ocamlc -syntax camlp4o -package $PACKS -o simulator -linkpkg vm.cmo resource.cmo simulator.cmo
 
--- dbus/vm/ocaml/simulator.ml
+++ dbus/vm/ocaml/simulator.ml
@@ -3,6 +3,11 @@ open Lwt_io
 
 let vm_start config =
   lwt () = printlf "vm_start %s" config in
+  (* Create a proxy for the remote object *)
+  lwt bus = OBus_bus.session () in
+  let proxy = OBus_proxy.make (OBus_peer.make bus "org.xenserver.vm") ["volume"] in
+  lwt (local_uri, id) = OBus_method.call Resource.Org_xenserver_api_resource.m_attach proxy "iscsi://target/lun" in
+  lwt () = printlf "  got local_uri %s id %s" local_uri id in
   return ()
 
 let vm_stop id =
@@ -16,6 +21,19 @@ let vm_interface =
    m_stop  = (fun obj id     -> vm_stop  id);
  })
 
+let volume_attach global_uri =
+  lwt () = printlf "volume_attach %s" global_uri in
+  return ("file://block/device", "some id")
+
+let volume_detach id = printlf "volume_detach %s" id 
+
+let volume_interface =
+ Resource.Org_xenserver_api_resource.(make {
+   m_attach = (fun obj global_uri -> volume_attach global_uri);
+   m_detach = (fun obj id         -> volume_detach id);
+ })
+
+
 lwt () =
   lwt bus = OBus_bus.session () in
 
@@ -27,6 +45,11 @@ lwt () =
   OBus_object.attach obj ();
 
   (* Export the object on the connection *)
+  OBus_object.export bus obj;
+
+  lwt _ = OBus_bus.request_name bus "org.xenserver.volume" in
+  let obj = OBus_object.make ~interfaces:[volume_interface] ["volume"] in
+  OBus_object.attach obj ();
   OBus_object.export bus obj;
 
   (* Wait forever *)
