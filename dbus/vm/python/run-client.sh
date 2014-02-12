fc859fc1c6012bb8f1963def88db0ecfed711d2b
David Scott <dave.scott@eu.citrix.com>
2014-02-12 13:31:21 +0000

    Add simple test scripts: run-{client,server}.sh
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

---
+++ dbus/vm/python/run-client.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./dbus-call.py --debug --uri=org.xenserver.foo:///org/xenserver/foo attach hello
---
+++ dbus/vm/python/run-server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./dbus-resource-script.py --debug -n org.xenserver.foo -s `pwd`/foo
