f23b34ee3ec3c7df358b34e38372c136ad03a6f6
Rob Hoes <rob.hoes@citrix.com>
2014-03-04 11:59:45 +0000

    Update HTML API docs to take into account new lifecycle info
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/doc/branding.js
+++ ocaml/doc/branding.js
@@ -13,46 +13,52 @@
  */
 
 function make_title() {
-	document.write('<title>Xapi Documentation</title>');
+	document.write('<title>Citrix XenServer Management API</title>');
 }
 
 function make_header(t) {
-	if (t == 'apidoc')
-		title = 'Xapi &ndash; XenAPI Documentation';
-	else if (t == 'codedoc')
-		title = 'Xapi &ndash; OCaml Code Documentation';
-	else
-		title = 'Xapi &ndash; Documentation';
-
-	html = '<h1 style="float:left"><a href="index.html" style="text-decoration: none">'
-		+ title
-		+ '</a></h1><ul id="menu"><li><a href="index.html">XenAPI Docs</a></li>'
-		+ '<li><a href="codedoc.html">Code Docs</a></li></ul>';
+	html = '<img src="http://citrix.com/site/resources/v4_includes/css/5.0/citrix_logo.gif" alt="Citrix" width="208px" height="36px" border="0" style="float:left">'
+		+ '<h1 style="float:left; font-size: 24px;"><a href="index.html">XenServer Management API</a></h1>'
 	document.getElementById('header').innerHTML = html;
 }
 
-first_release = 'midnight-ride';
+first_release = 'rio';
 
 function get_release_name(s)
 {
 	switch (s) {
 	case 'rio':
+		return 'XenServer 4.0';
+		break;
 	case 'miami':
+		return 'XenServer 4.1';
+		break;
 	case 'symc':
+		return 'XenServer 4.1.1';
+		break;
 	case 'orlando':
+		return 'XenServer 5.0';
+		break;
 	case 'orlando-update-1':
+		return 'XenServer 5.0 Update 1';
+		break;
 	case 'george':
+		return 'XenServer 5.5';
+		break;
 	case 'midnight-ride':
-		return 'XCP 0.5';
+		return 'XenServer 5.6';
 		break;
 	case 'cowley':
-		return 'XCP 1.0';
+		return 'XenServer 5.6 FP1';
 		break;
 	case 'boston':
-		return 'XCP 1.5';
+		return 'XenServer 6.0';
 		break;
 	case 'tampa':
-		return 'XCP 1.6;
+		return 'XenServer 6.1';
+		break;
+	case 'clearwater':
+		return 'XenServer 6.2';
 		break;
 	default:
 		return 'Unreleased';
