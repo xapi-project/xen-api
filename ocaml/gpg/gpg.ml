f93915e6e1396d4a9faf784549ccc0022859e748
John Else <john.else@citrix.com>
2014-04-23 16:42:53 +0100

    Whitespace: ocaml/gpg/gpg.ml
    $ camlp4o -printer o -no_comments ocaml/gpg/gpg.ml | md5sum
    83bf837cbe696c1c76f53893763c1617  -
    $ git checkout HEAD^
    $ camlp4o -printer o -no_comments ocaml/gpg/gpg.ml | md5sum
    83bf837cbe696c1c76f53893763c1617  -
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/gpg/gpg.ml
+++ ocaml/gpg/gpg.ml
@@ -25,13 +25,14 @@ let filename = ref ""
 let gpg_binary_path = "/usr/bin/gpg"
 let gpg_homedir = Filename.concat Fhs.optdir "gpg/"
 let gpg_pub_keyring = gpg_homedir ^ "pubring.gpg"
-let allowed_gpg_checksum =
-	[ "be00ee82bffad791edfba477508d5d84"; (* centos52 version *)
-	  "a267af68c53f5d998b982235bbccb01e"; (* centos53/54 version *)
-	  "da75ecb57ff12b2573f44466d36f395e"; (* centos64 version *) 
-	  "f52886b87126c06d419f408e32268b4e"; (* 64 bit product version *)
-	  "aa27ac0b0ebfd1278bf2386c343053db"; (* debian developer version *)
-	  "044d1327ea42400ac590195e0ec1e7e6"; ]
+let allowed_gpg_checksum = [
+	"be00ee82bffad791edfba477508d5d84"; (* centos52 version *)
+	"a267af68c53f5d998b982235bbccb01e"; (* centos53/54 version *)
+	"da75ecb57ff12b2573f44466d36f395e"; (* centos64 version *)
+	"f52886b87126c06d419f408e32268b4e"; (* 64 bit product version *)
+	"aa27ac0b0ebfd1278bf2386c343053db"; (* debian developer version *)
+	"044d1327ea42400ac590195e0ec1e7e6";
+]
 
 exception InvalidSignature
 
