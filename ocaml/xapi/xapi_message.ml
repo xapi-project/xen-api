abb5327de04d01f461769e1c896e8b8805dd0671
David Scott <dave.scott@eu.citrix.com>
2014-04-29 20:57:43 +0100

    event.from: prevent interference between parallel calls
    Since the new event.from API implementation was based on top
    of the existing event.next implementation it was possible
    for one thread to interfere with another because each event.from
    *shared* the 'subscription' record. In particular CloudStack
    suffered from the following interleaving:
    
    thread 1: event.from ["vm/"]
    thread 2: event.from ["task/"]
     <- event on vm
    thread 1: return successfully
    thread 2: fails with SESSION_INVALID because the subscription
      is marked as invalid in the on_session_deleted cleanup function
    
    In this patch we cleanly separate the event.next implementation
    (in the submodule Next) from the event.from implementation
    (in the submodule From). It's now clear that all calls to
    event.from use *separate* state, sharing only the session_id
    which is used to allow session.logout to be used to unblock
    blocked threads.
    
    This patch strengthens the event.from postcondition: the only time
    when an empty event set is returned is when the timeout has fired.
    Previously empty sets could be triggered by the race:
    
    1.     object is modified
    2. event.from sees the modification
    3.     object is destroyed
    4. event.from cannot find the snapshot, omits the modify event
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- ocaml/xapi/xapi_message.ml
+++ ocaml/xapi/xapi_message.ml
@@ -539,7 +539,7 @@ let get_since_for_events ~__context since =
 			 | (last_in_memory, _, _) :: _ when last_in_memory < since ->
 				   Some (List.filter_map
 							 (fun (gen,_ref,msg) ->
-								  if gen > since then Some (gen, Xapi_event.MCreate (_ref, msg)) else None)
+								  if gen > since then Some (gen, Xapi_event.Message.Create (_ref, msg)) else None)
 							 !in_memory_cache)
 			 | (last_in_memory, _, _) :: _ ->
 				 debug "get_since_for_events: last_in_memory (%Ld) > since (%Ld): Using slow message lookup" last_in_memory since;
@@ -551,11 +551,11 @@ let get_since_for_events ~__context since =
 	let result = match cached_result with
 		| Some x -> x
 		| None ->
-			List.map (fun (ts,x,y) -> (ts, Xapi_event.MCreate (x,y))) (get_from_generation since)
+			List.map (fun (ts,x,y) -> (ts, Xapi_event.Message.Create (x,y))) (get_from_generation since)
 	in
 	let delete_results = Mutex.execute deleted_mutex (fun () ->
 		let deleted = List.filter (fun (deltime,_ref) -> deltime > since) !deleted in
-		List.map (fun (ts , _ref) -> (ts,Xapi_event.MDel _ref)) deleted) in
+		List.map (fun (ts , _ref) -> (ts,Xapi_event.Message.Del _ref)) deleted) in
 	let all_results = result @ delete_results in
 	let newsince = List.fold_left (fun acc (ts,m) -> max ts acc) since all_results in
 	(newsince, List.map snd all_results)
@@ -605,7 +605,7 @@ let repopulate_cache () =
 
 let register_event_hook () =
 	repopulate_cache ();
-	Xapi_event.message_get_since_for_events := get_since_for_events
+	Xapi_event.Message.get_since_for_events := get_since_for_events
 
 (* Query params: cls=VM etc, obj_uuid=<..>, min_priority. Returns the last
    days worth of messages as an RSS feed. *)
