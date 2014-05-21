a926dc8f31be7ffdd9f62ddfdfbcf01a30222049
Jon Ludlam <jonathan.ludlam@citrix.com>
2014-05-21 23:59:48 +0100

    Remove Re_str
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- src/memory_server.ml
+++ src/memory_server.ml
@@ -159,11 +159,10 @@ let get_host_initial_free_memory _ dbg = 0L (* XXX *)
    the packages and are now setting them up) *)
 let get_total_memory () =
 	let ic = open_in "/proc/meminfo" in
-	let r = Re_str.regexp "[ \t\n]+" in
 	finally
 		(fun () ->
 			let rec loop () =
-				match Re_str.split_delim r (input_line ic) with
+				match Xstringext.String.split_f Xstringext.String.isspace (input_line ic) with
 				| [ "MemTotal:"; total; "kB" ] ->
 					Int64.(mul (of_string total) 1024L)
 				| _ -> loop () in
