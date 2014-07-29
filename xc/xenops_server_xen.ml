762ef45bf3eb5082aced846d5ca07f533f089f3b
Si Beaumont <simon.beaumont@citrix.com>
2014-07-29 11:16:43 +0100

    Don't log all Domain build errors as Bootloader errors
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- xc/xenops_server_xen.ml
+++ xc/xenops_server_xen.ml
@@ -1078,7 +1078,7 @@ module VM = struct
 				debug "%s" m;
 				raise (Bootloader_error (vm.Vm.id, x))
 			| e ->
-				let m = Printf.sprintf "VM = %s; domid = %d; Bootloader error: %s" vm.Vm.id domid (Printexc.to_string e) in
+				let m = Printf.sprintf "VM = %s; domid = %d; Error: %s" vm.Vm.id domid (Printexc.to_string e) in
 				debug "%s" m;
 				raise e
 
