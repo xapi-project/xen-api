43f4df026ae80cf00209cf2a7f092d465bb27520
David Scott <dave.scott@citrix.com>
2014-08-09 14:45:15 +0100

    Add a 'root' directory, which will contain the adapter directories
    Each directory will ultimately have a set of scripts inside. The
    queue name will be taken from the directory name.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- main.ml
+++ main.ml
@@ -11,13 +11,38 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
+module U = Unix
 open Core.Std
 open Async.Std
 
 open Xcp_service
 
-let version = "0.1"
+let description = String.concat ~sep:" " [
+  "Allow xapi storage adapters to be written as individual scripts.";
+  "To add a storage adapter, create a sub-directory in the --root directory";
+  "with the name of the adapter (e.g. org.xen.xcp.storage.mylvm) and place";
+  "the scripts inside.";
+]
+
+let root_dir = ref "/var/lib/xapi/storage-scripts"
+
+let resources = [
+  { Xcp_service.name = "root";
+    description = "directory whose sub-directories contain sets of per-operation scripts, one sub-directory per queue name";
+    essential = true;
+    path = root_dir;
+    perms = [ U.X_OK ];
+  }
+]
 
 let _ =
-  let () = configure2 ~name:"xapi-script-storage" ~version ~doc:"This allows storage adapters to be implemented as scripts" () in
-  ()
+  match configure2
+    ~name:"xapi-script-storage"
+    ~version:Version.version
+    ~doc:description
+    ~resources
+    () with
+  | `Ok () -> ()
+  | `Error x ->
+    Printf.fprintf stderr "Error: %s\n%!" x;
+    Pervasives.exit 1
