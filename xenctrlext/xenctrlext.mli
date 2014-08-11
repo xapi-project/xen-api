34116e1679c163db0ce16d6e6e998840bc712c1c
Si Beaumont <simon.beaumont@citrix.com>
2014-08-11 14:57:34 +0100

    CA-141364: Remove redundant (and erroneous bindings)
    We no longer need these bindings since this work is done in Xenguest. Also, it
    appears both of the bindings mistakenly make the same call into libxc.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- xenctrlext/xenctrlext.mli
+++ xenctrlext/xenctrlext.mli
@@ -17,8 +17,6 @@ open Xenctrl
 external get_boot_cpufeatures: handle ->  (int32 * int32 * int32 * int32 * int32 * int32 * int32 * int32) = "stub_xenctrlext_get_boot_cpufeatures" 
 
 external domain_set_timer_mode: handle -> domid -> int -> unit = "stub_xenctrlext_domain_set_timer_mode"
-external domain_set_hpet: handle -> domid -> int -> unit = "stub_xenctrlext_domain_set_hpet"
-external domain_set_vpt_align: handle -> domid -> int -> unit = "stub_xenctrlext_domain_set_vpt_align"
 
 external domain_send_s3resume: handle -> domid -> unit = "stub_xenctrlext_domain_send_s3resume"
 external domain_get_acpi_s_state: handle -> domid -> int = "stub_xenctrlext_domain_get_acpi_s_state"
