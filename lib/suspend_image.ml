32480cd98bf355817c1e1ff36b541f59c1dd9a37
Si Beaumont <simon.beaumont@citrix.com>
2014-08-18 14:39:38 +0100

    CA-142107: Log arguments before spawning convert-legacy-stream
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- lib/suspend_image.ml
+++ lib/suspend_image.ml
@@ -187,6 +187,8 @@ let with_conversion_script task name hvm fd f =
 	in
 	let (conv_th, conv_st) =
 		spawn_thread_and_close_fd "convert-legacy-stream" pipe_w (fun () ->
+			debug "Executing %s with args [ %s ]"
+				conv_script (String.concat "; " args);
 			Cancellable_subprocess.run task
 				[ fd_uuid, fd; pipe_w_uuid, pipe_w; ] conv_script args
 		)
