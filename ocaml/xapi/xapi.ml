41c38773f81c6ffd443f8854f13be92c94980d84
Si Beaumont <simon.beaumont@citrix.com>
2014-09-01 12:55:27 +0100

    CP-9401: Remove unused OEM patch stream handler
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/xapi.ml
+++ ocaml/xapi/xapi.ml
@@ -645,7 +645,6 @@ let common_http_handlers = [
   ("get_host_logs_download", (Http_svr.FdIO Xapi_logs_download.logs_download_handler));
   ("put_pool_patch_upload", (Http_svr.FdIO Xapi_pool_patch.pool_patch_upload_handler));
   ("get_pool_patch_download", (Http_svr.FdIO Xapi_pool_patch.pool_patch_download_handler));
-  ("put_oem_patch_stream", (Http_svr.FdIO Xapi_pool_patch.oem_patch_stream_handler));
   ("get_vncsnapshot", (Http_svr.FdIO Xapi_vncsnapshot.vncsnapshot_handler));
   ("get_pool_xml_db_sync", (Http_svr.FdIO Pool_db_backup.pull_database_backup_handler));
   ("put_pool_xml_db_sync", (Http_svr.FdIO Pool_db_backup.push_database_restore_handler));
