8746aac9f61b2d9ce46013ef4d1b1d05a2d864b3
John Else <john.else@citrix.com>
2014-09-03 14:53:01 +0100

    CA-143836: Add VDI.revert to SMAPI
    Signed-off-by: John Else <john.else@citrix.com>
    (cherry picked from commit d6ff4a21a3589ca7450366d42dfecd6ede46982d)
    
    Conflicts:
    	ocaml/xapi/storage_interface.ml
    	ocaml/xapi/storage_proxy.ml

--- ocaml/xapi/storage_access.ml
+++ ocaml/xapi/storage_access.ml
@@ -538,6 +538,23 @@ module SMAPIv1 = struct
 					raise (Vdi_does_not_exist vdi)
 				| Sm.MasterOnly -> redirect sr
 
+		let revert context ~dbg ~sr ~snapshot_info =
+			Server_helpers.exec_with_new_task "VDI.revert" ~subtask_of:(Ref.of_string dbg)
+				(fun __context ->
+					(* Clone the snapshot. *)
+					let new_vdi_info = clone context ~dbg ~sr ~vdi_info:snapshot_info in
+					(* Update snapshot links. *)
+					let vdi_ref = Db.VDI.get_by_uuid ~__context ~uuid:snapshot_info.snapshot_of in
+					let new_vdi_ref = Db.VDI.get_by_uuid ~__context ~uuid:new_vdi_info.vdi in
+					let snapshot_refs = Db.VDI.get_snapshots ~__context ~self:vdi_ref in
+					List.iter
+						(fun snapshot_ref -> Db.VDI.set_snapshot_of ~__context ~self:snapshot_ref ~value:new_vdi_ref)
+						snapshot_refs;
+					(* Destroy the original VDI. *)
+					destroy context ~dbg ~sr ~vdi:snapshot_info.snapshot_of;
+					(* Return the new VDI. *)
+					new_vdi_info)
+
 		let stat context ~dbg ~sr ~vdi =
 			try
 				Server_helpers.exec_with_new_task "VDI.stat" ~subtask_of:(Ref.of_string dbg)
