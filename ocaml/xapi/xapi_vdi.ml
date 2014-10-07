113f326c9c525e7c425a058be43c3be015c96f30
Ravi Pandey <ravi.pandey@citrix.com>
2014-10-07 09:02:39 +0100

    CA-145540: Restricting VDI snapshot during VDI copy
    Signed-off-by: Ravi Pandey <ravi.pandey@citrix.com>

--- ocaml/xapi/xapi_vdi.ml
+++ ocaml/xapi/xapi_vdi.ml
@@ -176,6 +176,10 @@ let check_operation_error ~__context ?(sr_records=[]) ?(pbd_records=[]) ?(vbd_re
 					Some (Api_errors.vdi_is_sharable, [ _ref ])
 				| `snapshot when reset_on_boot ->
 					Some (Api_errors.vdi_on_boot_mode_incompatible_with_operation, [])
+				| `snapshot ->
+				  if List.exists (fun (_, op) -> op = `copy) current_ops
+					then Some (Api_errors.operation_not_allowed, ["Snapshot operation not allowed during copy."])
+				  else None
 				| `copy ->
 					if List.mem record.Db_actions.vDI_type [ `ha_statefile; `redo_log ]
 					then Some (Api_errors.operation_not_allowed, ["VDI containing HA statefile or redo log cannot be copied (check the VDI's allowed operations)."])
