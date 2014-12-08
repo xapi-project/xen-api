5cf3be8ffa3a4e4ffad3e23b9767aaa2acc67b36
Jon Ludlam <jonathan.ludlam@citrix.com>
2014-12-08 13:56:10 +0000

    Allow ISO VBDs to be made from any SR
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- ocaml/xapi/xapi_vdi_helpers.ml
+++ ocaml/xapi/xapi_vdi_helpers.ml
@@ -23,14 +23,6 @@ open Threadext
 module D=Debug.Make(struct let name="xapi" end)
 open D
 
-(* We only support .iso files (from an iso SR) and block devices from
-   a local magic SR (eg /dev/hda) but NOT phantom_vbd block attach isos *)
-let assert_vdi_is_valid_iso ~__context ~vdi = 
-	let sr = Db.VDI.get_SR ~__context ~self:vdi in
-	let ct = Db.SR.get_content_type ~__context ~self:sr in
-	if ct <> "iso"
-	then raise (Api_errors.Server_error(Api_errors.vdi_is_not_iso, [ Ref.string_of vdi; ct ]))
-
 (* CA-26514: Block operations on 'unmanaged' VDIs *)
 let assert_managed ~__context ~vdi = 
   if not (Db.VDI.get_managed ~__context ~self:vdi)
