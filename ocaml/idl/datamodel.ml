4a26c14f3fb1965f8dfb4316530bd4fb5bc0f140
Euan Harris <euan.harris@citrix.com>
2015-02-10 15:24:09 +0000

    CP-11034: Add PGPU.is_system_display_device field
    Signed-off-by: Euan Harris <euan.harris@citrix.com>
    (cherry picked from commit 4287318c73ccf5bcac5ff7307fdda6af4fc706b7)

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -7919,6 +7919,7 @@ let pgpu =
 			field ~qualifier:StaticRO ~ty:Int ~lifecycle:[Published, rel_vgpu_tech_preview, ""] ~internal_only:true ~default_value:(Some (VInt Constants.pgpu_default_size)) "size" "Abstract size of this PGPU";
 			field ~qualifier:DynamicRO ~ty:(Map (Ref _vgpu_type, Int)) ~lifecycle:[Published, rel_vgpu_productisation, ""] ~default_value:(Some (VMap [])) "supported_VGPU_max_capacities" "A map relating each VGPU type supported on this GPU to the maximum number of VGPUs of that type which can run simultaneously on this GPU";
 			field ~qualifier:DynamicRO ~ty:(pgpu_dom0_access) ~lifecycle:[Published, rel_cream, ""] ~default_value:(Some (VEnum "enabled")) "dom0_access" "The accessibility of this device from dom0";
+			field ~qualifier:DynamicRO ~ty:Bool ~lifecycle:[Published, rel_cream, ""] ~default_value:(Some (VBool false)) "is_system_display_device" "Is this device the system display device";
 			]
 		()
 
--- ocaml/xapi/xapi_pgpu.ml
+++ ocaml/xapi/xapi_pgpu.ml
@@ -29,7 +29,8 @@ let calculate_max_capacities ~__context ~pCI ~size ~supported_VGPU_types =
 		supported_VGPU_types
 
 let create ~__context ~pCI ~gPU_group ~host ~other_config
-		~supported_VGPU_types ~size ~dom0_access =
+		~supported_VGPU_types ~size ~dom0_access 
+		~is_system_display_device =
 	let pgpu = Ref.make () in
 	let uuid = Uuid.to_string (Uuid.make_uuid ()) in
 	let supported_VGPU_max_capacities =
@@ -37,7 +38,8 @@ let create ~__context ~pCI ~gPU_group ~host ~other_config
 	in
 	Db.PGPU.create ~__context ~ref:pgpu ~uuid ~pCI
 		~gPU_group ~host ~other_config ~size
-		~supported_VGPU_max_capacities ~dom0_access;
+		~supported_VGPU_max_capacities ~dom0_access
+		~is_system_display_device;
 	Db.PGPU.set_supported_VGPU_types ~__context
 		~self:pgpu ~value:supported_VGPU_types;
 	Db.PGPU.set_enabled_VGPU_types ~__context
@@ -111,10 +113,13 @@ let update_gpus ~__context ~host =
 					(rf, rc)
 				with Not_found ->
 					let dom0_access = determine_dom0_access pci in
+				        let pci_addr =  Db.PCI.get_pci_id ~__context ~self:pci in
+				        let is_system_display_device = (system_display_device = (Some pci_addr)) in
 					let self = create ~__context ~pCI:pci
 							~gPU_group:(Ref.null) ~host ~other_config:[]
 							~supported_VGPU_types
 							~size:Constants.pgpu_default_size ~dom0_access
+							~is_system_display_device
 					in
 					let group = Xapi_gpu_group.find_or_create ~__context self in
 					Helpers.call_api_functions ~__context (fun rpc session_id ->
