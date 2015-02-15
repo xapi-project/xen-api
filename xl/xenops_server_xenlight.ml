92e61c3177bca6449fa30f43b859c93b74095ad5
Hui Zhang <hui.zhang@citrix.com>
2015-02-15 04:36:26 +0000

    CP-10531: Add device in guest_agent.
    To compute if network and storage paths are optimized,
    need to check whether the state of vif/vbd devices equal to 4.
    Add device node in guest_agent so xapi could retrieve the state.
    
    Signed-off-by: Hui Zhang <hui.zhang@citrix.com>

--- xl/xenops_server_xenlight.ml
+++ xl/xenops_server_xenlight.ml
@@ -2509,7 +2509,7 @@ module VM = struct
 							let subdirs = try List.map (fun x -> dir ^ "/" ^ x) (xs.Xs.directory (root ^ "/" ^ dir)) with _ -> [] in
 							this @ (List.concat (List.map (ls_lR root) subdirs)) in
 						let guest_agent =
-							[ "drivers"; "attr"; "data"; "control" ] |> List.map (ls_lR (Printf.sprintf "/local/domain/%d" di.domid)) |> List.concat in
+							[ "drivers"; "attr"; "data"; "control"; "device" ] |> List.map (ls_lR (Printf.sprintf "/local/domain/%d" di.domid)) |> List.concat in
 						let xsdata_state =
 							Domain.allowed_xsdata_prefixes |> List.map (ls_lR (Printf.sprintf "/local/domain/%d" di.domid)) |> List.concat in
 						let shadow_multiplier_target =
