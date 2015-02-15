96619c37e4e1855d70ef6f8f5531dff1f9436fd4
Hui Zhang <hui.zhang@citrix.com>
2015-02-15 04:54:02 +0000

    CP-10531: Update vm_guest_metrics to implement PV drivers update
    1. Introduce network_paths_optimized and storage_paths_optimized flags to vm_guest_metrics.
       They are used to represent if VM is using PV network and PV storage frontends.
       XenCenter could display them respectively.
    2. Update the computation of PV_drivers_up_to_date.
       The value is logical AND of network_paths_optimized and storage_paths_optimized.
       Also deprecate this flag since the network and storage flags are introduced.
    
    Signed-off-by: Hui Zhang <hui.zhang@citrix.com>

--- ocaml/xapi/import.ml
+++ ocaml/xapi/import.ml
@@ -534,6 +534,8 @@ module GuestMetrics : HandlerTools = struct
 			~memory:gm_record.API.vM_guest_metrics_memory
 			~disks:gm_record.API.vM_guest_metrics_disks
 			~networks:gm_record.API.vM_guest_metrics_networks
+			~network_paths_optimized:gm_record.API.vM_guest_metrics_network_paths_optimized
+			~storage_paths_optimized:gm_record.API.vM_guest_metrics_storage_paths_optimized
 			~other:gm_record.API.vM_guest_metrics_other
 			~last_updated:gm_record.API.vM_guest_metrics_last_updated
 			~other_config:gm_record.API.vM_guest_metrics_other_config
