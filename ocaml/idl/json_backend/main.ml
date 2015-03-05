a5b3efbe982e1eaf6037e52b68639d36cd62490f
Rob Hoes <rob.hoes@citrix.com>
2015-03-05 16:55:38 +0000

    JSON backend: generate release info for XenAPI docs
    This reuses some code from ocaml/doc/jsapi.ml.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/idl/json_backend/main.ml
+++ ocaml/idl/json_backend/main.ml
@@ -186,10 +186,8 @@ let jarray_of_enums enums =
 		]
 	) enums)
 
-let _ =
-	let api = Datamodel.all_api in
-	let objs = objects_of_api api in
-	let json = JArray (List.map (fun obj ->
+let json_of_objs objs =
+	JArray (List.map (fun obj ->
 		let fields, enums1 = fields_of_obj_with_enums obj in
 		let messages, enums2 = messages_of_obj_with_enums obj in
 		let enums = Listext.List.setify (enums1 @ enums2) in
@@ -202,6 +200,90 @@ let _ =
 			"lifecycle", jarray_of_lifecycle obj.obj_lifecycle;
 			"tag", JString (match obj.obj_doc_tags with [] -> "" | t :: _ -> string_of_doc_tag t);
 		]
-	) objs) in
-	print_endline (string_of_json 0 json)
+	) objs)
+
+let jobject_of_change (t, n, l, s) =
+	JObject [
+		"transition", JString (string_of_lifecycle_transition t ^ " " ^ s);
+		"name", JString n;
+		"log", JString l;
+	]
+
+let compare_changes (a_t, a_n, _, a_k) (b_t, b_n, _, b_k) =
+	let int_of_transition = function
+		| Published -> 0
+		| Extended -> 10
+		| Changed -> 20
+		| Deprecated -> 30
+		| Removed -> 40
+		| Prototyped -> 50
+	in
+	let int_of_kind = function
+		| "class" -> 0
+		| "field" -> 1
+		| "message" -> 2
+		| _ -> 3
+	in
+	let cmp = compare ((int_of_transition a_t) + (int_of_kind a_k)) ((int_of_transition b_t) + (int_of_kind b_k)) in
+	if cmp = 0 then
+		compare a_n b_n
+	else
+		cmp
+
+let releases objs =
+	let changes_in_release rel =
+		let search_obj obj =
+			let changes = List.filter (fun (transition, release, doc) -> release = rel) obj.obj_lifecycle in
+			let obj_changes = 
+				List.map (fun (transition, release, doc) ->
+					transition,
+					obj.name,
+					(if doc = "" && transition = Published then obj.description else doc),
+					"class"
+				) changes in
+			
+			let changes_for_msg m =
+				let changes = List.filter (fun (transition, release, doc) -> release = rel) m.msg_lifecycle in
+				List.map (fun (transition, release, doc) ->
+					transition,
+					obj.name ^ "." ^ m.msg_name,
+					(if doc = "" && transition = Published then m.msg_doc else doc),
+					"message"
+				) changes
+			in
+			let msgs = List.filter (fun m -> not m.msg_hide_from_docs) obj.messages in
+			let msg_changes = List.fold_left (fun l m -> l @ (changes_for_msg m)) [] msgs in
+			
+			let changes_for_field f =
+				let changes = List.filter (fun (transition, release, doc) -> release = rel) f.lifecycle in
+				let field_name = String.concat "_" f.full_name in
+				List.map (fun (transition, release, doc) ->
+					transition,
+					obj.name ^ "." ^ field_name,
+					(if doc = "" && transition = Published then f.field_description else doc),
+					"field"
+				) changes
+			in
+			let rec flatten_contents contents =
+				List.fold_left (fun l -> function
+					| Field f -> f :: l
+					| Namespace (name, contents) -> flatten_contents contents @ l
+				) [] contents
+			in
+			let fields = flatten_contents obj.contents in
+			let fields = List.filter (fun f -> not f.internal_only) fields in
+			let field_changes = List.fold_left (fun l f -> l @ (changes_for_field f)) [] fields in
+
+			obj_changes @ field_changes @ msg_changes
+		in
+		JArray (List.map search_obj objs |> List.flatten |> List.sort compare_changes |> List.map jobject_of_change)
+	in
+	let release_info = JObject (List.map (fun rel -> rel, changes_in_release rel) release_order) in
+	Unixext.write_string_to_file ("release_info.json") (string_of_json 0 release_info)
+
+let _ =
+	let api = Datamodel.all_api in
+	let objs = objects_of_api api in
+	Unixext.write_string_to_file "xenapi.json" (objs |> json_of_objs |> string_of_json 0);
+	releases objs
 
