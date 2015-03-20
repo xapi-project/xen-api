13521c8e96f2e02c7d76c736dd538ef844953667
David Scott <dave.scott@eu.citrix.com>
2015-03-20 20:07:18 +0000

    Unhook coveralls
    The test script stops processes with signals which causes problems
    with the bisect output.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- .coveralls.sh
+++ .coveralls.sh
@@ -4,4 +4,6 @@ eval `opam config env`
 opam install ocveralls -y
 make
 BISECT_FILE=_build/coverage ./basic-rpc-test.sh
-`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
+# We need to avoid killing processes, otherwise the output
+# files are truncated.
+#`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
