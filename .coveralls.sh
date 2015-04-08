6924f730fbda172429a48f27cb4116e7b420e067
David Scott <dave.scott@eu.citrix.com>
2015-04-08 20:24:43 +0100

    Unhook ocveralls again
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- .coveralls.sh
+++ .coveralls.sh
@@ -4,4 +4,5 @@ eval `opam config env`
 opam install ocveralls -y
 make
 BISECT_FILE=_build/coverage ./basic-rpc-test.sh
-`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
+# This needs debugging:
+#`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
