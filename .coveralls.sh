682702fcfb21f0e6a2dd33a77deccd770038ca32
David Scott <dave.scott@eu.citrix.com>
2015-04-08 18:05:02 +0100

    travis: attempt to hook coveralls back in
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- .coveralls.sh
+++ .coveralls.sh
@@ -4,6 +4,4 @@ eval `opam config env`
 opam install ocveralls -y
 make
 BISECT_FILE=_build/coverage ./basic-rpc-test.sh
-# We need to avoid killing processes, otherwise the output
-# files are truncated.
-#`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
+`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
