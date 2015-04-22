d440c9755d0303ff5353df2f5f53e5f7b656c2eb
David Scott <dave.scott@citrix.com>
2015-04-22 13:36:45 +0000

    test: the Lwt to Lwt test can now issue requests in parallel
    This stresses the co-ordination logic in the client library a little.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- core_test/client_main.ml
+++ core_test/client_main.ml
@@ -24,6 +24,7 @@ let name = ref "server"
 let payload = ref "hello"
 let timeout = ref None
 let shutdown = "shutdown"
+let nthreads = ref 1
 
 let (>>|=) m f = m >>= function
   | `Ok x -> f x
@@ -37,12 +38,21 @@ let main () =
     Client.rpc c !payload >>|= fun _ ->
     return () in
   let start = Unix.gettimeofday () in
-  lwt () = match !timeout with
+  let rec ints = function
+    | 0 -> []
+    | n -> n :: (ints (n - 1)) in
+  ( match !timeout with
     | None -> one ()
     | Some t ->
-      while_lwt (Unix.gettimeofday () -. start < t) do
-        one ()
-      done in
+      let rec thread n =
+        if (Unix.gettimeofday () -. start < t) then begin
+          one ()
+          >>= fun () ->
+          thread n
+        end else return () in
+      let threads = List.map thread (ints !nthreads) in
+      Lwt.join threads
+  ) >>= fun () ->
   let t = Unix.gettimeofday () -. start in
   Printf.printf "Finished %d RPCs in %.02f\n" !counter t;
   Client.rpc c shutdown >>|= fun _ ->
@@ -54,6 +64,7 @@ let _ =
     "-name", Arg.Set_string name, (Printf.sprintf "name to send message to (default %s)" !name);
     "-payload", Arg.Set_string payload, (Printf.sprintf "payload of message to send (default %s)" !payload);
     "-secs", Arg.String (fun x -> timeout := Some (float_of_string x)), (Printf.sprintf "number of seconds to repeat the same message for (default %s)" (match !timeout with None -> "None" | Some x -> string_of_float x));
+    "-threads", Arg.Set_int nthreads, (Printf.sprintf "number of parallel threads (default %d)" !nthreads);
   ] (fun x -> Printf.fprintf stderr "Ignoring unexpected argument: %s" x)
     "Send a message to a name, optionally waiting for a response";
 
