96f2795e7a8e868851ce58b214bf17cc7474d7f9
David Scott <dave.scott@citrix.com>
2015-05-25 12:31:53 +0000

    On error, close the accepted connection.
    This is more obvious now that we've delegated handling the connection
    to the `handle_connection` function.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- src/main.ml
+++ src/main.ml
@@ -20,10 +20,45 @@ open Xen_api_lwt_unix
 
 let uri = ref "http://127.0.0.1/"
 
+let handle_connection fd =
+  let rpc = make !uri in
+  let channel = Nbd_lwt_channel.of_fd fd in
+  Nbd_lwt_server.connect channel ()
+  >>= fun (name, t) ->
+  let uri = Uri.of_string name in
+  ( match Uri.user uri, Uri.password uri, Uri.get_query_param uri "session_id" with
+    | _, _, Some x ->
+      (* Validate the session *)
+      Session.get_uuid rpc x x
+      >>= fun _ ->
+      return (x, false)
+    | Some user, Some password, _ ->
+      Session.login_with_password rpc user password "1.0"
+      >>= fun session_id ->
+      return (session_id, true)
+  ) >>= fun (session_id, need_to_logout) ->
+  ( try_lwt
+      let path = Uri.path uri in (* note preceeding / *)
+      let vdi_uuid = if path <> "" then String.sub path 1 (String.length path - 1) else path in
+      (* FIXME: attach VDI *)
+      let filename = "/dev/null" in
+      Block.connect filename
+      >>= function
+      | `Error _ ->
+        Printf.fprintf stderr "Failed to open %s\n%!" filename;
+        fail (Failure filename)
+      | `Ok b ->
+        Nbd_lwt_server.serve t (module Block) b
+    finally
+      if need_to_logout
+      then Session.logout rpc session_id
+      else return ()
+  )
+
 let main port =
   let t =
-    let rpc = make !uri in
     let sock = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+    Lwt_unix.setsockopt sock Lwt_unix.SO_REUSEADDR true;
     let sockaddr = Lwt_unix.ADDR_INET(Unix.inet_addr_any, port) in
     Lwt_unix.bind sock sockaddr;
     Lwt_unix.listen sock 5;
@@ -32,38 +67,11 @@ let main port =
       >>= fun (fd, _) ->
       (* Background thread per connection *)
       let _ =
-        let channel = Nbd_lwt_channel.of_fd fd in
-        Nbd_lwt_server.connect channel ()
-        >>= fun (name, t) ->
-        let uri = Uri.of_string name in
-        ( match Uri.user uri, Uri.password uri, Uri.get_query_param uri "session_id" with
-          | _, _, Some x ->
-            (* Validate the session *)
-            Session.get_uuid rpc x x
-            >>= fun _ ->
-            return (x, false)
-          | Some user, Some password, _ ->
-            Session.login_with_password rpc user password "1.0"
-            >>= fun session_id ->
-            return (session_id, true)
-        ) >>= fun (session_id, need_to_logout) ->
-        ( try_lwt
-            let path = Uri.path uri in (* note preceeding / *)
-            let vdi_uuid = if path <> "" then String.sub path 1 (String.length path - 1) else path in
-            (* FIXME: attach VDI *)
-            let filename = "/dev/null" in
-            Block.connect filename
-            >>= function
-            | `Error _ ->
-              Printf.fprintf stderr "Failed to open %s\n%!" filename;
-              fail (Failure filename)
-            | `Ok b ->
-              Nbd_lwt_server.serve t (module Block) b
-          finally
-            if need_to_logout
-            then Session.logout rpc session_id
-            else return ()
-        ) in
+        Lwt.catch
+          (fun () -> handle_connection fd)
+          (fun e -> Printf.fprintf stderr "Caught %s\n%!" (Printexc.to_string e); return ())
+        >>= fun () ->
+        Lwt_unix.close fd in
       return ()
     done in
   Lwt_main.run t;
