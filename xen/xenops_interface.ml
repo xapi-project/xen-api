ec95628ea75d92f9f2e63c53afa3389ed519d6a0
John Else <john.else@citrix.com>
2015-05-29 12:38:06 +0100

    Add functions to convert between PCI address and string
    Signed-off-by: John Else <john.else@citrix.com>

--- xen/xenops_interface.ml
+++ xen/xenops_interface.ml
@@ -103,6 +103,14 @@ module Pci = struct
 	}
 	with sexp
 
+	let address_of_string str =
+		Scanf.sscanf str "%04x:%02x:%02x.%02x"
+			(fun domain bus dev fn -> {domain; bus; dev; fn})
+
+	let string_of_address address =
+		Printf.sprintf "%04x:%02x:%02x.%01x"
+			address.domain address.bus address.dev address.fn
+
 	type id = string * string
 
 	type t = {
