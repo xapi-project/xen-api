268575cef7987907f16c2a3213604988b2d37549
Koushik Chakravarty <Koushik.Chakravarty@citrix.com>
2015-06-24 10:05:20 +0000

    CP-12141: Remove all master/slave knowledge from xcp-rrdd
    This does the following:
    * Design made as part of CP-12182 has been implemented in xcp-rrdd and xen-api
    * pool_role_shared.ml has been removed from xcp-rrdd. rrdd API calls which
      rely on it have been changed to accept host addresses
    * unnecessary () arguments have been removed from API calls in
      rrd_interface.ml
    
    Signed-off-by: Koushik Chakravarty <Koushik.Chakravarty@citrix.com>

--- ocaml/xapi/rrdd_proxy.ml
+++ ocaml/xapi/rrdd_proxy.ml
@@ -202,14 +202,18 @@ let is_vm_on_localhost ~__context ~(vm_uuid : string) : bool =
 let push_rrd ~__context ~(vm_uuid : string) : unit =
 	let is_on_localhost = is_vm_on_localhost ~__context ~vm_uuid in
 	let domid = vm_uuid_to_domid ~__context ~uuid:vm_uuid in
-	log_and_ignore_exn (Rrdd.push_rrd ~vm_uuid ~domid ~is_on_localhost)
+	if is_on_localhost then
+		log_and_ignore_exn (fun () -> Rrdd.push_rrd_local ~vm_uuid ~domid )
+	else
+		let master = Pool_role.get_master_address () in
+		log_and_ignore_exn (fun () -> Rrdd.push_rrd_remote ~vm_uuid ~remote_address:master)
 
 let migrate_rrd ~__context ?remote_address ?session_id ~vm_uuid ~host_uuid () =
 	let remote_address = match remote_address with
 		| None -> Db.Host.get_address ~__context ~self:(Ref.of_string host_uuid)
 		| Some a -> a
 	in
-	log_and_ignore_exn (
+	log_and_ignore_exn ( fun () ->
 		Rrdd.migrate_rrd ~remote_address ?session_id ~vm_uuid ~host_uuid
 	)
 
@@ -220,12 +224,9 @@ module Deprecated = struct
 		try int_of_string (List.assoc Constants.rrd_update_interval other_config)
 		with _ -> 0
 
-	let load_rrd ~__context ~uuid ~is_host =
-		let domid =
-			match is_host with
-			| true -> 0
-			| false -> vm_uuid_to_domid ~__context ~uuid
-		in
+	let load_rrd ~__context ~uuid =
+		let master_address = Pool_role.get_master_address () in
+		let is_master = Pool_role.is_master () in
 		let timescale = get_timescale ~__context in
-		log_and_ignore_exn (Rrdd.Deprecated.load_rrd ~uuid ~domid ~is_host ~timescale)
+		log_and_ignore_exn (fun () -> Rrdd.Deprecated.load_rrd ~uuid ~master_address ~is_master ~timescale)
 end
