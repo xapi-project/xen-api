e6b24e74f58562c82fe748fa4e90f1154d83f18a
Ravi Pandey <ravi.pandey@citrix.com>
2015-06-24 10:03:02 +0000

    CP-12141: Remove master/slave knowledge from xcp-rrdd
    Signed-off-by: Ravi Pandey <ravi.pandey@citrix.com>

--- rrdd/rrdd_shared.ml
+++ rrdd/rrdd_shared.ml
@@ -122,11 +122,14 @@ let send_rrd ?(session_id : string option) ~(address : string)
 	);
 	debug "Sending RRD complete."
 
-let archive_rrd ?(save_stats_locally = Pool_role_shared.is_master ()) ~uuid
-		~rrd () =
+let archive_rrd ?(remote_address = None) ~uuid ~rrd () =
+	let local, address = match remote_address with
+		| None -> true, ""
+		| Some x -> false, x
+	in
 	debug "Archiving RRD for object uuid=%s %s" uuid
-		(if save_stats_locally then "to local disk" else "to remote master");
-	if save_stats_locally then begin
+		(if local then "to local disk" else "to remote master");
+	if local then begin
 		try
 			(* Stash away the rrd onto disk. *)
 			let exists =
@@ -151,6 +154,5 @@ let archive_rrd ?(save_stats_locally = Pool_role_shared.is_master ()) ~uuid
 	end else begin
 		(* Stream it to the master to store, or maybe to a host in the migrate case *)
 		debug "About to send to master.";
-		let address = Pool_role_shared.get_master_address () in
 		send_rrd ~address ~to_archive:true ~uuid ~rrd ()
 	end
