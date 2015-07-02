63150de8e8080d49a3e98cb3bc8a92bbf6a89dea
Sharad Yadav <sharad.yadav@citrix.com>
2015-07-02 17:05:42 +0100

    Use new path for fcoe driver
    Signed-off-by: Sharad Yadav <sharad.yadav@citrix.com>

--- lib/network_utils.ml
+++ lib/network_utils.ml
@@ -33,7 +33,7 @@ let modprobe = "/sbin/modprobe"
 let ethtool = ref "/sbin/ethtool"
 let bonding_dir = "/proc/net/bonding/"
 let dhcp6c = "/sbin/dhcp6c"
-let fcoedriver = ref "/opt/xensource/libexec/fcoe_driver.py"
+let fcoedriver = ref "/opt/xensource/libexec/fcoe_driver"
 
 let call_script ?(log_successful_output=false) script args =
 	try
