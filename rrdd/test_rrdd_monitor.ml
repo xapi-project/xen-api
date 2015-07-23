c49496c96aac45ebcbc511cf9aa0ec770de0612d
Euan Harris <euan.harris@citrix.com>
2015-07-23 12:35:41 +0100

    test_rrdd_monitor: Label test arguments and reformat
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- rrdd/test_rrdd_monitor.ml
+++ rrdd/test_rrdd_monitor.ml
@@ -11,41 +11,72 @@ let reset_rrdd_shared_state ctxt =
         Hashtbl.clear Rrdd_shared.vm_rrds;
         Rrdd_shared.host_rrd := None
 
-let update_rrds_test dss uuid_domids paused_vms
-	num_vm_rrds num_host_dss =
-	fun ctxt ->
+let update_rrds_test ~dss ~uuid_domids ~paused_vms
+	~expected_vm_rrds ~expected_host_dss = fun ctxt ->
 	OUnit2.bracket reset_rrdd_shared_state (fun () -> ignore) ctxt;
 	Rrdd_monitor.update_rrds 12345.0 dss uuid_domids paused_vms;
-	assert_equal_int num_vm_rrds (Hashtbl.length Rrdd_shared.vm_rrds);
+	assert_equal_int expected_vm_rrds (Hashtbl.length Rrdd_shared.vm_rrds);
 	match !Rrdd_shared.host_rrd with
 	| None -> assert_failure "host_rrd should have been created"
 	| Some info ->
-		assert_equal_int num_host_dss (List.length Rrdd_shared.(info.dss))
+		assert_equal_int expected_host_dss (List.length Rrdd_shared.(info.dss))
 
 let update_rrds = "update_rrds" >::: let open Rrd in [
-	("Null update" >::
-		update_rrds_test [] [] [] 0 0);
+	"Null update" >:: update_rrds_test
+		~dss:[]
+		~uuid_domids:[]
+		~paused_vms:[]
+		~expected_vm_rrds:0
+		~expected_host_dss:0;
 
-	("Single host update" >::
-		update_rrds_test [(Host, ds_a)] [] [] 0 1);
+	"Single host update" >:: update_rrds_test
+		~dss:[(Host, ds_a)]
+		~uuid_domids:[]
+		~paused_vms:[]
+		~expected_vm_rrds:0
+		~expected_host_dss:1;
 
-	("Multiple host updates" >::
-		update_rrds_test [(Host, ds_a); (Host, ds_a)] [] [] 0 2);
+	"Multiple host updates" >:: update_rrds_test
+		~dss:[(Host, ds_a); (Host, ds_a)]
+		~uuid_domids:[]
+		~paused_vms:[]
+		~expected_vm_rrds:0
+		~expected_host_dss:2;
 
-	("Single non-resident VM update" >::
-		update_rrds_test [(VM "a", ds_a)] [] [] 0 0);
+	"Single non-resident VM update" >:: update_rrds_test
+		~dss:[(VM "a", ds_a)]
+		~uuid_domids:[]
+		~paused_vms:[]
+		~expected_vm_rrds:0
+		~expected_host_dss:0;
 
-	("Multiple non-resident VM updates" >::
-		update_rrds_test [(VM "a", ds_a); (VM "b", ds_a)] [] [] 0 0);
+	"Multiple non-resident VM updates" >:: update_rrds_test
+		~dss:[(VM "a", ds_a); (VM "b", ds_a)]
+		~uuid_domids:[]
+		~paused_vms:[]
+		~expected_vm_rrds:0
+		~expected_host_dss:0;
 
-	("Single resident VM update" >::
-		update_rrds_test [(VM "a", ds_a)] [("a", 1)] [] 1 0);
+	"Single resident VM update" >:: update_rrds_test
+		~dss:[(VM "a", ds_a)]
+		~uuid_domids:[("a", 1)]
+		~paused_vms:[]
+		~expected_vm_rrds:1
+		~expected_host_dss:0;
 
-	("Multiple resident VM updates" >::
-		update_rrds_test [(VM "a", ds_a); (VM "b", ds_a)] [("a", 1); ("b", 1)] [] 2 0);
+	"Multiple resident VM updates" >:: update_rrds_test
+		~dss:[(VM "a", ds_a); (VM "b", ds_a)]
+		~uuid_domids:[("a", 1); ("b", 1)]
+		~paused_vms:[]
+		~expected_vm_rrds:2
+		~expected_host_dss:0;
 
-	("Multiple resident and non-resident VM updates" >::
-		update_rrds_test [(VM "a", ds_a); (VM "b", ds_a); (VM "c", ds_a)] [("a", 1); ("b", 1)] [] 2 0);
+	"Multiple resident and non-resident VM updates" >:: update_rrds_test
+		~dss:[(VM "a", ds_a); (VM "b", ds_a); (VM "c", ds_a)]
+		~uuid_domids:[("a", 1); ("b", 1)]
+		~paused_vms:[]
+		~expected_vm_rrds:2
+		~expected_host_dss:0;
 ]
 
 let suite = "rrdd monitor test" >:::
