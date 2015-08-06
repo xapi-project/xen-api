a185ec6e1bee1c0957465cb5e60287c3cb487506
Koushik Chakravarty <koushik.chakravarty@citrix.com>
2015-08-06 11:11:22 +0000

    CA-168029: Using xl pci for plug/unplug in xenopsd-xc
    This xl pci-attach and pci-detach are working now and this
    patch makes xenopsd-xc to use them.
    Also removing some dead code paths
    
    Signed-off-by: Koushik Chakravarty <koushik.chakravarty@citrix.com>

--- xc/device.mli
+++ xc/device.mli
@@ -143,14 +143,11 @@ sig
 
 	exception Cannot_use_pci_with_no_pciback of t list
 
-	val add : xc:Xenctrl.handle -> xs:Xenstore.Xs.xsh -> address list -> Xenctrl.domid -> unit
-	val release : xc:Xenctrl.handle -> xs:Xenstore.Xs.xsh -> hvm:bool
-		-> address list -> Xenctrl.domid -> int -> unit
+	val add : xs:Xenstore.Xs.xsh -> address list -> Xenctrl.domid -> unit
+	val release : address list -> Xenctrl.domid -> unit
 	val reset : xs:Xenstore.Xs.xsh -> address -> unit
 	val bind : address list -> supported_driver -> unit
-	val plug : Xenops_task.t -> xc:Xenctrl.handle -> xs:Xenstore.Xs.xsh -> address -> Xenctrl.domid -> unit
-	val unplug : Xenops_task.t -> xc:Xenctrl.handle -> xs:Xenstore.Xs.xsh -> address -> Xenctrl.domid -> unit
-	val list : xc:Xenctrl.handle -> xs:Xenstore.Xs.xsh -> Xenctrl.domid -> (int * address) list
+	val list : xs:Xenstore.Xs.xsh -> Xenctrl.domid -> (int * address) list
 end
 
 module Vfs :
