238ffcd836524e30ff548a624386a29fbf769bf0
John Else <john.else@citrix.com>
2015-08-17 14:06:44 +0100

    CA-178240: Shrink SR.scan race window
    The longer the time between calling Storage_access.SR.scan and checking
    the database to see what needs updating, the more time there is for
    something to change behind the scenes - for instance, the SM GC could
    call VDI.forget. Moving the SR.stat call means that this window is much
    smaller.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_sr.ml
+++ ocaml/xapi/xapi_sr.ml
@@ -421,9 +421,9 @@ let scan ~__context ~sr =
 		(fun () ->
 			let sr_uuid = Db.SR.get_uuid ~__context ~self:sr in
 			let vs = C.SR.scan ~dbg:(Ref.string_of task) ~sr:sr_uuid in
-			let sr_info = C.SR.stat ~dbg:(Ref.string_of task) ~sr:sr_uuid in
 			let db_vdis = Db.VDI.get_records_where ~__context ~expr:(Eq(Field "SR", Literal sr')) in
 			update_vdis ~__context ~sr:sr db_vdis vs;
+			let sr_info = C.SR.stat ~dbg:(Ref.string_of task) ~sr:sr_uuid in
 			let virtual_allocation = List.fold_left Int64.add 0L (List.map (fun v -> v.virtual_size) vs) in
 			Db.SR.set_virtual_allocation ~__context ~self:sr ~value:virtual_allocation;
 			Db.SR.set_physical_size ~__context ~self:sr ~value:sr_info.total_space;
