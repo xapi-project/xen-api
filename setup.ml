e651bb4b395837a7b26c28ea25cc4c0294b10f88
John Else <john.else@citrix.com>
2015-09-09 14:09:33 +0100

    Revert "CP-12141: Removing pool_role_shared.ml and its callee's"
    This reverts commit 05694c05bd3a266a4d9e91cef4c32b4cc26d01e5.

--- setup.ml
+++ setup.ml
@@ -6932,7 +6932,7 @@ let setup_t =
                       bs_nativeopt = [(OASISExpr.EBool true, ["-bin-annot"])]
                    },
                    {
-                      lib_modules = ["Stats"; "Constants"];
+                      lib_modules = ["Stats"; "Constants"; "Pool_role_shared"];
                       lib_pack = false;
                       lib_internal_modules = [];
                       lib_findlib_parent = None;
