377e4ea6b1a5b579999d3d49186d31c4ee3897ea
John Else <john.else@citrix.com>
2015-09-09 14:20:36 +0100

    Revert "CP-12141: Removing unecessary ()'s from rrd API calls"
    This reverts commit 78aea4995f80826b1a5d077f825e1f049cc8c027.
    
    Conflicts:
    	rrd/rrd_interface.ml

--- rrd/rrd_interface.ml
+++ rrd/rrd_interface.ml
@@ -48,12 +48,12 @@ type interdomain_info = {
 
 external has_vm_rrd : vm_uuid:string -> bool = ""
 
-external push_rrd_local : vm_uuid:string -> domid:int -> unit = ""
-external push_rrd_remote : vm_uuid:string -> remote_address:string -> unit = ""
-external remove_rrd : uuid:string -> unit = ""
+external push_rrd_local : vm_uuid:string -> domid:int -> unit -> unit = ""
+external push_rrd_remote : vm_uuid:string -> remote_address:string -> unit -> unit = ""
+external remove_rrd : uuid:string -> unit -> unit = ""
 external migrate_rrd : ?session_id:string -> remote_address:string ->
-	vm_uuid:string -> host_uuid:string -> unit = ""
-external send_host_rrd_to_master : master_address:string -> unit = ""
+	vm_uuid:string -> host_uuid:string -> unit -> unit = ""
+external send_host_rrd_to_master : master_address:string -> unit -> unit = ""
 external backup_rrds : ?remote_address:string option -> unit -> unit = ""
 
 external add_host_ds : ds_name:string -> unit = ""
@@ -71,11 +71,11 @@ external forget_sr_ds : sr_uuid:string -> ds_name:string -> unit = ""
 external query_possible_sr_dss : sr_uuid:string -> Data_source.t list = ""
 external query_sr_ds : sr_uuid:string -> ds_name:string -> float = ""
 
-external update_use_min_max : value:bool -> unit = ""
+external update_use_min_max : value:bool -> unit -> unit = ""
 
 external update_vm_memory_target : domid:int -> target:int64 -> unit = ""
 
-external set_cache_sr : sr_uuid:string -> unit = ""
+external set_cache_sr : sr_uuid:string -> unit -> unit = ""
 external unset_cache_sr : unit -> unit = ""
 
 module Plugin = struct
@@ -106,12 +106,12 @@ end
 module HA = struct
 	external enable_and_update :
 		statefile_latencies:Rrd.Statefile_latency.t list ->
-		heartbeat_latency:float -> xapi_latency:float -> unit = ""
+		heartbeat_latency:float -> xapi_latency:float -> unit -> unit = ""
 	external disable : unit -> unit = ""
 end
 
 module Deprecated = struct
 	(* Could change timescale to sum type, e.g. Slow | Fast.*)
 	external load_rrd : uuid:string -> master_address:string -> is_master:bool ->
-		timescale:int -> unit = ""
+		timescale:int -> unit -> unit = ""
 end
