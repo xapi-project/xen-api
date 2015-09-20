sharady/xcp-idl

CP-13995: Adding new xcp-rrdd interfaces
1) push_sr_rrd : Load sr-rrds from filesystem
2) archive_sr_rrd : Archive sr-rrds to filesystem
3) sr_rrds_path : Identify sr-rrds filesystem location

Signed-off-by: Sharad Yadav <sharad.yadav@citrix.com>

rrd/rrd_interface.ml
@@ -47,6 +47,9 @@ type interdomain_info = {
 (* The interface is defined by extern function declarations. *)
 
 external has_vm_rrd : vm_uuid:string -> bool = ""
+external sr_rrds_path : uid:string -> string = ""
+external archive_sr_rrd : sr_uuid:string -> unit = ""
+external push_sr_rrd : sr_uuid:string -> unit = ""
 
 external push_rrd_local : vm_uuid:string -> domid:int -> unit = ""
 external push_rrd_remote : vm_uuid:string -> remote_address:string -> unit = ""

xapi-project/xen-api

rrd/rrd_interface.ml
@@ -47,6 +47,9 @@ type interdomain_info = {
 (* The interface is defined by extern function declarations. *)
 
 external has_vm_rrd : vm_uuid:string -> bool = ""
+external sr_rrds_path : sr_uuid:string -> string = ""
+external archive_sr_rrd : sr_uuid:string -> unit = ""
+external push_sr_rrd : sr_uuid:string -> unit = ""
 
 external push_rrd : vm_uuid:string -> domid:int -> is_on_localhost:bool ->
 	unit -> unit = ""