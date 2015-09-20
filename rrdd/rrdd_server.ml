sharady/xcp-rrdd

CP-13995: Adding functions for sr level rrds into xcp-rrdd
1) push_sr_rrd : Load sr-rrds from filesystem
2) archive_sr_rrd : Archive sr-rrds to filesystem
3) sr_rrds_path : Identify sr-rrds filesystem location

Signed-off-by: Sharad Yadav <sharad.yadav@citrix.com>

rrdd/rrdd_server.ml
@@ -25,6 +25,38 @@ open Rrdd_shared
 module D = Debug.Make(struct let name="rrdd_server" end)
 open D
 
+let sr_rrds_path _ ~(uid : string) : string =
+	Filename.concat sr_rrds_location uid
+
+let archive_sr_rrd _ ~(sr_uuid : string) : unit =
+	if Mutex.try_lock mutex then begin
+		let srrds =
+			try
+				Hashtbl.fold (fun k v acc -> (k,v.rrd)::acc) sr_rrds []
+			with exn ->
+				Mutex.unlock mutex;
+				raise exn
+		in
+		Mutex.unlock mutex;
+		try
+			let rrd = List.assoc sr_uuid srrds in
+			archive_rrd_internal ~uuid:sr_uuid ~rrd ()
+		with Not_found -> ()
+	end
+
+let get_sr_rrd ~sr_uuid =
+	let path = Filename.concat sr_rrds_location sr_uuid in
+	rrd_of_gzip path
+
+let push_sr_rrd _ ~(sr_uuid : string) : unit =
+	try
+		let rrd = get_sr_rrd ~sr_uuid in
+		debug "Pushing RRD for SR uuid=%s locally" sr_uuid;
+		Mutex.execute mutex (fun _ ->
+			Hashtbl.replace sr_rrds sr_uuid {rrd; dss=[]; domid=0}
+		)
+	with _ -> ()
+
 let has_vm_rrd _ ~(vm_uuid : string) =
 	Mutex.execute mutex (fun _ -> Hashtbl.mem vm_rrds vm_uuid)
 

xapi-project/xen-api

rrdd/rrdd_server.ml
@@ -25,6 +25,38 @@ open Rrdd_shared
 module D = Debug.Make(struct let name="rrdd_server" end)
 open D
 
+let sr_rrds_path _ ~(sr_uuid : string) : string =
+	Filename.concat sr_rrds_location (sr_uuid ^ ".gz")
+
+let archive_sr_rrd _ ~(sr_uuid : string) : unit =
+	if Mutex.try_lock mutex then begin
+		let srrds =
+			try
+				Hashtbl.fold (fun k v acc -> (k,v.rrd)::acc) sr_rrds []
+			with exn ->
+				Mutex.unlock mutex;
+				raise exn
+		in
+		Mutex.unlock mutex;
+		try
+			let rrd = List.assoc sr_uuid srrds in
+			archive_rrd ~uuid:sr_uuid ~rrd ()
+		with Not_found -> ()
+	end
+
+let get_sr_rrd ~sr_uuid =
+	let path = Filename.concat sr_rrds_location sr_uuid in
+	rrd_of_gzip path
+
+let push_sr_rrd _ ~(sr_uuid : string) : unit =
+	try
+		let rrd = get_sr_rrd ~sr_uuid in
+		debug "Pushing RRD for SR uuid=%s locally" sr_uuid;
+		Mutex.execute mutex (fun _ ->
+			Hashtbl.replace sr_rrds sr_uuid {rrd; dss=[]; domid=0}
+		)
+	with _ -> ()
+
 let has_vm_rrd _ ~(vm_uuid : string) =
 	Mutex.execute mutex (fun _ -> Hashtbl.mem vm_rrds vm_uuid)
 