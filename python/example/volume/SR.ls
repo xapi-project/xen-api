c8dbe8fe739c12bf42b285d69b6ae13c932233ae
Robert Breker <robert.breker@citrix.com>
2015-09-22 13:08:15 +0000

    Automated pep8 fixes
    Signed-off-by: Robert Breker <robert.breker@citrix.com>

--- python/example/volume/SR.ls
+++ python/example/volume/SR.ls
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 
-import sys, urlparse
-import xapi, v
+import sys
+import urlparse
+import xapi
+import v
+
 
 class Implementation(v.SR_skeleton):
+
     def ls(self, dbg, sr):
         u = urlparse.urlparse(sr)
-        return [ {
+        return [{
             "key": "unknown-volume",
             "name": "unknown-volume",
             "description": "",
             "read_write": True,
             "virtual_size": 1,
             "uri": ["file:\/\/\/secondary\/sr\/unknown-volume"]
-        } ]
+        }]
 
 if __name__ == "__main__":
     cmd = v.SR_commandline(Implementation())
--- python/xapi/__init__.py
+++ python/xapi/__init__.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
-import os, sys, time, socket, traceback, syslog, json, argparse
+import os
+import sys
+import time
+import socket
+import traceback
+import syslog
+import json
+import argparse
+
 
 def success(result):
-    return { "Status": "Success", "Value": result }
+    return {"Status": "Success", "Value": result}
 
-def handle_exception(e, code = None, params = None):
+
+def handle_exception(e, code=None, params=None):
     s = sys.exc_info()
     files = []
     lines = []
@@ -13,25 +22,27 @@ def handle_exception(e, code = None, params = None):
         files.append(slot[0])
         lines.append(slot[1])
     backtrace = {
-      "error": str(s[1]),
-      "files": files,
-      "lines": lines,
+        "error": str(s[1]),
+        "files": files,
+        "lines": lines,
     }
     code = "SR_BACKEND_FAILURE"
-    params = [ str(s[1]) ]
+    params = [str(s[1])]
     if hasattr(e, "code"):
-      code = e.code
+        code = e.code
     if hasattr(e, "params"):
-      params = e.params
+        params = e.params
     results = {
-      "code": code,
-      "params": params,
-      "backtrace": backtrace,
+        "code": code,
+        "params": params,
+        "backtrace": backtrace,
     }
     print >>sys.stdout, json.dumps(results)
     sys.exit(1)
 
+
 class XenAPIException(Exception):
+
     def __init__(self, code, params):
         Exception.__init__(self)
         if type(code) <> type("") and type(code) <> type(u""):
@@ -41,38 +52,54 @@ class XenAPIException(Exception):
         self.code = code
         self.params = params
 
+
 class MissingDependency(Exception):
+
     def __init__(self, missing):
         self.missing = missing
+
     def __str__(self):
         return "There is a missing dependency: %s not found" % self.missing
 
+
 class Rpc_light_failure(Exception):
+
     def __init__(self, name, args):
         self.name = name
         self.args = args
+
     def failure(self):
         # rpc-light marshals a single result differently to a list of results
         args = list(self.args)
         marshalled_args = args
         if len(args) == 1:
             marshalled_args = args[0]
-        return { 'Status': 'Failure',
-                 'ErrorDescription': [ self.name, marshalled_args ] }
+        return {'Status': 'Failure',
+                'ErrorDescription': [self.name, marshalled_args]}
+
 
 class InternalError(Rpc_light_failure):
+
     def __init__(self, error):
-        Rpc_light_failure.__init__(self, "Internal_error", [ error ])
+        Rpc_light_failure.__init__(self, "Internal_error", [error])
+
 
 class UnmarshalException(InternalError):
+
     def __init__(self, thing, ty, desc):
-        InternalError.__init__(self, "UnmarshalException thing=%s ty=%s desc=%s" % (thing, ty, desc))
+        InternalError.__init__(
+            self, "UnmarshalException thing=%s ty=%s desc=%s" % (thing, ty, desc))
+
 
 class TypeError(InternalError):
+
     def __init__(self, expected, actual):
-        InternalError.__init__(self, "TypeError expected=%s actual=%s" % (expected, actual))
+        InternalError.__init__(
+            self, "TypeError expected=%s actual=%s" % (expected, actual))
+
 
 class UnknownMethod(InternalError):
+
     def __init__(self, name):
         InternalError.__init__(self, "Unknown method %s" % name)
 
@@ -84,11 +111,13 @@ def is_long(x):
     except:
         return False
 
+
 class ListAction(argparse.Action):
+
     def __call__(self, parser, namespace, values, option_string=None):
         k = values[0]
         v = values[1]
         if hasattr(namespace, self.dest) and getattr(namespace, self.dest) is not None:
             getattr(namespace, self.dest)[k] = v
         else:
-            setattr(namespace, self.dest, { k: v })
+            setattr(namespace, self.dest, {k: v})
