1950d9d1015a92f69cbcf15ff30de9dbc08ff76d
Si Beaumont <simon.beaumont@citrix.com>
2015-10-01 14:33:56 +0100

    CA-184513: Remove RRD terminology from raw Cstruct VDI helpers
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- ocaml/xapi/xapi_vdi_helpers.ml
+++ ocaml/xapi/xapi_vdi_helpers.ml
@@ -225,38 +225,36 @@ module VDI_CStruct = struct
 		curr_text
 
 	(* Format the vdi for the first time *)
-	let format_rrd_vdi cstruct =
+	let format_raw_vdi cstruct =
 		set_magic_number cstruct;
 		set_version cstruct
 
 end
 
-(* Write the rrd stats to the rrd-stats vdi *)
-let write_rrd ~__context ~vdi ~text =
+let write_raw ~__context ~vdi ~text =
 	Helpers.call_api_functions ~__context
 		(fun rpc session_id -> Sm_fs_ops.with_open_block_attached_device __context rpc session_id vdi `RW
 			(fun fd ->
 				let mapping = Bigarray.(Array1.map_file fd char c_layout true VDI_CStruct.vdi_size) in
 				let cstruct = Cstruct.of_bigarray mapping in
 				if (VDI_CStruct.get_magic_number cstruct) <> VDI_CStruct.magic_number then
-					VDI_CStruct.format_rrd_vdi cstruct;
+					VDI_CStruct.format_raw_vdi cstruct;
 				let new_text_len = String.length text in
 				if new_text_len >= (VDI_CStruct.vdi_size - VDI_CStruct.vdi_format_length) then
-					error "Failure on rrd-stats vdi write, text length is more than 4MiB"
+					error "Failure on raw vdi write, text length is more than 4MiB"
 				else
 					VDI_CStruct.write_to_vdi cstruct text new_text_len
 			)
 		)
 
-(* Read the rrd stats from the rrd-stats vdi *)
-let read_rrd ~__context ~vdi =
+let read_raw ~__context ~vdi =
 	Helpers.call_api_functions ~__context
 		(fun rpc session_id -> Sm_fs_ops.with_open_block_attached_device  __context rpc session_id vdi `RW
 			(fun fd ->
 				let mapping = Bigarray.(Array1.map_file fd char c_layout false VDI_CStruct.vdi_size) in
 				let cstruct = Cstruct.of_bigarray mapping in
 				if (VDI_CStruct.get_magic_number cstruct) <> VDI_CStruct.magic_number then begin
-					error "Failure on rrd-stats vdi read, vdi is not formatted";
+					error "Failure on raw vdi read, vdi is not formatted";
 					(* Nothing to return, VDI is not formatted *)
 					None
 				end
@@ -279,7 +277,7 @@ let maybe_push_sr_rrds ~__context ~sr =
 		let vdi = find_or_create_rrd_vdi ~__context ~sr in
 		let sr_uuid = Db.SR.get_uuid ~__context ~self:sr in
 		let sr_rrds_path = Rrdd.sr_rrds_path ~sr_uuid in
-		let gzipped_rrds = read_rrd ~__context ~vdi in
+		let gzipped_rrds = read_raw ~__context ~vdi in
 		begin match gzipped_rrds with
 			| None -> debug "stats vdi doesn't have rdds"
 			| Some x ->
@@ -295,4 +293,4 @@ let maybe_copy_sr_rrds ~__context ~sr ~archive =
 			Rrdd.archive_sr_rrd ~sr_uuid;
 		let sr_rrds_path = Rrdd.sr_rrds_path ~sr_uuid in
 		let contents = Unixext.string_of_file sr_rrds_path in
-		write_rrd ~__context ~vdi ~text:contents
+		write_raw ~__context ~vdi ~text:contents
