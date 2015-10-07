9f4c448022211e59e4f13d0bbedcbdb2a9f9b5ba
Rob Hoes <rob.hoes@citrix.com>
2015-10-07 15:23:01 +0100

    Add function to extend one CPU feature sets based on another
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/cpuid_helpers.mli
+++ ocaml/xapi/cpuid_helpers.mli
@@ -26,4 +26,5 @@ val assert_vm_is_compatible :
   host:[ `host ] API.Ref.t ->
   ?remote:(Rpc.call -> Rpc.response Client.Id.t) * 'a Ref.t -> unit -> unit
 
+val extend : int64 array -> int64 array -> int64 array
 val zero_extend : int64 array -> int -> int64 array
