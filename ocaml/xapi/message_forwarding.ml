ec0f311eebf2249f07c129a84b1e5e9cbf222873
John Else <john.else@citrix.com>
2015-10-22 15:36:57 +0100

    CA-185997: Tolerate connection loss when setting ssl_legacy
    Setting ssl_legacy now resets the master connection (see commit 9a5edf8)
    so the master now expects the slaves to disconnect and polls until the
    database reports that the slave has ssl_legacy set to the correct value.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/message_forwarding.ml
+++ ocaml/xapi/message_forwarding.ml
@@ -2025,8 +2025,18 @@ module Forward = functor(Local: Custom_actions.CUSTOM_ACTIONS) -> struct
 
 		let set_ssl_legacy ~__context ~self ~value =
 			info "Host.set_ssl_legacy: host = '%s'; value = %b" (host_uuid ~__context self) value;
-			let	local_fn = Local.Host.set_ssl_legacy ~self ~value in
-			do_op_on ~local_fn ~__context ~host:self (fun session_id rpc -> Client.Host.set_ssl_legacy rpc session_id self value)
+			let success () =
+				if Db.Host.get_ssl_legacy ~__context ~self = value
+				then Some ()
+				else None
+			in
+			let local_fn = Local.Host.set_ssl_legacy ~self ~value in
+			let fn () =
+				do_op_on ~local_fn ~__context ~host:self
+					(fun session_id rpc ->
+						Client.Host.set_ssl_legacy rpc session_id self value)
+			in
+			tolerate_connection_loss fn success 30.
 
 		let ha_disable_failover_decisions ~__context ~host =
 			info "Host.ha_disable_failover_decisions: host = '%s'" (host_uuid ~__context  host);
