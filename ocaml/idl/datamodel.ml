e88f407a0f782413ea02d11663c51a79c4d7a2a6
Thomas Sanders <thomas.sanders@citrix.com>
2015-12-18 13:49:28 +0000

    CP-15186 enable vendor-device (for Windows update)
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -7296,7 +7296,7 @@ let vm =
 	field ~qualifier:StaticRO ~in_product_since:rel_boston ~default_value:(Some (VInt 0L)) ~ty:Int "version" "The number of times this VM has been recovered";
 	field ~qualifier:StaticRO ~in_product_since:rel_clearwater ~default_value:(Some (VString "0:0")) ~ty:(String) "generation_id" "Generation ID of the VM";
 	field ~writer_roles:_R_VM_ADMIN ~qualifier:RW ~in_product_since:rel_cream ~default_value:(Some (VInt 0L)) ~ty:Int "hardware_platform_version" "The host virtual hardware platform version the VM can run on";
-	field ~qualifier:StaticRO ~lifecycle:[Prototyped, rel_dundee, "Experimental"] ~doc_tags:[Windows] ~default_value:(Some (VBool true)) ~ty:Bool "has_vendor_device" "Does nothing at present; will control the presence of the emulated C000 PCI device which triggers Windows Update to fetch or update drivers for XenServer.";
+	field ~qualifier:StaticRO ~lifecycle:[Published, rel_dundee, ""] ~doc_tags:[Windows] ~default_value:(Some (VBool true)) ~ty:Bool "has_vendor_device" "When an HVM guest starts, this controls the presence of the emulated C000 PCI device which triggers Windows Update to fetch or update PV drivers.";
     ])
 	()
 
