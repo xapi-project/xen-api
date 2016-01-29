907d78ddd7559ccb6e557f5e057e4a139b045772
Thomas Sanders <thomas.sanders@citrix.com>
2016-01-29 18:07:32 +0000

    Better places for files
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>

--- scripts/xapi-logrotate.sh
+++ scripts/xapi-logrotate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/sbin/logrotate /etc/xapi.d/xapi-logrotate.conf
+/usr/sbin/logrotate /etc/xensource/xapi-logrotate.conf
 EXITVALUE=$?
 if [ $EXITVALUE != 0 ]; then
     /usr/bin/logger -t $0 "ALERT exited abnormally with [$EXITVALUE]"
