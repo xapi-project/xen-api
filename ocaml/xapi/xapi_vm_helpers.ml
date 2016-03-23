8873c4c1c8172d01a248319ae89169950c8a6b1a
Jonathan Davies <jonathan.davies@citrix.com>
2016-03-23 15:15:10 +0000

    CA-203169: Add PV_drivers_detected field to VM_guest_metrics
    This field merely reflects the value of 'pv_drivers_detected' returned by
    xenopsd from VM.stat.
    
    This field can be used by clients as an indication of whether at least one VBD
    or VIF has successfully connected.
    
    Signed-off-by: Jonathan Davies <jonathan.davies@citrix.com>

--- ocaml/xapi/xapi_vm_helpers.ml
+++ ocaml/xapi/xapi_vm_helpers.ml
@@ -858,6 +858,7 @@ let copy_guest_metrics ~__context ~vm =
 			~networks:all.API.vM_guest_metrics_networks
 			~network_paths_optimized:all.API.vM_guest_metrics_network_paths_optimized
 			~storage_paths_optimized:all.API.vM_guest_metrics_storage_paths_optimized
+			~pV_drivers_detected:all.API.vM_guest_metrics_PV_drivers_detected
 			~other:all.API.vM_guest_metrics_other
 			~last_updated:all.API.vM_guest_metrics_last_updated
 			~other_config:all.API.vM_guest_metrics_other_config
