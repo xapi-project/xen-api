3d325917911699b88a2a05140d0a79467c434f5c
Rob Hoes <rob.hoes@citrix.com>
2016-04-26 14:25:02 +0100

    travis/deploy: safety improvements
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Make sure we're not echoing any sensitive data
+set +x
 set -o errexit -o nounset
 
 if [ "$TRAVIS_BRANCH" != "master" ]
@@ -7,6 +9,9 @@ then
   echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
   exit 0
 fi
+
+# Error out if $GH_TOKEN is empty or unset
+: ${GH_TOKEN:?"GH_TOKEN needs to be uploaded via travis-encrypt"}
 
 rev=$(git rev-parse --short HEAD)
 
@@ -20,7 +25,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 # Using token clone xapi-project.github.io.git
-git clone --quiet "https://$GH_TOKEN@github.com/xapi-project/xapi-project.github.io.git" > /dev/null
+git clone --quiet "https://$GH_TOKEN@github.com/xapi-project/xapi-project.github.io.git" &2>1 > /dev/null
 
 # Copy data we're interested in into the right place
 cd xapi-project.github.io
@@ -28,4 +33,4 @@ cp -f $HOME/xenapi.json _data/
 cp -f $HOME/release_info.json _data/
 
 git commit -am "Updated XenAPI docs based on xen-api/${rev}"
-git push -q origin master > /dev/null
+git push -q origin master &2>1 > /dev/null
