82a893093b053a9a57b26425a99745363e3197d1
Christian Lindig <christian.lindig@citrix.com>
2016-05-12 11:25:22 +0100

    CP-17268 combine all setup code for bisect_ppx into one function
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- lib/xenopsd.ml
+++ lib/xenopsd.ml
@@ -58,21 +58,7 @@ let path () = Filename.concat !sockets_path "xenopsd"
 let forwarded_path () = path () ^ ".forwarded" (* receive an authenticated fd from xapi *)
 let json_path () = path () ^ ".json"
 
-(** [tmpdir] holds the path to a directory for temporary files *)
-let tmpdir =
-  let env n   = try Sys.getenv n with Not_found -> "" in
-  let dirs    = [env "TMP"; env "TEMP"; "/tmp"; "/usr/tmp"; "/var/tmp"] in
-  let is_dir  = function 
-    | ""    -> false
-    | path  -> try Sys.is_directory path with Sys_error _ -> false
-  in
-    try
-      List.find is_dir dirs
-    with
-      Not_found -> 
-        error "can't find temp directory %s" __LOC__;
-        exit 1
- 
+
 module Server = Xenops_interface.Server(Xenops_server)
 
 let rpc_fn call =
@@ -137,9 +123,25 @@ let configure ?(specific_options=[]) ?(specific_essential_paths=[]) ?(specific_n
 (** set up the BISECT_FILE env var to point to a temp directory where
  *  the log files go
  *)
-let setup_bisect () =
+let setup_bisect name =
   let (//) = Filename.concat in
-  try 
+  let tmpdir =
+    let getenv n   = try Sys.getenv n with Not_found -> "" in
+    let dirs    = 
+      [ getenv "TMP"
+      ; getenv "TEMP"
+      ; "/tmp"
+      ; "/usr/tmp"
+      ; "/var/tmp"
+      ] in
+    let is_dir  = function 
+    | ""    -> false
+    | path  -> try Sys.is_directory path with Sys_error _ -> false
+    in try
+      List.find is_dir dirs
+    with
+      Not_found -> error "can't find temp directory %s" __LOC__; exit 1
+  in try 
     ignore (Sys.getenv "BISECT_FILE") 
   with Not_found ->
     Unix.putenv "BISECT_FILE" (tmpdir // Printf.sprintf "bisect-%s" name)
@@ -172,7 +174,7 @@ let main backend =
 	Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
   Sys.set_signal Sys.sigterm (Sys.Signal_handle signal_handler);
   
-  setup_bisect (); (* coverage profiling *)
+  setup_bisect name; (* set up coverage profiling *)
 
 	Xenops_utils.set_fs_backend
 		(Some (if !persist
