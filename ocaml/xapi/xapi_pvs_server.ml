e0f4e3fbd48b2e0cb4297eb58accf1ba6c17b2f3
John Else <john.else@citrix.com>
2016-06-20 14:56:08 +0100

    CP-17647: Implement PVS_server.[introduce|forget]
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_pvs_server.ml
+++ ocaml/xapi/xapi_pvs_server.ml
@@ -14,13 +14,39 @@
 
 (** Implementation of PVS Server *)
 
+open Stdext
+open Listext
+
 module D = Debug.Make(struct let name = "xapi_pvs_server" end)
 
-let not_implemented x =
-  raise (Api_errors.Server_error (Api_errors.not_implemented, [ x ]))
+let assert_ports_valid ~first_port ~last_port =
+  let assert_port_valid ~port ~name =
+    if port < 1L || port > 65535L
+    then raise Api_errors.(Server_error (value_not_supported, [
+        name; Int64.to_string port; "Port out of range"
+      ]))
+  in
+  assert_port_valid ~port:first_port ~name:"first_port";
+  assert_port_valid ~port:last_port ~name:"last_port";
+  if last_port < first_port
+  then raise Api_errors.(Server_error (value_not_supported ,[
+      "last_port"; Int64.to_string last_port; "last_port smaller than first_port";
+    ]))
 
 let introduce ~__context ~addresses ~first_port ~last_port ~farm =
-  not_implemented "PVS_server.introduce"
+  List.iter
+    (fun address -> Helpers.assert_is_valid_ip `ipv4 "addresses" address)
+    addresses;
+  assert_ports_valid ~first_port ~last_port;
+  if not (Db.is_valid_ref __context farm)
+  then raise Api_errors.(Server_error (invalid_value ,[
+      "farm"; Ref.string_of farm
+    ]));
+  let pvs_server = Ref.make () in
+  let uuid = Uuidm.to_string (Uuidm.create `V4) in
+  Db.PVS_server.create ~__context
+    ~ref:pvs_server ~uuid ~addresses:(List.setify addresses)
+    ~first_port ~last_port ~farm;
+  pvs_server
 
-let forget ~__context ~self =
-  not_implemented "PVS_server.forget"
+let forget ~__context ~self = Db.PVS_server.destroy ~__context ~self
