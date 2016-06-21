77d5715e63f8b9f217e22f68a0e41ee88b1797df
John Else <john.else@citrix.com>
2016-06-21 10:50:31 +0100

    Add xapi_pvs_proxy.mli
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ ocaml/xapi/xapi_pvs_proxy.mli
@@ -0,0 +1,26 @@
+(*
+ * Copyright (C) 2016 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+val create : __context:Context.t ->
+  farm:API.ref_PVS_farm ->
+  vif:API.ref_VIF ->
+  prepopulate:bool ->
+  API.ref_PVS_proxy
+
+val destroy : __context:Context.t -> self:API.ref_PVS_proxy -> unit
+
+val set_prepopulate : __context:Context.t ->
+  self:API.ref_PVS_proxy ->
+  value:bool -> unit
+
