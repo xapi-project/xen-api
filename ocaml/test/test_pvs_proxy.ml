1c54bf4275b7d0282dae8dd0a32da65b2210f60e
Christian Lindig <christian.lindig@citrix.com>
2016-06-27 15:12:34 +0100

    CP-17657 add unit test for GC'ing PVS_proxy objects
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/test/test_pvs_proxy.ml
+++ ocaml/test/test_pvs_proxy.ml
@@ -65,6 +65,23 @@ let test_destroy () =
   Xapi_pvs_proxy.destroy ~__context ~self:pvs_proxy;
   assert_equal (Db.is_valid_ref __context pvs_proxy) false
 
+let test_gc_proxy () =
+  let __context = make_test_database () in
+  let farm = make_pvs_farm ~__context () in
+  let vIF = make_vif ~__context ~device:"0" () in
+  let proxy1 = Xapi_pvs_proxy.create ~__context ~farm ~vIF ~prepopulate:true in
+  let proxy2 = Xapi_pvs_proxy.create ~__context ~farm ~vIF ~prepopulate:true in
+  ( Db_gc.gc_PVS_proxies ~__context
+  ; assert_equal (Db.PVS_proxy.get_farm ~__context ~self:proxy1) farm
+  ; assert_equal (Db.PVS_proxy.get_VIF ~__context ~self:proxy1) vIF
+  ; Db.PVS_proxy.set_farm ~__context ~self:proxy1 ~value:Ref.null
+  ; Db_gc.gc_PVS_proxies ~__context (* should collect the proxy *)
+  ; assert_equal false (Db.is_valid_ref __context proxy1)
+  ; Db.PVS_proxy.set_VIF ~__context ~self:proxy2 ~value:Ref.null
+  ; Db_gc.gc_PVS_proxies ~__context (* should collect the proxy *)
+  ; assert_equal false (Db.is_valid_ref __context proxy2)
+  )
+
 let test =
   "test_pvs_proxy" >:::
   [
@@ -73,4 +90,5 @@ let test =
     "test_create_invalid_farm" >:: test_create_invalid_farm;
     "test_create_invalid_vif" >:: test_create_invalid_vif;
     "test_destroy" >:: test_destroy;
+    "test_gc_proxy" >:: test_gc_proxy
   ]
