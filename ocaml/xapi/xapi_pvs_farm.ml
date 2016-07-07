c50bf13e57535f1ca5041a741cb894f44c14df6d
Rob Hoes <rob.hoes@citrix.com>
2016-07-08 00:17:33 +0100

    Add function to update local PVS-proxy daemon configuration
    This function causes the proxy configuration to be updated for all locally
    running proxies of a farm, taking into account starting and stopping proxies.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_pvs_farm.ml
+++ ocaml/xapi/xapi_pvs_farm.ml
@@ -15,7 +15,11 @@
 (* This module implements methods for the PVS_farm class *)
 
 module D = Debug.Make(struct let name = "xapi_pvs_farm" end)
+open D
 module E = Api_errors
+
+let proxy_port_name bridge =
+  "pvs-" ^ bridge
 
 let api_error msg xs = raise (E.Server_error (msg, xs))
 
@@ -35,6 +39,63 @@ let proxies ~__context ~self =
          ((Eq (Field "farm", Literal (Ref.string_of self)))
          ,(Eq (Field "currently_attached", Literal "true"))
          ))
+
+let metadata_of_farm ~__context ~self ~vdi ~proxies =
+  let open Network_interface in
+  let farm_rc = Db.PVS_farm.get_record ~__context ~self in
+  let servers =
+    List.map (fun self ->
+        let rc = Db.PVS_server.get_record ~__context ~self in
+        PVS_proxy.Server.{
+          uuid = rc.API.pVS_server_uuid;
+          addresses = List.map Unix.inet_addr_of_string rc.API.pVS_server_addresses;
+          first_port = Int64.to_int rc.API.pVS_server_first_port;
+          last_port = Int64.to_int rc.API.pVS_server_last_port;
+        }
+      ) farm_rc.API.pVS_farm_servers
+  in
+  let clients =
+    List.map (fun self ->
+        let vif = Db.PVS_proxy.get_VIF ~__context ~self in
+        let rc = Db.VIF.get_record ~__context ~self:vif in
+        let prepopulate = Db.PVS_proxy.get_prepopulate ~__context ~self in
+        PVS_proxy.Client.{
+          uuid = rc.API.vIF_uuid;
+          mac = rc.API.vIF_MAC;
+          interface = proxy_port_name (Db.Network.get_bridge ~__context ~self:rc.API.vIF_network);
+          prepopulate;
+        }
+      ) proxies
+  in
+  let vdi = Db.VDI.get_uuid ~__context ~self:vdi in
+  PVS_proxy.{
+    farm_uuid = farm_rc.API.pVS_farm_uuid;
+    farm_name = farm_rc.API.pVS_farm_name;
+    servers;
+    clients;
+    vdi;
+  }
+
+let update_farm_on_localhost ~__context ~self ~vdi ?(starting_proxies=[]) ?(stopping_proxies=[]) () =
+  debug "Updating PVS farm %s. Starting proxies: [%s]. Stopping proxies: [%s]"
+    (Ref.string_of self)
+    (String.concat ", " (List.map Ref.string_of starting_proxies))
+    (String.concat ", " (List.map Ref.string_of stopping_proxies));
+  let dbg = Context.string_of_task __context in
+  let running_proxies = proxies ~__context ~self in
+  let localhost = Helpers.get_localhost ~__context in
+  let local_running_proxies = List.filter (fun proxy ->
+      let vif = Db.PVS_proxy.get_VIF ~__context ~self:proxy in
+      let vm = Db.VIF.get_VM ~__context ~self:vif in
+      Db.VM.get_resident_on ~__context ~self:vm = localhost
+    ) running_proxies in
+  let proxies = starting_proxies @ (Listext.List.set_difference local_running_proxies stopping_proxies) in
+  let proxy_config = metadata_of_farm ~__context ~self ~vdi ~proxies in
+  if proxy_config.Network_interface.PVS_proxy.clients <> [] then
+    Network.Net.PVS_proxy.configure_farm dbg proxy_config
+  else
+    let uuid = Db.PVS_farm.get_uuid ~__context ~self in
+    Network.Net.PVS_proxy.remove_farm dbg uuid
 
 let forget ~__context ~self =
   let open Db_filter_types in
--- ocaml/xapi/xapi_pvs_proxy.ml
+++ ocaml/xapi/xapi_pvs_proxy.ml
@@ -21,9 +21,6 @@ module D = Debug.Make(struct let name = "xapi_pvs_proxy" end)
 let not_implemented x =
   raise (Api_errors.Server_error (Api_errors.not_implemented, [ x ]))
 
-let proxy_port_name bridge =
-  "pvs-" ^ bridge
-
 let start ~__context vif proxy =
   if not (Db.PVS_proxy.get_currently_attached ~__context ~self:proxy) then begin
     try
@@ -33,8 +30,8 @@ let start ~__context vif proxy =
       let dbg = Context.string_of_task __context in
       let network = Db.VIF.get_network ~__context ~self:vif in
       let bridge = Db.Network.get_bridge ~__context ~self:network in
-      let name = proxy_port_name bridge in
-      Network.Net.Bridge.add_port dbg ~bridge ~name ~kind:Network_interface.PVS_proxy ~interfaces:[] ();
+      let port_name = Xapi_pvs_farm.proxy_port_name bridge in
+      Network.Net.Bridge.add_port dbg ~bridge ~name:port_name ~kind:Network_interface.PVS_proxy ~interfaces:[] ();
       Db.PVS_proxy.set_currently_attached ~__context ~self:proxy ~value:true;
       Db.PVS_proxy.set_cache_SR ~__context ~self:proxy ~value:sr
     with Xapi_pvs_cache.No_cache_sr_available ->
@@ -46,8 +43,8 @@ let stop ~__context vif proxy =
     let dbg = Context.string_of_task __context in
     let network = Db.VIF.get_network ~__context ~self:vif in
     let bridge = Db.Network.get_bridge ~__context ~self:network in
-    let name = proxy_port_name bridge in
-    Network.Net.Bridge.remove_port dbg ~bridge ~name;
+    let port_name = Xapi_pvs_farm.proxy_port_name bridge in
+    Network.Net.Bridge.remove_port dbg ~bridge ~name:port_name;
     Db.PVS_proxy.set_currently_attached ~__context ~self:proxy ~value:false;
     Db.PVS_proxy.set_cache_SR ~__context ~self:proxy ~value:Ref.null
   end
