427c32d9968cf3e2e11027c123160a5d22cf126b
John Else <john.else@citrix.com>
2016-07-07 16:49:49 +0100

    Add doccomments to xapi_pvs_cache.mli
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_pvs_cache.mli
+++ ocaml/xapi/xapi_pvs_cache.mli
@@ -14,13 +14,31 @@
 
 exception No_cache_sr_available
 
+(** Check the pool for PVS cache SRs which are
+    a) visible to the specified host
+    b) listed as cache storage for the specified PVS farm.
+
+    Returns
+      Some (sr, Some vdi)
+      - if there is an SR available with a cache VDI
+      Some (sr, None)
+      - if there is an SR available with no cache VDI
+      None
+      - if there is no SR available
+
+    If multiple SRs are available, the algorithm will firstly prefer SRs which
+    already contain cache VDIs, and secondly will prefer SRs with
+    lexicographically lower UUIDs. *)
 val check_cache_availability :
   __context:Context.t ->
   host:API.ref_host ->
   farm:API.ref_PVS_farm ->
   (API.ref_SR * API.ref_VDI option) option
 
+(** Ensure a cache VDI exists in an SR suitable for this host and PVS farm.
+    If there is no suitable SR, raise [No_cache_sr_available]. *)
 val on_proxy_start :
   __context:Context.t -> host:API.ref_host -> farm:API.ref_PVS_farm -> unit
 
+(** Destroy all PVS cache VDIs in the specified SR. *)
 val on_sr_remove : __context:Context.t -> sr:API.ref_SR -> unit
