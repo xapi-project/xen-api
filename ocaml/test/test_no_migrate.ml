3a2aa435f990934db283192fb570c1f8d92ce2cd
Christian Lindig <christian.lindig@citrix.com>
2016-07-15 15:57:40 +0100

    CP-18172 add unit tests for platform:nomigrate. platform:nested-virt
    The unit tests test Xapi_vm_lifecycl.get_operation_error with the
    criticial operations suspend, checkpoint, pool_migrate, migrate_send.
    These should not permit migration in the presence of platform variables
    that indicate migration is blocked in general or that nested
    virtualisation is being used.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

---
+++ ocaml/test/test_no_migrate.ml
@@ -0,0 +1,128 @@
+(*
+ * Copyright (C) 2016 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open OUnit
+open Test_common
+
+module LC = Xapi_vm_lifecycle
+
+let critical =
+	[ `suspend
+	; `checkpoint
+	; `pool_migrate
+	; `migrate_send
+	]
+
+let ops =
+	[ `assert_operation_valid
+	; `awaiting_memory_live
+	; `call_plugin
+	; `changing_dynamic_range
+	; `changing_memory_limits
+	; `changing_memory_live
+	; `changing_shadow_memory
+	; `changing_shadow_memory_live
+	; `changing_static_range
+	; `changing_VCPUs
+	; `changing_VCPUs_live
+	; `checkpoint
+	; `clean_reboot
+	; `clean_shutdown
+	; `clone
+	; `copy
+	; `create_template
+	; `csvm
+	; `data_source_op
+	; `destroy
+	; `export
+	; `get_boot_record
+	; `hard_reboot
+	; `hard_shutdown
+	; `import
+	; `make_into_template
+	; `metadata_export
+	; `migrate_send
+	; `pause
+	; `pool_migrate
+	; `power_state_reset
+	; `provision
+	; `query_services
+	; `resume
+	; `resume_on
+	; `revert
+	; `reverting
+	; `send_sysrq
+	; `send_trigger
+	; `shutdown
+	; `snapshot
+	; `snapshot_with_quiesce
+	; `start
+	; `start_on
+	; `suspend
+	; `unpause
+	; `update_allowed_operations
+	]
+
+let base_case () =
+	let __context = make_test_database () in
+	let vm        = make_vm ~__context () in
+		( Db.VM.set_power_state ~__context ~self:vm ~value:`Running
+		; assert_equal ~msg:"suspend" None
+			 (LC.get_operation_error ~__context ~self:vm ~op:`suspend ~strict:false)
+		; assert_equal ~msg:"checkpoint" None
+			 (LC.get_operation_error ~__context ~self:vm ~op:`checkpoint ~strict:false)
+		; assert_equal ~msg:"pool_migrate" None
+			 (LC.get_operation_error ~__context ~self:vm ~op:`pool_migrate ~strict:false)
+		; assert_equal ~msg:"migrate_send" None
+			 (LC.get_operation_error ~__context ~self:vm ~op:`migrate_send ~strict:false)
+		)
+
+let with_platform platform check () =
+	let __context = make_test_database () in
+	let self      = make_vm ~__context () in
+	let ()        =
+		( Db.VM.set_power_state ~__context ~self ~value:`Running
+		; Db.VM.set_platform ~__context ~self ~value:platform
+		; Helpers.set_boot_record ~__context ~self
+			(Db.VM.get_record ~__context ~self)
+		) in
+	critical (* check all critical operations *)
+	|> List.map (fun op -> LC.get_operation_error ~__context ~self ~op ~strict:false)
+	|> List.for_all check
+	|> assert_bool "with_platform"
+
+let test =
+	let migrate = (=)  None  in (* no errors *)
+	let dont    = (<>) None  in (* errors - can't migrate *)
+	"test_no_migrate" >:::
+	[ "test_no_migrate_00" >:: base_case
+	; "test_no_migrate_01" >:: with_platform []                         migrate
+	; "test_no_migrate_02" >:: with_platform ["nested-virt","true"]     dont
+	; "test_no_migrate_03" >:: with_platform ["nested-virt","false"]    migrate
+	; "test_no_migrate_04" >:: with_platform ["nomigrate","true"]       dont
+	; "test_no_migrate_05" >:: with_platform ["nomigrate","false"]      migrate
+	; "test_no_migrate_06"
+		>:: with_platform ["nomigrate","true"; "nested-virt", "true"]     dont
+	; "test_no_migrate_07"
+		>:: with_platform ["nomigrate","true"; "nested-virt", "false"]    dont
+	; "test_no_migrate_08"
+		>:: with_platform ["nomigrate","false"; "nested-virt", "false"]   migrate
+	; "test_no_migrate_09"
+		>:: with_platform ["nomigrate","false"; "nested-virt", "true"]    dont
+	; "test_no_migrate_10" >:: with_platform ["nomigrate","TRUE"]       dont
+	; "test_no_migrate_12" >:: with_platform ["nomigrate","1"]          dont
+	; "test_no_migrate_12" >:: with_platform ["nested-virt","1"]        dont
+	; "test_no_migrate_13" >:: with_platform ["nested-virt","2"]        migrate
+	; "test_no_migrate_14" >:: with_platform ["nested-virt","0"]        migrate
+	]
