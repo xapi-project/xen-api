1d94e263381ed131c502033c02d86c3236c25488
Christian Lindig <christian.lindig@citrix.com>
2016-09-02 15:05:05 +0100

    CP-18294 rewrite unit test for mobility testing
    This series of commits changes the way VM mobility is decided: the
    relevant value are now picked up from VM_metrics and no longer from
    platform flags. This must be reflected in the tests. The test suite now
    populates the relevant fields in VM_metrics and tests the critical
    VM operations.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/test/test_no_migrate.ml
+++ ocaml/test/test_no_migrate.ml
@@ -17,96 +17,55 @@ open Test_common
 
 module LC = Xapi_vm_lifecycle
 
-let critical =
+let ops =
 	[ `suspend
 	; `checkpoint
 	; `pool_migrate
 	; `migrate_send
 	]
 
+let op_string = function
+	| `suspend      -> "suspend"
+	| `checkpoint   -> "checkpoint"
+	| `pool_migrate -> "pool_migrate"
+	| `migrate_send -> "migrate_send"
+	| _             -> "other"
 
-type mobility = Mobile | Static
-
-let platforms =
-	[ Static, ["nested-virt","true"]
-	; Mobile, ["nested-virt","false"]
-	; Static, ["nomigrate","true"]
-	; Mobile, ["nomigrate","false"]
-	; Static, ["nomigrate","true"; "nested-virt", "true"]
-	; Static, ["nomigrate","true"; "nested-virt", "false"]
-	; Mobile, ["nomigrate","false"; "nested-virt", "false"]
-	; Static, ["nomigrate","false"; "nested-virt", "true"]
-	; Static, ["nomigrate","TRUE"]
-	; Static, ["nomigrate","1"]
-	; Static, ["nested-virt","1"]
-	; Mobile, ["nested-virt","0"]
+let testcases =
+	(*nest , nomig, force, permitted *)
+	[ false, false, false, true
+	; false, false, true , true
+	; false, true , false, false
+	; false, true , true , true
+	; true , false, false, false
+	; true , false, true , true
+	; true , true , false, false
+	; true , true , true , true
 	]
 
-let simple () =
+let test (nv, nm, force, permitted) op =
 	let __context = make_test_database () in
 	let vm        = make_vm ~__context ~hVM_boot_policy:"" () in
+	let metrics   = Db.VM.get_metrics ~__context ~self:vm in
+	let strict    = not force in
 		( Db.VM.set_power_state ~__context ~self:vm ~value:`Running
-		; LC.get_operation_error ~__context ~self:vm ~op:`suspend ~strict:true
+		; Db.VM_metrics.set_nested_virt ~__context ~self:metrics ~value:nv
+		; Db.VM_metrics.set_nomigrate   ~__context ~self:metrics ~value:nm
+		; LC.get_operation_error ~__context ~self:vm ~op ~strict
 		|> function
-		| None          -> assert_bool "success" true
-		| Some (x,xs)   -> assert_failure (String.concat "|" (x::xs))
+		| None        when permitted     -> assert_bool "success" true
+		| None                           -> assert_failure (op_string op)
+		| Some (x,xs) when not permitted -> assert_bool "success" true
+		| Some (x,xs)                    -> assert_failure (op_string op)
 		)
 
-let base () =
-	let __context = make_test_database () in
-	let vm        = make_vm ~__context ~hVM_boot_policy:"" () in
-		( Db.VM.set_power_state ~__context ~self:vm ~value:`Running
-		; assert_equal ~msg:"suspend" None
-			 (LC.get_operation_error ~__context ~self:vm ~op:`suspend ~strict:true)
-		; assert_equal ~msg:"checkpoint" None
-			 (LC.get_operation_error ~__context ~self:vm ~op:`checkpoint ~strict:true)
-		; assert_equal ~msg:"pool_migrate" None
-			 (LC.get_operation_error ~__context ~self:vm ~op:`pool_migrate ~strict:true)
-		; assert_equal ~msg:"migrate_send" None
-			 (LC.get_operation_error ~__context ~self:vm ~op:`migrate_send ~strict:true)
-		)
-
-(** [with_platform] checks that mobility is signaled according to the
- * values found in the platform flags *)
-let with_platform () =
-	let __context = make_test_database () in
-	let self      = make_vm ~__context ~hVM_boot_policy:"" () in
-	let test platform predicate =
-		( Db.VM.set_power_state ~__context ~self ~value:`Running
-		; Db.VM.set_platform ~__context ~self ~value:platform
-		; Helpers.set_boot_record ~__context ~self
-			(Db.VM.get_record ~__context ~self)
-		; critical (* check all critical operations *)
-		|> List.map (fun op -> LC.get_operation_error ~__context ~self ~op ~strict:true)
-		|> List.for_all predicate
-		|> assert_bool "with_platform"
-		) in
-	let predicate = function
-	| Mobile  -> (=)  None    (* mobile, no error expected *)
-	| Static  -> (<>) None in (* not mobile, expect an error message *)
-	List.iter (fun (mobile, platform) -> test platform (predicate mobile)) platforms
-
-(** [override] checks that migration is always possible when --force is
- * used *)
-let override () =
-	let __context = make_test_database () in
-	let self      = make_vm ~__context ~hVM_boot_policy:"" () in
-	let test platform =
-		( Db.VM.set_power_state ~__context ~self ~value:`Running
-		; Db.VM.set_platform ~__context ~self ~value:platform
-		; Helpers.set_boot_record ~__context ~self
-			(Db.VM.get_record ~__context ~self)
-		; critical (* check all critical operations *)
-		|> List.map (fun op -> LC.get_operation_error ~__context ~self ~op ~strict:false)
-		|> List.for_all ((=) None) (* should not see any error *)
-		|> assert_bool "override"
-		) in
-	List.iter (fun (_, platform) -> test platform) platforms
+let test' op =
+	testcases |> List.iter (fun t -> test t op)
 
 let test = "test_no_migrate" >:::
-	[ "test_no_migrate_00" >:: simple
-	; "test_no_migrate_01" >:: base
-	; "test_no_migrate_02" >:: with_platform
-	; "test_no_migrate_03" >:: override
+	[ "test_no_migrate_00" >:: (fun () -> test' `suspend)
+	; "test_no_migrate_01" >:: (fun () -> test' `checkpoint)
+	; "test_no_migrate_02" >:: (fun () -> test' `pool_migrate)
+	; "test_no_migrate_03" >:: (fun () -> test' `migrate_send)
 	]
 
