9ca1f62b643d94077a5201583dba4af6f7339b04
Marcello Seri <marcello.seri@citrix.com>
2016-10-07 16:28:11 +0100

    include get_guest_metrics in the try/with block
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -1500,8 +1500,8 @@ let update_vm ~__context id =
           let update_pv_drivers_detected () =
             Opt.iter
               (fun (_, state) ->
-                 let gm = Db.VM.get_guest_metrics ~__context ~self in
-                 try
+                try
+                   let gm = Db.VM.get_guest_metrics ~__context ~self in
                    debug "xenopsd event: Updating VM %s PV drivers detected %b" id state.pv_drivers_detected;
                    Db.VM_guest_metrics.set_PV_drivers_detected ~__context ~self:gm ~value:state.pv_drivers_detected;
                    Db.VM_guest_metrics.set_PV_drivers_up_to_date ~__context ~self:gm ~value:state.pv_drivers_detected
