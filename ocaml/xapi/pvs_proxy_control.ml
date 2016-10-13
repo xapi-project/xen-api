fe4819108d8bc7ceae88ad36836db572c3a386a9
Rob Hoes <rob.hoes@citrix.com>
2016-10-13 15:43:16 +0100

    CA-224967: Don't remove proxy state from xenstore when OVS rules are removed
    When a proxied VM is rebooted, the pvsproxy daemon is left configured, but the
    OVS rules are removed and put back for the new VIF. This triggers and event
    from xenopsd to xapi to update PVS_proxy.currently_attached to false. Xapi also
    happens to remove the pvsproxy state from xenstore, even though the daemon is
    still configured. This causes the OVS rules to not be reinstated when the VM
    comes up again.
    
    The solution is to simply leave the pvsproxy state alone.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/pvs_proxy_control.ml
+++ ocaml/xapi/pvs_proxy_control.ml
@@ -271,8 +271,6 @@ let stop_proxy ~__context vif proxy =
     error "Unable to disable PVS proxy for VIF %s: %s." (Ref.string_of vif) reason
 
 let clear_proxy_state ~__context vif proxy =
-    let site = Db.PVS_proxy.get_site ~__context ~self:proxy in
-    State.remove_proxy ~__context site vif;
     Db.PVS_proxy.set_currently_attached ~__context ~self:proxy ~value:false;
     Db.PVS_proxy.set_status ~__context ~self:proxy ~value:`stopped
 
