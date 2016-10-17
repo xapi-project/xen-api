e3e8a8d0050d6265f8d9000cfbcd062b2a79dbf0
Christian Lindig <christian.lindig@citrix.com>
2016-10-17 16:34:29 +0100

    CA-225711 enable Pool.redo_log when default_sr is shared.
    This fell out of XOP-765. Note that the SR is considered shared when
    SR.shared is true. This indicates the capability of being shared, not
    necessarily the current state of sharing.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/xapi/xapi_pool.ml
+++ ocaml/xapi/xapi_pool.ml
@@ -1863,4 +1863,7 @@ let remove_from_guest_agent_config ~__context ~self ~key =
   Xapi_pool_helpers.apply_guest_agent_config ~__context
 
 let set_default_sr ~__context ~self ~sr =
-  Db.Pool.set_default_SR ~__context ~self ~value:sr
+  Db.Pool.set_default_SR ~__context ~self ~value:sr;
+  if Db.SR.get_shared ~__context ~self:sr then begin
+    enable_redo_log ~__context ~sr (* CA-225711 *)
+  end
