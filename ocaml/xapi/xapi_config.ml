5a4151fa67cdf2b9cae8ab6de090b0232838cde2
Jon Ludlam <jonathan.ludlam@citrix.com>
2016-11-23 16:32:39 +0000

    Switch to newer ocaml-rpc library
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- ocaml/xapi/xapi_config.ml
+++ ocaml/xapi/xapi_config.ml
@@ -25,11 +25,11 @@ let log_if_not_empty format_string value =
 
 let dump_config () =
   debug "Server configuration:";
-  log_if_not_empty "product_version: %s" (Version.product_version ());
-  log_if_not_empty "product_brand: %s" (Version.product_brand ());
-  debug "platform_version: %s" (Version.platform_version ());
-  debug "platform_name: %s" (Version.platform_name ());
-  debug "build_number: %s" (Version.build_number ());
-  debug "git changeset: %s" Version.git_id;
+  log_if_not_empty "product_version: %s" (Xapi_version.product_version ());
+  log_if_not_empty "product_brand: %s" (Xapi_version.product_brand ());
+  debug "platform_version: %s" (Xapi_version.platform_version ());
+  debug "platform_name: %s" (Xapi_version.platform_name ());
+  debug "build_number: %s" (Xapi_version.build_number ());
+  debug "git changeset: %s" Xapi_version.git_id;
   debug "version: %d.%d" version_major version_minor;
   (* debug "License filename: %s" !License_file.filename *)
