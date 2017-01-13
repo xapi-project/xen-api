f2007f0de3806892bc3b58f74a4fdb08469ac5d7
Sharad Yadav <sharad.yadav@citrix.com>
2017-01-13 08:04:24 +0000

    CP-14033: Add Falcon datamodel schema
    Signed-off-by: Sharad Yadav <sharad.yadav@citrix.com>
    (cherry picked from commit d479882cd4b27f63b09360ec955baf77387f303c)
    
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -84,9 +84,12 @@ let dundee_release_schema_minor_vsn = 93
 let ely_release_schema_major_vsn = 5
 let ely_release_schema_minor_vsn = 100
 
+let falcon_release_schema_major_vsn = 5
+let falcon_release_schema_minor_vsn = 101
+
 (* the schema vsn of the last release: used to determine whether we can upgrade or not.. *)
-let last_release_schema_major_vsn = dundee_release_schema_major_vsn
-let last_release_schema_minor_vsn = dundee_release_schema_minor_vsn
+let last_release_schema_major_vsn = ely_release_schema_major_vsn
+let last_release_schema_minor_vsn = ely_release_schema_minor_vsn
 
 (* List of tech-preview releases. Fields in these releases are not guaranteed to be retained when
  * upgrading to a full release. *)
