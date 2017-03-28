60431b6be334c833cb8b142a79cddbf3824968cc
Jon Ludlam <jonathan.ludlam@citrix.com>
2017-03-28 17:23:18 +0100

    Update to newest xcp.updates interface
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- ocaml/xapi/storage_task.ml
+++ ocaml/xapi/storage_task.ml
@@ -6,16 +6,16 @@ open D
 module Storage_task = Task_server.Task(Storage_interface)
 module Updates = Updates.Updates(Storage_interface)
 
-let updates = Updates.empty ()
+let scheduler = Scheduler.make ()
+let updates = Updates.empty scheduler
 let tasks = Storage_task.empty ()
 
-let signal task =
+let signal id =
   let open Storage_task in
-  Stdext.Threadext.Mutex.execute tasks.m
-    (fun () ->
-       if exists_locked tasks task then begin
-         debug "TASK.signal %s = %s" task ((find_locked tasks task).state |> Task.rpc_of_state |> Jsonrpc.to_string);
-         Updates.add (Dynamic.Task task) updates;
-       end else debug "TASK.signal %s (object deleted)" task
-    )
-
+  try
+    let handle = handle_of_id tasks id in
+    let state = get_state handle in
+    debug "TASK.signal %s = %s" id (state |> Task.rpc_of_state |> Jsonrpc.to_string);
+     Updates.add (Dynamic.Task id) updates
+  with
+    Does_not_exist _ -> debug "TASK.signal %s (object deleted)" id
--- ocaml/xapi/test_xapi_xenops.ml
+++ ocaml/xapi/test_xapi_xenops.ml
@@ -39,7 +39,6 @@ let setup_simulator () =
     Xenops_utils.set_fs_backend (Some (module Xenops_utils.MemFS: Xenops_utils.FS));
     Xenops_server.register_objects ();
     Xenops_server.set_backend (Some (module Xenops_server_simulator: Xenops_server_plugin.S));
-    Scheduler.start ();
     Xenops_server.WorkerPool.start 16;
     Xcp_client.use_switch := true;
     Xapi_xenops_queue.queue_override := ["simulator",Xenopsd.rpc_fn];
