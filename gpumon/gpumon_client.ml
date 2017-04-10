84c7e11a5b7284b699c70ad6ec2d162cfecf98dc
Frederico Mazzone <frederico.mazzone@citrix.com>
2017-04-10 17:15:55 +0100

    Add gpumon interface
    Commit 1 of 2 for new gpumon interface.
    
    Signed-off-by: Frederico Mazzone <frederico.mazzone@citrix.com>

---
+++ gpumon/gpumon_client.ml
@@ -0,0 +1,30 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open Gpumon_interface
+open Xcp_client
+
+let xml_url () = "file:" ^ xml_path
+
+module Client = Gpumon_interface.Client(struct
+	let rpc call =
+		if !use_switch
+		then json_switch_rpc queue_name call
+		else xml_http_rpc
+                        ~srcstr:(get_user_agent ())
+                        ~dststr:"gpumon"
+                        xml_url
+                        call
+end)
+
---
+++ gpumon/gpumon_interface.ml
@@ -0,0 +1,35 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+let service_name = "gpumon"
+let queue_name = Xcp_service.common_prefix ^ service_name
+let xml_path = "/var/xapi/" ^ service_name
+
+type debug_info = string
+
+type pgpu_address = string
+type pgpu_metadata = string
+type domid = int
+
+type incompatibility_reason = Host_driver | Guest_driver | GPU | Other
+type compatibility = Compatible | Incompatible of incompatibility_reason list
+
+(** Get the metadata for a pGPU, given its address (PCI bus ID). *)
+external get_pgpu_metadata: debug_info -> pgpu_address -> pgpu_metadata = ""
+
+(** Check compatibility between a VM's vGPU(s) and another pGPU.
+  * pgpu_address = PCI bus ID of the pGPU in which the VM is currently running.
+  * domid = domain ID of the VM in which the vGPU(s) is running.
+  * pgpu_metadata = metadata of the pGPU to check compatibility for. *)
+external get_pgpu_vm_compatibility: debug_info -> pgpu_address -> domid -> pgpu_metadata -> compatibility = ""
