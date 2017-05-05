8a0a6661187a2eb604928be4dbe703a72c93c520
Gabor Igloi <gabor.igloi@citrix.com>
2017-05-05 14:25:26 +0100

    Travis: Add OPAM build method and coverage
    Because our depext package names aren't compatible with older Ubuntus,
    we have to use a newer Ubuntu container for the OPAM build.
    
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

---
+++ .travis-xenserver-build-env.sh
@@ -0,0 +1,19 @@
+# SUMMARY:
+# Builds and tests xapi using xenserver-build-env, which installs the
+# dependencies as RPMs.
+
+set -uex
+
+wget https://raw.githubusercontent.com/xenserver/xenserver-build-env/master/utils/travis-build-repo.sh
+
+# only run deploy.sh when the build succeeds
+env \
+  CONTAINER_NAME=build-env \
+  OCAMLRUNPARAM=b \
+  REPO_PACKAGE_NAME=xapi \
+  REPO_CONFIGURE_CMD=./configure \
+  REPO_BUILD_CMD=make \
+  REPO_TEST_CMD='make test' \
+  REPO_DOC_CMD='make doc-json' \
+  bash travis-build-repo.sh && \
+  ( ( test $TRAVIS_PULL_REQUEST == "false" && test $TRAVIS_BRANCH == "master" && bash deploy.sh ) || true )
