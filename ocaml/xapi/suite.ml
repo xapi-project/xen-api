f8bd8e6e8600394999581b8696e9cc63e96748d2
Sharad Yadav <sharad.yadav@citrix.com>
2017-05-11 11:57:58 +0100

    CP-22202: Add unit tests for VLANs
    Signed-off-by: Sharad Yadav <sharad.yadav@citrix.com>

--- ocaml/xapi/suite.ml
+++ ocaml/xapi/suite.ml
@@ -60,6 +60,7 @@ let base_suite =
     Test_sdn_controller.test;
     Test_event.test;
     Test_extauth_plugin_ADpbis.test;
+    Test_vlan.test;
   ]
 
 let handlers = [
---
+++ ocaml/xapi/test_vlan.ml
@@ -0,0 +1,179 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open OUnit
+open Test_common
+
+let test_pool_introduce () =
+  let __context = make_test_database () in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let tagged_PIF = make_pif ~__context ~network ~host () in
+  let tag = 3201L in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let untagged_PIF = make_pif ~__context ~network:vlan_network ~host ~vLAN:tag () in
+  let vlan = Xapi_vlan.pool_introduce ~__context
+    ~tagged_PIF ~untagged_PIF ~tag ~other_config:[]
+  in
+  assert_equal vlan (Db.PIF.get_VLAN_master_of ~__context ~self:untagged_PIF);
+  assert_equal tagged_PIF (Db.VLAN.get_tagged_PIF ~__context ~self:vlan);
+  assert_equal untagged_PIF (Db.VLAN.get_untagged_PIF ~__context ~self:vlan);
+  assert_equal tag (Db.VLAN.get_tag ~__context ~self:vlan)
+
+let test_create_internal () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let device = "eth0" in
+  let host = make_host ~__context () in
+  let network = make_network ~__context ~bridge:"xapi0" () in
+  let tagged_PIF = make_pif ~__context ~network ~host () in
+  let vlan, untagged_PIF = Xapi_vlan.create_internal ~__context
+    ~host ~tagged_PIF ~tag ~network ~device
+  in
+  assert_equal vlan (Db.PIF.get_VLAN_master_of ~__context ~self:untagged_PIF);
+  assert_equal tagged_PIF (Db.VLAN.get_tagged_PIF ~__context ~self:vlan);
+  assert_equal untagged_PIF (Db.VLAN.get_untagged_PIF ~__context ~self:vlan);
+  assert_equal tag (Db.VLAN.get_tag ~__context ~self:vlan);
+  assert_equal network (Db.PIF.get_network ~__context ~self:untagged_PIF);
+  assert_equal device (Db.PIF.get_device ~__context ~self:untagged_PIF);
+  assert_equal host (Db.PIF.get_host ~__context ~self:untagged_PIF);
+  assert_equal tag (Db.PIF.get_VLAN ~__context ~self:untagged_PIF)
+
+let test_create_unmanaged_pif () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let tagged_PIF = make_pif ~__context ~network ~host ~managed:false () in
+  assert_raises_api_error
+    Api_errors.pif_unmanaged
+    ~args:[Ref.string_of tagged_PIF]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:vlan_network)
+
+let test_create_network_already_connected () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let tagged_PIF = make_pif ~__context ~network ~host () in
+  assert_raises_api_error
+    Api_errors.network_already_connected
+    ~args:[Ref.string_of host; Ref.string_of tagged_PIF]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:network)
+
+let test_create_pif_not_a_bond_slave () =
+  let __context = make_test_database () in
+  let dummy_bond = Ref.make () in
+  let tag = 3201L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let tagged_PIF = make_pif ~__context ~network ~host ~bond_slave_of:dummy_bond () in
+  Db.PIF.set_bond_slave_of ~__context ~self:tagged_PIF ~value:dummy_bond;
+  assert_raises_api_error
+    Api_errors.cannot_add_vlan_to_bond_slave
+    ~args:[Ref.string_of tagged_PIF]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:vlan_network)
+
+let test_create_pif_not_vlan_slave () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let tagged_PIF = make_pif ~__context ~network ~host ~vLAN:0L () in
+  assert_raises_api_error
+    Api_errors.pif_is_vlan
+    ~args:[Ref.string_of tagged_PIF]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:vlan_network)
+
+let test_create_invalid_tag () =
+  let __context = make_test_database () in
+  let tag = -1L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let tagged_PIF = make_pif ~__context ~network ~host () in
+  assert_raises_api_error
+    Api_errors.vlan_tag_invalid
+    ~args:[Int64.to_string tag]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:vlan_network);
+  let new_tag = 4095L in
+  assert_raises_api_error
+    Api_errors.vlan_tag_invalid
+    ~args:[Int64.to_string new_tag]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag:new_tag ~network:vlan_network)
+
+let test_create_vlan_already_exists () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let device = "eth0" in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let tagged_PIF = make_pif ~__context ~network ~host () in
+  let _ = Xapi_vlan.create_internal ~__context
+    ~host ~tagged_PIF ~tag ~network:vlan_network ~device in
+  let new_vlan_network = make_network ~__context ~bridge:"xapi1" () in
+  assert_raises_api_error
+    Api_errors.pif_vlan_exists
+    ~args:[device]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:new_vlan_network)
+
+let test_create_pif_has_tunnel_access () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let tunnel_network = make_network ~__context ~bridge:"xapi0" () in
+  let vlan_network = make_network ~__context ~bridge:"xapi1" () in
+  let transport_PIF = make_pif ~__context ~network ~host () in
+  let _, tagged_PIF = Xapi_tunnel.create_internal ~__context ~transport_PIF ~network:tunnel_network ~host in
+  assert_raises_api_error
+    Api_errors.is_tunnel_access_pif
+    ~args:[Ref.string_of tagged_PIF]
+    (fun () -> Xapi_vlan.create ~__context ~tagged_PIF ~tag ~network:vlan_network)
+
+let test_gc_vlan () =
+  let __context = make_test_database () in
+  let tag = 3201L in
+  let host = make_host ~__context () in
+  let network = make_network ~__context () in
+  let tagged_PIF = make_pif ~__context ~network ~host () in
+  let vlan_network = make_network ~__context ~bridge:"xapi0" () in
+  let untagged_PIF = make_pif ~__context ~network:vlan_network ~host ~vLAN:tag () in
+  let vlan = Xapi_vlan.pool_introduce ~__context
+    ~tagged_PIF ~untagged_PIF ~tag ~other_config:[]
+  in
+  assert_equal vlan (Db.PIF.get_VLAN_master_of ~__context ~self:untagged_PIF);
+  Db.PIF.set_host ~__context ~self:untagged_PIF ~value:Ref.null;
+  Db.PIF.set_network ~__context ~self:untagged_PIF ~value:Ref.null;
+  Db_gc.gc_PIFs ~__context;
+  assert_equal false (Db.is_valid_ref __context vlan)
+
+let test =
+  "test_vlan" >:::
+  [
+    "test_pool_introduce" >:: test_pool_introduce;
+    "test_create_internal" >:: test_create_internal;
+    "test_create_unmanged_pif" >:: test_create_unmanaged_pif;
+    "test_create_network_already_connected" >:: test_create_network_already_connected;
+    "test_create_pif_not_a_bond_slave" >:: test_create_pif_not_a_bond_slave;
+    "test_create_pif_not_vlan_slave" >:: test_create_pif_not_vlan_slave;
+    "test_create_invalid_tag" >:: test_create_invalid_tag;
+    "test_create_vlan_already_exists" >:: test_create_vlan_already_exists;
+    "test_create_pif_has_tunnel_access" >:: test_create_pif_has_tunnel_access;
+    "test_gc_vlan" >:: test_gc_vlan
+  ]
