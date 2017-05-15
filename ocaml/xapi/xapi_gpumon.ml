1e616f9268fc88e06e3ddebad09b45b51f226e53
Marcello Seri <marcello.seri@citrix.com>
2017-05-15 15:15:15 +0100

    [CP-20682] xapi_gpumon: update compatibility checking assertion
    We need to use the encoded_`pgpu_metadata` from the Db in place of the
    pgpu reference: this allows checking the compatibility against PGPUs from
    other pools. In addition we need to add the destination host to the
    parameters for the error messages metadata.
    
    Also we fix a spelling typo in the original function name.
    
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- ocaml/xapi/xapi_gpumon.ml
+++ ocaml/xapi/xapi_gpumon.ml
@@ -50,20 +50,10 @@ module Nvidia = struct
     in [key, metadata]
 
   (* N.B. the vgpu (and the vm) must be in the local host where this function runs *)
-  let assert_pgpu_is_compatibile_with_vm ~__context ~vm ~vgpu ~pgpu =
+  let assert_pgpu_is_compatible_with_vm ~__context ~vm ~vgpu ~dest_host ~encoded_pgpu_metadata =
     let dbg = Context.string_of_task __context in
     let vm_domid = Int64.to_int (Db.VM.get_domid ~__context ~self:vm) in
-    let pgpu_metadata () =
-      try
-        Db.PGPU.get_compatibility_metadata ~__context ~self:pgpu
-        |> List.assoc key
-        |> Stdext.Base64.decode
-      with
-      | Not_found ->
-          debug "Key %s is missing from the compatibility_metadata for pgpu %s" key (Ref.string_of pgpu);
-          let host = Db.PGPU.get_host ~__context ~self:pgpu in
-          raise Api_errors.(Server_error (nvidia_tools_error, [Ref.string_of host]))
-    in
+    let pgpu_metadata = Stdext.Base64.decode encoded_pgpu_metadata in
     let vgpu_impl = 
       vgpu
       |> (fun self -> Db.VGPU.get_type ~__context ~self)
@@ -81,7 +71,7 @@ module Nvidia = struct
       let compatibility = 
         try
           Gpumon_client.Client.Nvidia.get_pgpu_vm_compatibility dbg 
-            local_pgpu_address vm_domid (pgpu_metadata ())
+            local_pgpu_address vm_domid pgpu_metadata
         with
         | Gpumon_interface.NvmlInterfaceNotAvailable ->
           let host = Db.VM.get_resident_on ~__context ~self:vm in
@@ -91,15 +81,14 @@ module Nvidia = struct
       let open Gpumon_interface in
       match compatibility with
       | Compatible -> 
-        info "VM %s Nvidia vGPU is compatible with the destination pGPU %s"
-          (Ref.string_of vm) (Ref.string_of pgpu)
+        info "VM %s Nvidia vGPU is compatible with the destination pGPU on host %s"
+          (Ref.string_of vm) (Ref.string_of dest_host)
       | Incompatible reasons -> 
-        let host = Db.PGPU.get_host ~__context ~self:pgpu in
         raise Api_errors.(Server_error (
             vgpu_destination_incompatible,
             [ if List.mem Host_driver reasons then "host-driver" else "unknown"
             ; Ref.string_of vgpu
-            ; Ref.string_of host
+            ; Ref.string_of dest_host
             ]))
 
 end (* Nvidia *)
