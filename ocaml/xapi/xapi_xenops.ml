5b2b3269bb2fa7be96d84ee513c333e69e0be2e3
Marcello Seri <marcello.seri@citrix.com>
2017-06-07 10:58:14 +0100

    CP-22597: xapi_xenops: add other_config parameter introduced in idl
    This is used only to pass the vgpu_map during a cross pool migration,
    the mechanism is the same used for the vif_map and vdi_map
    
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -569,6 +569,7 @@ module MD = struct
       position = int_of_string vgpu.Db_actions.vGPU_device;
       physical_pci_address;
       implementation;
+      other_config = [];
     }
 
   let of_gvt_g_vgpu ~__context vm vgpu =
@@ -603,6 +604,7 @@ module MD = struct
         position = int_of_string vgpu.Db_actions.vGPU_device;
         physical_pci_address;
         implementation;
+        other_config = [];
       }
     with
     | Not_found -> failwith "Intel GVT-g settings not specified"
@@ -631,6 +633,7 @@ module MD = struct
         position = int_of_string vgpu.Db_actions.vGPU_device;
         physical_pci_address;
         implementation;
+        other_config = [];
       }
     with
     | Not_found -> failwith "AMD MxGPU settings not specified"
@@ -656,6 +659,7 @@ module MD = struct
             Xenops_interface.Pci.address_of_string
               (List.assoc Vm_platform.vgpu_pci_id vm.API.vM_platform);
           implementation;
+          other_config = [];
         }]
     end else
       List.fold_left
