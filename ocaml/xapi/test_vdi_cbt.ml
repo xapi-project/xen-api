1ce20a5628e893b860c23d1777409e6778ad3e7e
Gabor Igloi <gabor.igloi@citrix.com>
2017-07-10 13:41:30 +0100

    VDI.snapshot,clone: test that VDI.cbt_enabled is correctly set
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/test_vdi_cbt.ml
+++ ocaml/xapi/test_vdi_cbt.ml
@@ -5,7 +5,7 @@ let register_smapiv2_server (module S: Storage_interface.Server_impl with type c
   let dummy_query_result = Storage_interface.({ driver=""; name=""; description=""; vendor=""; copyright=""; version=""; required_api_version=""; features=[]; configuration=[]; required_cluster_stack=[] }) in
   Storage_mux.register sr_ref rpc "" dummy_query_result
 
-let override_cbt_functions ?vdi_enable_cbt ?vdi_disable_cbt () =
+let make_smapiv2_storage_server ?vdi_enable_cbt ?vdi_disable_cbt ?vdi_snapshot ?vdi_clone () =
   let default a b = match a with
     | Some a -> a
     | None -> b
@@ -16,11 +16,13 @@ let override_cbt_functions ?vdi_enable_cbt ?vdi_disable_cbt () =
       include Storage_skeleton.VDI
       let enable_cbt = default vdi_enable_cbt Storage_skeleton.VDI.enable_cbt
       let disable_cbt = default vdi_disable_cbt Storage_skeleton.VDI.disable_cbt
+      let snapshot = default vdi_snapshot Storage_skeleton.VDI.snapshot
+      let clone = default vdi_snapshot Storage_skeleton.VDI.clone
     end
   end : Storage_interface.Server_impl with type context = unit)
 
-let register_cbt_server ?vdi_enable_cbt ?vdi_disable_cbt sr_ref =
-  let s = override_cbt_functions ?vdi_enable_cbt ?vdi_disable_cbt () in
+let register_smapiv2_server ?vdi_enable_cbt ?vdi_disable_cbt ?vdi_snapshot ?vdi_clone sr_ref =
+  let s = make_smapiv2_storage_server ?vdi_enable_cbt ?vdi_disable_cbt ?vdi_snapshot ?vdi_clone () in
   register_smapiv2_server s sr_ref
 
 let test_cbt_enable_disable () =
@@ -34,7 +36,7 @@ let test_cbt_enable_disable () =
 
   let enable_cbt_params = ref None in
   let disable_cbt_params = ref None in
-  register_cbt_server
+  register_smapiv2_server
     ~vdi_enable_cbt:(fun _ ~dbg ~sr ~vdi -> enable_cbt_params := Some (sr, vdi))
     ~vdi_disable_cbt:(fun _ ~dbg ~sr ~vdi -> disable_cbt_params := Some (sr, vdi))
     sr_uuid;
@@ -53,6 +55,7 @@ let test_cbt_enable_disable () =
   Xapi_vdi.disable_cbt ~__context ~self:vdi_ref;
   assert_vdi_cbt_enabled_is false "VDI.disable_cbt should be idempotent"
 
+
 let test_set_metadata_of_pool_doesnt_allow_cbt_metadata_vdi () =
   let __context = Test_common.make_test_database () in
   let self = Test_common.make_vdi ~__context ~_type:`cbt_metadata () in
@@ -62,9 +65,39 @@ let test_set_metadata_of_pool_doesnt_allow_cbt_metadata_vdi () =
     Api_errors.(Server_error (vdi_incompatible_type, [Ref.string_of self; Record_util.vdi_type_to_string `cbt_metadata]))
     (fun () -> Xapi_vdi.set_metadata_of_pool ~__context ~self ~value:pool)
 
+let test_clone_and_snapshot_correctly_sets_cbt_enabled_field () =
+  let __context = Test_common.make_test_database () in
+  let host = Helpers.get_localhost ~__context in
+  let assert_cbt_enabled_field_is ~vdi ~value ~msg =
+    OUnit.assert_equal ~msg value (Db.VDI.get_cbt_enabled ~__context ~self:vdi) in
+
+  let sR = Test_common.make_sr ~__context () in
+  let _: _ API.Ref.t = Test_common.make_pbd ~__context ~host ~sR () in
+  let vdi =
+    let uuid = Test_common.make_uuid () in
+    Test_common.make_vdi ~__context ~sR ~uuid ~managed:true ()
+  in
+  let stub _ ~dbg ~sr ~vdi_info =
+    let uuid = Test_common.make_uuid () in
+    Storage_interface.{ default_vdi_info with vdi = uuid }
+  in
+  register_smapiv2_server ~vdi_snapshot:stub ~vdi_clone:stub (Db.SR.get_uuid ~__context ~self:sR);
+
+  let snapshot = Xapi_vdi.snapshot ~__context ~vdi ~driver_params:[] in
+  assert_cbt_enabled_field_is ~vdi:snapshot ~value:false ~msg:"The cbt_enabled property should be inherited from the snapshotted VDI - it should be false for the snapshot of a VDI with CBT disabled.";
+  let clone = Xapi_vdi.clone ~__context ~vdi ~driver_params:[] in
+  assert_cbt_enabled_field_is ~vdi:clone ~value:false ~msg:"CBT should always be disabled on the VDI created by VDI.clone";
+
+  Db.VDI.set_cbt_enabled ~__context ~self:vdi ~value:true;
+  let snapshot = Xapi_vdi.snapshot ~__context ~vdi ~driver_params:[] in
+  assert_cbt_enabled_field_is ~vdi:snapshot ~value:true ~msg:"The cbt_enabled property should be inherited from the snapshotted VDI - it should be true for the snapshot of a VDI with CBT enabled.";
+  let clone = Xapi_vdi.clone ~__context ~vdi ~driver_params:[] in
+  assert_cbt_enabled_field_is ~vdi:clone ~value:false ~msg:"CBT should always be disabled on the VDI created by VDI.clone"
+
 let test =
   let open OUnit in
   "test_vdi_cbt" >:::
   [ "test_cbt_enable_disable" >:: test_cbt_enable_disable
   ; "test_set_metadata_of_pool_doesnt_allow_cbt_metadata_vdi" >:: test_set_metadata_of_pool_doesnt_allow_cbt_metadata_vdi
+  ; "test_clone_and_snapshot_correctly_sets_cbt_enabled_field" >:: test_clone_and_snapshot_correctly_sets_cbt_enabled_field
   ]
