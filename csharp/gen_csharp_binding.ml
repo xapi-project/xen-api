8f830a736f0c437049d09e50a2831aec27d04dd9
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2017-08-07 13:09:15 +0100

    Moved long hardcoded text to a mustache template.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- csharp/gen_csharp_binding.ml
+++ csharp/gen_csharp_binding.ml
@@ -133,7 +133,6 @@ let rec main() =
   List.iter (fun x -> if generated x then gen_class_file x) classes;
   TypeSet.iter gen_enum !enums;
   gen_maps();
-  gen_i18n_errors();
   gen_http_actions();
   gen_relations();
   let sorted_members = List.sort String.compare !api_members in
@@ -1318,159 +1317,24 @@ and is_static_readonly field =
   | DynamicRO    -> "false"
   | _            -> "false"
 
-and i18n_header out_chan =
-  let print format = fprintf out_chan format in
-  print
-    "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<root>
-  <!--
-    Microsoft ResX Schema
-
-    Version 2.0
-
-    The primary goals of this format is to allow a simple XML format
-    that is mostly human readable. The generation and parsing of the
-    various data types are done through the TypeConverter classes
-    associated with the data types.
-
-    Example:
-
-    ... ado.net/XML headers & schema ...
-    <resheader name=\"resmimetype\">text/microsoft-resx</resheader>
-    <resheader name=\"version\">2.0</resheader>
-    <resheader name=\"reader\">System.Resources.ResXResourceReader, System.Windows.Forms, ...</resheader>
-    <resheader name=\"writer\">System.Resources.ResXResourceWriter, System.Windows.Forms, ...</resheader>
-    <data name=\"Name1\"><value>this is my long string</value><comment>this is a comment</comment></data>
-    <data name=\"Color1\" type=\"System.Drawing.Color, System.Drawing\">Blue</data>
-    <data name=\"Bitmap1\" mimetype=\"application/x-microsoft.net.object.binary.base64\">
-        <value>[base64 mime encoded serialized .NET Framework object]</value>
-    </data>
-    <data name=\"Icon1\" type=\"System.Drawing.Icon, System.Drawing\" mimetype=\"application/x-microsoft.net.object.bytearray.base64\">
-        <value>[base64 mime encoded string representing a byte array form of the .NET Framework object]</value>
-        <comment>This is a comment</comment>
-    </data>
-
-    There are any number of \"resheader\" rows that contain simple
-    name/value pairs.
-
-    Each data row contains a name, and value. The row also contains a
-    type or mimetype. Type corresponds to a .NET class that support
-    text/value conversion through the TypeConverter architecture.
-    Classes that don't support this are serialized and stored with the
-    mimetype set.
-
-    The mimetype is used for serialized objects, and tells the
-    ResXResourceReader how to depersist the object. This is currently not
-    extensible. For a given mimetype the value must be set accordingly:
-
-    Note - application/x-microsoft.net.object.binary.base64 is the format
-    that the ResXResourceWriter will generate, however the reader can
-    read any of the formats listed below.
-
-    mimetype: application/x-microsoft.net.object.binary.base64
-    value   : The object must be serialized with
-            : System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
-            : and then encoded with base64 encoding.
-
-    mimetype: application/x-microsoft.net.object.soap.base64
-    value   : The object must be serialized with
-            : System.Runtime.Serialization.Formatters.Soap.SoapFormatter
-            : and then encoded with base64 encoding.
-
-    mimetype: application/x-microsoft.net.object.bytearray.base64
-    value   : The object must be serialized into a byte array
-            : using a System.ComponentModel.TypeConverter
-            : and then encoded with base64 encoding.
-    -->
-  <xsd:schema id=\"root\" xmlns=\"\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:msdata=\"urn:schemas-microsoft-com:xml-msdata\">
-    <xsd:import namespace=\"http://www.w3.org/XML/1998/namespace\" />
-    <xsd:element name=\"root\" msdata:IsDataSet=\"true\">
-      <xsd:complexType>
-        <xsd:choice maxOccurs=\"unbounded\">
-          <xsd:element name=\"metadata\">
-            <xsd:complexType>
-              <xsd:sequence>
-                <xsd:element name=\"value\" type=\"xsd:string\" minOccurs=\"0\" />
-              </xsd:sequence>
-              <xsd:attribute name=\"name\" use=\"required\" type=\"xsd:string\" />
-              <xsd:attribute name=\"type\" type=\"xsd:string\" />
-              <xsd:attribute name=\"mimetype\" type=\"xsd:string\" />
-              <xsd:attribute ref=\"xml:space\" />
-            </xsd:complexType>
-          </xsd:element>
-          <xsd:element name=\"assembly\">
-            <xsd:complexType>
-              <xsd:attribute name=\"alias\" type=\"xsd:string\" />
-              <xsd:attribute name=\"name\" type=\"xsd:string\" />
-            </xsd:complexType>
-          </xsd:element>
-          <xsd:element name=\"data\">
-            <xsd:complexType>
-              <xsd:sequence>
-                <xsd:element name=\"value\" type=\"xsd:string\" minOccurs=\"0\" msdata:Ordinal=\"1\" />
-                <xsd:element name=\"comment\" type=\"xsd:string\" minOccurs=\"0\" msdata:Ordinal=\"2\" />
-              </xsd:sequence>
-              <xsd:attribute name=\"name\" type=\"xsd:string\" use=\"required\" msdata:Ordinal=\"1\" />
-              <xsd:attribute name=\"type\" type=\"xsd:string\" msdata:Ordinal=\"3\" />
-              <xsd:attribute name=\"mimetype\" type=\"xsd:string\" msdata:Ordinal=\"4\" />
-              <xsd:attribute ref=\"xml:space\" />
-            </xsd:complexType>
-          </xsd:element>
-          <xsd:element name=\"resheader\">
-            <xsd:complexType>
-              <xsd:sequence>
-                <xsd:element name=\"value\" type=\"xsd:string\" minOccurs=\"0\" msdata:Ordinal=\"1\" />
-              </xsd:sequence>
-              <xsd:attribute name=\"name\" type=\"xsd:string\" use=\"required\" />
-            </xsd:complexType>
-          </xsd:element>
-        </xsd:choice>
-      </xsd:complexType>
-    </xsd:element>
-  </xsd:schema>
-  <resheader name=\"resmimetype\">
-    <value>text/microsoft-resx</value>
-  </resheader>
-  <resheader name=\"version\">
-    <value>2.0</value>
-  </resheader>
-  <resheader name=\"reader\">
-    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
-  </resheader>
-  <resheader name=\"writer\">
-    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
-  </resheader>
-"
-and i18n_footer out_chan =
-  let print format = fprintf out_chan format in
-  print
-    "</root>\n"
 and gen_i18n_errors () =
   Friendly_error_names.parse_sr_xml sr_xml;
   Friendly_error_names.parse_resx resx_file;
-  let out_chan = open_out (Filename.concat destdir "FriendlyErrorNames.resx")
+  let errors = Friendly_error_names.friendly_names_all Datamodel.errors in
+  let json = `O [
+    "i18n_errors", `A (List.map (fun (x, y) ->
+      `O [
+        "i18n_error_key", `String x;
+        "i18n_error_description", `String y;
+      ];) errors);
+    ]
   in
-  finally (fun () ->
-      i18n_header out_chan;
-      List.iter (gen_i18n_error_field out_chan)
-        (Friendly_error_names.friendly_names_all Datamodel.errors);
-      i18n_footer out_chan)
-    (fun () -> close_out out_chan)
-
-
-and gen_i18n_error_field out_chan (error, desc) =
-  let print format = fprintf out_chan format in
-  (* Note that we can't use Xml.to_string for the whole block, because
-     we need the output to be whitespace-identical to what Visual Studio
-     would produce.  We need to use it for the inner <value> though, to
-     get the escaping right. *)
-  print "  <data name=\"%s\" xml:space=\"preserve\">\n    %s\n  </data>\n"
-    error
-    (Xml.to_string (Xml.Element("value", [], [(Xml.PCData desc)])))
+  render_file ("FriendlyErrorNames.mustache", "FriendlyErrorNames.resx") json templdir destdir
 
 let populate_releases ()=
   render_file ("ApiVersion.mustache", "ApiVersion.cs") json_releases templdir destdir
 
 let _ =
   main();
+  gen_i18n_errors();
   populate_releases()
