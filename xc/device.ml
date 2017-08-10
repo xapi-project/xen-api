1713c397d31890ac627db25630beff674a53e4c8
Liang Dai <liang.dai1@citrix.com>
2017-08-10 19:25:37 +0800

    CP-23007: Use /dev/fdset/<fdset-id> rather than /dev/fd/<fd> to change CD
    kernel clones the fd when receives file descriptor using SCM_RIGHTS, so
    it's safe to close fd without it being closed in QEMU.
    
    Opening file in /dev/fd make the kernel reevaluate permissions. we must
    avoid that and /dev/fdset is designed specially for this purpose (see
    qemu_open implementation)
    
    Also need to release fdset after changing CD.
    
    Signed-off-by: Liang Dai <liang.dai1@citrix.com>

--- xc/device.ml
+++ xc/device.ml
@@ -609,29 +609,33 @@ let qemu_media_change ~xs device _type params =
 	then begin
 		let open Qmp in
 		let cd = "ide1-cd1" in
-		match params with
-		| "" ->
+		if params = "" then
 			let qmp_cmd = Command(None, Eject (cd, Some true)) in
 			qmp_write device.frontend.domid qmp_cmd
-		| _ ->
+		else
 			try
 				let c = Qmp_protocol.connect (Printf.sprintf "/var/run/xen/qmp-libxl-%d" device.frontend.domid) in
-				let fd_of_c = Qmp_protocol.to_fd c in
-				let fd_of_cd = Unix.openfile params [ Unix.O_RDONLY ] 0o640 in
 				finally
-					(fun () -> ignore(Fd_send_recv.send_fd fd_of_c " " 0 1 [] fd_of_cd))
-					(fun () -> Unix.close fd_of_cd);
-				
-				let qmp_cmd = Command (None, Add_fd (Fd_send_recv.int_of_fd fd_of_cd)) in
-				Qmp_protocol.negotiate c;
-				Qmp_protocol.write c qmp_cmd;
-				let new_fd = match Qmp_protocol.read c with
-				| Success (None, Fd_info fd_info) -> fd_info.fd
-				| _ -> raise (Internal_error (Printf.sprintf "Get unexpected result after sending Qmp message: %s" (string_of_message qmp_cmd)))
-				in
-				let cmd = Command (None, Blockdev_change_medium (cd, "/dev/fd/" ^ (string_of_int new_fd))) in
-				Qmp_protocol.write c cmd;
-				Qmp_protocol.close c
+					(fun () ->
+						let fd_of_c = Qmp_protocol.to_fd c in
+						let fd_of_cd = Unix.openfile params [ Unix.O_RDONLY ] 0o640 in
+						finally
+							(fun () -> ignore(Fd_send_recv.send_fd fd_of_c " " 0 1 [] fd_of_cd))
+							(fun () -> Unix.close fd_of_cd);
+
+						let qmp_cmd = Command (None, Add_fd None) in
+						Qmp_protocol.negotiate c;
+						Qmp_protocol.write c qmp_cmd;
+						let fd_info = match Qmp_protocol.read c with
+							| Success (None, Fd_info x) -> x
+							| _ -> raise (Internal_error (Printf.sprintf "Get unexpected result after sending Qmp message: %s" (string_of_message qmp_cmd)))
+						in
+						finally
+							(fun () ->
+								Qmp_protocol.write c (Command (None, Blockdev_change_medium (cd, "/dev/fdset/" ^ (string_of_int fd_info.fdset_id)))))
+							(fun () ->
+								Qmp_protocol.write c (Command (None, Remove_fd fd_info.fdset_id))))
+					(fun () -> Qmp_protocol.close c)
 			with
 			| Unix.Unix_error(Unix.ECONNREFUSED, "connect", p) -> raise(Internal_error (Printf.sprintf "Failed to connnect qmp socket: %s" p))
 			| Unix.Unix_error(Unix.ENOENT, "open", p) -> raise(Internal_error (Printf.sprintf "Failed to open CD Image: %s" p))
