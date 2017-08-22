3919bf44c9d27cb25e05fdb74697602614faca35
Marcello Seri <marcello.seri@citrix.com>
2017-08-22 17:00:23 +0100

    Removed unused rewrite test executable
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- lib_test/rewrite.ml
+++
@@ -1,7 +0,0 @@
-(* Read in an RRD, write it out again *)
-(* This is to test input/output code *)
-
-let _ =
-  let rrd = Rrd_unix.of_file Sys.argv.(1) in
-  Rrd_unix.to_file rrd (Printf.sprintf "%s.new" Sys.argv.(1))
-
