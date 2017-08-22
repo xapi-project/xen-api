3046dcc520b63a498625030958920f53b3166d8c
Marcello Seri <marcello.seri@citrix.com>
2017-08-22 18:00:11 +0100

    Moved text_export in rrddump, the only place where it is used
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- unix/rrd_unix.ml
+++ unix/rrd_unix.ml
@@ -124,34 +124,3 @@ let to_fd ?(json=false) rrd fd = (if json then json_to_fd else xml_to_fd) rrd fd
 
 let to_file ?(json=false) rrd filename =
   atomic_write_to_file filename 0o644 (to_fd ~json rrd)
-
-(** WARNING WARNING: Do not call the following function from within xapi! *)
-let text_export rrd _grouping =
-  let open Rrd in
-  for rra_i=0 to Array.length rrd.rrd_rras - 1 do
-    let rra = rrd.rrd_rras.(rra_i) in
-    let start = rrd.last_updated -. (Int64.to_float (Int64.mul (Int64.of_int (rra.rra_pdp_cnt * rra.rra_row_cnt)) rrd.timestep)) in
-    Printf.printf "start=%f\n" start;
-    let rra_timestep = (Int64.mul rrd.timestep (Int64.of_int rra.rra_pdp_cnt)) in
-
-    (* Get the last and first times of the CDPs to be returned *)
-    let (last_cdp_time,_age) = get_times rrd.last_updated rra_timestep in
-
-    let time i = Int64.sub (last_cdp_time) (Int64.mul (Int64.of_int i) rra_timestep) in
-
-    for j=0 to Array.length rrd.rrd_dss - 1 do
-      Printf.printf "Doing ds: %s\n" rrd.rrd_dss.(j).ds_name;
-      let oc = open_out (Printf.sprintf "rrd_data_%s_%s_%Ld.dat" rrd.rrd_dss.(j).ds_name (cf_type_to_string rra.rra_cf) (Int64.mul (Int64.of_int (rra.rra_pdp_cnt* rra.rra_row_cnt)) rrd.timestep)) in
-      let rec do_data i accum =
-        if (time i < Int64.of_float start) || (i >= rra.rra_row_cnt) then (List.rev accum) else
-          do_data (i+1) ((time i, Fring.peek rra.rra_data.(j)  i)::accum)
-      in
-      let data = do_data 0 [] in
-      List.iter (fun (t,d) -> if not (Utils.isnan d) then Printf.fprintf oc "%Ld %f\n" t d) data;
-      close_out oc
-    done
-  done
-
-
-
-
