fc5958be35501c2d74eb63d575c3c3895eab0ccc
Akanksha Mathur <akanksha.mathur@citrix.com>
2017-09-15 09:38:26 +0100

    Add BVT for unused calls getting and modifying other_config fields of different classes
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

---
+++ ocaml/xapi/quicktest_bv_tests.ml
@@ -0,0 +1,94 @@
+open Quicktest_common
+
+let start session_id rpc =
+
+  (* helper functions for API/PBD calls *)
+  let with_api fn = fn ~rpc ~session_id in
+  let with_obj fn obj = with_api fn ~self:obj in
+
+  (* with_obj generalises to most classes, however, for some classes
+   * (SR,Host,VDI,etc), a mix of label ~self and ~sr,~vdi etc is used *)
+
+let get_set_config (class_name, obj, gt, st, permission) =
+    let conf_t = make_test ("Testing " ^ class_name ^ " 'get,set_other_config'") 4 in
+    try
+      (* set config to arbitrary (key,value) *)
+      start conf_t;
+      let dummy_value = [("dummy_key","dummy_val")] in
+      with_obj st obj ~value:dummy_value; (* set config value *)
+
+      (* read-writeable configs succeed if the value is set, read-only fails *)
+      if dummy_value = (with_obj gt obj)
+      then match permission with
+        | `RW -> success conf_t
+        | `RO -> failed conf_t "Error: config read-only, should throw PERMISSION_DENIED"
+      else failed conf_t "set_config failed";
+
+      (* read-only config returns success test if and only if it throws PERMISSION_DENIED *)
+    with
+      | (Failure hd) -> failed conf_t (Printf.sprintf "Error: could not find %s object" class_name)
+      | Api_errors.Server_error (x,_) when x = Api_errors.permission_denied && permission =`RO -> success conf_t;
+      | e -> failed conf_t (ExnHelper.string_of_exn e); in
+
+
+  (* check add_to and remove_from_other_config *)
+  let add_rm_other_config (class_name, obj, gt, add, rm)  =
+    let adrm_t = make_test ("Testing " ^ class_name ^ " 'add_to/remove_from_other_config'") 4 in
+    try
+      start adrm_t;
+
+      (* add ("key","value") to other_config *)
+      let key,value = "key","value" in
+      with_obj add obj ~key ~value;
+
+      (* remove from other config *)
+      if List.mem (key,value) (with_obj gt obj)
+      then with_obj rm obj ~key (* may need ignore *)
+      else failed adrm_t "Error: add_to_other_config failed";
+
+      (* test fails if (key,value) has not been removed *)
+      if List.mem (key,value) (with_obj gt obj)
+      then failed adrm_t "Error: remove_from_other_config failed"
+      else success adrm_t;
+
+    with
+      | (Failure hd) -> failed adrm_t (Printf.sprintf "Error: could not find %s object" class_name)
+      | e -> failed adrm_t (ExnHelper.string_of_exn e); in
+
+
+  (* main function to run test suite *)
+  let run_tests () =
+    let test_str_indent = 2 in
+    print_newline ();
+    let test = make_test "Begin basic verification test\n" test_str_indent in
+    try
+      start test;
+      (* for each class, must pass an object to the test *)
+      let obj class_name gt_all =
+        try
+          List.hd (with_api gt_all)
+        with _ -> failwith ("failed to acquire " ^ class_name ^ " object") in
+
+      begin
+        (* iterate test for other_config getters and setters over different classes *)
+        debug test "Testing get and set_other_config";
+        List.iter (fun (class_name, gt_all, gt, st, permission) ->
+          get_set_config (class_name, obj class_name gt_all, gt, st, permission))
+          Quicktest_bv_calls.get_set_config_calls_lst;
+        debug test "Finished testing get and set_other_config\n";
+
+        (* iterate test for other_config modifiers over different classes *)
+        debug test "Testing add_to and remove_from_other_config\n";
+        List.iter (fun (class_name, gt_all, gt, add, rm) ->
+          add_rm_other_config (class_name, obj class_name gt_all, gt, add, rm))
+          Quicktest_bv_calls.add_rm_config_calls_lst;
+        debug test "Finished testing add_to and remove_from_other_config\n";
+
+      end;
+
+      print_endline ((String.make test_str_indent ' ') ^ "Finished basic verification test\n");
+      success test;
+
+    with
+    | e -> failed test (ExnHelper.string_of_exn e);
+  in run_tests () ;
