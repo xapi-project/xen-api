db14913530d9b8c8a595a6877e32949bd6a9a07e
Taoyong Ding <taoyong.ding@citrix.com>
2017-09-18 06:13:15 -0700

    CP-24611: Add pusb scan function
    1. Add yojson lib, as pusb scan will get json format data by call
    usb_scan script
    2. Add pusb scan function
       a. scan will be called when xapi start or uevent.
    
    Signed-off-by: Taoyong Ding <taoyong.ding@citrix.com>

--- ocaml/xapi/xapi.ml
+++ ocaml/xapi/xapi.ml
@@ -919,6 +919,7 @@ let server_init() =
           "Synchronising tunnels on slave with master", [Startup.OnlySlave; Startup.NoExnRaising], Sync_networking.copy_tunnels_from_master ~__context;
 
           "SR scanning", [ Startup.OnlyMaster; Startup.OnThread ], Xapi_sr.scanning_thread;
+          "PUSB scanning", [], (fun () -> Xapi_pusb.scan_thread ~__context);
           "Updating pool cpu_info", [], (fun () -> Create_misc.create_pool_cpuinfo ~__context);
           "writing init complete", [], (fun () -> Helpers.touch_file !Xapi_globs.init_complete);
           (*      "Synchronising HA state with Pool", [ Startup.NoExnRaising ], Xapi_ha.synchronise_ha_state_with_pool; *)
--- ocaml/xapi/xapi_pusb.ml
+++ ocaml/xapi/xapi_pusb.ml
@@ -15,10 +15,10 @@
 open Stdext
 open Listext
 open Threadext
+open Xapi_pusb_helpers
 module D = Debug.Make(struct let name="xapi" end)
 open D
 
-let mutex = Mutex.create ()
 let create ~__context ~uSB_group ~host ~other_config ~path
       ~vendor_id ~vendor_desc ~product_id ~product_desc ~serial ~version ~description =
   let pusb = Ref.make () and uuid = Uuid.make_uuid () in
@@ -29,6 +29,62 @@ let create ~__context ~uSB_group ~host ~other_config ~path
   debug "PUSB ref='%s' created" (Ref.string_of pusb);
   pusb
 
+let scan_start ~__context usbs =
+  let host = Helpers.get_localhost ~__context in
+  let known_usb =
+    Db.PUSB.get_all_records ~__context
+    |> List.filter (fun (rf, rc) -> rc.API.pUSB_host = host)
+    |> get_known_usb
+  in
+  let local_usb = get_local_usb usbs in
+  (* Create the newly added pusbs *)
+  USBSet.iter (fun s -> let self = create ~__context ~uSB_group:(Ref.null) ~host ~other_config:[] ~path:s.USB.path ~vendor_id:s.USB.vendor_id
+                   ~vendor_desc:s.USB.vendor_desc ~product_id:s.USB.product_id ~product_desc:s.USB.product_desc ~serial:s.USB.serial
+                   ~version:s.USB.version ~description:s.USB.description in
+               let group = Xapi_pusb_helpers.find_or_create ~__context self in
+               Db.PUSB.set_USB_group ~__context ~self ~value:group
+  ) (USBSet.diff local_usb known_usb);
+
+  List.filter (fun (rf, rc) -> USBSet.mem (extract_known_usb_info rc) (USBSet.diff known_usb local_usb)) (Db.PUSB.get_all_records ~__context)
+  |> List.iter (fun (self, _) ->
+                  let usb_group = Db.PUSB.get_USB_group ~__context ~self in
+                  Db.PUSB.destroy ~__context ~self;
+                  let vusbs = Db.USB_group.get_VUSBs ~__context ~self:usb_group in
+                  List.iter (fun vusb -> Db.VUSB.destroy ~__context ~self:vusb) vusbs;
+                  Db.USB_group.destroy ~__context ~self:usb_group)
+
+let cond = Condition.create ()
+let mutex = Mutex.create ()
+let scan_required : bool ref = ref false
+
+let start_thread f =
+  ignore(Thread.create (fun () ->
+      while true do
+        Mutex.execute mutex (fun () ->
+            while not !scan_required do
+              Condition.wait cond mutex
+            done;
+            (* scan_required is true now. *)
+            scan_required := false;
+          );
+        try
+          f ();
+        with e ->
+          Printf.printf "Caught exception from scan_start '%s' \n%!" (Printexc.to_string e);
+          ()
+      done) ())
 
 let scan ~__context =
-  debug "pusb scan to do"
+  (* notify that scan is required. *)
+  Mutex.execute mutex (fun () ->
+      scan_required := true;
+      Condition.broadcast cond
+    )
+
+let scan_thread ~__context =
+  let f () =
+    let usbs = get_script_stdout () |> get_usbs in
+    scan_start ~__context usbs
+  in
+  start_thread f;
+  scan ~__context
