b512874fcc12c045b855b5bc17c2ba766b235afd
Marcello Seri <marcello.seri@citrix.com>
2017-09-19 10:45:50 +0100

    Update xenops VDA update function to not destroy VDA when the key is removed
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -1621,15 +1621,16 @@ let update_vm ~__context id =
                 let maybe_old_version = Opt.map (fun s -> s.vda_version) previous in
                 match state.vda_version, maybe_old_version with
                 | Some version, None ->
+                debug "xenopsd event: Detected VDA for VM %s with version %s" id version;
                   Xapi_vda.create ~__context ~vm:self ~version
                   |> ignore
                 | None, Some _ ->
-                  Xapi_vda.find_vda ~__context ~vm:self 
-                  |> Opt.iter (fun self -> Xapi_vda.destroy ~__context ~self)
+                  debug "xenopsd event: VDA for VM %s disappeared" id
                 | Some version, Some _ ->
+                debug "xenopsd event: Detected updated VDA version %s for VM %s" version id;
                   Xapi_vda.find_vda ~__context ~vm:self 
                   |> Opt.iter (fun self -> Db.VDA.set_version ~__context ~self ~value:version)
-                | None, None -> ()
+                | None, None -> (* This should not happen *) ()
               ) info
             with e ->
               error "Caught %s: while updating VDA version for VM %s" (Printexc.to_string e) id
