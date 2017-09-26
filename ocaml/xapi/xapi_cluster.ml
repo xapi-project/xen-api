208cc7fb461475e50e1c5fd5f4e60b16d5ff8540
Stefano Panella <stefano.panella@citrix.com>
2017-09-26 13:34:44 +0000

    CP-24678 CP-24679: Implement Cluster.create and Cluster_host.create
    Signed-off-by: Frederico Mazzone <frederico.mazzone@citrix.com>
    Signed-off-by: Stefano Panella <stefano.panella@citrix.com>

--- ocaml/xapi/xapi_cluster.ml
+++ ocaml/xapi/xapi_cluster.ml
@@ -17,21 +17,38 @@
 
 module D=Debug.Make(struct let name="xapi_cluster" end)
 open D
+open Cluster_interface
 
-let create ~__context ~network (* ~cluster_token *) ~cluster_stack ~pool_auto_join =
+let assert_cluster_can_be_created ~__context =
+  if Db.Cluster.get_all ~__context <> [] then
+    failwith "Cluster cannot be created because it already exists"
+
+let create ~__context ~network ~cluster_stack ~pool_auto_join =
+  assert_cluster_can_be_created ~__context;
   let cluster_ref = Ref.make () in
   let cluster_host_ref = Ref.make () in
   let cluster_uuid = Uuidm.to_string (Uuidm.create `V4) in
   let cluster_host_uuid = Uuidm.to_string (Uuidm.create `V4) in
+  (* For now we assume we have only one pool
+     TODO: get master ref explicitely passed in as parameter*)
   let pool = Db.Pool.get_all ~__context |> List.hd in
   let host = Db.Pool.get_master ~__context ~self:pool in
-  (* let cluster_token = xapi-clusterd.local.create ip in *)
-  Db.Cluster.create ~__context ~ref:cluster_ref ~uuid:cluster_uuid ~network ~cluster_token:cluster_uuid ~cluster_stack
-    ~pool_auto_join ~current_operations:[] ~allowed_operations:[] ~cluster_config:[]
-    ~other_config:[];
-  Db.Cluster_host.create ~__context ~ref:cluster_host_ref ~uuid:cluster_host_uuid ~cluster:cluster_ref ~host ~enabled:false
-    ~current_operations:[] ~allowed_operations:[] ~other_config:[];
-  cluster_ref
+
+  let ip = Xapi_cluster_host.ip_of_host ~__context ~network ~host in
+
+  (* TODO: locking*)
+  let result = Cluster_client.LocalClient.create (Cluster_client.rpc (fun () -> "")) ip in
+  match result with
+  | Result.Ok cluster_token ->
+    D.debug "Got OK from LocalClient.create";
+    Db.Cluster.create ~__context ~ref:cluster_ref ~uuid:cluster_uuid ~network ~cluster_token ~cluster_stack
+      ~pool_auto_join ~current_operations:[] ~allowed_operations:[] ~cluster_config:[]
+      ~other_config:[];
+    Db.Cluster_host.create ~__context ~ref:cluster_host_ref ~uuid:cluster_host_uuid ~cluster:cluster_ref ~host ~enabled:true
+      ~current_operations:[] ~allowed_operations:[] ~other_config:[];
+    D.debug "Created Cluster: %s and Cluster_host: %s" (Ref.string_of cluster_ref) (Ref.string_of cluster_host_ref);
+    cluster_ref
+  | Result.Error error -> Xapi_cluster_host.handle_error error
 
 let destroy ~__context ~self =
   (* TODO: call xapi-clusterd.Local.destroy *)
@@ -42,11 +59,14 @@ let pool_create ~__context ~pool ~cluster_stack ~network =
   let master = Db.Pool.get_master ~__context ~self:pool in
   let hosts = Db.Host.get_all ~__context in
 
-  (*let cluster_token = Uuidm.to_string (Uuidm.create `V4) in*)
-  let cluster = create ~__context ~network (* ~cluster_token *) ~cluster_stack:"corosync" ~pool_auto_join:true in
+  let cluster = create ~__context ~network ~cluster_stack:"corosync" ~pool_auto_join:true in
 
   List.iter (fun host ->
-    if master <> host then
-      Xapi_cluster_host.create ~__context ~cluster ~host |> ignore) hosts;
+      if master <> host then
+        (* We need to run this code to the slave *)
+        Helpers.call_api_functions ~__context (fun rpc session_id ->
+            let cluster_host_ref = Client.Client.Cluster_host.create ~rpc ~session_id ~cluster ~host in
+            D.debug "Created Cluster_host: %s" (Ref.string_of cluster_host_ref);
+          )) hosts;
 
   cluster
--- ocaml/xapi/xapi_cluster_host.ml
+++ ocaml/xapi/xapi_cluster_host.ml
@@ -17,14 +17,51 @@
 
 module D=Debug.Make(struct let name="xapi_cluster_host" end)
 open D
+open Cluster_interface
+
+let ip_of_host ~__context ~network ~host =
+  debug "Looking up PIF for network %s" (Ref.string_of network);
+  let pifs = Db.PIF.get_records_where ~__context
+      ~expr:Db_filter_types.(And (Eq(Literal (Ref.string_of host),Field "host"),
+                                  Eq(Literal (Ref.string_of network),Field "network"))) in
+  match pifs with
+  | [(ref, record)] ->
+    let ip = record.API.pIF_IP in
+    if ip = "" then failwith (Printf.sprintf "PIF %s does not have any IP" (Ref.string_of ref));
+    if not record.API.pIF_disallow_unplug then failwith (Printf.sprintf "PIF %s allows unplug" (Ref.string_of ref));
+    debug "Got IP %s for host %s" ip (Ref.string_of host);
+    Cluster_interface.IPv4 ip
+  | _ ->
+    let msg = Printf.sprintf "No PIF found for host:%s and network:%s" (Ref.string_of host) (Ref.string_of network) in
+    debug "%s" msg;
+    failwith msg
+
+let handle_error error =
+  match error with
+  | InternalError message -> failwith ("Internal Error: " ^ message)
+  | Unix_error message -> failwith ("Unix Error: " ^ message)
+
+let assert_cluster_host_can_be_created ~__context ~host =
+  if Db.Cluster_host.get_refs_where ~__context
+      ~expr:Db_filter_types.(Eq(Literal (Ref.string_of host),Field "host")) <> [] then
+    failwith "Cluster cannot be created because it already exists"
 
 let create ~__context ~cluster ~host =
   let ref = Ref.make () in
   let uuid = Uuidm.to_string (Uuidm.create `V4) in
-  (* call xapi-clusterd.join*)
-  Db.Cluster_host.create ~__context ~ref ~uuid ~cluster ~host ~enabled:false
-    ~current_operations:[] ~allowed_operations:[] ~other_config:[];
-  ref
+  let network = Db.Cluster.get_network ~__context ~self:cluster in
+  let cluster_token = Db.Cluster.get_cluster_token ~__context ~self:cluster in
+  let ip = ip_of_host ~__context ~network ~host in
+  let ip_list = List.map (fun cluster_host ->
+      ip_of_host ~__context ~network ~host:(Db.Cluster_host.get_host ~__context ~self:cluster_host)
+    ) (Db.Cluster.get_cluster_hosts ~__context ~self:cluster) in
+  let result = Cluster_client.LocalClient.join (Cluster_client.rpc (fun () -> "")) cluster_token ip ip_list in
+  match result with
+  | Result.Ok () ->
+    Db.Cluster_host.create ~__context ~ref ~uuid ~cluster ~host ~enabled:false
+      ~current_operations:[] ~allowed_operations:[] ~other_config:[];
+    ref
+  | Result.Error error -> handle_error error
 
 let destroy ~__context ~self =
   Db.Cluster_host.destroy ~__context ~self
