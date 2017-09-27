6faa5fb7f9ac8474086a8c6706073c1c0afcd9a3
Taoyong Ding <taoyong.ding@citrix.com>
2017-09-27 04:08:50 -0700

    CP-24611: Add pusb scan unit test
    Signed-off-by: Taoyong Ding <taoyong.ding@citrix.com>

---
+++ ocaml/xapi/test_pusb.ml
@@ -0,0 +1,59 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open OUnit
+open Test_common
+
+let create_base_environment () =
+  let __context = make_test_database () in
+  let pusb = make_sr ~__context () in
+  __context, pusb
+
+let start_thread ~__context info =
+  let usbs = Xapi_pusb_helpers.get_usbs info in
+  let f () = Xapi_pusb.scan_start ~__context usbs in
+  Xapi_pusb.start_thread f
+
+let test_scan_with_usb_add_and_remove () =
+  let __context = make_test_database () in
+  let test_pusb = "[{
+                    \"product-desc\": \"\",
+                    \"product-id\": \"5591\",
+                    \"description\": \"SanDisk Corp._4C530001131223117342\",
+                    \"vendor-desc\": \"SanDisk Corp.\",
+                    \"version\": \"3.00\",
+                    \"vendor-id\": \"0781\",
+                    \"path\": \"2-2\",
+                    \"serial\": \"4C530001131223117342\"
+                }]"
+  in
+  (* add usb*)
+  start_thread ~__context test_pusb;
+  Xapi_pusb.scan ~__context;
+  Thread.delay 1.0;
+  (* delete PUSB from DB*)
+  List.iter (fun (self, _) ->
+                  let usb_group = Db.PUSB.get_USB_group ~__context ~self in
+                  Db.PUSB.destroy ~__context ~self;
+                  Db.USB_group.destroy ~__context ~self:usb_group) (Db.PUSB.get_all_records ~__context);
+
+  Xapi_pusb.scan ~__context;
+  Thread.delay 1.0;
+  assert_equal 1 (List.length (Db.PUSB.get_all_records ~__context))
+
+let test =
+  "test_pusb" >:::
+  [
+	"test_scan_with_usb_add_and_remove" >:: test_scan_with_usb_add_and_remove;
+  ]
