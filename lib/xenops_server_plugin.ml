xapi-project/xenopsd

CP-20602: vGPU migration: add DEMU framing to the new stream
Also start DEMU on the receiver. This will need to get and fd for the stream
passed to it in a follow-up commit.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

lib/xenops_server_plugin.ml
@@ -84,6 +84,9 @@ module type S = sig
 		val save: Xenops_task.t -> progress_cb -> Vm.t -> flag list -> data -> unit
 		val restore: Xenops_task.t -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> string list -> unit
 
+		val save_vgpu: Xenops_task.t -> Vm.t -> Vgpu.t -> data -> unit
+		val restore_vgpu: Xenops_task.t -> Vm.t -> Vgpu.t -> data -> unit
+
 		val s3suspend: Xenops_task.t -> Vm.t -> unit
 		val s3resume: Xenops_task.t -> Vm.t -> unit
 

xc/device.ml
@@ -1825,6 +1825,9 @@ let stop ~xs ~qemu_domid domid  =
     stop_vgpu ();
     stop_qemu ()
 
+let restore_vgpu (task: Xenops_task.t) ~xs restore_fd domid vgpu vcpus =
+	start_vgpu ~xs task domid [vgpu] vcpus
+
 end (* End of module Dm *)
 
 let get_vnc_port ~xs domid = 

xc/device.mli
@@ -224,6 +224,7 @@ sig
 	val suspend : Xenops_task.t -> xs:Xenstore.Xs.xsh -> qemu_domid:int -> Xenctrl.domid -> unit
 	val resume : Xenops_task.t -> xs:Xenstore.Xs.xsh -> qemu_domid:int -> Xenctrl.domid -> unit
 	val stop : xs:Xenstore.Xs.xsh -> qemu_domid:int -> Xenctrl.domid -> unit
+	val restore_vgpu : Xenops_task.t -> xs:Xenstore.Xs.xsh -> Unix.file_descr -> Xenctrl.domid  -> Xenops_interface.Vgpu.t -> int -> unit
 end
 
 val get_vnc_port : xs:Xenstore.Xs.xsh -> Xenctrl.domid -> int option

xc/domain.ml
@@ -1047,6 +1047,23 @@ let restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_incr_g
 	            ~static_max_kib:info.memory_max ~target_kib:info.memory_target ~vcpus:info.vcpus ~extras
 	            xenguest_path domid fd
 
+let restore_vgpu (task: Xenops_task.t) ~xc ~xs domid fd vgpu vcpus =
+	let uuid = get_uuid ~xc domid in
+	debug "VM = %s; domid = %d; restore_vgpu_setup" (Uuid.to_string uuid) domid;
+	let open Suspend_image in let open Suspend_image.M in
+	let res =
+		read_header fd >>= function
+		| Demu, _ ->
+			debug "Read Demu header";
+			Device.Dm.restore_vgpu task ~xs fd domid vgpu vcpus;
+			return ()
+		| _ -> failwith "Unsupported"
+	in
+	match res with
+	| `Ok () ->
+		debug "VM = %s; domid = %d; restore_vgpu_setup complete" (Uuid.to_string uuid) domid
+	| `Error e -> raise e
+
 type suspend_flag = Live | Debug
 
 let write_libxc_record (task: Xenops_task.t) ~xc ~xs ~hvm xenguest_path domid uuid fd flags progress_callback qemu_domid do_suspend_callback =
@@ -1192,6 +1209,18 @@ let suspend (task: Xenops_task.t) ~xc ~xs ~hvm xenguest_path vm_str domid fd fla
 	in match res with
 	| `Ok () ->
 		debug "VM = %s; domid = %d; suspend complete" (Uuid.to_string uuid) domid
+	| `Error e -> raise e
+
+let suspend_vgpu (task: Xenops_task.t) ~xc ~xs domid fd =
+	let uuid = get_uuid ~xc domid in
+	debug "VM = %s; domid = %d; suspend_vgpu_setup" (Uuid.to_string uuid) domid;
+	let open Suspend_image in let open Suspend_image.M in
+	let res =
+		debug "Writing DEMU header";
+		write_header fd (Demu, 0L)
+	in match res with
+	| `Ok () ->
+		debug "VM = %s; domid = %d; suspend_vgpu_setup complete" (Uuid.to_string uuid) domid
 	| `Error e -> raise e
 
 let send_s3resume ~xc domid =

xc/domain.mli
@@ -151,6 +151,9 @@ val hvm_restore: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore
 (** Restore a domain using the info provided *)
 val restore: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> timeoffset:string -> extras:string list -> build_info -> string -> domid -> Unix.file_descr -> unit
 
+val restore_vgpu: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh ->
+	domid -> Unix.file_descr -> Xenops_interface.Vgpu.t -> int -> unit
+
 type suspend_flag = Live | Debug
 
 (** suspend a domain into the file descriptor *)
@@ -159,6 +162,9 @@ val suspend: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.
           -> ?progress_callback: (float -> unit)
 		  -> qemu_domid: int
           -> (unit -> unit) -> unit
+
+val suspend_vgpu: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh ->
+	domid -> Unix.file_descr -> unit
 
 (** send a s3resume event to a domain *)
 val send_s3resume: xc: Xenctrl.handle -> domid -> unit

xc/xenops_server_xen.ml
@@ -1573,6 +1573,35 @@ module VM = struct
                                 ) (fun () -> clean_memory_reservation task di.Xenctrl.domid)
 			) Newest task vm
 
+	let save_vgpu task vm vgpu data =
+		on_domain
+			(fun xc xs (task:Xenops_task.t) vm di ->
+				let domid = di.Xenctrl.domid in
+				with_data ~xc ~xs task data true
+					(fun fd ->
+						Domain.suspend_vgpu task ~xc ~xs domid fd
+					)
+			) Oldest task vm
+
+	let restore_vgpu task vm vgpu data =
+		on_domain
+			(fun xc xs (task:Xenops_task.t) vm di ->
+				let domid = di.Xenctrl.domid in
+				let k = vm.Vm.id in
+				let vmextra = DB.read_exn k in
+				let vcpus = match vmextra.VmExtra.persistent with
+					| { VmExtra.build_info = None } ->
+						error "VM = %s; No stored build_info: cannot safely restore" vm.Vm.id;
+						raise (Does_not_exist("build_info", vm.Vm.id))
+					| { VmExtra.build_info = Some build_info } ->
+						build_info.Domain.vcpus
+				in
+				with_data ~xc ~xs task data true
+					(fun fd ->
+						Domain.restore_vgpu task ~xc ~xs domid fd vgpu vcpus
+					)
+			) Newest task vm
+
 	let s3suspend =
 		(* XXX: TODO: monitor the guest's response; track the s3 state *)
 		on_domain

xapi-project/xen-api

lib/xenops_server_plugin.ml
@@ -84,6 +84,9 @@ module type S = sig
 		val save: Xenops_task.task_handle -> progress_cb -> Vm.t -> flag list -> data -> unit
 		val restore: Xenops_task.task_handle -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> string list -> unit
 
+		val save_vgpu: Xenops_task.task_handle -> Vm.t -> Vgpu.t -> data -> unit
+		val restore_vgpu: Xenops_task.task_handle -> Vm.t -> Vgpu.t -> data -> unit
+
 		val s3suspend: Xenops_task.task_handle -> Vm.t -> unit
 		val s3resume: Xenops_task.task_handle -> Vm.t -> unit
 

xc/device.ml
@@ -1824,6 +1824,9 @@ let stop ~xs ~qemu_domid domid  =
     stop_vgpu ();
     stop_qemu ()
 
+let restore_vgpu (task: Xenops_task.task_handle) ~xs restore_fd domid vgpu vcpus =
+	start_vgpu ~xs task domid [vgpu] vcpus
+
 end (* End of module Dm *)
 
 let get_vnc_port ~xs domid = 

xc/device.mli
@@ -224,6 +224,7 @@ sig
 	val suspend : Xenops_task.task_handle -> xs:Xenstore.Xs.xsh -> qemu_domid:int -> Xenctrl.domid -> unit
 	val resume : Xenops_task.task_handle -> xs:Xenstore.Xs.xsh -> qemu_domid:int -> Xenctrl.domid -> unit
 	val stop : xs:Xenstore.Xs.xsh -> qemu_domid:int -> Xenctrl.domid -> unit
+	val restore_vgpu : Xenops_task.task_handle -> xs:Xenstore.Xs.xsh -> Unix.file_descr -> Xenctrl.domid  -> Xenops_interface.Vgpu.t -> int -> unit
 end
 
 val get_vnc_port : xs:Xenstore.Xs.xsh -> Xenctrl.domid -> int option

xc/domain.ml
@@ -1049,6 +1049,23 @@ let restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid 
 	            ~static_max_kib:info.memory_max ~target_kib:info.memory_target ~vcpus:info.vcpus ~extras
 	            xenguest_path domid fd
 
+let restore_vgpu (task: Xenops_task.task_handle) ~xc ~xs domid fd vgpu vcpus =
+	let uuid = get_uuid ~xc domid in
+	debug "VM = %s; domid = %d; restore_vgpu_setup" (Uuid.to_string uuid) domid;
+	let open Suspend_image in let open Suspend_image.M in
+	let res =
+		read_header fd >>= function
+		| Demu, _ ->
+			debug "Read Demu header";
+			Device.Dm.restore_vgpu task ~xs fd domid vgpu vcpus;
+			return ()
+		| _ -> failwith "Unsupported"
+	in
+	match res with
+	| `Ok () ->
+		debug "VM = %s; domid = %d; restore_vgpu_setup complete" (Uuid.to_string uuid) domid
+	| `Error e -> raise e
+
 type suspend_flag = Live | Debug
 
 let write_libxc_record (task: Xenops_task.task_handle) ~xc ~xs ~hvm xenguest_path domid uuid fd flags progress_callback qemu_domid do_suspend_callback =
@@ -1195,6 +1212,18 @@ let suspend (task: Xenops_task.task_handle) ~xc ~xs ~hvm xenguest_path vm_str do
 	in match res with
 	| `Ok () ->
 		debug "VM = %s; domid = %d; suspend complete" (Uuid.to_string uuid) domid
+	| `Error e -> raise e
+
+let suspend_vgpu (task: Xenops_task.task_handle) ~xc ~xs domid fd =
+	let uuid = get_uuid ~xc domid in
+	debug "VM = %s; domid = %d; suspend_vgpu_setup" (Uuid.to_string uuid) domid;
+	let open Suspend_image in let open Suspend_image.M in
+	let res =
+		debug "Writing DEMU header";
+		write_header fd (Demu, 0L)
+	in match res with
+	| `Ok () ->
+		debug "VM = %s; domid = %d; suspend_vgpu_setup complete" (Uuid.to_string uuid) domid
 	| `Error e -> raise e
 
 let send_s3resume ~xc domid =

xc/domain.mli
@@ -151,6 +151,9 @@ val hvm_restore: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs
 (** Restore a domain using the info provided *)
 val restore: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> timeoffset:string -> extras:string list -> build_info -> string -> domid -> Unix.file_descr -> unit
 
+val restore_vgpu: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh ->
+	domid -> Unix.file_descr -> Xenops_interface.Vgpu.t -> int -> unit
+
 type suspend_flag = Live | Debug
 
 (** suspend a domain into the file descriptor *)
@@ -159,6 +162,9 @@ val suspend: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xe
           -> ?progress_callback: (float -> unit)
 		  -> qemu_domid: int
           -> (unit -> unit) -> unit
+
+val suspend_vgpu: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh ->
+	domid -> Unix.file_descr -> unit
 
 (** send a s3resume event to a domain *)
 val send_s3resume: xc: Xenctrl.handle -> domid -> unit

xc/xenops_server_xen.ml
@@ -1612,6 +1612,35 @@ module VM = struct
 					) (fun () -> clean_memory_reservation task di.Xenctrl.domid)
 			) Newest task vm
 
+	let save_vgpu task vm vgpu data =
+		on_domain
+			(fun xc xs (task:Xenops_task.task_handle) vm di ->
+				let domid = di.Xenctrl.domid in
+				with_data ~xc ~xs task data true
+					(fun fd ->
+						Domain.suspend_vgpu task ~xc ~xs domid fd
+					)
+			) Oldest task vm
+
+	let restore_vgpu task vm vgpu data =
+		on_domain
+			(fun xc xs (task:Xenops_task.task_handle) vm di ->
+				let domid = di.Xenctrl.domid in
+				let k = vm.Vm.id in
+				let vmextra = DB.read_exn k in
+				let vcpus = match vmextra.VmExtra.persistent with
+					| { VmExtra.build_info = None } ->
+						error "VM = %s; No stored build_info: cannot safely restore" vm.Vm.id;
+						raise (Does_not_exist("build_info", vm.Vm.id))
+					| { VmExtra.build_info = Some build_info } ->
+						build_info.Domain.vcpus
+				in
+				with_data ~xc ~xs task data true
+					(fun fd ->
+						Domain.restore_vgpu task ~xc ~xs domid fd vgpu vcpus
+					)
+			) Newest task vm
+
 	let s3suspend =
 		(* XXX: TODO: monitor the guest's response; track the s3 state *)
 		on_domain