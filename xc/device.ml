xapi-project/xenopsd

CP-20596: When starting DEMU, check if it is already running
Starting, resuming and migrating (receiving) a VM all use the same code path to
setup the device models after building the domain. This change allows us to
start DEMU early if desired, which we will do for migrations.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

xc/device.ml
@@ -1682,16 +1682,19 @@ let __start (task: Xenops_task.t) ~xs ~dmpath ?(timeout = !Xenopsd.qemu_dm_ready
 	let () = match info.disp with
 	| VNC (Vgpu [{implementation = Nvidia vgpu}], _, _, _, _)
 	| SDL (Vgpu [{implementation = Nvidia vgpu}], _) -> begin
-		(* The below line does nothing if the device is already bound to the
-		 * nvidia driver. We rely on xapi to refrain from attempting to run
-		 * a vGPU on a device which is passed through to a guest. *)
-		PCI.bind [vgpu.physical_pci_address] PCI.Nvidia;
-		let args = vgpu_args_of_nvidia domid info.vcpus vgpu in
-		let ready_path = Printf.sprintf "/local/domain/%d/vgpu-pid" domid in
-		let cancel = Cancel_utils.Vgpu domid in
-		let vgpu_pid = init_daemon ~task ~path:!Xc_path.vgpu ~args
-			~name:"vgpu" ~domid ~xs ~ready_path ~timeout:!Xenopsd.vgpu_ready_timeout ~cancel () in
-		Forkhelpers.dontwaitpid vgpu_pid
+		if not (Vgpu.is_running ~xs domid) then begin
+			(* The below line does nothing if the device is already bound to the
+			 * nvidia driver. We rely on xapi to refrain from attempting to run
+			 * a vGPU on a device which is passed through to a guest. *)
+			PCI.bind [vgpu.physical_pci_address] PCI.Nvidia;
+			let args = vgpu_args_of_nvidia domid info.vcpus vgpu in
+			let ready_path = Printf.sprintf "/local/domain/%d/vgpu-pid" domid in
+			let cancel = Cancel_utils.Vgpu domid in
+			let vgpu_pid = init_daemon ~task ~path:!Xc_path.vgpu ~args
+				~name:"vgpu" ~domid ~xs ~ready_path ~timeout:!Xenopsd.vgpu_ready_timeout ~cancel () in
+			Forkhelpers.dontwaitpid vgpu_pid
+		end else
+			D.info "Daemon %s is already running for domain %d" !Xc_path.vgpu domid
 	end
 	| VNC (Vgpu [{implementation = GVT_g vgpu}], _, _, _, _)
 	| SDL (Vgpu [{implementation = GVT_g vgpu}], _) ->

xapi-project/xen-api

xc/device.ml
@@ -1726,16 +1726,19 @@ let start_vgpu ~xs task domid vgpus vcpus =
 	let open Xenops_interface.Vgpu in
 	match vgpus with
 	| [{implementation = Nvidia vgpu}] ->
-		(* The below line does nothing if the device is already bound to the
-		 * nvidia driver. We rely on xapi to refrain from attempting to run
-		 * a vGPU on a device which is passed through to a guest. *)
-		PCI.bind [vgpu.physical_pci_address] PCI.Nvidia;
-		let args = vgpu_args_of_nvidia domid vcpus vgpu in
-		let ready_path = Printf.sprintf "/local/domain/%d/vgpu-pid" domid in
-		let cancel = Cancel_utils.Vgpu domid in
-		let vgpu_pid = init_daemon ~task ~path:!Xc_resources.vgpu ~args
-			~name:"vgpu" ~domid ~xs ~ready_path ~timeout:!Xenopsd.vgpu_ready_timeout ~cancel () in
-		Forkhelpers.dontwaitpid vgpu_pid
+		if not (Vgpu.is_running ~xs domid) then begin
+			(* The below line does nothing if the device is already bound to the
+			 * nvidia driver. We rely on xapi to refrain from attempting to run
+			 * a vGPU on a device which is passed through to a guest. *)
+			PCI.bind [vgpu.physical_pci_address] PCI.Nvidia;
+			let args = vgpu_args_of_nvidia domid vcpus vgpu in
+			let ready_path = Printf.sprintf "/local/domain/%d/vgpu-pid" domid in
+			let cancel = Cancel_utils.Vgpu domid in
+			let vgpu_pid = init_daemon ~task ~path:!Xc_resources.vgpu ~args
+				~name:"vgpu" ~domid ~xs ~ready_path ~timeout:!Xenopsd.vgpu_ready_timeout ~cancel () in
+			Forkhelpers.dontwaitpid vgpu_pid
+		end else
+			D.info "Daemon %s is already running for domain %d" !Xc_resources.vgpu domid
 	| [{implementation = GVT_g vgpu}] ->
 		PCI.bind [vgpu.physical_pci_address] PCI.I915
 	| [{implementation = MxGPU vgpu}] ->