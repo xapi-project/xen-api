5143d41c18dad6da4d36f13a676d1f88eae5d701
Iglói Gábor <gabor.igloi@citrix.com>
2017-09-30 13:31:42 +0100

    Test_vdi_cbt: fix unit tests failing due to new event loop in VDI.data_destroy
    These unit tests did not work any more because when VDI.data_destroy is
    allowed, it uses an event loop to watch VBD changes, which makes XenAPI
    calls using the Client module.
    
    Signed-off-by: Iglói Gábor <gabor.igloi@citrix.com>

---
+++ ocaml/xapi/test_event_common.ml
@@ -0,0 +1,18 @@
+
+let ps_start = ref false
+
+let start_periodic_scheduler () =
+  if !ps_start then () else begin
+    Xapi_periodic_scheduler.add_to_queue "dummy" (Xapi_periodic_scheduler.Periodic 60.0) 0.0 (fun () -> ());
+    Xapi_event.register_hooks ();
+    ignore(Thread.create Xapi_periodic_scheduler.loop ());
+    ps_start := true
+  end
+
+let event_setup_common () =
+  start_periodic_scheduler ();
+  let __context = Test_common.make_test_database () in
+  let session_id = Test_common.make_session ~__context () in
+  let __context = Mock.Context.make ~session_id "session context" in
+  (__context, session_id)
+
