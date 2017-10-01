84568aefa83308ff63a65ba8370d20c290939a15
Iglói Gábor <gabor.igloi@citrix.com>
2017-10-01 20:56:02 +0100

    Test_vdi_cbt: add fast data_destroy timing tests
    * Now it's possible to turn off the slower variant, that goes through
      message forwarding.
    
    * And fix a typo in the data_destroy timeout tests, a test scenario was
      duplicated.
    
    Signed-off-by: Iglói Gábor <gabor.igloi@citrix.com>

--- ocaml/xapi/test_vdi_cbt.ml
+++ ocaml/xapi/test_vdi_cbt.ml
@@ -302,9 +302,35 @@ let test_data_destroy =
       (Db.VDI.get_type ~__context ~self:vdi)
   in
 
-  let test_data_destroy_timing =
-    (* VDI.data_destroy currently waits for at most 4 second for the VBDs of
+  (** If [realistic_timing] is true, the tests will use the Client module and
+      the original timeouts, otherwise they will call the implementation in
+      Xapi_vdi directly with a smaller timeout to make the tests faster. The
+      former leads to more comprehensive tests, as it goes through message
+      forwarding, which also includes a timeout / retry mechanism, and can thus
+      impact the timing of data_destroy: when the VBD unplug is already in
+      progress, Xapi_vdi.check_operation_error and the message forwarding layer
+      will first wait for that operation to finish. *)
+  let test_data_destroy_timing ~realistic_timing =
+    let speedup = if realistic_timing then 1.0 else 10.0 in
+    (* VDI.data_destroy currently waits for at most 4 seconds for the VBDs of
        the VDI to be unplugged and destroyed *)
+    let timeout = 4.0 /. speedup in
+    (* We simulate a VBD.unplug call, which might take 1-2 seconds *)
+    let vbd_unplug_time = 1.5 /. speedup in
+    let other_delay = 0.2 /. speedup in
+    let message_switch_retry_delay = 20.0 /. speedup in
+    (* We have to leave some extra time for data_destroy to finish, otherwise
+       the tests will fail with a stricter timeout. *)
+    let timebox_timeout_delay = 1.0 in
+
+    let uses_client = realistic_timing in
+    let call_data_destroy ~__context ~self =
+      if uses_client then begin
+        let (rpc, session_id) = Test_common.make_client_params ~__context in
+        Client.Client.VDI.data_destroy ~rpc ~session_id ~self
+      end else
+        Xapi_vdi._data_destroy ~__context ~self ~timeout
+    in
 
     let start_vbd_unplug ~__context self =
       Db.VBD.set_current_operations ~__context ~self ~value:["", `unplug]
@@ -314,43 +340,44 @@ let test_data_destroy =
       Db.VBD.set_current_operations ~__context ~self ~value:[];
     in
 
+    let setup_test () =
+      let __context, sR, vDI = setup_test_for_data_destroy () in
+      let vM = Test_common.make_vm ~__context () in
+      let vbd = Test_common.make_vbd ~__context ~vDI ~vM ~currently_attached:true () in
+      (__context, vDI, vbd)
+    in
+
     let test_data_destroy_succeeds =
       (* The parameters tell us whether the VBD unplug operation has started/finished by the time we call VDI.data_destroy. *)
       let test_data_destroy_succeeds ~vbd_unplug_started ~vbd_unplug_finished () =
-        let __context, sR, vDI = setup_test_for_data_destroy () in
-        let vM = Test_common.make_vm ~__context () in
-        let vbd = Test_common.make_vbd ~__context ~vDI ~vM ~currently_attached:true ~current_operations:[] () in
+        let __context, vDI, vbd = setup_test () in
         if vbd_unplug_started then start_vbd_unplug ~__context vbd;
         if vbd_unplug_finished then finish_vbd_unplug ~__context vbd;
 
         let _: Thread.t = Thread.create (fun () ->
             if not vbd_unplug_started then begin
-              Thread.delay 0.2;
+              Thread.delay other_delay;
               start_vbd_unplug ~__context vbd
             end;
             if not vbd_unplug_finished then begin
-              (* Simulate a VBD.unplug call, which might take 1-2 seconds *)
-              Thread.delay 1.5;
+              Thread.delay vbd_unplug_time;
               finish_vbd_unplug ~__context vbd
             end;
-            Thread.delay 0.2;
+            Thread.delay other_delay;
             Db.VBD.destroy ~__context ~self:vbd
           ) ()
         in
-        (* Call data_destroy through the client, because when the VBD unplug is
-           already in progress, Xapi_vdi.check_operation_error and the message
-           forwarding layer will first wait for that operation to finish. *)
-        let (rpc, session_id) = Test_common.make_client_params ~__context in
+        let vbd_unplug_in_progress_when_data_destroy_called = vbd_unplug_started && (not vbd_unplug_finished) in
         (* If the VBD.unplug operation is in progress when VDI.data_destroy is
            called, the message forwarding layer will wait for the unplug to
            finish, and after each retry it sleeps for a random amount of time
            between 1 and 20 seconds. *)
-        let vbd_unplug_in_progress_when_data_destroy_called = vbd_unplug_started && (not vbd_unplug_finished) in
-        let timeout = if vbd_unplug_in_progress_when_data_destroy_called then 21.0 else 5.0 in
+        let timeout = if uses_client && vbd_unplug_in_progress_when_data_destroy_called then message_switch_retry_delay else timeout in
+        let timeout = timeout +. timebox_timeout_delay in
         Helpers.timebox
           ~timeout
           ~otherwise:(fun () -> failwith "data_destroy did not finish successfully in 5 seconds")
-          (fun () -> Client.Client.VDI.data_destroy ~rpc ~session_id ~self:vDI)
+          (fun () -> call_data_destroy ~__context ~self:vDI)
       in
       let open OUnit in
       "test_data_destroy_succeeds" >:::
@@ -363,84 +390,86 @@ let test_data_destroy =
     let test_data_destroy_times_out =
       (* The parameters tell us whether the VBD unplug operation has started/finished by the time VDI.data_destroy times out. *)
       let test_data_destroy_times_out ~vbd_unplug_started ~vbd_unplug_finished () =
-        let __context, sR, vDI = setup_test_for_data_destroy () in
-        let vM = Test_common.make_vm ~__context () in
-        let vbd = Test_common.make_vbd ~__context ~vDI ~vM ~currently_attached:true () in
+        let __context, vDI, vbd = setup_test () in
 
-        let _: Thread.t = Thread.create (fun () ->
+        let t = Thread.create (fun () ->
             (* The data_destroy operation will time out in 4 seconds *)
             let unplug_start_delay = match vbd_unplug_started, vbd_unplug_finished with
-              | true, true -> 1.0 (* unplug finishes after 2.5s, before the 4s timeout *)
-              | true, false -> 3.0 (* unplug finishes after 4.5s, after the 4s timeout *)
-              | false, false -> 4.1 (* unplug starts after the 4s timeout *)
-              | _ -> failwith "not tested"
+              | true, true -> timeout -. vbd_unplug_time -. other_delay (* unplug starts & finishes before the timeout *)
+              | true, false -> timeout -. (vbd_unplug_time /. 2.0) (* unplug starts before & finishes after the timeout *)
+              | false, false -> timeout +. other_delay (* unplug starts after the 4s timeout *)
+              | _ -> failwith "VBD.unplug finished but not started by the time VDI.data_destroy times out: impossible"
             in
+            assert (unplug_start_delay >= 0.0);
             Thread.delay unplug_start_delay;
             start_vbd_unplug ~__context vbd;
             (* Simulate a VBD.unplug call, which might take 1-2 seconds *)
-            Thread.delay 1.5;
+            Thread.delay vbd_unplug_time;
             (* We have to finish the VBD unplug in the unit test, otherwise
                message forwarding will wait for a long time for the in-progress
                VBD.unplug operation to finish *)
             finish_vbd_unplug ~__context vbd
           ) ()
         in
-        (* Call data_destroy through the client, because when the VBD unplug is
-           already in progress, Xapi_vdi.check_operation_error and the message
-           forwarding layer will first wait for that operation to finish. *)
-        let (rpc, session_id) = Test_common.make_client_params ~__context in
         (* If the timebox expires, we will fail because we will get an unexpected exception *)
+        let timeout = timeout +. timebox_timeout_delay in
         OUnit.assert_raises
           Api_errors.(Server_error (vdi_in_use, [Ref.string_of vDI; "data_destroy"]))
           (fun () ->
              Helpers.timebox
-               ~timeout:5.0
-               ~otherwise:(fun () -> failwith "data_destroy did not time out in 5 seconds")
-               (fun () -> Client.Client.VDI.data_destroy ~rpc ~session_id ~self:vDI)
-          )
+               ~timeout
+               ~otherwise:(fun () -> failwith "data_destroy did not time out")
+               (fun () -> call_data_destroy ~__context ~self:vDI)
+          );
+        (* Wait for the background thread to finish to prevent it failing with
+           DBCache_NotFound exceptions due to missing references - this
+           probably happens when it tries to use the VBD reference but its __context already
+           got garbage collected *)
+        Thread.join t
       in
 
       let test_data_destroy_times_out_when_nothing_happens_to_vbd () =
-        let __context, sR, vDI = setup_test_for_data_destroy () in
-        let vM = Test_common.make_vm ~__context () in
-        let _: _ API.Ref.t = Test_common.make_vbd ~__context ~vDI ~vM ~currently_attached:true () in
-        (* Call data_destroy through the client, because when the VBD unplug is
-           already in progress, Xapi_vdi.check_operation_error and the message
-           forwarding layer will first wait for that operation to finish. *)
-        let (rpc, session_id) = Test_common.make_client_params ~__context in
+        let __context, vDI, vbd = setup_test () in
+        let timeout = timeout +. timebox_timeout_delay in
         (* If the timebox expires, we will fail because we will get an unexpected exception *)
         OUnit.assert_raises
           Api_errors.(Server_error (vdi_in_use, [Ref.string_of vDI; "data_destroy"]))
           (fun () ->
              Helpers.timebox
-               ~timeout:5.0
-               ~otherwise:(fun () -> failwith "data_destroy did not time out in 5 seconds")
-               (fun () -> Client.Client.VDI.data_destroy ~rpc ~session_id ~self:vDI)
+               ~timeout
+               ~otherwise:(fun () -> failwith (Printf.sprintf "data_destroy did not time out in %f seconds" timeout))
+               (fun () -> call_data_destroy ~__context ~self:vDI)
           )
       in
       let open OUnit in
       "test_data_destroy_times_out" >:::
       [ "test_data_destroy_times_out_when_vbd_does_not_get_destroyed_in_time" >:: test_data_destroy_times_out ~vbd_unplug_started:true ~vbd_unplug_finished:true
       ; "test_data_destroy_times_out_when_vbd_unplug_does_not_finish_in_time" >:: test_data_destroy_times_out ~vbd_unplug_started:true ~vbd_unplug_finished:false
-      ; "test_data_destroy_times_out_when_vbd_unplug_is_not_started_in_time" >:: test_data_destroy_times_out ~vbd_unplug_started:true ~vbd_unplug_finished:false
+      ; "test_data_destroy_times_out_when_vbd_unplug_is_not_started_in_time" >:: test_data_destroy_times_out ~vbd_unplug_started:false ~vbd_unplug_finished:false
       ; "test_data_destroy_times_out_when_nothing_happens_to_vbd" >:: test_data_destroy_times_out_when_nothing_happens_to_vbd
       ]
     in
 
     let open OUnit in
-    "test_data_destroy_timing" >:::
+    let suffix = if realistic_timing then "_with_realistic_timing" else "_with_simulated_timing" in
+    ("test_data_destroy_timing" ^ suffix) >:::
     [ test_data_destroy_succeeds
     ; test_data_destroy_times_out
     ]
   in
 
+  (** This flag controls whether we should run the {!test_data_destroy_timing}
+      tests with realistic timing (with [realistic_timing = true). *)
+  let run_slow_tests = true in
+  let slow_tests = [ test_data_destroy_timing ~realistic_timing:true ] in
   let open OUnit in
   "test_data_destroy" >:::
   [ "test_vdi_after_data_destroy" >:: test_vdi_after_data_destroy
   ; "test_vdi_managed_data_destroy" >:: test_vdi_managed_data_destroy
   ; "test_does_not_change_vdi_type_to_cbt_metadata_if_it_fails" >:: test_does_not_change_vdi_type_to_cbt_metadata_if_it_fails
-  ; test_data_destroy_timing
-  ]
+  ; test_data_destroy_timing ~realistic_timing:false
+  ] @
+  (if run_slow_tests then slow_tests else [])
 
 (* behaviour verification VDI.list_changed_blocks *)
 let test_vdi_list_changed_blocks () =
