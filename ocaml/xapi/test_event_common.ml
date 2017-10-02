2ef9090e7ee4852dfb18fc3c17cdc642c482ee5a
Gabor Igloi <gabor.igloi@citrix.com>
2017-10-02 15:49:03 +0100

    Test_event_common: add mutex around starting scheduler
    To avoid starting it twice.
    
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/test_event_common.ml
+++ ocaml/xapi/test_event_common.ml
@@ -1,13 +1,17 @@
 
 let ps_start = ref false
 
+let scheduler_mutex = Mutex.create ()
+
 let start_periodic_scheduler () =
+  Mutex.lock scheduler_mutex;
   if !ps_start then () else begin
     Xapi_periodic_scheduler.add_to_queue "dummy" (Xapi_periodic_scheduler.Periodic 60.0) 0.0 (fun () -> ());
     Xapi_event.register_hooks ();
     ignore(Thread.create Xapi_periodic_scheduler.loop ());
     ps_start := true
-  end
+  end;
+  Mutex.unlock scheduler_mutex
 
 let event_setup_common () =
   start_periodic_scheduler ();
