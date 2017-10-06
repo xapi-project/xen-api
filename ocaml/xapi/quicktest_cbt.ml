a65732c6fbf0cc6e3ce583f7786f6f758042d9a8
Akanksha Mathur <akanksha.mathur@citrix.com>
2017-10-06 12:31:10 +0100

    CP-22019: Add function to check for attached SRs, refactor test structure

--- ocaml/xapi/quicktest_cbt.ml
+++ ocaml/xapi/quicktest_cbt.ml
@@ -22,30 +22,39 @@ let start session_id =
     failed test (Printf.sprintf "%s failed: %s" test_name
                    (ExnHelper.string_of_exn error)) in
 
+
   let test_assert ~test op ~msg =
     try assert op with assert_failure -> failed test msg in
-
-  let test_compare ~test l_op r_op ~msg =
-    let op = (l_op=r_op) in
-    test_assert ~test ~op ~msg in
-
 
   (* Overall test suite runs smaller unit tests *)
   let cbt_test = make_test "Testing changed block tracking" 2 in
   try
     start cbt_test;
-    (* Create new VDI, attach to lvm SR *)
+
+    (* generate list of SRs attached to a host *)
+    let list_of_attached_srs =
+      let is_attached sr =
+        let pbds = SR.get_PBDs ~session_id ~rpc:!rpc ~self:sr in
+        List.fold_left (||) false
+          (List.map (fun pbd ->
+               PBD.get_currently_attached ~session_id ~rpc:!rpc ~self:pbd
+             ) pbds) in
+      List.filter is_attached (SR.get_all ~session_id ~rpc:!rpc) in
+
+    (* find lvm SR (that passes data_destroy) to attach new VDI *)
     let sR = List.filter
-        (fun sr -> (SR.get_type ~session_id ~rpc:!rpc ~self:sr) = "lvm")
-        (SR.get_all ~session_id ~rpc:!rpc)
-             |> List.hd in
+        (fun sr ->
+           ((SR.get_type ~session_id ~rpc:!rpc ~self:sr) = "lvm")
+           && (not (SR.get_is_tools_sr ~session_id ~rpc:!rpc ~self:sr))
+        ) list_of_attached_srs |> List.hd in
+
     let vDI = VDI.create ~session_id ~rpc:!rpc ~name_label:"qt-cbt"
         ~name_description:"VDI for CBT quicktest" ~_type:`user ~sR
         ~sharable:false ~other_config:[] ~read_only:false ~sm_config:[]
         ~virtual_size:(2L ** mib) ~xenstore_data:[] ~tags:[] in
 
     (* Test enable/disable CBT, test cbt_enabled:false for new VDI *)
-    let enable_disable_cbt_test () =
+    let enable_disable_cbt_test vDI =
       let enable_cbt_test = make_test "Testing VDI.enable/disable_CBT" 4 in
       let get_cbt_status vDI = VDI.get_cbt_enabled ~session_id ~rpc:!rpc ~self:vDI in
       let test = enable_cbt_test in
@@ -60,16 +69,23 @@ let start session_id =
           ~msg:"VDI.enable_cbt failed";
         VDI.disable_cbt ~session_id ~rpc:!rpc ~self:vDI;
         test_assert ~test
-          (get_cbt_status vDI) (* disable_cbt fails *)
+          (not (get_cbt_status vDI)) (* disable_cbt fails *)
           ~msg:"VDI.disable_CBT failed";
         success enable_cbt_test;
       with e -> report_failure e "enable/disable CBT" enable_cbt_test in
 
     (* Call unit tests *)
-    enable_disable_cbt_test ();
+    let run_tests ~vDI =
+      List.iter (fun test -> test vDI)
+        [ enable_disable_cbt_test
+        ]; in
 
-    (* Finally, destroy VDI *)
-    VDI.destroy ~session_id ~rpc:!rpc ~self:vDI;
+    (* Finally, destroy VDI regardless of how test suite progresses *)
+    Xapi_stdext_pervasives.Pervasiveext.finally
+      (fun () -> run_tests ~vDI)
+      (fun () -> VDI.destroy ~session_id ~rpc:!rpc ~self:vDI);
+
+    (* Overall test will fail if VDI.destroy messes up, or any other exception is thrown *)
     success cbt_test
   with
   | (Failure hd) -> failed cbt_test "Could not find lvm SR, cannot create VDI"
