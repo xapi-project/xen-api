0aa462dd19dd491f9024a3f93858e87329787b8c
Marcus Granado <marcus.granado@citrix.com>
2017-10-09 15:05:38 +0100

    CP-24805: permit other defaults when ensuring device-model profile in platform
    This will be useful when applying different device-model profile defaults from
    other versions of XenServer during upgrade.
    
    Signed-off-by: Marcus Granado <marcus.granado@citrix.com>

--- ocaml/xapi/xapi_vm_helpers.ml
+++ ocaml/xapi/xapi_vm_helpers.ml
@@ -1068,9 +1068,9 @@ let with_vm_operation ~__context ~self ~doc ~op ?(strict=true) ?policy f =
          _ -> ())
 
 (* Device Model Profiles *)
-let ensure_device_model_profile_present ~__context ~hVM_boot_policy platform =
+let ensure_device_model_profile_present ~__context ~hVM_boot_policy ?(default_value=Vm_platform.default_device_model_default_value) platform =
   let is_hvm = hVM_boot_policy <> "" in
-  let default = Vm_platform.(device_model, default_device_model_default_value) in
+  let default = Vm_platform.(device_model, default_value) in
   if not is_hvm || List.mem_assoc Vm_platform.device_model platform then
     platform
   else (* only add device-model to an HVM VM platform if it is not already there *)
