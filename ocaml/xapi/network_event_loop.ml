67839c88d0d7e6015d01d435fc2c01a939b37fb8
Gabor Igloi <gabor.igloi@citrix.com>
2017-10-09 13:46:21 +0100

    Network_event_loop: query PIFs and use events as a notification only & enable network bridges
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/network_event_loop.ml
+++ ocaml/xapi/network_event_loop.ml
@@ -8,8 +8,6 @@ let _watch_networks __context ~update_firewall ~wait_after_failure_seconds =
 
   (* We keep track of the network objects in the database using this event loop. *)
   let classes = ["network"] in
-  (* We store the networks in a reference to record map. *)
-  let networks = StringMap.empty in
   (* We keep track of the interfaces that we last passed to the firewall script
      to allow NBD traffic on them. At startup, NBD is blocked on all
      interfaces. *)
@@ -18,32 +16,7 @@ let _watch_networks __context ~update_firewall ~wait_after_failure_seconds =
   let api_timeout = 60. in
   let timeout = 30. +. api_timeout +. !Db_globs.master_connection_reset_timeout in
 
-  let rec update_networks ~token ~networks =
-
-    let most_recent_state events =
-      let events_from_newest_to_oldest =
-        (* We need to sort the timestamp strings in decreasing order *)
-        List.sort (fun e1 e2 -> Event_types.(- String.compare e1.ts e2.ts)) events
-      in
-      (* We should immediately get a [Some x] after inspecting the first
-         element, because the modification and addition events contain a
-         snapshot of the object. *)
-      List.fold_left
-        (fun state_opt event ->
-           match state_opt with
-           | Some x as s -> s (* We already found the most recent state *)
-           | None ->
-             if event.Event_types.op = `del then Some Deleted
-             else
-               (* If we have the object snapshot, return that, otherwise keep looking. *)
-               event.Event_types.snapshot
-               |> Xapi_stdext_monadic.Opt.map
-                 (fun rpc -> Snapshot (API.network_t_of_rpc rpc))
-        )
-        None
-        events_from_newest_to_oldest
-    in
-
+  let wait_for_network_change ~token =
     let from =
       Helpers.call_api_functions ~__context
         (fun rpc session_id ->
@@ -52,62 +25,30 @@ let _watch_networks __context ~update_firewall ~wait_after_failure_seconds =
     in
     let events = from.Event_types.events in
     List.iter (fun event -> D.debug "got event %s" (Event_types.string_of_event event)) events;
-
-    let events_by_network_ref =
-      List.fold_left
-        (fun m event ->
-           let ref = event.Event_types.reference  in
-           let events = try StringMap.find ref m with _ -> [] in
-           let events = event :: events in
-           StringMap.add ref events m)
-        StringMap.empty
-        events
-    in
-
-    (* We remove the keys corrersponding to networks for which we do not have an update. *)
-    let most_recent_state_by_network_ref = StringMap.fold
-        (fun ref events m ->
-           match most_recent_state events with
-           | Some s -> StringMap.add ref s m
-           | None -> m
-        )
-        events_by_network_ref
-        StringMap.empty
-    in
-
-    let networks = StringMap.merge
-        (fun ref network most_recent_state ->
-           match most_recent_state with
-           (* We did not get an update about this network, keep our local copy. *)
-           | None -> network
-           (* The network has been deleted, remove our local copy. *)
-           | Some Deleted -> None
-           (* The network has been changed, update our local copy. *)
-           | Some (Snapshot s) -> Some s
-        )
-        networks
-        most_recent_state_by_network_ref
-    in
-    (from.Event_types.token, networks)
+    from.Event_types.token
   in
 
-  let rec loop ~token ~networks ~allowed_interfaces =
-    let (token, networks, allowed_interfaces) =
+  let rec loop ~token ~allowed_interfaces =
+    let (token, allowed_interfaces) =
       try
-        let token, networks = update_networks ~token ~networks in
-        let pifs =
-          StringMap.fold
-            (fun _ref network pifs -> network.API.network_PIFs @ pifs)
-            networks
-            []
-        in
+        let token = wait_for_network_change ~token in
         let localhost = Helpers.get_localhost ~__context in
-        let pifs = List.map (fun pif -> Db.PIF.get_record ~__context ~self:pif) pifs in
-        let pifs = List.filter
-            (fun pif -> pif.API.pIF_host = localhost && pif.API.pIF_physical = true)
+        let pifs = Db.Host.get_PIFs ~__context ~self:localhost in
+        let allowed_connected_networks =
+          Valid_ref_list.filter_map
+            (fun pif ->
+               let network = Db.PIF.get_network ~__context ~self:pif in
+               let purpose = Db.Network.get_purpose ~__context ~self:network in
+               if List.mem `nbd purpose || List.mem `insecure_nbd purpose then
+                 Some network
+               else
+                 None
+            )
             pifs
         in
-        let devices = List.map (fun pif -> pif.API.pIF_device) pifs in
+        let allowed_local_bridges = List.map (fun network -> Db.Network.get_bridge ~__context ~self:network) allowed_connected_networks in
+        let allowed_local_bridges = List.filter ((<>) "") allowed_local_bridges in
+        let devices = allowed_local_bridges in
         let interface_list = String.concat ", " devices in
         if not (Xapi_stdext_std.Listext.List.set_equiv devices allowed_interfaces) then begin
           D.debug "Updating the firewall to use the following interfaces for NBD: [%s]" interface_list;
@@ -115,25 +56,25 @@ let _watch_networks __context ~update_firewall ~wait_after_failure_seconds =
         end else begin
           D.debug "Not updating the firewall, because the set of interfaces to use for NBD did not change: [%s]" interface_list
         end;
-        (token, networks, devices)
+        (token, devices)
       with
       | Api_errors.Server_error (code, _) as e when code = Api_errors.events_lost ->
         D.warn "Lost events: %s" (ExnHelper.string_of_exn e);
-        ("", networks, allowed_interfaces)
+        ("", allowed_interfaces)
       | e ->
         (* In case of failures, for example due to the script, which updates
            the firewall rules, we start from scratch to ensure that we will
            process the missed events. *)
         D.error "Caught %s listening to events on network objects" (ExnHelper.string_of_exn e);
         Thread.delay wait_after_failure_seconds;
-        ("", networks, allowed_interfaces)
+        ("", allowed_interfaces)
     in
 
-    loop ~token ~networks ~allowed_interfaces
+    loop ~token ~allowed_interfaces
   in
 
   D.debug "Listening to events on network objects";
-  loop ~token:"" ~networks ~allowed_interfaces
+  loop ~token:"" ~allowed_interfaces
 
 let update_firewall interfaces_allowed_for_nbd =
   let args = "set" :: interfaces_allowed_for_nbd in
--- ocaml/xapi/test_network_event_loop.ml
+++ ocaml/xapi/test_network_event_loop.ml
@@ -28,98 +28,110 @@ let test_network_event_loop ~no_networks_at_start () =
       )
   in
 
-  let assert_received_params expected =
+  let assert_received_params msg expected =
     Thread.delay delay;
     match !received_params with
-    | None -> OUnit.assert_failure "The update_firewall function was not called"
+    | None -> OUnit.assert_failure ("The update_firewall function was not called: " ^ msg)
     | Some p ->
       Ounit_comparators.StringSet.(assert_equal (of_list expected) (of_list p))
   in
 
-  let assert_not_called () =
+  let assert_not_called msg () =
     Thread.delay delay;
-    OUnit.assert_equal ~msg:"update_firewall shouldn't have been called" None !received_params
+    OUnit.assert_equal ~msg:("update_firewall shouldn't have been called: " ^ msg) None !received_params
   in
 
   if no_networks_at_start then begin
     received_params := None;
     let _ : Thread.t = start_event_loop () in
-    assert_not_called ()
+    assert_not_called "There are no networks" ()
   end;
 
   (* Add a new network "network1" with a PIF connected to this host *)
   received_params := None;
-  let network1 = Test_common.make_network ~__context ~purpose:[`nbd] () in
-  assert_not_called ();
-  let pif1 = Test_common.make_pif ~__context ~network:network1 ~host:localhost ~device:"pif1" () in
+  let network1 = Test_common.make_network ~__context ~purpose:[`nbd] ~bridge:"bridge1" () in
+  assert_not_called "network1 has no PIF connected to this host" ();
+  let pif1 = Test_common.make_pif ~__context ~network:network1 ~host:localhost ~device:"network1_pif1" () in
   if not no_networks_at_start then begin
     let _ : Thread.t = start_event_loop () in ()
   end;
-  assert_received_params ["pif1"];
+  assert_received_params "network1 is now connected to this host with pif1" ["bridge1"];
 
   (* Add another PIF to network1 connected to this host *)
   received_params := None;
+  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network1 ~host:localhost ~device:"network1_pif2" () in
+  assert_not_called "We've already enabled NBD on this network's bridge" ();
+
+  received_params := None;
+  Db.Network.set_name_description ~__context ~self:network1 ~value:"irrelevant network object modification";
+  assert_not_called "only the network1's name label was modified" ();
+
+  (* Add a new network "network2" with a PIF connected to this host *)
+  received_params := None;
   fail_firewall_update := true;
-  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network1 ~host:localhost ~device:"pif2" () in
+  let network2 = Test_common.make_network ~__context ~purpose:[`nbd] ~bridge:"bridge2" () in
+  assert_not_called "network2 has no PIF connected to this host" ();
+  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network2 ~host:localhost ~device:"network2_pif1" () in
   (* After [delay] seconds, the event loop should have noticed the change and
      failed after trying to update the firewall *)
   Thread.delay delay;
   fail_firewall_update := false;
   (* Test that we do wait for the given delay in case of failures *)
-  assert_not_called ();
+  assert_not_called "The event loop should wait for the given delay in case of failures" ();
   (* Now wait for the event loop to reregister and continue *)
   Thread.delay network_event_loop_wait_after_failure_seconds;
   (* Test that transient failures in the script won't stop the event loop, and
      that we will eventually process the event we missed *)
-  assert_received_params ["pif1"; "pif2"];
+  assert_received_params "we should have noticed the addition of network2 and its PIF after the transient failure" ["bridge1"; "bridge2"];
 
+  (* Add a new network "network3" with a PIF connected to another host *)
   received_params := None;
-  Db.Network.set_name_description ~__context ~self:network1 ~value:"irrelevant network object modification";
-  assert_not_called ();
+  let network3 = Test_common.make_network ~__context ~purpose:[`nbd] ~bridge:"bridge3" () in
+  assert_not_called "network3 has no PIF connected to this host" ();
+  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network3 ~host:other_host ~device:"network3_pif1" () in
+  assert_not_called "pif1 of network3 is connected to another host" ();
 
-  (* Add a new network "network2" with a PIF connected to another host *)
+  (* Add another PIF to network3 connected to another host *)
   received_params := None;
-  let network2 = Test_common.make_network ~__context ~purpose:[`nbd] () in
-  assert_not_called ();
-  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network2 ~host:other_host ~device:"pifX" () in
-  assert_not_called ();
+  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network3 ~host:other_host ~device:"network3_pif2" () in
+  assert_not_called "pif2 of network3 is connected to another host" ();
 
-  (* Add another PIF to network2 connected to another host *)
+  (* Add a PIF to network3 connected to this host *)
   received_params := None;
-  let _ : _ API.Ref.t = Test_common.make_pif ~__context ~network:network2 ~host:other_host ~device:"pifY" () in
-  assert_not_called ();
-
-  (* Add a PIF to network2 connected to this host *)
-  received_params := None;
-  let pif3 = Test_common.make_pif ~__context ~network:network2 ~host:localhost ~device:"pif3" () in
-  assert_received_params ["pif1"; "pif2"; "pif3"];
+  let pif3 = Test_common.make_pif ~__context ~network:network3 ~host:localhost ~device:"network3_pif3" () in
+  assert_received_params "network3 is now connected to this host by pif3" ["bridge1"; "bridge2"; "bridge3"];
 
   (* Remove pif1 on network1 *)
   received_params := None;
   Db.PIF.destroy ~__context ~self:pif1;
-  assert_received_params ["pif2"; "pif3"];
+  assert_not_called "network1 still has a PIF connected to this host, so we shouldn't disallow its bridge" ();
 
   (* Remove network1 *)
   received_params := None;
   Db.Network.destroy ~__context ~self:network1;
-  assert_received_params ["pif3"];
+  assert_received_params "network1 has been removed" ["bridge2"; "bridge3"];
 
-  (* Remove pif3 on network2 *)
+  (* Remove pif3 of network3 *)
+  received_params := None;
+  Db.PIF.destroy ~__context ~self:pif3;
+  (* This was network3's only PIF connected to this host, so we should have disabled NBD on network3's bridge *)
+  assert_received_params "network3's only PIF connected to this host has been removed" ["bridge2"];
+
+  (* Remove network2 *)
   received_params := None;
   fail_firewall_update := true;
-  Db.PIF.destroy ~__context ~self:pif3;
+  Db.Network.destroy ~__context ~self:network2;
   (* After [delay] seconds, the event loop should have noticed the change and
      failed after trying to update the firewall *)
   Thread.delay delay;
   fail_firewall_update := false;
-  (* Test that we do wait for the given delay in case of failures *)
-  assert_not_called ();
+  assert_not_called "The event loop should wait for the given delay in case of failures" ();
   (* Now wait for the event loop to reregister and continue *)
   Thread.delay network_event_loop_wait_after_failure_seconds;
   (* Test that transient failures in the script won't stop the event loop, and
      that we will eventually process the event we missed, and update the
      firewall with the correct list of interfaces, even if it is the empty list *)
-  assert_received_params []
+  assert_received_params "no network is connected to this host now, we should have noticed this after the transient failure" []
 
 let test =
   let ((>:::), (>::)) = OUnit.((>:::), (>::)) in
