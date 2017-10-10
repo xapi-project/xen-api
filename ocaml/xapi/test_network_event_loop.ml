8e1774db4728a146e77e6fc7de6dd16ba301f16a
Gabor Igloi <gabor.igloi@citrix.com>
2017-10-10 15:25:29 +0100

    Add copyright statements to new files
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/test_network_event_loop.ml
+++ ocaml/xapi/test_network_event_loop.ml
@@ -1,3 +1,17 @@
+(*
+ * Copyright (C) 2017 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
 let test_network_event_loop ~no_nbd_networks_at_start () =
   let __context, _ = Test_event_common.event_setup_common () in
   (* We need to set test_mode to true to ensure that the event loop will use
