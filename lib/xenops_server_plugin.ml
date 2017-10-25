YarsinCitrix/xenopsd

Move the responsibility of vGPU restoring from VM_restore_vgpu to VM_restore
Currently, the VM_restore_vgpu operation, which is kicked off on the
destination of a migration by an HTTP request from VM_migrate on the sender, is
responsible for starting the receiving DEMU and passing it the file descriptor
of the connection.

Because there is now a single emu-manager process on the destination, which
coordinates both the main migration stream (memory image etc.) through
xenguest, as well as the vGPU stream through DEMU, it makes sense to drive this
from a single place in xenopsd. The main migration stream is driven from the
VM_receive_memory operation from the initial HTTP request to the destination.
This internally executes the VM_restore operation.

This patch prepares to move the handling of the vGPU stream from
VM_restore_vgpu to VM_restore. In particular, the following high-level
operations are changed (mostly in xenops_server; the following patches build on
this):

- An additional, optional `data` item is added to the arguments tuple of the
  VM_restore operation, similar to the one on VM_save. This is set to the fd of
  the vGPU migration connection, in VM_receive_memory. The complication is that
  this fd is not from the connection that VM_receive_memory runs off, but from
  the one that is handled by the VM.receive_vgpu function in a different,
  concurrently running thread. Some synchronisation between the two threads/ops
  is added for this reason.

- The VM_save_vgpu operation is deleted, since previous commits have made it a
  no-op.

- The VM_restore_vgpu operation is replaced by VGPU_start, since its only
  remaining job was to start DEMU. The new op has a Boolean argument to express
  whether DEMU must be started in restore mode or not.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

lib/xenops_server_plugin.ml
@@ -84,9 +84,6 @@ module type S = sig
 		val save: Xenops_task.t -> progress_cb -> Vm.t -> flag list -> data -> data option -> unit
 		val restore: Xenops_task.t -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> string list -> unit
 
-		val save_vgpu: Xenops_task.t -> Vm.t -> Vgpu.t -> data -> unit
-		val restore_vgpu: Xenops_task.t -> Vm.t -> Vgpu.t -> data -> unit
-
 		val s3suspend: Xenops_task.t -> Vm.t -> unit
 		val s3resume: Xenops_task.t -> Vm.t -> unit
 
@@ -139,6 +136,7 @@ module type S = sig
 		val get_device_action_request: Vm.id -> Vif.t -> device_action_request option
 	end
 	module VGPU : sig
+		val start: Xenops_task.t -> Vm.id -> Vgpu.t -> bool -> unit
 		val get_state: Vm.id -> Vgpu.t -> Vgpu.state
 	end
 	module UPDATES : sig

xapi-project/xen-api

lib/xenops_server_plugin.ml
@@ -84,9 +84,6 @@ module type S = sig
     val save: Xenops_task.task_handle -> progress_cb -> Vm.t -> flag list -> data -> data option -> unit
     val restore: Xenops_task.task_handle -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> string list -> unit
 
-    val save_vgpu: Xenops_task.task_handle -> Vm.t -> Vgpu.t -> data -> unit
-    val restore_vgpu: Xenops_task.task_handle -> Vm.t -> Vgpu.t -> data -> unit
-
     val s3suspend: Xenops_task.task_handle -> Vm.t -> unit
     val s3resume: Xenops_task.task_handle -> Vm.t -> unit
 
@@ -141,6 +138,7 @@ module type S = sig
     val get_device_action_request: Vm.id -> Vif.t -> device_action_request option
   end
   module VGPU : sig
+    val start: Xenops_task.task_handle -> Vm.id -> Vgpu.t -> bool -> unit
     val get_state: Vm.id -> Vgpu.t -> Vgpu.state
   end
   module VUSB :sig