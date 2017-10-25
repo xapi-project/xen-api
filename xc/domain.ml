YarsinCitrix/xenopsd

Send restore:xenguest in restore_libxc_record
The new protocol with emu-manager is for xenopsd to request restoring the next
bit from the file using the new "restore:" command followed by the name of the
thing to be restored, which is determined by the last read header in the
stream. First up is "restore:xenguest" for the "libxc" record.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

xc/domain.ml
@@ -814,6 +814,7 @@ let with_emu_manager_restore (task: Xenops_task.t) ~hvm ~store_port ~console_por
 	XenguestHelper.with_connection task xenguest_path domid args fds f
 
 let restore_libxc_record cnx domid uuid =
+	XenguestHelper.send_restore cnx "xenguest";
 	let line = XenguestHelper.receive_success cnx in
 	match Stdext.Xstringext.String.split ' ' line with
 	| [ store; console ] ->

xapi-project/xen-api

xc/domain.ml
@@ -818,6 +818,7 @@ let with_emu_manager_restore (task: Xenops_task.task_handle) ~hvm ~store_port ~c
   Emu_manager.with_connection task xenguest_path domid args fds f
 
 let restore_libxc_record cnx domid uuid =
+  Emu_manager.send_restore cnx "xenguest";
   let line = Emu_manager.receive_success cnx in
   match Stdext.Xstringext.String.split ' ' line with
   | [ store; console ] ->