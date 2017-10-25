YarsinCitrix/xenopsd

Plumb through new vGPU data argument in VM_restore backends
Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

lib/xenops_server_plugin.ml
@@ -82,7 +82,7 @@ module type S = sig
 		val wait_shutdown: Xenops_task.t -> Vm.t -> shutdown_request -> float -> bool
 
 		val save: Xenops_task.t -> progress_cb -> Vm.t -> flag list -> data -> data option -> unit
-		val restore: Xenops_task.t -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> string list -> unit
+		val restore: Xenops_task.t -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> data option -> string list -> unit
 
 		val s3suspend: Xenops_task.t -> Vm.t -> unit
 		val s3resume: Xenops_task.t -> Vm.t -> unit

xc/domain.ml
@@ -857,7 +857,10 @@ let consume_qemu_record fd limit domid uuid =
 		end
 	) (fun () -> Unix.close fd2)
 
-let restore_common (task: Xenops_task.t) ~xc ~xs ~hvm ~store_port ~store_domid ~console_port ~console_domid ~no_incr_generationid ~vcpus ~extras xenguest_path domid fd =
+let restore_common (task: Xenops_task.t) ~xc ~xs ~hvm ~store_port ~store_domid
+	~console_port ~console_domid ~no_incr_generationid ~vcpus ~extras
+	xenguest_path domid fd vgpu_fd =
+
 	let module DD = Debug.Make(struct let name = "mig64" end) in
 	let open DD in
 	let uuid = get_uuid ~xc domid in
@@ -946,7 +949,9 @@ let resume (task: Xenops_task.t) ~xc ~xs ~hvm ~cooperative ~qemu_domid domid =
 	resume_post ~xc	~xs domid;
 	if hvm then Device.Dm.resume task ~xs ~qemu_domid domid
 
-let pv_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid ~static_max_kib ~target_kib ~vcpus ~extras xenguest_path domid fd =
+let pv_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid
+	~no_incr_generationid ~static_max_kib ~target_kib ~vcpus ~extras
+	xenguest_path domid fd vgpu_fd =
 
 	(* Convert memory configuration values into the correct units. *)
 	let static_max_mib = Memory.mib_of_kib_used static_max_kib in
@@ -972,7 +977,7 @@ let pv_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_inc
 		~store_port ~store_domid
 		~console_port ~console_domid
 		~no_incr_generationid
-		~vcpus ~extras xenguest_path domid fd in
+		~vcpus ~extras xenguest_path domid fd vgpu_fd in
 
 	let local_stuff = [
 		"serial/0/limit",    string_of_int 65536;
@@ -983,7 +988,9 @@ let pv_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_inc
 	build_post ~xc ~xs ~vcpus ~target_mib ~static_max_mib
 		domid store_mfn store_port local_stuff vm_stuff
 
-let hvm_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid ~static_max_kib ~target_kib ~shadow_multiplier ~vcpus ~timeoffset ~extras xenguest_path domid fd =
+let hvm_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid
+	~no_incr_generationid ~static_max_kib ~target_kib ~shadow_multiplier ~vcpus
+	~timeoffset ~extras xenguest_path domid fd vgpu_fd =
 
 	(* Convert memory configuration values into the correct units. *)
 	let static_max_mib = Memory.mib_of_kib_used static_max_kib in
@@ -1008,7 +1015,7 @@ let hvm_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_in
 		~store_port ~store_domid
 		~console_port ~console_domid
 		~no_incr_generationid
-		~vcpus ~extras xenguest_path domid fd in
+		~vcpus ~extras xenguest_path domid fd vgpu_fd in
 	let local_stuff = [
 		"serial/0/limit",    string_of_int 65536;
 		"console/port",     string_of_int console_port;
@@ -1021,7 +1028,8 @@ let hvm_restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_in
 	build_post ~xc ~xs ~vcpus ~target_mib ~static_max_mib
 		domid store_mfn store_port local_stuff vm_stuff
 
-let restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid ~timeoffset ~extras info xenguest_path domid fd =
+let restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid
+	~no_incr_generationid ~timeoffset ~extras info xenguest_path domid fd vgpu_fd =
 	let restore_fct = match info.priv with
 	| BuildHVM hvminfo ->
 		hvm_restore task ~shadow_multiplier:hvminfo.shadow_multiplier
@@ -1031,7 +1039,7 @@ let restore (task: Xenops_task.t) ~xc ~xs ~store_domid ~console_domid ~no_incr_g
 		in
 	restore_fct ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid
 	            ~static_max_kib:info.memory_max ~target_kib:info.memory_target ~vcpus:info.vcpus ~extras
-	            xenguest_path domid fd
+	            xenguest_path domid fd vgpu_fd
 
 let restore_vgpu (task: Xenops_task.t) ~xc ~xs domid fd vgpu vcpus =
 	let uuid = get_uuid ~xc domid in

xc/domain.mli
@@ -135,21 +135,12 @@ val build: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xs
 (** resume a domain either cooperative or not *)
 val resume: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> hvm: bool -> cooperative: bool -> qemu_domid:int -> domid -> unit
 
-(** restore a PV domain into a fresh domain created with 'make' *)
-val pv_restore: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> static_max_kib:Int64.t 
-          -> target_kib:Int64.t -> vcpus:int -> extras:string list -> string -> domid -> Unix.file_descr
-          -> unit
-
-(** restore an HVM domain from the file descriptor into a fresh domain created
- *  with 'make' *)
-val hvm_restore: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> static_max_kib:Int64.t
-             -> target_kib:Int64.t -> shadow_multiplier:float
-             -> vcpus:int -> timeoffset:string -> extras:string list
-             -> string -> domid -> Unix.file_descr
-             -> unit
-
 (** Restore a domain using the info provided *)
-val restore: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> timeoffset:string -> extras:string list -> build_info -> string -> domid -> Unix.file_descr -> unit
+val restore: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh
+	-> store_domid:int -> console_domid:int -> no_incr_generationid:bool
+	-> timeoffset:string -> extras:string list -> build_info -> string -> domid
+	-> Unix.file_descr -> Unix.file_descr option
+	-> unit
 
 val restore_vgpu: Xenops_task.Xenops_task.t -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh ->
 	domid -> Unix.file_descr -> Xenops_interface.Vgpu.t -> int -> unit

xapi-project/xen-api

lib/xenops_server_plugin.ml
@@ -82,7 +82,7 @@ module type S = sig
     val wait_shutdown: Xenops_task.task_handle -> Vm.t -> shutdown_request -> float -> bool
 
     val save: Xenops_task.task_handle -> progress_cb -> Vm.t -> flag list -> data -> data option -> unit
-    val restore: Xenops_task.task_handle -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> string list -> unit
+    val restore: Xenops_task.task_handle -> progress_cb -> Vm.t -> Vbd.t list -> Vif.t list -> data -> data option -> string list -> unit
 
     val s3suspend: Xenops_task.task_handle -> Vm.t -> unit
     val s3resume: Xenops_task.task_handle -> Vm.t -> unit

xc/domain.ml
@@ -861,7 +861,10 @@ let consume_qemu_record fd limit domid uuid =
       end
     ) (fun () -> Unix.close fd2)
 
-let restore_common (task: Xenops_task.task_handle) ~xc ~xs ~hvm ~store_port ~store_domid ~console_port ~console_domid ~no_incr_generationid ~vcpus ~extras xenguest_path domid fd =
+let restore_common (task: Xenops_task.task_handle) ~xc ~xs ~hvm ~store_port ~store_domid
+    ~console_port ~console_domid ~no_incr_generationid ~vcpus ~extras
+    xenguest_path domid fd vgpu_fd =
+
   let module DD = Debug.Make(struct let name = "mig64" end) in
   let open DD in
   let uuid = get_uuid ~xc domid in
@@ -950,7 +953,9 @@ let resume (task: Xenops_task.task_handle) ~xc ~xs ~hvm ~cooperative ~qemu_domid
   resume_post ~xc	~xs domid;
   if hvm then Device.Dm.resume task ~xs ~qemu_domid domid
 
-let pv_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid ~static_max_kib ~target_kib ~vcpus ~extras xenguest_path domid fd =
+let pv_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid
+    ~no_incr_generationid ~static_max_kib ~target_kib ~vcpus ~extras
+    xenguest_path domid fd vgpu_fd =
 
   (* Convert memory configuration values into the correct units. *)
   let static_max_mib = Memory.mib_of_kib_used static_max_kib in
@@ -976,7 +981,7 @@ let pv_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_dom
       ~store_port ~store_domid
       ~console_port ~console_domid
       ~no_incr_generationid
-      ~vcpus ~extras xenguest_path domid fd in
+      ~vcpus ~extras xenguest_path domid fd vgpu_fd in
 
   let local_stuff = [
     "serial/0/limit",    string_of_int 65536;
@@ -988,7 +993,9 @@ let pv_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_dom
   build_post ~xc ~xs ~vcpus ~target_mib ~static_max_mib
     domid store_mfn store_port local_stuff vm_stuff
 
-let hvm_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid ~static_max_kib ~target_kib ~shadow_multiplier ~vcpus ~timeoffset ~extras xenguest_path domid fd =
+let hvm_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid
+    ~no_incr_generationid ~static_max_kib ~target_kib ~shadow_multiplier ~vcpus
+    ~timeoffset ~extras xenguest_path domid fd vgpu_fd =
 
   (* Convert memory configuration values into the correct units. *)
   let static_max_mib = Memory.mib_of_kib_used static_max_kib in
@@ -1013,7 +1020,7 @@ let hvm_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_do
       ~store_port ~store_domid
       ~console_port ~console_domid
       ~no_incr_generationid
-      ~vcpus ~extras xenguest_path domid fd in
+      ~vcpus ~extras xenguest_path domid fd vgpu_fd in
   let local_stuff = [
     "serial/0/limit",    string_of_int 65536;
     "console/port",     string_of_int console_port;
@@ -1027,7 +1034,8 @@ let hvm_restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_do
   build_post ~xc ~xs ~vcpus ~target_mib ~static_max_mib
     domid store_mfn store_port local_stuff vm_stuff
 
-let restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid ~timeoffset ~extras info xenguest_path domid fd =
+let restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid
+    ~no_incr_generationid ~timeoffset ~extras info xenguest_path domid fd vgpu_fd =
   let restore_fct = match info.priv with
     | BuildHVM hvminfo ->
       hvm_restore task ~shadow_multiplier:hvminfo.shadow_multiplier
@@ -1037,7 +1045,7 @@ let restore (task: Xenops_task.task_handle) ~xc ~xs ~store_domid ~console_domid 
   in
   restore_fct ~xc ~xs ~store_domid ~console_domid ~no_incr_generationid
     ~static_max_kib:info.memory_max ~target_kib:info.memory_target ~vcpus:info.vcpus ~extras
-    xenguest_path domid fd
+    xenguest_path domid fd vgpu_fd
 
 let restore_vgpu (task: Xenops_task.task_handle) ~xc ~xs domid fd vgpu vcpus =
   let uuid = get_uuid ~xc domid in

xc/domain.mli
@@ -135,21 +135,12 @@ val build: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xens
 (** resume a domain either cooperative or not *)
 val resume: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> hvm: bool -> cooperative: bool -> qemu_domid:int -> domid -> unit
 
-(** restore a PV domain into a fresh domain created with 'make' *)
-val pv_restore: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> static_max_kib:Int64.t
-  -> target_kib:Int64.t -> vcpus:int -> extras:string list -> string -> domid -> Unix.file_descr
-  -> unit
-
-(** restore an HVM domain from the file descriptor into a fresh domain created
- *  with 'make' *)
-val hvm_restore: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> static_max_kib:Int64.t
-  -> target_kib:Int64.t -> shadow_multiplier:float
-  -> vcpus:int -> timeoffset:string -> extras:string list
-  -> string -> domid -> Unix.file_descr
-  -> unit
-
 (** Restore a domain using the info provided *)
-val restore: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh -> store_domid:int -> console_domid:int -> no_incr_generationid:bool -> timeoffset:string -> extras:string list -> build_info -> string -> domid -> Unix.file_descr -> unit
+val restore: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh
+  -> store_domid:int -> console_domid:int -> no_incr_generationid:bool
+  -> timeoffset:string -> extras:string list -> build_info -> string -> domid
+  -> Unix.file_descr -> Unix.file_descr option
+  -> unit
 
 val restore_vgpu: Xenops_task.Xenops_task.task_handle -> xc: Xenctrl.handle -> xs: Xenstore.Xs.xsh ->
   domid -> Unix.file_descr -> Xenops_interface.Vgpu.t -> int -> unit