126ea72e1f8b58212687c190b4ae472ccaecd7b4
Edwin Török <edvin.torok@citrix.com>
2017-10-27 17:31:55 +0100

    redirect based on where the VDI is activated.

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -18,7 +18,7 @@ open Datamodel_types
 (* IMPORTANT: Please bump schema vsn if you change/add/remove a _field_.
               You do not have to bump vsn if you change/add/remove a message *)
 let schema_major_vsn = 5
-let schema_minor_vsn = 133
+let schema_minor_vsn = 134
 
 (* Historical schema versions just in case this is useful later *)
 let rio_schema_major_vsn = 5
@@ -89,6 +89,9 @@ let falcon_release_schema_minor_vsn = 120
 
 let inverness_release_schema_major_vsn = 5
 let inverness_release_schema_minor_vsn = 133
+
+let jura_release_schema_major_vsn = 5
+let jura_release_schema_minor_vsn = 134
 
 (* List of tech-preview releases. Fields in these releases are not guaranteed to be retained when
  * upgrading to a full release. *)
@@ -6875,6 +6878,7 @@ let vdi =
          field ~in_product_since:rel_boston ~qualifier:DynamicRO ~ty:Bool ~default_value:(Some (VBool false)) "metadata_latest" "Whether this VDI contains the latest known accessible metadata for the pool";
          field ~lifecycle:[Published, rel_dundee, ""] ~qualifier:DynamicRO ~ty:Bool ~default_value:(Some (VBool false)) "is_tools_iso" "Whether this VDI is a Tools ISO";
          field ~lifecycle:[Published, rel_inverness, ""] ~qualifier:DynamicRO ~ty:Bool ~default_value:(Some (VBool false)) "cbt_enabled" "True if changed blocks are tracked for this VDI" ~doc_tags:[Snapshots];
+         field ~lifecycle:[Prototyped, rel_jura, ""] ~qualifier:DynamicRO ~ty:(Ref _host) ~default_value:(Some (VRef null_ref)) "activated_on" "The host on which this VDI is activated, if any"
        ])
     ()
 
