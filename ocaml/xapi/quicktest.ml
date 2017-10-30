edcb8977a037a0f4338a8af934a34d515c2769ea
Edwin Török <edvin.torok@citrix.com>
2017-10-30 18:17:13 +0000

    quicktest: allow filtering by SR name
    By default some tests are run on all SRs, or only on the first SR.
    
    When debugging it is useful to run tests only on a specific SR (e.g. GFS2),
    it is also useful to filter this by a specific SR name rather than type because
    you could have multiple SRs attached of the same type, and some of them might be
    broken.
    
    When doing component level "medium" testing it'd also be useful to exercise all
    operations on the specific SR we are testing the integration with.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- ocaml/xapi/quicktest.ml
+++ ocaml/xapi/quicktest.ml
@@ -856,6 +856,7 @@ let _ =
     "-iso-sr-path", Arg.String (fun x -> Quicktest_storage.iso_path := x), "Path to ISO SR";
     "-single", Arg.String (fun x -> tests_to_run := [ x ]), Printf.sprintf "Only run one test (possibilities are %s)" (String.concat ", " all_tests) ;
     "-all", Arg.Unit (fun () -> tests_to_run := all_tests), Printf.sprintf "Run all tests (%s)" (String.concat ", " all_tests);
+    "-only-sr", Arg.String (fun x -> Quicktest_storage.only_sr_name := Some x), "Run tests only on SR with specified name";
     "-nocolour", Arg.Clear Quicktest_common.use_colour, "Don't use colour in the output" ]
     (fun x -> match !host, !username, !password with
        | "", _, _ -> host := x; rpc := rpc_remote; using_unix_domain_socket := false;
