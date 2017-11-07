831cc23f1bef840e4fa787fc37efe3bac1783202
Jon Ludlam <jonathan.ludlam@citrix.com>
2017-11-07 16:13:37 +0000

    CP-25621: Add iscsi_iqn to the CLI
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- ocaml/xapi/records.ml
+++ ocaml/xapi/records.ml
@@ -1240,6 +1240,8 @@ let host_record rpc session_id host =
       make_field ~name:"features"
         ~get:(fun () -> String.concat "; " (List.map get_uuid_from_ref (x ()).API.host_features))
         ~get_set:(fun () -> List.map get_uuid_from_ref (x ()).API.host_features) ();
+      make_field ~name:"iscsi_iqn"
+        ~get:(fun () -> (x ()).API.host_iscsi_iqn) ~set:(fun s -> Client.Host.set_iscsi_iqn rpc session_id host s) ();
     ]}
 
 let vdi_record rpc session_id vdi =
