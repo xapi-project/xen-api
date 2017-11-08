a31fdda0d17cbd9479945a5b9ae416db32532435
Jon Ludlam <jonathan.ludlam@citrix.com>
2017-11-08 14:40:00 +0000

    CP-25621: Add a unit test for xapi_host_helpers.ml
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

---
+++ ocaml/xapi/test_host_helpers.ml
@@ -0,0 +1,63 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open OUnit
+
+let setup_test_oc_watcher () =
+  let __context, _ = Test_event_common.event_setup_common () in
+  let calls = ref [] in
+  let test_rpc call =
+    match call.Rpc.name, call.Rpc.params with
+    | "host.set_iscsi_iqn", [_session_id_rpc;host_rpc;value_rpc] ->
+      let host = API.ref_host_of_rpc host_rpc in
+      let v = API.string_of_rpc value_rpc in
+      calls := (host,v) :: !calls;
+      Rpc.{success=true; contents=Rpc.String ""}
+    | _ -> Mock_rpc.rpc __context call
+  in
+  Context.set_test_rpc __context test_rpc;
+  let host1 = !Xapi_globs.localhost_ref in
+  let host2 = Test_common.make_host ~__context () in
+  let watcher = Xapi_host_helpers.InitiatorName.watch_other_configs ~__context 0.0 in
+  let token = watcher "" in
+  (__context, calls, host1, host2, watcher, token)
+
+let test_host1 () =
+  let (__context, calls, host1, host2, watcher, token) = setup_test_oc_watcher () in
+  (* Test1: update other_config:iscsi_iqn on host1, check it appears in the calls list *)
+  Db.Host.add_to_other_config ~__context ~self:host1 ~key:"iscsi_iqn" ~value:"test1";
+  let _token = watcher token in
+  assert_equal !calls [host1, "test1"]
+
+let test_host2 () =
+  let (__context, calls, host1, host2, watcher, token) = setup_test_oc_watcher () in
+  (* Test2: update other_config:iscsi_iqn on host2, check it appears in the calls list *)
+  Db.Host.add_to_other_config ~__context ~self:host2 ~key:"iscsi_iqn" ~value:"test2";
+  let _token = watcher token in
+  assert_equal !calls [host2, "test2"]
+
+let test_different_keys () =
+  (* Test3: verify that setting other other-config keys causes no set *)
+  let (__context, calls, host1, host2, watcher, token) = setup_test_oc_watcher () in
+  Db.Host.add_to_other_config ~__context ~self:host1 ~key:"not_iscsi_iqn" ~value:"test1";
+  let _token = watcher token in
+  assert_equal !calls []
+
+let test =
+  "iscsiinitiator" >:::
+  [
+    "test_host1" >:: test_host1;
+    "test_host2" >:: test_host2;
+    "test_different_keys" >:: test_different_keys;
+  ]
