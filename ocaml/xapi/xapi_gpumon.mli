4e9ed6505ac592591b0ecfe04096af399d7eeeb4
Christian Lindig <christian.lindig@citrix.com>
2017-11-30 22:34:26 +0000

    Add {update,clear}_vgpu_metadata
    This adds two functions to set and clear vGPU metadata for all vGPUs associated with a VM. The intended use case is to store data on VM.suspend and to clear it on VM.resume.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/xapi/xapi_gpumon.mli
+++ ocaml/xapi/xapi_gpumon.mli
@@ -18,6 +18,20 @@ val with_gpumon_stopped :
   ?timeout:float ->
   (unit -> 'a) -> 'a
 
+(** [update_vgpu_metadata] updates the vGPU compatibility metadata of
+ * all vGPUs associated with [vm].  *)
+val update_vgpu_metadata
+  : __context:Context.t
+  -> vm:API.ref_VM
+  -> unit
+
+(** [clear_vgpu_metadata] removes the compatibility metadata from all
+ * VMs associated with [vm] *)
+val clear_vgpu_metadata
+  : __context:Context.t
+  -> vm:API.ref_VM
+  -> unit
+
 module Nvidia : sig
   (** The key used in the metadata assoc-list *)
   val key: string
@@ -30,6 +44,34 @@ module Nvidia : sig
     dbg: string ->
     pgpu_pci_address: string ->
     (string * string) list
+
+  (** [is_nvidia] is true, if [vgpu] is an NVIDIA vGPU *)
+  val is_nvidia
+    : __context:Context.t
+    -> vgpu:API.ref_VGPU    (** valid reference *)
+    -> bool
+
+  (** Obtain opaque compatibility metadata for a vGPU and return
+   *  [(key, metadata)] association list. The metadata is a base-64
+   *  encoded blob that gets passed to functions checking compatibility;
+   *  the key is fixed to [key]. This function must be called on the
+   *  host where the vGPU is assigned to a pGPU.
+   *)
+  val get_vgpu_compatibility_metadata
+    : __context:Context.t
+    -> vgpu:API.ref_VGPU      (** must refer to NVIDIA vGPU *)
+    -> (string * string) list
+
+  (** Predicate [vgpu_pgpu_are_compatible] checks that vGPU and pGPU are
+   * comatible according to their abstract compatibility metadata. This
+   * code can run on any host. If no vGPU or pGPU metadata is
+   * available, compatibility is assumed.
+   *)
+  val vgpu_pgpu_are_compatible
+    : __context:Context.t
+    -> vgpu:API.ref_VGPU
+    -> pgpu:API.ref_PGPU
+    -> bool
 
   (** Check compatibility between a VM's vGPU(s) and another pGPU,
    *  and fail if they are not compatible. This function is assumed
