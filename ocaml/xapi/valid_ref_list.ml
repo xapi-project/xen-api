0c25f497bc7df7e7ceefbf756d1410cd28312549
Gabor Igloi <gabor.igloi@citrix.com>
2017-12-12 11:56:08 +0000

    Valid_ref_list: add iter function
    This function ignores invalid reference errors that happen when
    processing the list elements.
    
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/valid_ref_list.ml
+++ ocaml/xapi/valid_ref_list.ml
@@ -14,6 +14,8 @@ let for_all f l = not (exists (fun x -> not @@ f x) l)
 
 let map f = Stdext.Listext.List.filter_map (default_on_missing_ref (fun x -> Some (f x)) None)
 
+let iter f = List.iter (default_on_missing_ref f ())
+
 let flat_map f l = List.map (default_on_missing_ref f []) l |> List.flatten
 
 let filter_map f l = map f l |> List.filter ((<>) None) |> List.map Xapi_stdext_monadic.Opt.unbox
