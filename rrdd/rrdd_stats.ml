5e09251779b0d3e86f9796959a159acf4bb45f64
Marcello Seri <marcello.seri@citrix.com>
2017-12-12 14:38:46 +0000

    rrdd_stats: rough port oclock -> mtime
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- rrdd/rrdd_stats.ml
+++ rrdd/rrdd_stats.ml
@@ -126,12 +126,12 @@ let string_of_process_memory_info (x: process_memory_info) =
 
 (* Log the initial offset between our monotonic clock and UTC *)
 let initial_offset =
-	Unix.gettimeofday () -. (Int64.to_float (Oclock.gettime Oclock.monotonic) /. 1e9)
+	Unix.gettimeofday () -. (Int64.to_float (Mtime_clock.now_ns ()) /. 1e9)
 
 let print_system_stats () =
 	let mi = string_of_meminfo (meminfo ()) in
 	debug "system stats: %s" mi;
-	let current_offset = Unix.gettimeofday () -. (Int64.to_float (Oclock.gettime Oclock.monotonic) /. 1e9) in
+	let current_offset = Unix.gettimeofday () -. (Int64.to_float (Mtime_clock.now_ns ()) /. 1e9) in
 	debug "Clock drift: %.0f" (current_offset -. initial_offset)
 
 (* Obtains process IDs for the specified program.
