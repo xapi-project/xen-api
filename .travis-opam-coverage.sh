7b6a413f3aa744347d309f52dbbadc1046413192
Edwin Török <edvin.torok@citrix.com>
2018-01-04 18:37:14 +0000

    Update .travis-opam-coverage.sh
    This needs a few workaround due to embedded libpci
    
    coverage: use bash arrays
    
    Based on https://github.com/xapi-project/rrd-transport/commit/93d93c147d138c1edd9c1d367ae440e9db334523
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- .travis-opam-coverage.sh
+++ .travis-opam-coverage.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 # SUMMARY:
 # Builds & tests xapi with coverage in a Ubuntu 16.04 Docker container with
 # OCaml 4.04.2, then uploads the coverage information to coveralls.
 
-set -uex
+set -ex
 
 # Currently there is no way of specifying OPAM depexts for multiple versions of
 # a given disto, and our current depexts only work with Ubuntu >= 16.04, due to
@@ -15,7 +16,7 @@ docker run --rm --volume=$PWD:/mnt --workdir=/mnt \
   --env "TRAVIS=$TRAVIS" \
   --env "TRAVIS_JOB_ID=$TRAVIS_JOB_ID" \
   ocaml/opam:ubuntu-16.04_ocaml-4.04.2 \
-  bash -uex -c '
+  bash -ex -c '
 sudo apt-get update
 
 sudo chown -R $(whoami) .
@@ -25,13 +26,39 @@ opam repository remove default
 opam repository add xs-opam https://github.com/xapi-project/xs-opam.git
 
 # install the dependencies of xapi
+sudo apt install -y libpci-dev
 opam pin add --no-action xapi .
 opam depext --yes xapi
 opam install --deps-only xapi
 
 # build and test xapi with coverage, then submit the coverage information to coveralls
-sudo apt-get install --yes wget
-wget https://raw.githubusercontent.com/simonjbeaumont/ocaml-travis-coveralls/master/travis-coveralls.sh
-COV_CONF="./configure" bash -ex travis-coveralls.sh
+
+opam install -y bisect_ppx ocveralls
+
+export BISECT_ENABLE=YES
+
+ulimit -s 16384
+
+# the test fails when run on debian-stable (it passed on Ubuntu though)
+# disable it when running coverage
+# -p does not work to filter out subdirectories though
+# because the test in pci does not say which package it is part of
+rm -rf ocaml/pci/lib_test
+jbuilder runtest
+
+declare -a outs
+outs=($(find . | grep bisect.*.out))
+bisect-ppx-report -I $(dirname ${outs[1]}) -text report ${outs[@]}
+bisect-ppx-report -I $(dirname ${outs[1]}) -summary-only -text summary ${outs[@]}
+if [ -n "$HTML" ]; then bisect-ppx-report -I $(dirname ${outs[1]}) -html ../html-report ${outs[@]}; fi
+
+if [ -n "$TRAVIS" ]; then
+  echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
+  ocveralls --prefix _build/default ${outs[@]} --send
+else
+  echo "\$TRAVIS not set; displaying results of bisect-report..."
+  cat report
+  cat summary
+fi
 '
 
