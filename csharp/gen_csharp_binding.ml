d96d3e02eaa5a1c14ad29be8e63bd3e1c4a7bfe5
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2018-01-06 18:29:47 +0000

    CA-277216: Check if a parameter is bound before assigning its value to the corresponding field.
    Otherwise the default values assigned when the record was created are reset by
    the framework's default values.
    
    Also: minor document format regression.
    
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- csharp/gen_csharp_binding.ml
+++ csharp/gen_csharp_binding.ml
@@ -516,7 +516,8 @@ namespace XenAPI
 "
   else
     print
-      "                System.Diagnostics.Debug.Assert(false, \"Cannot create instances of this type on the server\");
+      "
+                System.Diagnostics.Debug.Assert(false, \"Cannot create instances of this type on the server\");
                 return \"\";
 ";
 
