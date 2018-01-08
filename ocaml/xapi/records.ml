0554087ebde404873fcd4fcef048e2cfb18fa199
Yang Qian <yang.qian@citrix.com>
2018-01-08 10:51:47 +0800

    fixup! fixup! fixup! CP-25797 Add network SR-IOV model
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- ocaml/xapi/records.ml
+++ ocaml/xapi/records.ml
@@ -229,8 +229,8 @@ let network_sriov_record rpc session_id network_sriov =
     fields =
       [
         make_field ~name:"uuid"                     ~get:(fun () -> (x ()).API.network_sriov_uuid) ();
-        make_field ~name:"physical-pif-uuid"        ~get:(fun () -> get_uuid_from_ref (x ()).API.network_sriov_physical_PIF) ();
-        make_field ~name:"logical-pif-uuid"         ~get:(fun () -> get_uuid_from_ref (x ()).API.network_sriov_logical_PIF) ();
+        make_field ~name:"physical-PIF"        ~get:(fun () -> get_uuid_from_ref (x ()).API.network_sriov_physical_PIF) ();
+        make_field ~name:"logical-PIF"         ~get:(fun () -> get_uuid_from_ref (x ()).API.network_sriov_logical_PIF) ();
         make_field ~name:"requires-reboot"          ~get:(fun () -> string_of_bool (x ()).API.network_sriov_requires_reboot) ();
       ]}
 
