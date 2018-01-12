56102f751b3bdad16f9892e30b90ecfa6bfd28f8
Wei Xie <wei.xie@citrix.com>
2018-01-12 18:10:34 +0800

    Add stub functions
    Signed-off-by: Wei Xie <wei.xie@citrix.com>

--- networkd/network_server.ml
+++ networkd/network_server.ml
@@ -1007,6 +1007,27 @@ module Bridge = struct
 end
 
 module S = Network_interface.Interface_API(Idl.GenServerExn ())
+module Sriov = struct
+	open Xcp_pci
+
+	let enable _ dbg ~name =
+		Debug.with_thread_associated dbg (fun () ->
+			debug "Enable NET-SRIOV by name: %s" name;
+			Ok Modprobe_successful
+		) ()
+
+	let disable _ dbg ~name =
+		Debug.with_thread_associated dbg (fun () ->	
+			debug "Disable NET-SRIOV by name: %s" name;
+			Ok Modprobe_successful
+		) ()
+
+	let make_vf_config _ dbg ~pci_address ~vf_info =
+		Debug.with_thread_associated dbg (fun () ->	
+			let pcibuspath = string_of_address pci_address in
+			debug "Config VF with pci address: %s" pcibuspath;
+		) ()
+end
 
 module PVS_proxy = struct
     open S.PVS_proxy
