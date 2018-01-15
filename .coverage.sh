8bf3576871854f91cfb1c50f5a6fa01ddca27d80
Marcello Seri <marcello.seri@citrix.com>
2018-01-15 13:52:29 +0000

    coverage: fix use of bash arrays
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- .coverage.sh
+++ .coverage.sh
@@ -17,13 +17,13 @@ export BISECT_ENABLE=YES
 jbuilder runtest
 
 outs=($(find . | grep bisect.*.out))
-bisect-ppx-report -I $(dirname ${outs[1]}) -text report $outs
-bisect-ppx-report -I $(dirname ${outs[1]}) -summary-only -text summary $outs
-if [ -n "$HTML" ]; then bisect-ppx-report -I $(dirname ${outs[1]}) -html ../html-report $outs; fi
+bisect-ppx-report -I $(dirname ${outs[1]}) -text report ${outs[@]}
+bisect-ppx-report -I $(dirname ${outs[1]}) -summary-only -text summary ${outs[@]}
+if [ -n "$HTML" ]; then bisect-ppx-report -I $(dirname ${outs[1]}) -html ../html-report ${outs[@]}; fi
 
 if [ -n "$TRAVIS" ]; then
   echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
-  ocveralls --prefix _build/default $outs --send
+  ocveralls --prefix _build/default ${outs[@]} --send
 else
   echo "\$TRAVIS not set; displaying results of bisect-report..."
   cat report
