8ab1ac761bdb67e6e53164e78d41ffab610288ea
Marcello Seri <marcello.seri@citrix.com>
2018-01-31 14:50:14 +0000

    coverage: be less smart
    Copy over all the files, include all folders and avoid using arrays.
    
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- .coverage.sh
+++ .coverage.sh
@@ -8,23 +8,33 @@ mkdir -p $COVERAGE_DIR
 pushd $COVERAGE_DIR
 if [ -z "$KEEP" ]; then trap "popd; rm -rf $COVERAGE_DIR" EXIT; fi
 
+# copy over everything
 $(which cp) -r ../* .
 
+# prepare the environment
+eval `opam config env`
 opam install bisect_ppx ocveralls -y
 
+# run the tests with coverage enabled
 export BISECT_ENABLE=YES
 jbuilder runtest
 
-outs=$(find . | grep bisect.*.out)
-bisect-ppx-report -I $(dirname $outs[1]) -text report $outs
-bisect-ppx-report -I $(dirname $outs[1]) -summary-only -text summary $outs
-if [ -n "$HTML" ]; then bisect-ppx-report -I $(dirname $outs[1]) -html ../html-report $outs; fi
+# move all bisect files here
+find . -name 'bisect*.out' -exec mv {} . \;
+
+DIRECTORIES=$(find _build/default -type d -not -path '*/\.*')
+printf -v INCLUDES " -I %s" $DIRECTORIES
+
+bisect-ppx-report bisect*.out $INCLUDES -text report
+bisect-ppx-report bisect*.out $INCLUDES -summary-only -text summary
+bisect-ppx-report bisect*.out $INCLUDES -html report-html
 
 if [ -n "$TRAVIS" ]; then
   echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
-  ocveralls --prefix _build/default $outs --send
+  ocveralls --prefix _build/default bisect*.out --send
 else
-  echo "\$TRAVIS not set; displaying results of bisect-report..."
+  echo "\$TRAVIS not set; displaying results of bisect-ppx-report..."
   cat report
   cat summary
 fi
+
