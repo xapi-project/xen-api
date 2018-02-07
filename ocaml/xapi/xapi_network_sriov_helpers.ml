ec6f6bbc3396da5ba1c5f98761e22477086b6f79
Yang Qian <yang.qian@citrix.com>
2018-02-07 15:58:11 +0800

    CP-25699 call networkd when enable/disable sriov
    1. call networkd to enable/disable sriov in bring_pif_up/bring_pif_down
    2. when disabling sriov, only call networkd when all of PIFs that use same
    driver in the host has disabled sriov
    
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- ocaml/xapi/xapi_network_sriov_helpers.ml
+++ ocaml/xapi/xapi_network_sriov_helpers.ml
@@ -17,6 +17,87 @@ open Db_filter_types
 module D = Debug.Make(struct let name="xapi" end)
 open D
 
+let sriov_bring_up ~__context ~self =
+  let open Network_interface in
+  let update_sriov_with_result result =
+    let mode, require_reboot = match result with
+      | Sysfs_successful -> `sysfs, false
+      | Modprobe_successful -> `modprobe, false
+      | Modprobe_successful_requires_reboot -> `modprobe, true
+    in
+    let sriov = List.hd (Db.PIF.get_sriov_logical_PIF_of ~__context ~self) in
+    let physical_pif = Db.Network_sriov.get_physical_PIF ~__context ~self:sriov in
+    info "Enable network sriov on PIF %s successful, mode: %s need_reboot: %b" (Ref.string_of physical_pif) (Record_util.network_sriov_configuration_mode_to_string mode) require_reboot;
+    Db.Network_sriov.set_configuration_mode ~__context ~self:sriov ~value:mode;
+    Db.Network_sriov.set_requires_reboot ~__context ~self:sriov ~value:require_reboot;
+    Db.PIF.set_currently_attached ~__context ~self ~value:(not require_reboot)
+  in
+  let device = Db.PIF.get_device ~__context ~self in
+  begin
+    match Net.Sriov.enable "enable_sriov" ~name:device with
+    | Ok result -> update_sriov_with_result result
+    | Error error ->
+      Db.PIF.set_currently_attached ~__context ~self ~value:false;
+      raise (Api_errors.(Server_error (network_sriov_enable_failed, [Ref.string_of self; error])))
+  end;
+  update_sriovs ~__context
+
+let require_operation_on_pci_device ~__context ~self =
+  let is_sriov_enabled ~pif =
+    match Db.PIF.get_sriov_logical_PIF_of ~__context ~self:pif with
+    | [] -> false
+    | sriov :: _ ->
+      Db.PIF.get_currently_attached ~__context ~self:pif = true || Db.Network_sriov.get_requires_reboot ~__context ~self:sriov = true
+  in
+  if is_sriov_enabled ~pif:self then begin
+    let sriov = List.hd (Db.PIF.get_sriov_logical_PIF_of ~__context ~self) in
+    match Db.Network_sriov.get_configuration_mode ~__context ~self:sriov with
+    | `sysfs -> true
+    | `unknown -> false
+    | `modprobe ->
+      let host = Db.PIF.get_host ~__context ~self in
+      let physical_pif = Db.Network_sriov.get_physical_PIF ~__context ~self:sriov in
+      let pci = Db.PIF.get_PCI ~__context ~self:physical_pif in
+      let driver_name = Db.PCI.get_driver_name ~__context ~self:pci in
+      (* Filter the network SR-IOV logical PIF on local host which *)
+      (* 1. has same driver name with me *)
+      (* 2. PIF.currently_attached = `true` or Network_sriov.requires_reboot = `true`. Aka the PIF that enabled SR-IOV or will enable SR-IOV after reboot *)
+      (* If the final list just contains me, should call networkd to disable SR-IOV for the device. *)
+      Db.PIF.get_records_where ~__context ~expr:(And (
+          Eq (Field "host", Literal (Ref.string_of host)),
+          Not (Eq (Field "sriov_logical_PIF_of", Literal "()"))
+        ))
+      |> List.filter (fun (_, pif_rec) ->
+          let sriov = List.hd pif_rec.API.pIF_sriov_logical_PIF_of in
+          let physical_pif = Db.Network_sriov.get_physical_PIF ~__context ~self:sriov in
+          let pci = Db.PIF.get_PCI ~__context ~self:physical_pif in
+          Db.PCI.get_driver_name ~__context ~self:pci = driver_name
+        )
+      |> List.filter (fun (pif_ref, pif_rec) ->
+          is_sriov_enabled ~pif:pif_ref
+        )
+      |> List.length
+      |> (=) 1
+  end
+  else false
+
+let sriov_bring_down ~__context ~self =
+  let sriov = List.hd (Db.PIF.get_sriov_logical_PIF_of ~__context ~self) in
+  let physical_pif = Db.Network_sriov.get_physical_PIF ~__context ~self:sriov in
+  if require_operation_on_pci_device ~__context ~self then begin
+    debug "Disable network sriov on pci device. PIF: %s" (Ref.string_of self);
+    let open Network_interface in
+    let device = Db.PIF.get_device ~__context ~self in
+    match Net.Sriov.disable "disable_sriov" ~name:device with
+    | Ok -> ()
+    | Error error ->
+      raise (Api_errors.(Server_error (network_sriov_disable_failed, [Ref.string_of self; error])))
+  end;
+  info "Disable network sriov on PIF %s successful" (Ref.string_of physical_pif);
+  Db.PIF.set_currently_attached ~__context ~self ~value:false;
+  Db.Network_sriov.set_requires_reboot ~__context ~self:sriov ~value:false;
+  update_sriovs ~__context
+
 let is_device_underneath_same_type ~__context pif1 pif2 =
   let get_device_info pif =
     let pci = Db.PIF.get_PCI ~__context ~self:pif in
