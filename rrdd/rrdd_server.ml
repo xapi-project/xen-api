e3ef02001ba34d312629ef6ff9003354386c4310
Akanksha Mathur <akanksha.mathur@citrix.com>
2018-02-14 12:31:48 +0000

    CP-26583: Update error handling to use PPX-based idl
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

--- rrdd/rrdd_server.ml
+++ rrdd/rrdd_server.ml
@@ -29,7 +29,7 @@ let archive_sr_rrd _ ~(sr_uuid : string) : string =
       try (Hashtbl.find sr_rrds sr_uuid)
       with Not_found ->
         let msg = Printf.sprintf "No RRD found for SR: %s." sr_uuid in
-        raise (Archive_failed(msg))
+        raise (Rrdd_error (Archive_failed(msg)))
     ) in
   try
     archive_rrd_internal ~uuid:sr_uuid ~rrd:sr_rrd.rrd ();
@@ -37,12 +37,12 @@ let archive_sr_rrd _ ~(sr_uuid : string) : string =
       Filename.concat  Rrdd_libs.Constants.rrd_location (sr_uuid ^ ".gz") in
     if not (Xapi_stdext_unix.Unixext.file_exists archive_path) then begin
       let msg = Printf.sprintf "Archive not found: %s." archive_path in
-      raise (Archive_failed(msg))
+      raise (Rrdd_error (Archive_failed(msg)))
     end;
     archive_path
   with e ->
     let msg = Printf.sprintf "Exception raised: %s." (Printexc.to_string e) in
-    raise (Archive_failed(msg))
+    raise (Rrdd_error (Archive_failed(msg)))
 
 let push_sr_rrd _ ~(sr_uuid : string) ~(path : string) : unit =
   try
