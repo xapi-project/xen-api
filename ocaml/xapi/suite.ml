6937cf29893c8047f7cae91b9b2f7fd5d84c54fd
Gabor Igloi <gabor.igloi@citrix.com>
2018-02-16 14:23:16 +0000

    Convert Test_vdi_cbt to Alcotest
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/suite.ml
+++ ocaml/xapi/suite.ml
@@ -27,7 +27,6 @@ let base_suite =
     Test_pool_db_backup.test;
     Test_xapi_db_upgrade.test;
     Test_ca91480.test;
-    Test_vdi_cbt.test;
     Test_ha_vm_failover.test;
     Test_map_check.test;
     Test_pool_apply_edition.test;
