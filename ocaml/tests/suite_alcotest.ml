ffaf40e1c36d65443cdbfe45d9ad40df32f33d86
Gabor Igloi <gabor.igloi@citrix.com>
2018-02-23 09:51:41 +0000

    Convert Test_basic to Alcotest
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/tests/suite_alcotest.ml
+++ ocaml/tests/suite_alcotest.ml
@@ -11,5 +11,6 @@ let () =
     ; "Test_host", Test_host.test
     ; "Test_vdi_cbt", Test_vdi_cbt.test
     ; "Test_db_lowlevel", Test_db_lowlevel.test
-    ; "Test_vlan", Test_vlan.test;
+    ; "Test_vlan", Test_vlan.test
+    ; "Test_basic", Test_basic.test
     ]
--- ocaml/tests/test_basic.ml
+++ ocaml/tests/test_basic.ml
@@ -12,11 +12,7 @@
  * GNU Lesser General Public License for more details.
  *)
 
-open OUnit
 open Test_common
-
-let test_always_pass () = assert_equal 1 1
-let test_always_fail () = skip "This will fail" ; assert_equal 1 0
 
 let test_mock_db () =
   let __context = Mock.make_context_with_new_db "Mock context" in
@@ -27,19 +23,7 @@ let test_mock_db () =
   ignore (Db.Blob.get_record ~__context ~self:blob_ref) ;
   ignore (Db.VM.get_all_records ~__context) ;
   let blob_name = Db.Blob.get_name_label ~__context ~self:blob_ref in
-  assert_equal blob_name "BLOB"
-
-let test_assert_licensed_storage_motion () = skip "TODO" ;
-  let __context = Mock.make_context_with_new_db "Mock context" in
-  let licensed = try Xapi_vm_migrate.assert_licensed_storage_motion ~__context; true
-    with _ -> false in
-  assert_bool "Not licensed for SXM" licensed
+  Alcotest.(check string) "blob name_label" blob_name "BLOB"
 
 let test =
-  "test_basic" >:::
-  [
-    "test_always_pass" >:: test_always_pass ;
-    "test_always_fail" >:: test_always_fail ;
-    "test_mock_db" >:: test_mock_db ;
-    "test_assert_licensed_storage_motion" >:: test_assert_licensed_storage_motion ;
-  ]
+  [ "test_mock_db", `Quick, test_mock_db ]
