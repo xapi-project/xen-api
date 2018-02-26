9624602eadddbfb8988b6e589356216a2ad4de51
Edwin Török <edvin.torok@citrix.com>
2018-02-26 16:50:09 +0000

    CP-26340: SR.probe changes to match xcp-idl
    We have 3 categories of probe results depending where you use it:
    * attachable: suitable for SR.attach
    * creatable: suitable for SR.create
    * incomplete: suitable for further SR.probe
    
    This is done by converting the response from SMAPIv3 plugin, which returns an
    SR, and a complete boolean flag.
    Reject with an error incompatible results
    (such as a configuration that is both complete and has an SR,
     a complete configuration without a uuid,
     or an incomplete configuration with a uuid).
    "complete" in SMAPIv3 means it is usable for SR.create.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- main.ml
+++ main.ml
@@ -568,29 +568,47 @@ let process root_dir name =
       let open Deferred.Result.Monad_infix in
       fork_exec_rpc root_dir (script root_dir name `Volume "SR.probe") args Xapi_storage.Volume.Types.SR.Probe.Out.t_of_rpc
       >>= fun response ->
-      let srs = List.filter_map ~f:(fun probe_result ->
-        match probe_result.sr with
-        | Some sr_stat -> Some (sr_stat.Xapi_storage.Volume.Types.sr, {
-          Storage_interface.name_label = sr_stat.Xapi_storage.Volume.Types.name;
-        name_description = sr_stat.Xapi_storage.Volume.Types.description;
-        total_space = sr_stat.Xapi_storage.Volume.Types.total_space;
-        free_space = sr_stat.Xapi_storage.Volume.Types.free_space;
-        clustered = sr_stat.Xapi_storage.Volume.Types.clustered;
-        health = match sr_stat.Xapi_storage.Volume.Types.health with
-          | Xapi_storage.Volume.Types.Healthy _ -> Healthy
-          | Xapi_storage.Volume.Types.Recovering _ -> Recovering
-        });
-        | None -> None
-      ) response in
-      let uris = List.filter_map ~f:(fun probe_result ->
-        (* FIXME *)
-        if probe_result.complete then
-          Some (Jsonrpc.to_string (R.Dict (List.Assoc.map ~f:R.rpc_of_string probe_result.configuration)))
-        else
-          None
-      ) response in
-      let result = Storage_interface.(Probe { srs; uris }) in
-      Deferred.Result.return (R.success (Args.SR.Probe.rpc_of_response result))
+        let pp_probe_result () probe_result =
+          Xapi_storage.Volume.Types.SR.Probe.Out.rpc_of_t [probe_result] |>
+          Jsonrpc.to_string
+        in
+        response
+        |> List.map ~f:(fun probe_result ->
+          let uuid = List.Assoc.find probe_result.Xapi_storage.Volume.Types.configuration ~equal:String.equal "uuid" in
+          let open Deferred.Or_error in
+          match probe_result.Xapi_storage.Volume.Types.sr, probe_result.Xapi_storage.Volume.Types.complete, uuid with
+          | Some sr_stat, false, Some _uuid ->
+              return (`Attachable (probe_result.configuration, {
+                  Storage_interface.name_label = sr_stat.Xapi_storage.Volume.Types.name;
+                  name_description = sr_stat.Xapi_storage.Volume.Types.description;
+                  total_space = sr_stat.Xapi_storage.Volume.Types.total_space;
+                  free_space = sr_stat.Xapi_storage.Volume.Types.free_space;
+                  clustered = sr_stat.Xapi_storage.Volume.Types.clustered;
+                  health = match sr_stat.Xapi_storage.Volume.Types.health with
+                  | Xapi_storage.Volume.Types.Healthy _ -> Healthy
+                  | Xapi_storage.Volume.Types.Recovering _ -> Recovering
+                }));
+          | Some _sr, true, _ ->
+              errorf "A configuration cannot be suitable for both SR.create and SR.attach: %a" pp_probe_result probe_result
+          | Some _sr, _, None ->
+              errorf "A configuration is not attachable without a uuid: %a" pp_probe_result probe_result
+          | None, false, Some _uuid ->
+              errorf "A configuration with a uuid cannot be incomplete: %a" pp_probe_result probe_result
+          | None, false, None ->
+              return (`Incomplete probe_result.configuration)
+          | None, true, Some _uuid ->
+              return (`Creatable probe_result.configuration)
+          | None, true, None ->
+              errorf "A configuration cannot be complete without a uuid: %a" pp_probe_result probe_result)
+        |> Deferred.Or_error.combine_errors
+        |> Deferred.Result.map_error ~f:(fun err -> err |> Error.to_string_hum |> R.rpc_of_string)
+        >>>= fun results ->
+        let init = { attachable = []; creatable = []; incomplete = [] } in
+        List.fold_left ~init results ~f:(fun response -> function
+          | `Attachable x -> { response with attachable = x :: response.attachable }
+          | `Creatable x -> { response with creatable = x :: response.creatable }
+          | `Incomplete x -> { response with incomplete = x :: response.incomplete })
+        |> (fun x -> Storage_interface.Probe x) |> Args.SR.Probe.rpc_of_response |> R.success |> Deferred.Result.return
     end
   | { R.name = "SR.create"; R.params = [ args ] } ->
     let args = Args.SR.Create.request_of_rpc args in
