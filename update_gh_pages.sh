d241a163bd58d15164cf755a2648628c27d289df
Gabor Igloi <gabor.igloi@citrix.com>
2018-02-27 15:34:38 +0000

    Fix gh-pages generation
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
+set -ex
 
 make html
 rm -fr /tmp/doc
-rsync -av doc/gen/ /tmp/doc/
+rsync -av _build/default/doc/gen/ /tmp/doc/
 git checkout gh-pages
 mv .git /tmp/doc/
 rsync --delete -av /tmp/doc/ .
