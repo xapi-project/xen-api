480887d99bdff695ff684b97ecf08affc1e8168f
Akanksha Mathur <akanksha.mathur@citrix.com>
2018-02-27 18:10:03 +0000

    Port test_cluster to alcotest
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

--- ocaml/xapi/suite.ml
+++ ocaml/xapi/suite.ml
@@ -70,7 +70,6 @@ let base_suite =
     Test_pusb.test;
     Test_cluster_host.test;
     Test_clustering_allowed_operations.test;
-    Test_cluster.test;
     Test_host_helpers.test;
     Test_clustering.test;
   ]
