7061623c73e97d34b901196f0168cf6d1356f8a8
Gabor Igloi <gabor.igloi@citrix.com>
2018-03-05 16:21:48 +0000

    CP-27591: Reorganize libraries
    Now the generator module provides the library too, since the bindings
    are generated by ocaml-rpc itself.
    
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- generator/src/main.ml
+++ generator/src/main.ml
@@ -5,7 +5,7 @@ let _ =
 
   List.iter
     (fun api ->
-       with_output_file (Printf.sprintf "../../../../python/xapi/storage/api/v4/%s.py" api.Codegen.Interfaces.name)
+       with_output_file (Printf.sprintf "./xapi/storage/api/v4/%s.py" api.Codegen.Interfaces.name)
          (fun oc ->
             let p = Pythongen.of_interfaces api |> Pythongen.string_of_ts in
             output_string oc p
