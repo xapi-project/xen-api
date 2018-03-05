0f8e34dd1e9f17a783a9dbec0c0219ef19a0d568
Akanksha Mathur <16429238+minishrink@users.noreply.github.com>
2018-03-05 10:30:38 +0000

    Port Test_clustering and Test_clustering_allowed_operations to Alcotest
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

--- ocaml/tests/suite_alcotest.ml
+++ ocaml/tests/suite_alcotest.ml
@@ -4,6 +4,7 @@ let () =
   (* Alcotest hides the standard output of successful tests,
      so we will probably not exceed the 4MB limit in Traivs *)
   Debug.log_to_stdout ();
+
   Alcotest.run "Base suite"
     [ "Test_valid_ref_list", Test_valid_ref_list.test
     ; "Test_vdi_allowed_operations", Test_vdi_allowed_operations.test
@@ -17,4 +18,7 @@ let () =
     ; "Test_cluster", Test_cluster.test
     ; "Test_cluster_host", Test_cluster_host.test
     ; "Test_client", Test_client.test
+    ; "Test_clustering", Test_clustering.test
+    ; "Test_clustering_allowed_operations", Test_clustering_allowed_operations.test
     ]
+
