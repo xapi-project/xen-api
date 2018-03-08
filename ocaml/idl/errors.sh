2fcf58cdfd1a51289a7536eb84ff5eebe93a4097
Marcello Seri <marcello.seri@citrix.com>
2018-03-08 17:36:58 +0000

    Further unused files cleanup
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- ocaml/idl/errors.sh
+++ ocaml/idl/errors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in `cat api_errors.ml | grep let | awk '{print $2}' | sort ` ; 
+for f in `cat ../xapi-consts/api_errors.ml | grep let | awk '{print $2}' | sort ` ; 
 do 
   if grep $f datamodel.ml >/dev/null 2>&1 ; 
       then echo "$f yes" ; 
