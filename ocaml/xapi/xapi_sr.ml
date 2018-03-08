ba06386ef80e49a14560a89650666c75ac1a00ba
Gabor Igloi <gabor.igloi@citrix.com>
2018-03-08 14:14:05 +0000

    Add SR.probe_ext
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- ocaml/xapi/xapi_sr.ml
+++ ocaml/xapi/xapi_sr.ml
@@ -244,6 +244,53 @@ let probe = call_probe ~f:(
       Buffer.contents buf
   )
 
+let probe_ext ~__context ~host ~device_config ~_type ~sm_config =
+  let to_xenapi_sr_health =
+    let open Storage_interface in
+    function Healthy -> `healthy | Recovering -> `recovering
+  in
+  let to_xenapi_sr_stat Storage_interface.{
+      name_label;
+      name_description;
+      total_space;
+      free_space;
+      clustered;
+      health} =
+    API.{
+      sr_stat_name_label = name_label;
+      sr_stat_name_description = name_description;
+      sr_stat_total_space = total_space;
+      sr_stat_free_space = free_space;
+      sr_stat_clustered = clustered;
+      sr_stat_health = to_xenapi_sr_health health
+    }
+  in
+  let to_xenapi_probe_result Storage_interface.{configuration; complete; sr; extra_info} =
+    API.{
+      probe_result_configuration = configuration;
+      probe_result_complete = complete;
+      probe_result_sr = Xapi_stdext_monadic.Opt.map to_xenapi_sr_stat sr;
+      probe_result_extra_info = extra_info
+    }
+  in
+  debug "SR.probe sm_config=[ %s ]" (String.concat "; " (List.map (fun (k, v) -> k ^ " = " ^ v) sm_config));
+  let _type = String.lowercase_ascii _type in
+  let open Storage_interface in
+  let open Storage_access in
+
+  let queue = !Storage_interface.queue_name ^ "." ^ _type in
+  let uri () = Storage_interface.uri () ^ ".d/" ^ _type in
+  let rpc = external_rpc queue uri in
+  let module Client = Storage_interface.Client(struct let rpc = rpc end) in
+  let dbg = Context.string_of_task __context in
+
+  transform_storage_exn
+    (fun () ->
+       match Client.SR.probe ~dbg ~queue ~device_config ~sm_config with
+       | Raw x -> raise Api_errors.(Server_error (sr_operation_not_supported, []))
+       | Probe results -> List.map to_xenapi_probe_result results
+    )
+
 (* Create actually makes the SR on disk, and introduces it into db, and creates PBD record for current host *)
 let create  ~__context ~host ~device_config ~(physical_size:int64) ~name_label ~name_description
     ~_type ~content_type ~shared ~sm_config =
