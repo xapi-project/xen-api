92db01a3eefd41d2957d2ae7dc5121bb11ac8e02
Akanksha Mathur <akanksha.mathur@citrix.com>
2018-03-26 10:20:53 +0000

    Whitespace change
    Add a space between arrow and text
    
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

--- ocaml/xapi/message_forwarding.ml
+++ ocaml/xapi/message_forwarding.ml
@@ -64,7 +64,7 @@ let call_slave_with_session remote_rpc_fn __context host (task_opt: API.ref_task
   let hostname = Db.Host.get_address ~__context ~self:host in
   let session_id = Xapi_session.login_no_password ~__context ~uname:None ~host ~pool:true ~is_local_superuser:true ~subject:(Ref.null) ~auth_user_sid:"" ~auth_user_name:"" ~rbac_permissions:[] in
   Pervasiveext.finally
-    (fun ()->f session_id (remote_rpc_fn __context hostname task_opt))
+    (fun () -> f session_id (remote_rpc_fn __context hostname task_opt))
     (fun () -> Xapi_session.destroy_db_session ~__context ~self:session_id)
 
 let call_slave_with_local_session remote_rpc_fn __context host (task_opt: API.ref_task option) f =
