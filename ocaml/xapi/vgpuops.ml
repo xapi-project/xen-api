720cf003cc273444de82e6b8f12c4ce1da1b5251
Wei Xie <wei.xie@citrix.com>
2018-04-12 19:46:54 +0800

    CA-286165: Only non-VF PCI need to update dependencies
    Signed-off-by: Wei Xie <wei.xie@citrix.com>

--- ocaml/xapi/vgpuops.ml
+++ ocaml/xapi/vgpuops.ml
@@ -105,6 +105,7 @@ let add_pcis_to_vm ~__context host vm pci =
   let devs : ((int * (int * int * int * int))) list = List.rev (snd (List.fold_left (fun (i, acc) pci -> i + 1, (i, pci) :: acc) (0, []) devs)) in
   (* Update VM other_config for PCI passthrough *)
   let value = String.concat "," (List.map Pciops.to_string devs) in
+  debug "Adding PCIs to a VM with 'other config': %s" value; 
   Db.VM.add_to_other_config ~__context ~self:vm ~key:Xapi_globs.vgpu_pci ~value
 
 let reserve_free_virtual_function ~__context vm pf =
