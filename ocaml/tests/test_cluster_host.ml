2222fd76e14c8aaf61290f3b7453684aa65892a5
Akanksha Mathur <akanksha.mathur@citrix.com>
2018-04-30 16:36:50 +0100

    CP-26912: Update clustering tests with Cluster_host.PIF and Cluster.network changes
    CP-26912: Update uses of Cluster.(pool_)create in tests
    
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

--- ocaml/tests/test_cluster_host.ml
+++ ocaml/tests/test_cluster_host.ml
@@ -17,13 +17,12 @@ open Xapi_cluster_host
 let create_cluster ~__context pool_auto_join =
   let cluster_ref = Ref.make () in
   let cluster_uuid = Uuidm.to_string (Uuidm.create `V4) in
-  let network = Test_common.make_network ~__context () in
-  Db.Cluster.create ~__context ~ref:cluster_ref ~uuid:cluster_uuid ~network ~cluster_token:"token"
+  Db.Cluster.create ~__context ~ref:cluster_ref ~uuid:cluster_uuid ~cluster_token:"token"
     ~cluster_stack:Constants.default_smapiv3_cluster_stack ~token_timeout:5000L ~token_timeout_coefficient:1000L ~allowed_operations:[]
     ~current_operations:[] ~pool_auto_join ~cluster_config:[] ~other_config:[];
   cluster_ref
 
-let assert_ref_option =
+let check_cluster_option =
   Alcotest.(check (option (Alcotest_comparators.ref ()) ))
 
 let test_dbsync_join () =
@@ -31,14 +30,14 @@ let test_dbsync_join () =
   let cluster = create_cluster ~__context true in
   let localhost = Helpers.get_localhost ~__context in
   let result = sync_required ~__context ~host:localhost in
-  assert_ref_option "Cluster option" result (Some (cluster))
+  check_cluster_option "test_dbsync_join" (Some (cluster)) result
 
 let test_dbsync_nojoin () =
   let __context = Test_common.make_test_database () in
   let _cluster = create_cluster ~__context false in
   let localhost = Helpers.get_localhost ~__context in
   let result = sync_required ~__context ~host:localhost in
-  assert_ref_option "Cluster option" result None
+  check_cluster_option "test_dbsync_nojoin" None result
 
 let pif_plug_rpc __context call =
   match call.Rpc.name, call.Rpc.params with
@@ -48,12 +47,13 @@ let pif_plug_rpc __context call =
     let self = ref_PIF_of_rpc self_rpc in
     Db.PIF.set_currently_attached ~__context ~self ~value:true;
     Rpc.{success=true; contents = Rpc.String ""}
-  | "Cluster_host.create", [session_id_rpc;cluster_rpc;host_rpc] ->
+  | "Cluster_host.create", [session_id_rpc;cluster_rpc;host_rpc;pif_rpc] ->
     let open API in
     let _session_id = ref_session_of_rpc session_id_rpc in
     let cluster = ref_Cluster_of_rpc cluster_rpc in
     let host = ref_host_of_rpc host_rpc in
-    ignore(Test_common.make_cluster_host ~__context ~cluster ~host ());
+    let pIF = ref_PIF_of_rpc pif_rpc in
+    ignore(Test_common.make_cluster_host ~__context ~cluster ~host ~pIF ());
     Rpc.{success=true; contents = Rpc.String ""}
   | _ -> failwith "Unexpected RPC"
 
@@ -61,40 +61,45 @@ let pif_plug_rpc __context call =
 let test_fix_prereq () =
   let __context = Test_common.make_test_database () in
   Context.set_test_rpc __context (pif_plug_rpc __context);
-  let exn = "we_havent_decided_on_the_exception_yet" in
-  let cluster = create_cluster ~__context true in
-  let network = Db.Cluster.get_network ~__context ~self:cluster in
+  let network = Test_common.make_network ~__context () in
   let localhost = Helpers.get_localhost ~__context in
   let pifref = Test_common.make_pif ~__context ~network ~host:localhost () in
   let pif = Xapi_clustering.pif_of_host ~__context network localhost in
-  Alcotest.check_raises "Should fail when checking PIF prequisites"
-    (Failure exn)
-    (fun () ->
-      try
-        Xapi_cluster_host.fix_pif_prerequisites __context pif
-      with _ ->
-        failwith exn);
+  Alcotest.check_raises
+    "Should fail when checking PIF prequisites"
+    Api_errors.(Server_error (pif_has_no_network_configuration, [ Ref.string_of pifref ]))
+    (fun () -> Xapi_cluster_host.fix_pif_prerequisites __context pif);
   Db.PIF.set_IP ~__context ~self:pifref ~value:"1.1.1.1";
   let pif = Xapi_clustering.pif_of_host ~__context network localhost in
   Xapi_cluster_host.fix_pif_prerequisites ~__context pif;
   let pif = Xapi_clustering.pif_of_host ~__context network localhost in
-  Alcotest.(check unit) "Assert PIF prerequisites without error"
-    (Xapi_clustering.assert_pif_prerequisites pif) ()
+  Alcotest.(check unit)
+    "PIF prerequisites have now been fixed"
+    () (Xapi_clustering.assert_pif_prerequisites pif)
 
 let test_create_as_necessary () =
   let __context = Test_common.make_test_database () in
   Context.set_test_rpc __context (pif_plug_rpc __context);
   let cluster = create_cluster ~__context true in
-  let network = Db.Cluster.get_network ~__context ~self:cluster in
   let localhost = Helpers.get_localhost ~__context in
+  let network = Test_common.make_network ~__context () in
   let pifref = Test_common.make_pif ~__context ~network ~host:localhost () in
   Db.PIF.set_IP ~__context ~self:pifref ~value:"1.1.1.1";
   let _pif = Xapi_clustering.pif_of_host ~__context network localhost in
   let result = sync_required ~__context ~host:localhost in
-  assert_ref_option "Cluster option" result (Some cluster);
+  check_cluster_option "sync_required without an existing cluster_host" (Some cluster) result;
+  Alcotest.check_raises
+    "create_as_necessary should fail if autojoin is set and the pool master has no cluster_host"
+    Api_errors.(Server_error (internal_error,
+      [ Printf.sprintf "No cluster_host master found for cluster %s" (Ref.string_of cluster) ]))
+    (fun () -> Xapi_cluster_host.create_as_necessary ~__context ~host:localhost);
+  let _ = Test_common.make_cluster_host ~__context ~pIF:(fst _pif) ~host:(Helpers.get_master ~__context) ~cluster () in
   Xapi_cluster_host.create_as_necessary ~__context ~host:localhost;
   let result = sync_required ~__context ~host:localhost in
-  assert_ref_option "Cluster option" result None
+  check_cluster_option "sync_required with an existing cluster_host" None result;
+  let host = Test_common.make_host ~__context () in
+  let result = sync_required ~__context ~host in
+  check_cluster_option "sync_required with an existing cluster_host on master but not given host" (Some cluster) result
 
 (* CA-275728 *)
 let test_destroy_forbidden_when_sr_attached () =
