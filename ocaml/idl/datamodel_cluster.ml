0db0d498324f982ec44aa50d8def3ac3d7a245db
Edwin Török <edvin.torok@citrix.com>
2018-05-24 13:42:29 +0100

    CA-290471: fix wrong token timeout in XenCenter
    Use same units in the object and in the API.
    The timeout set by XenCenter 20000s, which is wrong, it should've been
    20s (which is what you get from the CLI).
    XenCenter creates an object and then reads the field to find out the
    default, because the SDK doesn't expose the defaults for parameters.
    
    So we should really use the same units for API parameters and internal
    object fields. Especially that this field is not used by XAPI at all,
    it is only there so that the admin can inspect what values were used
    to create the cluster.
    
    Changing this field will not change the actual timeouts used by the
    cluster, because the timeout values cannot be changed at runtime.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- ocaml/idl/datamodel_cluster.ml
+++ ocaml/idl/datamodel_cluster.ml
@@ -171,13 +171,19 @@ let t =
           ~ty:Bool "pool_auto_join" ~default_value:(Some (VBool true))
           "True if automatically joining new pool members to the cluster. This will be `true` in the first release"
 
-      ; field   ~qualifier:StaticRO ~lifecycle
-          ~ty:Int "token_timeout" ~default_value:(Some (VInt 20000L))
-          "The corosync token timeout in ms"
+      ; field   ~qualifier:StaticRO ~lifecycle:[
+            Prototyped, rel_kolkata, "the unit is milliseconds";
+            Changed, rel_lima, "the unit is now seconds"
+          ]
+          ~ty:Float "token_timeout" ~default_value:(Some (VFloat Constants.default_token_timeout_s))
+          "The corosync token timeout in seconds"
 
-      ; field   ~qualifier:StaticRO ~lifecycle
-          ~ty:Int "token_timeout_coefficient" ~default_value:(Some (VInt 1000L))
-          "The corosync token timeout coefficient in ms"
+      ; field   ~qualifier:StaticRO ~lifecycle:[
+            Prototyped, rel_kolkata, "the unit is milliseconds";
+            Changed, rel_lima, "the unit is now seconds"
+          ]
+          ~ty:Float "token_timeout_coefficient" ~default_value:(Some (VFloat Constants.default_token_timeout_coefficient_s))
+          "The corosync token timeout coefficient in seconds"
 
 
       ; field   ~qualifier:StaticRO ~lifecycle
--- ocaml/xapi/records.ml
+++ ocaml/xapi/records.ml
@@ -2087,10 +2087,10 @@ let cluster_record rpc session_id cluster =
           ~get:(fun () -> (x ()).API.cluster_cluster_stack)
           ()
       ; make_field ~name:"token-timeout"
-          ~get:(fun () -> Int64.to_string((x ()).API.cluster_token_timeout))
+          ~get:(fun () -> string_of_float ((x ()).API.cluster_token_timeout))
           ()
       ; make_field ~name:"token-timeout-coefficient"
-          ~get:(fun () -> Int64.to_string((x ()).API.cluster_token_timeout_coefficient))
+          ~get:(fun () -> string_of_float ((x ()).API.cluster_token_timeout_coefficient))
           ()
       ; make_field ~name:"pending-forget" ~hidden:true
           ~get:(fun () -> String.concat "; " (x ()).API.cluster_pending_forget)
