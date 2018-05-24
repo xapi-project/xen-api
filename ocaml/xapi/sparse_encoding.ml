053b56307ba72964ce87dd6b4ee12a2499ff7b6a
Marcello Seri <marcello.seri@citrix.com>
2018-05-24 11:22:51 +0100

    xapi/sparse_encoding: make safe-string-compliant
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- ocaml/xapi/sparse_encoding.ml
+++ ocaml/xapi/sparse_encoding.ml
@@ -19,23 +19,25 @@ module Unmarshal = struct
   let int64 (s, offset) =
     let (<<<) a b = Int64.shift_left a b
     and (|||) a b = Int64.logor a b in
-    let a = Int64.of_int (int_of_char (s.[offset + 0]))
-    and b = Int64.of_int (int_of_char (s.[offset + 1]))
-    and c = Int64.of_int (int_of_char (s.[offset + 2]))
-    and d = Int64.of_int (int_of_char (s.[offset + 3]))
-    and e = Int64.of_int (int_of_char (s.[offset + 4]))
-    and f = Int64.of_int (int_of_char (s.[offset + 5]))
-    and g = Int64.of_int (int_of_char (s.[offset + 6]))
-    and h = Int64.of_int (int_of_char (s.[offset + 7])) in
+    let int64_of idx = Int64.of_int (int_of_char (Bytes.get s (offset + idx))) in
+    let a = int64_of 0
+    and b = int64_of 1
+    and c = int64_of 2
+    and d = int64_of 3
+    and e = int64_of 4
+    and f = int64_of 5
+    and g = int64_of 6
+    and h = int64_of 7 in
     (a <<< 0) ||| (b <<< 8) ||| (c <<< 16) ||| (d <<< 24) ||| (e <<< 32) ||| (f <<< 40) ||| (g <<< 48) ||| (h <<< 56),
     (s, offset + 8)
   let int32 (s, offset) =
     let (<<<) a b = Int32.shift_left a b
     and (|||) a b = Int32.logor a b in
-    let a = Int32.of_int (int_of_char (s.[offset + 0]))
-    and b = Int32.of_int (int_of_char (s.[offset + 1]))
-    and c = Int32.of_int (int_of_char (s.[offset + 2]))
-    and d = Int32.of_int (int_of_char (s.[offset + 3])) in
+    let int_of idx = Int32.of_int (int_of_char (Bytes.get s (offset + idx))) in
+    let a = int_of 0
+    and b = int_of 1
+    and c = int_of 2
+    and d = int_of 3 in
     (a <<< 0) ||| (b <<< 8) ||| (c <<< 16) ||| (d <<< 24), (s, offset + 4)
 end
 
@@ -55,7 +57,7 @@ module Marshal = struct
     List.iteri (fun i v ->
       Bytes.set result i (char_of_int @@ Int64.to_int v)
     ) [a; b; c; d; e; f; g; h];
-    Bytes.unsafe_to_string result
+    result
   let int32 x =
     let (>>>) a b = Int32.shift_right_logical a b
     and (&&&) a b = Int32.logand a b in
@@ -67,15 +69,14 @@ module Marshal = struct
     List.iteri (fun i v ->
       Bytes.set result i (char_of_int @@ Int32.to_int v)
     ) [a; b; c; d];
-    Bytes.unsafe_to_string result
-
+    result
 end
 
 module Chunk = struct
   (** Represents an single block of data to write *)
   type t = {
     start: int64;
-    data: string;
+    data: bytes;
   }
 
   let really_write fd offset buf off len =
@@ -86,31 +87,31 @@ module Chunk = struct
   (** Writes a single block of data to the output device *)
   let write fd x =
     ignore(Unix.LargeFile.lseek fd x.start Unix.SEEK_SET);
-    really_write fd x.start x.data 0 (String.length x.data)
+    really_write fd x.start x.data 0 (Bytes.length x.data)
 
   (** Reads a type t from a file descriptor *)
   let unmarshal fd =
-    let buf = String.make 12 '\000' in
-    Stdext.Unixext.really_read fd buf 0 (String.length buf);
+    let buf = Bytes.make 12 '\000' in
+    Stdext.Unixext.really_read fd buf 0 (Bytes.length buf);
     let stream = (buf, 0) in
     let start, stream = Unmarshal.int64 stream in
     let len, stream = Unmarshal.int32 stream in
-    let payload = String.make (Int32.to_int len) '\000' in
-    Stdext.Unixext.really_read fd payload 0 (String.length payload);
+    let payload = Bytes.make (Int32.to_int len) '\000' in
+    Stdext.Unixext.really_read fd payload 0 (Bytes.length payload);
     { start = start; data = payload }
 
   (** Writes a type t from a file descriptor *)
   let marshal fd x =
     let start' = Marshal.int64 x.start in
-    let len' = Marshal.int32 (Int32.of_int (String.length x.data)) in
-    really_write fd 0L start' 0 (String.length start');
-    really_write fd 8L len' 0 (String.length len');
-    really_write fd 12L x.data 0 (String.length x.data)
+    let len' = Marshal.int32 (Int32.of_int (Bytes.length x.data)) in
+    really_write fd 0L start' 0 (Bytes.length start');
+    really_write fd 8L len' 0 (Bytes.length len');
+    really_write fd 12L x.data 0 (Bytes.length x.data)
 
   (** Fold [f] across all ts unmarshalled from [fd] *)
   let rec fold f init fd =
     let x = unmarshal fd in
-    if x.data = ""
+    if x.data = Bytes.empty
     then init
     else fold f (f init x) fd
 end
