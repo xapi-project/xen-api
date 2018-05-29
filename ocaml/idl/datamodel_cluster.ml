80f6e04c9c929811c99bfa74af53bb3290ab4c1d
Edwin Török <edvin.torok@citrix.com>
2018-05-29 16:14:36 +0100

    fixup! CA-290471: fix wrong token timeout in XenCenter

--- ocaml/idl/datamodel_cluster.ml
+++ ocaml/idl/datamodel_cluster.ml
@@ -17,6 +17,11 @@ let cluster_operation =
 
 let lifecycle = [Prototyped, rel_kolkata, ""]
 
+let lifecycle_timeout = [
+            Prototyped, rel_kolkata, "the unit is milliseconds";
+            Changed, rel_lima, "the unit is now seconds"
+          ]
+
 let timeout_params =
   [ {param_type=Float;
      param_name="token_timeout";
@@ -171,17 +176,11 @@ let t =
           ~ty:Bool "pool_auto_join" ~default_value:(Some (VBool true))
           "True if automatically joining new pool members to the cluster. This will be `true` in the first release"
 
-      ; field   ~qualifier:StaticRO ~lifecycle:[
-            Prototyped, rel_kolkata, "the unit is milliseconds";
-            Changed, rel_lima, "the unit is now seconds"
-          ]
+      ; field   ~qualifier:StaticRO ~lifecycle:lifecycle_timeout
           ~ty:Float "token_timeout" ~default_value:(Some (VFloat Constants.default_token_timeout_s))
           "The corosync token timeout in seconds"
 
-      ; field   ~qualifier:StaticRO ~lifecycle:[
-            Prototyped, rel_kolkata, "the unit is milliseconds";
-            Changed, rel_lima, "the unit is now seconds"
-          ]
+      ; field   ~qualifier:StaticRO ~lifecycle:lifecycle_timeout
           ~ty:Float "token_timeout_coefficient" ~default_value:(Some (VFloat Constants.default_token_timeout_coefficient_s))
           "The corosync token timeout coefficient in seconds"
 
