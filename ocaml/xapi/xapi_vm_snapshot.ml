ea7dc9a010a7fe3343895c9a4db2cebdfdc5ed1c
Rob Hoes <rob.hoes@citrix.com>
2018-06-01 12:09:47 +0100

    Rename Xapi_vm_snapshot.default_values to overrides
    This better reflects what is it for. It is an assoc list of key/value pairs
    that is passed to `copy_vm_fields`. When this function find a
    `snapshot_metadata` key that is also in the `overrides` list, it will use the
    value from `overrides` rather than the one from the snapshot.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_vm_snapshot.ml
+++ ocaml/xapi/xapi_vm_snapshot.ml
@@ -241,7 +241,7 @@ let checkpoint ~__context ~vm ~new_name =
 (********************************************************************************)
 
 (* The following code have to run on the master as it manipulates the DB cache directly. *)
-let copy_vm_fields ~__context ~metadata ~dst ~do_not_copy ~default_values =
+let copy_vm_fields ~__context ~metadata ~dst ~do_not_copy ~overrides =
   if not (Pool_role.is_master ()) then
     raise Api_errors.(Server_error(internal_error, ["copy_vm_fields: Aborting because the host is not master"]));
   debug "copying metadata into %s" (Ref.string_of dst);
@@ -250,8 +250,8 @@ let copy_vm_fields ~__context ~metadata ~dst ~do_not_copy ~default_values =
   List.iter
     (fun (key,value) ->
        let value =
-         if List.mem_assoc key default_values
-         then List.assoc key default_values
+         if List.mem_assoc key overrides
+         then List.assoc key overrides
          else value in
        if not (List.mem key do_not_copy)
        then DB.write_field db Db_names.vm (Ref.string_of dst) key value)
@@ -435,7 +435,7 @@ let do_not_copy = [
   "power_state";
 ]
 
-let default_values = [
+let overrides = [
   Db_names.ha_always_run, "false";
 ]
 
@@ -463,7 +463,7 @@ let revert_vm_fields ~__context ~snapshot ~vm =
     if post_MNR
     then do_not_copy
     else extended_do_not_copy in
-  copy_vm_fields ~__context ~metadata:snap_metadata ~dst:vm ~do_not_copy ~default_values;
+  copy_vm_fields ~__context ~metadata:snap_metadata ~dst:vm ~do_not_copy ~overrides;
   TaskHelper.set_progress ~__context 0.1
 
 let revert ~__context ~snapshot ~vm =
@@ -516,7 +516,7 @@ let	create_vm_from_snapshot ~__context ~snapshot =
              ~__context rpc session_id snap_record in
          begin try
              Db.VM.set_uuid ~__context ~self:new_vm ~value:vm_uuid;
-             copy_vm_fields ~__context ~metadata:snap_metadata ~dst:new_vm ~do_not_copy:do_not_copy ~default_values;
+             copy_vm_fields ~__context ~metadata:snap_metadata ~dst:new_vm ~do_not_copy:do_not_copy ~overrides;
              List.iter (fun (snap,_) -> Db.VM.set_snapshot_of ~__context ~self:snap ~value:new_vm) snapshots;
              new_vm
            with e ->
