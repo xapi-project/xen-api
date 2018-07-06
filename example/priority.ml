6595363e95d43dbdd1090ca854b649edf7d6f154
Jon Ludlam <jonathan.ludlam@citrix.com>
2018-07-06 14:17:28 +0100

    Squashed 'vendor/async/' content from commit 02eab59b6
    git-subtree-dir: vendor/async
    git-subtree-split: 02eab59b61c2a59228d2549563069f2f002061db

---
+++ example/priority.ml
@@ -0,0 +1,26 @@
+open Core
+open Async
+
+let printf = Print.printf
+
+module Priority = Async.Priority (* to avoid omake confusion *)
+
+let normal = Priority.normal
+let low = Priority.low
+
+let one name priority =
+  Scheduler.schedule ~priority (fun () ->
+    upon Deferred.unit (fun () ->
+      let rec loop i =
+        if i > 0 then begin
+          printf "%s %d\n" name i;
+          upon Deferred.unit (fun () -> loop (i -1));
+        end
+      in
+      loop 10))
+
+let () = one "low" low
+let () = one "normal" normal
+let () = upon (Clock.after (sec 1.)) (fun () -> shutdown 0)
+
+let () = never_returns (Scheduler.go ())
