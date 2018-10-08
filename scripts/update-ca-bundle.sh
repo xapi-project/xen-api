3fb47e1c5ad57d176609ce3fb59c45f8711e8434
min.li <min.li1@citrix.com>
2018-10-08 18:25:39 +0800

    CP-29196: Enable FIPS mode if existence of cc preparations (#3722)
    * CP-29196: Enable FIPS mode if existence of cc preparations
    
    * pool-cert-install use trust instead of rehas
    * Add uninstall
    * Add update ca bundle script
    * Rework comments
    1. Add function 'update_ca_bundle'.
    2. Define 'certificate_path' in xapi instead of Stunnel.
    3. Refine 'CC_PREPARATIONS' checking to highlight 'x' dummy
    * Use temp file in 'update-ca-bundle.sh' to avoid potential incomplete file
    
    cherry picked from: d22816b5f16
    
    Signed-off-by: Min Li <min.li1@citrix.com>

---
+++ scripts/update-ca-bundle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright (c) Citrix Systems 2008. All rights reserved.
+#
+
+set -e
+
+mkdir -p /etc/stunnel
+find /etc/stunnel/certs -name '*.pem' | xargs cat > /etc/stunnel/xapi-stunnel-ca-bundle.pem.tmp
+mv /etc/stunnel/xapi-stunnel-ca-bundle.pem.tmp /etc/stunnel/xapi-stunnel-ca-bundle.pem
