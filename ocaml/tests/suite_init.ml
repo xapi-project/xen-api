62a7488b7442a825c8eb85bbc97bc3024c309dd1
Konstantina Chremmou <konstantina.chremmou@citrix.com>
2018-11-23 01:42:22 +0000

    Reference xapi-inventory instead of xcp-inventory; the latter is being deprecated.
    Signed-off-by: Konstantina Chremmou <konstantina.chremmou@citrix.com>

--- ocaml/tests/suite_init.ml
+++ ocaml/tests/suite_init.ml
@@ -20,7 +20,7 @@ let harness_init () =
   Debug.log_to_stdout ();
   Printexc.record_backtrace true;
   Inventory.inventory_filename :=
-    Filename.concat Test_common.working_area "xcp-inventory";
+    Filename.concat Test_common.working_area "xapi-inventory";
   Xcp_client.use_switch := false;
   Pool_role.set_pool_role_for_test ();
   Xapi.register_callback_fns ();
