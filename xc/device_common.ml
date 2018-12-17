xapi-project/xenopsd

CP-28662: use varstored for VM start/stop of UEFI guests
Signed-off-by: Edwin Török <edvin.torok@citrix.com>

xc/device_common.ml
@@ -339,6 +339,8 @@ let demu_restore_path : (_, _, _) format = "/var/lib/xen/demu-resume.%d"
 let var_run_xen_path = "/var/run/xen"
 let qmp_libxl_path = (sprintf "%s/qmp-libxl-%d") var_run_xen_path
 let qmp_event_path = (sprintf "%s/qmp-event-%d") var_run_xen_path
+let efivars_init_path = (sprintf "%s/efi-vars-init-%d.dat") var_run_xen_path
+let efivars_save_path = (sprintf "%s/efi-vars-save-%d.dat") var_run_xen_path
 
 (* Where qemu writes its state and is signalled *)
 let device_model_path ~qemu_domid domid = sprintf "/local/domain/%d/device-model/%d" qemu_domid domid

xc/device_common.mli
@@ -98,6 +98,8 @@ val demu_restore_path: (int -> 'a, 'b, 'a) format
 val var_run_xen_path: string
 val qmp_libxl_path: int -> string
 val qmp_event_path: int -> string
+val efivars_init_path: int -> string
+val efivars_save_path: int -> string
 
 (** Directory in xenstore where qemu writes its state *)
 val device_model_path: qemu_domid:int -> int -> string

xc/xenops_server_xen.ml
@@ -1000,6 +1000,10 @@ module VM = struct
 
     let platformdata = vm.platformdata |> default "acpi_s3" "0" |> default "acpi_s4" "0" in
 
+    let is_uefi = match ty with
+      | HVM { firmware = Some Uefi; _ } -> true
+      | _ -> false in
+
     {
       Domain.ssidref = vm.ssidref;
       hvm = hvm;
@@ -1009,6 +1013,7 @@ module VM = struct
       platformdata = platformdata @ vcpus;
       bios_strings = vm.bios_strings;
       has_vendor_device = vm.has_vendor_device;
+      is_uefi;
     }
 
   let create_exn (task: Xenops_task.task_handle) memory_upper_bound vm =

xapi-project/xen-api

xc/device_common.ml
@@ -339,6 +339,8 @@ let demu_restore_path : (_, _, _) format = "/var/lib/xen/demu-resume.%d"
 let var_run_xen_path = "/var/run/xen"
 let qmp_libxl_path = (sprintf "%s/qmp-libxl-%d") var_run_xen_path
 let qmp_event_path = (sprintf "%s/qmp-event-%d") var_run_xen_path
+let efivars_resume_path = (sprintf "%s/efi-vars-resume-%d.dat") var_run_xen_path
+let efivars_save_path = (sprintf "%s/efi-vars-save-%d.dat") var_run_xen_path
 
 (* Where qemu writes its state and is signalled *)
 let device_model_path ~qemu_domid domid = sprintf "/local/domain/%d/device-model/%d" qemu_domid domid

xc/device_common.mli
@@ -98,6 +98,8 @@ val demu_restore_path: (int -> 'a, 'b, 'a) format
 val var_run_xen_path: string
 val qmp_libxl_path: int -> string
 val qmp_event_path: int -> string
+val efivars_resume_path: int -> string
+val efivars_save_path: int -> string
 
 (** Directory in xenstore where qemu writes its state *)
 val device_model_path: qemu_domid:int -> int -> string

xc/xenops_server_xen.ml
@@ -1016,6 +1016,10 @@ module VM = struct
 
     let platformdata = vm.platformdata |> default "acpi_s3" "0" |> default "acpi_s4" "0" in
 
+    let is_uefi = match ty with
+      | HVM { firmware = Uefi _; _ } -> true
+      | _ -> false in
+
     {
       Domain.ssidref = vm.ssidref;
       hvm = hvm;
@@ -1025,6 +1029,7 @@ module VM = struct
       platformdata = platformdata @ vcpus;
       bios_strings = vm.bios_strings;
       has_vendor_device = vm.has_vendor_device;
+      is_uefi;
     }
 
   let create_exn (task: Xenops_task.task_handle) memory_upper_bound vm final_id =