xapi-project/xenopsd

CP-28663: implement VM.suspend/resume for UEFI
This adds a new Varstored record type to the suspend image.

Varstored gets stopped with a SIGTERM which causes it to write out all
the UEFI variables to the specified path.
We read back the file and put it into the suspend (migration) stream.
On resume we read the varstored record from the suspend image and write
it out to the resume path, which is picked up by varstored on resume.
We tell varstored to resume by explicitly passing --resume.

It is important to stop varstored at the correct place
(varstored exposes a PCI device to the guest):
* if we stop varstored after QEMU is killed then varstored won't be able
to reattach on resume because the device will be considered in use
* if we stop varstored while the guest is still running then upon resume
the PCI config space will be different (even if varstored attempts to restore it),
which means the guest is not able to access it anymore
* if we stop it when the guest is suspended but QEMU is still running
    then varstored is able to successfully reattach and a (Linux) guest
    able to successfully access the UEFI variables

We cannot immediately write out the varstored record when we stop it
(we would corrupt the migration stream by doing so), so we use the
efivars save path as a buffer to store the variables until we can.
This file (and all efi var related ones) will get deleted when we stop
qemu and the other daemons.

Stopping varstored will delete the xenstore pid key, so attempting to stop
it a second time (when qemu is stopped) is harmless.

Signed-off-by: Edwin Török <edvin.torok@citrix.com>

xc/domain.ml
@@ -1041,6 +1041,12 @@ type suspend_flag = Live | Debug
               restore_and_wait Vgpu >>= fun () ->
               debug "Restored DEMU state";
               process_header fd res
+            | Varstored, len ->
+              debug "Read varstored record header (length=%Ld)" len;
+              let efivars = Io.read fd (Io.int_of_int64_exn len) in
+              debug "Read varstored record contents";
+              Device.Dm.restore_varstored task ~xs ~efivars domid;
+              process_header fd res
             | End_of_image, _ ->
               debug "Read suspend image footer";
               res
@@ -1298,6 +1304,7 @@ type suspend_flag = Live | Debug
             if domain_type = `hvm then (
               debug "VM = %s; domid = %d; suspending qemu-dm" (Uuid.to_string uuid) domid;
               Device.Dm.suspend task ~xs ~qemu_domid ~dm domid;
+              Device.Dm.suspend_varstored task ~xs domid |> ignore
             );
             send_done cnx;
             wait_for_message ()
@@ -1347,11 +1354,21 @@ type suspend_flag = Live | Debug
         Unix.close fd2
       )
 
+  let write_varstored_record task ~xs domid main_fd =
+    let open Suspend_image in let open Suspend_image.M in
+    let varstored_record = Device.Dm.suspend_varstored task ~xs domid in
+    let varstored_rec_len = String.length varstored_record in
+    debug "Writing varstored record (length=%d)" varstored_rec_len;
+    write_header main_fd (Varstored, Int64.of_int varstored_rec_len) >>= fun () ->
+    debug "Writing varstored record contents";
+    Io.write main_fd varstored_record;
+    return ()
+
   (* suspend register the callback function that will be call by linux_save
    * and is in charge to suspend the domain when called. the whole domain
    * context is saved to fd
   *)
-  let suspend (task: Xenops_task.task_handle) ~xc ~xs ~domain_type ~dm ~manager_path vm_str domid main_fd vgpu_fd flags ?(progress_callback = fun _ -> ()) ~qemu_domid do_suspend_callback =
+  let suspend (task: Xenops_task.task_handle) ~xc ~xs ~domain_type ~is_uefi ~dm ~manager_path vm_str domid main_fd vgpu_fd flags ?(progress_callback = fun _ -> ()) ~qemu_domid do_suspend_callback =
     let module DD = Debug.Make(struct let name = "mig64" end) in
     let open DD in
     let hvm = domain_type = `hvm in
@@ -1377,6 +1394,7 @@ type suspend_flag = Live | Debug
       Io.write main_fd xenops_record;
       suspend_emu_manager ~task ~xc ~xs ~domain_type ~dm ~manager_path ~domid ~uuid ~main_fd ~vgpu_fd ~flags
         ~progress_callback ~qemu_domid ~do_suspend_callback >>= fun () ->
+      (if is_uefi then write_varstored_record task ~xs domid main_fd else return ()) >>= fun () ->
       (* Qemu record (if this is a hvm domain) *)
       (* Currently Qemu suspended inside above call with the libxc memory
          		* image, we should try putting it below in the relevant section of the

xc/xenops_server_xen.ml
@@ -1772,6 +1772,9 @@ module VM = struct
 
          with_data ~xc ~xs task data true
            (fun fd ->
+              let is_uefi = match vm.ty with
+                | HVM { firmware = Some Uefi; _ } -> true
+                | _ -> false in
               let vm_str = Vm.sexp_of_t vm |> Sexplib.Sexp.to_string in
               let vgpu_fd =
                 match vgpu_data with
@@ -1782,7 +1785,7 @@ module VM = struct
               in
               let manager_path = choose_emu_manager vm.Vm.platformdata in
               Domain.suspend task ~xc ~xs ~domain_type ~dm:(dm_of ~vm) ~progress_callback
-                ~qemu_domid ~manager_path vm_str domid fd vgpu_fd flags'
+                ~qemu_domid ~manager_path ~is_uefi vm_str domid fd vgpu_fd flags'
                 (fun () ->
                    (* SCTX-2558: wait more for ballooning if needed *)
                    wait_ballooning task vm;

xapi-project/xen-api

xc/domain.ml
@@ -1023,6 +1023,12 @@ type suspend_flag = Live | Debug
               restore_and_wait Vgpu >>= fun () ->
               debug "Restored DEMU state";
               process_header fd res
+            | Varstored, len ->
+              debug "Read varstored record header (domid=%d length=%Ld)" domid len;
+              let efivars = Io.read fd (Io.int_of_int64_exn len) in
+              debug "Read varstored record contents (domid=%d)" domid;
+              Device.Dm.restore_varstored task ~xs ~efivars domid;
+              process_header fd res
             | End_of_image, _ ->
               debug "Read suspend image footer";
               res
@@ -1280,6 +1286,7 @@ type suspend_flag = Live | Debug
             if domain_type = `hvm then (
               debug "VM = %s; domid = %d; suspending qemu-dm" (Uuid.to_string uuid) domid;
               Device.Dm.suspend task ~xs ~qemu_domid ~dm domid;
+              Device.Dm.suspend_varstored task ~xs domid |> ignore
             );
             send_done cnx;
             wait_for_message ()
@@ -1329,11 +1336,21 @@ type suspend_flag = Live | Debug
         Unix.close fd2
       )
 
+  let write_varstored_record task ~xs domid main_fd =
+    let open Suspend_image in let open Suspend_image.M in
+    let varstored_record = Device.Dm.suspend_varstored task ~xs domid in
+    let varstored_rec_len = String.length varstored_record in
+    debug "Writing varstored record (domid=%d length=%d)" domid varstored_rec_len;
+    write_header main_fd (Varstored, Int64.of_int varstored_rec_len) >>= fun () ->
+    debug "Writing varstored record contents (domid=%d)" domid;
+    Io.write main_fd varstored_record;
+    return ()
+
   (* suspend register the callback function that will be call by linux_save
    * and is in charge to suspend the domain when called. the whole domain
    * context is saved to fd
   *)
-  let suspend (task: Xenops_task.task_handle) ~xc ~xs ~domain_type ~dm ~manager_path vm_str domid main_fd vgpu_fd flags ?(progress_callback = fun _ -> ()) ~qemu_domid do_suspend_callback =
+  let suspend (task: Xenops_task.task_handle) ~xc ~xs ~domain_type ~is_uefi ~dm ~manager_path vm_str domid main_fd vgpu_fd flags ?(progress_callback = fun _ -> ()) ~qemu_domid do_suspend_callback =
     let module DD = Debug.Make(struct let name = "mig64" end) in
     let open DD in
     let hvm = domain_type = `hvm in
@@ -1359,6 +1376,7 @@ type suspend_flag = Live | Debug
       Io.write main_fd xenops_record;
       suspend_emu_manager ~task ~xc ~xs ~domain_type ~dm ~manager_path ~domid ~uuid ~main_fd ~vgpu_fd ~flags
         ~progress_callback ~qemu_domid ~do_suspend_callback >>= fun () ->
+      (if is_uefi then write_varstored_record task ~xs domid main_fd else return ()) >>= fun () ->
       (* Qemu record (if this is a hvm domain) *)
       (* Currently Qemu suspended inside above call with the libxc memory
          		* image, we should try putting it below in the relevant section of the

xc/xenops_server_xen.ml
@@ -1789,6 +1789,9 @@ module VM = struct
 
          with_data ~xc ~xs task data true
            (fun fd ->
+              let is_uefi = match vm.ty with
+                | HVM { firmware = Uefi _; _ } -> true
+                | _ -> false in
               let vm_str = Vm.sexp_of_t vm |> Sexplib.Sexp.to_string in
               let vgpu_fd =
                 match vgpu_data with
@@ -1799,7 +1802,7 @@ module VM = struct
               in
               let manager_path = choose_emu_manager vm.Vm.platformdata in
               Domain.suspend task ~xc ~xs ~domain_type ~dm:(dm_of ~vm) ~progress_callback
-                ~qemu_domid ~manager_path vm_str domid fd vgpu_fd flags'
+                ~qemu_domid ~manager_path ~is_uefi vm_str domid fd vgpu_fd flags'
                 (fun () ->
                    (* SCTX-2558: wait more for ballooning if needed *)
                    wait_ballooning task vm;