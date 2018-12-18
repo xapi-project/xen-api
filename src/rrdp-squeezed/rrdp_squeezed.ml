6c4b94e27e52151a8ab8d46926ee101d0df41883
Christian Lindig <christian.lindig@citrix.com>
2018-12-18 10:39:21 +0000

    Reduce compiler warnings
    Address warnings from "make build":
    
    * unused variables
    * redundant open statements
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- src/rrdp-squeezed/rrdp_squeezed.ml
+++ src/rrdp-squeezed/rrdp_squeezed.ml
@@ -13,7 +13,6 @@
  *)
 
 open Xapi_stdext_std
-open Xenstore
 open Rrdd_plugin
 
 module Process = Process(struct let name="xcp-rrdd-squeezed" end)
@@ -57,7 +56,7 @@ module MemoryActions = struct
 
   let watch_token domid = Printf.sprintf "xcp-rrdd-plugins/squeezed:domain-%d" domid
 
-  let watch_fired xc _ path domains watches =
+  let watch_fired _xc _ path domains _watches =
     D.debug "Watch fired on %s" path;
     let read_new_value domid current_memory_values =
       let domid = int_of_string domid in
