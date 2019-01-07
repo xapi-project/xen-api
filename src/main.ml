5b8b8a5a57fdc264a0d518d36e7b55c155c9ee22
Christian Lindig <christian.lindig@citrix.com>
2019-01-07 11:19:18 +0000

    CA-303570 make cipher suites more explicit
    Bind string for ciphers to its own name.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- src/main.ml
+++ src/main.ml
@@ -192,9 +192,11 @@ let help = [
 let certfile =
   let doc = "Path to file containing TLS certificate." in
   Arg.(value & opt string "" & info ["certfile"] ~doc)
+
 let ciphersuites =
   let doc = "Set of ciphersuites for TLS (specified in the format accepted by OpenSSL, stunnel etc.)" in
-  Arg.(value & opt string "!EXPORT:RSA+AES128-SHA256" & info ["ciphersuites"] ~doc)
+  let suites = "!EXPORT:RSA+AES128-SHA256" in
+  Arg.(value & opt string suites & info ["ciphersuites"] ~doc)
 
 let cmd =
   let doc = "Expose VDIs over authenticated NBD connections" in
