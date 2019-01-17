60d6a296fe3af816c69c3d98eac841d99bae3d0e
Tim Smith <tim.smith@citrix.com>
2019-01-17 15:02:06 +0000

    CA-306395 Convert sendfile to direct_copy
    In the general case it is better to use read() and write() with O_DIRECT
    for large data transfers than to unconditionally send everything via
    sendfile(), particularly when the output is not a pipe or a socket.
    
    This converts the previous sendfile wrapper into a more general
    direct_copy wrapper. Later, if we decide we need to get more performance
    between certain types of filedescriptors, we can make use of the
    stub_init() function which currently allocates the buffer record an
    engine selector in the direct_copy_handle based on information obtained
    from fstat() on the two filedescriptors.
    
    Incorporates changes from Rob Hoes <rob.hoes@citrix.com>:
        Remove unused reference to sendfile binding
        Add init and cleanup stubs for sendfile replacement
    
    Signed-off-by: Tim Smith <tim.smith@citrix.com>
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- src/direct_copy_stubs.c
+++ src/direct_copy_stubs.c
@@ -23,6 +23,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include <caml/alloc.h>
 #include <caml/memory.h>
@@ -31,65 +32,152 @@
 #include <caml/callback.h>
 #include <caml/bigarray.h>
 
-#ifdef __linux__
-# include <linux/fs.h>
-# include <sys/sendfile.h>
-#endif
-
 /* ocaml/ocaml/unixsupport.c */
 extern void uerror(char *cmdname, value cmdarg);
 #define Nothing ((value) 0)
 
-#define NOT_IMPLEMENTED (-1)
-#define TRIED_AND_FAILED (1)
-#define OK 0
+enum direct_copy_rc {
+  OK                   = 0,
+  TRIED_AND_FAILED     = 1,
+  READ_FAILED          = 2,
+  WRITE_FAILED         = 3,
+  WRITE_UNEXPECTED_EOF = 4
+};
 
-CAMLprim value stub_sendfile64(value in_fd, value out_fd, value len){
-  CAMLparam3(in_fd, out_fd, len);
+#define XFER_BUFSIZ (2*1024*1024)
+
+struct direct_copy_handle {
+  int  in_fd;
+  int  out_fd;
+  char *buffer;
+};
+
+CAMLprim value stub_init(value in_fd, value out_fd)
+{
+  CAMLparam2(in_fd, out_fd);
   CAMLlocal1(result);
-  size_t c_len = Int64_val(len);
-  size_t bytes;
   int c_in_fd = Int_val(in_fd);
   int c_out_fd = Int_val(out_fd);
+  struct direct_copy_handle *cpinfo = NULL;
+  int flags;
 
-  int rc = NOT_IMPLEMENTED;
+  /* This is where we will keep the handle on return to OCaml. The
+   * Abstract tag teaches OCaml's garbage collector not to mess with
+   * it */
+  result = alloc(1, Abstract_tag);
 
-  enter_blocking_section();
+  /* initialise handle */
+  cpinfo = malloc(sizeof(struct direct_copy_handle));
+  if (!cpinfo) caml_raise_out_of_memory();
+  cpinfo->buffer = NULL;
+  if (posix_memalign((void **)&cpinfo->buffer, sysconf(_SC_PAGESIZE), XFER_BUFSIZ)) {
+      free(cpinfo);
+      caml_raise_out_of_memory();
+  }
+  cpinfo->in_fd = c_in_fd;
+  cpinfo->out_fd = c_out_fd;
 
 #ifdef __linux__
-  rc = TRIED_AND_FAILED;
-  bytes = sendfile(c_out_fd, c_in_fd, NULL, c_len);
-  if (bytes != -1) {
-      rc = OK;
-  } else if (errno == EINVAL) {
-      int flags;
-      int result = 0;
-
-      flags = fcntl(c_in_fd, F_GETFL, NULL);
-      if (flags > 0) {
-          result = fcntl(c_in_fd, F_SETFL, flags & ~O_DIRECT);
-
-          if (result == 0) {
-              bytes = sendfile(c_out_fd, c_in_fd, NULL, c_len);
-              if (bytes != -1) {
-                  rc = OK;
-              }
-              fcntl(c_in_fd, F_SETFL, flags);
-          }
-      }
-  }
+  /* Force the output to have O_DIRECT if possible.
+     Because it may not be possible, ignore any error
+     we might get on setting the flag.
+  */
+  flags = fcntl(c_out_fd, F_GETFL, NULL);
+  if (flags > 0 && !(flags & O_DIRECT))
+    fcntl(c_out_fd, F_SETFL, flags | O_DIRECT);
 #endif
 
+  Field(result, 0) = (uintptr_t)cpinfo;
+  CAMLreturn(result);
+
+}
+
+CAMLprim value stub_cleanup(value handle)
+{
+  CAMLparam1(handle);
+  struct direct_copy_handle *cpinfo = NULL;
+
+  assert(Is_block(handle) && Tag_val(handle) == Abstract_tag);
+  cpinfo = (struct direct_copy_handle *)Field(handle, 0);
+
+  free(cpinfo->buffer);
+  free(cpinfo);
+  Field(handle, 0) = (uintptr_t)NULL;
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value stub_direct_copy(value handle, value len){
+  CAMLparam2(handle, len);
+  CAMLlocal1(result);
+  size_t c_len = Int64_val(len);
+  struct direct_copy_handle *cpinfo = NULL;
+  size_t bytes;
+  size_t remaining;
+  enum direct_copy_rc rc;
+
+  assert(Is_block(handle) && Tag_val(handle) == Abstract_tag);
+  cpinfo = (struct direct_copy_handle *)Field(handle, 0);
+  if (!cpinfo) caml_failwith("direct_copy: NULL handle");
+
+  /* Calling enter_blocking_section() actually releases the OCaml
+   * runtime lock, so no OCaml exceptions may be thrown, and no OCaml
+   * values may be accessed, until it is reacquired. Also this
+   * means other OCaml threads may do things while this is going
+   * on so the caller must be careful. */
+  enter_blocking_section();
+
+  rc = TRIED_AND_FAILED;
+  bytes = 0;
+  
+  remaining = c_len;
+  while (remaining > 0) {
+    ssize_t bread;
+    ssize_t bwritten = 0;
+
+    bread = read(cpinfo->in_fd, cpinfo->buffer, (remaining < XFER_BUFSIZ)?remaining:XFER_BUFSIZ) ;
+    if (bread < 0) {
+        rc = READ_FAILED;
+        goto fail;
+    }
+    while (bwritten < bread) {
+      ssize_t ret;
+
+      ret = write(cpinfo->out_fd, cpinfo->buffer + bwritten, bread - bwritten);
+      if (ret == 0) {
+          rc = WRITE_UNEXPECTED_EOF;
+          goto fail;
+      }
+      if (ret < 0) {
+          rc = WRITE_FAILED;
+          goto fail;
+      }
+      bytes += ret;
+      bwritten += ret;
+      remaining -= ret;
+    }
+  }
+  rc = OK;
+fail:
+
   leave_blocking_section();
+  /* Now that the OCaml runtime lock is reacquired, it is safe to
+   * raise OCaml exceptions */
 
   switch (rc) {
-    case NOT_IMPLEMENTED:
-      caml_failwith("This platform does not support sendfile()");
-      break;
     case TRIED_AND_FAILED:
-      uerror("sendfile", Nothing);
+      caml_failwith("direct_copy: General error");
       break;
-    default: break;
+    case WRITE_FAILED:
+      uerror("write", Nothing);
+      break;
+    case READ_FAILED:
+      uerror("read", Nothing);
+      break;
+    case WRITE_UNEXPECTED_EOF:
+      caml_failwith("direct_copy: Unexpected EOF on write");
+      break;
+    case OK:
+      break;
   }
   result = caml_copy_int64(bytes);
   CAMLreturn(result);
