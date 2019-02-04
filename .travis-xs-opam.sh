770e28dedb1dbb08df26c75989cf7053219215e8
Christian Lindig <christian.lindig@citrix.com>
2019-02-04 17:05:20 +0000

    Simplify .travis-xs-opam.sh (2)
    Remove unused TRAVIS_JOB_ID, TRAVIS variables.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- .travis-xs-opam.sh
+++ .travis-xs-opam.sh
@@ -6,8 +6,6 @@ PACKAGE="xapi"
 PINS="xapi:."
 BASE_REMOTE="https://github.com/xapi-project/xs-opam.git"
 DISTRO="debian-9-ocaml-4.07"
-TRAVIS="$TRAVIS"
-TRAVIS_JOB_ID="$TRAVIS_JOB_ID"
 
 wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-docker.sh
 . .travis-docker.sh
