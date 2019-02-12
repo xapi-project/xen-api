4c1e2af8c16ef3e522179454d4b42bb66de41c1e
Pau Ruiz Safont <pau.safont@citrix.com>
2019-02-12 10:36:11 +0000

    maintenance: whitespace
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- networkd/network_server.ml
+++ networkd/network_server.ml
@@ -174,7 +174,7 @@ module Sriov = struct
       ) ()
 
   let disable dbg name =
-    Debug.with_thread_associated dbg (fun () ->	
+    Debug.with_thread_associated dbg (fun () ->
         debug "Disable network SR-IOV by name: %s" name;
         match config_sriov ~enable:false name with
         | Ok _ -> (Ok:disable_result)
@@ -200,7 +200,7 @@ module Sriov = struct
       (fun () -> Ip.set_vf_rate dev index 0) rate
 
   let make_vf_config dbg pci_address (vf_info : sriov_pci_t) =
-    Debug.with_thread_associated dbg (fun () ->	
+    Debug.with_thread_associated dbg (fun () ->
         let vlan = Opt.map Int64.to_int vf_info.vlan
         and rate = Opt.map Int64.to_int vf_info.rate
         and pcibuspath = Xcp_pci.string_of_address pci_address in
