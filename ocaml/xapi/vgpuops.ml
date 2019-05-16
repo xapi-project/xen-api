30d9517cd27450ca06beff0b12f44e966c030703
liulinC <lin.liu@citrix.com>
2019-05-16 17:03:56 +0800

    CP-30660: Dry run the allocate vGPU to pGPU
    This commit two functionalities:
    1. Dry run allocation vGPU to pGPUs
    2. Support assign multiple vGPUs to VM
    
    To dry run the allocation process, List.fold_left is used to
    iterate all the vGPUs of the VM.
    For each iteration
    1. Dry run allocation of the vGPU. The previous allocation List will
    be used to correct the capacity in database in dry run mode.
    2. Record the allcation of current vGPU for the left vGPUs in previous
    allocation list
    3. List.fold_left accumulate the previous allocation list every
    iteration
    
    Signed-off-by: Lin Liu <lin.liu@citrix.com>
    Signed-off-by: Michael Z <michael.zhao@citrix.com>

--- ocaml/xapi/vgpuops.ml
+++ ocaml/xapi/vgpuops.ml
@@ -18,7 +18,7 @@ open Stdext
 open Listext
 open Xstringext
 
-type vgpu = {
+type vgpu_t = {
   vgpu_ref: API.ref_VGPU;
   gpu_group_ref: API.ref_GPU_group;
   devid: int;
@@ -27,7 +27,7 @@ type vgpu = {
   requires_passthrough: [ `PF | `VF ] option;
 }
 
-let vgpu_of_vgpu ~__context vm_r vgpu =
+let vgpu_of_ref ~__context vgpu =
   let vgpu_r = Db.VGPU.get_record ~__context ~self:vgpu in
   {
     vgpu_ref = vgpu;
@@ -39,7 +39,7 @@ let vgpu_of_vgpu ~__context vm_r vgpu =
   }
 
 let vgpus_of_vm ~__context vm_r =
-  List.map (vgpu_of_vgpu ~__context vm_r) vm_r.API.vM_VGPUs
+  List.map (vgpu_of_ref ~__context ) vm_r.API.vM_VGPUs
 
 let fail_creation vm vgpu =
   match vgpu.requires_passthrough with
@@ -55,10 +55,19 @@ let fail_creation vm vgpu =
         Ref.string_of vgpu.gpu_group_ref
       ]))
 
-let allocate_vgpu_to_gpu ~__context vm host vgpu =
+let allocate_vgpu_to_gpu ?(dry_run=false) ?(pre_allocate_list=[]) ~__context vm host vgpu =
+  (* Get all pGPU from the host *)
   let available_pgpus = Db.Host.get_PGPUs ~__context ~self:host in
+  (* Get all pGPU from the required groups *)
   let compatible_pgpus = Db.GPU_group.get_PGPUs ~__context ~self:vgpu.gpu_group_ref in
-  let pgpus = List.intersect compatible_pgpus available_pgpus in
+  let vgpu_type = Db.VGPU.get_type ~__context ~self:vgpu.vgpu_ref in
+
+  (* Make a asso list as follows
+   * [(p1,capacity1);(p2,capacity2)...] *)
+  let pgpu_capacity_assoc = List.intersect compatible_pgpus available_pgpus
+                            |> List.map (fun self -> (self, Xapi_pgpu_helpers.get_remaining_capacity  ~__context ~pre_allocate_list ~self ~vgpu_type))
+                            |> List.filter (fun (_,capacity) -> capacity > 0L ) in
+
   (* Sort the pgpus in lists of equal optimality for vGPU placement based on
    * the GPU groups allocation algorithm *)
   let sort_desc =
@@ -66,28 +75,16 @@ let allocate_vgpu_to_gpu ~__context vm host vgpu =
     | `depth_first -> false
     | `breadth_first -> true
   in
-  let sorted_pgpus = Helpers.sort_by_schwarzian ~descending:sort_desc
-      (fun pgpu ->
-         Helpers.call_api_functions ~__context (fun rpc session_id ->
-             Client.Client.PGPU.get_remaining_capacity ~rpc ~session_id
-               ~self:pgpu ~vgpu_type:vgpu.type_ref))
-      pgpus
-  in
-  let rec choose_pgpu = function
-    | [] -> None
-    | pgpu :: remaining ->
-      try
-        Xapi_pgpu_helpers.assert_capacity_exists_for_VGPU_type ~__context
-          ~self:pgpu ~vgpu_type:vgpu.type_ref;
-        Some pgpu
-      with _ -> choose_pgpu remaining
-  in
-  match choose_pgpu sorted_pgpus with
-  | None -> fail_creation vm vgpu
-  | Some pgpu ->
-    Db.VGPU.set_scheduled_to_be_resident_on ~__context
-      ~self:vgpu.vgpu_ref ~value:pgpu;
-    pgpu
+  (* Sort the pGPU list by the capacity for the target vGPU *)
+  Helpers.sort_by_schwarzian ~descending:sort_desc
+    (fun pgpu -> List.assoc pgpu pgpu_capacity_assoc )
+    (List.map (fun (pgpu,_)-> pgpu) pgpu_capacity_assoc)
+  |> function
+  | [] -> fail_creation vm vgpu
+  | hd::tail ->
+    if not dry_run then
+      Db.VGPU.set_scheduled_to_be_resident_on ~__context ~self:vgpu.vgpu_ref ~value:hd;
+    (vgpu.vgpu_ref,hd)::pre_allocate_list
 
 (* Take a PCI device and assign it, and any dependent devices, to the VM *)
 let add_pcis_to_vm ~__context host vm pci =
@@ -117,8 +114,8 @@ let reserve_free_virtual_function ~__context vm pf =
         (* We may still need to load the driver... do that and try again *)
         let pf_host = Db.PCI.get_host ~__context ~self:pf in
         Helpers.call_api_functions ~__context (fun rpc session_id ->
-          Client.Client.Host.mxgpu_vf_setup rpc session_id pf_host
-        );
+            Client.Client.Host.mxgpu_vf_setup rpc session_id pf_host
+          );
         get false
       end else
         (* This probably means that our capacity checking went wrong! *)
@@ -127,28 +124,29 @@ let reserve_free_virtual_function ~__context vm pf =
   get true
 
 let add_vgpus_to_vm ~__context host vm vgpus =
-  (* Only support a maximum of one virtual GPU per VM for now. *)
   (try Db.VM.remove_from_other_config ~__context ~self:vm ~key:Xapi_globs.vgpu_pci with _ -> ());
-  match vgpus with
-  | [] -> ()
-  | vgpu :: _ ->
-    match vgpu.requires_passthrough with
-    | Some `PF ->
-      debug "Creating passthrough VGPUs";
-      let pgpu = allocate_vgpu_to_gpu ~__context vm host vgpu in
-      let pci = Db.PGPU.get_PCI ~__context ~self:pgpu in
-      add_pcis_to_vm ~__context host vm pci
-    | Some `VF ->
-      Pool_features.assert_enabled ~__context ~f:Features.VGPU;
-      debug "Creating SR-IOV VGPUs";
-      let pgpu = allocate_vgpu_to_gpu ~__context vm host vgpu in
-      Db.PGPU.get_PCI ~__context ~self:pgpu
-      |> reserve_free_virtual_function ~__context vm
-      |> add_pcis_to_vm ~__context host vm
-    | None ->
-      Pool_features.assert_enabled ~__context ~f:Features.VGPU;
-      debug "Creating virtual VGPUs";
-      ignore (allocate_vgpu_to_gpu ~__context vm host vgpu)
+  debug "vGPUs allocated to VM (%s) are: %s" (Ref.string_of vm)
+    (List.map (fun vgpu -> Ref.string_of vgpu.vgpu_ref) vgpus
+     |> String.concat ";");
+  List.iter (fun vgpu ->
+      match vgpu.requires_passthrough with
+      | Some `PF ->
+        debug "Creating passthrough VGPUs";
+        let pgpu = List.assoc vgpu.vgpu_ref (allocate_vgpu_to_gpu ~__context vm host vgpu) in
+        let pci = Db.PGPU.get_PCI ~__context ~self:pgpu in
+        add_pcis_to_vm ~__context host vm pci
+      | Some `VF ->
+        Pool_features.assert_enabled ~__context ~f:Features.VGPU;
+        debug "Creating SR-IOV VGPUs";
+        let pgpu = List.assoc vgpu.vgpu_ref (allocate_vgpu_to_gpu ~__context vm host vgpu) in
+        Db.PGPU.get_PCI ~__context ~self:pgpu
+        |> reserve_free_virtual_function ~__context vm
+        |> add_pcis_to_vm ~__context host vm
+      | None ->
+        Pool_features.assert_enabled ~__context ~f:Features.VGPU;
+        debug "Creating virtual VGPUs";
+        ignore (allocate_vgpu_to_gpu ~__context vm host vgpu)
+    ) vgpus
 
 
 (* The two functions below are the main entry points of this module *)
@@ -160,7 +158,7 @@ let add_vgpus_to_vm ~__context host vm vgpus =
 let create_vgpus ~__context host (vm, vm_r) hvm =
   let vgpus = vgpus_of_vm ~__context vm_r in
   if vgpus <> [] && not hvm then
-      raise (Api_errors.Server_error (Api_errors.feature_requires_hvm, ["vGPU- and GPU-passthrough needs HVM"]));
+    raise (Api_errors.Server_error (Api_errors.feature_requires_hvm, ["vGPU- and GPU-passthrough needs HVM"]));
   add_vgpus_to_vm ~__context host vm vgpus
 
 (* This function is called from Xapi_xenops, after forwarding, so possibly on a slave. *)
