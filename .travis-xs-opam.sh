1618aa412f933fddff79e1d8584343157f63e9f3
Christian Lindig <christian.lindig@citrix.com>
2019-06-05 13:41:53 +0100

    Revert "Use branch feature/REQ-720/master for xs-opam in travis"
    This reverts commit 551a0032f20db43ca6449327adda15245a82d2fb.

--- .travis-xs-opam.sh
+++ .travis-xs-opam.sh
@@ -5,7 +5,6 @@ set -ex
 PACKAGE="xapi"
 PINS="xapi:. xapi-cli-protocol:. xapi-client:. xapi-consts:. xapi-database:. xapi-datamodel:. xapi-types:. xe:."
 BASE_REMOTE="https://github.com/xapi-project/xs-opam.git"
-BASE_REMOTE_BRANCH="feature/REQ-720/master"
 DISTRO="debian-unstable"
 OCAML_VERSION="4.07"
 
