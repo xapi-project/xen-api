5f3f5eeeac3f809a6f52263aef6016c2859f2234
Christian Lindig <christian.lindig@citrix.com>
2019-06-19 12:57:28 +0100

    CA-321930 XSI-374 GPU compatibility check not done on snapshot revert
    When saving a snapshot, make sure to save VGPU metadata such that it is
    available on resume.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/xapi/xapi_vgpu.ml
+++ ocaml/xapi/xapi_vgpu.ml
@@ -50,7 +50,8 @@ let get_valid_device ~__context ~device ~vM ~vGPUs =
   else
     raise Api_errors.(Server_error (invalid_device, [device]))
 
-let create' ~__context  ~vM ~gPU_group ~device ~other_config ~_type ~powerstate_check =
+let create' ~__context  ~vM ~gPU_group ~device ~other_config ~_type 
+  ~powerstate_check ~compatibility_metadata =
   let vgpu = Ref.make () in
   let uuid = Uuid.to_string (Uuid.make_uuid ()) in
   if not (Pool_features.is_enabled ~__context Features.GPU) then
@@ -86,7 +87,7 @@ let create' ~__context  ~vM ~gPU_group ~device ~other_config ~_type ~powerstate_
       Db.VGPU.create ~__context ~ref:vgpu ~uuid ~vM ~gPU_group ~device:device_id
         ~currently_attached:false ~other_config ~_type ~resident_on:Ref.null
         ~scheduled_to_be_resident_on:Ref.null
-        ~compatibility_metadata:[]
+        ~compatibility_metadata
         ~extra_args:""
       ;
     );
@@ -100,7 +101,8 @@ let create' ~__context  ~vM ~gPU_group ~device ~other_config ~_type ~powerstate_
 *)
 let create ~__context  ~vM ~gPU_group ~device ~other_config ~_type =
   let powerstate_check = not (Db.VM.get_is_a_snapshot ~__context ~self:vM) in
-  create' ~__context  ~vM ~gPU_group ~device ~other_config ~_type ~powerstate_check
+  create' ~__context  ~vM ~gPU_group ~device ~other_config ~_type 
+    ~powerstate_check ~compatibility_metadata:[]
 
 let destroy ~__context ~self =
   let vm = Db.VGPU.get_VM ~__context ~self in
@@ -119,6 +121,7 @@ let copy ~__context ~vm vgpu =
       ~other_config:all.API.vGPU_other_config
       ~_type:all.API.vGPU_type
       ~powerstate_check:false
+      ~compatibility_metadata:all.API.vGPU_compatibility_metadata
   in
   if all.API.vGPU_currently_attached then
     Db.VGPU.set_currently_attached ~__context ~self:vgpu ~value:true;
