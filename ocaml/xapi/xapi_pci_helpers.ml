e3e744a48662943c3d99378732096bea41d6df21
Christian Lindig <christian.lindig@citrix.com>
2019-08-15 13:33:27 +0100

    REQ-627 CP-32057 cache sys-fs queries
    To address concerns about repeated queries into /sys/bus/pci to
    determine whether a PCI device is virtual or not, cache the results
    while scanning devices.
    
    The cache is local to get_host_pcis() and thus there is no danger of
    stale cache entries.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/xapi/xapi_pci_helpers.ml
+++ ocaml/xapi/xapi_pci_helpers.ml
@@ -45,27 +45,48 @@ let address_of_dev x =
   let open Pci.Pci_dev in
   Printf.sprintf "%04x:%02x:%02x.%d" x.domain x.bus x.dev x.func
 
-(** [is_virtual_pci "0000:37:00.4"] is true, if this designates a
- * virtual PCI function (VF), false otherwise. Only a VF has a "physfn"
- * symbolic link.
- *)
-let is_virtual_pci addr =
-  let path = Printf.sprintf "/sys/bus/pci/devices/%s/physfn" addr in
-  try
-    ignore @@ Unix.readlink path; true
-  with _ -> false
+(* Check for a PCI device whether it is virtual and remember the result
+ * such that it can be looked up later *)
+module PCIcache: sig
+  type t
+  type addr = string (* "0000:37:00.4" *)
+  val make: unit -> t
+  val is_virtual: t -> addr -> bool
+end = struct
+  type t = (string, bool) Hashtbl.t
+  type addr = string
+
+  (** [is_virtual_pci "0000:37:00.4"] is true, if this designates a
+   * virtual PCI function (VF), false otherwise. Only a VF has a "physfn"
+   * symbolic link.
+   *)
+  let is_virtual addr =
+    let path = Printf.sprintf "/sys/bus/pci/devices/%s/physfn" addr in
+    try
+      ignore @@ Unix.readlink path; true
+    with _ -> false
+
+  let make () = Hashtbl.create 100
+  let is_virtual t addr =
+    try
+      Hashtbl.find t addr
+    with Not_found ->
+      let v = is_virtual addr in
+      Hashtbl.replace t addr v; v
+end
+
 
 (** [is_related_to x y] is true, if two non-virtual PCI devices
  *  only differ in their function.
  *)
-let is_related_to (x:Pci.Pci_dev.t) (y:Pci.Pci_dev.t) =
+let is_related_to cache (x:Pci.Pci_dev.t) (y:Pci.Pci_dev.t) =
   let open Pci.Pci_dev in
   x.domain = y.domain
   && x.bus = y.bus
   && x.dev = y.dev
   && x.func <> y.func
-  && not @@ is_virtual_pci @@ address_of_dev x
-  && not @@ is_virtual_pci @@ address_of_dev y
+  && not @@ PCIcache.is_virtual cache @@ address_of_dev x
+  && not @@ PCIcache.is_virtual cache @@ address_of_dev y
 
 let get_host_pcis () =
   let default ~msg v =
@@ -76,6 +97,7 @@ let get_host_pcis () =
   let open Pci in
   with_access (fun access ->
       let devs = get_devices access in
+      let cache = PCIcache.make () in
       List.map (fun d ->
           let open Pci_dev in
           debug "get_host_pcis: vendor=%04x device=%04x class=%04x"
@@ -105,7 +127,7 @@ let get_host_pcis () =
                           ; name = lookup_class_name access d.device_class
                                    |> default ~msg:"class name" }
           in
-          let related_devs = List.filter (is_related_to d) devs in
+          let related_devs = List.filter (is_related_to cache d) devs in
           { address;
             vendor; device; subsystem_vendor; subsystem_device; pci_class;
             related = List.map address_of_dev related_devs; driver_name; 
