005e2bb8096a66f31c16209c865417c9dabce769
Pau Ruiz Safont <pau.safont@citrix.com>
2019-09-30 11:27:36 +0100

    CP-32138: Use collaborative log reporter
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- cli/wsproxy.ml
+++ cli/wsproxy.ml
@@ -117,9 +117,34 @@ let handler sock msg =
     proxy sock addr protocol
   | _ -> Logs_lwt.warn (fun m -> m "Malformed msg: not proxying")
 
+(* Reporter taken from
+ * https://erratique.ch/software/logs/doc/Logs_lwt/index.html#report_ex
+ * under ISC License *)
+let lwt_reporter () =
+  let buf_fmt ~like =
+    let b = Buffer.create 512 in
+    Fmt.with_buffer ~like b,
+    fun () -> let m = Buffer.contents b in Buffer.reset b; m
+  in
+  let app, app_flush = buf_fmt ~like:Fmt.stdout in
+  let dst, dst_flush = buf_fmt ~like:Fmt.stderr in
+  let reporter = Logs_fmt.reporter ~app ~dst () in
+  let report src level ~over k msgf =
+    let k () =
+      let write () = match level with
+      | Logs.App -> Lwt_io.write Lwt_io.stdout (app_flush ())
+      | _ -> Lwt_io.write Lwt_io.stderr (dst_flush ())
+      in
+      let unblock () = over (); Lwt.return_unit in
+      Lwt.finalize write unblock |> Lwt.ignore_result;
+      k ()
+    in
+    reporter.Logs.report src level ~over:(fun () -> ()) k msgf;
+  in
+  { Logs.report = report }
 
 let _ =
-  Logs.set_reporter (Logs_fmt.reporter ());
+  Logs.set_reporter (lwt_reporter ());
   (* Enable logging for all levels *)
   Logs.set_level ~all:true (Some Logs.Debug);
   let filename = "/var/run/wsproxy.pid" in
