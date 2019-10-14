b23e2a002a6bf668cd3a7c27ae85014559785e6f
Christian Lindig <christian.lindig@citrix.com>
2019-10-14 13:37:12 +0100

    Revert "REQ-627 set up Travis (revert this later!)"
    Reverting Travis setup in preparation of a merge to master.
    
    This reverts commit 8d4260b4872b7041eb1df845772b4057ba2d979a.

--- .travis-xs-opam.sh
+++ .travis-xs-opam.sh
@@ -5,7 +5,6 @@ set -ex
 PACKAGE="xapi"
 PINS="xapi:. xapi-cli-protocol:. xapi-client:. xapi-consts:. xapi-database:. xapi-datamodel:. xapi-types:. xe:."
 BASE_REMOTE="https://github.com/xapi-project/xs-opam.git"
-BASE_REMOTE_BRANCH="REQ-627"
 
 wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-docker.sh
 wget https://raw.githubusercontent.com/xapi-project/xs-opam/master/tools/xs-opam-ci.env
