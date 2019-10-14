f4a2580d53abadef112b74f898be108fa62eeb2b
Edwin Török <edvin.torok@citrix.com>
2019-10-14 15:34:26 +0100

    Backport warning fix from Xen
    We should really upstream these Xen bindings, but meantime:
    http://xenbits.xen.org/gitweb/?p=xen.git;a=commitdiff;h=2adc90908fbb1e614c477e29f2d45eda94570795
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- c_stubs/xenctrlext_stubs.c
+++ c_stubs/xenctrlext_stubs.c
@@ -32,21 +32,20 @@
 #define _D(__d) ((uint32_t)Int_val(__d))
 
 /* From xenctrl_stubs */
-#define ERROR_STRLEN 1024
 static void failwith_xc(xc_interface *xch)
 {
-        static char error_str[ERROR_STRLEN];
+        static char error_str[XC_MAX_ERROR_MSG_LEN + 6];
         if (xch) {
                 const xc_error *error = xc_get_last_error(xch);
                 if (error->code == XC_ERROR_NONE)
-                        snprintf(error_str, ERROR_STRLEN, "%d: %s", errno, strerror(errno));
+                        snprintf(error_str, sizeof(error_str), "%d: %s", errno, strerror(errno));
                 else
-                        snprintf(error_str, ERROR_STRLEN, "%d: %s: %s",
+                        snprintf(error_str, sizeof(error_str), "%d: %s: %s",
                                  error->code,
                                  xc_error_code_to_desc(error->code),
                                  error->message);
         } else {
-                snprintf(error_str, ERROR_STRLEN, "Unable to open XC interface");
+                snprintf(error_str, sizeof(error_str), "Unable to open XC interface");
         }
         caml_raise_with_string(*caml_named_value("xc.error"), error_str);
 }
