f05d709e726afb7657011601f9f1f238b40e51ee
Pau Ruiz Safont <pau.safont@citrix.com>
2019-10-15 15:36:01 +0100

    CA-329043: Do not produce default values that are out-of-bounds
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- lib/rrd.ml
+++ lib/rrd.ml
@@ -60,13 +60,6 @@ let cf_type_to_string cf =
   | CF_Min -> "MIN"
   | CF_Last -> "LAST"
 
-let cf_init_value cf =
-  match cf with
-  | CF_Average -> 0.0
-  | CF_Min -> infinity
-  | CF_Max -> neg_infinity
-  | CF_Last -> nan
-
 (** The CDP preparation scratch area.
     The 'value' field should be accumulated in such a way that it always
     contains the value that will eventually be the CDP. This means that
@@ -163,6 +156,16 @@ let copy_rrd x =
     rrd_rras = Array.map copy_rra x.rrd_rras;
   }
 
+(* CA-329043: avoid producing out-of-range rates *)
+let cf_init_value cf ds =
+  let default = match cf with
+  | CF_Average -> 0.0
+  | CF_Min -> infinity
+  | CF_Max -> neg_infinity
+  | CF_Last -> nan
+  in
+  min ds.ds_max (max ds.ds_min default)
+
 (** Helper function to get the start time and age of the current/last PDP *)
 let get_times time timestep =
   let starttime = timestep *** ((Int64.of_float time) /// timestep) in
@@ -224,8 +227,12 @@ let rra_update rrd proc_pdp_st elapsed_pdp_st pdps =
 
         (* Reinitialise the CDP preparation area *)
         let new_start_pdp_offset = (elapsed_pdp_st - start_pdp_offset) mod rra.rra_pdp_cnt in
-        let cdp_init = cf_init_value rra.rra_cf in
-        Array.iter (fun cdp -> cdp.cdp_unknown_pdps <- 0; cdp.cdp_value <- cdp_init) rra.rra_cdps;
+        Array.iteri (fun i cdp ->
+          let ds = rrd.rrd_dss.(i) in
+          let cdp_init = cf_init_value rra.rra_cf ds in
+          cdp.cdp_unknown_pdps <- 0;
+          cdp.cdp_value <- cdp_init
+        ) rra.rra_cdps;
         do_cfs rra new_start_pdp_offset pdps;
         match rra.rra_updatehook with None -> () | Some f -> f rrd rra_step_cnt
       end
@@ -397,7 +404,10 @@ let rrd_create dss rras timestep inittime =
     rrd_rras=Array.map (fun rra ->
         { rra with
           rra_data = Array.init (Array.length dss) (fun _ -> Fring.make rra.rra_row_cnt nan);
-          rra_cdps = Array.init (Array.length dss) (fun _ -> {cdp_value=0.0; cdp_unknown_pdps=0})
+          rra_cdps = Array.init (Array.length dss) (fun i ->
+            let ds = dss.(i) in
+            let cdp_init = cf_init_value rra.rra_cf ds in
+            {cdp_value=cdp_init; cdp_unknown_pdps=0})
         }) rras;
   } in
   let values = Array.map (fun ds -> ds.ds_last) dss in
@@ -417,11 +427,13 @@ let rrd_add_ds rrd now newds =
     let npdps = Int64.of_float now /// rrd.timestep in
     {rrd with
      rrd_dss = Array.append rrd.rrd_dss [|newds|];
-     rrd_rras = Array.map (fun rra ->
+     rrd_rras = Array.mapi (fun i rra ->
+        let ds = rrd.rrd_dss.(i) in
+        let cdp_init = cf_init_value rra.rra_cf ds in
          let nunknowns = Int64.to_int (Int64.rem npdps (Int64.of_int rra.rra_pdp_cnt)) in
          { rra with
            rra_data = Array.append rra.rra_data [| Fring.make rra.rra_row_cnt nan |];
-           rra_cdps = Array.append rra.rra_cdps [| {cdp_value=cf_init_value rra.rra_cf; cdp_unknown_pdps=nunknowns} |]; }) rrd.rrd_rras;
+           rra_cdps = Array.append rra.rra_cdps [| {cdp_value=cdp_init; cdp_unknown_pdps=nunknowns} |]; }) rrd.rrd_rras;
     }
 
 (** Remove the named DS from an RRD. Removes all of the data associated with it, too *)
