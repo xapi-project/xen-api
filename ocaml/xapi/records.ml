7f79f70c25449563c784c0ba78b5a070f45f7808
lippirk <ben.anson@citrix.com>
2019-10-30 11:27:57 +0000

    maintenance: add current_operations to task record
    Signed-off-by: lippirk <ben.anson@citrix.com>

--- ocaml/xapi/records.ml
+++ ocaml/xapi/records.ml
@@ -338,6 +338,7 @@ let task_record rpc session_id task =
       make_field ~name:"finished"    ~get:(fun () -> Date.to_string (x ()).API.task_finished) ();
       make_field ~name:"error_info"         ~get:(fun () -> String.concat "; " (x ()).API.task_error_info) ();
       make_field ~name:"allowed_operations" ~get:(fun () -> String.concat "; " (List.map Record_util.task_allowed_operations_to_string (x ()).API.task_allowed_operations)) ();
+      make_field ~name:"current_operations" ~get:(fun () -> (x ()).API.task_current_operations |> List.map (fun (_, op) -> Record_util.task_allowed_operations_to_string op) |> String.concat "; ") ();
       make_field ~name:"other-config" ~get:(fun () -> Record_util.s2sm_to_string "; " (x ()).API.task_other_config)
         ~add_to_map:(fun k v -> Client.Task.add_to_other_config rpc session_id task k v)
         ~remove_from_map:(fun k -> Client.Task.remove_from_other_config rpc session_id task k)
