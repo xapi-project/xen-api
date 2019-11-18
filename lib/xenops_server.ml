xapi-project/xen-api

commit a7563620e89a891c5a454c1d932ac3f12d0c4c3a

CA-315450 pause VM before shutdown
To make sure a domain is not running before destroying its device model,
pause it.

Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

lib/xenops_server.ml
@@ -947,7 +947,12 @@ let rec atomics_of_operation = function
     ) @ (List.map (fun vusb -> VUSB_unplug vusb.Vusb.id)
            (VUSB_DB.vusbs id)
         ) @ simplify ([
-        (* At this point we have a shutdown domain (ie Needs_poweroff) *)
+        (* CA-315450: in a hard shutdown or snapshot revert,
+         * timeout=None and VM_shutdown_domain is not called. To avoid
+         * any interference, we pause the domain before destroying the
+         * device model.
+         *)
+        VM_pause id;
         VM_destroy_device_model id;
       ] @ (
           let vbds = VBD_DB.vbds id in

commit ae07e23de355a3b43aa36c7de533cecb31da4076

CA-330830: ignore errors during VM.pause on hard shutdown
In particular Domain_not_built.
This fixes a regression introduced by a7563620e89a891c5a454c1d932ac3f12d0c4c3a.

VM_check_state looks at the domain and what actions should be performed.
On an incomplete migration you might get a VM_shutdown as required
action. However VM_shutdown calls VM_pause, which now fails because the
domain was not built yet.
We should let VM_shutdown proceed in this case to finish cleaning up:
if the domain is not built then there is nothing to pause, we ensured
that it is not running.

Signed-off-by: Edwin Török <edvin.torok@citrix.com>

lib/xenops_server.ml
@@ -116,6 +116,7 @@ type atomic =
   | VM_delay of (Vm.id * float) (** used to suppress fast reboot loops *)
   | VM_rename of (Vm.id * Vm.id)
   | Parallel of Vm.id * string * atomic list
+  | Best_effort of atomic
 
 [@@deriving rpcty]
 
@@ -988,7 +989,7 @@ let rec atomics_of_operation = function
          * any interference, we pause the domain before destroying the
          * device model.
          *)
-        VM_pause id;
+        Best_effort (VM_pause id);
         VM_destroy_device_model id;
         Parallel (id, (Printf.sprintf "VBD.unplug vm=%s" id), List.map (fun vbd -> VBD_unplug (vbd.Vbd.id, true)) vbds)
       ]
@@ -1151,6 +1152,10 @@ let rec perform_atomic ~progress_callback ?subtask:_ ?result (op: atomic) (t: Xe
   let module B = (val get_backend () : S) in
   Xenops_task.check_cancelling t;
   match op with
+  | Best_effort atom ->
+    begin try perform_atomic ~progress_callback ?result atom t
+    with e -> debug "Ignoring error during best-effort operation: %s" (Printexc.to_string e)
+    end
   | Parallel (_id, description, atoms) ->
     (* parallel_id is a unused unique name prefix for a parallel worker queue *)
     let parallel_id = Printf.sprintf "Parallel:task=%s.atoms=%d.(%s)" (Xenops_task.id_of_handle t) (List.length atoms) description in
@@ -1660,6 +1665,8 @@ and trigger_cleanup_after_failure_atom op t =
   | VM_restore (id, _, _)
   | VM_delay (id, _) ->
     immediate_operation dbg id (VM_check_state id)
+  | Best_effort op ->
+    trigger_cleanup_after_failure_atom op t
   | Parallel (_id, _description, ops) ->
     List.iter (fun op->trigger_cleanup_after_failure_atom op t) ops
   | VM_rename (id1,id2) ->