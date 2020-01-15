3500b9025018773774e9e8802a67bbe01a9f7b81
Pau Ruiz Safont <pau.safont@citrix.com>
2020-01-15 11:01:33 +0000

    REQ-453: Set up travis to use correct xs-opam branch (delete me before merging to master)

--- .travis-xs-opam.sh
+++ .travis-xs-opam.sh
@@ -4,7 +4,7 @@ set -ex
 
 PACKAGE="xapi"
 PINS="xapi:. xapi-cli-protocol:. xapi-client:. xapi-consts:. xapi-database:. xapi-datamodel:. xapi-types:. xe:."
-BASE_REMOTE="https://github.com/xapi-project/xs-opam.git"
+BASE_REMOTE_BRANCH="feature/REQ-453/master"
 
 wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-docker.sh
 wget https://raw.githubusercontent.com/xapi-project/xs-opam/master/tools/xs-opam-ci.env
