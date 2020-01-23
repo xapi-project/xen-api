693a427c45ab109300f4eb2f53b5dcb1e48b4d83
Pau Ruiz Safont <pau.safont@citrix.com>
2020-01-23 14:51:39 +0000

    CP-32678: update to X509 0.9.0
    Previously hostname could be empty if there were no hostnames available
    in the certificate. Now the hostname from xapi's database is picked
    instead, xapi_vdi was adapted.
    
    Signing certificates needs the usage of result as it verifies before
    actually signing the certificate, test code was adapted.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/tests/test_certificates.ml
+++ ocaml/tests/test_certificates.ml
@@ -3,6 +3,8 @@
 open Certificates
 open Api_errors
 
+open Rresult.R.Infix
+
 (* Initialize RNG for testing certificates *)
 let () = Nocrypto_entropy_unix.initialize ()
 
@@ -110,13 +112,13 @@ let test_invalid_key key_name error reason () =
 
 let valid_keys_tests =
   List.map (fun name ->
-    "Validation of a supported key: " ^ name, `Quick, test_valid_key name)
+    "Validation of a supported key: " ^ name, `Quick, Ok (test_valid_key name))
   valid_private_keys
 
 let invalid_keys_tests =
   List.map (fun (name, error, reason) ->
     "Validation of an unsupported key: " ^ name,
-    `Quick, test_invalid_key name error reason)
+    `Quick, Ok (test_invalid_key name error reason))
   invalid_private_keys
 
 let test_valid_cert cert time pkey () =
@@ -128,52 +130,55 @@ let test_invalid_cert cert time pkey error reason () =
     (fun () -> validate_certificate Leaf cert time pkey)
 
 let load_pkcs8 name =
-  match X509.Private_key.decode_pem (Cstruct.of_string (load_test_data name)) with
-  | Ok pkey ->
-      pkey
-  | Error (`Msg msg) ->
-    raise (Failure
-      (Printf.sprintf "Could not load private key with name '%s': %s" name msg))
+  X509.Private_key.decode_pem (Cstruct.of_string (load_test_data name))
+  |> Rresult.R.reword_error (fun (`Msg msg) ->
+    `Msg (Printf.sprintf "Could not load private key with name '%s': %s" name msg))
 
 let sign_cert host_name ~pkey_sign digest pkey_leaf =
   let csr = X509.Signing_request.create [host_name] ~digest pkey_leaf in
-  X509.Signing_request.sign csr ~valid_from ~valid_until ~digest pkey_sign [host_name]
+  X509.Signing_request.sign csr ~valid_from ~valid_until ~digest ~hash_whitelist:[digest] pkey_sign [host_name]
 
 let sign_leaf_cert host_name digest pkey_leaf =
-  let pkey_sign = load_pkcs8 "pkey_rsa_4096" in
+  load_pkcs8 "pkey_rsa_4096" >>= fun pkey_sign ->
   sign_cert host_name ~pkey_sign digest pkey_leaf
-  |> X509.Certificate.encode_pem
-  |> Cstruct.to_string
+  >>| X509.Certificate.encode_pem
+  >>| Cstruct.to_string
 
 let valid_leaf_cert_tests =
   List.map (fun (name, pkey_leaf_name, time, digest) ->
-    let pkey_leaf = load_pkcs8 pkey_leaf_name in
-    let cert = sign_leaf_cert host_name digest pkey_leaf in
-    "Validation of a supported certificate: " ^ name,
-    `Quick, test_valid_cert cert (time_of_rfc3339 time) pkey_leaf)
+    let cert_test =
+      load_pkcs8 pkey_leaf_name >>= fun pkey_leaf ->
+      sign_leaf_cert host_name digest pkey_leaf >>| fun cert ->
+      test_valid_cert cert (time_of_rfc3339 time) pkey_leaf
+    in
+    "Validation of a supported certificate: " ^ name, `Quick, cert_test)
   valid_leaf_certificates
 
-let corrupt_leaf_cert_tests =
-  List.map (fun (cert_name, pkey_name, time, error, reason) ->
+
+let test_corrupt_leaf_cert (cert_name, pkey_name, time, error, reason) =
   let cert = load_test_data cert_name in
-  let pkey = load_pkcs8 pkey_name in
   let time = time_of_rfc3339 time in
-  let test () = Alcotest.check_raises ""
-    (server_error error reason)
-    (fun () -> validate_certificate Leaf cert time pkey)
+  let test_cert = load_pkcs8 pkey_name >>| fun pkey ->
+    let test () = Alcotest.check_raises ""
+      (server_error error reason)
+      (fun () -> validate_certificate Leaf cert time pkey)
+    in
+    test
   in
-  "Validation of a corrupted certificate", `Quick, test)
-  corrupt_certificates
+  "Validation of a corrupted certificate", `Quick, test_cert
+
+let test_invalid_leaf_cert (name, pkey_leaf_name, pkey_expected_name, time, digest, error, reason) =
+  let test_cert =
+    load_pkcs8 pkey_leaf_name >>= fun pkey_leaf ->
+    load_pkcs8 pkey_expected_name >>= fun pkey_expected ->
+    sign_leaf_cert host_name digest pkey_leaf >>| fun cert ->
+    test_invalid_cert cert (time_of_rfc3339 time) pkey_expected error reason
+  in
+  "Validation of an unsupported certificate: " ^ name, `Quick, test_cert
 
 let invalid_leaf_cert_tests =
-  List.map (fun (name, pkey_leaf_name, pkey_expected_name, time, digest, error, reason) ->
-    let pkey_leaf = load_pkcs8 pkey_leaf_name in
-    let pkey_expected = load_pkcs8 pkey_expected_name in
-    let cert = sign_leaf_cert host_name digest pkey_leaf in
-    "Validation of an unsupported certificate: " ^ name,
-    `Quick, test_invalid_cert cert (time_of_rfc3339 time) pkey_expected error reason)
-  invalid_leaf_certificates
-  @ corrupt_leaf_cert_tests
+  List.map test_corrupt_leaf_cert corrupt_certificates @
+  List.map test_invalid_leaf_cert invalid_leaf_certificates
 
 let test_valid_cert_chain chain time pkey () =
   validate_certificate Chain chain time pkey
@@ -185,22 +190,41 @@ let test_invalid_cert_chain cert time pkey error reason () =
 
 let valid_chain_cert_tests =
   let time = time_of_rfc3339 "2020-02-01T00:00:00+00:00" in
-  let pkey, chain = List.fold_left (fun (pkey_sign, chain) pkey ->
-    let cert = sign_cert host_name ~pkey_sign `SHA256 pkey in
-    pkey, cert :: chain
-  ) ((load_pkcs8 "pkey_rsa_4096"), []) key_chain in
-  let chain = Cstruct.to_string (X509.Certificate.encode_pem_multiple chain) in
-  ["Validation of a supported certificate chain",
-  `Quick, test_valid_cert_chain chain time pkey]
+  let test_cert =
+    load_pkcs8 "pkey_rsa_4096" >>= fun pkey_root ->
+    let pkey, chain = List.fold_left (fun (pkey_sign, chain_result) pkey ->
+      let result = chain_result
+        >>| fun chain -> sign_cert host_name ~pkey_sign `SHA256 pkey
+        >>| fun cert -> cert :: chain
+      in
+      pkey, Rresult.R.join result
+    ) (pkey_root, Ok []) key_chain in
+    chain
+    >>| X509.Certificate.encode_pem_multiple
+    >>| Cstruct.to_string
+    >>| fun chain -> test_valid_cert_chain chain time pkey
+  in
+  ["Validation of a supported certificate chain", `Quick, test_cert]
 
 let invalid_chain_cert_tests =
   List.map (fun (chain_name, pkey_name, time, error, reason) ->
     let chain = load_test_data chain_name in
-    let pkey = load_pkcs8 pkey_name in
-    "Validation of an unsupported certificate chain",
-    `Quick, test_invalid_cert_chain chain (time_of_rfc3339 time) pkey error reason)
+    let test_cert =
+      load_pkcs8 pkey_name >>| fun pkey ->
+      test_invalid_cert_chain chain (time_of_rfc3339 time) pkey error reason
+    in
+    "Validation of an unsupported certificate chain", `Quick, test_cert)
   corrupt_chain_certificates
 
-let test = valid_keys_tests @ invalid_keys_tests @
-           valid_leaf_cert_tests @ invalid_leaf_cert_tests @
-           valid_chain_cert_tests @ invalid_chain_cert_tests
+let load_test = function
+  | name, speed, Ok test ->
+      name, speed, test
+  | name, speed, Error (`Msg msg) ->
+      name, speed, Alcotest.fail
+        (Printf.sprintf "Problem preparing test '%s': %s" name msg)
+
+let all_tests = valid_keys_tests @ invalid_keys_tests @
+                valid_leaf_cert_tests @ invalid_leaf_cert_tests @
+                valid_chain_cert_tests @ invalid_chain_cert_tests
+
+let test = List.map load_test all_tests
