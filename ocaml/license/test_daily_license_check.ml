fa78cdb655bbbc91666a5a999404b7e7b553edee
Pau Ruiz Safont <pau.safont@citrix.com>
2020-03-09 15:31:05 +0000

    license: enable unit-tests
    Ported them to alcotest as well
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/license/test_daily_license_check.ml
+++ ocaml/license/test_daily_license_check.ml
@@ -12,74 +12,74 @@
  * GNU Lesser General Public License for more details.
  *)
 
-open OUnit
-open Test_highlevel
 open Daily_license_check
 
-module Tests = Generic.Make (struct
-    module Io = struct
-      type input_t = (string * string) list * (string * (string * string) list) list
-      type output_t = result
-      let string_of_input_t = Test_printers.(pair (assoc_list string string) (assoc_list string (assoc_list string string)))
-      let string_of_output_t = function
-        | Good -> "Not expired"
-        | Expiring hosts -> "Expiring soon on hosts: " ^ (Test_printers.(list string) hosts)
-        | Expired hosts -> "Expired on hosts: " ^ (Test_printers.(list string) hosts)
-    end
+let pp_expiry fmt = function
+  | Good ->
+    Fmt.pf fmt "Good"
+  | Expiring hosts ->
+    Fmt.pf fmt "Expiring soon on %a" Fmt.(Dump.list @@ string) hosts
+  | Expired hosts ->
+    Fmt.pf fmt "Expired on %a" Fmt.(Dump.list @@ string) hosts
 
-    let now = Xapi_stdext_date.Date.(to_float (of_string "20160601T04:00:00Z"))
+(** Testable type for Alcotest *)
+let expiry =
+  let equals x y = match x, y with
+   | Good, Good ->
+     true
+   | Expiring hosts_x, Expiring hosts_y
+   | Expired hosts_x, Expired hosts_y when hosts_x = hosts_y ->
+     true
+   | _ ->
+     false
+  in
+  Alcotest.testable pp_expiry equals
 
-    let transform = fun (pool_license_state, all_license_params) ->
-      check_license now pool_license_state all_license_params
+let check_time =
+ Xapi_stdext_date.Date.(to_float (of_string "20160601T04:00:00Z"))
 
-    let tests = [
-      (["expiry", "20170101T00:00:00Z"], []),
-      Good;
+let test_expiry ((pool_license_state, all_license_params), expected) () =
+  let result = check_license check_time pool_license_state all_license_params in
+  Alcotest.check expiry "pool_expiry" expected result
 
-      (["expiry", "20160701T04:01:00Z"], []),
-      Good;
+let expiry_samples =
+  [ (["expiry", "20170101T00:00:00Z"], []),
+    Good
+  ; (["expiry", "20160701T04:01:00Z"], []),
+    Good
+  ; (["expiry", "20160701T04:00:00Z"], []),
+    Expiring []
+  ; (["expiry", "20160616T00:00:00Z"], []),
+    Expiring []
+  ; (["expiry", "20160601T04:00:01Z"], []),
+    Expiring []
+  ; (["expiry", "20160601T04:00:00Z"], []),
+    Expired []
+  ; (["expiry", "20160101T00:00:00Z"], []),
+    Expired []
+  ; (["expiry", "20160615T00:00:00Z"],
+    ["host0", ["expiry", "20160615T00:00:00Z"];
+     "host1", ["expiry", "20160615T00:00:00Z"]]),
+    Expiring ["host1"; "host0"]
+  ; (["expiry", "20160615T00:00:00Z"],
+    ["host0", ["expiry", "20160615T00:00:00Z"];
+     "host1", ["expiry", "20160715T00:00:00Z"]]),
+    Expiring ["host0"]
+  ; (["expiry", "20160101T00:00:00Z"],
+    ["host0", ["expiry", "20160601T00:00:00Z"];
+     "host1", ["expiry", "20150601T00:00:00Z"]]),
+    Expired ["host1"; "host0"]
+  ; (["expiry", "20160101T00:00:00Z"],
+    ["host0", ["expiry", "20170601T00:00:00Z"];
+     "host1", ["expiry", "20150601T00:00:00Z"]]),
+    Expired ["host1"]
+]
 
-      (["expiry", "20160701T04:00:00Z"], []),
-      Expiring [];
-
-      (["expiry", "20160616T00:00:00Z"], []),
-      Expiring [];
-
-      (["expiry", "20160601T04:00:01Z"], []),
-      Expiring [];
-
-      (["expiry", "20160601T04:00:00Z"], []),
-      Expired [];
-
-      (["expiry", "20160101T00:00:00Z"], []),
-      Expired [];
-
-      (["expiry", "20160615T00:00:00Z"],
-       ["host0", ["expiry", "20160615T00:00:00Z"];
-        "host1", ["expiry", "20160615T00:00:00Z"]]),
-      Expiring ["host1"; "host0"];
-
-      (["expiry", "20160615T00:00:00Z"],
-       ["host0", ["expiry", "20160615T00:00:00Z"];
-        "host1", ["expiry", "20160715T00:00:00Z"]]),
-      Expiring ["host0"];
-
-      (["expiry", "20160101T00:00:00Z"],
-       ["host0", ["expiry", "20160601T00:00:00Z"];
-        "host1", ["expiry", "20150601T00:00:00Z"]]),
-      Expired ["host1"; "host0"];
-
-      (["expiry", "20160101T00:00:00Z"],
-       ["host0", ["expiry", "20170601T00:00:00Z"];
-        "host1", ["expiry", "20150601T00:00:00Z"]]),
-      Expired ["host1"];
-    ]
-  end)
-
-let base_suite =
-  "base_suite" >:::
-  [ "test_daily_license_check" >::: Tests.tests ]
+let test =
+  List.mapi (fun i spec ->
+    Printf.sprintf "Test licence checks as run daily #%d" i,
+      `Quick, test_expiry spec) expiry_samples
 
 let () =
-  Printexc.record_backtrace true;
-  ounit2_of_ounit1 base_suite |> OUnit2.run_test_tt_main
+  Alcotest.run "Daily license checks"
+  [("Daily license checks", test)]
