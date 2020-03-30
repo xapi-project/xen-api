cabf4301a0da7525d434b197810c70436b5849b0
Rob Hoes <rob.hoes@citrix.com>
2020-03-30 15:22:34 +0100

    CA-337113: Turn create_tools_sr into remove_tools_sr
    The Tools ISO has been removed from dom0, so there is no need anymore for the
    Tools SR.
    
    The removal of the Tools SR is best-effort: a warning is logged if it fails (as
    done before for redundant SRs). Failure occurs if there is still some VM that
    has a Tools VDI attached to it. The cleanup code runs each time the master xapi
    start, so once this situation has been resolved, the SR will eventually get
    cleaned up.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/dbsync_master.ml
+++ ocaml/xapi/dbsync_master.ml
@@ -107,90 +107,23 @@ let release_locks ~__context =
   List.iter (fun self -> Db.VM.set_scheduled_to_be_resident_on ~__context ~self ~value:Ref.null)
     (Db.VM.get_all ~__context)
 
-let create_tools_sr __context name_label name_description sr_introduce maybe_create_pbd =
-  let create_magic_sr name_label name_description other_config =
-    (* Create a new SR and PBD record *)
-    (* N.b. dbsync_slave is called _before_ this, so we can't rely on the PBD creating code in there
-       		   to make the PBD for the shared tools SR *)
-    let sr =
-      sr_introduce
-        ~uuid:(Uuid.to_string (Uuid.make_uuid()))
-        ~name_label ~name_description
-        ~_type:"iso" ~content_type:"iso" ~shared:true ~sm_config:[]
-    in
-    Db.SR.set_other_config ~__context ~self:sr ~value:other_config;
-    Db.SR.set_is_tools_sr ~__context ~self:sr ~value:true;
-    (* Master has created this shared SR, lets make PBDs for all of the slaves too. Nb. device-config is same for all hosts *)
-    let hosts = Db.Host.get_all ~__context in
-    List.iter (fun host -> ignore (maybe_create_pbd sr Xapi_globs.tools_sr_pbd_device_config host)) hosts;
-    sr
-  in
-  let other_config = [
-    Xapi_globs.xensource_internal, "true";
-    Xapi_globs.tools_sr_tag, "true";
-    Xapi_globs.i18n_key, "xenserver-tools";
-    (Xapi_globs.i18n_original_value_prefix ^ "name_label"), name_label;
-    (Xapi_globs.i18n_original_value_prefix ^ "name_description"), name_description
-  ] in
+let remove_tools_sr __context =
   let destroy self =
     try Db.SR.destroy ~__context ~self with e ->
       warn "failed to destroy redundant tools SR %s: %s"
         (Ref.string_of self) (Printexc.to_string e) in
-  let sr =
-    let srs = Db.SR.get_all ~__context in
-    let tools_srs =
-      List.filter (fun self -> Db.SR.get_is_tools_sr ~__context ~self) srs in
-    let old_srs =
-      List.filter (fun self ->
-          let other_config = Db.SR.get_other_config ~__context ~self in
-          (Db.SR.get_is_tools_sr ~__context ~self = false) &&
-          ((List.mem_assoc Xapi_globs.tools_sr_tag other_config) ||
-           (List.mem_assoc Xapi_globs.xensource_internal other_config))
-        ) srs in
-    match tools_srs with
-    | sr :: others ->
-      (* Let there be only one Tools SR *)
-      List.iter destroy others;
-      List.iter destroy old_srs;
-      sr
-    | [] ->
-      (* First check if there is an SR with the old tags on it, which needs upgrading (set is_tools_sr). *)
-      (* We cannot do this in xapi_db_upgrade, because that runs later. *)
-      match old_srs with
-      | sr :: others ->
-        Db.SR.set_is_tools_sr ~__context ~self:sr ~value:true;
-        List.iter destroy others; (* destroy bogus Tool SRs CA-300103 *)
-        sr
-      | [] ->
-        create_magic_sr name_label name_description other_config
-  in
-  (* Ensure fields are up-to-date *)
-  Db.SR.set_name_label ~__context ~self:sr ~value:name_label;
-  Db.SR.set_name_description ~__context ~self:sr ~value:name_description;
-  let other_config =
-    (* Keep any existing keys/value pair besides the required ones *)
-    let oc = Db.SR.get_other_config ~__context ~self:sr in
-    let keys = List.map fst other_config in
-    let extra = List.filter (fun (k, _) -> not (List.mem k keys)) oc in
-    extra @ other_config
-  in
-  Db.SR.set_other_config ~__context ~self:sr ~value:other_config;
-  List.iter
-    (fun self ->
-       Db.PBD.set_device_config ~__context ~self
-         ~value:Xapi_globs.tools_sr_pbd_device_config)
-    (Db.SR.get_PBDs ~__context ~self:sr)
-
-let create_tools_sr_noexn __context =
-  let name_label = Xapi_globs.tools_sr_name () in
-  let name_description = Xapi_globs.tools_sr_description () in
-  Helpers.call_api_functions ~__context (fun rpc session_id ->
-      let sr_introduce = Client.SR.introduce ~rpc ~session_id in
-      let maybe_create_pbd  = Create_storage.maybe_create_pbd rpc session_id in
-      Helpers.log_exn_continue "creating tools SR" (fun () ->
-          create_tools_sr __context name_label name_description sr_introduce maybe_create_pbd
-        ) ()
-    )
+  let srs = Db.SR.get_all ~__context in
+  let tools_srs =
+    List.filter (fun self -> Db.SR.get_is_tools_sr ~__context ~self) srs in
+  let old_srs =
+    List.filter (fun self ->
+        let other_config = Db.SR.get_other_config ~__context ~self in
+        (Db.SR.get_is_tools_sr ~__context ~self = false) &&
+        ((List.mem_assoc Xapi_globs.tools_sr_tag other_config) ||
+         (List.mem_assoc Xapi_globs.xensource_internal other_config))
+      ) srs in
+  (* Destroy any Tools SRs *)
+  List.iter destroy (tools_srs @ old_srs)
 
 let ensure_vm_metrics_records_exist __context =
   List.iter (fun vm ->
@@ -248,6 +181,6 @@ let update_env __context =
   (* Update the SM plugin table *)
   Storage_access.on_xapi_start ~__context;
 
-  create_tools_sr_noexn __context;
+  remove_tools_sr __context;
 
   ensure_vm_metrics_records_exist_noexn __context
