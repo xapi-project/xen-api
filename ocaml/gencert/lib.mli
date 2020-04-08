750b855b0c278477489aa54c04959af7d018bafb
Pau Ruiz Safont <pau.safont@citrix.com>
2020-04-08 10:57:49 +0100

    CP-32708: Move certificate installation and validation to gencert
    Also moved the related tests to gencert
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/gencert/lib.mli
+++ ocaml/gencert/lib.mli
@@ -13,3 +13,38 @@
  *)
 
 val get_management_ip_addr : dbg:string -> string option
+val install_server_certificate :
+  ?pem_chain:string option ->
+  pem_leaf:string ->
+  pkcs8_private_key:string ->
+  server_cert_path:string ->
+  (X509.Certificate.t, [> `Msg of string * string list ]) Result.result
+(** [install_server_certificate pem_chain pem_leaf pkcs8_private_key
+     server_cert_path] writes a PKCS12 containing [pkcs8_private_key],
+     [pem_leaf] and [pem_chain] to the path [server_cert_path].
+
+     The 3 elements of the PKCS12 are validated before writing the file and
+     the file is written atomically.
+
+     In the case validation fails or the file could not be written an error is
+     returned, if the file was successfully written the leaf certificate is
+     returned for further inspection, if needed. *)
+
+(** The following functions are exposed exclusively for unit-testing, please
+    do not use them directly, they are not stable *)
+
+type t_certificate = Leaf | Chain
+
+val validate_private_key :
+  string ->
+  ([> `RSA of Mirage_crypto_pk.Rsa.priv ],
+   [> `Msg of string * string list ])
+  Result.result
+
+val validate_certificate :
+  t_certificate ->
+  string ->
+  Ptime.t ->
+  [> `RSA of Mirage_crypto_pk.Rsa.priv ] ->
+  (X509.Certificate.t, [> `Msg of string * string list ])
+  Rresult.result
--- ocaml/xapi/certificates.ml
+++ ocaml/xapi/certificates.ml
@@ -21,7 +21,6 @@ module D=Debug.Make(struct let name="certificates" end)
 open D
 
 type t_trusted = CA_Certificate | CRL
-type t_server = Leaf | Chain
 
 let c_rehash = "/usr/bin/c_rehash"
 let pem_certificate_header = "-----BEGIN CERTIFICATE-----"
@@ -324,121 +323,14 @@ let hostnames_of_pem_cert pem =
   |> X509.Certificate.decode_pem
   >>| X509.Certificate.hostnames
 
-let validate_private_key pkcs8_private_key =
-  let ensure_key_length = function
-  | `RSA priv ->
-    let length = Mirage_crypto_pk.Rsa.priv_bits priv in
-    if length < 2048 || length > 4096 then
-      Error (`Msg (server_certificate_key_rsa_length_not_supported,
-        [Int.to_string length]))
-    else
-      Ok (`RSA priv)
-  in
-
-  let raw_pem = Cstruct.of_string pkcs8_private_key in
-  X509.Private_key.decode_pem raw_pem
-  |> R.reword_error
-      (fun (`Msg err_msg) ->
-        let unknown_algorithm = "Unknown algorithm " in
-        if Astring.String.is_prefix ~affix:"multi-prime RSA" err_msg then
-          `Msg (server_certificate_key_rsa_multi_not_supported, [])
-        else if Astring.String.is_prefix ~affix:unknown_algorithm err_msg then
-          `Msg (server_certificate_key_algorithm_not_supported,
-            [Astring.String.with_range ~first:(String.length unknown_algorithm) err_msg])
-        else
-          `Msg (server_certificate_key_invalid, []))
-  >>= ensure_key_length
-
-let validate_certificate kind pem now private_key =
-  let ensure_keys_match private_key certificate =
-    let public_key = X509.Certificate.public_key certificate in
-    match public_key, private_key with
-    | `RSA pub, `RSA priv when pub = Mirage_crypto_pk.Rsa.pub_of_priv priv ->
-        Ok certificate
-    | _ ->
-        Error (`Msg (server_certificate_key_mismatch, []))
-  in
-
-  let ensure_validity ~time certificate =
-    let to_string = Ptime.to_rfc3339 ~tz_offset_s:0 in
-    let not_before, not_after = X509.Certificate.validity certificate in
-
-    if Ptime.is_earlier time not_before then
-      Error (`Msg (server_certificate_not_valid_yet,
-        [(to_string time); (to_string not_before)]))
-    else if Ptime.is_later time not_after then
-      Error (`Msg (server_certificate_expired,
-          [(to_string time); (to_string not_after)]))
-    else
-      Ok certificate
-  in
-
-  let ensure_sha256_signature_algorithm certificate =
-    match X509.Certificate.signature_algorithm certificate with
-    | Some (_, `SHA256) ->
-        Ok certificate
-    | _ ->
-        Error (`Msg (server_certificate_signature_not_supported, []))
-  in
-
-  let raw_pem = Cstruct.of_string pem in
-  (match kind with
-  | Leaf ->
-    X509.Certificate.decode_pem raw_pem
-    |> R.reword_error
-        (fun _ ->
-          `Msg (server_certificate_invalid, []))
-    >>= ensure_keys_match private_key
-    >>= ensure_validity ~time:now
-    >>= ensure_sha256_signature_algorithm
-    >>| (fun cert -> [cert])
-  | Chain ->
-    X509.Certificate.decode_pem_multiple raw_pem
-    |> R.reword_error
-        (fun _ ->
-          `Msg (server_certificate_chain_invalid, []))
-    )
-  |> function
-    | Ok (cert :: _) -> Ok cert
-    | Ok [] -> Error (`Msg (internal_error, []))
-    | Error msg -> Error msg
-
 let install_server_certificate ?(pem_chain = None) ~pem_leaf ~pkcs8_private_key =
-  let now = Ptime_clock.now () in
-
-  let result = validate_private_key pkcs8_private_key >>= fun priv ->
-  validate_certificate Leaf pem_leaf now priv >>= fun cert ->
-
-  Option.fold
-    ~none:(Ok [ pkcs8_private_key; pem_leaf ])
-    ~some:(fun pem_chain ->
-      validate_certificate Chain pem_chain now priv >>= fun _ignored ->
-      Ok [ pkcs8_private_key; pem_leaf; pem_chain ]
-    ) pem_chain >>= fun server_cert_components ->
-
-  let cert_server = server_cert_components
-    |> String.concat "\n\n"
-    |> Bytes.unsafe_of_string
+  let server_cert_path = !Xapi_globs.server_cert_path in
+  let installation =
+    Gencertlib.Lib.install_server_certificate
+      ~pem_chain ~pem_leaf ~pkcs8_private_key ~server_cert_path
   in
-  let owner_ro = 0o400 in
-  let atomic_write fd =
-    Unix.write fd cert_server 0 (Bytes.length cert_server)
-  in
-
-  try
-    let _:int =
-      Xapi_stdext_unix.Unixext.atomic_write_to_file
-        !Xapi_globs.server_cert_path owner_ro atomic_write
-    in
-    Ok cert
-  with Unix.Unix_error(err, _, _) ->
-    Error (`Msg (internal_error,
-      [Printf.sprintf "certificates: could not write server certificate to \
-       disk. Reason: %s" (Unix.error_message err)]))
-  >>= function
+  match installation with
   | Ok cert ->
       cert
   | Error (`Msg (err, msg)) ->
       raise_server_error msg err
-  in
-  R.get_ok result
