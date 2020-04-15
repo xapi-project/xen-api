lippirk/stdext

CA-333908 accept YYYY-MM-DD date format
We additionally accept iso8601 datetime strings in the form
"YYYY-MM-DDThh:mm:ssZ". This is achieved by trying to parse using
ptime first, and falling back on the old method for backwards
compatibility.

Signed-off-by: lippirk <ben.anson@citrix.com>

lib/xapi-stdext-date/date.ml
@@ -12,18 +12,8 @@
  * GNU Lesser General Public License for more details.
  *)
 
-type iso8601 = string
+(* ==== RFC822 ==== *)
 type rfc822 = string
-
-let of_float x = 
-  let time = Unix.gmtime x in
-  Printf.sprintf "%04d%02d%02dT%02d:%02d:%02dZ"
-    (time.Unix.tm_year+1900)
-    (time.Unix.tm_mon+1)
-    time.Unix.tm_mday
-    time.Unix.tm_hour
-    time.Unix.tm_min
-    time.Unix.tm_sec
 
 let months = [| "Jan"; "Feb"; "Mar"; "Apr"; "May"; "Jun"; 
                 "Jul"; "Aug"; "Sep"; "Oct"; "Nov"; "Dec" |]
@@ -38,35 +28,86 @@ let rfc822_of_float x =
 
 let rfc822_to_string x = x
 
+(* ==== ISO8601/RFC3339 ==== *)
+type old_time = string
+type error = string
+type p_time = Ptime.t * Ptime.tz_offset_s option
+
+(** the name doesn't make much sense anymore, but is kept for compatibility reasons
+  * if string is received in rfc3339 format, timezone will be preserved between to/from string
+    but not once converted to a float
+*)
+type iso8601 =
+  | P_time of p_time        (* rfc3339 - accepts only the date in YYYY-MM-DD form *)
+  | Old of old_time * error (* iso8601 - accepts both YYYYMMDD & YYYY-MM-DD *)
+
+ let of_string x =
+  let r = Ptime.of_rfc3339 x in
+  match r with
+  | Ok (t, tz, _) -> P_time (t, tz) (** prefer to use ptime
+                                      * it will only accept dashes *)
+  | Error e -> match Ptime.rfc3339_error_to_msg r with
+               | Error (`Msg err_msg) -> Old (x, err_msg)
+               | Ok x -> failwith "impossible" (* ptime providing bad api *)
+
+let to_string = function
+  | Old (x, _)     -> x
+  | P_time (t, tz) -> Ptime.to_rfc3339 ?tz_offset_s:tz t
+
+let of_float x =
+  let time = Unix.gmtime x in
+  Printf.sprintf "%04d-%02d-%02dT%02d:%02d:%02dZ"
+    (time.Unix.tm_year+1900)
+    (time.Unix.tm_mon+1)
+    time.Unix.tm_mday
+    time.Unix.tm_hour
+    time.Unix.tm_min
+    time.Unix.tm_sec |> of_string
+
 (* Convert tm in localtime to calendar time, x *)
-let to_float_localtime x = 
-  Scanf.sscanf x "%04d%02d%02dT%02d:%02d:%02d"
-    (fun y mon d h min s ->
-       fst (Unix.mktime { Unix.tm_year = y - 1900;
-                          tm_mon = mon - 1;
-                          tm_mday = d;
-                          tm_hour = h;
-                          tm_min = min;
-                          tm_sec = s;
-                          (* These are ignored: *)
-                          tm_wday = 0; tm_yday = 0; tm_isdst = true;
-                        }))
+let to_float_localtime x =
+  let to_float y mon d h min s =
+    fst Unix.(mktime { tm_year = y - 1900;
+                       tm_mon = mon - 1;
+                       tm_mday = d;
+                       tm_hour = h;
+                       tm_min = min;
+                       tm_sec = s;
+                       (* These are ignored: *)
+                       tm_wday = 0; tm_yday = 0; tm_isdst = true;
+                     })
+  in
+  match x with
+  | P_time (t, tz) ->
+    let ((y, mon, d), ((h, min, s), _)) = Ptime.to_date_time ?tz_offset_s:tz t in
+    to_float y mon d h min s
+  | Old (x, _) -> begin
+      try
+        Scanf.sscanf x "%04d%02d%02dT%02d:%02d:%02d" (fun y mon d h min s ->
+          to_float y mon d h min s
+        )
+      with e -> invalid_arg (Printf.sprintf "date.ml:to_float_localtime: %s" x)
+    end
 
 (* Convert tm in UTC back into calendar time x (using offset between above
    UTC and localtime fns to determine offset between UTC and localtime, then
    correcting for this)
 *)
 let to_float x =
-  let t = Unix.time() in
-  let offset = (to_float_localtime (of_float t)) -. t in
-  (to_float_localtime x) -. offset
+  let t = Unix.time () in
+  let offset = (t |> of_float |> to_float_localtime) -. t in
+  to_float_localtime x -. offset
 
-let to_string x = x
-let of_string x = x
-
-let assert_utc x =
-  try
-    Scanf.sscanf x "%_[0-9]T%_[0-9]:%_[0-9]:%_[0-9]Z" ()
-  with _ -> invalid_arg x
+let assert_utc = function
+  | Old (x, _)    -> begin
+                       try Scanf.sscanf x "%_[0-9]T%_[0-9]:%_[0-9]:%_[0-9]Z" ()
+                       with _ -> invalid_arg (Printf.sprintf "date.ml:assert_utc: %s" x)
+                     end
+  | P_time _ -> ()
 
 let never = of_float 0.0
+
+let eq x y = match x, y with
+  | Old _, P_time _ | P_time _, Old _-> false
+  | P_time x, P_time y               -> x = y
+  | Old (x, _), Old (y, _)           -> x = y

xapi-project/xen-api

lib/xapi-stdext-date/date.ml
@@ -12,18 +12,8 @@
  * GNU Lesser General Public License for more details.
  *)
 
-type iso8601 = string
+(* ==== RFC822 ==== *)
 type rfc822 = string
-
-let of_float x = 
-  let time = Unix.gmtime x in
-  Printf.sprintf "%04d%02d%02dT%02d:%02d:%02dZ"
-    (time.Unix.tm_year+1900)
-    (time.Unix.tm_mon+1)
-    time.Unix.tm_mday
-    time.Unix.tm_hour
-    time.Unix.tm_min
-    time.Unix.tm_sec
 
 let months = [| "Jan"; "Feb"; "Mar"; "Apr"; "May"; "Jun"; 
                 "Jul"; "Aug"; "Sep"; "Oct"; "Nov"; "Dec" |]
@@ -38,35 +28,79 @@ let rfc822_of_float x =
 
 let rfc822_to_string x = x
 
+(* ==== ISO8601/RFC3339 ==== *)
+
+(** the name doesn't make much sense anymore, but is kept for compatibility reasons *)
+type iso8601 =
+  | UTC of Ptime.t   (* rfc3339 - accepts only the date in YYYY-MM-DD format *)
+  | Legacy of string (* iso8601 - accepts both YYYYMMDD & YYYY-MM-DD *)
+
+ let of_string x =
+  (** prefer to parse with ptime, but rfc3339 does not accept YYYYMMDD)
+    * we fallback on legacy parsing to accept iso8601 datetimes *)
+  match x |> Ptime.of_rfc3339 |> Ptime.rfc3339_error_to_msg with
+  | Error _       -> let assert_utc x =
+                       try Scanf.sscanf x "%_[0-9]T%_[0-9]:%_[0-9]:%_[0-9]Z" ()
+                       with _ -> invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
+                     in
+                     assert_utc x; Legacy x
+  | Ok (t, tz, _) -> match tz with
+                     | None | Some 0 -> UTC t
+                     | Some _        -> invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
+
+let to_string = function
+  | Legacy x -> x
+  | UTC t    -> Ptime.to_rfc3339 ~tz_offset_s:0 (* to ensure Z printed, rather than +00:00 *)
+                                 t
+
+let of_float x =
+  let time = Unix.gmtime x in
+  Printf.sprintf "%04d-%02d-%02dT%02d:%02d:%02dZ"
+    (time.Unix.tm_year+1900)
+    (time.Unix.tm_mon+1)
+    time.Unix.tm_mday
+    time.Unix.tm_hour
+    time.Unix.tm_min
+    time.Unix.tm_sec |> of_string
+
 (* Convert tm in localtime to calendar time, x *)
-let to_float_localtime x = 
-  Scanf.sscanf x "%04d%02d%02dT%02d:%02d:%02d"
-    (fun y mon d h min s ->
-       fst (Unix.mktime { Unix.tm_year = y - 1900;
-                          tm_mon = mon - 1;
-                          tm_mday = d;
-                          tm_hour = h;
-                          tm_min = min;
-                          tm_sec = s;
-                          (* These are ignored: *)
-                          tm_wday = 0; tm_yday = 0; tm_isdst = true;
-                        }))
+let to_float_localtime x =
+  let datetime_to_float y mon d h min s =
+    fst Unix.(mktime { tm_year = y - 1900;
+                       tm_mon = mon - 1;
+                       tm_mday = d;
+                       tm_hour = h;
+                       tm_min = min;
+                       tm_sec = s;
+                       (* These are ignored: *)
+                       tm_wday = 0; tm_yday = 0; tm_isdst = true;
+                     })
+  in
+  match x with
+  | UTC t    ->
+    let ((y, mon, d), ((h, min, s), _)) = Ptime.to_date_time t in
+    datetime_to_float y mon d h min s
+  | Legacy x ->
+      try
+        Scanf.sscanf x "%04d%02d%02dT%02d:%02d:%02d" (fun y mon d h min s ->
+          datetime_to_float y mon d h min s
+        )
+      with e -> invalid_arg (Printf.sprintf "date.ml:to_float_localtime: %s" x)
 
 (* Convert tm in UTC back into calendar time x (using offset between above
    UTC and localtime fns to determine offset between UTC and localtime, then
    correcting for this)
 *)
 let to_float x =
-  let t = Unix.time() in
-  let offset = (to_float_localtime (of_float t)) -. t in
-  (to_float_localtime x) -. offset
+  let t = Unix.time () in
+  let offset = (t |> of_float |> to_float_localtime) -. t in
+  to_float_localtime x -. offset
 
-let to_string x = x
-let of_string x = x
-
-let assert_utc x =
-  try
-    Scanf.sscanf x "%_[0-9]T%_[0-9]:%_[0-9]:%_[0-9]Z" ()
-  with _ -> invalid_arg x
+let assert_utc _ = ()
 
 let never = of_float 0.0
+
+let eq x y = match x, y with
+  | Legacy _, UTC _ | UTC _, Legacy _ -> false
+  | UTC x, UTC y                      -> x = y
+  | Legacy x, Legacy y                -> x = y