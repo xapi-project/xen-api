9f599db85e14ea9f97df5b6df94087ec7cc4b618
Pau Ruiz Safont <pau.safont@citrix.com>
2020-07-17 11:07:07 +0100

    tests: drop http server startup
    No tests use it and it introduces racing problems with network sockets.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/tests/common/suite_init.ml
+++ ocaml/tests/common/suite_init.ml
@@ -1,21 +1,5 @@
-let handlers =
-  [
-    ("get_services", Http_svr.FdIO Xapi_services.get_handler)
-  ; ("post_services", Http_svr.FdIO Xapi_services.post_handler)
-  ; ("put_services", Http_svr.FdIO Xapi_services.put_handler)
-  ; ("post_root", Http_svr.BufIO (Api_server.callback false))
-  ; ("post_json", Http_svr.BufIO (Api_server.callback true))
-  ; ("post_jsonrpc", Http_svr.BufIO Api_server.jsoncallback)
-  ; ("post_remote_db_access", Http_svr.BufIO Db_remote_cache_access_v1.handler)
-  ; ( "post_remote_db_access_v2"
-    , Http_svr.BufIO Db_remote_cache_access_v2.handler )
-  ]
-
-let start_server handlers =
-  List.iter Xapi_http.add_handler handlers ;
-  Xapi.listen_unix_socket "/tmp/xapi-test/xapi-unit-test-socket"
-
 let harness_init () =
+  Xapi_stdext_unix.Unixext.mkdir_safe (Test_common.working_area) 0o755 ;
   (* Alcotest hides the standard output of successful tests,
      so we will probably not exceed the 4MB limit in Travis *)
   Debug.log_to_stdout () ;
@@ -24,5 +8,4 @@ let harness_init () =
     Filename.concat Test_common.working_area "xapi-inventory" ;
   Xcp_client.use_switch := false ;
   Pool_role.set_pool_role_for_test () ;
-  Xapi.register_callback_fns () ;
-  start_server handlers
+  Xapi.register_callback_fns ()
--- ocaml/tests/test_daemon_manager.ml
+++ ocaml/tests/test_daemon_manager.ml
@@ -146,4 +146,6 @@ let test =
   ; ("test_timeout_fail", `Slow, test_timeout_fail)
   ]
 
-let () = Alcotest.run "Daemon Manager suite" [("Test_daemon_manager", test)]
+let () = 
+  Suite_init.harness_init () ;
+  Alcotest.run "Daemon Manager suite" [("Test_daemon_manager", test)]
