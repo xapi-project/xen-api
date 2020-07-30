f275e84604fa841387250a837ccdac26965bc8a6
lippirk <ben.anson@citrix.com>
2020-07-30 09:41:25 +0100

    maintenance: get rrdd version from dune
    To avoid having to manually set the version, just
    get it using the dune-build-info library.
    
    Signed-off-by: lippirk <ben.anson@citrix.com>

--- bin/rrdd/version.ml
+++ bin/rrdd/version.ml
@@ -1,1 +1,5 @@
-let version = "0.12.0"
+let version =
+  (* returns n/a unless you add a version to dune-project *)
+  match Build_info.V1.version () with
+  | None -> "n/a"
+  | Some v -> Build_info.V1.Version.to_string v
