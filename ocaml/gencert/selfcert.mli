d5f91f247d96f8dc79c8da4aabff2d563459ddb4
Christian Lindig <christian.lindig@citrix.com>
2020-08-27 14:24:23 +0100

    CP-33823 wire up Selfcert.host() when creating SSL certs
    Replace calls to a script which calls openssl with calls to use new
    library function Selfsign.host.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/gencert/selfcert.mli
+++ ocaml/gencert/selfcert.mli
@@ -1,9 +1,9 @@
+val host : string -> string list -> string -> unit
+(** [certify hostname alt_names path] creates (atomically) a PEM file at
+[path] for hostname with alternative names [alt_names]. *)
+
 module CLI : sig
   val main : unit -> unit
   (** [main] implements a command line interface which can be used to 
     build a stand-alone binary *)
-
-  val host : string -> string list -> string -> unit
-  (** [certify hostname alt_names path] creates (atomically) a PEM file at
-  [path] for hostname with alternative names [alt_names]. *)
 end
