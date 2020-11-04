f1a98f0055fab3e11660ae4afb98c9aef940e99f
Christian Lindig <christian.lindig@citrix.com>
2020-11-04 16:41:55 +0000

    CP-35021 introduce new API error for vGPU suspend
    When vGPU suspension is unsupported, the generic error message is
    confusion for the user. Introduce a new error
    VGPU_SUSPENSION_NOT_SUPPORTED, catch the generic error and raise the
    specific error instead.
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/xapi/xapi_vm.ml
+++ ocaml/xapi/xapi_vm.ml
@@ -570,8 +570,15 @@ let suspend ~__context ~vm =
   |> List.iter (fun (vgpu, pgpu) ->
          debug "VM.suspend checking vgpu/pgpu support for suspend: %s/%s"
            (Ref.string_of vgpu) (Ref.string_of pgpu) ;
-         Xapi_pgpu_helpers.assert_destination_pgpu_is_compatible_with_vm
-           ~__context ~vm ~host ~vgpu ~pgpu ()) ;
+         try
+           Xapi_pgpu_helpers.assert_destination_pgpu_is_compatible_with_vm
+             ~__context ~vm ~host ~vgpu ~pgpu ()
+         with
+         | Api_errors.Server_error (e, params)
+         when e = Api_errors.vgpu_destination_incompatible
+         ->
+           raise
+             Api_errors.(Server_error (vgpu_suspension_not_supported, params))) ;
 
   Xapi_gpumon.update_vgpu_metadata ~__context ~vm ;
   Xapi_xenops.suspend ~__context ~self:vm ;
