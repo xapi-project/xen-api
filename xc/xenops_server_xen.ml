80d6513e00e0051a0183800ca2c9dd9b20108a49
Edwin Török <edvin.torok@citrix.com>
2020-11-04 20:04:39 +0000

    XSA-354: ls_lR: limit depth
    We only want to read a few levels deep into the xenstore tree of the
    guest.  Limit the depth at which we read keys to further reduce DoS
    potential.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>
    Acked-by: Christian Lindig <christian.lindig@citrix.com>

--- xc/xenops_server_xen.ml
+++ xc/xenops_server_xen.ml
@@ -2628,10 +2628,19 @@ module VM = struct
             let quota = !Xenopsd.vm_xenstore_ls_lR_quota in
             let quota, guest_agent =
               [
-                "drivers"; "attr"; "data"; "control"; "feature"; "xenserver/attr"
+                ("drivers", 0)
+              ; ("attr", 3) (* attr/vif/0/ipv4/0, attr/eth0/ipv6/0/addr *)
+              ; ("data", 0)
+                (* in particular avoid data/volumes which contains many entries for each disk *)
+              ; ("control", 0)
+              ; ("feature/hotplug", 0)
+              ; ("xenserver/attr", 3) (* xenserver/attr/net-sriov-vf/0/ipv4/1 *)
               ]
               |> List.fold_left
-                   (ls_lR (Printf.sprintf "/local/domain/%d" di.Xenctrl.domid))
+                   (fun acc (dir, depth) ->
+                     ls_lR ~depth
+                       (Printf.sprintf "/local/domain/%d" di.Xenctrl.domid)
+                       acc dir)
                    (quota, [])
               |> fun (quota, acc) ->
               (quota, map_tr (fun (k, v) -> (k, Xenops_utils.utf8_recode v)) acc)
