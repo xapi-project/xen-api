6d5ad384091650489d543ee384301e7be6c0b370
Ben Anson <ben.anson@citrix.com>
2020-12-03 09:58:33 +0000

    maintenance: detect schema updates which are missing version bumps
    We already bumped the schema version in
    54d6e8a9c97760d910675bee809d11f7ae9bffe3,
    so we don't need to do it again
    
    Signed-off-by: Ben Anson <ben.anson@citrix.com>
    (cherry picked from commit d63efa50f1d6e1ee47bf9752c892c10b09722b44)

---
+++ ocaml/idl/schematest.ml
@@ -0,0 +1,27 @@
+let hash x = Digest.string x |> Digest.to_hex
+
+(* BEWARE: if this changes, check that schema has been bumped accordingly *)
+let last_known_schema_hash = "0aa4a3589b8f91858b268617871d9dc9"
+
+let current_schema_hash : string =
+  let open Datamodel_types in
+  let hash_of_obj x =
+    List.map rpc_of_content x.contents
+    |> List.map Jsonrpc.to_string
+    |> String.concat ""
+    |> hash
+  in
+  Datamodel.all_system |> List.map hash_of_obj |> String.concat ":" |> hash
+
+let () =
+  if last_known_schema_hash <> current_schema_hash then (
+    Printf.eprintf
+      {|
+
+New schema hash ('%s') doesn't match the last known one. Please bump the
+datamodel schema versions if necessary, and update 'last_known_schema_hash'.
+
+|}
+      current_schema_hash ;
+    exit 1
+  )
