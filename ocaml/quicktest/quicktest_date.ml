0c50cba8664541b0b79b6a660c36c9974f316ac8
Ben Anson <ben.anson@citrix.com>
2020-12-16 17:05:23 +0000

    CP-34602: test get_server_localtime and message.get_since
    Signed-off-by: Ben Anson <ben.anson@citrix.com>
    (cherry picked from commit 0088c6aa988c946b217376b53526be6eca2c2d0e)

---
+++ ocaml/quicktest/quicktest_date.ml
@@ -0,0 +1,38 @@
+module Client = Client.Client
+module Date = Xapi_stdext_date.Date
+
+let test_host_get_server_localtime rpc session_id () =
+  let host = Client.Host.get_by_uuid ~rpc ~session_id ~uuid:Qt.localhost_uuid in
+  let (_ : Date.iso8601) =
+    Client.Host.get_server_localtime ~rpc ~session_id ~host
+  in
+  ()
+
+let test_message_get_since rpc session_id () =
+  let test_with_format format' =
+    let stdout, _ =
+      Forkhelpers.execute_command_get_output "/bin/date"
+        [Printf.sprintf "+%s" format'; "-d"; "yesterday"]
+    in
+    let yesterday = String.trim stdout |> Date.of_string in
+    let (_ : ('a API.Ref.t * API.message_t) list) =
+      Client.Message.get_since ~rpc ~session_id ~since:yesterday
+    in
+    ()
+  in
+  [
+    "%Y-%m-%dT%H:%M:%SZ"
+  ; "%Y-%m-%dT%H:%M:%S"
+  ; "%Y%m%dT%H:%M:%SZ"
+  ; "%Y%m%dT%H:%M:%S"
+  ]
+  |> List.iter test_with_format
+
+let tests () =
+  let open Qt_filter in
+  [
+    [("host.get_server_localtime", `Quick, test_host_get_server_localtime)]
+    |> conn
+  ; [("message.get_since", `Quick, test_message_get_since)] |> conn
+  ]
+  |> List.concat
