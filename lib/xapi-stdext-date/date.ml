9d7c9291eb63991769052474f78cda8d76e282ce
Pau Ruiz Safont <pau.safont@citrix.com>
2020-12-17 15:32:19 +0000

    date: allow timezones other than UTC for printing
    The timezone print also does not try to dictate the formats that are
    allowed as ptime already does this.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- lib/xapi-stdext-date/date.ml
+++ lib/xapi-stdext-date/date.ml
@@ -15,7 +15,7 @@
 (* ==== RFC822 ==== *)
 type rfc822 = string
 
-let months = [| "Jan"; "Feb"; "Mar"; "Apr"; "May"; "Jun"; 
+let months = [| "Jan"; "Feb"; "Mar"; "Apr"; "May"; "Jun";
                 "Jul"; "Aug"; "Sep"; "Oct"; "Nov"; "Dec" |]
 let days = [| "Sun"; "Mon"; "Tue"; "Wed"; "Thu"; "Fri"; "Sat" |]
 
@@ -30,9 +30,11 @@ let rfc822_to_string x = x
 
 (* ==== ISO8601/RFC3339 ==== *)
 
-type print_type = PrintLocal | PrintUTC
+type print_timezone = Empty | TZ of string
 (* we must store the print_type with iso8601 to handle the case where the local time zone is UTC *)
-type iso8601 = Ptime.date * Ptime.time * print_type
+type iso8601 = Ptime.date * Ptime.time * print_timezone
+
+let utc = TZ "Z"
 
 let of_dt print_type dt = let (date, time) = dt in (date, time, print_type)
 let to_dt (date, time, _) = (date, time)
@@ -57,23 +59,22 @@ let best_effort_iso8601_to_rfc3339 x =
   match tz with
   | None | Some "" ->
     (* the caller didn't specify a tz. we must try to add one so that ptime can at least attempt to parse *)
-    (Printf.sprintf "%sZ" x, PrintLocal)
-  | Some _ ->
-    (* the caller specified a tz. we assume it's UTC because we don't accept anything else *)
-    (x, PrintUTC)
+    (Printf.sprintf "%sZ" x, Empty)
+  | Some tz  ->
+    (x, TZ tz)
 
 let of_string x =
-  let (rfc3339, print_type) = best_effort_iso8601_to_rfc3339 x in
+  let (rfc3339, print_timezone) = best_effort_iso8601_to_rfc3339 x in
   match Ptime.of_rfc3339 rfc3339 |> Ptime.rfc3339_error_to_msg with
   | Error (`Msg e) -> invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
   | Ok (t, tz, _)  -> match tz with
-                      | None | Some 0 -> Ptime.to_date_time t |> of_dt print_type
+                      | None | Some 0 -> Ptime.to_date_time t |> of_dt print_timezone
                       | Some _        -> invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
 
 let to_string ((y,mon,d), ((h,min,s), _), print_type) =
   match print_type with
-  | PrintUTC   -> Printf.sprintf "%04i%02i%02iT%02i:%02i:%02iZ" y mon d h min s
-  | PrintLocal -> Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i" y mon d h min s
+  | TZ tz -> Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i%s" y mon d h min s tz
+  | Empty -> Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i" y mon d h min s
 
 let to_ptime_t t =
   match to_dt t |> Ptime.of_date_time with
@@ -85,13 +86,13 @@ let to_ptime_t t =
 let of_float s =
   match Ptime.of_float_s s with
   | None -> invalid_arg (Printf.sprintf "date.ml:of_float: %f" s)
-  | Some t -> Ptime.to_date_time t |> of_dt PrintUTC
+  | Some t -> Ptime.to_date_time t |> of_dt utc
 
 let to_float t = to_ptime_t t |> Ptime.to_float_s
 
 let _localtime current_tz_offset t =
   let tz_offset_s = current_tz_offset |> Option.value ~default:0 in
-  let localtime = t |> Ptime.to_date_time ~tz_offset_s |> of_dt PrintLocal in
+  let localtime = t |> Ptime.to_date_time ~tz_offset_s |> of_dt Empty in
   let (_, (_, localtime_offset), _) = localtime in
   if localtime_offset <> tz_offset_s then
     invalid_arg (
