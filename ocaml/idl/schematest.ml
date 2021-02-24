6c21163963b4c5564155b17cedf154e108c28c00
Ben Anson <ben.anson@citrix.com>
2021-02-24 17:15:36 +0000

    REQ-403 bump schema version
    Signed-off-by: Ben Anson <ben.anson@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -1,7 +1,7 @@
 let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly *)
-let last_known_schema_hash = "5a6605a08c21f658bebcbfee9e93dc62"
+let last_known_schema_hash = "872773fe55299078f775870ca929b613"
 
 let current_schema_hash : string =
   let open Datamodel_types in
