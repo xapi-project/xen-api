28499855bc8c6e585a28737c6494cb9ae370180d
Pau Ruiz Safont <pau.safont@citrix.com>
2021-03-01 17:11:36 +0000

    CP-32669: Remove vendored PCI library
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/pci/stubgen/ffi_types_stubgen.ml
+++
@@ -1,3 +0,0 @@
-let () =
-  print_endline "#include <pci/pci.h>" ;
-  Cstubs.Types.write_c Format.std_formatter (module Ffi_bindings.Types)
--- ocaml/pci/stubgen/libpci_check.c
+++
@@ -1,6 +0,0 @@
-#include <pci/pci.h>
-
-int main() {
-  /* Check we have >= 3.2.0 */
-  return !(PCI_LIB_VERSION >= 0x030200);
-}
