f915bf96b6f64ab62a5801b2b1286494c76f8311
Ben Anson <ben.anson@citrix.com>
2021-03-15 15:19:28 +0000

    CP-36099 REQ-403 comment about cert locations
    Signed-off-by: Ben Anson <ben.anson@citrix.com>

--- ocaml/xapi/certificates.ml
+++ ocaml/xapi/certificates.ml
@@ -19,6 +19,16 @@ module D = Debug.Make (struct let name = "certificates" end)
 
 open D
 
+(* Certificate locations:
+  * a) stunnel external             = /etc/xensource/xapi-ssl.pem
+  * b) stunnel SNI (internal)       = /etc/xensource/xapi-pool-tls.pem
+  * c) user trusted cert folder     = /etc/stunnel/certs/
+  * d) internal trusted cert folder = /etc/stunnel/certs-pool/
+  * e) appliance trusted bundle     = /etc/stunnel/xapi-stunnel-ca-bundle.pem
+  * f) host-in-pool trusted bundle  = /etc/stunnel/xapi-pool-ca-bundle.pem
+  *
+  * Note that the bundles (e) and (f) are generated automatically using the contents of (c) and (d) respectively *)
+
 type t_trusted = CA_Certificate | CRL
 
 let c_rehash = "/usr/bin/c_rehash"
