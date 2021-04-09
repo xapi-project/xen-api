223764261f3f5075787a1aa1acfe368ae3ba68de
Pau Ruiz Safont <pau.safont@citrix.com>
2021-04-09 16:04:20 +0100

    CP-34942: Adapt tests to work with alcotest 1.3.0
    For some reason running the test functions for the first time free up
    exactly 6 words on alcotest 1.3.0. Run them on compile-time so they are
    not affected by changed in alcotest.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- resources/test/unixfd_test.ml
+++ resources/test/unixfd_test.ml
@@ -17,13 +17,7 @@ let memory_usage_growth f =
   gc_live_words s0 ;
   f () ;
   gc_live_words s1 ;
-  Logs.debug (fun m -> m "Memory usage (words): %d -> %d" !s0 !s1) ;
-  !s1 - !s0
-
-let test_unixfd_noleak () =
-  (* this just ensures we didn't stash something into a global *)
-  let delta = memory_usage_growth noleak in
-  Alcotest.(check int "memory leak in words" 0 delta)
+  (!s0, !s1 - !s0)
 
 let count_fds () =
   let d = Unix.opendir "/proc/self/fd" in
@@ -55,15 +49,6 @@ let leak_detected (count0, count1, count2) =
 
 let count0, count1, count2 = (ref 0, ref 0, ref 0)
 
-let test_unixfd_leak_detected () =
-  let delta =
-    memory_usage_growth (fun () -> leak_detected (count0, count1, count2))
-  in
-  Alcotest.(check int "memory leak in words" 0 delta) ;
-  Alcotest.(
-    check int "count of open FDs after pipe 2 higher" (!count0 + 2) !count1) ;
-  Alcotest.(check int "count of open FDs same as original" !count0 !count2)
-
 let noleak2 () =
   let moved1, moved2 =
     count0 := count_fds () ;
@@ -75,17 +60,67 @@ let noleak2 () =
   Safe.within (Safe.move_exn moved2) @@ ignore ;
   count2 := count_fds ()
 
-let test_unixfd_noleak2 () =
+type test_result = {
+    memory: int
+  ; memory_growth: int
+  ; fds_before: int option
+  ; fds_after: int option
+  ; fds_after_cleanup: int option
+}
+
+let test_unixfd_noleak =
   (* this just ensures we didn't stash something into a global *)
-  let delta = memory_usage_growth noleak2 in
-  Alcotest.(
-    check int "count of open FDs after pipe 2 higher" (!count0 + 2) !count1) ;
-  Alcotest.(check int "count of open FDs same as original" !count0 !count2) ;
-  Alcotest.(check int "memory leak in words" 0 delta)
+  let memory, memory_growth = memory_usage_growth noleak in
+  {
+    memory
+  ; memory_growth
+  ; fds_before= None
+  ; fds_after= None
+  ; fds_after_cleanup= None
+  }
+
+let test_unixfd_noleak2 =
+  (* this just ensures we didn't stash something into a global *)
+  let memory, memory_growth = memory_usage_growth noleak2 in
+  {
+    memory
+  ; memory_growth
+  ; fds_before= Some !count0
+  ; fds_after= Some !count1
+  ; fds_after_cleanup= Some !count2
+  }
+
+let test_unixfd_leak_detected =
+  let memory, memory_growth =
+    memory_usage_growth (fun () -> leak_detected (count0, count1, count2))
+  in
+  {
+    memory
+  ; memory_growth
+  ; fds_before= Some !count0
+  ; fds_after= Some !count1
+  ; fds_after_cleanup= Some !count2
+  }
+
+let validate_result
+    {memory; memory_growth; fds_before; fds_after; fds_after_cleanup} () =
+  Logs.debug (fun m ->
+      m "Memory usage (words): %d -> %d" memory (memory + memory_growth)) ;
+  Alcotest.(check int "memory leak in words" 0 memory_growth) ;
+  match (fds_before, fds_after, fds_after_cleanup) with
+  | Some before, Some after, Some after_cleanup ->
+      Alcotest.(
+        check int "count of open FDs after pipe 2 higher" (before + 2) after) ;
+      Alcotest.(
+        check int "count of open FDs same as original" before after_cleanup)
+  | None, None, None ->
+      ()
+  | _ ->
+      Alcotest.fail "Error in test setup"
 
 let tests =
   [
-    (__LOC__, `Quick, test_unixfd_noleak)
-  ; (__LOC__, `Quick, test_unixfd_noleak2)
-  ; (__LOC__, `Quick, test_unixfd_leak_detected)
+    (__LOC__, `Quick, validate_result test_unixfd_noleak)
+  ; (__LOC__, `Quick, validate_result test_unixfd_noleak2)
+  ; (__LOC__, `Quick, validate_result test_unixfd_leak_detected)
   ]
