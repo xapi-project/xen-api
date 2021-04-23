b87e3f022d967e699133bd7c18304e67478ec48d
Lin Liu <lin.liu@citrix.com>
2021-04-23 06:26:07 +0000

    CP-36085: Merge start AD backend daemon to on_enable
    PBIS needs to start backend daemon before on_enable
    winbind needs to start backend daemon after on_enable
    Merge start backend daemon to on_enable from higher level
    
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- ocaml/xapi/xapi_systemctl.ml
+++ ocaml/xapi/xapi_systemctl.ml
@@ -21,6 +21,8 @@ open D
 
 type t = Start | Stop
 
+exception Systemctl_fail of string
+
 let to_string = function Start -> "start" | Stop -> "stop"
 
 let perform ~wait_until_success ~service ~timeout op =
@@ -30,7 +32,7 @@ let perform ~wait_until_success ~service ~timeout op =
     ignore
       (Forkhelpers.execute_command_get_output !Xapi_globs.systemctl
          [op_str; service]) ;
-    if wait_until_success then
+    if wait_until_success then (
       let is_active = Fe_systemctl.is_active service in
       let success_cond () =
         match op with Start -> is_active | Stop -> is_active |> not
@@ -39,10 +41,14 @@ let perform ~wait_until_success ~service ~timeout op =
         Helpers.retry_until_timeout ~timeout
           (Printf.sprintf "trying to %s %s" op_str service)
           success_cond
-      with _ -> debug "Fail to %s %s timeout %f" op_str service timeout
+      with e ->
+        debug "Fail to %s %s timeout %f" op_str service timeout ;
+        raise e
+    )
   with e ->
-    error "Fail to %s %s with error %s" op_str service
-      (ExnHelper.string_of_exn e)
+    let err_str = ExnHelper.string_of_exn e in
+    error "Fail to %s %s with error %s" op_str service err_str ;
+    raise (Systemctl_fail err_str)
 
 let stop ?(timeout = 5.) ~wait_until_success service =
   perform ~wait_until_success ~service ~timeout Stop
