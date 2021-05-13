6bdc235252d3c0d87dfeacddcfc8d66f37d82b84
Ming Lu <ming.lu@citrix.com>
2021-05-13 01:51:05 +0000

    CP-35955: Clean up pending guidances
    Signed-off-by: Ming Lu <ming.lu@citrix.com>

--- ocaml/xapi/xapi_xenops.ml
+++ ocaml/xapi/xapi_xenops.ml
@@ -3413,7 +3413,8 @@ let set_resident_on ~__context ~self =
   refresh_vm ~__context ~self ;
   !trigger_xenapi_reregister () ;
   (* Any future XenAPI updates will trigger events, but we might have missed one so: *)
-  Xenopsd_metadata.update ~__context ~self
+  Xenopsd_metadata.update ~__context ~self ;
+  Db.VM.remove_pending_guidances ~__context ~self ~value:`restart_device_model
 
 let update_debug_info __context t =
   let task = Context.get_task_id __context in
