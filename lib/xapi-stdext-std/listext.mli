a769bc19f447b4c2bd1eb85c40ea1deec80e835c
Pau Ruiz Safont <pau.safont@citrix.com>
2021-05-21 17:08:24 +0100

    CP-34643: Reorder functions in listext interface
    This allows to direct users away from certain functions which should be
    avoided and offer alternatives in the documentation.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- lib/xapi-stdext-std/listext.mli
+++ lib/xapi-stdext-std/listext.mli
@@ -12,17 +12,33 @@
  * GNU Lesser General Public License for more details.
  *)
 module List : sig
-  val setify : 'a list -> 'a list
+  (** {1 Comparison} *)
 
-  val subset : 'a list -> 'a list -> bool
+  val is_sorted : ('a -> 'a -> int) -> 'a list -> bool
+  (** [is_sorted cmp l] returns whether [l] is sorted according to [cmp]. *)
 
-  val set_equiv : 'a list -> 'a list -> bool
+  (** {1 Iterators} *)
 
-  val inv_assoc : 'a -> ('b * 'a) list -> 'b
-  (** Perform a lookup on an association list of (value, key) pairs. *)
+  val take : int -> 'a list -> 'a list
+  (** [take n list] returns the first [n] elements of [list] (or less if list
+      is shorter).*)
+
+  val drop : int -> 'a list -> 'a list
+  (** [drop n list] returns the list without the first [n] elements of [list]
+      (or [] if list is shorter). *)
+
+  val rev_mapi : (int -> 'a -> 'b) -> 'a list -> 'b list
+  (** [rev_map f l] gives the same result as {!Stdlib.List.rev}[ (]
+      {!Stdlib.List.mapi}[ f l)], but is tail-recursive and more efficient. *)
 
   val map_tr : ('a -> 'b) -> 'a list -> 'b list
-  (** A tail-recursive map. *)
+  (** [map_tr f l] is {!Stdlib.List.rev}[ (]{!Stdlib.List.rev_map}[ f l)]. *)
+
+  val mapi_tr : (int -> 'a -> 'b) -> 'a list -> 'b list
+  (** [mapi_tr f l] is {!Stdlib.List.rev}[ (]{!rev_mapi}[ f l)]. *)
+
+  val unbox_list : 'a option list -> 'a list
+  (** Unbox all values from the option list. *)
 
   val count : ('a -> bool) -> 'a list -> int
   (** Count the number of list elements matching the given predicate. *)
@@ -31,12 +47,9 @@ module List : sig
   (** Find the indices of all elements matching the given predicate. *)
 
   val iteri_right : (int -> 'a -> unit) -> 'a list -> unit
+  (** [iteri_right f l] is {!Stdlib.List.iteri}[ f (]{!Stdlib.List.rev}[ l)] *)
 
-  val rev_mapi : (int -> 'a -> 'b) -> 'a list -> 'b list
-  (** Map the given function over a list in reverse order. *)
-
-  val mapi_tr : (int -> 'a -> 'b) -> 'a list -> 'b list
-  (** Tail-recursive [mapi]. *)
+  (** {1 Using indices to manipulate lists} *)
 
   val chop : int -> 'a list -> 'a list * 'a list
   (** [chop k l] splits [l] at index [k] to return a pair of lists. Raises
@@ -48,11 +61,11 @@ module List : sig
       greater than the length of [l]. *)
 
   val chop_tr : int -> 'a list -> 'a list * 'a list
-  (** Tail-recursive [chop]. *)
+  (** Tail-recursive {!chop}. *)
 
   val dice : int -> 'a list -> 'a list list
   (** [dice k l] splits [l] into lists with [k] elements each. Raises
-      invalid_arg if [List.length l] is not divisible by [k]. *)
+      {!Invalid_arg} if [List.length l] is not divisible by [k]. *)
 
   val sub : int -> int -> 'a list -> 'a list
   (** [sub from to l] returns the sub-list of [l] that starts at index [from]
@@ -72,70 +85,13 @@ module List : sig
   val morph : int -> ('a -> 'a) -> 'a list -> 'a list
   (** Apply the given function to the element at the given index. *)
 
-  val between : 'a -> 'a list -> 'a list
-  (** Insert the element [e] between every pair of adjacent elements in the
-      given list. *)
+  (** {1 Association Lists} *)
 
-  val between_tr : 'a -> 'a list -> 'a list
-  (** Tail-recursive [between]. *)
-
-  val randomize : 'a list -> 'a list
-  (** Generate a random permutation of the given list. *)
-
-  val distribute : 'a -> 'a list -> 'a list list
-  (** Distribute the given element over the given list, returning a list of
-      lists with the new element in each position. *)
-
-  val permute : 'a list -> 'a list list
-  (** Generate all permutations of the given list. *)
-
-  val rle_eq : ('a -> 'a -> bool) -> 'a list -> ('a * int) list
-  (** Run-length encode the given list using the given equality function. *)
-
-  val rle : 'a list -> ('a * int) list
-  (** Run-length encode the given list using built-in equality. *)
-
-  val unrle : (int * 'a) list -> 'a list
-  (** Decode a run-length encoded list. *)
-
-  val inner :
-       (('a -> 'b -> 'c -> 'd) -> 'e -> 'f -> 'g -> 'h)
-    -> 'e
-    -> ('b -> 'c -> 'i)
-    -> 'f
-    -> 'g
-    -> ('a -> 'i -> 'd)
-    -> 'h
-  (** Compute the inner product of two lists. *)
-
-  val is_sorted : ('a -> 'a -> int) -> 'a list -> bool
-  (** Returns true if and only if the given list is in sorted order
-      according to the given comparison function.  *)
-
-  val intersect : 'a list -> 'a list -> 'a list
-  (** Returns the intersection of two lists. *)
-
-  val set_difference : 'a list -> 'a list -> 'a list
-  (** Returns the set difference of two lists *)
+  val make_assoc : ('a -> 'b) -> 'a list -> ('a * 'b) list
 
   val assoc_default : 'a -> ('a * 'b) list -> 'b -> 'b
   (** Act as List.assoc, but return the given default value if the
       key is not in the list. *)
-
-  val map_assoc_with_key :
-    ('k -> 'v1 -> 'v2) -> ('k * 'v1) list -> ('k * 'v2) list
-  (** [map_assoc_with_key op al] transforms every value in [al] based on the
-      key and the value using [op]. *)
-
-  val take : int -> 'a list -> 'a list
-  (** [take n list] returns the first [n] elements of [list] (or less if list
-      is shorter).*)
-
-  val drop : int -> 'a list -> 'a list
-  (** [drop n list] returns the list without the first [n] elements of [list]
-      (or [] if list is shorter). *)
-
-  val tails : 'a list -> 'a list list
 
   val replace_assoc : 'a -> 'b -> ('a * 'b) list -> ('a * 'b) list
   (** Replace the value belonging to a key in an association list. Adds the key/value pair
@@ -148,17 +104,88 @@ module List : sig
       which the key does not exist in [update]. In other words, it is like [replace_assoc]
       but then given a whole assoc list of updates rather than a single key/value pair. *)
 
-  val make_assoc : ('a -> 'b) -> 'a list -> ('a * 'b) list
+  val map_assoc_with_key :
+    ('k -> 'v1 -> 'v2) -> ('k * 'v1) list -> ('k * 'v2) list
+  (** [map_assoc_with_key op al] transforms every value in [al] based on the
+      key and the value using [op]. *)
 
-  val unbox_list : 'a option list -> 'a list
-  (** Unbox all values from the option list. *)
+  val inv_assoc : 'a -> ('b * 'a) list -> 'b
+  (** Perform a lookup on an association list of (value, key) pairs. *)
 
   val restrict_with_default : 'v -> 'k list -> ('k * 'v) list -> ('k * 'v) list
   (** [restrict_with_default default keys al] makes a new association map
       from [keys] to previous values for [keys] in [al]. If a key is not found
       in [al], the [default] is used. *)
 
+  (** {1 Run-length encoded lists}
+      There are no known users of these functions. *)
+
+  val rle : 'a list -> ('a * int) list
+    [@@deprecated
+      "No known users, consider creating a proper datatype, this kind of list \
+       might be confused with association lists"]
+  (** Run-length encodes the given list using polimorphic equality *)
+
+  val unrle : (int * 'a) list -> 'a list
+    [@@deprecated "No known users"]
+  (** Decode a run-length encoded list. *)
+
+  val rle_eq : ('a -> 'a -> bool) -> 'a list -> ('a * int) list
+    [@@deprecated "No known users"]
+  (** [rle_eq eq l] run-length encodes [l] using [eq] *)
+
+  (** {1 Generative functions}
+      These are usually useful for coding challenges like Advent of Code.*)
+
   val range : int -> int -> int list
   (** range lower upper = [lower; lower + 1; ...; upper - 1]
-      Returns the empty list if lower >= upper. *)
+      Returns the empty list if lower >= upper.
+      Consider building an {!Stdlib.Seq}, it's more flexible *)
+
+  val between : 'a -> 'a list -> 'a list
+  (** [between e l] Intersperses [e] between elements of [l]. *)
+
+  val between_tr : 'a -> 'a list -> 'a list
+  (** Tail-recursive {!between}. *)
+
+  val randomize : 'a list -> 'a list [@@deprecated "Not used"]
+  (** Generate a random permutation of the given list. *)
+
+  val distribute : 'a -> 'a list -> 'a list list [@@deprecated "Not used"]
+  (** Distribute the given element over the given list, returning a list of
+      lists with the new element in each position. *)
+
+  val permute : 'a list -> 'a list list [@@deprecated "Not used"]
+  (** Generate all permutations of the given list. *)
+
+  val inner :
+       (('a -> 'b -> 'c -> 'd) -> 'e -> 'f -> 'g -> 'h)
+    -> 'e
+    -> ('b -> 'c -> 'i)
+    -> 'f
+    -> 'g
+    -> ('a -> 'i -> 'd)
+    -> 'h
+  (** Compute the inner product of two lists. *)
+
+  val tails : 'a list -> 'a list list
+
+  (** {1 Lists as sets, avoid}
+      Please use Set.Make instead, these functions have quadratic costs! *)
+
+  val setify : 'a list -> 'a list
+  (** [setify a] removes all duplicates from [a] while maintaining order.
+      Please use [List.sort_uniq] instead to deduplicate lists if possible *)
+
+  val subset : 'a list -> 'a list -> bool
+  (** [subset a b] returns whether all elements in [b] can be found in [a]*)
+
+  val set_equiv : 'a list -> 'a list -> bool
+
+  val set_difference : 'a list -> 'a list -> 'a list
+  (** Returns the set difference of two lists *)
+
+  val intersect : 'a list -> 'a list -> 'a list
+  (** Returns the intersection of two lists. *)
+
 end
