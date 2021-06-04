3a67a86ffa2f0c7f5f1be2a4fab1929a40a84779
Lin Liu <lin.liu@citrix.com>
2021-06-04 10:43:10 +0000

    CP-36090: Enable python unittest for extauth-hook-AD
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- .github/python-nosetests.sh
+++ .github/python-nosetests.sh
@@ -6,5 +6,8 @@ set -uex
 sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ trusty main universe"
 sudo apt-get update
 sudo apt-get install -y python-mock python-nose
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py |sudo python2
+pip install enum
 
 nosetests scripts scripts/examples scripts/examples/python
+PYTHONPATH=scripts/plugins python2 -m unittest -v test_extauth_hook_AD
