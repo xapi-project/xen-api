fc6ffe6fe672a0cf4dcbdd634b8e4bb1fb54bcbf
Rob Hoes <rob.hoes@citrix.com>
2021-06-11 13:23:45 +0000

    Fix SDK build
    Commit 63cbb1d8 introduced a function in the datamodel that has a
    return type `Set (Set String)`, which is a valid datamodel type, but
    it is not yet recognised by the API bindings (SDK).
    
    This patch just adds match cases to the code generators, such that
    `make sdk` does not fail. This will need a follow-up patch to correctly
    handle unmarshalling for the type.
    
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/sdk-gen/c/gen_c_binding.ml
+++ ocaml/sdk-gen/c/gen_c_binding.ml
@@ -1339,6 +1339,10 @@ and c_type_of_ty needed record = function
   | Set String ->
       needed := StringSet.add "string_set" !needed ;
       "struct xen_string_set *"
+  | Set (Set String) ->
+      (* TODO: implement this new type correctly *)
+      needed := StringSet.add "string_set" !needed ;
+      "struct xen_string_set *"
   | Set (Record n) ->
       needed := StringSet.add (n ^ "_decl") !needed ;
       sprintf "struct %s_set *" (record_typename n)
