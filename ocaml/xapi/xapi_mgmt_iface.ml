xapi-project/xen-api

commit d1fea385e2247f8138ab4a09f6ea7330b622d83c

CP-34727: Move Xapi_stunnel_server.restart out of Xapi_mgmt_iface.Server
This is needed, because we may need to restart stunnel even if the
mode of the (TCP) server has not changed.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

ocaml/xapi/xapi_mgmt_iface.ml
@@ -35,11 +35,15 @@ module Server : sig
   val update : __context:Context.t -> listening_mode -> unit
 
   val current_mode : unit -> listening_mode
+
+  val is_ipv6_enabled : unit -> bool
 end = struct
   (* Keep track of the management interface server thread.
      Stores a key into the table in Http_srv which identifies the server thread bound
      to the management IP. *)
   let management_servers = ref []
+
+  let ipv6_enabled = ref false
 
   let stop () =
     debug "Shutting down the old management interface (if any)" ;
@@ -50,35 +54,35 @@ end = struct
      _the_ management interface. Hosts in a pool use the IP addresses of this interface
      to communicate with each other. *)
   let start ~__context ?addr () =
-    let socket, stunnel_accept =
+    let socket =
       match addr with
       | None -> (
           info "Starting new server (listening on all IP addresses)" ;
           try
             (* Is it IPv6 ? *)
             let addr = Unix.inet6_addr_any in
-            (Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port)), ":::")
+            let socket =
+              Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port))
+            in
+            ipv6_enabled := true ;
+            socket
           with _ ->
             (* No. *)
             let addr = Unix.inet_addr_any in
-            (Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port)), "")
+            let socket =
+              Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port))
+            in
+            ipv6_enabled := false ;
+            socket
         )
-      | Some ip -> (
+      | Some ip ->
           info "Starting new server (listening on %s)" ip ;
           let addr = Unix.inet_addr_of_string ip in
           let sockaddr = Unix.ADDR_INET (addr, Constants.http_port) in
-          ( Xapi_http.bind sockaddr
-          , match Unix.domain_of_sockaddr sockaddr with
-            | Unix.PF_INET6 ->
-                "::1:"
-            | _ ->
-                "127.0.0.1:"
-          )
-        )
+          Xapi_http.bind sockaddr
     in
     Http_svr.start Xapi_http.server socket ;
     management_servers := socket :: !management_servers ;
-    Xapi_stunnel_server.restart ~__context ~accept:stunnel_accept ;
     if Pool_role.is_master () && addr = None then
       (* NB if we synchronously bring up the management interface on a master with a blank
          database this can fail... this is ok because the database will be synchronised later *)
@@ -114,6 +118,8 @@ end = struct
     mode := next_mode
 
   let current_mode () = !mode
+
+  let is_ipv6_enabled () = !ipv6_enabled
 end
 
 module Client_certificate_auth_server = struct
@@ -184,7 +190,8 @@ let mgmt_is_enabled () = Server.current_mode () = Any
 let run ~__context ~mgmt_enabled =
   Mutex.execute management_m (fun () ->
       Client_certificate_auth_server.update ~__context ~mgmt_enabled ;
-      next_server_mode ~mgmt_enabled |> Server.update ~__context
+      next_server_mode ~mgmt_enabled |> Server.update ~__context ;
+      Xapi_stunnel_server.restart ~__context (Server.is_ipv6_enabled ()) ;
   )
 
 let reconfigure_himn ~__context ~addr =

ocaml/xapi/xapi_stunnel_server.ml
@@ -181,8 +181,22 @@ let update_certificates ~__context () =
   | exception e ->
       error "Failed to update host certificates: %s" (Printexc.to_string e)
 
-let restart ~__context ~accept =
+let restart ~__context ipv6_enabled =
   try
+    let accept =
+      let management_enabled =
+        Xapi_inventory.lookup Xapi_inventory._management_interface <> ""
+      in
+      match (management_enabled, ipv6_enabled) with
+      | true, true ->
+          ":::"
+      | true, false ->
+          ""
+      | false, true ->
+          "::1:"
+      | false, false ->
+          "127.0.0.1:"
+    in
     info "Restarting stunnel (accepting connections on %s)" accept ;
     let client_auth_name =
       let pool = Helpers.get_pool ~__context in

ocaml/xapi/xapi_stunnel_server.mli
@@ -16,7 +16,7 @@
     There exist scripts (e.g. xe-toolstack-restart) which also manipulate
     the stunnel daemon but they do this directly (not via ocaml). *)
 
-val restart : __context:Context.t -> accept:string -> unit
+val restart : __context:Context.t -> bool -> unit
 (** restart stunnel, possibly changing the config file *)
 
 val reload : unit -> unit

commit eb12c3928219689b18dce81cc811cda1846a8b61

CA-358898: handle IPv6 state when management disabled
Regression in recent commit (d1fea38).

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

ocaml/xapi/xapi_mgmt_iface.ml
@@ -79,6 +79,7 @@ end = struct
           info "Starting new server (listening on %s)" ip ;
           let addr = Unix.inet_addr_of_string ip in
           let sockaddr = Unix.ADDR_INET (addr, Constants.http_port) in
+          ipv6_enabled := Unix.domain_of_sockaddr sockaddr = Unix.PF_INET6 ;
           Xapi_http.bind sockaddr
     in
     Http_svr.start Xapi_http.server socket ;