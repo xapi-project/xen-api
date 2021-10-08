xapi-project/xen-api

commit d1fea385e2247f8138ab4a09f6ea7330b622d83c

CP-34727: Move Xapi_stunnel_server.restart out of Xapi_mgmt_iface.Server
This is needed, because we may need to restart stunnel even if the
mode of the (TCP) server has not changed.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

ocaml/xapi/xapi_mgmt_iface.ml
@@ -35,11 +35,15 @@ module Server : sig
   val update : __context:Context.t -> listening_mode -> unit
 
   val current_mode : unit -> listening_mode
+
+  val is_ipv6_enabled : unit -> bool
 end = struct
   (* Keep track of the management interface server thread.
      Stores a key into the table in Http_srv which identifies the server thread bound
      to the management IP. *)
   let management_servers = ref []
+
+  let ipv6_enabled = ref false
 
   let stop () =
     debug "Shutting down the old management interface (if any)" ;
@@ -50,35 +54,35 @@ end = struct
      _the_ management interface. Hosts in a pool use the IP addresses of this interface
      to communicate with each other. *)
   let start ~__context ?addr () =
-    let socket, stunnel_accept =
+    let socket =
       match addr with
       | None -> (
           info "Starting new server (listening on all IP addresses)" ;
           try
             (* Is it IPv6 ? *)
             let addr = Unix.inet6_addr_any in
-            (Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port)), ":::")
+            let socket =
+              Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port))
+            in
+            ipv6_enabled := true ;
+            socket
           with _ ->
             (* No. *)
             let addr = Unix.inet_addr_any in
-            (Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port)), "")
+            let socket =
+              Xapi_http.bind (Unix.ADDR_INET (addr, Constants.http_port))
+            in
+            ipv6_enabled := false ;
+            socket
         )
-      | Some ip -> (
+      | Some ip ->
           info "Starting new server (listening on %s)" ip ;
           let addr = Unix.inet_addr_of_string ip in
           let sockaddr = Unix.ADDR_INET (addr, Constants.http_port) in
-          ( Xapi_http.bind sockaddr
-          , match Unix.domain_of_sockaddr sockaddr with
-            | Unix.PF_INET6 ->
-                "::1:"
-            | _ ->
-                "127.0.0.1:"
-          )
-        )
+          Xapi_http.bind sockaddr
     in
     Http_svr.start Xapi_http.server socket ;
     management_servers := socket :: !management_servers ;
-    Xapi_stunnel_server.restart ~__context ~accept:stunnel_accept ;
     if Pool_role.is_master () && addr = None then
       (* NB if we synchronously bring up the management interface on a master with a blank
          database this can fail... this is ok because the database will be synchronised later *)
@@ -114,6 +118,8 @@ end = struct
     mode := next_mode
 
   let current_mode () = !mode
+
+  let is_ipv6_enabled () = !ipv6_enabled
 end
 
 module Client_certificate_auth_server = struct
@@ -184,7 +190,8 @@ let mgmt_is_enabled () = Server.current_mode () = Any
 let run ~__context ~mgmt_enabled =
   Mutex.execute management_m (fun () ->
       Client_certificate_auth_server.update ~__context ~mgmt_enabled ;
-      next_server_mode ~mgmt_enabled |> Server.update ~__context
+      next_server_mode ~mgmt_enabled |> Server.update ~__context ;
+      Xapi_stunnel_server.restart ~__context (Server.is_ipv6_enabled ()) ;
   )
 
 let reconfigure_himn ~__context ~addr =

ocaml/xapi/xapi_stunnel_server.ml
@@ -181,8 +181,22 @@ let update_certificates ~__context () =
   | exception e ->
       error "Failed to update host certificates: %s" (Printexc.to_string e)
 
-let restart ~__context ~accept =
+let restart ~__context ipv6_enabled =
   try
+    let accept =
+      let management_enabled =
+        Xapi_inventory.lookup Xapi_inventory._management_interface <> ""
+      in
+      match (management_enabled, ipv6_enabled) with
+      | true, true ->
+          ":::"
+      | true, false ->
+          ""
+      | false, true ->
+          "::1:"
+      | false, false ->
+          "127.0.0.1:"
+    in
     info "Restarting stunnel (accepting connections on %s)" accept ;
     let client_auth_name =
       let pool = Helpers.get_pool ~__context in

ocaml/xapi/xapi_stunnel_server.mli
@@ -16,7 +16,7 @@
     There exist scripts (e.g. xe-toolstack-restart) which also manipulate
     the stunnel daemon but they do this directly (not via ocaml). *)
 
-val restart : __context:Context.t -> accept:string -> unit
+val restart : __context:Context.t -> bool -> unit
 (** restart stunnel, possibly changing the config file *)
 
 val reload : unit -> unit

commit 2bc09523c1af09067b518487869b50123d6dcf96

CA-359214: Only restart stunnel if the config file has changed
This avoids unnecessarily breaking connections.

This addresses a regression introduced in d1fea38, which caused stunnel
to be restarted no matter whether a new management server was started.

The patch renames Xapi_stunnel_server.restart to .sync to reflect that
the function no longer always restarts stunnel. Also the logging is
improved.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

ocaml/xapi/xapi_mgmt_iface.ml
@@ -183,7 +183,7 @@ let run ~__context ?mgmt_enabled () =
   Mutex.execute management_m (fun () ->
       Client_certificate_auth_server.update ~__context ~mgmt_enabled ;
       next_server_mode ~mgmt_enabled |> Server.update ~__context ;
-      Xapi_stunnel_server.restart ~__context (Server.is_ipv6_enabled ())
+      Xapi_stunnel_server.sync ~__context (Server.is_ipv6_enabled ())
   )
 
 let reconfigure_himn ~__context ~addr =

ocaml/xapi/xapi_stunnel_server.ml
@@ -25,8 +25,9 @@ let pool_cert = !Xapi_globs.server_cert_internal_path
 
 (** protect ourselves from concurrent writes to files *)
 module Config : sig
-  val update : accept:string -> client_auth_name:string option -> unit
-  (** create or update stunnel config file *)
+  val update : accept:string -> client_auth_name:string option -> bool
+  (** Create or update stunnel config file.
+      Returns a Boolean indicating whether a change was made. *)
 end = struct
   let m = Mutex.create ()
 
@@ -127,13 +128,14 @@ end = struct
         | Some current_accept, Some current_client_auth_name
           when current_accept = accept
                && current_client_auth_name = client_auth_name ->
-            ()
+            false
         | _ ->
             (* Update the stunnel config file, which will be picked
                up when restarting the service *)
             current_accept := Some accept ;
             current_client_auth_name := Some client_auth_name ;
-            update_xapi_ssl_config_file ~accept ~client_auth_name
+            update_xapi_ssl_config_file ~accept ~client_auth_name ;
+            true
     )
 end
 
@@ -184,7 +186,7 @@ let update_certificates ~__context () =
   | exception e ->
       error "Failed to update host certificates: %s" (Printexc.to_string e)
 
-let restart ~__context ipv6_enabled =
+let sync ~__context ipv6_enabled =
   try
     let accept =
       let management_enabled =
@@ -200,7 +202,6 @@ let restart ~__context ipv6_enabled =
       | false, false ->
           "127.0.0.1:"
     in
-    info "Restarting stunnel (accepting connections on %s)" accept ;
     let client_auth_name =
       let pool = Helpers.get_pool ~__context in
       if
@@ -211,12 +212,17 @@ let restart ~__context ipv6_enabled =
       else
         None
     in
-    Config.update ~accept ~client_auth_name ;
-    (* we do not worry about generating certificates here, because systemd will handle this for us, via the gencert service *)
-    if not @@ is_enabled () then systemctl_ "enable" ;
-    systemctl_ "restart" ;
-    (* the stunnel start may have caused gencert to generate new certificates: now sync the DB *)
-    update_certificates ~__context ()
+    info "Synchronising stunnel (accept = %s, client_auth_name = %s)" accept
+      (Option.value ~default:"(none)" client_auth_name) ;
+    let needs_restart = Config.update ~accept ~client_auth_name in
+    if needs_restart then (
+      (* we do not worry about generating certificates here, because systemd will handle this for us, via the gencert service *)
+      if not @@ is_enabled () then systemctl_ "enable" ;
+      systemctl_ "restart" ;
+      (* the stunnel start may have caused gencert to generate new certificates: now sync the DB *)
+      update_certificates ~__context ()
+    ) else
+      debug "No configuration changes: not restarting stunnel"
   with e ->
     Backtrace.is_important e ;
     D.error "Xapi_stunnel_server.restart: failed to restart stunnel" ;

ocaml/xapi/xapi_stunnel_server.mli
@@ -16,8 +16,8 @@
     There exist scripts (e.g. xe-toolstack-restart) which also manipulate
     the stunnel daemon but they do this directly (not via ocaml). *)
 
-val restart : __context:Context.t -> bool -> unit
-(** restart stunnel, possibly changing the config file *)
+val sync : __context:Context.t -> bool -> unit
+(** update config file and (re)start stunnel, if needed *)
 
 val reload : ?wait:float -> unit -> unit
 (** reload (potentially updated) configuration *)