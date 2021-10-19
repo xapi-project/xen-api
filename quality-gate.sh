a419effc61042f69dc27fe2e8876e9f1d4d63c1e
Pau Ruiz Safont <pau.safont@citrix.com>
2021-10-19 14:06:43 +0100

    maintenance: update quality gate
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- quality-gate.sh
+++ quality-gate.sh
@@ -3,7 +3,7 @@
 set -e
 
 list-hd () {
-  N=309
+  N=310
   LIST_HD=$(git grep -r --count 'List.hd' -- **/*.ml | cut -d ':' -f 2 | paste -sd+ - | bc)
   if [ "$LIST_HD" -eq "$N" ]; then
     echo "OK counted $LIST_HD usages"
@@ -25,7 +25,7 @@ verify-cert () {
 }
 
 mli-files () {
-  N=337
+  N=348
   # do not count ml files from the tests in ocaml/{tests/perftest/quicktest}
   MLIS=$(git ls-files -- '**/*.mli' | grep -vE "ocaml/tests|ocaml/perftest|ocaml/quicktest" | xargs -I {} sh -c "echo {} | cut -f 1 -d '.'" \;)
   MLS=$(git  ls-files -- '**/*.ml'  | grep -vE "ocaml/tests|ocaml/perftest|ocaml/quicktest" | xargs -I {} sh -c "echo {} | cut -f 1 -d '.'" \;)
@@ -40,12 +40,12 @@ mli-files () {
 }
 
 structural-equality () {
-  N=1
+  N=2
   EQ=$(git grep -r --count ' == ' -- '**/*.ml' ':!ocaml/sdk-gen/**/*.ml' | cut -d ':' -f 2 | paste -sd+ - | bc)
   if [ "$EQ" -eq "$N" ]; then
     echo "OK counted $EQ usages of ' == '"
   else
-    echo "ERROR expected $N usages of ' == '; use = rather than ==" 1>&2
+    echo "ERROR expected $N usages of ' == ', got $EQ; use = rather than ==" 1>&2
     exit 1
   fi
 
