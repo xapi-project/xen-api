651790e7c71ca3aab5e08e48887c42a8111a416e
Edwin Török <edvin.torok@citrix.com>
2021-10-19 11:14:53 +0100

    Maintenance: fix update_gh_pages.sh to work without Travis
    For now you can run this manually to update
    https://xapi-project.github.io/xapi-storage/#introduction
    
    We'll need to add a github workflow to update that on every master push.
    However the script already knows how to clone the github-pages branch of
    that repo, so we won't need to add any redirects.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- xapi-storage/update_gh_pages.sh
+++ xapi-storage/update_gh_pages.sh
@@ -9,32 +9,22 @@ set +x
 
 set -e
 
-if [ -z "$TRAVIS" -o "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
-  echo "This is not a Travis-ci build on the master branch, doing nothing..."
-  exit 0
-else
-  echo "Updating docs on Github pages..."
-fi
+echo "Updating docs on Github pages..."
 
-# Error out if $GH_TOKEN is empty or unset
-if [ -z "$GH_TOKEN" ]; then
-  echo "GH_TOKEN not found"
-  exit 1
-fi
-
-jbuilder build generator/src/main.exe
+dune build generator/src/main.exe
 
 DOCDIR=.gh-pages
 if [ -n "$KEEP" ]; then trap "rm -rf $DOCDIR" EXIT; fi
 rm -rf $DOCDIR
 
 # Don't expose GH_TOKEN
-git clone --quiet --branch=slate https://${GH_TOKEN}@github.com/xapi-project/xapi-storage $DOCDIR > /dev/null 2>&1
+git clone --quiet --branch=slate git@github.com/xapi-project/xapi-storage $DOCDIR > /dev/null 2>&1
 rm -rf $DOCDIR/source/includes/*
-jbuilder exec generator/src/main.exe -- gen_markdown --path=$DOCDIR/source/includes
+dune exec generator/src/main.exe -- gen_markdown --path=$DOCDIR/source/includes
 git -C $DOCDIR config user.email "travis@travis-ci.org"
 git -C $DOCDIR config user.name "Travis"
-(cd $DOCDIR; git add *)
+(cd $DOCDIR; git add .)
 git -C $DOCDIR commit --allow-empty -am "Travis build $TRAVIS_BUILD_NUMBER pushed docs to slate"
 # Don't expose GH_TOKEN
 git -C $DOCDIR push origin slate > /dev/null 2>&1
+echo "Docs pushed"
