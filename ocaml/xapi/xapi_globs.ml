6da2b0e3ad21159c2d441214f8930eb856b50886
Lin Liu <lin.liu@citrix.com>
2021-12-20 02:48:53 +0000

    CP-38763: Kerberos type default to All
    Set winbind_kerberos_encryption_type default to all as
    * Some old DC only support legacy encryption type
    * One way trust needs extra configuration in DC to support
    strong encrytion type
    
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -931,8 +931,7 @@ let winbind_machine_pwd_timeout = ref (7 * 24 * 3600)
 
 let winbind_update_closest_kdc_interval = ref (3600. *. 24.) (* every day *)
 
-let winbind_kerberos_encryption_type =
-  ref Kerberos_encryption_types.Winbind.Strong
+let winbind_kerberos_encryption_type = ref Kerberos_encryption_types.Winbind.All
 
 let tdb_tool = ref "/usr/bin/tdbtool"
 
