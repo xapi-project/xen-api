e7e61fe4af410150836880ebc998b1186a3a7188
Rob Hoes <rob.hoes@citrix.com>
2022-02-10 13:38:14 +0000

    Reformat
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/idl/datamodel_lifecycle.ml
+++ ocaml/idl/datamodel_lifecycle.ml
@@ -1,11 +1,5 @@
-let prototyped_of_class = function
-  | _ ->
-      None
+let prototyped_of_class = function _ -> None
 
-let prototyped_of_field = function
-  | _ ->
-      None
+let prototyped_of_field = function _ -> None
 
-let prototyped_of_message = function
-  | _ ->
-      None
+let prototyped_of_message = function _ -> None
