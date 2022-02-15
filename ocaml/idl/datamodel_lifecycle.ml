c5d936be043d56f473cc72323c9355baf871fdde
Pau Ruiz Safont <pau.safont@citrix.com>
2022-02-15 11:30:22 +0000

    maintenance: ignore formatting of datamodel_lifecycle.ml
    and change it to the autogenerated formatting
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/idl/datamodel_lifecycle.ml
+++ ocaml/idl/datamodel_lifecycle.ml
@@ -1,5 +1,11 @@
-let prototyped_of_class = function _ -> None
+let prototyped_of_class = function
+  | _ ->
+      None
 
-let prototyped_of_field = function _ -> None
+let prototyped_of_field = function
+  | _ ->
+      None
 
-let prototyped_of_message = function _ -> None
+let prototyped_of_message = function
+  | _ ->
+      None
