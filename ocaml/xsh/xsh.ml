xapi-project/xen-api

commit c6511f0a48e1c058dd43bb02fc9503be0c1aca42

CA-337546: update to new Stunnel_cache API with Safe_resources support
This required moving the stunnel_processes cleanup function to where the
tunnel was opened (convert from [connect] to [with_connect] API).

Purging needs a slight change since we no longer raise Not_found,
but return None instead.

Retrying to open the connection over TCP also needs to be made only
if the connection itself fails, not if the function [f] fails,
hence the use of a fallback variable.

Signed-off-by: Edwin Török <edvin.torok@citrix.com>

ocaml/xsh/xsh.ml
@@ -61,12 +61,13 @@ let proxy (ain: Unix.file_descr) (aout: Unix.file_descr) (bin: Unix.file_descr) 
     (try Unix.close aout with _ -> ());
     (try Unix.close bout with _ -> ())
 
-let open_tcp_ssl server =
+let with_open_tcp_ssl server f =
   let port = 443 in
   (* We don't bother closing fds since this requires our close_and_exec wrapper *)
-  let x = Stunnel.connect ~use_fork_exec_helper:false
-      ~write_to_log:(fun _ -> ()) server port in
-  x.Stunnel.fd
+  let open Safe_resources in
+  Stunnel.with_connect ~use_fork_exec_helper:false
+      ~write_to_log:(fun _ -> ()) server port @@ fun x ->
+  f Unixfd.(!(x.Stunnel.fd))
 
 let _ =
   let host = Sys.argv.(1) in
@@ -74,6 +75,6 @@ let _ =
   let session = try Sys.getenv "XSH_SESSION" with _ -> failwith "Session not provided" in
   let args = List.map (fun arg -> "&arg="^arg) (List.tl (List.tl (List.tl (Array.to_list Sys.argv)))) in
   let req = Printf.sprintf "CONNECT /remotecmd?session_id=%s&cmd=%s%s http/1.0\r\n\r\n" session cmd (String.concat "" args) in
-  let fd = open_tcp_ssl host in
+  with_open_tcp_ssl host @@ fun fd ->
   Unix.write_substring fd req 0 (String.length req) |> ignore;
   proxy Unix.stdin Unix.stdout fd (Unix.dup fd)

ocaml/database/master_connection.ml
@@ -19,6 +19,7 @@
 *)
 
 open Xapi_stdext_threads.Threadext
+open Safe_resources
 
 type db_record = (string * string) list * (string * (string list)) list
 module D = Debug.Make(struct let name = "master_connection" end)
@@ -53,20 +54,20 @@ let force_connection_reset () =
        		   combination, we pop out (remove) its links until Not_found is raised.
        		   Here, we have two such combinations, i.e. verify_cert=true/false, as
        		   host and port are fixed values. *)
-    let purge_stunnels verify_cert =
-      try
-        while true do
-          let st = Stunnel_cache.remove host port verify_cert in
-          try Stunnel.disconnect ~wait:false ~force:true st with _ -> ()
-        done
-      with Not_found -> () in
+    let rec purge_stunnels verify_cert =
+      match
+        Stunnel_cache.with_remove host port verify_cert @@ fun st ->
+        try Stunnel.disconnect ~wait:false ~force:true st with _ -> ()
+      with None -> () (* not found in cache: stop *)
+         | Some () -> purge_stunnels verify_cert
+    in
     purge_stunnels true; purge_stunnels false;
     info "force_connection_reset: all cached connections to the master have been purged";
   end;
   match !my_connection with
   | None -> ()
   | Some st_proc ->
-    (info "stunnel reset pid=%d fd=%d" (Stunnel.getpid st_proc.Stunnel.pid) (Xapi_stdext_unix.Unixext.int_of_file_descr st_proc.Stunnel.fd);
+    (info "stunnel reset pid=%d fd=%d" (Stunnel.getpid st_proc.Stunnel.pid) (Xapi_stdext_unix.Unixext.int_of_file_descr Unixfd.(!(st_proc.Stunnel.fd)));
      Unix.kill (Stunnel.getpid st_proc.Stunnel.pid) Sys.sigterm)
 
 (* whenever a call is made that involves read/write to the master connection, a timestamp is
@@ -127,14 +128,14 @@ let on_database_connection_established = ref (fun () -> ())
 let open_secure_connection () =
   let host = (!get_master_address) () in
   let port = !Db_globs.https_port in
-  let st_proc = Stunnel.connect ~use_fork_exec_helper:true
+  Stunnel.with_connect ~use_fork_exec_helper:true
       ~extended_diagnosis:true
-      ~write_to_log:(fun x -> debug "stunnel: %s\n" x) host port in
-  let fd_closed = Thread.wait_timed_read st_proc.Stunnel.fd 5. in
+      ~write_to_log:(fun x -> debug "stunnel: %s\n" x) host port @@ fun st_proc ->
+  let fd_closed = Thread.wait_timed_read Unixfd.(!(st_proc.Stunnel.fd)) 5. in
   let proc_quit = try Unix.kill (Stunnel.getpid st_proc.Stunnel.pid) 0; false with e -> true in
   if not fd_closed && not proc_quit then begin
-    info "stunnel connected pid=%d fd=%d" (Stunnel.getpid st_proc.Stunnel.pid) (Xapi_stdext_unix.Unixext.int_of_file_descr st_proc.Stunnel.fd);
-    my_connection := Some st_proc;
+    info "stunnel connected pid=%d fd=%d" (Stunnel.getpid st_proc.Stunnel.pid) (Xapi_stdext_unix.Unixext.int_of_file_descr Unixfd.(!(st_proc.Stunnel.fd)));
+    my_connection := Some (Stunnel.move_out_exn st_proc);
     !on_database_connection_established ()
   end else begin
     info "stunnel disconnected fd_closed=%s proc_quit=%s" (string_of_bool fd_closed) (string_of_bool proc_quit);
@@ -191,11 +192,11 @@ let do_db_xml_rpc_persistent_with_reopen ~host ~path (req: string) : Db_interfac
                   let res = match response.Http.Response.content_length with
                     | None -> raise Content_length_required
                     | Some l ->
-                      Xapi_stdext_unix.Unixext.really_read_string fd (Int64.to_int l)
+                      Xapi_stdext_unix.Unixext.really_read_string Unixfd.(!fd) (Int64.to_int l)
                   in
                   write_ok := true;
                   result := res (* yippeee! return and exit from while loop *)
-                ) fd
+                ) Unixfd.(!fd)
             )
       with
       | Http_svr.Client_requested_size_over_limit ->

ocaml/xe-cli/newcli.ml
@@ -35,7 +35,6 @@ let xeusessl = ref true
 let xedebug = ref false
 let xedebugonfail = ref false
 
-let stunnel_processes = ref []
 let debug_channel = ref None
 let debug_file = ref None
 
@@ -228,33 +227,62 @@ let parse_args =
       end in
     args_rest @ extras_rest @ rcs_rest @ !reserve_args
 
-let open_tcp_ssl server =
+let exit_status = ref 1
+
+let with_open_tcp_ssl server f =
   let port = get_xapiport true in
   debug "Connecting via stunnel to [%s] port [%d]\n%!" server port;
   (* We don't bother closing fds since this requires our close_and_exec wrapper *)
-  let x = Stunnel.connect ~use_fork_exec_helper:false
-      ~write_to_log:(fun x -> debug "stunnel: %s\n%!" x)
-      ~extended_diagnosis:(!debug_file <> None) server port in
-  stunnel_processes := x :: !stunnel_processes;
-  Unix.in_channel_of_descr x.Stunnel.fd, Unix.out_channel_of_descr x.Stunnel.fd
+  let open Safe_resources in
+  Stunnel.with_connect ~use_fork_exec_helper:false
+    ~write_to_log:(fun x -> debug "stunnel: %s\n%!" x)
+    ~extended_diagnosis:(!debug_file <> None) server port @@ fun x ->
+  Pervasiveext.finally (fun () -> Unixfd.with_channels x.Stunnel.fd f)
+    (fun () ->
+       if Sys.file_exists x.Stunnel.logfile then
+         begin
+           if !exit_status <> 0 then
+             (debug "\nStunnel diagnosis:\n\n";
+              try Stunnel.diagnose_failure x
+              with e -> debug "%s\n" (Printexc.to_string e));
+           try Unix.unlink x.Stunnel.logfile with _ -> ()
+         end;
+       Stunnel.disconnect ~wait:false ~force:true x
+    )
 
-let open_tcp server =
+let with_open_tcp server f =
   if !xeusessl && not(is_localhost server) then (* never use SSL on-host *)
-    open_tcp_ssl server
+    with_open_tcp_ssl server f
   else (
     let host = Unix.gethostbyname server in
     let addr = host.Unix.h_addr_list.(0) in
-    Unix.open_connection (Unix.ADDR_INET (addr,get_xapiport false))
+    let open Safe_resources in
+    Unixfd.with_open_connection ~loc:__LOC__ (Unix.ADDR_INET (addr,get_xapiport false))
+    @@ fun ufd -> Unixfd.with_channels ufd f
   )
 
-let open_channels () =
-  if is_localhost !xapiserver then (
-    try
-      Unix.open_connection (Unix.ADDR_UNIX (Filename.concat "/var/lib/xcp" "xapi"))
-    with _ ->
-      open_tcp !xapiserver
-  ) else
-    open_tcp !xapiserver
+let with_open_channels f =
+  let wrap chs =
+    try Ok (f chs)
+    with e ->
+      Backtrace.is_important e;
+      Error e
+  in
+  let result =
+    if is_localhost !xapiserver then (
+      try
+        let open Safe_resources in
+        Unixfd.with_open_connection (Unix.ADDR_UNIX (Filename.concat "/var/lib/xcp" "xapi"))
+          ~loc:__LOC__ @@ fun chs ->
+        Unixfd.with_channels chs wrap
+      with _ ->
+        with_open_tcp !xapiserver wrap
+    ) else
+      with_open_tcp !xapiserver wrap
+  in
+  match result with
+  | Ok r -> r
+  | Error e -> raise e
 
 let http_response_code x = match String.split ' ' x with
   | _ :: code :: _ -> int_of_string code
@@ -466,7 +494,7 @@ let main_loop ifd ofd permitted_filenames =
       let delay = ref 0.1 in
       let rec keep_connection () =
         try
-          let ic, oc = open_tcp server in
+          with_open_tcp server @@ fun (ic, oc) ->
           delay := 0.1;
           Pervasiveext.finally
             (fun () -> connection ic oc)
@@ -504,7 +532,7 @@ let main_loop ifd ofd permitted_filenames =
               then Printf.sprintf "\r\nContent-length: %Ld" stats.Unix.LargeFile.st_size
               else "" in
             let file_ch = open_in_bin filename in
-            let ic, oc = open_tcp server in
+            with_open_tcp server @@ fun (ic, oc) ->
             debug "PUTting to path [%s]\n%!" path;
             Printf.fprintf oc "PUT %s HTTP/1.0%s\r\n\r\n" path content_length;
             flush oc;
@@ -543,7 +571,7 @@ let main_loop ifd ofd permitted_filenames =
           let rec doit url =
             let (server,path) = parse_url url in
             debug "Opening connection to server '%s' path '%s'\n%!" server path;
-            let ic, oc = open_tcp server in
+            with_open_tcp server @@ fun (ic, oc) ->
             Printf.fprintf oc "GET %s HTTP/1.0\r\n\r\n" path;
             flush oc;
             (* Get the result header immediately *)
@@ -599,7 +627,6 @@ let main_loop ifd ofd permitted_filenames =
   match !exit_code with Some c -> c | _ -> assert false
 
 let main () =
-  let exit_status = ref 1 in
   let _ =  try
       Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
       Sys.set_signal Sys.sigint (Sys.Signal_handle (fun _ -> exit 1));
@@ -618,7 +645,7 @@ let main () =
 
       if List.length args < 1 then raise Usage else
         begin
-          let ic, oc = open_channels () in
+          with_open_channels @@ fun (ic, oc) ->
           Printf.fprintf oc "POST /cli HTTP/1.0\r\n";
           let args = args @ [("username="^ !xapiuname);("password="^ !xapipword)] in
           let args = String.concat "\n" args in
@@ -669,16 +696,6 @@ let main () =
       error "Client Side error: %s.\n" e
     | e ->
       error "Unhandled exception\n%s\n" (Printexc.to_string e) in
-  List.iter (fun p ->
-      if Sys.file_exists p.Stunnel.logfile then
-        begin
-          if !exit_status <> 0 then
-            (debug "\nStunnel diagnosis:\n\n";
-             try Stunnel.diagnose_failure p
-             with e -> debug "%s\n" (Printexc.to_string e));
-          try Unix.unlink p.Stunnel.logfile with _ -> ()
-        end;
-      Stunnel.disconnect ~wait:false ~force:true p) !stunnel_processes;
   begin match !debug_file, !debug_channel with
     | Some f, Some ch -> begin
         close_out ch;

commit 96a965439a4d9f710f565da9a131d4561945a13a

CA-366801 UPD-825 xsh: fix XAPI blob sync and EBADF
Backport of 11cce30639d84a8ffc7d3f9ddc5c5b23d0f9d018

xsh was closing the file descriptor both in the 'proxy' function and in
the stunnel wrapper code.  Push down the Unixfd.(!) dereference into the
proxy code to make it obvious that we have a Unix.close there, and
replace that with Unixfd.safe_close which avoids double close.

This was causing blob sync to fail in XAPI with an rsync protocol error
(due to the extra EBADF printed by xsh).

Fixes: c6511f0a48e1c058dd43bb02fc9503be0c1aca42 "CA-337546: update to new Stunnel_cache API with Safe_resources support"

Signed-off-by: Edwin Török <edvin.torok@citrix.com>
Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

ocaml/xsh/xsh.ml
@@ -22,12 +22,14 @@ type endpoint = {
 let make_endpoint fdin fdout =
   {fdin; fdout; buffer= Bytes.make 4096 '\000'; buffer_len= 0}
 
-let proxy (ain : Unix.file_descr) (aout : Unix.file_descr)
-    (bin : Unix.file_descr) (bout : Unix.file_descr) =
-  let a' = make_endpoint ain aout and b' = make_endpoint bin bout in
+open Safe_resources
+
+let proxy (ain : Unix.file_descr) (aout : Unix.file_descr) (bin : Unixfd.t)
+    (bout : Unix.file_descr) =
+  let a' = make_endpoint ain aout and b' = make_endpoint Unixfd.(!bin) bout in
   Unix.set_nonblock ain ;
   Unix.set_nonblock aout ;
-  Unix.set_nonblock bin ;
+  Unix.set_nonblock Unixfd.(!bin) ;
   Unix.set_nonblock bout ;
   let can_read x = x.buffer_len < Bytes.length x.buffer - 1 in
   let can_write x = x.buffer_len > 0 in
@@ -47,7 +49,8 @@ let proxy (ain : Unix.file_descr) (aout : Unix.file_descr)
   try
     while true do
       let r =
-        (if can_read a' then [ain] else []) @ if can_read b' then [bin] else []
+        (if can_read a' then [ain] else [])
+        @ if can_read b' then [Unixfd.(!bin)] else []
       in
       let w =
         (if can_write a' then [bout] else [])
@@ -62,11 +65,11 @@ let proxy (ain : Unix.file_descr) (aout : Unix.file_descr)
     done
   with _ -> (
     (try Unix.clear_nonblock ain with _ -> ()) ;
-    (try Unix.clear_nonblock bin with _ -> ()) ;
+    (try Unix.clear_nonblock Unixfd.(!bin) with _ -> ()) ;
     (try Unix.clear_nonblock aout with _ -> ()) ;
     (try Unix.clear_nonblock bout with _ -> ()) ;
     (try Unix.close ain with _ -> ()) ;
-    (try Unix.close bin with _ -> ()) ;
+    (try Unixfd.safe_close bin with _ -> ()) ;
     (try Unix.close aout with _ -> ()) ;
     try Unix.close bout with _ -> ()
   )
@@ -74,11 +77,10 @@ let proxy (ain : Unix.file_descr) (aout : Unix.file_descr)
 let with_open_tcp_ssl server f =
   let port = 443 in
   (* We don't bother closing fds since this requires our close_and_exec wrapper *)
-  let open Safe_resources in
   Stunnel.with_connect ~use_fork_exec_helper:false
     ~write_to_log:(fun _ -> ())
     server port
-  @@ fun x -> f Unixfd.(!(x.Stunnel.fd))
+  @@ fun x -> f x.Stunnel.fd
 
 let _ =
   let host = Sys.argv.(1) in
@@ -96,5 +98,5 @@ let _ =
       session cmd (String.concat "" args)
   in
   with_open_tcp_ssl host @@ fun fd ->
-  Unix.write_substring fd req 0 (String.length req) |> ignore ;
-  proxy Unix.stdin Unix.stdout fd (Unix.dup fd)
+  Unix.write_substring Unixfd.(!fd) req 0 (String.length req) |> ignore ;
+  proxy Unix.stdin Unix.stdout fd (Unix.dup Unixfd.(!fd))