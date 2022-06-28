e1b32324b26693e238ad7e233f953f9f14af6776
Edwin Török <edvin.torok@citrix.com>
2022-06-28 14:32:59 +0100

    Maintenance: commit @update-dm-lifecycle results
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- ocaml/idl/datamodel_lifecycle.ml
+++ ocaml/idl/datamodel_lifecycle.ml
@@ -6,13 +6,13 @@ let prototyped_of_field = function
   | "Repository", "gpgkey_path" ->
       Some "22.12.0"
   | "host", "last_software_update" ->
-      Some "22.19.0-next"
+      Some "22.21.0"
   | _ ->
       None
 
 let prototyped_of_message = function
   | "Repository", "apply_livepatch" ->
-      Some "22.19.0-next"
+      Some "22.21.0"
   | "Repository", "set_gpgkey_path" ->
       Some "22.12.0"
   | "message", "destroy_many" ->
