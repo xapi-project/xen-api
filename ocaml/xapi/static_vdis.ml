481611bcdcbcf6fb0201912259a45b66c3f3d765
Pau Ruiz Safont <pau.safont@citrix.com>
2022-10-12 16:38:33 +0100

    CA-364194: Allow creation of statefiles to time out
    Currently enabling HA can stall because the GFS2 calls when adding the
    HA statefile might stall indefinitely when there are connection issues
    
    Now xapi waits a minute before failing, signalling the user that
    something has gone awry
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- ocaml/xapi/static_vdis.ml
+++ ocaml/xapi/static_vdis.ml
@@ -36,7 +36,7 @@ let permanent_vdi_attach ~__context ~vdi ~reason =
       )
   ) ;
   ignore
-    (Helpers.call_script !Xapi_globs.static_vdis
+    (Helpers.call_script ~timeout:60.0 !Xapi_globs.static_vdis
        ["add"; Db.VDI.get_uuid ~__context ~self:vdi; reason]
     ) ;
   (* VDI will be attached on next boot; attach it now too *)
