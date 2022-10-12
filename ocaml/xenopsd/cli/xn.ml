a50df660096d25be0b6c9df4b9af712a0a3f793c
Rob Hoes <rob.hoes@citrix.com>
2022-10-12 12:18:02 +0000

    Named arguments in Xn.migrate
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xenopsd/cli/xn.ml
+++ ocaml/xenopsd/cli/xn.ml
@@ -1013,9 +1013,8 @@ let resume _copts disk x =
 
 let resume copts disk x = diagnose_error (need_vm (resume copts disk) x)
 
-let migrate x url compress verify_cert =
-  let open Vm in
-  let vm, _ = find_by_name x in
+let migrate ~id ~url ~compress ~verify_cert =
+  let vm, _ = find_by_name id in
   let bool b =
     match String.lowercase_ascii b with
     | "t" | "true" | "on" | "1" ->
@@ -1023,7 +1022,7 @@ let migrate x url compress verify_cert =
     | _ ->
         false
   in
-  Client.VM.migrate dbg vm.id [] [] [] url (bool compress) (bool verify_cert)
+  Client.VM.migrate dbg vm.Vm.id [] [] [] url (bool compress) (bool verify_cert)
   |> wait_for_task dbg
 
 let trim limit str =
@@ -1542,11 +1541,11 @@ let old_main () =
   | ["help"] | [] ->
       usage () ; exit 0
   | ["migrate"; id; url] ->
-      migrate id url "false" "false" |> task
+      migrate ~id ~url ~compress:"false" ~verify_cert:"false" |> task
   | ["migrate"; id; url; compress] ->
-      migrate id url compress "false" |> task
+      migrate ~id ~url ~compress ~verify_cert:"false" |> task
   | ["migrate"; id; url; compress; verify_cert] ->
-      migrate id url compress verify_cert |> task
+      migrate ~id ~url ~compress ~verify_cert |> task
   | ["vbd-list"; id] ->
       vbd_list id
   | ["pci-add"; id; idx; bdf] ->
