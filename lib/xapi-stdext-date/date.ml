a9cda200fb530a536f614ad363a0a8a38e648e91
Pau Ruiz Safont <pau.safont@citrix.com>
2022-11-09 10:00:40 +0000

    ocamlformat: format date library
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- lib/xapi-stdext-date/date.ml
+++ lib/xapi-stdext-date/date.ml
@@ -15,15 +15,28 @@
 (* ==== RFC822 ==== *)
 type rfc822 = string
 
-let months = [| "Jan"; "Feb"; "Mar"; "Apr"; "May"; "Jun";
-                "Jul"; "Aug"; "Sep"; "Oct"; "Nov"; "Dec" |]
-let days = [| "Sun"; "Mon"; "Tue"; "Wed"; "Thu"; "Fri"; "Sat" |]
+let months =
+  [|
+     "Jan"
+   ; "Feb"
+   ; "Mar"
+   ; "Apr"
+   ; "May"
+   ; "Jun"
+   ; "Jul"
+   ; "Aug"
+   ; "Sep"
+   ; "Oct"
+   ; "Nov"
+   ; "Dec"
+  |]
+
+let days = [|"Sun"; "Mon"; "Tue"; "Wed"; "Thu"; "Fri"; "Sat"|]
 
 let rfc822_of_float x =
   let time = Unix.gmtime x in
-  Printf.sprintf "%s, %d %s %d %02d:%02d:%02d GMT"
-    days.(time.Unix.tm_wday) time.Unix.tm_mday
-    months.(time.Unix.tm_mon) (time.Unix.tm_year+1900)
+  Printf.sprintf "%s, %d %s %d %02d:%02d:%02d GMT" days.(time.Unix.tm_wday)
+    time.Unix.tm_mday months.(time.Unix.tm_mon) (time.Unix.tm_year + 1900)
     time.Unix.tm_hour time.Unix.tm_min time.Unix.tm_sec
 
 let rfc822_to_string x = x
@@ -31,12 +44,16 @@ let rfc822_to_string x = x
 (* ==== ISO8601/RFC3339 ==== *)
 
 type print_timezone = Empty | TZ of string
+
 (* we must store the print_type with iso8601 to handle the case where the local time zone is UTC *)
 type iso8601 = Ptime.date * Ptime.time * print_timezone
 
 let utc = TZ "Z"
 
-let of_dt print_type dt = let (date, time) = dt in (date, time, print_type)
+let of_dt print_type dt =
+  let date, time = dt in
+  (date, time, print_type)
+
 let to_dt (date, time, _) = (date, time)
 
 let best_effort_iso8601_to_rfc3339 x =
@@ -44,44 +61,55 @@ let best_effort_iso8601_to_rfc3339 x =
    * (b) add UTC tz if no tz provided *)
   let x =
     try
-      Scanf.sscanf x "%04d%02d%02dT%s"
-        (fun y mon d rest ->
-          Printf.sprintf "%04d-%02d-%02dT%s" y mon d rest)
-    with _ ->
-      x
+      Scanf.sscanf x "%04d%02d%02dT%s" (fun y mon d rest ->
+          Printf.sprintf "%04d-%02d-%02dT%s" y mon d rest
+      )
+    with _ -> x
   in
   let tz =
     try
-      Scanf.sscanf x "%04d-%02d-%02dT%02d:%02d:%02d%s"
-        (fun _ _ _ _ _ _ tz -> Some tz)
+      Scanf.sscanf x "%04d-%02d-%02dT%02d:%02d:%02d%s" (fun _ _ _ _ _ _ tz ->
+          Some tz
+      )
     with _ -> None
   in
   match tz with
   | None | Some "" ->
-    (* the caller didn't specify a tz. we must try to add one so that ptime can at least attempt to parse *)
-    (Printf.sprintf "%sZ" x, Empty)
-  | Some tz  ->
-    (x, TZ tz)
+      (* the caller didn't specify a tz. we must try to add one so that ptime can at least attempt to parse *)
+      (Printf.sprintf "%sZ" x, Empty)
+  | Some tz ->
+      (x, TZ tz)
 
 let of_iso8601 x =
-  let (rfc3339, print_timezone) = best_effort_iso8601_to_rfc3339 x in
+  let rfc3339, print_timezone = best_effort_iso8601_to_rfc3339 x in
   match Ptime.of_rfc3339 rfc3339 |> Ptime.rfc3339_error_to_msg with
-  | Error (`Msg e) -> invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
-  | Ok (t, tz, _)  -> match tz with
-                      | None | Some 0 -> Ptime.to_date_time t |> of_dt print_timezone
-                      | Some _        -> invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
+  | Error _ ->
+      invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
+  | Ok (t, tz, _) -> (
+    match tz with
+    | None | Some 0 ->
+        Ptime.to_date_time t |> of_dt print_timezone
+    | Some _ ->
+        invalid_arg (Printf.sprintf "date.ml:of_string: %s" x)
+  )
 
-let to_rfc3339 ((y,mon,d), ((h,min,s), _), print_type) =
+let to_rfc3339 ((y, mon, d), ((h, min, s), _), print_type) =
   match print_type with
-  | TZ tz -> Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i%s" y mon d h min s tz
-  | Empty -> Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i" y mon d h min s
+  | TZ tz ->
+      Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i%s" y mon d h min s tz
+  | Empty ->
+      Printf.sprintf "%04i%02i%02iT%02i:%02i:%02i" y mon d h min s
 
 let to_ptime_t t =
   match to_dt t |> Ptime.of_date_time with
-  | Some t -> t
+  | Some t ->
+      t
   | None ->
-    let (_, (_, offset), _) = t in
-    invalid_arg (Printf.sprintf "date.ml:to_t: dt='%s', offset='%i' is invalid" (to_rfc3339 t) offset)
+      let _, (_, offset), _ = t in
+      invalid_arg
+        (Printf.sprintf "date.ml:to_t: dt='%s', offset='%i' is invalid"
+           (to_rfc3339 t) offset
+        )
 
 let to_ptime = to_ptime_t
 
@@ -89,21 +117,23 @@ let of_ptime t = Ptime.to_date_time t |> of_dt utc
 
 let of_unix_time s =
   match Ptime.of_float_s s with
-  | None -> invalid_arg (Printf.sprintf "%s: %f" __FUNCTION__ s)
-  | Some t -> of_ptime t
+  | None ->
+      invalid_arg (Printf.sprintf "%s: %f" __FUNCTION__ s)
+  | Some t ->
+      of_ptime t
 
 let to_unix_time t = to_ptime_t t |> Ptime.to_float_s
 
 let _localtime current_tz_offset t =
   let tz_offset_s = current_tz_offset |> Option.value ~default:0 in
   let localtime = t |> Ptime.to_date_time ~tz_offset_s |> of_dt Empty in
-  let (_, (_, localtime_offset), _) = localtime in
+  let _, (_, localtime_offset), _ = localtime in
   if localtime_offset <> tz_offset_s then
-    invalid_arg (
-      Printf.sprintf "date.ml:_localtime: offsets don't match. offset='%i', t='%s'"
-        tz_offset_s
-        (Ptime.to_rfc3339 t)
-    );
+    invalid_arg
+      (Printf.sprintf
+         "date.ml:_localtime: offsets don't match. offset='%i', t='%s'"
+         tz_offset_s (Ptime.to_rfc3339 t)
+      ) ;
   localtime
 
 let _localtime_string current_tz_offset t =
@@ -129,4 +159,3 @@ let to_string = to_rfc3339
 let of_float = of_unix_time
 
 let to_float = to_unix_time
-
--- lib/xapi-stdext-date/date.mli
+++ lib/xapi-stdext-date/date.mli
@@ -18,19 +18,19 @@
 (** An ISO-8601 date/time type. *)
 type iso8601
 
-(** Convert ptime to time in UTC *)
 val of_ptime : Ptime.t -> iso8601
+(** Convert ptime to time in UTC *)
 
+val to_ptime : iso8601 -> Ptime.t
 (** Convert date/time to a ptime value: the number of seconds since 00:00:00
     UTC, 1 Jan 1970. Assumes the underlying iso8601 is in UTC *)
-val to_ptime : iso8601 -> Ptime.t
 
-(** Convert calendar time [x] (as returned by e.g. Unix.time), to time in UTC. *)
 val of_unix_time : float -> iso8601
+(** Convert calendar time [x] (as returned by e.g. Unix.time), to time in UTC. *)
 
+val to_unix_time : iso8601 -> float
 (** Convert date/time to a unix timestamp: the number of seconds since 
     00:00:00 UTC, 1 Jan 1970. Assumes the underlying iso8601 is in UTC *)
-val to_unix_time : iso8601 -> float
 
 val to_rfc3339 : iso8601 -> string
 (** Convert date/time to an RFC-3339-formatted string. It also complies with
@@ -40,35 +40,36 @@ val of_iso8601 : string -> iso8601
 (** Convert ISO 8601 formatted string to a date/time value. Does not accept a
     timezone annotated datetime - i.e. string must be UTC, and end with a Z *)
 
-(** Same as [of_unix_time] *)
 val of_float : float -> iso8601
+(** Same as [of_unix_time] *)
 
-(** Same as [to_unix_time] *)
 val to_float : iso8601 -> float
+(** Same as [to_unix_time] *)
 
-(** Convert date/time to an ISO 8601 formatted string. *)
 val to_string : iso8601 -> string
+(** Convert date/time to an ISO 8601 formatted string. *)
 
+val of_string : string -> iso8601
 (** Convert ISO 8601 formatted string to a date/time value.
   * Does not accept a timezone annotated datetime - i.e. string must be UTC, and end with a Z *)
-val of_string : string -> iso8601
 
+val assert_utc : iso8601 -> unit
+  [@@deprecated
+    "assertions performed inside constructors, so this fn does nothing"]
 (** Raises an Invalid_argument exception if the given date is not a UTC date.
  *  A UTC date is an ISO 8601 strings that ends with the character 'Z'. *)
-val assert_utc : iso8601 -> unit
-[@@deprecated "assertions performed inside constructors, so this fn does nothing"]
 
+val epoch : iso8601
 (** 00:00:00 UTC, 1 Jan 1970, in UTC *)
-val epoch: iso8601
 
+val never : iso8601
 (** Same as [epoch] *)
-val never: iso8601
 
-(** Count the number of seconds passed since 00:00:00 UTC, 1 Jan 1970, in UTC *)
 val now : unit -> iso8601
+(** Count the number of seconds passed since 00:00:00 UTC, 1 Jan 1970, in UTC *)
 
-(** exposed for testing *)
 val _localtime_string : Ptime.tz_offset_s option -> Ptime.t -> string
+(** exposed for testing *)
 
 val localtime : unit -> iso8601
 
@@ -77,10 +78,10 @@ val localtime : unit -> iso8601
 (** An RFC 822 date/time type. *)
 type rfc822
 
-(** Convert calendar time [x] (as returned by e.g. Unix.time), to RFC 822. *)
 val rfc822_of_float : float -> rfc822
+(** Convert calendar time [x] (as returned by e.g. Unix.time), to RFC 822. *)
 
-(** Convert RFC 822 date/time to a formatted string. *)
 val rfc822_to_string : rfc822 -> string
+(** Convert RFC 822 date/time to a formatted string. *)
 
 val eq : iso8601 -> iso8601 -> bool
