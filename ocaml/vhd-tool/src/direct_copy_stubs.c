c61d2ad82db0e9428fca1fb14b9475bdc1c4e23e
Edwin Török <edvin.torok@citrix.com>
2022-12-07 09:36:45 +0000

    vhd-tool/direct_copy_stubs: fix setting of O_DIRECT flag
    If a file descriptor is open in the normal way then its flags will be 0,
    and the code here that attempts to enable O_DIRECT will do nothing.
    
    This appears to have been an incorrect way of inverting an error condition
    `ret < 0`, the negation of which is `ret >= 0`, and not `ret > 0`.
    
    We open the file with O_DIRECT elsewhere though (in vhd-format), but fix this
    code to avoid confusion.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- ocaml/vhd-tool/src/direct_copy_stubs.c
+++ ocaml/vhd-tool/src/direct_copy_stubs.c
@@ -86,7 +86,7 @@ CAMLprim value stub_init(value in_fd, value out_fd)
      we might get on setting the flag.
   */
   flags = fcntl(c_out_fd, F_GETFL, NULL);
-  if (flags > 0 && !(flags & O_DIRECT))
+  if (flags >= 0 && !(flags & O_DIRECT))
     fcntl(c_out_fd, F_SETFL, flags | O_DIRECT);
 #endif
 
@@ -141,7 +141,7 @@ CAMLprim value stub_direct_copy(value handle, value len){
 
   rc = TRIED_AND_FAILED;
   bytes = 0;
-  
+
   remaining = c_len;
   while (remaining > 0) {
     ssize_t bread;
