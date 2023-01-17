fb0aa353ebf41969d09a90f3bca2def97c0eb974
Edwin Török <edwin.torok@cloud.com>
2023-01-17 09:38:58 +0000

    wip

--- scripts/container-pool-dev/dev.sh
+++ scripts/container-pool-dev/dev.sh
@@ -6,10 +6,19 @@ IMAGE=cpd
 
 # we could also use COPY/ADD for *.opam but sometimes that is very very slow
 # with podman, better to just mount it so we get latest
+# also podman build doesn't have the right flags for userns=keep-id
+# (userns=host doesn't work), so use build to build just depext container,
+# and use run for the rest
 podman build \
     -t "${IMAGE}" \
+    -f scripts/container-pool-dev/Containerfile \
+    ../../
+
+podman run \
+    --name="${IMAGE}-run" \
+    --userns=keep-id \
+    "${IMAGE}" \
     -v "${HOME}/.cache/dune:/home/opam/.cache/dune:rw,z" \
     -v "${HOME}/.opam/download-cache:/home/opam/.opam/download-cache:rw,z" \
     -v "$(pwd):/home/opam/xapi:rw,z" \
-    -f scripts/container-pool-dev/Containerfile \
-    ../../
+    opam install -y --deps-only xapi
