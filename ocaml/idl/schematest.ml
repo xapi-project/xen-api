dc725558d35ea63a7870b684e64fff678ee0f4e1
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2023-02-07 15:17:16 +0000

    CA-374989: Bump datamodel version
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -1,7 +1,7 @@
 let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly *)
-let last_known_schema_hash = "24ec58d6f7fb24a2a6c48aa4973521ed"
+let last_known_schema_hash = "021c461d774cbfc67ba65e5616d21f41"
 
 let current_schema_hash : string =
   let open Datamodel_types in
