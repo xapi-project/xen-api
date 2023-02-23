fb54bae407d334eb0e00b415eb9089804902c4d1
Rob Hoes <rob.hoes@citrix.com>
2023-02-23 17:56:51 +0000

    CP-40388: define VDI_ACTIVATE_READONLY in Smint
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/smint.ml
+++ ocaml/xapi/smint.ml
@@ -44,6 +44,7 @@ type capability =
   | Vdi_snapshot
   | Vdi_resize
   | Vdi_activate
+  | Vdi_activate_readonly
   | Vdi_deactivate
   | Vdi_update
   | Vdi_introduce
@@ -82,6 +83,7 @@ let string_to_capability_table =
   ; ("VDI_CLONE", Vdi_clone)
   ; ("VDI_SNAPSHOT", Vdi_snapshot)
   ; ("VDI_ACTIVATE", Vdi_activate)
+  ; ("VDI_ACTIVATE_READONLY", Vdi_activate_readonly)
   ; ("VDI_DEACTIVATE", Vdi_deactivate)
   ; ("VDI_UPDATE", Vdi_update)
   ; ("VDI_INTRODUCE", Vdi_introduce)
