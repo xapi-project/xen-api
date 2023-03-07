3b9f8c75fd0c977d5791c60122357c33af6e5345
Ming Lu <ming.lu@citrix.com>
2023-03-07 06:49:02 +0000

    CP-41574: Update DB schema
    Signed-off-by: Ming Lu <ming.lu@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -1,7 +1,7 @@
 let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly *)
-let last_known_schema_hash = "021c461d774cbfc67ba65e5616d21f41"
+let last_known_schema_hash = "8de3ec5ab553be646f6ecfa1aa4c207b"
 
 let current_schema_hash : string =
   let open Datamodel_types in
