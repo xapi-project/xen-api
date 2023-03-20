b4b671c5db5ff745a929419ebd7185ddc38ca671
jameshensmancitrix <james.hensman@citrix.com>
2023-03-20 09:41:25 +0000

    CP-42441 Added Status to Span type, set on finish
    Signed-off-by: jameshensmancitrix <james.hensman@citrix.com>

--- ocaml/xenopsd/lib/xenops_server.ml
+++ ocaml/xenopsd/lib/xenops_server.ml
@@ -1814,7 +1814,7 @@ let with_tracing ~name ~task f =
   let context = Xenops_task.tracing task in
   let parent : Tracing.t = Option.bind context Tracing.t_of_string in
   let tracer = Tracing.TracerProviders.get_default_tracer ~name in
-  let span = Tracing.Tracer.start ~tracer ~name ~parent in
+  let span = Tracing.Tracer.start ~tracer ~name ~parent () in
   match span with
   | Ok span ->
       let sub_context = Tracing.json_of_t span in
