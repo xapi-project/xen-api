fb99a47b1df48b395210c5a2c9779bc7442679cc
Edwin Török <edwin.torok@cloud.com>
2023-03-20 18:29:35 +0000

    [maintenance]: speed up fe_test.sh when max_fds is very large
    E.g with 'ulimit -n' 524288 this test is very slow because forkexecd closes
    all potentially open file descriptors.
    
    The test assumes a max fds of at least 1024, so set that.
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/forkexecd/test/fe_test.sh
+++ ocaml/forkexecd/test/fe_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export OCAMLRUNPARAM=b
 # Use user-writable directories
 export TMPDIR=${TMPDIR:-/tmp}
 export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-$TMPDIR}
@@ -7,6 +8,7 @@ export FE_TEST=1
 
 SOCKET=${XDG_RUNTIME_DIR}/xapi/forker/main
 
+ulimit -n 1024
 ../src/fe_main.exe &
 MAIN=$!
 cleanup () {
@@ -14,6 +16,7 @@ cleanup () {
 }
 trap cleanup EXIT
 for _ in $(seq 1 100); do
-    test -S ${SOCKET} || sleep 0.1
+    test -S ${SOCKET} && break
+    sleep 0.1
 done
 echo "" | ./fe_test.exe 16
