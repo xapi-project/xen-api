62ff5e74065c810d79cc5c6db751b6121646d5bd
Edwin Török <edwin.torok@cloud.com>
2023-03-20 18:29:35 +0000

    [maintenance]: reduce basic-rpc-test time
    Remove most sleeps, and reduce test duration from 5 seconds to 1.
    (If we do want to run a performance test we can increase these again)
    
    Run just a basic test for 0.1 seconds instead of a performance test for 5s by
    default.
    (can still be tweaked by overriding SECS)
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/message-switch/core_test/basic-rpc-test.sh
+++ ocaml/message-switch/core_test/basic-rpc-test.sh
@@ -3,38 +3,42 @@ set -e
 
 SPATH=${TMPDIR:-/tmp}/sock_s
 SWITCHPATH=${TMPDIR:-/tmp}/switch_s
+SECS=0.1
 
-
-rm -rf ${SWITCHPATH} && mkdir -p ${SWITCHPATH}
+rm -rf "${SWITCHPATH}" && mkdir -p "${SWITCHPATH}"
 
 echo Test message switch serial processing
 
 echo Checking the switch can start late
-./server_unix_main.exe -path $SPATH &
-sleep 1
-../switch/switch_main.exe --path $SPATH --statedir ${SWITCHPATH} &
-./client_unix_main.exe -path $SPATH -secs 5
-sleep 2
+./server_unix_main.exe -path "${SPATH}" &
+SERVER=$!
+sleep "${SECS}"
+../switch/switch_main.exe --path "${SPATH}" --statedir "${SWITCHPATH}" &
+./client_unix_main.exe -path "${SPATH}" -secs "${SECS}"
+wait "${SERVER}"
 
 echo Performance test of Unix to Unix
-./server_unix_main.exe -path $SPATH &
-./client_unix_main.exe -path $SPATH -secs 5
-sleep 2
+./server_unix_main.exe -path "${SPATH}" &
+SERVER=$!
+./client_unix_main.exe -path "${SPATH}" -secs "${SECS}"
+wait "${SERVER}"
 
 echo Performance test of Lwt to Lwt
-lwt/server_main.exe -path $SPATH &
-lwt/client_main.exe -path $SPATH -secs 5
-sleep 2
+lwt/server_main.exe -path "${SPATH}" &
+SERVER=$!
+lwt/client_main.exe -path "${SPATH}" -secs "${SECS}"
+wait "${SERVER}"
 
 echo Performance test of Async to Lwt
-lwt/server_main.exe -path $SPATH &
-async/client_async_main.exe -path $SPATH -secs 5
-sleep 2
+lwt/server_main.exe -path "${SPATH}" &
+SERVER=$!
+async/client_async_main.exe -path "${SPATH}" -secs "${SECS}"
+wait "${SERVER}"
 
 echo Performance test of Async to Async
-async/server_async_main.exe -path $SPATH &
-async/client_async_main.exe -path $SPATH -secs 5
-sleep 2
+async/server_async_main.exe -path "${SPATH}" &
+SERVER=$!
+async/client_async_main.exe -path "${SPATH}" -secs "${SECS}"
+wait "${SERVER}"
 
-../cli/main.exe shutdown --path $SPATH
-sleep 2
+../cli/main.exe shutdown --path "${SPATH}"
