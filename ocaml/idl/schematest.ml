c9565f329fde341b99ac3825f460d2835a0c63c4
Gang Ji <gang.ji@citrix.com>
2023-03-30 17:12:19 +0800

    update new schema hash to fix test failure
    Signed-off-by: Gang Ji <gang.ji@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -1,7 +1,7 @@
 let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly *)
-let last_known_schema_hash = "9ebc7e426e5e40455ef56edb38576c27"
+let last_known_schema_hash = "4c64949949d7f6fba87cbb9b9ac4ff40"
 
 let current_schema_hash : string =
   let open Datamodel_types in
