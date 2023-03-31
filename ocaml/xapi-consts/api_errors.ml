af23079033739a504c1dc41017fb9318475b6c74
Christian Lindig <christian.lindig@cloud.com>
2023-03-31 16:07:47 +0100

    CP-42455 Revert disable DMC: update error messages
    This reverts commit 0375d2a7c4bc9b50484dd7e18dbcaf248146f59e
    
    Signed-off-by: Christian Lindig <christian.lindig@cloud.com>

--- ocaml/xapi-consts/api_errors.ml
+++ ocaml/xapi-consts/api_errors.ml
@@ -1276,8 +1276,6 @@ let invalid_repository_proxy_url = "INVALID_REPOSITORY_PROXY_URL"
 
 let invalid_repository_proxy_credential = "INVALID_REPOSITORY_PROXY_CREDENTIAL"
 
-let dynamic_memory_control_unavailable = "DYNAMIC_MEMORY_CONTROL_UNAVAILABLE"
-
 let apply_livepatch_failed = "APPLY_LIVEPATCH_FAILED"
 
 let update_guidance_changed = "UPDATE_GUIDANCE_CHANGED"
