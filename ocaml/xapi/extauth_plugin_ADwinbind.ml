275a31a9fde4a59c87f54b0b9f12a5228d474fbe
Lin Liu <lin.liu@citrix.com>
2023-04-05 10:20:23 +0000

    CA-376294: Extract hostname from FQDN
    The local hostname may be set to FQDN, which is likely to exceed
    the netbios limit (15 chars), Then the netbios is compressed and
    randomized to meet the limit.
    However, user may pre-create the machine account to join domain
    with the hostname part of FQDN, thus we need to extract the
    hostname from the FQDN as netbios to join domain.
    
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- ocaml/xapi/extauth_plugin_ADwinbind.ml
+++ ocaml/xapi/extauth_plugin_ADwinbind.ml
@@ -991,17 +991,32 @@ module Winbind = struct
     in
     String.init len (fun _ -> random_char ())
 
-  let build_netbios_name () =
+  let build_netbios_name localhost_name =
     (* Winbind follow https://docs.microsoft.com/en-US/troubleshoot/windows-server/identity/naming-conventions-for-computer-domain-site-ou#domain-names to limit netbios length to 15
      * Compress the hostname if exceed the length *)
-    let hostname = get_localhost_name () in
-    if String.length hostname > max_netbios_name_length then
+
+    (* The localhost_name may be FQDN, need to extract hostname from it, see XSI-1407
+     * hostname always be the first part of FQDN *)
+    let hostname =
+      Domain_name.of_string localhost_name
+      |> Result.map (fun x -> Domain_name.get_label x 0)
+      |> Result.join
+      |> function
+      | Error _ ->
+          raise
+            (generic_ex "Failed to extract hostname from FQDN %s" localhost_name)
+      | Ok x ->
+          x
+    in
+    if String.length hostname > max_netbios_name_length then (
       (* format hostname to prefix-random each with 7 chars *)
       let len = 7 in
       let prefix = String.sub hostname 0 len in
       let suffix = random_string len in
-      Printf.sprintf "%s-%s" prefix suffix
-    else
+      let netbios_name = Printf.sprintf "%s-%s" prefix suffix in
+      warn "hostname length exceed limit, use '%s' instead" netbios_name ;
+      netbios_name
+    ) else
       hostname
 end
 
@@ -1217,7 +1232,7 @@ let build_netbios_name ~config_params =
       else
         name
   | None ->
-      Winbind.build_netbios_name ()
+      get_localhost_name () |> Winbind.build_netbios_name
 
 let build_dns_hostname_option ~config_params =
   let key = "dns-hostname" in
