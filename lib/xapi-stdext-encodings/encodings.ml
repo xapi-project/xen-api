40bd10c0161ed2f614528be921cac5d08cea9989
Edwin Török <edwin.torok@cloud.com>
2023-05-17 18:21:16 +0100

    xapi-stdext-encodings: read 4 bytes at a time in ASCII fastpath
    Speeds up validation considerably, achieving > 1700 MiB/s.
    
    The idea to use subtraction and bit twiddling for checking the range of
    bytes in a word is from:
    https://graphics.stanford.edu/~seander/bithacks.html#HasLessInWord
    
    However the formula is simplified significantly taking advantage that we
    also check that the MSB in each byte (0x80) is clear in the first place.
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- lib/xapi-stdext-encodings/encodings.ml
+++ lib/xapi-stdext-encodings/encodings.ml
@@ -80,73 +80,73 @@ end
 exception Validation_error of int * exn
 
 module UTF8_XML : STRING_VALIDATOR = struct
-  let validate = XML_UTF8_UCS_validator.validate
 
   let decode_continuation_byte byte =
     if byte land 0b11000000 = 0b10000000 then byte land 0b00111111 else
       raise UTF8_continuation_byte_invalid
-    [@@inline]
 
   let rec decode_continuation_bytes string last value index =
     if index <= last then
-      let chunk = (decode_continuation_byte[@inlined]) (Char.code string.[index]) in
+      let chunk = decode_continuation_byte (Char.code string.[index]) in
       let value = (value lsl 6) lor chunk in
       decode_continuation_bytes string last value (index + 1)
     else value
 
-  let validate_character_long_utf8 string byte index =
+  let validate_character_utf8 string byte index =
     let value, width =
+      if byte land 0b10000000 = 0b00000000 then (byte,                1) else
       if byte land 0b11100000 = 0b11000000 then (byte land 0b0011111, 2) else
       if byte land 0b11110000 = 0b11100000 then (byte land 0b0001111, 3) else
       if byte land 0b11111000 = 0b11110000 then (byte land 0b0000111, 4) else
         raise UTF8_header_byte_invalid
     in
-    let value = decode_continuation_bytes string (index+width-1) value (index+1)
+    let value =
+        if width = 1 then value
+        else decode_continuation_bytes string (index+width-1) value (index+1)
     in
-    validate (Uchar.unsafe_of_int value);
+    XML_UTF8_UCS_validator.validate (Uchar.unsafe_of_int value);
     width
-    [@@inline never][@@local never][@@specialise never]
     
-  let validate_byte byte = validate (Uchar.unsafe_of_int byte)[@@inline]
-  
-  let validate_character_utf8 string byte index =
-    if byte land 0b10000000 = 0b00000000 then begin
-      validate (Uchar.unsafe_of_int byte);
-      1
-    end else validate_character_long_utf8 string byte index
-    [@@inline]
-
-  let validate_character string index =
-   if index + 1 < String.length string then
-      let i16 = String.get_uint16_le string index in
-      if (i16 land 0x80_80 = 0) then begin
-        validate_byte (i16 land 0xff);
-        validate_byte (i16 lsr 8);
-        2
-      end
-    else
-      validate_character_utf8 string (i16 land 0xff) index
-    else
-    let byte = Char.code string.[index] in
-    validate_character_utf8 string byte index
-  [@@inline]
-
-  let raise_validation_error index error = raise (Validation_error(index, error))
-  [@@inline never][@@local never][@@specialise never]
-
   let rec validate_aux string length index =
     if index = length then ()
     else
     let width =
-      try (validate_character[@inlined]) string index
+      try
+            let byte = string.[index] |> Char.code in
+            validate_character_utf8 string byte index
       with
       | Invalid_argument _ -> raise String_incomplete
-      | error -> raise_validation_error index error
+      | error -> raise (Validation_error(index, error))
     in
     validate_aux string length (index + width)
 
   let validate string =
     validate_aux string (String.length string) 0
+
+  let rec validate_with_fastpath string stop pos =
+    if pos < stop then
+        (* the compiler is smart enough to optimize the 'int32' away here,
+           and not allocate *)
+        let i32 = String.get_int32_ne string pos |> Int32.to_int in
+        (* test that for all bytes 0x20 <= byte < 0x80.
+         If any is <0x20 it would cause a negative value to appear in that byte,
+         which we can detect if we use 0x80 as a mask.
+         Byte >= 0x80 can be similarly detected with a mask of 0x80 on each byte.
+         We don't want to see a 0x80 from either of these, hence we bitwise or the 2 values together.
+       *)
+        if (i32 lor (i32 - 0x20_20_20_20)) land 0x80_80_80_80 = 0 then
+            validate_with_fastpath string stop (pos + 4)
+        else (* when the condition doesn't hold fall back to full UTF8 decoder *)
+            validate_aux string (String.length string) pos
+    else
+        validate_aux string (String.length string) pos
+
+  let validate_with_fastpath string =
+      validate_with_fastpath string (String.length string - 3) 0
+
+  let validate =
+     if Sys.word_size = 64 then validate_with_fastpath
+     else validate
 
   let is_valid string =
     try validate string; true with _ -> false
