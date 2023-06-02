1074ebd8ad5ada95533b8a55e3279311d8b6077d
Gang Ji <gang.ji@citrix.com>
2023-06-02 13:28:20 +0800

    update last_known_schema_hash to pass make test
    Signed-off-by: Gang Ji <gang.ji@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -2,7 +2,7 @@ let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
-let last_known_schema_hash = "2faedbc55d9d5c16b55ce4f6fc71ec38"
+let last_known_schema_hash = "fccdb26e75ad7b029f700a31fbf95328"
 
 let current_schema_hash : string =
   let open Datamodel_types in
