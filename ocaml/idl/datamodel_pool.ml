1b2ce84f16d0a9140ef31ee4e1f254a45d0f9b70
gangj <62988402+gangj@users.noreply.github.com>
2023-06-02 14:13:10 +0800

    Update comment for pool field: "update_sync_frequency"
    Co-authored-by: Pau Ruiz Safont <psafont@users.noreply.github.com>
    Signed-off-by: gangj <62988402+gangj@users.noreply.github.com>

--- ocaml/idl/datamodel_pool.ml
+++ ocaml/idl/datamodel_pool.ml
@@ -1432,8 +1432,8 @@ let t =
             "time of the last update sychronization"
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:update_sync_frequency
             ~default_value:(Some (VEnum "weekly")) "update_sync_frequency"
-            "The frequency at which updates are synced from remote CDN: daily \
-             or weekly."
+            "The frequency at which updates are synchronized from a remote CDN: \
+             daily or weekly."
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:Int "update_sync_day"
             ~default_value:(Some (VInt 0L))
             "Which day of one period the update sychronization is scheduled, \
