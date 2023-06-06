aa7600219a85600e516075a8b513e000f56bb391
Gang Ji <gang.ji@citrix.com>
2023-06-06 14:22:13 +0800

    refactor unit tests
    Signed-off-by: Gang Ji <gang.ji@citrix.com>

--- ocaml/tests/test_pool_periodic_update_sync.ml
+++ ocaml/tests/test_pool_periodic_update_sync.ml
@@ -15,270 +15,143 @@
 open Test_highlevel
 open Pool_periodic_update_sync
 
-module TestUpdateSyncDelay = struct
-  let utc_offset_s = 0 (* UTC timezone for test *)
+type time_without_tz = int * int * int
 
-  let utc8_offset_s = 8 * 60 * 60 (* UTC +8 timezone for test *)
+let timezones = [(0, "UTC"); (8 * 60 * 60, "UTC +8"); (-1 * 60 * 60, "UTC -1")]
 
-  let utc_1_offset_s = -1 * 60 * 60 (* UTC -1 timezone for test *)
+let of_date_time_tz d t tz_s = Ptime.of_date_time (d, (t, tz_s)) |> Option.get
 
-  let test_day_of_next_sync_1 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((13, 14, 15), tz_offset_s))
-      |> Option.get
-    in
-    let res = day_of_next_sync ~now ~tz_offset_s ~frequency:Daily in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 5), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    Alcotest.(check bool)
-      "test_day_of_next_sync_1, daily" true
-      (Ptime.equal res next_sync)
+module TestDayOfNextSync = struct
+  let ptime = Alcotest.testable Ptime.pp Ptime.equal
 
-  let test_day_of_next_sync_2 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 31), ((13, 14, 15), tz_offset_s))
-      |> Option.get
-    in
-    let res = day_of_next_sync ~now ~tz_offset_s ~frequency:Daily in
-    let next_sync =
-      Ptime.of_date_time ((2023, 6, 1), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    Alcotest.(check bool)
-      "test_day_of_next_sync_2, daily, end of month" true
-      (Ptime.equal res next_sync)
+  type test_case = {
+      name: string
+    ; now: Ptime.date * time_without_tz
+    ; frequency: frequency
+    ; expected: Ptime.date * time_without_tz
+  }
 
-  let test_day_of_next_sync_3 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 12, 31), ((13, 14, 15), tz_offset_s))
-      |> Option.get
-    in
-    let res = day_of_next_sync ~now ~tz_offset_s ~frequency:Daily in
-    let next_sync =
-      Ptime.of_date_time ((2024, 1, 1), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    Alcotest.(check bool)
-      "test_day_of_next_sync_3, daily, end of year" true
-      (Ptime.equal res next_sync)
-
-  let test_day_of_next_sync_4 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((13, 14, 15), tz_offset_s))
-      |> Option.get
-    in
-    let res = day_of_next_sync ~now ~tz_offset_s ~frequency:(Weekly 6) in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 6), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    Alcotest.(check bool)
-      "test_day_of_next_sync_4, weekly, this week" true
-      (Ptime.equal res next_sync)
-
-  let test_day_of_next_sync_5 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((13, 14, 15), tz_offset_s))
-      |> Option.get
-    in
-    let res = day_of_next_sync ~now ~tz_offset_s ~frequency:(Weekly 1) in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 8), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    Alcotest.(check bool)
-      "test_day_of_next_sync_5, weekly, next week" true
-      (Ptime.equal res next_sync)
-
-  let test_day_of_next_sync_6 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((13, 14, 15), tz_offset_s))
-      |> Option.get
-    in
-    let res = day_of_next_sync ~now ~tz_offset_s ~frequency:(Weekly 4) in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 11), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    Alcotest.(check bool)
-      "test_day_of_next_sync_6, weekly, next week 2" true
-      (Ptime.equal res next_sync)
-
-  let test_time_until_next_sync_1 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((0, 0, 1), tz_offset_s)) |> Option.get
-    in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 6), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    let delay = (2. *. 24. *. 60. *. 60.) -. 1. in
-    let res = time_until_next_sync ~now ~next_sync in
-    Alcotest.(check (float Float.epsilon))
-      "test_time_until_next_sync_1" delay
-      (Ptime.Span.to_float_s res)
-
-  let test_time_until_next_sync_2 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((23, 0, 1), tz_offset_s)) |> Option.get
-    in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 5), ((1, 0, 2), tz_offset_s)) |> Option.get
-    in
-    let delay = (2. *. 60. *. 60.) +. 1. in
-    let res = time_until_next_sync ~now ~next_sync in
-    Alcotest.(check (float Float.epsilon))
-      "test_time_until_next_sync_2" delay
-      (Ptime.Span.to_float_s res)
-
-  let test_time_until_next_sync_3 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 4, 29), ((0, 0, 1), tz_offset_s)) |> Option.get
-    in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 4), ((0, 1, 6), tz_offset_s)) |> Option.get
-    in
-    let delay = (5. *. 24. *. 60. *. 60.) -. 1. +. 66. in
-    let res = time_until_next_sync ~now ~next_sync in
-    Alcotest.(check (float Float.epsilon))
-      "test_update_sync_delay_for_next_schedule_internal_3, next month" delay
-      (Ptime.Span.to_float_s res)
-
-  let test_time_until_next_sync_4 tz_offset_s =
-    let now =
-      Ptime.of_date_time ((2023, 5, 4), ((23, 0, 1), tz_offset_s)) |> Option.get
-    in
-    let next_sync =
-      Ptime.of_date_time ((2023, 5, 5), ((0, 0, 0), tz_offset_s)) |> Option.get
-    in
-    let delay = 2. *. 60. *. 60. in
-    let res = time_until_next_sync ~now ~next_sync in
-    Alcotest.(check (float Float.epsilon))
-      "test_time_until_next_sync_4" delay
-      (Ptime.Span.to_float_s res)
-
-  let test =
+  let test_cases =
     [
-      ( "day_of_next_sync daily, UTC TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_1 utc_offset_s
-      )
-    ; ( "day_of_next_sync daily, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_1 utc8_offset_s
-      )
-    ; ( "day_of_next_sync daily, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_1 utc_1_offset_s
-      )
-    ; ( "day_of_next_sync daily, end of month, UTC TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_2 utc_offset_s
-      )
-    ; ( "day_of_next_sync daily, end of month, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_2 utc8_offset_s
-      )
-    ; ( "day_of_next_sync daily, end of month, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_2 utc_1_offset_s
-      )
-    ; ( "day_of_next_sync daily, end of year, UTC TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_3 utc_offset_s
-      )
-    ; ( "day_of_next_sync daily, end of year, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_3 utc8_offset_s
-      )
-    ; ( "day_of_next_sync daily, end of year, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_3 utc_1_offset_s
-      )
-    ; ( "day_of_next_sync weekly, this week, UTC TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_4 utc_offset_s
-      )
-    ; ( "day_of_next_sync weekly, this week, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_4 utc8_offset_s
-      )
-    ; ( "day_of_next_sync weekly, this week, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_4 utc_1_offset_s
-      )
-    ; ( "day_of_next_sync weekly, next week, UTC TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_5 utc_offset_s
-      )
-    ; ( "day_of_next_sync weekly, next week, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_5 utc8_offset_s
-      )
-    ; ( "day_of_next_sync weekly, next week, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_5 utc_1_offset_s
-      )
-    ; ( "day_of_next_sync weekly, next week, 2, UTC TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_6 utc_offset_s
-      )
-    ; ( "day_of_next_sync weekly, next week, 2, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_6 utc8_offset_s
-      )
-    ; ( "day_of_next_sync weekly, next week, 2, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_day_of_next_sync_6 utc_1_offset_s
-      )
-    ; ( "calc_delay, more than 2 hours, UTC TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_1 utc_offset_s
-      )
-    ; ( "calc_delay, more than 2 hours, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_1 utc8_offset_s
-      )
-    ; ( "calc_delay, more than 2 hours, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_1 utc_1_offset_s
-      )
-    ; ( "calc_delay, 2 hours and 1 sec, UTC TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_2 utc_offset_s
-      )
-    ; ( "calc_delay, 2 hours and 1 sec, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_2 utc8_offset_s
-      )
-    ; ( "calc_delay, 2 hours and 1 sec, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_2 utc_1_offset_s
-      )
-    ; ( "calc_delay, next month, UTC TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_3 utc_offset_s
-      )
-    ; ( "calc_delay, next month, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_3 utc8_offset_s
-      )
-    ; ( "calc_delay, next month, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_3 utc_1_offset_s
-      )
-    ; ( "calc_delay, less than 2 hours, UTC TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_4 utc_offset_s
-      )
-    ; ( "calc_delay, less than 2 hours, UTC +8 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_4 utc8_offset_s
-      )
-    ; ( "calc_delay, less than 2 hours, UTC -1 TZ"
-      , `Quick
-      , fun () -> test_time_until_next_sync_4 utc_1_offset_s
-      )
+      {
+        name= "daily"
+      ; now= ((2023, 5, 4), (13, 14, 15))
+      ; frequency= Daily
+      ; expected= ((2023, 5, 5), (0, 0, 0))
+      }
+    ; {
+        name= "daily, end of month"
+      ; now= ((2023, 5, 31), (13, 14, 15))
+      ; frequency= Daily
+      ; expected= ((2023, 6, 1), (0, 0, 0))
+      }
+    ; {
+        name= "daily, end of year"
+      ; now= ((2023, 12, 31), (13, 14, 15))
+      ; frequency= Daily
+      ; expected= ((2024, 1, 1), (0, 0, 0))
+      }
+    ; {
+        name= "weekly, this week"
+      ; now= ((2023, 5, 4), (13, 14, 15))
+      ; frequency= Weekly 6
+      ; expected= ((2023, 5, 6), (0, 0, 0))
+      }
+    ; {
+        name= "weekly, different day of next week"
+      ; now= ((2023, 5, 4), (13, 14, 15))
+      ; frequency= Weekly 1
+      ; expected= ((2023, 5, 8), (0, 0, 0))
+      }
+    ; {
+        name= "weekly, same day of next week"
+      ; now= ((2023, 5, 4), (13, 14, 15))
+      ; frequency= Weekly 4
+      ; expected= ((2023, 5, 11), (0, 0, 0))
+      }
     ]
+
+  let test {name; now= d_a, t_a; frequency; expected= d_b, t_b} tz_offset_s () =
+    let expected = of_date_time_tz d_b t_b tz_offset_s in
+    let now = of_date_time_tz d_a t_a tz_offset_s in
+    let actual = day_of_next_sync ~now ~tz_offset_s ~frequency in
+    Alcotest.check ptime name expected actual
+
+  let tests_for_each_tz_from_test_case ({name; _} as test_case) =
+    let test_from_tz (tz, tz_name) =
+      let name = Printf.sprintf "%s, %s" name tz_name in
+      (name, `Quick, test {test_case with name} tz)
+    in
+    List.map test_from_tz timezones
+
+  let tests = List.concat_map tests_for_each_tz_from_test_case test_cases
+end
+
+module TestTimeUntilNextSync = struct
+  let ptime_span = Alcotest.testable Ptime.Span.pp Ptime.Span.equal
+
+  type test_case = {
+      name: string
+    ; now: Ptime.date * time_without_tz
+    ; next_sync: Ptime.date * time_without_tz
+    ; expected: int
+  }
+
+  let test_cases =
+    [
+      {
+        name= "same month, more than 2 hours"
+      ; now= ((2023, 5, 4), (0, 0, 1))
+      ; next_sync= ((2023, 5, 6), (0, 0, 0))
+      ; expected= (2 * 24 * 60 * 60) - 1
+      }
+    ; {
+        name= "different month, more than 2 hours"
+      ; now= ((2023, 4, 29), (0, 0, 1))
+      ; next_sync= ((2023, 5, 4), (0, 1, 6))
+      ; expected= (5 * 24 * 60 * 60) - 1 + 60 + 6
+      }
+    ; {
+        name= "2 hours and 1 second"
+      ; now= ((2023, 5, 4), (23, 0, 1))
+      ; next_sync= ((2023, 5, 5), (1, 0, 2))
+      ; expected= (2 * 60 * 60) + 1
+      }
+    ; {
+        name= "2 hours and 1 second"
+      ; now= ((2023, 5, 4), (23, 0, 1))
+      ; next_sync= ((2023, 5, 5), (1, 0, 1))
+      ; expected= 2 * 60 * 60
+      }
+    ; {
+        name= "1 hour and 59 second"
+      ; now= ((2023, 5, 4), (23, 0, 1))
+      ; next_sync= ((2023, 5, 5), (1, 0, 0))
+      ; expected= 2 * 60 * 60
+      }
+    ]
+
+  let test {name; now= d_a, t_a; next_sync= d_b, t_b; expected} tz_offset_s () =
+    let next_sync = of_date_time_tz d_b t_b tz_offset_s in
+    let now = of_date_time_tz d_a t_a tz_offset_s in
+    let actual = time_until_next_sync ~now ~next_sync in
+    Alcotest.check ptime_span name (Ptime.Span.of_int_s expected) actual
+
+  let tests_for_each_tz_from_test_case ({name; _} as test_case) =
+    let test_from_tz (tz, tz_name) =
+      let name = Printf.sprintf "%s, %s" name tz_name in
+      (name, `Quick, test {test_case with name} tz)
+    in
+    List.map test_from_tz timezones
+
+  let tests = List.concat_map tests_for_each_tz_from_test_case test_cases
 end
 
 let tests =
-  make_suite "pool_periodic_update_sync_"
-    [("periodic_update_sync", TestUpdateSyncDelay.test)]
+  make_suite "pool_periodic_update_sync-"
+    [
+      ("day_of_next_sync", TestDayOfNextSync.tests)
+    ; ("time_until_next_sync", TestTimeUntilNextSync.tests)
+    ]
 
 let () = Alcotest.run "Pool Periodic Update Sync" tests
