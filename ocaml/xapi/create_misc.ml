29555cb49e220958e4bb85a83409eae07b8c33f3
minglumlu <ming.lu@cloud.com>
2023-06-08 20:15:08 +0800

    CP-42999: Return new "preview" in return of v6 "get_version"
    This commit adds a new field "preview" in return of v6 interface
    "get_version".
    
    To achieve this, the change has to extend the return type of
    "get_version" interface so that more version related information could
    be supported.
    
    Signed-off-by: Ming Lu <ming.lu@cloud.com>

--- ocaml/xapi/create_misc.ml
+++ ocaml/xapi/create_misc.ml
@@ -545,15 +545,12 @@ let make_software_version ~__context host_info =
   let option_to_list k o = match o with None -> [] | Some x -> [(k, x)] in
   let v6_version =
     (* Best-effort attempt to read the date-based version from v6d *)
-    try
-      match V6_client.get_version "make_software_version" with
-      | "" ->
-          []
-      | dbv ->
-          [("dbv", dbv)]
-    with
-    | Api_errors.Server_error (code, []) when code = Api_errors.v6d_failure ->
-      []
+    match V6_client.get_version "make_software_version" with
+    | l ->
+        l
+    | exception Api_errors.Server_error (code, [])
+      when code = Api_errors.v6d_failure ->
+        []
   in
   Xapi_globs.software_version ()
   @ v6_version
