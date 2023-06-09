xapi-project/xen-api

commit 604cfbd58dab77d27110dcb61d707ef8ea11b782

CP-41842 Created Observer class with IDL functions to manage Open Telemetry providers
Signed-off-by: jameshensmancitrix <james.hensman@citrix.com>

ocaml/idl/datamodel_common.ml
@@ -303,6 +303,8 @@ let _diagnostics = "Diagnostics"
 
 let _repository = "Repository"
 
+let _observer = "Observer"
+
 let update_guidances =
   Enum
     ( "update_guidances"

ocaml/idl/datamodel.ml
@@ -7810,6 +7810,7 @@ let all_system =
   ; Datamodel_certificate.t
   ; Datamodel_diagnostics.t
   ; Datamodel_repository.t
+  ; Datamodel_observer.t
   ]
 
 (* If the relation is one-to-many, the "many" nodes (one edge each) must come before the "one" node (many edges) *)
@@ -8042,6 +8043,7 @@ let expose_get_all_messages_for =
   ; _certificate
   ; _repository
   ; _vtpm
+  ; _observer
   ]
 
 let no_task_id_for = [_task; (* _alert; *) _event]

ocaml/idl/datamodel_observer.ml
@@ -1,0 +1,153 @@
+(*
+ * Copyright (C) 2023 Cloud Software Group
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+
+open Datamodel_types
+open Datamodel_common
+open Datamodel_roles
+
+let register =
+  call ~name:"register" ~in_oss_since:None ~lifecycle:[] ~hide_from_docs:true
+    ~doc:"Register a observer on a particular host"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; (Ref _host, "host", "The host to be registered on")
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let unregister =
+  call ~name:"unregister" ~in_oss_since:None ~lifecycle:[] ~hide_from_docs:true
+    ~doc:"Unegister a observer on a particular host"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; (Ref _host, "host", "The host to be unregistered on")
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let set_hosts =
+  call ~name:"set_hosts" ~in_oss_since:None ~lifecycle:[]
+    ~doc:"Sets the hosts that the observer is to be registered on"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; (Set (Ref _host), "value", "Hosts the observer is registered on")
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let set_enabled =
+  call ~name:"set_enabled" ~in_oss_since:None ~lifecycle:[]
+    ~doc:
+      "Enable / disable this observer which will stop the observer from \
+       producing observability information"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; ( Bool
+        , "value"
+        , "If the observer is to be enabled (true) or disabled (false)"
+        )
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let set_attributes =
+  call ~name:"set_attributes" ~in_oss_since:None ~lifecycle:[]
+    ~doc:
+      "Set the attributes of an observer. These are used to emit metadata by \
+       the observer"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; ( Map (String, String)
+        , "value"
+        , "The attributes that the observer emits as part of the data"
+        )
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let set_endpoints =
+  call ~name:"set_endpoints" ~in_oss_since:None ~lifecycle:[]
+    ~doc:"Set the file/HTTP endpoints the observer sends data to"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; ( Set String
+        , "value"
+        , "The endpoints that the observer will export data to. A URL or the \
+           string 'bugtool'. This can refer to an enpoint to the local file \
+           system"
+        )
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let set_components =
+  call ~name:"set_components" ~in_oss_since:None ~lifecycle:[]
+    ~doc:
+      "Set the components on which the observer will broadcast to. i.e. xapi, \
+       xenopsd, networkd, etc"
+    ~params:
+      [
+        (Ref _observer, "self", "The observer")
+      ; (Set String, "value", "The components the observer will broadcast to")
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
+let t =
+  create_obj ~name:_observer
+    ~descr:
+      "Describes a observer which will control observability activity in the \
+       Toolstack"
+    ~doccomments:[] ~gen_constructor_destructor:true ~gen_events:true
+    ~in_db:true ~lifecycle:[] ~persist:PersistEverything ~in_oss_since:None
+    ~messages_default_allowed_roles:_R_POOL_ADMIN
+    ~contents:
+      ([
+         uid _observer ~lifecycle:[]
+       ; namespace ~name:"name" ~contents:(names None RW) ()
+       ]
+      @ [
+          field ~qualifier:StaticRO ~ty:(Set (Ref _host)) ~lifecycle:[] "hosts"
+            "The list of hosts the observer is active on. An empty list means \
+             all hosts"
+            ~ignore_foreign_key:true ~default_value:(Some (VSet []))
+        ; field ~qualifier:StaticRO
+            ~ty:(Map (String, String))
+            ~lifecycle:[] "attributes"
+            "Attributes that observer will add to the data they produce"
+            ~default_value:(Some (VMap []))
+        ; field ~qualifier:StaticRO ~ty:(Set String) ~lifecycle:[] "endpoints"
+            "The list of endpoints where data is exported to. Each endpoint is \
+             a URL or the string 'bugtool' refering to the internal logs"
+            ~default_value:(Some (VSet []))
+        ; field ~qualifier:StaticRO ~ty:(Set String) ~lifecycle:[] "components"
+            "The list of xenserver components the observer will broadcast. An \
+             empty list means all components"
+            ~default_value:(Some (VSet []))
+        ; field ~qualifier:StaticRO ~ty:Bool ~lifecycle:[] "enabled"
+            "This denotes if the observer is enabled. true if it is enabled \
+             and false if it is disabled"
+            ~default_value:(Some (VBool false))
+        ]
+      )
+    ~messages:
+      [
+        register
+      ; unregister
+      ; set_hosts
+      ; set_enabled
+      ; set_attributes
+      ; set_endpoints
+      ; set_components
+      ]
+    ()

ocaml/idl/dune
@@ -6,7 +6,7 @@
     datamodel_pool datamodel_cluster datamodel_cluster_host dm_api escaping
     datamodel_values datamodel_schema datamodel_certificate
     datamodel_diagnostics datamodel_repository datamodel_lifecycle
-    datamodel_vtpm)
+    datamodel_vtpm datamodel_observer)
   (libraries
     ppx_sexp_conv.runtime-lib
     rpclib.core

ocaml/idl/schematest.ml
@@ -2,7 +2,7 @@ let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
-let last_known_schema_hash = "c0f465587c9f59f8c17d1afa3c2998b2"
+let last_known_schema_hash = "6e38b31df3f16f18608bf5eaf74ae5d2"
 
 let current_schema_hash : string =
   let open Datamodel_types in

ocaml/tests/common/test_common.ml
@@ -652,3 +652,10 @@ let make_cluster_and_hosts ~__context extra_hosts =
     make_cluster_host ~__context ~cluster ~host ~pIF ()
   in
   (cluster, cluster_host :: List.mapi build_cluster_host extra_hosts)
+
+let make_observer ~__context ?(ref = Ref.make ()) ?(uuid = make_uuid ())
+    ?(name_label = "") ?(name_description = "") ?(hosts = []) ?(enabled = false)
+    ?(attributes = []) ?(endpoints = []) ?(components = []) () =
+  Db.Observer.create ~__context ~ref ~uuid ~name_label ~name_description ~hosts
+    ~attributes ~endpoints ~components ~enabled ;
+  ref

ocaml/xapi/api_server.ml
@@ -128,6 +128,7 @@ module Actions = struct
   module Certificate = Certificates
   module Diagnostics = Xapi_diagnostics
   module Repository = Repository
+  module Observer = Xapi_observer
 end
 
 (** Use the server functor to make an XML-RPC dispatcher. *)

ocaml/xapi/message_forwarding.ml
@@ -629,6 +629,14 @@ functor
           Ref.string_of repository
       with _ -> "invalid"
 
+    let observer_uuid ~__context observer =
+      try
+        if Pool_role.is_master () then
+          Db.Observer.get_uuid ~__context ~self:observer
+        else
+          Ref.string_of observer
+      with _ -> "invalid"
+
     module Session = struct
       include Local.Session
 
@@ -6431,5 +6439,129 @@ functor
             Client.Repository.apply_livepatch ~rpc ~session_id ~host ~component
               ~base_build_id ~base_version ~base_release ~to_version ~to_release
         )
+    end
+
+    module Observer = struct
+      module RefSet = Set.Make (struct
+        type t = [`host] Ref.t
+
+        let compare = Ref.compare
+      end)
+
+      let create ~__context ~name_label ~name_description ~hosts ~attributes
+          ~endpoints ~components ~enabled =
+        info "Observer.create: name_label=%s" name_label ;
+        let self =
+          Local.Observer.create ~__context ~name_label ~name_description ~hosts
+            ~attributes ~endpoints ~components ~enabled
+        in
+        let local_fn =
+          Local.Observer.register ~self ~host:(Helpers.get_localhost ~__context)
+        in
+        Xapi_observer.observed_hosts_of ~__context hosts
+        |> List.iter (fun host ->
+               do_op_on ~__context ~host ~local_fn (fun session_id rpc ->
+                   Client.Observer.register ~rpc ~session_id ~self ~host
+               )
+           ) ;
+        self
+
+      let register ~__context ~self ~host =
+        info "Observer.register: self=%s" (observer_uuid ~__context self) ;
+        let local_fn = Local.Observer.register ~self ~host in
+        let client_fn session_id rpc =
+          Client.Observer.register ~rpc ~session_id ~self ~host
+        in
+        do_op_on ~__context ~host ~local_fn client_fn
+
+      let unregister ~__context ~self ~host =
+        info "Observer.unregister: self=%s" (observer_uuid ~__context self) ;
+        let local_fn = Local.Observer.unregister ~self ~host in
+        let client_fn session_id rpc =
+          Client.Observer.unregister ~rpc ~session_id ~self ~host
+        in
+        do_op_on ~__context ~host ~local_fn client_fn
+
+      let destroy ~__context ~self =
+        info "Observer.destroy: self=%s" (observer_uuid ~__context self) ;
+        let hosts = Db.Observer.get_hosts ~__context ~self in
+        let local_fn =
+          Local.Observer.register ~self ~host:(Helpers.get_localhost ~__context)
+        in
+        Xapi_observer.observed_hosts_of ~__context hosts
+        |> List.iter (fun host ->
+               do_op_on ~__context ~host ~local_fn (fun session_id rpc ->
+                   Client.Observer.unregister ~rpc ~session_id ~self ~host
+               )
+           ) ;
+        Local.Observer.destroy ~__context ~self
+
+      let set_hosts ~__context ~self ~value =
+        let uuid = observer_uuid ~__context self in
+        info "Observer.set_hosts: self=%s value=%s" uuid
+          (String.concat ";" (List.map (host_uuid ~__context) value)) ;
+        Local.Observer.set_hosts ~__context ~self ~value ;
+        let new_hosts =
+          RefSet.of_list (Xapi_observer.observed_hosts_of ~__context value)
+        in
+        let old_hosts =
+          RefSet.of_list
+            (Xapi_observer.observed_hosts_of ~__context
+               (Db.Observer.get_hosts ~__context ~self)
+            )
+        in
+        let to_add = RefSet.diff new_hosts old_hosts in
+        let to_remove = RefSet.diff old_hosts new_hosts in
+        let localhost = Helpers.get_localhost ~__context in
+        let local_fn = Local.Observer.unregister ~self ~host:localhost in
+        RefSet.iter
+          (fun host ->
+            do_op_on ~__context ~host ~local_fn (fun session_id rpc ->
+                Client.Observer.unregister ~rpc ~session_id ~self ~host
+            )
+          )
+          to_remove ;
+        let local_fn = Local.Observer.register ~self ~host:localhost in
+        RefSet.iter
+          (fun host ->
+            do_op_on ~__context ~host ~local_fn (fun session_id rpc ->
+                Client.Observer.register ~rpc ~session_id ~self ~host
+            )
+          )
+          to_add
+
+      let set_enabled ~__context ~self ~value =
+        info "Observer.set_enabled: self=%s value=%B"
+          (observer_uuid ~__context self)
+          value ;
+        let fn ~rpc ~session_id ~host:_ =
+          Client.Observer.set_enabled ~rpc ~session_id ~self ~value
+        in
+        Xapi_pool_helpers.call_fn_on_slaves_then_master ~__context fn
+
+      let set_attributes ~__context ~self ~value =
+        (* attributes will be kept out of the logs *)
+        info "Observer.set_attributes: self=%s" (observer_uuid ~__context self) ;
+        let fn ~rpc ~session_id ~host:_ =
+          Client.Observer.set_attributes ~rpc ~session_id ~self ~value
+        in
+        Xapi_pool_helpers.call_fn_on_slaves_then_master ~__context fn
+
+      let set_endpoints ~__context ~self ~value =
+        (* endpoints will be kept out of the logs *)
+        info "Observer.set_endpoints: self=%s" (observer_uuid ~__context self) ;
+        let fn ~rpc ~session_id ~host:_ =
+          Client.Observer.set_endpoints ~rpc ~session_id ~self ~value
+        in
+        Xapi_pool_helpers.call_fn_on_slaves_then_master ~__context fn
+
+      let set_components ~__context ~self ~value =
+        info "Observer.set_components: self=%s value=%s"
+          (observer_uuid ~__context self)
+          (value |> String.concat ",") ;
+        let fn ~rpc ~session_id ~host:_ =
+          Client.Observer.set_components ~rpc ~session_id ~self ~value
+        in
+        Xapi_pool_helpers.call_fn_on_slaves_then_master ~__context fn
     end
   end

commit deee3b86ef4d0f8b7c674707c3132599022c6fbc

Tracing: bump database schema
This is needed to fix a XAPI startup error after the database schema has been
changed.

Fixes: 604cfbd58 ("CP-41842 Created Observer class with IDL functions to manage Open Telemetry providers")
Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/idl/datamodel_common.ml
@@ -10,7 +10,7 @@ open Datamodel_roles
               to leave a gap for potential hotfixes needing to increment the schema version.*)
 let schema_major_vsn = 5
 
-let schema_minor_vsn = 762
+let schema_minor_vsn = 763
 
 (* Historical schema versions just in case this is useful later *)
 let rio_schema_major_vsn = 5