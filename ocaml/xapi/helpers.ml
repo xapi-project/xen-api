xapi-project/xen-api

commit 0ac9dd7c87539d3c213132e489aee7cf53fa577d

CP-42013: Do not apply recommended guidances automatically
1. add a new field "recommended_guidances" in both host and vm
   datamodel
2. return "recommended_guidances" together with "warnings" as update
   applying results from host.apply_updates
3. add API host.apply_recommended_guidances
4. rename "immediate_guidances" to "recommended_guidances"

Signed-off-by: Gang Ji <gang.ji@citrix.com>

ocaml/xapi/message_forwarding.ml
@@ -3999,6 +3999,14 @@ functor
         do_op_on ~local_fn ~__context ~host:self (fun session_id rpc ->
             Client.Host.set_https_only ~rpc ~session_id ~self ~value
         )
+
+      let apply_recommended_guidances ~__context ~self =
+        let uuid = host_uuid ~__context self in
+        info "Host.apply_recommended_guidance: host = %s" uuid ;
+        let local_fn = Local.Host.apply_recommended_guidances ~self in
+        do_op_on ~local_fn ~__context ~host:self (fun session_id rpc ->
+            Client.Host.apply_recommended_guidances ~rpc ~session_id ~self
+        )
     end
 
     module Host_crashdump = struct

ocaml/xapi/xapi_host.ml
@@ -1047,7 +1047,8 @@ let create ~__context ~uuid ~name_label ~name_description:_ ~hostname ~address
       )
     ~control_domain:Ref.null ~updates_requiring_reboot:[] ~iscsi_iqn:""
     ~multipathing:false ~uefi_certificates:"" ~editions:[] ~pending_guidances:[]
-    ~tls_verification_enabled ~last_software_update:Date.never ;
+    ~tls_verification_enabled ~last_software_update:Date.never
+    ~recommended_guidances:[] ;
   (* If the host we're creating is us, make sure its set to live *)
   Db.Host_metrics.set_last_updated ~__context ~self:metrics
     ~value:(Date.of_float (Unix.gettimeofday ())) ;
@@ -2999,11 +3000,15 @@ let apply_updates ~__context ~self ~hash =
   in
   Db.Host.set_last_software_update ~__context ~self
     ~value:(get_servertime ~__context ~host:self) ;
-  Repository.apply_immediate_guidances ~__context ~host:self ~guidances ;
-  (* The warnings may not be returned to async caller if this happens
-   * on the coordinator host and the 'restartToolstack' is required.
-   *)
-  warnings
+  List.map
+    (fun g ->
+      [
+        Api_errors.updates_require_recommended_guidance
+      ; Updateinfo.Guidance.to_string g
+      ]
+    )
+    guidances
+  @ warnings
 
 let cc_prep () =
   let cc = "CC_PREPARATIONS" in
@@ -3032,3 +3037,59 @@ let set_https_only ~__context ~self ~value =
   | true ->
       (* it is illegal changing the firewall/https config in CC/FIPS mode *)
       raise (Api_errors.Server_error (Api_errors.illegal_in_fips_mode, []))
+
+let try_restart_device_models_for_recommended_guidances ~__context ~host =
+  (* Restart device models of all running HVM VMs on the host by doing
+   * local migrations if it is required by recommended guidances. *)
+  Repository_helpers.do_with_device_models ~__context ~host
+  @@ fun (ref, record) ->
+  match
+    ( List.mem `restart_device_model
+        (Db.VM.get_recommended_guidances ~__context ~self:ref)
+    , record.API.vM_power_state
+    , Helpers.has_qemu_currently ~__context ~self:ref
+    )
+  with
+  | true, `Running, true ->
+      Xapi_vm_migrate.pool_migrate ~__context ~vm:ref ~host
+        ~options:[("live", "true")] ;
+      None
+  | true, `Paused, true ->
+      error "VM 'ref=%s' is paused, can't restart device models for it"
+        (Ref.string_of ref) ;
+      Some ref
+  | _ ->
+      (* No `restart_device_model as recommended guidance for this VM or no
+       * device models are running for this VM *)
+      None
+
+let apply_recommended_guidances ~__context ~self =
+  try
+    let open Updateinfo in
+    Db.Host.get_recommended_guidances ~__context ~self |> function
+    | [] ->
+        try_restart_device_models_for_recommended_guidances ~__context
+          ~host:self
+    | [`reboot_host] ->
+        reboot ~__context ~host:self
+    | [`restart_toolstack] ->
+        try_restart_device_models_for_recommended_guidances ~__context
+          ~host:self ;
+        restart_agent ~__context ~host:self
+    | l ->
+        let host' = Ref.string_of self in
+        error
+          "Found wrong guidance(s) when applying recommended guidances on host \
+           ref='%s': %s"
+          host'
+          (String.concat ";"
+             (List.map Guidance.to_string
+                (List.map Guidance.of_update_guidance l)
+             )
+          ) ;
+        raise Api_errors.(Server_error (apply_guidance_failed, [host']))
+  with e ->
+    let host' = Ref.string_of self in
+    error "applying recommended guidances on host ref='%s' failed: %s" host'
+      (ExnHelper.string_of_exn e) ;
+    raise Api_errors.(Server_error (apply_guidance_failed, [host']))

ocaml/idl/datamodel_errors.ml
@@ -1941,6 +1941,8 @@ let _ =
     ~doc:"The repository proxy username/password is invalid." () ;
   error Api_errors.apply_livepatch_failed ["livepatch"]
     ~doc:"Failed to apply a livepatch." () ;
+  error Api_errors.updates_require_recommended_guidance ["recommended_guidance"]
+    ~doc:"Requires recommended guidance after applying updates." () ;
   error Api_errors.update_guidance_changed ["guidance"]
     ~doc:"Guidance for the update has changed" () ;
 

ocaml/idl/datamodel_host.ml
@@ -1709,7 +1709,10 @@ let apply_updates =
         )
       ]
     ~result:
-      (Set (Set String), "The list of warnings happened in applying updates")
+      ( Set (Set String)
+      , "The list of results after applying updates, including livepatch apply \
+         failures and recommended guidances"
+      )
     ~allowed_roles:(_R_POOL_OP ++ _R_CLIENT_CERT)
     ()
 
@@ -1739,6 +1742,21 @@ let set_https_only =
         , "value"
         , "true - http port 80 will be blocked, false - http port 80 will be \
            open"
+        )
+      ]
+    ~allowed_roles:_R_POOL_OP ()
+
+let apply_recommended_guidances =
+  call ~name:"apply_recommended_guidances"
+    ~doc:
+      "apply all recommended guidances both on the host and on all HVM VMs on \
+       the host after updates are applied on the host"
+    ~lifecycle:[]
+    ~params:
+      [
+        ( Ref _host
+        , "self"
+        , "The host whose recommended guidances will be applied"
         )
       ]
     ~allowed_roles:_R_POOL_OP ()
@@ -1877,6 +1895,7 @@ let t =
       ; apply_updates
       ; copy_primary_host_certs
       ; set_https_only
+      ; apply_recommended_guidances
       ]
     ~contents:
       ([
@@ -2098,6 +2117,9 @@ let t =
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:Bool
             ~default_value:(Some (VBool false)) "https_only"
             "Reflects whether port 80 is open (false) or not (true)"
+        ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:(Set update_guidances)
+            "recommended_guidances" ~default_value:(Some (VSet []))
+            "The set of recommended guidances after applying updates"
         ]
       )
     ()

ocaml/idl/datamodel_vm.ml
@@ -2141,6 +2141,9 @@ let t =
             ~ty:(Set update_guidances) "pending_guidances"
             ~default_value:(Some (VSet []))
             "The set of pending guidances after applying updates"
+        ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:(Set update_guidances)
+            "recommended_guidances" ~default_value:(Some (VSet []))
+            "The set of recommended guidances after applying updates"
         ]
       )
     ()

ocaml/idl/schematest.ml
@@ -2,7 +2,7 @@ let hash x = Digest.string x |> Digest.to_hex
 
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
-let last_known_schema_hash = "37049c34c08a2494d23606ec9585328f"
+let last_known_schema_hash = "52dba8fb7b9be91d45f8b3d96ae87f1d"
 
 let current_schema_hash : string =
   let open Datamodel_types in

ocaml/tests/common/test_common.ml
@@ -209,7 +209,8 @@ let make_host2 ~__context ?(ref = Ref.make ()) ?(uuid = make_uuid ())
     ~control_domain:Ref.null ~updates_requiring_reboot:[] ~iscsi_iqn:""
     ~multipathing:false ~uefi_certificates:"" ~editions:[] ~pending_guidances:[]
     ~tls_verification_enabled
-    ~last_software_update:(Xapi_host.get_servertime ~__context ~host:ref) ;
+    ~last_software_update:(Xapi_host.get_servertime ~__context ~host:ref)
+    ~recommended_guidances:[] ;
   ref
 
 let make_pif ~__context ~network ~host ?(device = "eth0")

ocaml/xapi-cli-server/cli_frontend.ml
@@ -1036,6 +1036,15 @@ let rec cmdtable_data : (string * cmd_spec) list =
       ; flags= [Host_selectors]
       }
     )
+  ; ( "host-apply-recommended-guidances"
+    , {
+        reqd= []
+      ; optn= []
+      ; help= "Apply recommended guidances on a host."
+      ; implementation= No_fd Cli_operations.host_apply_recommended_guidances
+      ; flags= [Host_selectors]
+      }
+    )
   ; ( "patch-upload"
     , {
         reqd= ["file-name"]

commit 2c3774ea2e8172e953a272216669c6c7abf48b0f

CA-376144: handle host.apply_recommended_guidances first on pool coordinator
Instead of proxying the request directly to pool member, first handler the
request on pool coordinator, only proxy "VM.pool_migrate", "Host.reboot",
"Host.restart_agent" to pool member.

This resolves issue introduced in 0ac9dd7:
CP-42013: Do not apply recommended guidances automatically

Signed-off-by: Gang Ji <gang.ji@citrix.com>

ocaml/xapi/helpers.ml
@@ -823,6 +823,13 @@ let is_pool_master ~__context ~host =
   let master_id = Db.Host.get_uuid ~__context ~self:master in
   host_id = master_id
 
+let assert_we_are_master ~__context =
+  if not (is_pool_master ~__context ~host:(get_localhost ~__context)) then
+    raise
+      Api_errors.(
+        Server_error (host_is_slave, [Pool_role.get_master_address ()])
+      )
+
 (* Host version compare helpers *)
 let compare_int_lists : int list -> int list -> int =
  fun a b ->

ocaml/xapi/message_forwarding.ml
@@ -4002,11 +4002,8 @@ functor
 
       let apply_recommended_guidances ~__context ~self =
         let uuid = host_uuid ~__context self in
-        info "Host.apply_recommended_guidance: host = %s" uuid ;
-        let local_fn = Local.Host.apply_recommended_guidances ~self in
-        do_op_on ~local_fn ~__context ~host:self (fun session_id rpc ->
-            Client.Host.apply_recommended_guidances ~rpc ~session_id ~self
-        )
+        info "Host.apply_recommended_guidances: host = %s" uuid ;
+        Local.Host.apply_recommended_guidances ~__context ~self
     end
 
     module Host_crashdump = struct

ocaml/xapi/xapi_host.ml
@@ -2985,6 +2985,7 @@ let get_host_updates_handler (req : Http.Request.t) s _ =
 
 let apply_updates ~__context ~self ~hash =
   (* This function runs on master host *)
+  Helpers.assert_we_are_master ~__context ;
   Pool_features.assert_enabled ~__context ~f:Features.Updates ;
   let guidances, warnings =
     Xapi_pool_helpers.with_pool_operation ~__context
@@ -3039,8 +3040,10 @@ let set_https_only ~__context ~self ~value =
       raise (Api_errors.Server_error (Api_errors.illegal_in_fips_mode, []))
 
 let try_restart_device_models_for_recommended_guidances ~__context ~host =
-  (* Restart device models of all running HVM VMs on the host by doing
-   * local migrations if it is required by recommended guidances. *)
+  (* This function runs on master host: restart device models of all running
+   * HVM VMs on the host by doing local migrations if it is required by
+   * recommended guidances. *)
+  Helpers.assert_we_are_master ~__context ;
   Repository_helpers.do_with_device_models ~__context ~host
   @@ fun (ref, record) ->
   match
@@ -3051,8 +3054,10 @@ let try_restart_device_models_for_recommended_guidances ~__context ~host =
     )
   with
   | true, `Running, true ->
-      Xapi_vm_migrate.pool_migrate ~__context ~vm:ref ~host
-        ~options:[("live", "true")] ;
+      Helpers.call_api_functions ~__context (fun rpc session_id ->
+          Client.Client.VM.pool_migrate ~rpc ~session_id ~vm:ref ~host
+            ~options:[("live", "true")]
+      ) ;
       None
   | true, `Paused, true ->
       error "VM 'ref=%s' is paused, can't restart device models for it"
@@ -3063,21 +3068,25 @@ let try_restart_device_models_for_recommended_guidances ~__context ~host =
        * device models are running for this VM *)
       None
 
-let apply_recommended_guidances ~__context ~self =
+let apply_recommended_guidances ~__context ~self:host =
+  (* This function runs on master host *)
+  Helpers.assert_we_are_master ~__context ;
   try
     let open Updateinfo in
-    Db.Host.get_recommended_guidances ~__context ~self |> function
+    Db.Host.get_recommended_guidances ~__context ~self:host |> function
     | [] ->
-        try_restart_device_models_for_recommended_guidances ~__context
-          ~host:self
+        try_restart_device_models_for_recommended_guidances ~__context ~host
     | [`reboot_host] ->
-        reboot ~__context ~host:self
+        Helpers.call_api_functions ~__context (fun rpc session_id ->
+            Client.Client.Host.reboot ~rpc ~session_id ~host
+        )
     | [`restart_toolstack] ->
-        try_restart_device_models_for_recommended_guidances ~__context
-          ~host:self ;
-        restart_agent ~__context ~host:self
+        try_restart_device_models_for_recommended_guidances ~__context ~host ;
+        Helpers.call_api_functions ~__context (fun rpc session_id ->
+            Client.Client.Host.restart_agent ~rpc ~session_id ~host
+        )
     | l ->
-        let host' = Ref.string_of self in
+        let host' = Ref.string_of host in
         error
           "Found wrong guidance(s) when applying recommended guidances on host \
            ref='%s': %s"
@@ -3089,7 +3098,7 @@ let apply_recommended_guidances ~__context ~self =
           ) ;
         raise Api_errors.(Server_error (apply_guidance_failed, [host']))
   with e ->
-    let host' = Ref.string_of self in
+    let host' = Ref.string_of host in
     error "applying recommended guidances on host ref='%s' failed: %s" host'
       (ExnHelper.string_of_exn e) ;
     raise Api_errors.(Server_error (apply_guidance_failed, [host']))

ocaml/xapi/xapi_psr.ml
@@ -527,18 +527,6 @@ let start =
       if is_ha_enabled then
         raise Api_errors.(Server_error (ha_is_enabled, []))
     in
-    let assert_we_are_master () =
-      if
-        not
-          (Helpers.is_pool_master ~__context
-             ~host:(Helpers.get_localhost ~__context)
-          )
-      then
-        raise
-          Api_errors.(
-            Server_error (host_is_slave, [Pool_role.get_master_address ()])
-          )
-    in
     let assert_all_hosts_alive () =
       let live_hosts = Helpers.get_live_hosts ~__context |> HostSet.of_list in
       let all_hosts_list =
@@ -564,7 +552,7 @@ let start =
           set_up_psr_pending_flag (fun () ->
               Pool_features.assert_enabled ~__context
                 ~f:Features.Pool_secret_rotation ;
-              assert_we_are_master () ;
+              Helpers.assert_we_are_master ~__context ;
               Assert.master_state_valid () ;
               let[@warning "-8"] (master :: members) =
                 assert_all_hosts_alive ()