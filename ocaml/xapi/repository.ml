xapi-project/xen-api

commit 47bfe89e048c2dc41e19ede17a7bad7a44932938

CA-378778: Calculate host guidance correctly
When there remains 'recommended_guidances' or 'pending_guidances' from
previous update not applied for a host, the 'recommended_guidances' and
'pending_guidances' are not calculated correctly: the previous guidance
should be taken into account to calculate the new 'recommended_guidances'
and 'pending_guidances'.

---------

Signed-off-by: Gang Ji <gang.ji@citrix.com>

ocaml/xapi/repository.ml
@@ -641,12 +641,12 @@ let set_restart_device_models ~__context ~host ~kind =
   | `Running, true | `Paused, true -> (
     match kind with
     | Guidance.Absolute ->
-        Db.VM.add_pending_guidances ~__context ~self:ref
-          ~value:`restart_device_model ;
+        Db.VM.set_pending_guidances ~__context ~self:ref
+          ~value:[`restart_device_model] ;
         None
     | Guidance.Recommended ->
-        Db.VM.add_recommended_guidances ~__context ~self:ref
-          ~value:`restart_device_model ;
+        Db.VM.set_recommended_guidances ~__context ~self:ref
+          ~value:[`restart_device_model] ;
         None
   )
   | _ ->
@@ -656,39 +656,46 @@ let set_restart_device_models ~__context ~host ~kind =
 let set_pending_guidances ~__context ~host ~guidances =
   let open Guidance in
   guidances
-  |> List.iter (function
-       | RebootHost ->
-           Db.Host.add_pending_guidances ~__context ~self:host
-             ~value:`reboot_host
-       | RebootHostOnLivePatchFailure ->
-           Db.Host.add_pending_guidances ~__context ~self:host
-             ~value:`reboot_host_on_livepatch_failure
-       | RestartToolstack ->
-           Db.Host.add_pending_guidances ~__context ~self:host
-             ~value:`restart_toolstack
-       | RestartDeviceModel ->
-           set_restart_device_models ~__context ~host ~kind:Absolute
-       | g ->
-           warn "Unsupported pending guidance %s, ignoring it."
-             (Guidance.to_string g)
+  |> List.fold_left
+       (fun acc g ->
+         match g with
+         | RebootHost ->
+             `reboot_host :: acc
+         | RebootHostOnLivePatchFailure ->
+             `reboot_host_on_livepatch_failure :: acc
+         | RestartToolstack ->
+             `restart_toolstack :: acc
+         | RestartDeviceModel ->
+             set_restart_device_models ~__context ~host ~kind:Absolute ;
+             acc
+         | g ->
+             warn "Unsupported pending guidance %s, ignoring it."
+               (Guidance.to_string g) ;
+             acc
        )
+       []
+  |> fun gs -> Db.Host.set_pending_guidances ~__context ~self:host ~value:gs
 
 let set_recommended_guidances ~__context ~host ~guidances =
   let open Guidance in
   guidances
-  |> List.iter (function
-       | RebootHost ->
-           Db.Host.add_recommended_guidances ~__context ~self:host
-             ~value:`reboot_host
-       | RestartToolstack ->
-           Db.Host.add_recommended_guidances ~__context ~self:host
-             ~value:`restart_toolstack
-       | RestartDeviceModel ->
-           set_restart_device_models ~__context ~host ~kind:Recommended
-       | g ->
-           warn "Unsupported recommended guidance %s, ignoring it."
-             (Guidance.to_string g)
+  |> List.fold_left
+       (fun acc g ->
+         match g with
+         | RebootHost ->
+             `reboot_host :: acc
+         | RestartToolstack ->
+             `restart_toolstack :: acc
+         | RestartDeviceModel ->
+             set_restart_device_models ~__context ~host ~kind:Absolute ;
+             acc
+         | g ->
+             warn "Unsupported recommended guidance %s, ignoring it."
+               (Guidance.to_string g) ;
+             acc
        )
+       []
+  |> fun gs -> Db.Host.set_recommended_guidances ~__context ~self:host ~value:gs
 
 let apply_livepatches' ~__context ~host ~livepatches =
   List.partition_map
@@ -749,18 +756,27 @@ let apply_updates' ~__context ~host ~updates_info ~livepatches ~acc_rpm_updates
       ) ;
   (* Evaluate recommended/pending guidances *)
   let recommended_guidances, pending_guidances =
-    let recommended_guidances' =
+    let new_recommended_gs =
       (* EvacuateHost will be applied before applying updates *)
       eval_guidances ~updates_info ~updates:acc_rpm_updates ~kind:Recommended
         ~livepatches:successful_livepatches ~failed_livepatches
       |> List.filter (fun g -> g <> Guidance.EvacuateHost)
     in
+    let recommended_guidances' =
+      merge_with_unapplied_guidances ~__context ~host ~kind:Recommended
+        ~guidances:new_recommended_gs
+    in
 
-    let pending_guidances' =
+    let new_pending_gs =
       eval_guidances ~updates_info ~updates:acc_rpm_updates ~kind:Absolute
         ~livepatches:[] ~failed_livepatches:[]
       |> List.filter (fun g -> not (List.mem g recommended_guidances'))
     in
+    let pending_guidances' =
+      merge_with_unapplied_guidances ~__context ~host ~kind:Absolute
+        ~guidances:new_pending_gs
+    in
+
     match failed_livepatches with
     | [] ->
         (* No livepatch should be applicable now *)

ocaml/xapi/repository_helpers.ml
@@ -650,6 +650,20 @@ let eval_guidances ~updates_info ~updates ~kind ~livepatches ~failed_livepatches
   |> GuidanceSet.resort_guidances ~kind
   |> GuidanceSet.elements
 
+let merge_with_unapplied_guidances ~__context ~host ~kind ~guidances =
+  let open GuidanceSet in
+  ( match kind with
+  | Guidance.Absolute ->
+      Db.Host.get_pending_guidances ~__context ~self:host
+  | Guidance.Recommended ->
+      Db.Host.get_recommended_guidances ~__context ~self:host
+  )
+  |> List.map (fun g -> Guidance.of_update_guidance g)
+  |> of_list
+  |> union (of_list guidances)
+  |> resort_guidances ~kind
+  |> elements
+
 let repoquery_sep = ":|"
 
 let get_repoquery_fmt () =

commit c705bc4428c206cbde2da44273d72727ddc85703

CA-380043: VM recommended guidances is not set correctly
This fixes an issue introduced in 47bfe89e:
CA-378778: Calculate host guidance correctly
where the "recommended_guidances" was set into "pending_guidances" by mistake.

Signed-off-by: Gang Ji <gang.ji@citrix.com>

ocaml/xapi/repository.ml
@@ -687,7 +687,7 @@ let set_recommended_guidances ~__context ~host ~guidances =
          | RestartToolstack ->
              `restart_toolstack :: acc
          | RestartDeviceModel ->
-             set_restart_device_models ~__context ~host ~kind:Absolute ;
+             set_restart_device_models ~__context ~host ~kind:Recommended ;
              acc
          | g ->
              warn "Unsupported recommended guidance %s, ignoring it."