fc91f125db5e2cd58b5e9a1c760d9de96b338e30
Christian Lindig <christian.lindig@cloud.com>
2023-08-08 14:28:24 +0100

    CA-379112 update comments in message_forwarding.ml
    Update comment that explains the order of operations.
    
    Signed-off-by: Christian Lindig <christian.lindig@cloud.com>

--- ocaml/xapi/message_forwarding.ml
+++ ocaml/xapi/message_forwarding.ml
@@ -5760,8 +5760,10 @@ functor
         ) ;
         (* We always plug the master PBD first and unplug it last. If this is the
          * first PBD plugged for this SR (proxy: the PBD being plugged is for the
-         * master) then we should perform an initial SR scan and perform some
-         * asynchronous start-of-day operations in the callback.
+         * master) then we should perform an initial SR scan. CA-379112
+         * changed the scan from asynchronous to synchronous as
+         * otherwise on return the xapi DB does not have the correct
+         * information.
          * Note the current context contains a completed real task and we should
          * not reuse it for what is effectively another call. *)
         if is_master_pbd then
