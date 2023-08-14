xapi-project/xen-api

commit 3d039f3259a39a96c9f36dd0c9b6a2036626f78c

CA-380580: cross-pool migration: no CPU checks for halted VMs
CPU checks are needed only for running VMs that are being migrated, to
check for compatibility with the remote-host's CPUs.

Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

ocaml/xapi/xapi_vm_migrate.ml
@@ -1182,6 +1182,7 @@ let migrate_send' ~__context ~vm ~dest ~live:_ ~vdi_map ~vif_map ~vgpu_map
      We look at the VDIs of the VM, the VDIs of all of the snapshots, and any
      suspend-image VDIs. *)
   let vm_uuid = Db.VM.get_uuid ~__context ~self:vm in
+  let power_state = Db.VM.get_power_state ~__context ~self:vm in
   let vbds = Db.VM.get_VBDs ~__context ~self:vm in
   let vifs = Db.VM.get_VIFs ~__context ~self:vm in
   let snapshots = Db.VM.get_snapshots ~__context ~self:vm in
@@ -1240,7 +1241,7 @@ let migrate_send' ~__context ~vm ~dest ~live:_ ~vdi_map ~vif_map ~vgpu_map
   let suspends_vdis =
     List.fold_left
       (fun acc vm ->
-        if Db.VM.get_power_state ~__context ~self:vm = `Suspended then
+        if power_state = `Suspended then
           let vdi = Db.VM.get_suspend_VDI ~__context ~self:vm in
           let sr = Db.VDI.get_SR ~__context ~self:vdi in
           if
@@ -1456,7 +1457,7 @@ let migrate_send' ~__context ~vm ~dest ~live:_ ~vdi_map ~vif_map ~vgpu_map
             in
             inter_pool_metadata_transfer ~__context ~remote ~vm ~vdi_map
               ~vif_map ~vgpu_map ~dry_run:false ~live:true ~copy
-              ~check_cpu:(not force)
+              ~check_cpu:((not force) && power_state <> `Halted)
           in
           let vm = List.hd vms in
           let () =
@@ -1833,7 +1834,7 @@ let assert_can_migrate ~__context ~vm ~dest ~live:_ ~vdi_map ~vif_map ~options
           not
             (inter_pool_metadata_transfer ~__context ~remote ~vm ~vdi_map
                ~vif_map ~vgpu_map ~dry_run:true ~live:true ~copy
-               ~check_cpu:(not force)
+               ~check_cpu:((not force) && power_state <> `Halted)
             = []
             )
         then

commit 3c613a0509c39436b90151374c4c363e7f3b208b

CA-380789: Not get power_state from snapshots with suspend VDIs
This commit is to fix an issue which was introduced by 3d039f3.
In VM migration, the suspend_vdis will be get from snapshots the
power_state of which is `Suspended. The individual snapshots are
represented as VM records in XAPI as well. They are different with the
base VM record.

This commit reverts this part to the logic prior to 3d039f3.

Signed-off-by: Ming Lu <ming.lu@cloud.com>

ocaml/xapi/xapi_vm_migrate.ml
@@ -1241,7 +1241,7 @@ let migrate_send' ~__context ~vm ~dest ~live:_ ~vdi_map ~vif_map ~vgpu_map
   let suspends_vdis =
     List.fold_left
       (fun acc vm ->
-        if power_state = `Suspended then
+        if Db.VM.get_power_state ~__context ~self:vm = `Suspended then
           let vdi = Db.VM.get_suspend_VDI ~__context ~self:vm in
           let sr = Db.VDI.get_SR ~__context ~self:vdi in
           if