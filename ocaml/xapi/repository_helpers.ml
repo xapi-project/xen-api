aa9416c70a83e2d94bfbdad70b078fe8ab8c12f5
Ming Lu <ming.lu@cloud.com>
2023-08-18 11:18:25 +0800

    CA-381133: Remove usage of host|VM.recommended_guidances
    Signed-off-by: Ming Lu <ming.lu@cloud.com>

--- ocaml/xapi/repository_helpers.ml
+++ ocaml/xapi/repository_helpers.ml
@@ -650,18 +650,14 @@ let eval_guidances ~updates_info ~updates ~kind ~livepatches ~failed_livepatches
   |> GuidanceSet.resort_guidances ~kind
   |> GuidanceSet.elements
 
-let merge_with_unapplied_guidances ~__context ~host ~kind ~guidances =
+let merge_with_unapplied_guidances ~__context ~host ~guidances =
   let open GuidanceSet in
-  ( match kind with
-  | Guidance.Absolute ->
-      Db.Host.get_pending_guidances ~__context ~self:host
-  | Guidance.Recommended ->
-      Db.Host.get_recommended_guidances ~__context ~self:host
-  )
+  Db.Host.get_pending_guidances ~__context ~self:host
   |> List.map (fun g -> Guidance.of_update_guidance g)
+  |> List.filter (fun g -> g <> Guidance.RebootHostOnLivePatchFailure)
   |> of_list
   |> union (of_list guidances)
-  |> resort_guidances ~kind
+  |> resort_guidances ~kind:Guidance.Recommended
   |> elements
 
 let repoquery_sep = ":|"
