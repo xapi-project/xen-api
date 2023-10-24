xapi-project/xen-api

commit a5e99ec2fab765d102cfe31a1551ca6dcbc5b3c2

Install suspend_image_viewer
This is useful for debugging. It cannot be easily run out of Dom0 due to its dependency on
verify-stream-v2 provided by Xen.

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

Makefile
@@ -116,6 +116,8 @@ install: build doc sdk doc-json
 	scripts/install.sh 755 _build/install/default/bin/vncproxy $(DESTDIR)$(OPTDIR)/debug/vncproxy
 # ocaml/perftest
 	scripts/install.sh 755 _build/install/default/bin/perftest $(DESTDIR)$(OPTDIR)/debug/perftest
+# ocaml/suspend_image_viewer
+	scripts/install.sh 755 _build/install/default/bin/perftest $(DESTDIR)$(OPTDIR)/debug/suspend_image_viewer
 # ocaml/mpathalert
 	scripts/install.sh 755 _build/install/default/bin/mpathalert $(DESTDIR)$(OPTDIR)/bin/mpathalert
 # ocaml/license

ocaml/xenopsd/suspend_image_viewer/dune
@@ -1,5 +1,6 @@
 (executable
- (name suspend_image_viewer)
+ (public_name suspend_image_viewer)
+ (package xapi-xenopsd-xc)
  (libraries
   cmdliner
   forkexec

commit 775f9a4782637a896bc8d847004640104ebcce35

Fix suspend-image-viewer binary
Fixes: a5e99ec2f ("Install suspend_image_viewer")
Signed-off-by: Edwin Török <edwin.torok@cloud.com>

Makefile
@@ -119,7 +119,7 @@ install: build doc sdk doc-json
 # ocaml/perftest
 	scripts/install.sh 755 _build/install/default/bin/perftest $(DESTDIR)$(OPTDIR)/debug/perftest
 # ocaml/suspend-image-viewer
-	scripts/install.sh 755 _build/install/default/bin/perftest $(DESTDIR)$(OPTDIR)/debug/suspend-image-viewer
+	scripts/install.sh 755 _build/install/default/bin/suspend-image-viewer $(DESTDIR)$(OPTDIR)/debug/suspend-image-viewer
 # ocaml/mpathalert
 	scripts/install.sh 755 _build/install/default/bin/mpathalert $(DESTDIR)$(OPTDIR)/bin/mpathalert
 # ocaml/license