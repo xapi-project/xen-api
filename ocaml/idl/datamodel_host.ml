23ab5690225072e9c267fb61dc267818d7c9df9e
Gang Ji <gang.ji@citrix.com>
2023-12-04 16:44:34 +0800

    add more description for the new pending fields
    Signed-off-by: Gang Ji <gang.ji@citrix.com>

--- ocaml/idl/datamodel_host.ml
+++ ocaml/idl/datamodel_host.ml
@@ -2142,7 +2142,8 @@ let t =
         ; field ~qualifier:DynamicRO ~in_product_since:"1.303.0"
             ~ty:(Set update_guidances) "pending_guidances"
             ~default_value:(Some (VSet []))
-            "The set of pending mandatory guidances after applying updates"
+            "The set of pending mandatory guidances after applying updates, \
+             which must be applied, as otherwise there may be e.g. VM failures"
         ; field ~qualifier:DynamicRO ~in_product_since:"1.313.0" ~ty:Bool
             "tls_verification_enabled" ~default_value:(Some (VBool false))
             "True if this host has TLS verifcation enabled"
@@ -2166,10 +2167,14 @@ let t =
              updates synced from remote CDN, otherwise 'no'"
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:(Set update_guidances)
             "pending_guidances_recommended" ~default_value:(Some (VSet []))
-            "The set of pending recommended guidances after applying updates"
+            "The set of pending recommended guidances after applying updates, \
+             which we suggest that most users should follow, but if not \
+             followed, will not cause a failure"
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:(Set update_guidances)
             "pending_guidances_full" ~default_value:(Some (VSet []))
-            "The set of pending full guidances after applying updates"
+            "The set of pending full guidances after applying updates, which a \
+             user should follow if they want to make the update fully \
+             effective, but the 'average user' doesn't need to"
         ]
       )
     ()
