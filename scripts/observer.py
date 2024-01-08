5b828c6a9ae074f863ef014163c04dde35691851
Steven Woods <steven.woods@citrix.com>
2024-01-08 17:17:04 +0000

    CP-46374: Add a minimal observer.py
    Signed-off-by: Steven Woods <steven.woods@citrix.com>

---
+++ scripts/observer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+  # run a program passed as parameter, with its original arguments
+  import runpy
+  import sys
+
+  # shift all the argvs one left
+  sys.argv = sys.argv[1:]
+  argv0=sys.argv[0]
+
+  def run(file):
+    runpy.run_path(file, run_name='__main__')
+
+  run(argv0)
