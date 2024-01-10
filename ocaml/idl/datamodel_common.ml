xapi-project/xen-api

commit d557b697aee89bc4ae99a40ac70953e681e9fce6

CP-38020: introduce Host.numa_affinity_policy
The default is still Any, but it can now be set to Best_effort at runtime,
without restarting the toolstack.
This won't change the placement of already running VMs though.

Use an explicit 'default' as the default, so that we can change the default in the future
without having to update the database, but still respect the user's choice if they have explicitly set a certain policy.
And if they haven't explicitly changed this then we'll override the default without changing the value in the DB.

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/idl/datamodel_common.ml
@@ -10,7 +10,7 @@ open Datamodel_roles
               to leave a gap for potential hotfixes needing to increment the schema version.*)
 let schema_major_vsn = 5
 
-let schema_minor_vsn = 769
+let schema_minor_vsn = 770
 
 (* Historical schema versions just in case this is useful later *)
 let rio_schema_major_vsn = 5

ocaml/idl/datamodel_host.ml
@@ -1668,6 +1668,36 @@ let cleanup_pool_secret =
       ]
     ~allowed_roles:_R_LOCAL_ROOT_ONLY ~hide_from_docs:true ()
 
+let host_numa_affinity_policy =
+  Enum
+    ( "host_numa_affinity_policy"
+    , [
+        ("any", "VMs are spread across all available NUMA nodes")
+      ; ( "best-effort"
+        , "VMs are placed on the smallest number of NUMA nodes that they fit \
+           using soft-pinning, but the policy doesn't guarantee a balanced \
+           placement, falling back to the 'any' policy."
+        )
+      ; ( "default"
+        , "Use the NUMA affinity policy that is the default for the current \
+           version"
+        )
+      ]
+    )
+
+let set_numa_affinity_policy =
+  call ~name:"set_numa_affinity_policy" ~lifecycle:[]
+    ~doc:"Set VM placement NUMA affinity policy"
+    ~params:
+      [
+        (Ref _host, "self", "The host")
+      ; ( host_numa_affinity_policy
+        , "value"
+        , "The NUMA affinity policy to apply to a host"
+        )
+      ]
+    ~allowed_roles:_R_POOL_ADMIN ()
+
 let host_sched_gran =
   Enum
     ( "host_sched_gran"
@@ -1925,6 +1955,7 @@ let t =
       ; cleanup_pool_secret
       ; set_sched_gran
       ; get_sched_gran
+      ; set_numa_affinity_policy
       ; emergency_disable_tls_verification
       ; emergency_reenable_tls_verification
       ; cert_distrib_atom
@@ -2164,6 +2195,9 @@ let t =
             ~default_value:(Some (VEnum "unknown"))
             "Default as 'unknown', 'yes' if the host is up to date with \
              updates synced from remote CDN, otherwise 'no'"
+        ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:host_numa_affinity_policy
+            "numa_affinity_policy" ~default_value:(Some (VEnum "default"))
+            "NUMA-aware VM memory and vCPU placement policy"
         ]
       )
     ()

ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "96da2c136aa13d4f8e71dd40fe2b84af"
+let last_known_schema_hash = "2ec2aeef2405d6bd73ab6beb44185532"
 
 let current_schema_hash : string =
   let open Datamodel_types in

ocaml/xapi-cli-server/record_util.ml
@@ -706,6 +706,14 @@ let host_sched_gran_to_string = function
   | `socket ->
       "socket"
 
+let host_numa_affinity_policy_to_string = function
+  | `any ->
+      "any"
+  | `besteffort ->
+      "best-effort"
+  | `default ->
+      "default"
+
 let pgpu_dom0_access_to_string x = host_display_to_string x
 
 let string_to_vdi_onboot s =

ocaml/xapi/xapi_host.ml
@@ -1039,6 +1039,7 @@ let create ~__context ~uuid ~name_label ~name_description:_ ~hostname ~address
     ~cpu_configuration:[] (* !!! FIXME hard coding *)
     ~cpu_info:[] ~chipset_info ~memory_overhead:0L
     ~sched_policy:"credit" (* !!! FIXME hard coding *)
+    ~numa_affinity_policy:`default
     ~supported_bootloaders:(List.map fst Xapi_globs.supported_bootloaders)
     ~suspend_image_sr:Ref.null ~crash_dump_sr:Ref.null ~logging:[] ~hostname
     ~address ~metrics ~license_params ~boot_free_mem:0L ~ha_statefiles:[]
@@ -2855,6 +2856,10 @@ let notify_send_new_pool_secret ~__context ~host:_ ~old_ps ~new_ps =
 
 let cleanup_pool_secret ~__context ~host:_ ~old_ps ~new_ps =
   Xapi_psr.cleanup ~__context ~old_ps ~new_ps
+
+let set_numa_affinity_policy ~__context ~self ~value =
+  Db.Host.set_numa_affinity_policy ~__context ~self ~value ;
+  Xapi_xenops.set_numa_affinity_policy ~__context ~value
 
 let set_sched_gran ~__context ~self ~value =
   if Helpers.get_localhost ~__context <> self then

ocaml/xapi/xapi_xenops.ml
@@ -3116,7 +3116,30 @@ let resync_all_vms ~__context =
   in
   List.iter (fun vm -> refresh_vm ~__context ~self:vm) resident_vms_in_db
 
+let set_numa_affinity_policy ~__context ~value =
+  let dbg = Context.string_of_task __context in
+  let open Xapi_xenops_queue in
+  let module Client = (val make_client (default_xenopsd ()) : XENOPS) in
+  let value =
+    let open Xenops_interface.Host in
+    match value with
+    | `any ->
+        Any
+    | `besteffort ->
+        Best_effort
+    | `default ->
+        Any
+  in
+  Client.HOST.set_numa_affinity_policy dbg value
+
 let on_xapi_restart ~__context =
+  let host = Helpers.get_localhost ~__context in
+  let value = Db.Host.get_numa_affinity_policy ~__context ~self:host in
+  info "Setting NUMA affinity policy in xenopsd on startup to %s"
+    (Record_util.host_numa_affinity_policy_to_string value) ;
+  set_numa_affinity_policy ~__context ~value ;
+
+  info "Resynchronizing VM state with xenopsd" ;
   resync_resident_on ~__context ;
   (* For all available xenopsds, start the event thread. This will cause
      events on everything xenopsd knows about, hence a refresh of all VMs. *)

ocaml/tests/common/test_common.ml
@@ -208,7 +208,7 @@ let make_host2 ~__context ?(ref = Ref.make ()) ?(uuid = make_uuid ())
     ~display:`enabled ~virtual_hardware_platform_versions:[]
     ~control_domain:Ref.null ~updates_requiring_reboot:[] ~iscsi_iqn:""
     ~multipathing:false ~uefi_certificates:"" ~editions:[] ~pending_guidances:[]
-    ~tls_verification_enabled
+    ~tls_verification_enabled ~numa_affinity_policy:`default
     ~last_software_update:(Xapi_host.get_servertime ~__context ~host:ref)
     ~recommended_guidances:[] ~latest_synced_updates_applied:`unknown ;
   ref

ocaml/xapi/message_forwarding.ml
@@ -4017,6 +4017,15 @@ functor
             Client.Host.get_sched_gran ~rpc ~session_id ~self
         )
 
+      let set_numa_affinity_policy ~__context ~self ~value =
+        info "Host.set_numa_affinity_policy: host='%s' policy='%s'"
+          (host_uuid ~__context self)
+          (Record_util.host_numa_affinity_policy_to_string value) ;
+        let local_fn = Local.Host.set_numa_affinity_policy ~self ~value in
+        do_op_on ~local_fn ~__context ~host:self (fun session_id rpc ->
+            Client.Host.set_numa_affinity_policy ~rpc ~session_id ~self ~value
+        )
+
       let emergency_disable_tls_verification ~__context =
         info "Host.emergency_disable_tls_verification" ;
         Local.Host.emergency_disable_tls_verification ~__context

commit e22b30d0f33dac671d89dfd8374ccce458e63ea9

fix(NUMA): 'default' is a keyword in some SDK languages
'auto' is a keyword too.
Lets use 'default-policy' instead.

Fixes: d557b697a ("CP-38020: introduce Host.numa_affinity_policy")
Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/idl/datamodel_common.ml
@@ -10,7 +10,7 @@ open Datamodel_roles
               to leave a gap for potential hotfixes needing to increment the schema version.*)
 let schema_major_vsn = 5
 
-let schema_minor_vsn = 770
+let schema_minor_vsn = 771
 
 (* Historical schema versions just in case this is useful later *)
 let rio_schema_major_vsn = 5

ocaml/idl/datamodel_host.ml
@@ -1673,12 +1673,12 @@ let host_numa_affinity_policy =
     ( "host_numa_affinity_policy"
     , [
         ("any", "VMs are spread across all available NUMA nodes")
-      ; ( "best-effort"
+      ; ( "best_effort"
         , "VMs are placed on the smallest number of NUMA nodes that they fit \
            using soft-pinning, but the policy doesn't guarantee a balanced \
            placement, falling back to the 'any' policy."
         )
-      ; ( "default"
+      ; ( "default_policy"
         , "Use the NUMA affinity policy that is the default for the current \
            version"
         )
@@ -2196,7 +2196,8 @@ let t =
             "Default as 'unknown', 'yes' if the host is up to date with \
              updates synced from remote CDN, otherwise 'no'"
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:host_numa_affinity_policy
-            "numa_affinity_policy" ~default_value:(Some (VEnum "default"))
+            "numa_affinity_policy"
+            ~default_value:(Some (VEnum "default_policy"))
             "NUMA-aware VM memory and vCPU placement policy"
         ]
       )

ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "2ec2aeef2405d6bd73ab6beb44185532"
+let last_known_schema_hash = "29a820cd4c81617f07b43c9b4f934317"
 
 let current_schema_hash : string =
   let open Datamodel_types in

ocaml/xapi-cli-server/record_util.ml
@@ -709,21 +709,23 @@ let host_sched_gran_to_string = function
 let host_numa_affinity_policy_to_string = function
   | `any ->
       "any"
-  | `besteffort ->
-      "best-effort"
-  | `default ->
-      "default"
+  | `best_effort ->
+      "best_effort"
+  | `default_policy ->
+      "default_policy"
 
 let host_numa_affinity_policy_of_string = function
   | "any" ->
       `any
-  | "best-effort" ->
-      `besteffort
-  | "default" ->
-      `default
+  | "best_effort" ->
+      `best_effort
+  | "default_policy" ->
+      `default_policy
   | s ->
       raise
-        (Record_failure ("Expected 'any', 'best-effort' or 'default', got " ^ s))
+        (Record_failure
+           ("Expected 'any', 'best_effort' or 'default_policy', got " ^ s)
+        )
 
 let pgpu_dom0_access_to_string x = host_display_to_string x
 

ocaml/xapi/xapi_host.ml
@@ -1039,7 +1039,7 @@ let create ~__context ~uuid ~name_label ~name_description:_ ~hostname ~address
     ~cpu_configuration:[] (* !!! FIXME hard coding *)
     ~cpu_info:[] ~chipset_info ~memory_overhead:0L
     ~sched_policy:"credit" (* !!! FIXME hard coding *)
-    ~numa_affinity_policy:`default
+    ~numa_affinity_policy:`default_policy
     ~supported_bootloaders:(List.map fst Xapi_globs.supported_bootloaders)
     ~suspend_image_sr:Ref.null ~crash_dump_sr:Ref.null ~logging:[] ~hostname
     ~address ~metrics ~license_params ~boot_free_mem:0L ~ha_statefiles:[]

ocaml/xapi/xapi_xenops.ml
@@ -3125,9 +3125,9 @@ let set_numa_affinity_policy ~__context ~value =
     match value with
     | `any ->
         Any
-    | `besteffort ->
+    | `best_effort ->
         Best_effort
-    | `default ->
+    | `default_policy ->
         Any
   in
   Client.HOST.set_numa_affinity_policy dbg value

ocaml/tests/common/test_common.ml
@@ -208,7 +208,7 @@ let make_host2 ~__context ?(ref = Ref.make ()) ?(uuid = make_uuid ())
     ~display:`enabled ~virtual_hardware_platform_versions:[]
     ~control_domain:Ref.null ~updates_requiring_reboot:[] ~iscsi_iqn:""
     ~multipathing:false ~uefi_certificates:"" ~editions:[] ~pending_guidances:[]
-    ~tls_verification_enabled ~numa_affinity_policy:`default
+    ~tls_verification_enabled ~numa_affinity_policy:`default_policy
     ~last_software_update:(Xapi_host.get_servertime ~__context ~host:ref)
     ~recommended_guidances:[] ~latest_synced_updates_applied:`unknown ;
   ref