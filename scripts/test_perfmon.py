eda664ddf6253ad440f764629aca80b158fd9cfb
Stephen Cheng <stephen.cheng@cloud.com>
2024-01-18 16:16:18 +0800

    CA-388295: Fix bugs from py2->py3 conversion in perfmon and hfx_filename
    Signed-off-by: Stephen Cheng <stephen.cheng@cloud.com>

--- scripts/test_perfmon.py
+++ scripts/test_perfmon.py
@@ -5,35 +5,13 @@
 import unittest
 from mock import MagicMock, patch
 import sys
-import os
-import subprocess
 import math
+from import_file import get_module
 
 # mock modules to avoid dependencies
 sys.modules["XenAPI"] = MagicMock()
 
-def import_from_file(module_name, file_path):
-    """Import a file as a module"""
-    if sys.version_info.major == 2:
-        return None
-    else:
-        from importlib import machinery, util
-        loader = machinery.SourceFileLoader(module_name, file_path)
-        spec = util.spec_from_loader(module_name, loader)
-        assert spec
-        assert spec.loader
-        module = util.module_from_spec(spec)
-        # Probably a good idea to add manually imported module stored in sys.modules
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
-        return module
-
-def get_module():
-    """Import the perfmon script as a module for executing unit tests on functions"""
-    testdir = os.path.dirname(__file__)
-    return import_from_file("perfmon", testdir + "/perfmon")
-
-perfmon = get_module()
+perfmon = get_module("perfmon", "/perfmon")
 @unittest.skipIf(sys.version_info < (3, 0), reason="requires python3")
 @patch("subprocess.getoutput")
 class TestGetPercentage(unittest.TestCase):
--- scripts/test_static_vdis.py
+++ scripts/test_static_vdis.py
@@ -5,36 +5,14 @@
 import unittest
 from mock import MagicMock
 import sys
-import os
-import subprocess
 import tempfile
+from import_file import get_module
 
 # mock modules to avoid dependencies
 sys.modules["XenAPI"] = MagicMock()
 sys.modules["inventory"] = MagicMock()
 
-def import_from_file(module_name, file_path):
-    """Import a file as a module"""
-    if sys.version_info.major == 2:
-        return None
-    else:
-        from importlib import machinery, util
-        loader = machinery.SourceFileLoader(module_name, file_path)
-        spec = util.spec_from_loader(module_name, loader)
-        assert spec
-        assert spec.loader
-        module = util.module_from_spec(spec)
-        # Probably a good idea to add manually imported module stored in sys.modules
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
-        return module
-
-def get_module():
-    """Import the static-vdis script as a module for executing unit tests on functions"""
-    testdir = os.path.dirname(__file__)
-    return import_from_file("static_vdis", testdir + "/static-vdis")
-
-static_vdis = get_module()
+static_vdis = get_module("static_vdis", "/static-vdis")
 
 @unittest.skipIf(sys.version_info < (3, 0), reason="requires python3")     
 class TestReadWriteFile(unittest.TestCase):
