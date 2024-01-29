01834a98855d4c1de4b22098ecba48eee45662ca
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-01-29 11:27:17 +0000

    Test for usb_reset.py: Test mount()/umount() using a rootless container
    A rootless container like using unshare --map-root-user --mount is a
    good way to test the correct calling convention for mount()/umount():
    
    This structure has already been in use in the unit-tests of python-libs
    and status-report.
    
    Most of this the contepts used already in use in these projects it is
    likely good to describe them for for reviewers:
    
    - Update my previous container for py2.7 to use an updated setup-python
      for unshare() in unit tests (GitHub doesn't support nested namespaces)
    
    - Reuse my rootless container implementation which is already successfully
      used in python-libs and status-report unit tests since many months.
    
    - Reuse my universal import_file_as_module() implementation from
      status-report which allows to confirm the test correctness using
      Python 2.7 and use it as well for also confirming the Python3 fix.
    
    - Use a context manager test fixture to temporarily mock module imports:
    
      Tests mocking module imports permanently on the global level without
      a proper context manager to cleantup these mocks are bad because when
      other tests need to mock them differently, they would have to clean
      these mocks before importing or installing their own mocks:
      Clean up after work is finished, to not interfere with other tests.
    
    - Add a sufficient testcase to test usb_reset.py: mount() and umount()
      without mocking the system or library calls in any way.
    
    - Add these tests in a new sub-directory for unit tests for migrating
      all unit test files to it for separation of delivered scripts from
      unit tests that are not delivered.
    
    - Implemenent the new sub-directory as a Python module to allow for
      non-deprecated relative imports: Absolute imports within a module
      are deprecated and are warned on by static type checkers.
    
    - Add the pylint project dictionary to pass spellchecks for the new code.
    
    - Add the .sourcery.yaml for the Sourcery checker to not warn to use
      f-strings, which would prevent the test case from being validated
      using the stable and proven-in-use Python2-compatible usb_reset.py
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

---
+++ scripts/unit_test/test_usb_reset_mount.py
@@ -0,0 +1,19 @@
+"""scripts/unit_test/test_usb_reset_mount.py: Test usb_reset.mount and .umount"""
+from __future__ import print_function
+
+import sys
+
+import pytest
+
+from .import_helper import import_file_as_module, mocked_modules
+
+
+@pytest.mark.skipif(sys.version_info > (3, 0), reason="u,mount needs Py3 fixes")
+def test_usb_reset_mount_umount(private_mount_namespace):
+    """Test usb_reset.mount and .umount"""
+    assert private_mount_namespace
+    with mocked_modules("xcp", "xcp.logger"):
+        usb_reset = import_file_as_module("scripts/usb_reset.py")
+        usb_reset.log.error = print
+        usb_reset.mount(source="tmpfs", target="/tmp", fs="tmpfs")
+        usb_reset.umount("/tmp")
