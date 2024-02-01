2891c857d2df897336e50d8f66e852cf1db39d01
Stephen Cheng <stephen.cheng@cloud.com>
2024-02-01 12:34:13 +0800

    Fix pylint and pytype warnings
    Signed-off-by: Stephen Cheng <stephen.cheng@cloud.com>

--- scripts/import_file.py
+++ scripts/import_file.py
@@ -1,14 +1,16 @@
-# Used for importing a non-".py" file as a module
-
+"""
+This module is used for importing a non-".py" file as a module
+"""
 import sys
 import os
+if sys.version_info.major > 2:
+    from importlib import machinery, util
 
 def import_from_file(module_name, file_path):
     """Import a file as a module"""
     # Only for python3, but CI has python2 pytest check, so add this line
     if sys.version_info.major == 2:
         return None
-    from importlib import machinery, util
     loader = machinery.SourceFileLoader(module_name, file_path)
     spec = util.spec_from_loader(module_name, loader)
     assert spec
@@ -20,5 +22,6 @@ def import_from_file(module_name, file_path):
     return module
 
 def get_module(module_name, file_path):
+    """get the module from a file"""
     testdir = os.path.dirname(__file__)
     return import_from_file(module_name, testdir + file_path)
--- scripts/test_hfx_filename.py
+++ scripts/test_hfx_filename.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# unittest for hfx_filename
+"""
+This module provides unittest for hfx_filename
+"""
 
+import sys
 import unittest
 from mock import MagicMock, patch, call
-import sys
 from import_file import get_module
-
 
 # mock modules to avoid dependencies
 sys.modules["XenAPI"] = MagicMock()
@@ -17,18 +17,26 @@ hfx_filename = get_module("hfx_filename", "/hfx_filename")
 @unittest.skipIf(sys.version_info < (3, 0), reason="requires python3")
 @patch("socket.socket")
 class TestRpc(unittest.TestCase):
-    
+    """
+    This class tests blow functions:
+    rpc()
+    db_get_uuid()
+    read_field()
+    """
     def test_rpc(self, mock_socket):
+        """
+        Tests rpc
+        """
         mock_connected_socket = MagicMock()
         mock_socket.return_value = mock_connected_socket
-        
+
         recv_data = b"HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\nHelloWorld"
         mock_connected_socket.recv.return_value = recv_data
 
         session_id = 0
         request = "socket request"
         body = hfx_filename.rpc(session_id, request)
-        
+
         # Assert that the socket methods were called as expected
         expected_data = [
             b"POST /remote_db_access?session_id=0 HTTP/1.0\r\n",
@@ -38,23 +46,25 @@ class TestRpc(unittest.TestCase):
             b"socket request"
         ]
         mock_connected_socket.send.assert_has_calls([call(data) for data in expected_data])
-        
+
         expected_return = "HelloWorld"
         self.assertEqual(expected_return, body)
-        
+
     def test_rpc_international_character(self, mock_socket):
+        """
+        Tests rpc using non-ascii characters
+        """
         mock_connected_socket = MagicMock()
         mock_socket.return_value = mock_connected_socket
-        
+
         recv_data = b"HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\nHelloWorld"
         mock_connected_socket.recv.return_value = recv_data
 
         session_id = 0
         # Use international character"socket 请求" as request
-        # Not using literal string is for passing python2 check
         request = "socket 请求"
         body = hfx_filename.rpc(session_id, request)
-        
+
         # Assert that the socket methods were called as expected
         expected_data = [
             b"POST /remote_db_access?session_id=0 HTTP/1.0\r\n",
@@ -64,44 +74,54 @@ class TestRpc(unittest.TestCase):
             request.encode('utf-8')
         ]
         mock_connected_socket.send.assert_has_calls([call(data) for data in expected_data])
-        
+
         expected_return = "HelloWorld"
         self.assertEqual(expected_return, body)
-    
+
     def test_db_get_uuid(self, mock_socket):
+        """
+        Tests db_get_uuid 
+        """
         mock_connected_socket = MagicMock()
         mock_socket.return_value = mock_connected_socket
-        
+
         header = "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\n"
-        body = "<value><array><data><value>success</value><value>HelloWorld</value></data></array></value>"
+        body = ("<value><array><data><value>success</value><value>HelloWorld"
+                "</value></data></array></value>")
         recv_data = (header + body).encode('utf-8')
         mock_connected_socket.recv.return_value = recv_data
-        
+
         expected_response = "HelloWorld"
         response = hfx_filename.db_get_by_uuid(0, "pool_patch", "22345")
         self.assertEqual(expected_response, response)
-    
+
     def test_read_field(self, mock_socket):
+        """
+        Tests read_field 
+        """
         mock_connected_socket = MagicMock()
         mock_socket.return_value = mock_connected_socket
-        
+
         header = "HTTP/1.1 200 OK\r\nContent-Length: 10\r\n\r\n"
-        body = "<value><array><data><value>success</value><value>file_name</value></data></array></value>"
+        body = ("<value><array><data><value>success</value><value>file_name"
+                "</value></data></array></value>")
         recv_data = (header + body).encode('utf-8')
         mock_connected_socket.recv.return_value = recv_data
-        
+
         expected_filename = "file_name"
         filename = hfx_filename.read_field(0, "pool_patch", "filename", "rf")
         self.assertEqual(expected_filename, filename)
-        
-    
 @unittest.skipIf(sys.version_info < (3, 0), reason="requires python3")
 class TestParse(unittest.TestCase):
-    
-    def test_parse_string(self): 
-        txt = "<value><array><data><value>success</value><value>abcde</value></data></array></value>" 
+    """
+    This class tests function parse_string()
+    """
+    def test_parse_string(self):
+        """
+        Tests parse_string
+        """
+        txt = ("<value><array><data><value>success</value><value>abcde"
+               "</value></data></array></value>")
         expected_txt = "abcde"
         return_txt = hfx_filename.parse_string(txt)
         self.assertEqual(expected_txt, return_txt)
-        
-        
