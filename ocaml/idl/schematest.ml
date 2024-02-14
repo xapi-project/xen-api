2c2d3f63abd34eec8ad9e489d532d49a0cdc5cd9
Rob Hoes <rob.hoes@citrix.com>
2024-02-14 17:41:26 +0000

    Update schematest
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "e4a6f29203bc65b75e969b9d4570933e"
+let last_known_schema_hash = "186131ad48f40dff30246e8e0c0dbf0a"
 
 let current_schema_hash : string =
   let open Datamodel_types in
