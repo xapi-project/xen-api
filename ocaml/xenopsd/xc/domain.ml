xapi-project/xen-api

commit 664de7608df8460cc3517f708b31f887059c83c8

Xen-4.15+: CDF_NESTED_VIRT
Only `platform:nested-virt=true` sets this field.
The previous experimental field `platform:exp-nested-hvm=true` is now dropped.

`platform:nested-virt` has been present in the product since Dundee, any users
of `exp-nested-hvm` can be updated to use that instead.
This doesn't mean that nested virt is supported yet: its implementation is
still incomplete.

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xenopsd/xc/domain.ml
@@ -66,6 +66,7 @@ type domain_create_flag = Xenctrl.domain_create_flag =
   | CDF_OOS_OFF
   | CDF_XS_DOMAIN
   | CDF_IOMMU
+  | CDF_NESTED_VIRT
 [@@deriving rpcty]
 
 type domain_create_iommu_opts = Xenctrl.domain_create_iommu_opts =
@@ -340,18 +341,25 @@ let make ~xc ~xs vm_info vcpus domain_config uuid final_uuid no_sharept =
   let iommu = vm_info.pci_passthrough in
   if iommu then
     assert_capability CAP_DirectIO ~on_error:(fun () -> "IOMMU unavailable") ;
+  let nested_virt = get_platform_key ~key:"nested_virt" ~default:false require_hvm in
 
-  info "VM = %s; Creating %s%s%s" (Uuidx.to_string uuid)
+  info "VM = %s; Creating %s%s%s%s" (Uuidx.to_string uuid)
     (if hvm then "HVM" else "PV")
     (if hap then " HAP" else "")
-    (if iommu then " IOMMU" else "") ;
+    (if iommu then " IOMMU" else "")
+    (if nested_virt then " NESTEDVIRT" else "") ;
 
   let config =
     {
       ssidref= vm_info.ssidref
     ; handle= Uuidx.to_string uuid
     ; flags=
-        [(hvm, CDF_HVM); (hap, CDF_HAP); (iommu, CDF_IOMMU)]
+        [
+          (hvm, CDF_HVM)
+        ; (hap, CDF_HAP)
+        ; (iommu, CDF_IOMMU)
+        ; (nested_virt, CDF_NESTED_VIRT)
+        ]
         |> List.filter_map (fun (cond, flag) -> if cond then Some flag else None)
     ; iommu_opts=
         ( match no_sharept with

commit 5d3d7a9bbbb9463bc190730e1b5ff0f52d83ee81

CA-389506: fix platform:nested_virt typo
Nested virt is not a supported feature yet, but when moving the setting
from Xen to toolstack as part of the Xen-4.17 update I typoed the name of the setting.

It was platform/nested-virt in xenguest.patch before

Fixes: 664de7608d ("Xen-4.15+: CDF_NESTED_VIRT")
Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xenopsd/xc/domain.ml
@@ -351,7 +351,7 @@ let make ~xc ~xs vm_info vcpus domain_config uuid final_uuid no_sharept =
   if iommu then
     assert_capability CAP_DirectIO ~on_error:(fun () -> "IOMMU unavailable") ;
   let nested_virt =
-    get_platform_key ~key:"nested_virt" ~default:false require_hvm
+    get_platform_key ~key:"nested-virt" ~default:false require_hvm
   in
   let vpmu = get_platform_key ~key:"vpmu" ~default:false (fun _ -> Ok ()) in
 