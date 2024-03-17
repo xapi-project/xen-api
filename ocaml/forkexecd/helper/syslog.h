a0a1c9eaf89e61dc02651ed2ae4643b76e6180d4
Frediano Ziglio <frediano.ziglio@cloud.com>
2024-03-17 10:37:24 +0000

    IH-533: Remove usage of forkexecd daemon to execute processes
    Forkexecd was written to avoid some issues with Ocaml and
    multi-threading.
    Instead use C code to launch processes and avoid these issues.
    Interface remains unchanged from Ocaml side but implementation rely
    entirely on C code.
    vfork() is used to avoid performance memory issue.
    Reap of the processes are done directly.
    Code automatically reap child processes to avoid zombies.
    One small helper is used to better separate Ocaml and C code and
    handling syslog redirection. This allows to better debug in
    case of issues.
    Syslog handling is done in a separate process allowing to restart
    the toolstack and keep launched programs running;
    note that even with forkexecd daemon one process was used for this
    purpose.
    Code tries to keep compatibility with forkexecd, in particular:
    - SIGPIPE is ignored in the parent;
    - /dev/null is open with O_WRONLY even for stdin;
    - file descriptors are limited to 1024.
    We use close_range (if available) to reduce system calls to close
    file descriptors.
    Cgroup is set to avoid systemd closing processes on toolstack restart.
    There's a fuzzer program to check file remapping algorithm; for this
    reason the algorithm is in a separate file.
    
    To turn internal debug on you need to set FORKEXECD_DEBUG_LOGS C
    preprocessor macro to 1.
    
    Signed-off-by: Frediano Ziglio <frediano.ziglio@cloud.com>

---
+++ ocaml/forkexecd/helper/syslog.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+
+// Quote and forward every line from "fd" to the syslog.
+// "fd" will be closed.
+bool forward_to_syslog(int fd, const char *key, int child_pid);
