a0176da7352f573448e3178df81172af6394f34d
Edwin Török <edwin.torok@cloud.com>
2024-04-24 16:23:48 +0100

    CP-49135: open /dev/urandom just once
    Reduces the amount of file descriptors kept open,
    and avoids 2 syscalls when creating sessions.
    
    Creating session uuids/refs is ~3.8x faster now:
    
    Before:
    ```
    uuidx creation/Uuidx.make_uuid_urnd (ns):
     { monotonic-clock per run = 5224.741331 (confidence: 5263.321869 to 5187.101530);
       r² = Some 0.997606 }
    ```
    
    After:
    ```
    uuidx creation/Uuidx.make_uuid_urnd (ns):
     { monotonic-clock per run = 1369.962560 (confidence: 1378.331674 to 1362.496608);
       r² = Some 0.998731 }
    ```
    
    Adjust forkexecd test, where the called program is an OCaml program linked with Uuidx,
    and thus has 1 extra FD open (that it didn't receive/inherit from the parent).
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/libs/uuid/uuidx.ml
+++ ocaml/libs/uuid/uuidx.ml
@@ -38,26 +38,20 @@ let is_uuid str = match of_string str with None -> false | Some _ -> true
 
 let dev_urandom = "/dev/urandom"
 
-let read_bytes dev n =
-  let fd = Unix.openfile dev [Unix.O_RDONLY] 0o640 in
-  let finally body_f clean_f =
-    try
-      let ret = body_f () in
-      clean_f () ; ret
-    with e -> clean_f () ; raise e
-  in
-  finally
-    (fun () ->
-      let buf = Bytes.create n in
-      let read = Unix.read fd buf 0 n in
-      if read <> n then
-        raise End_of_file
-      else
-        Bytes.to_string buf
-    )
-    (fun () -> Unix.close fd)
+let dev_urandom_fd = Unix.openfile dev_urandom [Unix.O_RDONLY] 0o640
+(* we can't close this in at_exit, because Crowbar runs at_exit, and
+   it'll fail because this FD will then be closed
+*)
 
-let make_uuid_urnd () = of_bytes (read_bytes dev_urandom 16) |> Option.get
+let read_bytes dev n =
+  let buf = Bytes.create n in
+  let read = Unix.read dev buf 0 n in
+  if read <> n then
+    raise End_of_file
+  else
+    Bytes.to_string buf
+
+let make_uuid_urnd () = of_bytes (read_bytes dev_urandom_fd 16) |> Option.get
 
 (* Use the CSPRNG-backed urandom *)
 let make = make_uuid_urnd
@@ -65,7 +59,7 @@ let make = make_uuid_urnd
 type cookie = string
 
 let make_cookie () =
-  read_bytes dev_urandom 64
+  read_bytes dev_urandom_fd 64
   |> String.to_seq
   |> Seq.map (fun c -> Printf.sprintf "%1x" (int_of_char c))
   |> List.of_seq
