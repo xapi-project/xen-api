6d7c5dc626364853e67b2a8efba572f119bb532f
Edwin Török <edwin.torok@cloud.com>
2024-05-02 21:19:55 +0100

    CP-307933: [prepare]: introduce field type in DB_ACCESS
    field=string for now, but will be changed to Schema.CachedValue.t later.
    
    There are separate types for input and output fields, e.g. we may want to perform UTF8 validation on input,
    which would modify both Value.t and its serialized form, so we can avoid serializing twice.
    
    No functional change
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/database/db_interface.mli
+++ ocaml/database/db_interface.mli
@@ -33,15 +33,15 @@ type db_ref = string
 
 type uuid = string
 
-type regular_fields = (field_name * field) list
+type 'field regular_fields = (field_name * 'field) list
 
 type associated_fields = (field_name * db_ref list) list
 
 (** dictionary of regular fields x dictionary of associated set_ref values *)
-type db_record = regular_fields * associated_fields
+type 'field db_record = 'field regular_fields * associated_fields
 
 (** The client interface to the database *)
-module type DB_ACCESS = sig
+module type DB_ACCESS_COMMON = sig
   val initialise : unit -> unit
   (** [initialise ()] must be called before any other function in this
       		interface *)
@@ -61,11 +61,6 @@ module type DB_ACCESS = sig
   (** [find_refs_with_filter tbl expr] returns a list of all references
       		to rows which match [expr] *)
 
-  val read_field_where : Db_ref.t -> Db_cache_types.where_record -> field list
-  (** [read_field_where {tbl,return,where_field,where_value}] returns a
-      		list of the [return] fields in table [tbl] where the [where_field]
-      		equals [where_value] *)
-
   val db_get_by_uuid : Db_ref.t -> table -> uuid -> db_ref
   (** [db_get_by_uuid tbl uuid] returns the single object reference
       		associated with [uuid] *)
@@ -79,33 +74,12 @@ module type DB_ACCESS = sig
   (** [db_get_by_name_label tbl label] returns the list of object references
       		associated with [label] *)
 
-  val create_row : Db_ref.t -> table -> regular_fields -> db_ref -> unit
-  (** [create_row tbl kvpairs ref] create a new row in [tbl] with
-      		key [ref] and contents [kvpairs] *)
-
   val delete_row : Db_ref.t -> db_ref -> table -> unit
   (** [delete_row context tbl ref] deletes row [ref] from table [tbl] *)
 
-  val write_field : Db_ref.t -> table -> db_ref -> field_name -> field -> unit
-  (** [write_field context tbl ref fld val] changes field [fld] to [val] in
-      		row [ref] in table [tbl] *)
-
-  val read_field : Db_ref.t -> table -> field_name -> db_ref -> field
-  (** [read_field context tbl fld ref] returns the value of field [fld]
-      		in row [ref] in table [tbl] *)
-
-  val read_record : Db_ref.t -> table -> db_ref -> db_record
-  (** [read_record tbl ref] returns
-      		[ (field, value) ] * [ (set_ref fieldname * [ ref ]) ] *)
-
-  val read_records_where :
-    Db_ref.t -> table -> Db_filter_types.expr -> (db_ref * db_record) list
-  (** [read_records_where tbl expr] returns a list of the values returned
-      		by read_record that match the expression *)
-
   val process_structured_field :
        Db_ref.t
-    -> field_name * field
+    -> field_name * string
     -> table
     -> field_name
     -> db_ref
@@ -115,4 +89,47 @@ module type DB_ACCESS = sig
       		value of field [fld] in row [ref] in table [tbl] according to [op]
       		which may be one of AddSet RemoveSet AddMap RemoveMap with
       		arguments [kv] *)
+end
+
+module type DB_ACCESS_FIELD = sig
+  type field_in
+
+  type field_out
+
+  val read_field_where : Db_ref.t -> Db_cache_types.where_record -> field list
+  (** [read_field_where {tbl,return,where_field,where_value}] returns a
+      		list of the [return] fields in table [tbl] where the [where_field]
+      		equals [where_value] *)
+
+  val create_row :
+    Db_ref.t -> table -> field_in regular_fields -> db_ref -> unit
+  (** [create_row tbl kvpairs ref] create a new row in [tbl] with
+      		key [ref] and contents [kvpairs] *)
+
+  val write_field : Db_ref.t -> table -> db_ref -> field_name -> field -> unit
+  (** [write_field context tbl ref fld val] changes field [fld] to [val] in
+      		row [ref] in table [tbl] *)
+
+  val read_field : Db_ref.t -> table -> field_name -> db_ref -> field
+  (** [read_field context tbl fld ref] returns the value of field [fld]
+      		in row [ref] in table [tbl] *)
+
+  val read_record : Db_ref.t -> table -> db_ref -> field_out db_record
+  (** [read_record tbl ref] returns
+      		[ (field, value) ] * [ (set_ref fieldname * [ ref ]) ] *)
+
+  val read_records_where :
+       Db_ref.t
+    -> table
+    -> Db_filter_types.expr
+    -> (db_ref * field_out db_record) list
+  (** [read_records_where tbl expr] returns a list of the values returned
+      		by read_record that match the expression *)
+end
+
+module type DB_ACCESS = sig
+  include DB_ACCESS_COMMON
+
+  include
+    DB_ACCESS_FIELD with type field_in = string and type field_out = string
 end
