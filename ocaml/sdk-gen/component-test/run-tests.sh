9439b256db6b7d81e936dc85303f961d49224ed6
Fei Su <fei.su@cloud.com>
2024-05-21 16:02:09 +0800

    Set up Github Action for go SDK component test (#5588)
    * CP-48777  Set up Github Action for go sdk component test
    
    Signed-off-by: Fei Su <fei.su@cloud.com>
    
    * update for using the generated sdk
    
    Signed-off-by: Fei Su <fei.su@cloud.com>
    
    ---------
    
    Signed-off-by: Fei Su <fei.su@cloud.com>

---
+++ ocaml/sdk-gen/component-test/run-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+
+start_jsonrpc_server() {
+    echo "Starting JSONRPC server"
+    python3 jsonrpc-server/server.py &
+    JSONRPC_SERVER_PID=$!
+    sleep 1
+}
+
+start_jsonrpc_go_client() {
+    echo "Starting JSONRPC Go client"
+
+    cd jsonrpc-client/go
+    # ensure that all dependencies are satisfied
+    go mod tidy
+    # build client.go and run it
+    go test main_test.go -v &
+    JSONRPC_GO_CLIENT_PID=$!
+}
+
+trap 'kill $JSONRPC_SERVER_PID $JSONRPC_GO_CLIENT_PID 2>/dev/null' EXIT
+
+main() {
+    cd "$SCRIPT_PATH"
+    start_jsonrpc_server
+    start_jsonrpc_go_client
+
+    # Wait for the component test to finish
+    wait $JSONRPC_GO_CLIENT_PID
+
+    # Shut down the server to reduce future problems when testing other clients.
+    kill $JSONRPC_SERVER_PID
+}
+
+main
