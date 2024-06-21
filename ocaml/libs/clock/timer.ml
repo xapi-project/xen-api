3341cc176b7755fe0c644d8980afffa198bc7c59
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2024-06-21 16:57:13 +0100

    IH-628, clock: add timer module
    This module can be used to know when a certain amount has passed since the
    timer started by polling. Useful for encoding timeouts, schedule actions
    periodically and similar.
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

---
+++ ocaml/libs/clock/timer.ml
@@ -0,0 +1,63 @@
+type t = {start: Ptime.t; elapsed: Mtime_clock.counter; duration: Mtime.Span.t}
+
+type countdown = Remaining of Mtime.Span.t | Expired of Mtime.Span.t
+
+let span_is_shorter a ~than:b = Mtime.Span.compare a b < 0
+
+let span_is_longer a ~than:b = Mtime.Span.compare a b > 0
+
+let start ~duration =
+  {start= Ptime_clock.now (); elapsed= Mtime_clock.counter (); duration}
+
+let duration {duration; _} = duration
+
+let elapsed t = Mtime_clock.count t.elapsed
+
+let remaining t =
+  let elapsed = Mtime_clock.count t.elapsed in
+  let difference = Mtime.Span.abs_diff elapsed t.duration in
+  if span_is_shorter elapsed ~than:t.duration then
+    Remaining difference
+  else
+    Expired difference
+
+let has_expired t =
+  let elapsed = Mtime_clock.count t.elapsed in
+  not (span_is_shorter elapsed ~than:t.duration)
+
+let deadline_of t =
+  Mtime.Span.to_uint64_ns t.duration
+  |> Int64.to_float
+  |> Ptime.Span.of_float_s
+  |> Option.get
+  |> Ptime.(Span.add Ptime.(to_span t.start))
+  |> Ptime.Span.to_float_s
+
+let shorten_by dur t =
+  let duration =
+    if span_is_longer dur ~than:t.duration then
+      Mtime.Span.zero
+    else
+      Mtime.Span.abs_diff dur t.duration
+  in
+  {t with duration}
+
+let extend_by dur t =
+  let duration = Mtime.Span.add dur t.duration in
+  {t with duration}
+
+let pp =
+  let open Fmt in
+  record
+    [
+      field "elapsed" elapsed Mtime.Span.pp
+    ; field "duration" duration Mtime.Span.pp
+    ]
+
+(* Conversion functions *)
+
+(* Rounding errors when there are more than 2^44 seconds, or about ~55 years.
+   *)
+let span_to_s span = Mtime.Span.to_float_ns span |> fun ns -> ns *. 1e-9
+
+let s_to_span s = Mtime.Span.of_float_ns (s *. 1e9 |> Float.round)
