9f9511884812a05db647764513dd92f86558e5d0
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-06-24 12:00:00 +0200

    CP-49910/pylint: Add docstrings and comments for link-vms-by-sr.py
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- python3/libexec/link-vms-by-sr.py
+++ python3/libexec/link-vms-by-sr.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
-# Populate a directory of symlinks partitioning VMs by SR
+"""
+link-vms-by-sr.py - Populate the given input_directory with VM metadata files,
+                    and create a directory structure of
+                    symlinks to the metadata files, partitioning VMs by SR UUID.
+
+Usage:
+    link-vms-by-sr.py -d <input_dir>
+
+The script uses the XenAPI to get a list of VMs in each SR
+and get the metadata for each VM, writing the metadata to the input_dir,
+and creating symlink directories in the input_dir/by-sr directory.
+
+Below the input_dir, given by -d <input_dir>:
+- In the <input_dir>/all/ directory, store all VM metadata files.
+- In the <input_dir>/by-sr/ directory, create symlinks to the VM metadata files,
+  partitioned by a directory structure of SR UUIDs.
+"""
 # (c) Anil Madhavapeddy, Citrix Systems Inc, 2008
 
 from __future__ import print_function
@@ -15,18 +31,21 @@ import XenAPI
 
 
 def logout(session):
+    """atexit handler to logout of the xapi session, ignoring any exceptions"""
     with contextlib.suppress(Exception):
         session.xenapi.session.logout()
 
 
-def get_input_dir():
+def get_input_dir_from_argparser():
+    """Parse command line arguments (-d input_dir) and return the input directory"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", dest="input_dir", required=True, help="Specify the input directory")
     args = parser.parse_args()
     return args.input_dir
 
 
-def get_vms_in_sr(session):
+def get_vms_in_sr_from_xapi(session):
+    """Return a dictionary of SR UUIDs to VM UUIDs"""
     vms = session.xenapi.VM.get_all_records()
     vbds = session.xenapi.VBD.get_all_records()
     vdis = session.xenapi.VDI.get_all_records()
@@ -67,13 +86,18 @@ def get_vms_in_sr(session):
 
 
 def main():
+    """Main function to save VM metadata files and link them by SR UUID"""
+
+    # Get a session for the local host, login and register a logout handler
     session = XenAPI.xapi_local()
     session.xenapi.login_with_password("", "", "1.0", "xen-api-scripts-linkvmsbysr.py")
     atexit.register(logout, session)
 
-    input_dir = get_input_dir()
-    vms_in_sr = get_vms_in_sr(session)
+    # Parse the input directory and get the VMs in each SR
+    input_dir = get_input_dir_from_argparser()
+    vms_in_sr = get_vms_in_sr_from_xapi(session)
 
+    # Create the directory structure and populate it with symlinks
     for sruuid in list(vms_in_sr.keys()):
         linkdir = "{}/by-sr/{}".format(input_dir, sruuid)
         if Path(linkdir).is_dir():
