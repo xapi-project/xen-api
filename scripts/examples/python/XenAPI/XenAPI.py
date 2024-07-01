xapi-project/xen-api

commit 500a1f70a65e2b1bd0a276a202597db2a4c654d6

XenAPI: suppress pytype false positives
pytype claims that there is no 'errno' in the socket module, but there is:
```
Python 3.6.8 (default, Nov 16 2020, 16:55:22)
[GCC 4.8.5 20150623 (Red Hat 4.8.5-44)] on linux
Type "help", "copyright", "credits" or "license" for more information.
>>> import socket
>>> socket.errno.ETIMEDOUT
110
```

Also `session.local_logout` is a remote method that is proxied by `__getattr__`,
I don't know why `pytype` complains about that one, and not about `logout`, but suppress it.

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

scripts/examples/python/XenAPI/XenAPI.py
@@ -181,7 +181,8 @@ class Session(xmlrpclib.ServerProxy):
             self.last_login_params = params
             self.API_version = self._get_api_version()
         except socket.error as e:
-            if e.errno == socket.errno.ETIMEDOUT:
+            # pytype false positive: there is a socket.errno in both py2 and py3
+            if e.errno == socket.errno.ETIMEDOUT: # pytype: disable=module-attr
                 raise xmlrpclib.Fault(504, 'The connection timed out')
             else:
                 raise e
@@ -189,7 +190,8 @@ class Session(xmlrpclib.ServerProxy):
     def _logout(self):
         try:
             if self.last_login_method.startswith("slave_local"):
-                return _parse_result(self.session.local_logout(self._session))
+                # Proxied function, pytype can't see it
+                return _parse_result(self.session.local_logout(self._session)) # pytype: disable=attribute-error
             else:
                 return _parse_result(self.session.logout(self._session))
         finally:

commit 84c2ba642ecbc072d462d1bae4e164df396270ad

fix(XenAPI.py): fix pylint warning
Fixes: 500a1f70a6 ("XenAPI: suppress pytype false positives")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

scripts/examples/python/XenAPI/XenAPI.py
@@ -222,7 +222,9 @@ class Session(xmlrpclib.ServerProxy):
         try:
             if self.last_login_method.startswith("slave_local"):
                 # Proxied function, pytype can't see it
-                return _parse_result(self.session.local_logout(self._session)) # pytype: disable=attribute-error
+                # pytype: disable=attribute-error
+                return _parse_result(self.session.local_logout(self._session))
+                # pytype: enable=attribute-error
             else:
                 return _parse_result(self.session.logout(self._session))
         finally: