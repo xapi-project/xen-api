ef27a66102f10ca83afee82866cd72c349c16708
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-07-08 12:00:00 +0200

    Improve spelling and fix typos in Python3 scripts
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- scripts/plugins/extauth-hook-AD.py
+++ scripts/plugins/extauth-hook-AD.py
@@ -277,7 +277,7 @@ class UsersList(DynamicPam):
             logger.debug("Permit user %s, Current sid is %s",
                          formatted_name, sid)
             self._lines.append(formatted_name)
-            # If ssh key is permittd in authorized_keys,
+            # If the ssh key is permitted in the authorized_keys file,
             # The original name is compared, add UPN and original name
             if self._backend == ADBackend.BD_PBIS and name != formatted_name:
                 self._lines.append(name)
@@ -311,7 +311,7 @@ class GroupsList(DynamicPam):
 
 class KeyValueConfig(ADConfig):
     """
-     Only support configure files with key value in each line, seperated by sep
+     Only support configure files with key value in each line, separated by sep
      Otherwise, it will be just copied and un-configurable
      If multiple lines with the same key exists, only the first line will be configured
     """
@@ -475,7 +475,7 @@ def after_extauth_enable(session, args):
 
 
 def after_xapi_initialize(session, args):
-    """Callback afer xapi initialize"""
+    """Callback after xapi initialization"""
     return refresh_all_configurations(session, args, "after_xapi_initialize")
 
 
@@ -485,7 +485,7 @@ def after_subject_add(session, args):
 
 
 def after_subject_remove(session, args):
-    """Callbackk after remove subject"""
+    """Callback after remove subject"""
     return refresh_dynamic_pam(session, args, "after_subject_remove")
 
 
