a97ab0680d913c93ae854c5d510e8eeebf343368
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-08-07 12:00:00 +0200

    xenopsd: Add pytest for common.VIF.get_ethtool()
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- ocaml/xenopsd/scripts/test_common_class_vif.py
+++ ocaml/xenopsd/scripts/test_common_class_vif.py
@@ -76,3 +76,48 @@ def test_get_locking_mode(input_params, expected_output):
     send_to_syslog.assert_called_once_with(
         "Got locking config: " + repr(expected_output)
     )
+
+
+@pytest.mark.parametrize(
+    "input_params, expected_results",
+    [
+        pytest.param(
+            {"other_config": [("ethtool-speed", "true"), ("ethtool-duplex", "false")]},
+            [("speed", True), ("duplex", False)],
+        ),
+        pytest.param(
+            {"other_config": [("ethtool-rx", "off"), ("ethtool-tso", "on")]},
+            [("rx", False), ("tso", True)],
+        ),
+        pytest.param(
+            {"other_config": [("ethtool-rx", "Wrong")]},
+            [],
+        ),
+    ],
+)
+def test_get_ethtool(input_params, expected_results):
+    """Test VIF.get_ethtool() using the VIF class test parameters defined above."""
+
+    # Arrange
+    class MockVIF(common.VIF):
+        """Mock class to simulate a VIF object containing the get_ethtool method"""
+
+        def __init__(self, json_data):  # pylint: disable=super-init-not-called
+            self.json = json_data
+            self.vm_uuid = "vm_uuid"
+            self.devid = 1
+
+    # Act: Get the locking mode configuration for the input params from the VIF object:
+    with patch("common.send_to_syslog") as send_to_syslog:
+        if expected_results:
+            with pytest.raises(TypeError) as ex:
+                MockVIF(input_params).get_ethtool()
+            # Assert the expected error message, will be fixed in the next commit:
+            assert "list.append() takes exactly one argument (2 given)" == str(ex.value)
+        else:
+            MockVIF(input_params).get_ethtool()
+
+    if not expected_results:
+        send_to_syslog.assert_called_once_with(
+            "VIF vm_uuid/1: ignoring ethtool argument rx=Wrong (use true/false)"
+        )
