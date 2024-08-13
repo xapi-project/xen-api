2f2c92de2e5b49c3286712b9f732ea96b6522197
Colin James <colin.barr@cloud.com>
2024-08-13 14:23:10 +0100

    CP-50053: Add authentication cache
    Provides a simple authentication cache built using a bounded priority search
    queue.
    
    The structure of the cache is effectively a (bounded) priority search queue,
    mapping usernames to entries - where entries have an explicit time-to-live and
    can store arbitrary data (optionally secured with a cryptographic hash).
    
    Also provides a configurable expiry span in the form of a global in Xapi_globs,
    "external_authentication_expiry". This time span is added to the current
    time - upon entering data into the cache - to provide an expiration time. Its
    default value is 5 minutes. The input is specified in seconds.
    
    The underlying priority search queue treats the minimum entry as being the
    entry with highest priority. Therefore, as entries are compared by their
    expiration time, the entry that is soonest to expire is the one that is evicted
    when attempting to add a new entry when the cache is at capacity.
    
    Signed-off-by: Colin James <colin.barr@cloud.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -1029,6 +1029,8 @@ let cert_thumbprint_header_value_sha1 = ref "sha-1:master"
 let cert_thumbprint_header_response =
   ref "x-xenapi-response-host-certificate-thumbprint"
 
+let external_authentication_expiry = ref Mtime.Span.(5 * min)
+
 let observer_endpoint_http_enabled = ref false
 
 let observer_endpoint_https_enabled = ref false
