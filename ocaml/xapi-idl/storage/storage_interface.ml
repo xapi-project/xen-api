c40a48c85351b27098e0ae32f39dec48e306d5d4
Vincent Liu <shuntian.liu2@cloud.com>
2024-08-21 15:56:11 +0100

    Revert "CP-51042: Raise error in sr-scan when SR.stat finds an unhealthy SR"
    This reverts commit 0fbc9d55e33e1930df937f2693f97d4daae8702e due to
    problems in lun resizing.
    
    Signed-off-by: Vincent Liu <shuntian.liu2@cloud.com>

--- ocaml/xapi-idl/storage/storage_interface.ml
+++ ocaml/xapi-idl/storage/storage_interface.ml
@@ -361,7 +361,6 @@ module Errors = struct
     | Content_ids_do_not_match of (string * string)
     | Missing_configuration_parameter of string
     | Internal_error of string
-    | Sr_unhealthy of sr_health
     | Unknown_error
   [@@default Unknown_error] [@@deriving rpcty]
 end
