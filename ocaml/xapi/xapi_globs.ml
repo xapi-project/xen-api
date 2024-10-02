xapi-project/xen-api

commit 6635a00d68e5097e6eda8f44d78e0c7cd0589aec

CP-49136: Introduce PRNG for generating non-secret UUIDs
Not yet enabled by default, behind xapi.conf feature flag.
Will be enabled by default after more testing/auditing of the code.

Pool secret and Session are the only secret uuids/refs,
for everything else avoid the overhead of reading from /dev/urandom every time,
and use an internal PRNG.

Both the opaqueref and UUID of a Session is a secret, because you can retrieve one if you can guess the other,
so both must be created using CSPRNG.

This is about 15x faster than using /dev/urandom:
```
╭───────────────────────────────────────┬───────────────────────────┬───────────────────────────┬───────────────────────────╮
│name                                   │  major-allocated          │  minor-allocated          │  monotonic-clock          │
├───────────────────────────────────────┼───────────────────────────┼───────────────────────────┼───────────────────────────┤
│  uuidx creation/Uuidx.make            │             0.0001 mjw/run│             8.0001 mnw/run│             88.1485 ns/run│
│  uuidx creation/Uuidx.make_uuid_urnd  │             0.0000 mjw/run│            10.0014 mnw/run│           1369.9626 ns/run│
╰───────────────────────────────────────┴───────────────────────────┴───────────────────────────┴───────────────────────────╯
```

Uses the type system to ensure that we cannot create Refs or Uuids of type 'session' with the wrong function.
Example type error from a testcase that has ben fixed:
```
File "ocaml/tests/test_client.ml", line 18, characters 47-57:
18 |       Test_common.make_session ~__context ~ref:session_id
                                                    ^^^^^^^^^^
Error: This expression has type [< Uuidx.not_session ] Ref.t
       but an expression was expected of type [ `session ] Ref.t
       Type [< Uuidx.not_session ] = [< `session of [ `use_make_uuid_urnd ] ]
       is not compatible with type [ `session ]
       Types for tag `session are incompatible
```

ocaml/xapi/xapi_globs.ml
@@ -1612,6 +1612,12 @@ let other_options =
     , (fun () -> string_of_bool !disable_webserver)
     , "Disable the host webserver"
     )
+  ; ( "use-prng-uuid-gen"
+      (* eventually this'll be the default, except for Sessions *)
+    , Arg.Unit (fun () -> Uuidx.make_default := Uuidx.make_uuid_fast)
+    , (fun () -> !Uuidx.make_default = Uuidx.make_uuid_fast |> string_of_bool)
+    , "Use PRNG based UUID generator instead of CSPRNG"
+    )
   ]
 
 (* The options can be set with the variable xapiflags in /etc/sysconfig/xapi.

ocaml/libs/uuid/dune
@@ -4,6 +4,7 @@
   (modules uuidx)
   (libraries
     unix (re_export uuidm)
+    threads.posix
   )
   (wrapped false)
 )

ocaml/libs/uuid/uuidx.ml
@@ -131,8 +131,20 @@ let read_bytes dev n =
 
 let make_uuid_urnd () = of_bytes (read_bytes dev_urandom_fd 16) |> Option.get
 
-(* Use the CSPRNG-backed urandom *)
-let make = make_uuid_urnd
+(** Use non-CSPRNG by default, for CSPRNG see {!val:make_uuid_urnd} *)
+let make_uuid_fast =
+  let uuid_state = Random.State.make_self_init () in
+  (* On OCaml 5 we could use Random.State.split instead,
+     and on OCaml 4 the mutex may not be strictly needed
+  *)
+  let m = Mutex.create () in
+  let finally () = Mutex.unlock m in
+  let gen = Uuidm.v4_gen uuid_state in
+  fun () -> Mutex.lock m ; Fun.protect ~finally gen
+
+let make_default = ref make_uuid_urnd
+
+let make () = !make_default ()
 
 type cookie = string
 

ocaml/libs/uuid/uuidx.mli
@@ -122,6 +122,13 @@ val make : unit -> [< not_secret] t
 (** Create a fresh UUID *)
 
 val make_uuid_urnd : unit -> [< secret] t
+(** [make_uuid_urnd ()] generate a UUID using a CSPRNG.
+     Currently this reads from /dev/urandom directly. *)
+
+val make_uuid_fast : unit -> [< not_secret] t
+(** [make_uuid_fast ()] generate a UUID using a PRNG.
+    Don't use this to generate secrets, see {!val:make_uuid_urnd} for that instead.
+ *)
 
 val pp : Format.formatter -> [< not_secret] t -> unit
 
@@ -129,7 +136,7 @@ val equal : 'a t -> 'a t -> bool
 
 val is_uuid : string -> bool
 
-val of_string : string -> 'a t option
+val of_string : string -> [< not_secret] t option
 (** Create a UUID from a string. *)
 
 val to_string : 'a t -> string
@@ -177,3 +184,8 @@ module Hash : sig
   (* UUID Version 5 derived from argument string and namespace UUID *)
   val string : string -> [< not_secret] t
 end
+
+(**/**)
+
+(* just for feature flag, to be removed *)
+val make_default : (unit -> [< not_secret] t) ref

ocaml/tests/bench/bench_uuid.ml
@@ -1,5 +1,7 @@
 open Bechamel
 
+let () = Uuidx.make_default := Uuidx.make_uuid_fast
+
 let benchmarks =
   Test.make_grouped ~name:"uuidx creation"
     [

ocaml/xapi-types/ref.mli
@@ -46,7 +46,7 @@ val to_option : 'a t -> 'a t option
 (** [to_option ref] returns [None] when [ref] is [Ref.Null] or [Some ref]
     otherwise *)
 
-val short_string_of : 'a t -> string
+val short_string_of : [< not_secret] t -> string
 
 val of_string : string -> [< not_secret] t
 
@@ -60,6 +60,6 @@ val is_dummy : 'a t -> bool
 
 val name_of_dummy : 'a t -> string
 
-val really_pretty_and_small : 'a t -> string
+val really_pretty_and_small : [< not_secret] t -> string
 
 val pp : Format.formatter -> 'a t -> unit

ocaml/xapi/helpers.ml
@@ -1955,7 +1955,9 @@ end = struct
     (* by default we generate the pool secret using /dev/urandom,
        but if a script to generate the pool secret exists, use that instead *)
     let make_urandom () =
-      Stdlib.List.init 3 (fun _ -> Uuidx.(make_uuid_urnd () |> to_string))
+      Stdlib.List.init 3 (fun _ ->
+          Uuidx.((make_uuid_urnd () : [`session] t) |> to_string)
+      )
       |> String.concat "/"
     in
     let make_script () =

commit a3ab2287e91167c4fd5996f7ef62dd560c2fe921

fix(xapi)): cannot compare functional values
Fixes: 6635a00d68e5 ("CP-49136: Introduce PRNG for generating non-secret UUIDs")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xapi/xapi_globs.ml
@@ -1619,7 +1619,7 @@ let other_options =
   ; ( "use-prng-uuid-gen"
       (* eventually this'll be the default, except for Sessions *)
     , Arg.Unit (fun () -> Uuidx.make_default := Uuidx.make_uuid_fast)
-    , (fun () -> !Uuidx.make_default = Uuidx.make_uuid_fast |> string_of_bool)
+    , (fun () -> !Uuidx.make_default == Uuidx.make_uuid_fast |> string_of_bool)
     , "Use PRNG based UUID generator instead of CSPRNG"
     )
   ; ( "reuse-pool-sessions"

quality-gate.sh
@@ -40,7 +40,7 @@ mli-files () {
 }
 
 structural-equality () {
-  N=9
+  N=10
   EQ=$(git grep -r --count ' == ' -- '**/*.ml' ':!ocaml/sdk-gen/**/*.ml' | cut -d ':' -f 2 | paste -sd+ - | bc)
   if [ "$EQ" -eq "$N" ]; then
     echo "OK counted $EQ usages of ' == '"