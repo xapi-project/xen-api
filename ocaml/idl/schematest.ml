6aeb11560ae3baad415b0528b728611685284368
Gang Ji <62988402+gangj@users.noreply.github.com>
2024-10-23 10:18:36 +0800

    CP-51393: Datamodel: update Repository for syncing from a remote pool (#6049)
    - add a new type of origin: "remote_pool"
    - add a new API: "introduce_remote_pool" to init a remote_pool repository
    - add a new field: "certificate" for a remote_pool repository
    - for a remote_pool repository, binary_url will be reused to hold the
      base URL of binary packages in the local repository of the remote pool in
      https://<coordinator-ip>/repository format
    
    Signed-off-by: Gang Ji <gang.ji@cloud.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,11 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
+<<<<<<< HEAD
 let last_known_schema_hash = "18df8c33434e3df1982e11ec55d1f3f8"
+=======
+let last_known_schema_hash = "aba698bd66b04e0145f07130e6db9cad"
+>>>>>>> CP-51393: Datamodel: update Repository for syncing from a remote pool (#6049)
 
 let current_schema_hash : string =
   let open Datamodel_types in
