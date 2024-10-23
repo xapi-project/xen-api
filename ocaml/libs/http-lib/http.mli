beeb20a9ad1e7a430ead643860468f4e46baf4a3
Gabriel Buica <danutgabriel.buica@cloud.com>
2024-10-23 10:51:45 +0100

    CP-50537: Propagate originator as a http request header
    XenAPI.py now passes an additional originator header when making
    requests to xapi, if the "ORIGINATOR" env var is present.
    Sm_exec now passes an env var, "ORIGINATOR=SM".
    
    To classify the threads correctly, we first need to determine the
    requests originators. This commit makes it possibly to explicitly
    state the originators as headers.
    
    Signed-off-by: Gabriel Buica <danutgabriel.buica@cloud.com>

--- ocaml/libs/http-lib/http.mli
+++ ocaml/libs/http-lib/http.mli
@@ -129,6 +129,8 @@ module Request : sig
   val to_wire_string : t -> string
   (** [to_wire_string t] returns a string which could be sent to a server *)
 
+  val with_originator_of : t option -> (string option -> unit) -> unit
+
   val traceparent_of : t -> Tracing.Span.t option
 
   val with_tracing :
