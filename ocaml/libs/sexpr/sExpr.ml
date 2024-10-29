36f9993f4b9e964fe2d56bc78bf88a9efda896bd
Colin James <colin.barr@cloud.com>
2024-10-29 13:06:49 +0000

    Remove notion of weird string from sexpr library
    Removes the remnants of an escaping mechanism once used in xapi. It
    appears that the format "<<xxx<value<xxx<" was an early escaping format.
    
    Also removes the effort at line tracking from the lexer actions. Since
    the .mli for the generated lexer was only present to have this line
    variable, it is deleted.
    
    Signed-off-by: Colin James <colin.barr@cloud.com>

--- ocaml/libs/sexpr/sExpr.ml
+++ ocaml/libs/sexpr/sExpr.ml
@@ -11,11 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-type t =
-  | Node of t list
-  | Symbol of string
-  | String of string
-  | WeirdString of string * string
+type t = Node of t list | Symbol of string | String of string
 
 let unescape_buf buf s =
   let aux esc = function
@@ -84,30 +80,12 @@ let string_of sexpr =
             List.iter (fun i -> Buffer.add_char buf ' ' ; __string_of_rec i) l
         ) ;
         Buffer.add_char buf ')'
-    | Symbol s | String s | WeirdString (_, s) ->
+    | Symbol s | String s ->
         Buffer.add_string buf "\'" ;
         Buffer.add_string buf (escape s) ;
         Buffer.add_string buf "\'"
   in
   __string_of_rec sexpr ; Buffer.contents buf
-
-let weird_of_string x =
-  let random_chars = "abcdefghijklmnopqrstuvwxyz" in
-  let randchar () =
-    String.sub random_chars (Random.int (String.length random_chars)) 1
-  in
-  (* true if the parent string contains child as a substring, starting the
-     search forward from offset *)
-  let rec has_substring parent offset child =
-    String.length parent - offset >= String.length child
-    && (String.sub parent offset (String.length child) = child
-       || has_substring parent (offset + 1) child
-       )
-  in
-  let rec find delim =
-    if has_substring x 0 delim then find (delim ^ randchar ()) else delim
-  in
-  WeirdString (find "xxx", x)
 
 let rec output_fmt ff = function
   | Node list ->
@@ -121,12 +99,5 @@ let rec output_fmt ff = function
             aux ~first t
       in
       Format.fprintf ff "@[(" ; aux list ; Format.fprintf ff ")@]"
-  | Symbol s | String s | WeirdString (_, s) ->
+  | Symbol s | String s ->
       Format.fprintf ff "\"%s\"" (escape s)
-
-(*
-  | Symbol s ->
-      Format.fprintf ff "%s" s
-  | WeirdString(tag, s) ->
-      Format.fprintf ff "<<%s<%s<%s<" tag s tag
-*)
