a8ba0b9ecf71d9f61ded9b9a2129f8295427a130
Vincent Liu <shuntian.liu2@cloud.com>
2024-11-04 14:54:17 +0000

    Refactor feature processing logic in `Smint`
    The previous feature processing logic was a bit ad hoc with various
    random conversion functions. Abstract them into one module and refactor
    a few functions to make their uses more explicit.
    
    Also try to encourage users, when processing features, to convert them
    into `Feature.t` and use the provided functions in this module rather
    than writing `List.mem` directly.
    
    Signed-off-by: Vincent Liu <shuntian.liu2@cloud.com>

--- ocaml/xapi/smint.ml
+++ ocaml/xapi/smint.ml
@@ -21,99 +21,96 @@ open D
 
 type vdi_info = {vdi_info_uuid: string option; vdi_info_location: string}
 
-(** Very primitive first attempt at a set of backend features *)
-type capability =
-  | Sr_create
-  | Sr_delete
-  | Sr_attach
-  | Sr_detach
-  | Sr_scan
-  | Sr_probe
-  | Sr_update
-  | Sr_supports_local_caching
-  | Sr_stats
-  | Sr_metadata
-  | Sr_trim
-  | Sr_multipath
-  | Vdi_create
-  | Vdi_delete
-  | Vdi_attach
-  | Vdi_detach
-  | Vdi_mirror
-  | Vdi_clone
-  | Vdi_snapshot
-  | Vdi_resize
-  | Vdi_activate
-  | Vdi_activate_readonly
-  | Vdi_deactivate
-  | Vdi_update
-  | Vdi_introduce
-  | Vdi_resize_online
-  | Vdi_generate_config
-  | Vdi_attach_offline
-  | Vdi_reset_on_boot
-  | Vdi_configure_cbt
-  | Large_vdi  (** Supports >2TB VDIs *)
-  | Thin_provisioning
-  | Vdi_read_caching
+module Feature = struct
+  (** Very primitive first attempt at a set of backend features *)
+  type capability =
+    | Sr_create
+    | Sr_delete
+    | Sr_attach
+    | Sr_detach
+    | Sr_scan
+    | Sr_probe
+    | Sr_update
+    | Sr_supports_local_caching
+    | Sr_stats
+    | Sr_metadata
+    | Sr_trim
+    | Sr_multipath
+    | Vdi_create
+    | Vdi_delete
+    | Vdi_attach
+    | Vdi_detach
+    | Vdi_mirror
+    | Vdi_clone
+    | Vdi_snapshot
+    | Vdi_resize
+    | Vdi_activate
+    | Vdi_activate_readonly
+    | Vdi_deactivate
+    | Vdi_update
+    | Vdi_introduce
+    | Vdi_resize_online
+    | Vdi_generate_config
+    | Vdi_attach_offline
+    | Vdi_reset_on_boot
+    | Vdi_configure_cbt
+    | Large_vdi  (** Supports >2TB VDIs *)
+    | Thin_provisioning
+    | Vdi_read_caching
 
-type feature = capability * int64
+  type t = capability * int64
 
-let string_to_capability_table =
-  [
-    ("SR_CREATE", Sr_create)
-  ; ("SR_DELETE", Sr_delete)
-  ; ("SR_ATTACH", Sr_attach)
-  ; ("SR_DETACH", Sr_detach)
-  ; ("SR_SCAN", Sr_scan)
-  ; ("SR_PROBE", Sr_probe)
-  ; ("SR_UPDATE", Sr_update)
-  ; ("SR_SUPPORTS_LOCAL_CACHING", Sr_supports_local_caching)
-  ; ("SR_METADATA", Sr_metadata)
-  ; ("SR_TRIM", Sr_trim)
-  ; ("SR_MULTIPATH", Sr_multipath)
-  ; ("SR_STATS", Sr_stats)
-  ; ("VDI_CREATE", Vdi_create)
-  ; ("VDI_DELETE", Vdi_delete)
-  ; ("VDI_ATTACH", Vdi_attach)
-  ; ("VDI_DETACH", Vdi_detach)
-  ; ("VDI_MIRROR", Vdi_mirror)
-  ; ("VDI_RESIZE", Vdi_resize)
-  ; ("VDI_RESIZE_ONLINE", Vdi_resize_online)
-  ; ("VDI_CLONE", Vdi_clone)
-  ; ("VDI_SNAPSHOT", Vdi_snapshot)
-  ; ("VDI_ACTIVATE", Vdi_activate)
-  ; ("VDI_ACTIVATE_READONLY", Vdi_activate_readonly)
-  ; ("VDI_DEACTIVATE", Vdi_deactivate)
-  ; ("VDI_UPDATE", Vdi_update)
-  ; ("VDI_INTRODUCE", Vdi_introduce)
-  ; ("VDI_GENERATE_CONFIG", Vdi_generate_config)
-  ; ("VDI_ATTACH_OFFLINE", Vdi_attach_offline)
-  ; ("VDI_RESET_ON_BOOT", Vdi_reset_on_boot)
-  ; ("VDI_CONFIG_CBT", Vdi_configure_cbt)
-  ; ("LARGE_VDI", Large_vdi)
-  ; ("THIN_PROVISIONING", Thin_provisioning)
-  ; ("VDI_READ_CACHING", Vdi_read_caching)
-  ]
+  let string_to_capability_table =
+    [
+      ("SR_CREATE", Sr_create)
+    ; ("SR_DELETE", Sr_delete)
+    ; ("SR_ATTACH", Sr_attach)
+    ; ("SR_DETACH", Sr_detach)
+    ; ("SR_SCAN", Sr_scan)
+    ; ("SR_PROBE", Sr_probe)
+    ; ("SR_UPDATE", Sr_update)
+    ; ("SR_SUPPORTS_LOCAL_CACHING", Sr_supports_local_caching)
+    ; ("SR_METADATA", Sr_metadata)
+    ; ("SR_TRIM", Sr_trim)
+    ; ("SR_MULTIPATH", Sr_multipath)
+    ; ("SR_STATS", Sr_stats)
+    ; ("VDI_CREATE", Vdi_create)
+    ; ("VDI_DELETE", Vdi_delete)
+    ; ("VDI_ATTACH", Vdi_attach)
+    ; ("VDI_DETACH", Vdi_detach)
+    ; ("VDI_MIRROR", Vdi_mirror)
+    ; ("VDI_RESIZE", Vdi_resize)
+    ; ("VDI_RESIZE_ONLINE", Vdi_resize_online)
+    ; ("VDI_CLONE", Vdi_clone)
+    ; ("VDI_SNAPSHOT", Vdi_snapshot)
+    ; ("VDI_ACTIVATE", Vdi_activate)
+    ; ("VDI_ACTIVATE_READONLY", Vdi_activate_readonly)
+    ; ("VDI_DEACTIVATE", Vdi_deactivate)
+    ; ("VDI_UPDATE", Vdi_update)
+    ; ("VDI_INTRODUCE", Vdi_introduce)
+    ; ("VDI_GENERATE_CONFIG", Vdi_generate_config)
+    ; ("VDI_ATTACH_OFFLINE", Vdi_attach_offline)
+    ; ("VDI_RESET_ON_BOOT", Vdi_reset_on_boot)
+    ; ("VDI_CONFIG_CBT", Vdi_configure_cbt)
+    ; ("LARGE_VDI", Large_vdi)
+    ; ("THIN_PROVISIONING", Thin_provisioning)
+    ; ("VDI_READ_CACHING", Vdi_read_caching)
+    ]
 
-let capability_to_string_table =
-  List.map (fun (k, v) -> (v, k)) string_to_capability_table
+  let capability_to_string_table =
+    List.map (fun (k, v) -> (v, k)) string_to_capability_table
 
-let string_of_capability c = List.assoc c capability_to_string_table
+  let known_features = List.map fst string_to_capability_table
 
-let string_of_feature (c, v) =
-  Printf.sprintf "%s/%Ld" (string_of_capability c) v
+  let capability_to_string c = List.assoc c capability_to_string_table
 
-let has_capability (c : capability) fl = List.mem_assoc c fl
+  let to_string (c, v) = Printf.sprintf "%s/%Ld" (capability_to_string c) v
 
-let capability_of_feature : feature -> capability = fst
+  let capability_of : t -> capability = fst
 
-let known_features = List.map fst string_to_capability_table
+  let unparse (f, v) = f ^ "/" ^ Int64.to_string v
 
-let unparse_feature (f, v) = f ^ "/" ^ Int64.to_string v
-
-let parse_string_int64_features features =
-  let scan feature =
+  let string_int64_of_string_opt feature =
     match String.split_on_char '/' feature with
     | [] ->
         None
@@ -131,30 +128,46 @@ let parse_string_int64_features features =
     | feature :: _ ->
         error "SM.feature: unknown feature %s" feature ;
         None
-  in
-  features
-  |> List.filter_map scan
-  |> List.sort_uniq (fun (x, _) (y, _) -> compare x y)
 
-(** [compat_features features1 features2] finds the compatible features in the input
-features lists. We assume features backwards compatible, i.e. if there are FOO/1 and
+  (** [compat_features features1 features2] finds the compatible features in the input
+  features lists. We assume features backwards compatible, i.e. if there are FOO/1 and
   FOO/2 are present, then we assume they can both do FOO/1*)
-let compat_features features1 features2 =
-  let features2 = List.to_seq features2 |> Hashtbl.of_seq in
-  List.filter_map
-    (fun (f1, v1) ->
-      match Hashtbl.find_opt features2 f1 with
-      | Some v2 ->
-          Some (f1, Int64.min v1 v2)
-      | None ->
-          None
-    )
-    features1
+  let compat_features features1 features2 =
+    let features2 = List.to_seq features2 |> Hashtbl.of_seq in
+    List.filter_map
+      (fun (f1, v1) ->
+        match Hashtbl.find_opt features2 f1 with
+        | Some v2 ->
+            Some (f1, Int64.min v1 v2)
+        | None ->
+            None
+      )
+      features1
 
-let parse_capability_int64_features strings =
-  List.map
-    (function c, v -> (List.assoc c string_to_capability_table, v))
-    (parse_string_int64_features strings)
+  let of_string_int64_opt (c, v) =
+    List.assoc_opt c string_to_capability_table |> Option.map (fun c -> (c, v))
+
+  (** [has_capability c fl] will test weather the required capability [c] is present 
+  in the feature list [fl]. Callers should use this function to test if a feature
+    is available rather than directly using membership functions on a feature list
+    as this function might have special logic for some features. *)
+  let has_capability (c : capability) fl = List.mem_assoc c fl
+
+  (** [parse_string_int64 features] takes a [features] list in its plain string
+  forms such as "VDI_MIRROR/2" and parses them into the form of (VDI_MIRROR, 2).
+  If the number is malformated, default to (VDI_MIRROR, 1). It will also deduplicate
+  based on the capability ONLY, and randomly choose a verion, based on the order
+  it appears in the input list.
+  *)
+  let parse_string_int64 features =
+    List.filter_map string_int64_of_string_opt features
+    |> List.sort_uniq (fun (x, _) (y, _) -> compare x y)
+
+  (** [parse_capability_int64 features] is similar to [parse_string_int64_features features]
+  but parses the input list into a [t list] *)
+  let parse_capability_int64 features =
+    parse_string_int64 features |> List.filter_map of_string_int64_opt
+end
 
 type sr_driver_info = {
     sr_driver_filename: string
@@ -164,7 +177,7 @@ type sr_driver_info = {
   ; sr_driver_copyright: string
   ; sr_driver_version: string
   ; sr_driver_required_api_version: string
-  ; sr_driver_features: feature list
+  ; sr_driver_features: Feature.t list
   ; sr_driver_text_features: string list
   ; sr_driver_configuration: (string * string) list
   ; sr_driver_required_cluster_stack: string list
