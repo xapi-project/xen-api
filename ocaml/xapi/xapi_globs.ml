f9a523dccf805af91e8b4bc942cb5457b0834f93
Edwin Török <edwin.torok@cloud.com>
2024-11-07 17:47:31 +0000

    CA-401652: stunnel_cache: set stunnel size limit based on host role
    Introduce separate coordinator_max_stunnel_cache and member_max_stunnel_cache settings,
    and set these on XAPI startup based on host role.
    
    No functional change, the defaults for both match the previous hardcoded
    value (70).
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -1011,6 +1011,10 @@ let header_total_timeout_tcp = ref 60.
 let max_header_length_tcp = ref 1024
 (* Maximum accepted size of HTTP headers in bytes (on TCP only) *)
 
+let coordinator_max_stunnel_cache = ref 70
+
+let member_max_stunnel_cache = ref 70
+
 let conn_limit_tcp = ref 800
 
 let conn_limit_unix = ref 1024
@@ -1142,6 +1146,8 @@ let xapi_globs_spec =
   ; ("header_read_timeout_tcp", Float header_read_timeout_tcp)
   ; ("header_total_timeout_tcp", Float header_total_timeout_tcp)
   ; ("max_header_length_tcp", Int max_header_length_tcp)
+  ; ("coordinator_max_stunnel_cache", Int coordinator_max_stunnel_cache)
+  ; ("member_max_stunnel_cache", Int member_max_stunnel_cache)
   ; ("conn_limit_tcp", Int conn_limit_tcp)
   ; ("conn_limit_unix", Int conn_limit_unix)
   ; ("conn_limit_clientcert", Int conn_limit_clientcert)
