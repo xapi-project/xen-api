e8bde26fe9f9fd35a110723b47c425bae28bca20
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2024-12-18 11:21:56 +0000

    CA-403422: lengthen the timeout for xenopsd's serialized tasks
    Historically parallel operations were run in tasks as part of serial
    operations, and serial tasks were not run as part of parallel ones. This
    changed recently, causing some timeouts that did not happen before. To
    mitigate this issue, now the timeouts for tasks are 20 minutes per single
    serialized operation, instead of 20 minutes per task.
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- ocaml/xenopsd/lib/xenops_server.ml
+++ ocaml/xenopsd/lib/xenops_server.ml
@@ -278,6 +278,15 @@ let rec name_of_atomic = function
   | Best_effort atomic ->
       Printf.sprintf "Best_effort (%s)" (name_of_atomic atomic)
 
+let rec atomic_expires_after = function
+  | Serial (_, _, ops) ->
+      List.map atomic_expires_after ops |> List.fold_left ( +. ) 0.
+  | Parallel (_, _, ops) ->
+      List.map atomic_expires_after ops |> List.fold_left Float.max 0.
+  | _ ->
+      (* 20 minutes, in seconds *)
+      1200.
+
 type vm_migrate_op = {
     vmm_id: Vm.id
   ; vmm_vdi_map: (string * string) list
@@ -2341,16 +2350,17 @@ and queue_atomics_and_wait ~progress_callback ~max_parallel_atoms dbg id ops =
                let atom_id =
                  Printf.sprintf "%s.chunk=%d.atom=%d" id chunk_idx atom_idx
                in
-               queue_atomic_int ~progress_callback dbg atom_id op
+               (queue_atomic_int ~progress_callback dbg atom_id op, op)
              )
              ops
          in
          let timeout_start = Unix.gettimeofday () in
          List.map
-           (fun task ->
+           (fun (task, op) ->
              let task_id = Xenops_task.id_of_handle task in
+             let expiration = atomic_expires_after op in
              let completion =
-               event_wait updates task ~from ~timeout_start 1200.0
+               event_wait updates task ~from ~timeout_start expiration
                  (is_task task_id) task_ended
              in
              (task_id, task, completion)
