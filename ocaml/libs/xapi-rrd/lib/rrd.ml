xapi-project/xen-api

commit a897b53abca2c0dc1f71967a914bd10a60a6299e

CA-391651: Use per-datasource last_updated timestamp during updating and archiving
This ensures that the interval between the current timestamp and previous
update is calculated correctly.

Keep the same external interface through rrdd_http_handler by adding an
'internal' parameter to xml exporting functions.

Signed-off-by: Andrii Sultanov <andrii.sultanov@cloud.com>

ocaml/libs/xapi-rrd/lib/rrd.ml
@@ -129,6 +129,7 @@ type ds = {
   ; mutable ds_value: float  (** Current calculated rate of the PDP *)
   ; mutable ds_unknown_sec: float
         (** Number of seconds that are unknown in the current PDP *)
+  ; mutable ds_last_updated: float  (** Last time this datasource was updated *)
 }
 [@@deriving rpc]
 
@@ -202,6 +203,7 @@ let copy_ds x =
   ; ds_last= x.ds_last
   ; ds_value= x.ds_value
   ; ds_unknown_sec= x.ds_unknown_sec
+  ; ds_last_updated= x.ds_last_updated
   }
 
 let copy_rrd x =
@@ -379,12 +381,14 @@ let ds_update rrd timestamp valuesandtransforms new_rrd =
      plugin, correspondingly they all have the same timestamp.
      Further refactoring is needed if timestamps per measurement are to be
      introduced. *)
-  let interval = timestamp -. rrd.last_updated in
+  let first_ds_index, _ = valuesandtransforms.(0) in
+  let last_updated = rrd.rrd_dss.(first_ds_index).ds_last_updated in
+  let interval = timestamp -. last_updated in
   (* Work around the clock going backwards *)
   let interval = if interval < 0. then 5. else interval in
 
   (* start time (st) and age of the last processed pdp and the currently occupied one *)
-  let proc_pdp_st, _proc_pdp_age = get_times rrd.last_updated rrd.timestep in
+  let proc_pdp_st, _proc_pdp_age = get_times last_updated rrd.timestep in
   let occu_pdp_st, occu_pdp_age = get_times timestamp rrd.timestep in
 
   (* The number of pdps that should result from this update *)
@@ -412,6 +416,7 @@ let ds_update rrd timestamp valuesandtransforms new_rrd =
     Array.map
       (fun (i, {value; _}) ->
         let v = process_ds_value rrd.rrd_dss.(i) value interval new_rrd in
+        rrd.rrd_dss.(i).ds_last_updated <- timestamp ;
         (i, v)
       )
       valuesandtransforms
@@ -548,6 +553,7 @@ let ds_create name ty ?(min = neg_infinity) ?(max = infinity) ?(mrhb = infinity)
   ; ds_last= init
   ; ds_value= 0.0
   ; ds_unknown_sec= 0.0
+  ; ds_last_updated= 0.0
   }
 
 let rrd_create dss rras timestep timestamp =
@@ -706,11 +712,11 @@ let from_xml input =
   let read_header i =
     ignore (get_el "version" i) ;
     let step = get_el "step" i in
-    let last_update = get_el "lastupdate" i in
+    let last_update = float_of_string (get_el "lastupdate" i) in
     (step, last_update)
   in
 
-  let read_dss i =
+  let read_dss i rrd_last_update =
     let read_ds i =
       read_block "ds"
         (fun i ->
@@ -722,6 +728,10 @@ let from_xml input =
           ignore (get_el "last_ds" i) ;
           let value = get_el "value" i in
           let unknown_sec = get_el "unknown_sec" i in
+          let last_updated =
+            try float_of_string (get_el "last_updated" i)
+            with _ -> rrd_last_update
+          in
           {
             ds_name= name
           ; ds_ty=
@@ -742,11 +752,12 @@ let from_xml input =
           ; (* float_of_string "last_ds"; *)
             ds_value= float_of_string value
           ; ds_unknown_sec= float_of_string unknown_sec
+          ; ds_last_updated= last_updated
           }
         )
         i
     in
-    let dss = read_all "ds" read_ds i [] in
+    let dss = Array.of_list (read_all "ds" read_ds i []) in
     dss
   in
 
@@ -791,7 +802,7 @@ let from_xml input =
         let cols = try Array.length data.(0) with _ -> -1 in
         let db =
           Array.init cols (fun i ->
-              let ds = List.nth dss i in
+              let ds = dss.(i) in
               Fring.make rows nan ds.ds_min ds.ds_max
           )
         in
@@ -844,13 +855,13 @@ let from_xml input =
   read_block "rrd"
     (fun i ->
       let step, last_update = read_header i in
-      let dss = read_dss i in
+      let dss = read_dss i last_update in
       let rras = read_rras dss i in
       let rrd =
         {
-          last_updated= float_of_string last_update
+          last_updated= last_update
         ; timestep= Int64.of_string step
-        ; rrd_dss= Array.of_list dss
+        ; rrd_dss= dss
         ; rrd_rras= Array.of_list rras
         }
       in
@@ -884,7 +895,7 @@ let from_xml input =
     )
     input
 
-let xml_to_output rrd output =
+let xml_to_output internal rrd output =
   (* We use an output channel for Xmlm-compat buffered output. Provided we flush
      at the end we should be safe. *)
   let tag n fn output =
@@ -906,7 +917,9 @@ let xml_to_output rrd output =
         tag "value" (data (Utils.f_to_s ds.ds_value)) output ;
         tag "unknown_sec"
           (data (Printf.sprintf "%d" (int_of_float ds.ds_unknown_sec)))
-          output
+          output ;
+        if internal then
+          tag "last_updated" (data (Utils.f_to_s ds.ds_last_updated)) output
       )
       output
   in
@@ -968,9 +981,7 @@ let xml_to_output rrd output =
     (fun output ->
       tag "version" (data "0003") output ;
       tag "step" (data (Int64.to_string rrd.timestep)) output ;
-      tag "lastupdate"
-        (data (Printf.sprintf "%Ld" (Int64.of_float rrd.last_updated)))
-        output ;
+      tag "lastupdate" (data (Utils.f_to_s rrd.last_updated)) output ;
       do_dss rrd.rrd_dss output ;
       do_rras rrd.rrd_rras output
     )
@@ -1002,6 +1013,7 @@ module Json = struct
       ; ("last_ds", string (ds_value_to_string ds.ds_last))
       ; ("value", float ds.ds_value)
       ; ("unknown_sec", float ds.ds_unknown_sec)
+      ; ("last_updated", float ds.ds_last_updated)
       ]
 
   let cdp x =

ocaml/libs/xapi-rrd/unix/rrd_unix.ml
@@ -30,12 +30,13 @@ let with_out_channel_output fd f =
     )
     (fun () -> Out_channel.close_noerr oc)
 
-let xml_to_fd rrd fd = with_out_channel_output fd (Rrd.xml_to_output rrd)
+let xml_to_fd internal rrd fd =
+  with_out_channel_output fd (Rrd.xml_to_output internal rrd)
 
 let json_to_fd rrd fd =
   let payload = Rrd.json_to_string rrd |> Bytes.unsafe_of_string in
   let len = Bytes.length payload in
   Unix.write fd payload 0 len |> ignore
 
-let to_fd ?(json = false) rrd fd =
-  (if json then json_to_fd else xml_to_fd) rrd fd
+let to_fd ?(json = false) ?(internal = false) rrd fd =
+  (if json then json_to_fd else xml_to_fd internal) rrd fd

ocaml/libs/xapi-rrd/unix/rrd_unix.mli
@@ -1,11 +1,11 @@
 (*
   Copyright (C) Citrix Systems Inc.
- 
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published
   by the Free Software Foundation; version 2.1 only. with the special
   exception on linking described in file LICENSE.
- 
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -15,5 +15,10 @@
     This module provides Unix tools for dealing with RRDs
  *)
 
-val to_fd : ?json:bool -> Rrd.rrd -> Unix.file_descr -> unit
-(** Serialize the rrd to xml / json and offer it through a file descriptor *)
+val to_fd : ?json:bool -> ?internal:bool -> Rrd.rrd -> Unix.file_descr -> unit
+(** Serialize the rrd to xml / json and offer it through a file descriptor.
+
+    If [internal] is true (false is the default), then the output is not
+    guaranteed to be compatible with external tools, and can only be parsed
+    by xcp-rrdd.
+ *)

ocaml/xcp-rrdd/bin/rrdd/rrdd_shared.ml
@@ -137,7 +137,7 @@ let send_rrd ?(session_id : string option)
   let open Xmlrpc_client in
   with_transport transport
     (with_http request (fun (_response, fd) ->
-         try Rrd_unix.to_fd rrd fd with _ -> log_backtrace ()
+         try Rrd_unix.to_fd ~internal:true rrd fd with _ -> log_backtrace ()
      )
     ) ;
   debug "Sending RRD complete."
@@ -161,7 +161,8 @@ let archive_rrd_internal ?(transport = None) ~uuid ~rrd () =
             0o755 ;
           let base_filename = Rrdd_libs.Constants.rrd_location ^ "/" ^ uuid in
           Xapi_stdext_unix.Unixext.atomic_write_to_file (base_filename ^ ".gz")
-            0o644 (fun fd -> Gzip.Default.compress fd (Rrd_unix.to_fd rrd)
+            0o644 (fun fd ->
+              Gzip.Default.compress fd (Rrd_unix.to_fd ~internal:true rrd)
           ) ;
           (* If there's an uncompressed one hanging around, remove it. *)
           Xapi_stdext_unix.Unixext.unlink_safe base_filename

ocaml/xcp-rrdd/bin/rrdd/xcp_rrdd.ml
@@ -514,7 +514,9 @@ let do_monitor_write xc writers =
       let dom0_stats =
         tagged_dom0_stats
         |> List.to_seq
-        |> Seq.map (fun (name, (timestamp, dss)) -> (timestamp, List.to_seq dss))
+        |> Seq.map (fun (_name, (timestamp, dss)) ->
+               (timestamp, List.to_seq dss)
+           )
       in
       let plugins_stats = Rrdd_server.Plugin.read_stats () in
       let stats = Seq.append plugins_stats dom0_stats in

ocaml/xcp-rrdd/bin/rrddump/rrddump.ml
@@ -32,10 +32,11 @@ let text_export rrd =
       Int64.sub last_cdp_time (Int64.mul (Int64.of_int i) rra_timestep)
     in
     for j = 0 to Array.length rrd.rrd_dss - 1 do
-      Printf.printf "Doing ds: %s\n" rrd.rrd_dss.(j).ds_name ;
+      let ds = rrd.rrd_dss.(j) in
+      Printf.printf "Doing ds: %s\n" ds.ds_name ;
       let oc =
         open_out
-          (Printf.sprintf "rrd_data_%s_%s_%Ld.dat" rrd.rrd_dss.(j).ds_name
+          (Printf.sprintf "rrd_data_%s_%s_%Ld.dat" ds.ds_name
              (cf_type_to_string rra.rra_cf)
              (Int64.mul
                 (Int64.of_int (rra.rra_pdp_cnt * rra.rra_row_cnt))

commit d3612d10fcc00bccf7a74ebd568c230c9cfad045

CA-407322 - libs/rrd: Keep lastupdate XML field as int, XenCenter relies on it
Instead of waiting for a complicated fix on XC side, switch back to outputting
integers when dumping XMLs.

Implementations that already switched to float should be good, since converting
int to float is fine, but not the other way around.

XML is an outdated format, JSON should be preferrably used, and it will keep
using floats with larger precision.

Fixes: a897b53 ("CA-391651: Use per-datasource last_updated timestamp during updating and archiving")

Signed-off-by: Andrii Sultanov <andrii.sultanov@cloud.com>

ocaml/libs/xapi-rrd/lib/rrd.ml
@@ -999,7 +999,14 @@ let xml_to_output internal rrd output =
     (fun output ->
       tag "version" (data "0003") output ;
       tag "step" (data (Int64.to_string rrd.timestep)) output ;
-      tag "lastupdate" (data (Utils.f_to_s rrd.last_updated)) output ;
+      (* XenCenter and other legacy users expect this to be an integer
+         representing the number of seconds, so keep it an integer to avoid
+         breaking them. Some other libraries (e.g. the Python rrd parser,
+         the C rrd parser, xenrt) will expect a float and can easily convert
+         an int to it. *)
+      tag "lastupdate"
+        (data (Printf.sprintf "%Ld" (Int64.of_float rrd.last_updated)))
+        output ;
       do_dss rrd.rrd_dss output ;
       do_rras rrd.rrd_rras output
     )