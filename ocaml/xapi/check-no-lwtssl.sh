xapi-project/xen-api

commit 044dc15ee4e32e7d6d31de2c927330ea5bc674c4

CP-45016: Add support for specifying nbd export in sparse_dd
Previously we hardcoded the export to be empty string when the nbd
client negotiates with the server. This patch allows specifying this
as a protocol parameter.

This is useful for SXM of SMAPIv3 SRs when we copy the snapshot from
source to destination, where the export name is needed when we copy
using nbd.

Signed-off-by: Vincent Liu <shuntian.liu2@cloud.com>

ocaml/xapi/dune
@@ -159,6 +159,7 @@
     uri
     uuid
     uuidm
+    vhd_lib
     x509
     xapi_aux
     xapi-backtrace

ocaml/xapi/sparse_dd_wrapper.ml
@@ -72,7 +72,8 @@ end
 exception Cancelled
 
 (** Use the new external sparse_dd program *)
-let dd_internal progress_cb base prezeroed verify_cert infile outfile size =
+let dd_internal progress_cb base prezeroed verify_cert ?(proto = None) infile
+    outfile size =
   let pipe_read, pipe_write = Unix.pipe () in
   let to_close = ref [pipe_read; pipe_write] in
   let close x =
@@ -87,6 +88,15 @@ let dd_internal progress_cb base prezeroed verify_cert infile outfile size =
         match
           Forkhelpers.with_logfile_fd "sparse_dd" (fun log_fd ->
               let sparse_dd_path = !Xapi_globs.sparse_dd in
+              let proto_args =
+                match proto with
+                | None ->
+                    []
+                | Some (StreamCommon.Nbd export) ->
+                    ["-dest-proto"; "nbd"; "-nbd-export"; export]
+                | Some p ->
+                    ["-dest-proto"; StreamCommon.string_of_protocol p]
+              in
               let verify_args =
                 match verify_cert with
                 | None ->
@@ -119,6 +129,7 @@ let dd_internal progress_cb base prezeroed verify_cert infile outfile size =
                   ; (if prezeroed then ["-prezeroed"] else [])
                   ; (match base with None -> [] | Some x -> ["-base"; x])
                   ; verify_args
+                  ; proto_args
                   ]
               in
               debug "%s %s" sparse_dd_path (String.concat " " args) ;
@@ -164,7 +175,7 @@ let dd_internal progress_cb base prezeroed verify_cert infile outfile size =
         | Forkhelpers.Success _ ->
             progress_cb (Finished None)
         | Forkhelpers.Failure (log, End_of_file) ->
-            error "Error while trying to read progress from sparse_dd" ;
+            error "Error while trying to read progress from sparse_dd: %s" log ;
             raise (Api_errors.Server_error (Api_errors.vdi_copy_failed, [log]))
         | Forkhelpers.Failure (log, exn) ->
             error "Failure from sparse_dd: %s raising %s" log
@@ -179,13 +190,13 @@ let dd_internal progress_cb base prezeroed verify_cert infile outfile size =
     )
     (fun () -> close pipe_read ; close pipe_write)
 
-let dd ?(progress_cb = fun _ -> ()) ?base ~verify_cert prezeroed =
+let dd ?(progress_cb = fun _ -> ()) ?base ~verify_cert ?proto prezeroed =
   dd_internal
     (function Continuing x -> progress_cb x | _ -> ())
-    base prezeroed verify_cert
+    base prezeroed ~proto verify_cert
 
-let start ?(progress_cb = fun _ -> ()) ?base ~verify_cert prezeroed infile
-    outfile size =
+let start ?(progress_cb = fun _ -> ()) ?base ~verify_cert ?(proto = None)
+    prezeroed infile outfile size =
   let m = Mutex.create () in
   let c = Condition.create () in
   let pid = ref None in
@@ -206,8 +217,8 @@ let start ?(progress_cb = fun _ -> ()) ?base ~verify_cert prezeroed infile
   let _ =
     Thread.create
       (fun () ->
-        dd_internal thread_progress_cb base prezeroed verify_cert infile outfile
-          size
+        dd_internal thread_progress_cb base prezeroed verify_cert ~proto infile
+          outfile size
       )
       ()
   in

ocaml/xapi/storage_migrate.ml
@@ -562,7 +562,20 @@ module MigrateLocal = struct
       finally
         (fun () ->
           debug "activating RW datapath %s on remote" remote_dp ;
-          ignore (Remote.VDI.attach2 dbg remote_dp dest dest_vdi true) ;
+          let backend =
+            Remote.VDI.attach3 dbg remote_dp dest dest_vdi vm true
+          in
+          let _, _, _, nbds =
+            Storage_interface.implementations_of_backend backend
+          in
+          let proto =
+            match nbds with
+            | [] ->
+                None
+            | uri :: _ ->
+                let _socket, export = Storage_interface.parse_nbd_uri uri in
+                Some (StreamCommon.Nbd export)
+          in
           Remote.VDI.activate dbg remote_dp dest dest_vdi ;
           with_activated_disk ~dbg ~sr ~vdi:base_vdi ~dp:base_dp
             (fun base_path ->
@@ -574,7 +587,7 @@ module MigrateLocal = struct
                   let dd =
                     Sparse_dd_wrapper.start
                       ~progress_cb:(progress_callback 0.05 0.9 task)
-                      ~verify_cert ?base:base_path true (Option.get src)
+                      ~verify_cert ~proto ?base:base_path true (Option.get src)
                       dest_vdi_url remote_vdi.virtual_size
                   in
                   Storage_task.with_cancel task

ocaml/vhd-tool/cli/dune
@@ -4,7 +4,7 @@
   (libraries
     astring
 
-    local_lib
+    vhd_lib
     cmdliner
     cstruct
     forkexec

ocaml/vhd-tool/src/dune
@@ -4,7 +4,7 @@
     (language c)
     (names direct_copy_stubs)
   )
-  (name local_lib)
+  (name vhd_lib)
   (wrapped false)
   (libraries
     astring

ocaml/vhd-tool/test/dune
@@ -4,7 +4,7 @@
     alcotest
     alcotest-lwt
     lwt
-    local_lib
+    vhd_lib
     vhd-format
     vhd-format-lwt
   )

ocaml/vhd-tool/cli/sparse_dd.ml
@@ -65,6 +65,10 @@ let sni = ref None
 
 let cert_bundle_path = ref None
 
+let dest_proto = ref None
+
+let nbd_export = ref ""
+
 let string_opt = function None -> "None" | Some x -> x
 
 let machine_readable_progress = ref false
@@ -157,6 +161,16 @@ let options =
     , Arg.String (fun x -> cert_bundle_path := Some x)
     , (fun () -> string_opt !cert_bundle_path)
     , "path to a CA certificate bundle"
+    )
+  ; ( "dest-proto"
+    , Arg.String (fun x -> dest_proto := Some x)
+    , (fun () -> string_opt !dest_proto)
+    , "destination protocol to be used for copying the disk"
+    )
+  ; ( "nbd-export"
+    , Arg.String (fun x -> nbd_export := x)
+    , (fun () -> !nbd_export)
+    , "nbd export name to be used, only useful when dest-proto is nbd"
     )
   ]
 
@@ -288,6 +302,15 @@ let _ =
     debug "Must have -size argument\n" ;
     exit 1
   ) ;
+  let dest_proto =
+    match !dest_proto with
+    | Some "nbd" ->
+        Some (StreamCommon.Nbd !nbd_export)
+    | Some x ->
+        Some (StreamCommon.protocol_of_string x)
+    | None ->
+        None
+  in
   let size = !size in
   let base = !base in
   (* Helper function to bring an int into valid range *)
@@ -485,7 +508,7 @@ let _ =
   in
   let t =
     stream_t >>= fun s ->
-    Impl.write_stream common s destination None !prezeroed progress None
+    Impl.write_stream common s destination dest_proto !prezeroed progress None
       !good_ciphersuites verify_cert
   in
   if destination_format = "vhd" then

ocaml/vhd-tool/src/impl.ml
@@ -220,7 +220,7 @@ let[@warning "-27"] stream_human _common _ s _ _ ?(progress = no_progress_bar)
   Printf.printf "# end of stream\n" ;
   return None
 
-let stream_nbd _common c s prezeroed _ ?(progress = no_progress_bar) () =
+let stream_nbd _common c s prezeroed ~export ?(progress = no_progress_bar) () =
   let open Nbd_unix in
   let c =
     {
@@ -231,7 +231,7 @@ let stream_nbd _common c s prezeroed _ ?(progress = no_progress_bar) () =
     }
   in
 
-  Client.negotiate c "" >>= fun (server, size, _flags) ->
+  Client.negotiate c export >>= fun (server, size, _flags) ->
   (* Work to do is: non-zero data to write + empty sectors if the
      target is not prezeroed *)
   D.debug "%s nbd negotiation done, size is %Ld" __FUNCTION__ size ;
@@ -988,7 +988,7 @@ let write_stream common s destination destination_protocol prezeroed progress
       return (c, [NoProtocol; Human; Tar])
   | File_descr fd ->
       Channels.of_raw_fd fd >>= fun c ->
-      return (c, [Nbd; NoProtocol; Chunked; Human; Tar])
+      return (c, [dummy_nbd; NoProtocol; Chunked; Human; Tar])
   | Sockaddr sockaddr ->
       let sock = socket sockaddr in
       Lwt.catch
@@ -996,7 +996,7 @@ let write_stream common s destination destination_protocol prezeroed progress
         (fun e -> Lwt_unix.close sock >>= fun () -> Lwt.fail e)
       >>= fun () ->
       Channels.of_raw_fd sock >>= fun c ->
-      return (c, [Nbd; NoProtocol; Chunked; Human; Tar])
+      return (c, [dummy_nbd; NoProtocol; Chunked; Human; Tar])
   | Https uri' | Http uri' -> (
       (* TODO: https is not currently implemented *)
       let port =
@@ -1016,7 +1016,6 @@ let write_stream common s destination destination_protocol prezeroed progress
       let host = Scanf.ksscanf host (fun _ _ -> host) "[%s@]" Fun.id in
       Lwt_unix.getaddrinfo host (string_of_int port) [] >>= fun he ->
       if he = [] then raise Not_found ;
-
       let sockaddr = (List.hd he).Unix.ai_addr in
       let sock = socket sockaddr in
       Lwt.catch
@@ -1077,7 +1076,7 @@ let write_stream common s destination destination_protocol prezeroed progress
               List.mem_assoc te headers && List.assoc te headers = "nbd"
             in
             if advertises_nbd then
-              return (c, [Nbd])
+              return (c, [dummy_nbd])
             else
               return (c, [Chunked; NoProtocol])
           else
@@ -1094,7 +1093,12 @@ let write_stream common s destination destination_protocol prezeroed progress
         D.info "Using protocol: %s\n%!" (string_of_protocol t) ;
         t
   in
-  if not (List.mem destination_protocol possible_protocols) then
+  (* when checking for the validity of the protocol, we only care about the protocol
+     itself and not the export name, if it was nbd. Convert all Nbd export to Nbd "" *)
+  let equal_to_dest_proto =
+    ( = ) (match destination_protocol with Nbd _ -> dummy_nbd | y -> y)
+  in
+  if not (List.exists equal_to_dest_proto possible_protocols) then
     fail
       (Failure
          (Printf.sprintf "this destination only supports protocols: [ %s ]"
@@ -1104,18 +1108,17 @@ let write_stream common s destination destination_protocol prezeroed progress
   else
     let start = Unix.gettimeofday () in
     ( match destination_protocol with
-    | Nbd ->
-        stream_nbd
+    | Nbd export ->
+        stream_nbd common c s prezeroed ~export ~progress ()
     | Human ->
-        stream_human
+        stream_human common c s prezeroed tar_filename_prefix ~progress ()
     | Chunked ->
-        stream_chunked
+        stream_chunked common c s prezeroed tar_filename_prefix ~progress ()
     | Tar ->
-        stream_tar
+        stream_tar common c s prezeroed tar_filename_prefix ~progress ()
     | NoProtocol ->
-        stream_raw
+        stream_raw common c s prezeroed tar_filename_prefix ~progress ()
     )
-      common c s prezeroed tar_filename_prefix ~progress ()
     >>= fun p ->
     c.Channels.close () >>= fun () ->
     match p with
@@ -1322,7 +1325,7 @@ let serve common_options source source_fd source_format source_protocol
     let supported_formats = ["raw"] in
     if not (List.mem destination_format supported_formats) then
       failwith (Printf.sprintf "%s is not a supported format" destination_format) ;
-    let supported_protocols = [NoProtocol; Chunked; Nbd; Tar] in
+    let supported_protocols = [NoProtocol; Chunked; dummy_nbd; Tar] in
     if not (List.mem source_protocol supported_protocols) then
       failwith
         (Printf.sprintf "%s is not a supported source protocol"
@@ -1412,7 +1415,7 @@ let serve common_options source source_fd source_format source_protocol
         match (source_format, source_protocol) with
         | "raw", NoProtocol ->
             serve_raw_to_raw common_options size
-        | "raw", Nbd ->
+        | "raw", Nbd _ ->
             serve_nbd_to_raw common_options size
         | "raw", Chunked ->
             serve_chunked_to_raw common_options

commit c07e5b573da5c26a7a6df27ede4756e46a5524a3

CP-53951: Drop SSL and Lwt dependency from XAPI
XAPI dependended on vhd_lib, which depends on ssl and lwt.
XAPI is not yet ready for Lwt (and mixing Lwt with blocking code is not a good idea).

The library was only used to gain access to 1 variant used for parameter passing inside XAPI.
Replace this with a polymorphic variant, and drop the dependency.
Also allows to drop a match case that was dead code (proto was only ever set to NBD).

Fixes: 044dc15ee4 ("CP-45016: Add support for specifying nbd export in sparse_dd")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xapi/check-no-lwtssl.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+
+SSL=libssl
+CRYPTO=libcrypto
+# This is an OCaml library, but when it is linked
+# we also configure it to link libev, so look for that
+LWT=libev
+DEPS="${SSL}|${CRYPTO}|${LWT}"
+
+ldd "$1" | grep -q -E "${DEPS}" 2>&1
+if [ $? -eq 1 ]; then
+	echo -e "\n\033[32;1m[OK]\033[0m $1 does not depend on ${DEPS}";
+	exit 0
+else
+	echo -e "\n\033[31;1m[ERROR]\033[0m $1 depends on ${DEPS}";
+	exit 1
+fi

ocaml/xapi/dune
@@ -189,7 +189,6 @@
   uri
   uuid
   uuidm
-  vhd_lib
   x509
   xapi_aux
   xapi-backtrace
@@ -331,3 +330,11 @@
   xapi-idl
   xapi-log
   xapi-stdext-unix))
+
+(rule
+ (alias runtest)
+ (package xapi)
+ (deps
+  (:x xapi_main.exe))
+ (action
+  (run ./check-no-lwtssl.sh %{x})))

ocaml/xapi/sparse_dd_wrapper.ml
@@ -92,10 +92,8 @@ let dd_internal progress_cb base prezeroed verify_cert ?(proto = None) infile
                 match proto with
                 | None ->
                     []
-                | Some (StreamCommon.Nbd export) ->
+                | Some (`NBD export) ->
                     ["-dest-proto"; "nbd"; "-nbd-export"; export]
-                | Some p ->
-                    ["-dest-proto"; StreamCommon.string_of_protocol p]
               in
               let verify_args =
                 match verify_cert with

ocaml/xapi/storage_migrate.ml
@@ -572,7 +572,7 @@ module MigrateLocal = struct
                 None
             | uri :: _ ->
                 let _socket, export = Storage_interface.parse_nbd_uri uri in
-                Some (StreamCommon.Nbd export)
+                Some (`NBD export)
           in
           Remote.VDI.activate3 dbg remote_dp dest dest_vdi vm ;
           with_activated_disk ~dbg ~sr ~vdi:base_vdi ~dp:base_dp ~vm