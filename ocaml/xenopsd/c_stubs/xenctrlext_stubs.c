8e0b25372738c0c1736a8537dd26406c7a5a1906
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2025-03-19 14:29:19 +0000

    xenctrlext: do not truncate the amount of memory in claims to 32 bits
    Int_val truncates values to a 32-bit int. Instead use Long_val, which does not
    suffer from this.
    
    This is a problem when claiming more than ≈ 9706GiBs for a domain.
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- ocaml/xenopsd/c_stubs/xenctrlext_stubs.c
+++ ocaml/xenopsd/c_stubs/xenctrlext_stubs.c
@@ -678,7 +678,7 @@ CAMLprim value stub_xenctrlext_domain_claim_pages(value xch_val, value domid_val
         int retval, the_errno;
         xc_interface* xch = xch_of_val(xch_val);
         uint32_t domid = Int_val(domid_val);
-        unsigned long nr_pages = Int_val(nr_pages_val);
+        unsigned long nr_pages = Long_val(nr_pages_val);
 
         caml_release_runtime_system();
         retval = xc_domain_claim_pages(xch, domid, nr_pages);
