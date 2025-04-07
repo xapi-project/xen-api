31c2b3c5d46e52ed6af9b8263b2b42bd32650fbc
Gang Ji <gang.ji@cloud.com>
2025-04-07 10:44:47 +0800

    Fixup
    Useless here, the local DB will be dropped soon as the joinner will
    switch to the remote DB of the new coordinator.
    
    And latest_synced_updates_applied will be set to `unknown in host.create
    in remote DB as default value.
    
    Signed-off-by: Gang Ji <gang.ji@cloud.com>

--- ocaml/xapi/xapi_pool.ml
+++ ocaml/xapi/xapi_pool.ml
@@ -1688,8 +1688,6 @@ let join_common ~__context ~master_address ~master_username ~master_password
             "Unable to set the write the new pool certificates to the disk : %s"
             (ExnHelper.string_of_exn e)
       ) ;
-      Db.Host.set_latest_synced_updates_applied ~__context ~self:me
-        ~value:`unknown ;
       (* this is where we try and sync up as much state as we can
          with the master. This is "best effort" rather than
          critical; if we fail part way through this then we carry
