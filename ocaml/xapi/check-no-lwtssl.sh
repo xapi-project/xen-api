xapi-project/xen-api

commit 0832ae5171113c3792e5bd327673e400f971c3ac

CP-53951: Drop SSL and Lwt dependency from XAPI
XAPI dependended on vhd_lib, which depends on ssl and lwt.
XAPI is not yet ready for Lwt (and mixing Lwt with blocking code is not a good idea).

The library was only used to gain access to 1 variant used for parameter passing inside XAPI.
Replace this with a polymorphic variant, and drop the dependency.
Also allows to drop a match case that was dead code (proto was only ever set to NBD).

Fixes: 044dc15ee4 ("CP-45016: Add support for specifying nbd export in sparse_dd")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xapi/check-no-lwtssl.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+
+SSL=libssl
+CRYPTO=libcrypto
+# This is an OCaml library, but when it is linked
+# we also configure it to link libev, so look for that
+LWT=libev
+DEPS="${SSL}|${CRYPTO}|${LWT}"
+
+ldd "$1" | grep -q -E "${DEPS}" 2>&1
+if [ $? -eq 1 ]; then
+	echo -e "\n\033[32;1m[OK]\033[0m $1 does not depend on ${DEPS}";
+	exit 0
+else
+	echo -e "\n\033[31;1m[ERROR]\033[0m $1 depends on ${DEPS}";
+	exit 1
+fi

ocaml/xapi/dune
@@ -189,7 +189,6 @@
   uri
   uuid
   uuidm
-  vhd_lib
   x509
   xapi_aux
   xapi-backtrace
@@ -332,3 +331,11 @@
   xapi-idl
   xapi-log
   xapi-stdext-unix))
+
+(rule
+ (alias runtest)
+ (package xapi)
+ (deps
+  (:x xapi_main.exe))
+ (action
+  (run ./check-no-lwtssl.sh %{x})))

ocaml/xapi/sparse_dd_wrapper.ml
@@ -92,10 +92,8 @@ let dd_internal progress_cb base prezeroed verify_cert ?(proto = None) infile
                 match proto with
                 | None ->
                     []
-                | Some (StreamCommon.Nbd export) ->
+                | Some (`NBD export) ->
                     ["-dest-proto"; "nbd"; "-nbd-export"; export]
-                | Some p ->
-                    ["-dest-proto"; StreamCommon.string_of_protocol p]
               in
               let verify_args =
                 match verify_cert with

ocaml/xapi/storage_migrate.ml
@@ -256,7 +256,7 @@ module MigrateLocal = struct
                 None
             | uri :: _ ->
                 let _socket, export = Storage_interface.parse_nbd_uri uri in
-                Some (StreamCommon.Nbd export)
+                Some (`NBD export)
           in
           Remote.VDI.activate3 dbg remote_dp dest dest_vdi vm ;
           with_activated_disk ~dbg ~sr ~vdi:base_vdi ~dp:base_dp ~vm

commit be3b059e29d4486718675afe0641c592415a82a3

CI: allow XAPI linking with Lwt for now
It sneaks in via xenstore_transport and tar.unix, fixing that requires
patching those 2 libraries first.
For now partially revert this test: only test for linking with OpenSSL,
not Lwt.

Fixes: 0832ae517 ("CP-53951: Drop SSL and Lwt dependency from XAPI")
Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ocaml/xapi/check-no-lwtssl.sh
@@ -2,10 +2,7 @@
 
 SSL=libssl
 CRYPTO=libcrypto
-# This is an OCaml library, but when it is linked
-# we also configure it to link libev, so look for that
-LWT=libev
-DEPS="${SSL}|${CRYPTO}|${LWT}"
+DEPS="${SSL}|${CRYPTO}"
 
 ldd "$1" | grep -q -E "${DEPS}" 2>&1
 if [ $? -eq 1 ]; then