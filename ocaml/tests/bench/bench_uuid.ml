5b06182ea887a4d2506db1be499b6fe8219e75cc
Edwin Török <edwin.torok@cloud.com>
2025-04-28 17:22:36 +0100

    CP-54828: import latest benchmark CLI from ocaml-rpc
    Better progress indication, CLI tunables, and the ability to dump raw data to a directory,
    that can be used by `ministat`.
    
    Example usage:
    
    ```
    mkdir /tmp/pool
    dune exec ./bench_pool_field.exe --profile=release -- -d /tmp/pool --quota 20
    ...
    dune exec ./bench_pool_field.exe --profile=release -- -d /tmp/pool --quota 20
    for i in 'Db.Pool.get_all_records' 'Rpc.t -> pool_t' 'pool_t -> Rpc.t'; do ~/git/ministat/ministat -s "/tmp/pool/${OLD}/${i}.dat" "/tmp/pool/${NEW}/$i.dat" -c 99.5; done
    ```
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- ocaml/tests/bench/bench_uuid.ml
+++ ocaml/tests/bench/bench_uuid.ml
@@ -1,10 +1,9 @@
 open Bechamel
 
 let benchmarks =
-  Test.make_grouped ~name:"uuidx creation"
-    [
-      Test.make ~name:"Uuidx.make_uuid_urnd" (Staged.stage Uuidx.make_uuid_urnd)
-    ; Test.make ~name:"Uuidx.make" (Staged.stage Uuidx.make)
-    ]
+  [
+    Test.make ~name:"Uuidx.make_uuid_urnd" (Staged.stage Uuidx.make_uuid_urnd)
+  ; Test.make ~name:"Uuidx.make" (Staged.stage Uuidx.make)
+  ]
 
 let () = Bechamel_simple_cli.cli benchmarks
