3be8ea9c867fad477e3ecc7febeeb30c3c3e8cfa
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2025-05-15 17:10:21 +0100

    CP-53642: change default NUMA placement policy to best-effort
    We've seen that using the policy can be up to 10% faster than using any is some
    workflows, while not observing workflows that were negatively affected. The
    policy per VM can always be change if need be.
    
    Note that currently sometime the best-effort falls back to the same behaviour,
    especially when restarting on starting more than one VM at a time. This needs
    xen patches to be fixed:
    https://lore.kernel.org/xen-devel/20250314172502.53498-1-alejandro.vallejo@cloud.com/T/#ma1246e352ea3cce71c7ddc26d1329a368548b3b2
    
    Now the deprecated numa-placement configuration option for xenopsd does nothing.
    It was exclusively used to enable Best_effort, since now it's the default,
    there's no point in setting the option. It's value depends on whether the
    default option is best_effort or not, as per the spec.
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- ocaml/xenopsd/lib/xenopsd.ml
+++ ocaml/xenopsd/lib/xenopsd.ml
@@ -59,8 +59,6 @@ let feature_flags_path = ref "/etc/xenserver/features.d"
 
 let pvinpvh_xen_cmdline = ref "pv-shim console=xen"
 
-let numa_placement_compat = ref false
-
 (* O(N^2) operations, until we get a xenstore cache, so use a small number here *)
 let vm_guest_agent_xenstore_quota = ref 128
 
@@ -242,8 +240,11 @@ let options =
     , "Command line for the inner-xen for PV-in-PVH guests"
     )
   ; ( "numa-placement"
-    , Arg.Bool (fun x -> numa_placement_compat := x)
-    , (fun () -> string_of_bool !numa_placement_compat)
+    , Arg.Bool (fun _ -> ())
+    , (fun () ->
+        string_of_bool
+          (!Xenops_server.default_numa_affinity_policy = Best_effort)
+      )
     , "NUMA-aware placement of VMs (deprecated, use XAPI setting)"
     )
   ; ( "pci-quarantine"
