50e0452b5034882efe32cacb095883bd8013316e
Christian Lindig <christian.lindig@cloud.com>
2025-06-11 14:31:42 +0100

    CP-308455 VM.sysprep implement mkdtemp
    We want to create a temporary directory that will be used to hold files
    for creating an ISO. There is no existing function that creates all
    necessary directories in a predicatble way.
    
    Signed-off-by: Christian Lindig <christian.lindig@cloud.com>

--- ocaml/xapi/vm_sysprep.ml
+++ ocaml/xapi/vm_sysprep.ml
@@ -21,41 +21,68 @@ let ( // ) = Filename.concat
 
 let finally = Xapi_stdext_pervasives.Pervasiveext.finally
 
-let tmp_dir = Filename.get_temp_dir_name ()
+let temp_dir = Filename.get_temp_dir_name ()
 
 let sr_dir = "/opt/opt/iso"
 
 let genisoimage = "/usr/bin/genisoimage"
+
+let failwith_fmt fmt = Printf.ksprintf failwith fmt
+
+let prng = Random.State.make_self_init ()
+
+let temp_name prefix suffix =
+  let rnd = Random.State.bits prng land 0xFFFFFF in
+  Printf.sprintf "%s%06x%s" prefix rnd suffix
+
+(** [mkdtmp] creates a directory in [dir] and returns its path. If [dir]
+    does not yet exist it is created. It is a an error if [dir] is not a
+    directory. *)
+let mkdtemp ?(dir = temp_dir) ?(perms = 0o700) prefix suffix =
+  ( match Sys.file_exists dir with
+  | true when not (Sys.is_directory dir) ->
+      failwith_fmt "s: %s is not a directory" __FUNCTION__ dir
+  | true ->
+      ()
+  | false ->
+      Unixext.mkdir_rec dir perms
+  ) ;
+  let rec loop = function
+    | n when n >= 20 ->
+        failwith_fmt "s: can't create directory in %s" __FUNCTION__ dir
+    | n -> (
+        let path = Filename.concat dir (temp_name prefix suffix) in
+        try Sys.mkdir path perms ; path with Sys_error _ -> loop (n + 1)
+      )
+  in
+  loop 0
+
+(** Crteate a temporary directory, and pass its path to [f]. Once [f]
+    returns the directory is removed again *)
+let with_temp_dir ?(dir = temp_dir) ?(perms = 0o700) prefix suffix f =
+  let dir = mkdtemp ~dir ~perms prefix suffix in
+  finally (fun () -> f dir) (fun () -> Unixext.rm_rec dir)
 
 (** name of the ISO we will use for a VMi; this is not a path *)
 let iso_name ~vm_uuid =
   let now = Ptime_clock.now () |> Ptime.to_rfc3339 in
   Printf.sprintf "config-%s-%s.iso" vm_uuid now
 
-(** taken from OCaml 5 stdlib *)
-let temp_dir ?(dir = tmp_dir) ?(perms = 0o700) prefix suffix =
-  let rec try_name counter =
-    let name = Filename.temp_file ~temp_dir:dir prefix suffix in
-    try Sys.mkdir name perms ; name
-    with Sys_error _ as e ->
-      if counter >= 20 then raise e else try_name (counter + 1)
-  in
-  try_name 0
-
-(** Crteate a temporary directory, and pass its path to [f]. Once [f]
-    returns the directory is removed again *)
-let with_temp_dir ?(dir = tmp_dir) ?(perms = 0o700) prefix suffix f =
-  let dir = temp_dir ~dir ~perms prefix suffix in
-  finally (fun () -> f dir) (fun () -> Unixext.rm_rec dir)
-
 let make_iso ~vm_uuid ~unattend =
   try
     let _iso = sr_dir // iso_name ~vm_uuid in
-    Xapi_stdext_unix.Unixext.mkdir_rec sr_dir 0o755
-    (* Unixext.write_string_to_file path unattend *)
+    Xapi_stdext_unix.Unixext.mkdir_rec sr_dir 0o755 ;
+    with_temp_dir ~dir:"/var/tmp/xapi" "sysprep-" "-iso" @@ fun temp_dir ->
+    debug "%s: %s = %b" __FUNCTION__ temp_dir (Sys.file_exists temp_dir) ;
+    let path = temp_dir // "unattend.xml" in
+    Unixext.write_string_to_file path unattend ;
+    debug "%s: written to %s" __FUNCTION__ path
   with e ->
     let msg = Printexc.to_string e in
     Helpers.internal_error "%s failed: %s" __FUNCTION__ msg
 
 (* This function is executed on the host where [vm] is running *)
-let sysprep ~__context ~vm ~unattend = debug "%s" __FUNCTION__
+let sysprep ~__context ~vm ~unattend =
+  debug "%s" __FUNCTION__ ;
+  let vm_uuid = Db.VM.get_uuid ~__context ~self:vm in
+  make_iso ~vm_uuid ~unattend
