57cbad4fc5f32ecb2aad5caba209713f3c95851a
Steven Woods <steven.woods@cloud.com>
2025-06-25 21:46:39 +0100

    Move common retry_econnrefused function to xcp_client
    Signed-off-by: Steven Woods <steven.woods@cloud.com>

--- ocaml/xapi-idl/storage/storage_client.ml
+++ ocaml/xapi-idl/storage/storage_client.ml
@@ -13,25 +13,11 @@
  *)
 
 open Storage_interface
-open Xcp_client
 
-let rec retry_econnrefused f =
-  try f () with
-  | Unix.Unix_error (Unix.ECONNREFUSED, "connect", _) ->
-      (* debug "Caught ECONNREFUSED; retrying in 5s"; *)
-      Thread.delay 5. ; retry_econnrefused f
-  | e ->
-      (* error "Caught %s: does the storage service need restarting?"
-         (Printexc.to_string e); *)
-      raise e
-
-module Client = Storage_interface.StorageAPI (Idl.Exn.GenClient (struct
+module Client = StorageAPI (Idl.Exn.GenClient (struct
   let rpc call =
-    retry_econnrefused (fun () ->
-        if !use_switch then
-          json_switch_rpc !queue_name call
-        else
-          xml_http_rpc ~srcstr:(get_user_agent ()) ~dststr:"storage"
-            Storage_interface.uri call
+    Xcp_client.(
+      retry_and_switch_rpc call ~use_switch:!use_switch ~queue_name:!queue_name
+        ~dststr:"storage" ~uri
     )
 end))
