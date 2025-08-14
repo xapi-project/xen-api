7461f8ab75309930f8c9c22e181e343e14dd724e
Guillaume <guillaume.thouvenin@vates.tech>
2025-08-14 16:28:32 +0200

    Allow selection of image format during migration
    This patch allows specifying the destination format for individual VDIs
    mapped to a destination SR. It adds a new parameter to `VM.migrate_send`
    and `VM.assert_can_migrate` API. It also adds a new parameter to XE CLI.
    The format to specify the image format is `image-format:<source VDI
    UUID>=<destination image format>`. If the given image format cannot be
    validated, an error is returned.
    
    It also adds a new parameter to `VDI.pool-migrate`. This new parameter
    allows to provide a string that is the destination format. This string is
    used to check whether the destination SR supports the expected format. If
    the check fails or cannot be performed due to missing information on the
    destination SR, an error is returned.
    
    Signed-off-by: Guillaume <guillaume.thouvenin@vates.tech>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -5443,6 +5443,10 @@ module VDI = struct
         [
           (Ref _vdi, "vdi", "The VDI to migrate")
         ; (Ref _sr, "sr", "The destination SR")
+        ; ( String
+          , "dest_img_format"
+          , "The image format to use on destination SR"
+          )
         ; (Map (String, String), "options", "Other parameters")
         ]
       ~result:(Ref _vdi, "The new reference of the migrated VDI.")
