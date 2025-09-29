b7a44cb11973d691bd5ddc6621fa2fc441b2eb7c
Changlei Li <changlei.li@cloud.com>
2025-09-29 13:59:18 +0800

    CP-54471 Add host.ntp_mode and host.ntp_custom_servers
    Signed-off-by: Changlei Li <changlei.li@cloud.com>

--- ocaml/idl/datamodel_host.ml
+++ ocaml/idl/datamodel_host.ml
@@ -2544,6 +2544,37 @@ let set_max_cstate =
       ]
     ~allowed_roles:_R_POOL_OP ()
 
+let host_ntp_mode =
+  Enum
+    ( "host_ntp_mode"
+    , [
+        ("ntp_mode_dhcp", "Using NTP servers assigned by DHCP to sync time")
+      ; ( "ntp_mode_custom"
+        , "Using custom NTP servers configured by user to sync time"
+        )
+      ; ("ntp_mode_default", "Using default NTP servers to sync time")
+      ]
+    )
+
+let set_ntp_mode =
+  call ~name:"set_ntp_mode" ~lifecycle:[] ~doc:"Set the NTP mode for the host"
+    ~params:
+      [
+        (Ref _host, "self", "The host")
+      ; (host_ntp_mode, "value", "The NTP mode to set")
+      ]
+    ~allowed_roles:_R_POOL_OP ()
+
+let set_ntp_custom_servers =
+  call ~name:"set_ntp_custom_servers" ~lifecycle:[]
+    ~doc:"Set the custom NTP servers for the host"
+    ~params:
+      [
+        (Ref _host, "self", "The host")
+      ; (Set String, "value", "The set of custom NTP servers to configure")
+      ]
+    ~allowed_roles:_R_POOL_OP ()
+
 (** Hosts *)
 let t =
   create_obj ~in_db:true
@@ -2689,6 +2720,8 @@ let t =
       ; set_console_idle_timeout
       ; set_ssh_auto_mode
       ; set_max_cstate
+      ; set_ntp_mode
+      ; set_ntp_custom_servers
       ]
     ~contents:
       ([
@@ -3156,6 +3189,13 @@ let t =
         ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:Bool
             ~default_value:(Some (VBool false)) "secure_boot"
             "Whether the host has booted in secure boot mode"
+        ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:host_ntp_mode
+            ~default_value:(Some (VEnum "ntp_mode_dhcp")) "ntp_mode"
+            "Indicates NTP servers are assigned by DHCP, or configured by \
+             user, or the default servers"
+        ; field ~qualifier:DynamicRO ~lifecycle:[] ~ty:(Set String)
+            ~default_value:(Some (VSet [])) "ntp_custom_servers"
+            "The set of NTP servers configured for the host"
         ]
       )
     ()
--- ocaml/xapi-cli-server/records.ml
+++ ocaml/xapi-cli-server/records.ml
@@ -3402,6 +3402,27 @@ let host_record rpc session_id host =
       ; make_field ~name:"secure-boot"
           ~get:(fun () -> string_of_bool (x ()).API.host_secure_boot)
           ()
+      ; make_field ~name:"ntp-mode"
+          ~get:(fun () ->
+            Record_util.host_ntp_mode_to_string (x ()).API.host_ntp_mode
+          )
+          ~set:(fun value ->
+            Client.Host.set_ntp_mode ~rpc ~session_id ~self:host
+              ~value:(Record_util.host_ntp_mode_of_string value)
+          )
+          ()
+      ; make_field ~name:"ntp-custom-servers"
+          ~get:(fun () -> concat_with_comma (x ()).API.host_ntp_custom_servers)
+          ~get_set:(fun () -> (x ()).API.host_ntp_custom_servers)
+          ~set:(fun value ->
+            Client.Host.set_ntp_custom_servers ~rpc ~session_id ~self:host
+              ~value:
+                (String.split_on_char ',' value
+                |> List.map String.trim
+                |> List.filter (( <> ) "")
+                )
+          )
+          ()
       ]
   }
 
