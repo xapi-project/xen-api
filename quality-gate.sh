b48e85e533baf494a1a28f2cf01752d18b548b45
Pau Ruiz Safont <pau.safont@vates.tech>
2025-10-16 11:10:49 +0100

    xapi: remove some usages of common, exception-raising functions
    The Not_found and hd exceptions keep popping up, and it's difficult to
    find them when there are no backtraces logged.
    Remove usages if them, even if they are not problematic so the actual
    problematic ones can be flushed out over time.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@vates.tech>

--- quality-gate.sh
+++ quality-gate.sh
@@ -3,7 +3,7 @@
 set -e
 
 list-hd () {
-  N=274
+  N=268
   LIST_HD=$(git grep -r --count 'List.hd' -- **/*.ml | cut -d ':' -f 2 | paste -sd+ - | bc)
   if [ "$LIST_HD" -eq "$N" ]; then
     echo "OK counted $LIST_HD List.hd usages"
