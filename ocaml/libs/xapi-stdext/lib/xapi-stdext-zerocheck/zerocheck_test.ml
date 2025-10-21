ca8821f59b61588166fa4afb0bfd0d407502d0a2
Pau Ruiz Safont <pau.safont@vates.tech>
2025-10-21 17:15:40 +0100

    zerocheck: fix check for strings longer than 2 GiBs
    The issue was that the 61-bit ocaml value was being truncated to 32
    bits.
    
    Also make the pointer to the string const to prevent changing the
    pointer to the string.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@vates.tech>

--- ocaml/libs/xapi-stdext/lib/xapi-stdext-zerocheck/zerocheck_test.ml
+++ ocaml/libs/xapi-stdext/lib/xapi-stdext-zerocheck/zerocheck_test.ml
@@ -13,27 +13,31 @@
 
 module Zerocheck = Xapi_stdext_zerocheck.Zerocheck
 
-let big_non_zero =
-  let spec = [("2 GiBs", 2_147_483_647); ("2 GiBs + 1", 2147483647 + 1)] in
-  let test size () =
-    let non_zeroes = Bytes.make size '\x00' in
-    Bytes.set non_zeroes (size - 1) '\x01' ;
-    let non_zeroes = Bytes.unsafe_to_string non_zeroes in
-    let expected = true in
-    let actual = not (Zerocheck.is_all_zeros non_zeroes size) in
-    Alcotest.(check bool) "The last is not zero" expected actual
-  in
-  let tests = List.map (fun (name, size) -> (name, `Quick, test size)) spec in
-  ("Non-zeroes", tests)
+module Str = struct
+  let big_non_zero =
+    let spec = [("2 GiBs", 2_147_483_647); ("2 GiBs + 1", 2147483647 + 1)] in
+    let test size () =
+      let non_zeroes = Bytes.make size '\x00' in
+      Bytes.set non_zeroes (size - 1) '\x01' ;
+      let non_zeroes = Bytes.unsafe_to_string non_zeroes in
+      let expected = true in
+      let actual = not (Zerocheck.is_all_zeros non_zeroes size) in
+      Alcotest.(check bool) "The last byte is not zero" expected actual
+    in
+    List.map (fun (name, size) -> (name, `Quick, test size)) spec
 
-let big_zero =
-  let test () =
-    let size = 2147483647 + 1 in
-    let zeroes = String.make size '\x00' in
-    let expected = true in
-    let actual = Zerocheck.is_all_zeros zeroes size in
-    Alcotest.(check bool) "All are zeroes" expected actual
-  in
-  ("All zeroes", [("2 GiBs + 1", `Quick, test)])
+  let big_zero =
+    let test () =
+      let size = 2147483647 + 1 in
+      let zeroes = String.make size '\x00' in
+      let expected = true in
+      let actual = Zerocheck.is_all_zeros zeroes size in
+      Alcotest.(check bool) "All bytes are zero" expected actual
+    in
+    [("2 GiBs + 1", `Quick, test)]
 
-let () = Alcotest.run "Zerocheck" [big_zero; big_non_zero]
+  let tests =
+    [("String: Not all zeroes", big_non_zero); ("String: all zeroes", big_zero)]
+end
+
+let () = Alcotest.run "Zerocheck" Str.tests
