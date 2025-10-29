a5163593ba6e6d8f8c854e6644a089b546cb549b
Christian Lindig <christian.lindig@citrix.com>
2025-10-29 15:43:42 +0000

    xenopsd/xc: adapt claim_pages to new single numa node version (CP-53658)
    Now the numa node needs to be passed. A special value of (~0U) is used to
    signify that no node is meant to be used. Since this is arch-dependent,
    and contained in a long in x86_64, an int is used to encode the value.
    
    Also remove the exception that was guarding the codepath to use this case
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/libs/xenctrl-ext/xenctrlext.ml
+++ ocaml/libs/xenctrl-ext/xenctrlext.ml
@@ -124,7 +124,6 @@ end
 exception Not_available
 
 let domain_claim_pages handle domid ?(numa_node = NumaNode.none) nr_pages =
-  if numa_node <> NumaNode.none then raise Not_available ;
   stub_domain_claim_pages handle domid numa_node nr_pages
 
 let get_nr_nodes handle =
