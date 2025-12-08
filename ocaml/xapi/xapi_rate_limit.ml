ede7f9f6f5b77453f80a6b9702f5e69d67a50459
Christian Pardillo Laursen <christian.pardillolaursen@citrix.com>
2025-12-08 14:48:40 +0000

    xapi_rate_limit: Replace xapi_globs support with datamodel
    The rate limiting can no longer be set from xapi_globs. Instead, the rate
    limiter is initialised from the database on startup now.
    
    Signed-off-by: Christian Pardillo Laursen <christian.pardillolaursen@citrix.com>

--- ocaml/xapi/xapi_rate_limit.ml
+++ ocaml/xapi/xapi_rate_limit.ml
@@ -56,31 +56,14 @@ let destroy ~__context ~self =
     ~user_agent:record.rate_limit_client_id ;
   Db.Rate_limit.destroy ~__context ~self
 
-let register_xapi_globs () =
-  let configs = !Xapi_globs.rate_limited_clients in
+let register ~__context =
   List.iter
-    (fun s ->
-      match String.split_on_char ':' s with
-      | [user_agent; burst_s; fill_s] -> (
-        match (float_of_string_opt burst_s, float_of_string_opt fill_s) with
-        | Some burst_size, Some fill_rate ->
-            D.debug
-              "Adding user agent %s to bucket table with burst size %f and \
-               fill rate %f"
-              user_agent burst_size fill_rate ;
-            if
-              not
-                (Rate_limit.Bucket_table.add_bucket bucket_table ~user_agent
-                   ~burst_size ~fill_rate
-                )
-            then
-              D.error
-                "Bucket creation failed for user agent %s: invalid fill rate %f"
-                user_agent fill_rate
-        | _ ->
-            D.debug "Skipping invalid numeric values in: %s\n" s
-      )
-      | _ ->
-          D.debug "Skipping invalid item format: %s\n" s
+    (fun (_, bucket) ->
+      ignore
+        (Bucket_table.add_bucket bucket_table
+           ~fill_rate:bucket.API.rate_limit_fill_rate
+           ~user_agent:bucket.API.rate_limit_client_id
+           ~burst_size:bucket.API.rate_limit_burst_size
+        )
     )
-    configs
+    (Db.Rate_limit.get_all_records ~__context)
