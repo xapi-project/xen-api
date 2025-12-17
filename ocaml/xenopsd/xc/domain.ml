xapi-project/xen-api

commit fb66dfc030d3b5c007bacbcaecce75fa0cab96d1

CA-421847: set vcpu affinity if node claim succeeded
Signed-off-by: Marcus Granado <marcus.granado@cloud.com>

ocaml/xenopsd/xc/domain.ml
@@ -967,17 +967,26 @@ let numa_placement domid ~vcpus ~cores ~memory affinity =
             Array.map2 NUMAResource.min_memory (Array.of_seq nodes) a
       in
       numa_resources := Some nodea ;
-      let memory_plan =
+      let cpu_affinity, memory_plan =
         match Softaffinity.plan ~vm host nodea with
         | None ->
             D.debug "NUMA-aware placement failed for domid %d" domid ;
-            []
+            (None, [])
         | Some (cpu_affinity, mem_plan) ->
+            (Some cpu_affinity, mem_plan)
+      in
+      let set_vcpu_affinity = function
+        | None ->
+            D.debug "%s: not setting vcpu affinity for domain %d" __FUNCTION__
+              domid
+        | Some cpu_affinity ->
+            D.debug "%s: setting vcpu affinity for domain %d: %s" __FUNCTION__
+              domid
+              (Fmt.to_to_string CPUSet.pp_dump cpu_affinity) ;
             let cpus = CPUSet.to_mask cpu_affinity in
             for i = 0 to vcpus - 1 do
               set_affinity affinity xcext domid i cpus
-            done ;
-            mem_plan
+            done
       in
       (* Xen only allows a single node when using memory claims, or none at all. *)
       let* numa_node, node =
@@ -994,6 +1003,7 @@ let numa_placement domid ~vcpus ~cores ~memory affinity =
       let nr_pages = Int64.div memory 4096L |> Int64.to_int in
       try
         Xenctrlext.domain_claim_pages xcext domid ~numa_node nr_pages ;
+        set_vcpu_affinity cpu_affinity ;
         Some (node, memory)
       with
       | Xenctrlext.Not_available ->

commit 4437591c45d3aede7cce4440935162941f45672f

CA-422187: fix NUMA on XS8
On XS8 we always raise an exception when attempting to claim from a single
node.
We wanted to only use soft affinity when the single node claim succeeded (which
is the correct fix on XS9, where this API is available).
However this meant that we've effectively completely disabled NUMA support on
XS8, without any way to turn it on.

Always use soft affinity when the single-node claim API is unavailable, this
should keep NUMA working on XS8.

On XS9 Xen itself would never raise ENOSYS (it has a `err = errno = 0` on
ENOSYS).

Fixes: fb66dfc03 ("CA-421847: set vcpu affinity if node claim succeeded")

Signed-off-by: Edwin Török <edwin.torok@citrix.com>

ocaml/xenopsd/xc/domain.ml
@@ -1012,6 +1012,7 @@ let numa_placement domid ~vcpus ~cores ~memory affinity =
           (* Xen does not provide the interface to claim pages from a single NUMA
              node, ignore the error and continue. *)
           D.debug "NUMAClaim domid %d: local claim not available" domid ;
+          set_vcpu_affinity cpu_affinity ;
           None
       | Xenctrlext.Unix_error (errno, _) ->
           D.info