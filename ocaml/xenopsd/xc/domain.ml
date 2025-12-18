xapi-project/xen-api

commit 060d7925017716cdbba73a0c3efe4da2ff24242b

CA-422188: either always use claims or never use claims
Do not mix using claims with not using claims.
Xen cannot currently guarantee that it'll honour a VM's memory claim,
unless all other VMs also use claims.

Global claims have existed since a long time in Xen,
so this should be safe to do on both XS8 and XS9.

Signed-off-by: Edwin Török <edwin.torok@citrix.com>

ocaml/xenopsd/xc/domain.ml
@@ -1002,6 +1002,8 @@ let numa_placement domid ~vcpus ~cores ~memory affinity =
       in
       let nr_pages = Int64.div memory 4096L |> Int64.to_int in
       try
+        D.debug "NUMAClaim domid %d: local claim on node %d: %d pages" domid
+          node nr_pages ;
         Xenctrlext.domain_claim_pages xcext domid ~numa_node nr_pages ;
         set_vcpu_affinity cpu_affinity ;
         Some (node, memory)
@@ -1009,6 +1011,7 @@ let numa_placement domid ~vcpus ~cores ~memory affinity =
       | Xenctrlext.Not_available ->
           (* Xen does not provide the interface to claim pages from a single NUMA
              node, ignore the error and continue. *)
+          D.debug "NUMAClaim domid %d: local claim not available" domid ;
           None
       | Xenctrlext.Unix_error (errno, _) ->
           D.info
@@ -1109,10 +1112,29 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
               and cores =
                 Xenops_server.cores_of_numa_affinity_policy pin ~vcpus
               in
-              numa_placement domid ~vcpus ~cores
-                ~memory:(Int64.mul memory.required_host_free_mib 1048576L)
-                affinity
-              |> Option.map fst
+              let memory = Int64.mul memory.required_host_free_mib 1048576L in
+              match numa_placement domid ~vcpus ~cores ~memory affinity with
+              | None ->
+                  (* Always perform a global claim when NUMA placement is
+                     enabled, and single node claims failed or were
+                     unavailable:
+                     This tries to ensures that memory allocated for this
+                     domain won't use up memory claimed by other domains.
+                     If claims are mixed with non-claims then Xen can't
+                     currently guarantee that it would honour the existing
+                     claims.
+                     A failure here is a hard failure: we'd fail allocating
+                     memory later anyway
+                  *)
+                  let nr_pages = Int64.div memory 4096L |> Int64.to_int in
+                  let xcext = Xenctrlext.get_handle () in
+                  D.debug "NUMAClaim domid %d: global claim: %d pages" domid
+                    nr_pages ;
+                  Xenctrlext.domain_claim_pages xcext domid
+                    ~numa_node:Xenctrlext.NumaNode.none nr_pages ;
+                  None
+              | Some (plan, _) ->
+                  Some plan
         )
   in
   let store_chan, console_chan = create_channels ~xc uuid domid in

commit 95367e1aa6b965f43c5bd30a3de7dd1fac4fce16

CA-422187: safer defaults for global claims
Xen may have already allocated some memory for the domain, and the overhead is
only an estimate.
A global claim failing is a hard failure, so instead use a more conservative
estimate: `memory.build_start_mib`.
This is similar to `required_host_free_mib`, but doesn't take overhead into
account.

Eventually we'd want to have another argument to the create hypercall that
tells it what NUMA node(s) to use, and then we can include all the overhead too
there.

For the single node claim keep the amount as it was, it is only a best effort
claim.

Fixes: 060d79250 ("CA-422188: either always use claims or never use claims")

Signed-off-by: Edwin Török <edwin.torok@citrix.com>

ocaml/xenopsd/xc/domain.ml
@@ -1115,6 +1115,8 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
               in
               let memory =
                 Int64.(mul memory.required_host_free_mib (shift_left 1L 20))
+              and memory_hard =
+                Int64.(mul memory.build_start_mib (shift_left 1L 20))
               in
               match numa_placement domid ~vcpus ~cores ~memory affinity with
               | None ->
@@ -1129,7 +1131,7 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
                      A failure here is a hard failure: we'd fail allocating
                      memory later anyway
                   *)
-                  let nr_pages = Int64.div memory 4096L |> Int64.to_int in
+                  let nr_pages = Int64.div memory_hard 4096L |> Int64.to_int in
                   let xcext = Xenctrlext.get_handle () in
                   D.debug "NUMAClaim domid %d: global claim: %d pages" domid
                     nr_pages ;