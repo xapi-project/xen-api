xapi-project/xen-api

commit 95367e1aa6b965f43c5bd30a3de7dd1fac4fce16

CA-422187: safer defaults for global claims
Xen may have already allocated some memory for the domain, and the overhead is
only an estimate.
A global claim failing is a hard failure, so instead use a more conservative
estimate: `memory.build_start_mib`.
This is similar to `required_host_free_mib`, but doesn't take overhead into
account.

Eventually we'd want to have another argument to the create hypercall that
tells it what NUMA node(s) to use, and then we can include all the overhead too
there.

For the single node claim keep the amount as it was, it is only a best effort
claim.

Fixes: 060d79250 ("CA-422188: either always use claims or never use claims")

Signed-off-by: Edwin Török <edwin.torok@citrix.com>

ocaml/xenopsd/xc/domain.ml
@@ -1115,6 +1115,8 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
               in
               let memory =
                 Int64.(mul memory.required_host_free_mib (shift_left 1L 20))
+              and memory_hard =
+                Int64.(mul memory.build_start_mib (shift_left 1L 20))
               in
               match numa_placement domid ~vcpus ~cores ~memory affinity with
               | None ->
@@ -1129,7 +1131,7 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
                      A failure here is a hard failure: we'd fail allocating
                      memory later anyway
                   *)
-                  let nr_pages = Int64.div memory 4096L |> Int64.to_int in
+                  let nr_pages = Int64.div memory_hard 4096L |> Int64.to_int in
                   let xcext = Xenctrlext.get_handle () in
                   D.debug "NUMAClaim domid %d: global claim: %d pages" domid
                     nr_pages ;

commit 02c6ed1a7e4088b49225a0d730d6077eda8202f3

CA-422187: do not claim shadow_mib, it has already been allocated
When rebooting lots of VMs in parallel we might run out of memory
and fail to boot all the VMs again.
This is because we overestimate the amount of memory required, and claim too
much. That memory is released when the domain build finishes, but when building
domains in parallel it'll temporarily result in an out of memory error.

Instead try to claim only what is left to be allocated: the p2m map and shadow
map have already been allocated by this point.

Fixes: 95367e1aa ("CA-422187: safer defaults for global claims")

Signed-off-by: Edwin Török <edwin.torok@citrix.com>

ocaml/xapi-idl/memory/memory.ml
@@ -192,6 +192,9 @@ module Memory_model (D : MEMORY_MODEL_DATA) = struct
     static_max_mib --- Int64.of_int video_mib +++ D.shim_mib static_max_mib
 
   let build_start_mib static_max_mib target_mib video_mib =
+    D.extra_internal_mib
+    +++ D.extra_external_mib
+    +++
     if D.can_start_ballooned_down then
       target_mib --- Int64.of_int video_mib +++ D.shim_mib target_mib
     else

ocaml/xenopsd/xc/domain.ml
@@ -1114,8 +1114,6 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
                 Xenops_server.cores_of_numa_affinity_policy pin ~vcpus
               in
               let memory =
-                Int64.(mul memory.required_host_free_mib (shift_left 1L 20))
-              and memory_hard =
                 Int64.(mul memory.build_start_mib (shift_left 1L 20))
               in
               match numa_placement domid ~vcpus ~cores ~memory affinity with
@@ -1131,7 +1129,7 @@ let build_pre ~xc ~xs ~vcpus ~memory ~hard_affinity domid =
                      A failure here is a hard failure: we'd fail allocating
                      memory later anyway
                   *)
-                  let nr_pages = Int64.div memory_hard 4096L |> Int64.to_int in
+                  let nr_pages = Int64.div memory 4096L |> Int64.to_int in
                   let xcext = Xenctrlext.get_handle () in
                   D.debug "NUMAClaim domid %d: global claim: %d pages" domid
                     nr_pages ;