ac35717607adf1d0de093f09c905b1b06da05a0b
Ming Lu <ming.lu@cloud.com>
2026-01-07 19:17:25 +0800

    Bump up schema
    Signed-off-by: Ming Lu <ming.lu@cloud.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "d8cb04ccddfd91ca3f0f9074dcf7c219"
+let last_known_schema_hash = "2d9b32747561c7b15924a59e8ea20893"
 
 let current_schema_hash : string =
   let open Datamodel_types in
