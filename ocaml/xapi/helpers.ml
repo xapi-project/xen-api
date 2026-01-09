430c43a80be9ffbc1791457d7622ff1b2f2dc4a2
Lin Liu <lin.liu01@citrix.com>
2026-01-09 09:19:06 +0000

    CP-309972: Configurable between ldap and ldaps during join domain
    Signed-off-by: Lin Liu <lin.liu01@citrix.com>
    (cherry picked from commit 74fa3df227479224bbe1d9b67a1983903e41ba3d)

--- ocaml/xapi/helpers.ml
+++ ocaml/xapi/helpers.ml
@@ -2637,3 +2637,10 @@ let set_map_with_rbac ~__context ~self ~value ~get_fn ~set_fn ~match_protected
         (Rbac.disallowed_permission_exn ~extra_dmsg ~extra_msg ~__context
            ~permission:disallowed ~action
         )
+let ldaps_enabled_in_config ~config =
+  match List.assoc_opt "ldaps" config with
+  (* Default to false, true iff v = true (case-insensitive) *)
+  | Some v when bool_of_string_opt (String.lowercase_ascii v) = Some true ->
+      true
+  | _ ->
+      false
