747333cd54132d84b671d7b84e5ae9eb0b466091
Christian Lindig <christian.lindig@citrix.com>
2026-01-14 10:58:30 +0000

    Update API lifecycle information
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- ocaml/idl/datamodel_lifecycle.ml
+++ ocaml/idl/datamodel_lifecycle.ml
@@ -154,9 +154,9 @@ let prototyped_of_field = function
   | "VM", "actions__after_softreboot" ->
       Some "23.1.0"
   | "pool", "vm_console_idle_timeout" ->
-      Some "26.0.0-next"
+      Some "26.1.0"
   | "pool", "limit_console_sessions" ->
-      Some "26.0.0-next"
+      Some "26.1.0"
   | "pool", "ha_reboot_vm_on_internal_shutdown" ->
       Some "25.16.0"
   | "pool", "license_server" ->
