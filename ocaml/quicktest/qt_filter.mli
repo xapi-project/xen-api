87eabf9ec21ff098f806f9b5a60dbd17290f3d2b
Edwin Török <edwin.torok@citrix.com>
2026-02-10 11:56:30 +0000

    CP-311148: add VM memory quicktest using a memtest ISO
    Signed-off-by: Edwin Török <edwin.torok@citrix.com>

--- ocaml/quicktest/qt_filter.mli
+++ ocaml/quicktest/qt_filter.mli
@@ -49,6 +49,8 @@ module SR : sig
 
   val random : srs -> srs
 
+  val iso_srs : srs
+
   val not_iso : srs -> srs
 
   val is_iso : srs -> srs
@@ -88,3 +90,5 @@ end
 val sr : SR.srs -> (Qt.sr_info -> 'b, 'b) filter
 
 val vm_template : string -> (API.ref_VM -> 'b, 'b) filter
+
+val memtest_iso : ?prefix:string -> (API.vDI_t -> 'a, 'a) filter
