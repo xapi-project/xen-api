9ef93b9dca11311aadead2b4c5ebda52b4f70dfc
Edwin Török <edwin.torok@citrix.com>
2026-03-18 09:13:39 +0000

    [maintenance]: drop obsolete files and remove public_name
    To avoid conflict with xs-opam
    
    Signed-off-by: Edwin Török <edwin.torok@citrix.com>

--- ocaml/libs/backtrace/test/raiser.ml
+++ ocaml/libs/backtrace/test/raiser.ml
@@ -9,25 +9,25 @@ let no_backtraces = ref false
 
 let test_no_backtraces () =
   Printexc.record_backtrace false ;
-  try Test_lib.Log.with_thread_associated "Backtrace lab" foo () with _ -> ()
+  try Backtrace_test_lib.Log.with_thread_associated "Backtrace lab" foo () with _ -> ()
 
 let reraise = ref false
 
 let test_reraise () =
   Printexc.record_backtrace true ;
-  try Test_lib.Log.with_thread_associated "Backtrace lab" bar () with _ -> ()
+  try Backtrace_test_lib.Log.with_thread_associated "Backtrace lab" bar () with _ -> ()
 
 let v1_with_backtrace = ref false
 
 let test_v1_with_backtrace () =
   Printexc.record_backtrace false ;
-  try Test_lib.Log.with_thread_associated_old "Backtrace lab" foo () with _ -> ()
+  try Backtrace_test_lib.Log.with_thread_associated_old "Backtrace lab" foo () with _ -> ()
 
 let raise_again = ref false
 
 let test_raise_again () =
   Printexc.record_backtrace true ;
-  try Test_lib.Log.with_thread_associated "Backtrace lab" baz () with _ -> ()
+  try Backtrace_test_lib.Log.with_thread_associated "Backtrace lab" baz () with _ -> ()
 
 let usage = Printf.sprintf "%s" Sys.argv.(0)
 let speclist = [
@@ -35,7 +35,7 @@ let speclist = [
 ; ("-reraise", Arg.Set reraise, "Test reraise")
 ; ("-v1-with-backtrace", Arg.Set v1_with_backtrace, "Test v1-with-backtrace")
 ; ("-raise-again", Arg.Set raise_again, "Test raise-again")
-] 
+]
 
 let () =
   Arg.parse speclist (Fun.const ()) usage ;
--- ocaml/libs/backtrace/test/reraise.t
+++ ocaml/libs/backtrace/test/reraise.t
@@ -6,10 +6,10 @@
   $ ./raiser.exe -reraise
   Backtrace lab failed with exception Failure("bar")
   Raised Failure("bar")
-  1/4 raiser.exe Raised at file test/raiser.ml, line 1
-  2/4 raiser.exe Called from file test/raiser.ml, line 4
-  3/4 raiser.exe Called from file lib/backtrace.ml, line 251
-  4/4 raiser.exe Called from file test/log.ml, line 55
+  1/4 raiser.exe Raised at file ocaml/libs/backtrace/test/raiser.ml, line 1
+  2/4 raiser.exe Called from file ocaml/libs/backtrace/test/raiser.ml, line 4
+  3/4 raiser.exe Called from file ocaml/libs/backtrace/lib/backtrace.ml, line 251
+  4/4 raiser.exe Called from file ocaml/libs/backtrace/test/log.ml, line 55
   
 
   $ ./raiser.exe -v1-with-backtrace
@@ -20,8 +20,8 @@
   $ ./raiser.exe -raise-again
   Backtrace lab failed with exception Failure("foo")
   Raised Failure("foo")
-  1/4 raiser.exe Raised at file test/raiser.ml, line 1
-  2/4 raiser.exe Called from file test/raiser.ml, line 6
-  3/4 raiser.exe Called from file test/raiser.ml, line 6
-  4/4 raiser.exe Called from file test/log.ml, line 55
+  1/4 raiser.exe Raised at file ocaml/libs/backtrace/test/raiser.ml, line 1
+  2/4 raiser.exe Called from file ocaml/libs/backtrace/test/raiser.ml, line 6
+  3/4 raiser.exe Called from file ocaml/libs/backtrace/test/raiser.ml, line 6
+  4/4 raiser.exe Called from file ocaml/libs/backtrace/test/log.ml, line 55
   
