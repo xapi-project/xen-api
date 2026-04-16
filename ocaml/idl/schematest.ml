3819341d79a4021a4892e9b535f25bc55607dda7
Stephen Cheng <stephen.cheng@citrix.com>
2026-04-16 11:12:38 +0800

    change schema hash
    Signed-off-by: Stephen Cheng <stephen.cheng@citrix.com>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "32bbba07579ca8844fa6162164530268"
+let last_known_schema_hash = "16f819993b7cbb2c8036395d6d872046"
 
 let current_schema_hash : string =
   let open Datamodel_types in
