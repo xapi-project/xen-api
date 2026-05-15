b4357005219653ade8360be9909816d1126831c8
Pau Ruiz Safont <pau.safont@vates.tech>
2026-05-15 15:27:31 +0100

    quicktest: reduce amount of repetition in snapshot tests
    No change in behaviour
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@vates.tech>
    (cherry picked from commit 797bf5ff7b42f0510245118b2ab05cb1b15fd0bf)

--- ocaml/quicktest/quicktest_vm_snapshot.ml
+++ ocaml/quicktest/quicktest_vm_snapshot.ml
@@ -1,125 +1,95 @@
+(* Copyright (C) 2026 Vates.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation; version 2.1 only. with the special
+   exception on linking described in file LICENSE.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+*)
+let ( let@ ) f x = f x
+
+let create_vbd_disk rpc session_id vm vdi n =
+  Client.Client.VBD.create ~rpc ~session_id ~vM:vm ~vDI:vdi ~userdevice:n
+    ~bootable:false ~mode:`RW ~_type:`Disk ~unpluggable:true ~empty:false
+    ~other_config:[] ~qos_algorithm_type:"" ~qos_algorithm_params:[] ~device:""
+    ~currently_attached:true
+
 (** Set up snapshot test: create a small VM with a selection of VBDs *)
 let with_setup rpc session_id sr vm_template f =
   print_endline "Setting up test VM" ;
   let uuid = Client.Client.VM.get_uuid ~rpc ~session_id ~self:vm_template in
   print_endline (Printf.sprintf "Template has uuid: %s%!" uuid) ;
-  let vdi =
-    Client.Client.VDI.create ~rpc ~session_id ~name_label:"small"
-      ~name_description:__LOC__ ~sR:sr
+  let@ vm = Qt.VM.with_new rpc session_id ~template:vm_template ~sr in
+  print_endline (Printf.sprintf "Installed new VM") ;
+  print_endline
+    (Printf.sprintf "Using SR: %s"
+       (Client.Client.SR.get_name_label ~rpc ~session_id ~self:sr)
+    ) ;
+  let@ vdi =
+    Qt.VDI.with_new rpc session_id ~name_label:"small" ~name_description:__LOC__
       ~virtual_size:Int64.(mul (mul 4L 1024L) 1024L)
-      ~_type:`user ~sharable:false ~read_only:false ~other_config:[]
-      ~xenstore_data:[] ~sm_config:[] ~tags:[]
+      sr
   in
-  let vdi2 =
-    Client.Client.VDI.create ~rpc ~session_id ~name_label:"small2"
-      ~name_description:__LOC__ ~sR:sr
+  ignore (create_vbd_disk rpc session_id vm vdi "0") ;
+  let@ vdi2 =
+    Qt.VDI.with_new rpc session_id ~name_label:"small2"
+      ~name_description:__LOC__
       ~virtual_size:Int64.(mul (mul 4L 1024L) 1024L)
-      ~_type:`user ~sharable:false ~read_only:false ~other_config:[]
-      ~xenstore_data:[] ~sm_config:[] ~tags:[]
+      sr
   in
-  Qt.VM.with_new rpc session_id ~template:vm_template (fun vm ->
-      print_endline (Printf.sprintf "Installed new VM") ;
-      print_endline
-        (Printf.sprintf "Using SR: %s"
-           (Client.Client.SR.get_name_label ~rpc ~session_id ~self:sr)
-        ) ;
-      ignore
-        (Client.Client.VBD.create ~rpc ~session_id ~vM:vm ~vDI:vdi
-           ~userdevice:"0" ~bootable:false ~mode:`RW ~_type:`Disk
-           ~unpluggable:true ~empty:false ~other_config:[]
-           ~qos_algorithm_type:"" ~qos_algorithm_params:[] ~device:""
-           ~currently_attached:true
-        ) ;
-      ignore
-        (Client.Client.VBD.create ~rpc ~session_id ~vM:vm ~vDI:vdi2
-           ~userdevice:"1" ~bootable:false ~mode:`RW ~_type:`Disk
-           ~unpluggable:true ~empty:false ~other_config:[]
-           ~qos_algorithm_type:"" ~qos_algorithm_params:[] ~device:""
-           ~currently_attached:true
-        ) ;
-      f rpc session_id vm vdi vdi2 ;
-      Client.Client.VDI.destroy ~rpc ~session_id ~self:vdi ;
-      Client.Client.VDI.destroy ~rpc ~session_id ~self:vdi2
-  )
+  ignore (create_vbd_disk rpc session_id vm vdi2 "1") ;
+  f rpc session_id vm vdi vdi2
+
+let take_snapshot ?(ignore_vdis = []) rpc session_id vm ~origin =
+  Client.Client.VM.snapshot ~rpc ~session_id ~vm
+    ~new_name:(Printf.sprintf "Snapshot:%s" origin)
+    ~ignore_vdis
+
+let is_user_device rpc session_id n vbd =
+  Client.Client.VBD.get_userdevice ~rpc ~session_id ~self:vbd = n
+
+let get_vdi_with_user_device rpc session_id vbds n =
+  let vbd =
+    match List.find_opt (is_user_device rpc session_id n) vbds with
+    | None ->
+        Alcotest.fail (Printf.sprintf "Couldn't find VBD on snapshot %s" n)
+    | Some vbd ->
+        vbd
+  in
+  Client.Client.VBD.get_VDI ~rpc ~session_id ~self:vbd
+
+let get_snapshot_of_vdi rpc session_id vbds n =
+  let snap = get_vdi_with_user_device rpc session_id vbds n in
+  Client.Client.VDI.get_snapshot_of ~rpc ~session_id ~self:snap
+
+let check_vdi_snapshot_of rpc session_id vbds ~vdi n =
+  let snapshot_of = get_snapshot_of_vdi rpc session_id vbds n in
+  assert (snapshot_of = vdi)
 
 let test_snapshot rpc session_id vm vdi vdi2 =
-  let snapshot =
-    Client.Client.VM.snapshot ~rpc ~session_id ~vm ~new_name:"Snapshot"
-      ~ignore_vdis:[]
-  in
+  let snapshot = take_snapshot rpc session_id vm ~origin:__FUNCTION__ in
   let vbds = Client.Client.VM.get_VBDs ~rpc ~session_id ~self:snapshot in
-  let snap_vbd =
-    match
-      List.find_opt
-        (fun vbd ->
-          Client.Client.VBD.get_userdevice ~rpc ~session_id ~self:vbd = "0"
-        )
-        vbds
-    with
-    | None ->
-        Alcotest.fail "Couldn't find VBD on snapshot"
-    | Some vbd ->
-        vbd
-  in
-  let snap_vbd2 =
-    match
-      List.find_opt
-        (fun vbd ->
-          Client.Client.VBD.get_userdevice ~rpc ~session_id ~self:vbd = "1"
-        )
-        vbds
-    with
-    | None ->
-        Alcotest.fail "Couldn't find VBD on snapshot"
-    | Some vbd ->
-        vbd
-  in
-  let snap_vdi = Client.Client.VBD.get_VDI ~rpc ~session_id ~self:snap_vbd in
-  let snap_vdi2 = Client.Client.VBD.get_VDI ~rpc ~session_id ~self:snap_vbd2 in
-  let orig_vdi =
-    Client.Client.VDI.get_snapshot_of ~rpc ~session_id ~self:snap_vdi
-  in
-  let orig_vdi2 =
-    Client.Client.VDI.get_snapshot_of ~rpc ~session_id ~self:snap_vdi2
-  in
-  assert (orig_vdi = vdi) ;
-  assert (orig_vdi2 = vdi2)
+
+  check_vdi_snapshot_of rpc session_id vbds ~vdi "0" ;
+  check_vdi_snapshot_of rpc session_id vbds ~vdi:vdi2 "1"
 
 let test_snapshot_ignore_vdi rpc session_id vm vdi vdi2 =
   let snapshot =
-    Client.Client.VM.snapshot ~rpc ~session_id ~vm ~new_name:"Snapshot"
-      ~ignore_vdis:[vdi2]
+    take_snapshot rpc session_id vm ~origin:__FUNCTION__ ~ignore_vdis:[vdi2]
   in
   let vbds = Client.Client.VM.get_VBDs ~rpc ~session_id ~self:snapshot in
-  let snap_vbd =
-    match
-      List.find_opt
-        (fun vbd ->
-          Client.Client.VBD.get_userdevice ~rpc ~session_id ~self:vbd = "0"
-        )
-        vbds
-    with
-    | None ->
-        Alcotest.fail "Couldn't find VBD on snapshot"
-    | Some vbd ->
-        vbd
+  let has_been_snapshot n =
+    List.exists (is_user_device rpc session_id n) vbds
   in
-  assert (
-    not
-      (List.exists
-         (fun vbd ->
-           Client.Client.VBD.get_userdevice ~rpc ~session_id ~self:vbd = "1"
-         )
-         vbds
-      )
-  ) ;
-  let snap_vdi = Client.Client.VBD.get_VDI ~rpc ~session_id ~self:snap_vbd in
-  let orig_vdi =
-    Client.Client.VDI.get_snapshot_of ~rpc ~session_id ~self:snap_vdi
-  in
-  assert (orig_vdi = vdi)
 
-let test rpc session_id sr_info vm_template () =
+  assert (not (has_been_snapshot "1")) ;
+  check_vdi_snapshot_of rpc session_id vbds ~vdi "0"
+
+let test_snapshots rpc session_id sr_info vm_template () =
   let sr = sr_info.Qt.sr in
   List.iter
     (with_setup rpc session_id sr vm_template)
@@ -127,10 +97,7 @@ let test rpc session_id sr_info vm_template () =
 
 let tests () =
   let open Qt_filter in
-  [
-    [("VM snapshot tests", `Slow, test)]
-    |> conn
-    |> sr SR.(all |> allowed_operations [`vdi_create])
-    |> vm_template Qt.VM.Template.other
-  ]
-  |> List.concat
+  [("VM snapshot tests", `Slow, test_snapshots)]
+  |> conn
+  |> sr SR.(all |> allowed_operations [`vdi_create])
+  |> vm_template Qt.VM.Template.other
