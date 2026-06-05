6b0b2b167eab79eb9f907c06d6db6c6b946faf0a
Mark Syms <mark.syms@citrix.com>
2026-06-05 10:35:45 +0100

    Make use of losetup conditional on file type
    Use non-deprecated list operation instead of -a
    
    Signed-off-by: Mark Syms <mark.syms@citrix.com>

--- ocaml/xapi-storage/python/examples/volume/org.xen.xapi.storage.simple-file/volume.py
+++ ocaml/xapi-storage/python/examples/volume/org.xen.xapi.storage.simple-file/volume.py
@@ -2,13 +2,13 @@
 #
 # Copyright (C) Cloud Software Group, Inc.
 #
-# This program is free software; you can redistribute it and/or modify 
-# it under the terms of the GNU Lesser General Public License as published 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation; version 2.1 only.
 #
-# This program is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
