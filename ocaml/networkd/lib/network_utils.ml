616b58c2a36ba9e9e3bd777351aafe7dbdbbca71
Sebastien Marie <semarie@kapouay.eu.org>
2026-06-19 15:34:30 +0200

    dhclient: add `set_stale` (and remove `remove_conf_file`) in public interface
    In `set_gateway_interface` and `set_dns_interface` we are removing the 
    configuration file to trigger a restart of Dhclient on the next `ensure_running` 
    call.
    
    By setting gateway or DNS interface, it affects the (possibly) running dhclient 
    process and it will need restarting on the old and/or current interface.
    
    If the changes are done in A -> B -> A way, the DHCP configuration will be the same,
    but a restart it still needed.
    
    By marking the configuration as dirty, it ensures that the DHCP client will be restarted.
    
    
    Signed-off-by: Sebastien Marie <semarie@kapouay.eu.org>

--- ocaml/networkd/lib/network_utils.ml
+++ ocaml/networkd/lib/network_utils.ml
@@ -913,7 +913,9 @@ end
 module Dhclient : sig
   type interface = string
 
-  val remove_conf_file : ?ipv6:bool -> interface -> unit
+  val set_stale : ?ipv6:bool -> interface -> unit
+  (** set_stale: mark the DHCP configuration to be stale. Next call of `ensure_running`
+  will necessary trigger a restart. *)
 
   val stop : ?ipv6:bool -> interface -> unit
   (** stop: stop the DHCP client managing [interface] if running and to unconfigure addresses. *)
@@ -1105,6 +1107,13 @@ end = struct
         (* remove the pid file *)
         Unix.unlink (pid_file ~ipv6 interface)
       with _ -> ()
+
+  let set_stale ?(ipv6 = false) interface =
+    (* set the configuration dirty by removing the configuration file.
+     * dhclient will still run nicely, but `ensure_running` will stop/start it
+     * as the configuration will not match the (removed) configuration file.
+     *)
+    remove_conf_file ~ipv6 interface
 
   let ensure_running ?(ipv6 = false) interface options =
     if not (is_running ~ipv6 interface) then
