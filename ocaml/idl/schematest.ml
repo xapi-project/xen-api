186d6ea26e2199ed3d6ec32cdc1db257500b68d6
Lin Liu <Lin.Liu01@cloud.com>
2026-06-30 10:10:53 +0000

    Revert "CP-311971: enable AD extauth cache by default for new pools"
    This reverts commit a4ea921884ae3c104522e9f057c2f8e8fbf284db.

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "247f7730c01d0ded87750158f5aabaa6"
+let last_known_schema_hash = "55b4697e1735492cbdeb63f04fa82fda"
 
 let current_schema_hash : string =
   let open Datamodel_types in
