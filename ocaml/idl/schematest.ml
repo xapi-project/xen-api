e4f2488cdf7b538227d015041f0382e7123492dc
Stephen Cheng <stephen.cheng@citrix.com>
2026-07-08 10:25:16 +0800

    Drop redundant secureboot_certificates_state recompute
    Every VM import operation (VM.import, VM.import_metadata, DR recover,
    cross-pool migrate) creates the VM through create_from_record, which drives
    VM.create -> Xapi_vm.create. That already computes
    secureboot_certificates_state against the importing pool.
    
    Signed-off-by: Stephen Cheng <stephen.cheng@citrix.com>
    (cherry picked from commit 406574e9889611ec7880b95f74a047ce34f29a8d)

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "04b63180cf2880844d1258210ed6e0a5"
+let last_known_schema_hash = "811f0a66a5f69db0fcb2e0378a27d81c"
 
 let current_schema_hash : string =
   let open Datamodel_types in
