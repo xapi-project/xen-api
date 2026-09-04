2f0d10879713e5d12aa1ab32042ad7ab6565e630
Ming Lu <ming.lu@cloud.com>
2026-09-04 16:26:35 +0800

    Revert "Add "hvm-pirq" platform key"
    This reverts commit e9c10d613f0139129dbbf9f2f56fc43f38517f65.
    
    Signed-off-by: Ming Lu <ming.lu@cloud.com>

--- ocaml/xapi/vm_platform.ml
+++ ocaml/xapi/vm_platform.ml
@@ -63,8 +63,6 @@ let nested_virt = "nested-virt"
 
 let vcpu_unrestricted = "vcpu-unrestricted"
 
-let hvm_pirq = "hvm-pirq"
-
 let tpm_version = "tpm_version"
 
 (* The default value of device model should be set as
@@ -108,7 +106,6 @@ let filtered_flags =
   ; featureset
   ; nested_virt
   ; vcpu_unrestricted
-  ; hvm_pirq
   ]
 
 (* Other keys we might want to write to the platform map. *)
